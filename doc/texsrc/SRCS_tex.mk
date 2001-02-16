a2694958dc4fae0a98917ddfc4070048fa98aaaa
Robert Jacob <jacob@mcs.anl.gov>
2001-02-16 22:49:43 +0000

    .tex files are built from the .F90 source in this directory using the Makefile and SRCS_tex.mk.  The resulting .tex files are not under CVS control since they can always be rebuilt from the .F90 files.

---
+++ doc/texsrc/SRCS_tex.mk
@@ -0,0 +1,12 @@
+TEXFILES= 		\
+m_AttrVect.tex          \
+m_GlobalMap.tex         \
+m_GlobalSegMap.tex      \
+m_Accumulator.tex       \
+m_SparseMatrix.tex      \
+m_Navigator.tex         \
+m_AttrVectComms.tex     \
+m_AccumulatorComms.tex  \
+m_GeneralGrid.tex	\
+m_MatAttrVectMul.tex    \
+m_Router.tex
