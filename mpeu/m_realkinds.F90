c514fbe16896014b85645bcd9026d3e4cfc6e49f
Robert Jacob <jacob@mcs.anl.gov>
2004-01-23 20:36:03 +0000

    Add FP kind type.  Set it equal to DP.

--- mpeu/m_realkinds.F90
+++ mpeu/m_realkinds.F90
@@ -21,6 +21,7 @@
       public :: kind_r		! default real
       public :: SP		! default REAL
       public :: DP		! default DOUBLE_PRECISION
+      public :: FP		! general floating point precision
 
       real*4,parameter :: R4=1.
       real*8,parameter :: R8=1.
@@ -29,12 +30,16 @@
       integer,parameter :: SP = kind(1.  )
       integer,parameter :: DP = kind(1.D0)
 
+!     Set the current default floating point precision 
+      integer,parameter :: FP = DP
+
       integer,parameter :: kind_r4=kind(R4)
       integer,parameter :: kind_r8=kind(R8)
       integer,parameter :: kind_r =kind(R )
 
 ! !REVISION HISTORY:
 ! 	19Feb98 - Jing Guo <guo@thunder> - initial prototype/prolog/code
+! 	23Jan03 - R. Jacob <jacob@mcs.anl.gov> - add FP
 !EOP
 !_______________________________________________________________________
   character(len=*),parameter :: myname='m_realkinds'
