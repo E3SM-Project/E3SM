d29d25d19f87e445890cb9ceca730d0ed5b560ec
Ray Loy <rloy@mcs.anl.gov>
2010-10-11 21:55:38 +0000

    Added mpi_op_create

--- mpi-serial/mpi.h
+++ mpi-serial/mpi.h
@@ -231,6 +231,8 @@ extern int MPI_Alltoallv(void *sendbuf, int *sendcounts,
                          void *recvbuf, int *recvcounts,
                          int *rdispls, MPI_Datatype recvtype,
                          MPI_Comm comm) ;
+extern int MPI_Op_create(MPI_User_function *function, int commute, 
+                         MPI_Op *op);
 extern MPI_Op MPI_Op_f2c(MPI_Fint op);
 extern MPI_Fint MPI_Op_c2f(MPI_Op op);
 extern MPI_Comm mpi_comm_new(void);
