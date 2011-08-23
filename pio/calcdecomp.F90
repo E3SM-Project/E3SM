ccad656a0cd60bea7efcfccfa45b5eb40b0d212c
Jim Edwards <edwards.jim@gmail.com>
2011-08-23 14:03:05 +0000

    another new startandcount algorythm

--- pio/calcdecomp.F90
+++ pio/calcdecomp.F90
@@ -17,6 +17,12 @@ module calcdecomp
 
 contains 
 
+!
+!  Determine start and kount values for an array of global size gdims over at most num_io_procs tasks.
+!  The algorythm creates contigous blocks of approximate size stripesize.  Stripesize should be adjusted
+!  to be optimal for the filesystem being used.   The actual number of io tasks used is output in variable
+!  use_io_procs
+!
   subroutine CalcStartandCount(basetype, ndims, gdims, num_io_procs, iorank, start, kount, use_io_procs)
     integer(i4), intent(in) :: ndims, num_io_procs, iorank, basetype
     integer(i4), intent(in) :: gdims(ndims)
@@ -29,8 +35,7 @@ contains
     integer :: minbytes = stripeSize-256   ! minimum number of contigous blocks in bytes to put on a IO task
     integer :: maxbytes = stripeSize+256   ! maximum length of contigous block in bytes to put on a IO task
 
-    integer :: minblocksize, basesize, maxiosize
-
+    integer :: minblocksize, basesize, maxiosize, ioprocs, tiorank
 
     select case(basetype)
     case(PIO_int)
@@ -43,133 +48,59 @@ contains
 
     minblocksize = minbytes/basesize
 
-    p=1
-    do i=1,ndims
-       p=p*gdims(i)
-    end do
+    p=product(gdims)
     use_io_procs = max(1,min(ceiling(real(p)/real(minblocksize)),num_io_procs))
 
     start(:)=1
     kount(:)=0
     if(iorank>=use_io_procs) return 
 
-    dims(:) = 0
+    kount=gdims
 
-    call outerdimfirst(use_io_procs, ndims, gdims,  minblocksize, maxbytes/basesize, dims)
-
-    !    print *,__LINE__,dims
-
-    kount(:)=gdims(:)
-    p=1
-    maxiosize = 1
-    do i=1,ndims
-       p=p*dims(i)
-       if(dims(i)>1) then
-          kount(i)=gdims(i)/dims(i)
-          subrank = (p*iorank+1)/use_io_procs
-
-          start(i)= mod(kount(i)*subrank ,kount(i)*dims(i)) +1
-
-          extras = gdims(i)-kount(i)*dims(i)
-
-          ! We return the maxio size so that buffers for serial netcdf are always big enough
-          !
-          ! We couldnt divide into equally sized domains, we have extras
-          ! So we add those to some of the io tasks
-          !
-	  if(i==4) print *, iorank,extras,subrank,p,use_io_procs           
-          if(extras>0 .and. subrank >= p-extras) then
-             start(i)=start(i)+ (subrank+extras-p) 
-             kount(i)=kount(i)+1
+    ioprocs=use_io_procs
+    tiorank=iorank
+    do i=ndims,1,-1
+       if(gdims(i)>1) then
+          if(gdims(i)>=ioprocs) then
+             call computestartandcount(gdims(i),ioprocs,tiorank,start(i),kount(i))
+             exit  ! Decomposition is complete
+          else
+             ! The current dimension cannot complete the decomposition.   Decompose this 
+             ! dimension in groups then go on to decompose the next dimesion in each of those
+             ! groups.
+             call computestartandcount(gdims(i),gdims(i),gdims(i)*iorank/ioprocs  , start(i),kount(i))
+             ioprocs=ioprocs/gdims(i)
+             tiorank=mod(iorank,ioprocs)
           end if
-          
-          if(mod(kount(i)*(p*(iorank+1)/use_io_procs),kount(i)*dims(i))+1<start(i)) then
-             kount(i) = gdims(i)-start(i)+1
-          end if
-               
-
        end if
     end do
-
-    
 
   end subroutine Calcstartandcount
 
-  subroutine outerdimfirst(iocnt,ndims,extent,minblocksize,maxblocksize,dims)
+!
+! Compute start and kount values to distribute gdim over ioprocs
+! as evenly as possible.  gdim must be >= ioprocs
+!
+!
+  subroutine computestartandcount(gdim,ioprocs,rank,start,kount)
     implicit none
-    integer, intent(in) :: iocnt,ndims
-    integer, intent(in) :: extent(ndims),minblocksize,maxblocksize
-    integer, intent(out) :: dims(ndims)
+    integer,intent(in) :: gdim,ioprocs,rank
+    integer(kind=pio_offset),intent(out) :: start,kount
+    integer :: remainder
 
-    integer :: sub(0:ndims)
-    integer :: lextent(ndims)
-    integer :: i, p, j(1), k, tsize, myiocnt
-    logical :: done
-
-    tsize = product(extent)
-    dims = 1
-
-    myiocnt=iocnt
-
-    i=ndims
-
-    do while(tsize>maxblocksize .and. i>0)
-       myiocnt=iocnt
-       if(extent(i)>1) then
-          if(extent(i)<myiocnt) then
-             dims(i)=max(2,gcd(extent(i),myiocnt))
-             myiocnt=max(2,myiocnt/dims(i))
-          else
-             dims(i)=iocnt/product(dims(i:ndims))
-          endif
-          tsize=product(extent)/product(dims)
-       end if
-       !        print *,i,tsize,myiocnt,dims(i)
-       i=i-1
-    end do
-
-    !     print *, __LINE__,i,iocnt,myiocnt,tsize,maxblocksize,minblocksize, dims
-    !     stop
-    return
-
-  end subroutine outerdimfirst
-  recursive function gcd( a, b ) result( divisor )
-
-    ! Note that the function itself is not declared when the result
-    ! variable is present.  The type of the function is the type of 
-    ! the result variable.  Thus, only the result variable may be 
-    ! declared. 
-
-    integer divisor
-    integer, intent(in) :: a, b
-
-    integer m, n
-
-    ! Multiple statements may be written on a single source line
-    ! provided they are delimited with semicolons.
-
-    m = abs(a); n = abs(b)
-    if ( m > n ) call swap( m, n )  ! Insure that m <= n.
-
-    ! When the function invokes itself recursively, the result variable
-    ! should be used to store the result of the function.  The function
-    ! name is used to invoke the function.  Thus, the function name should
-    ! not appear on the left-hand side of an assignment statement.
-
-    if ( m == 0 ) then 
-       divisor = n
-    else
-       divisor = gcd( mod( n, m ), m )
+    if(gdim<ioprocs) then
+       stop 'bad arguments'
     end if
 
-  end function gcd
-  subroutine swap( x, y )
-    integer, intent(inout) :: x, y
+    kount = gdim/ioprocs
+    start = kount*rank+1
+    remainder = gdim-kount*ioprocs
+    if(remainder>=ioprocs-rank) then
+       kount=kount+1
+       start=start+max(0,(rank+remainder-ioprocs))
+    end if
+  end subroutine computestartandcount
 
-    integer tmp
-
-    tmp = x; x = y; y = tmp 
-  end subroutine swap
 
 end module calcdecomp
 
@@ -178,29 +109,59 @@ program sandctest
   use calcdecomp
   implicit none
   
-!  integer, parameter :: ndims=4
-!  integer, parameter :: gdims(ndims) = (/144,96,30,3/)
-  integer, parameter :: ndims=3
-  integer, parameter :: gdims(ndims) = (/576,384,1/)
-  integer, parameter :: num_io_procs=16
-
-  integer :: psize, n
-
+  integer, parameter :: ndims=4
+!  integer, parameter :: gdims(ndims) = (/2,64,100,87/)
+!  integer, parameter :: ndims=3
+!  integer, parameter :: gdims(ndims) = (/12,95,97/)
+  integer, parameter :: num_io_procs=17
+  integer :: gdims(ndims)
+  integer :: psize, n, i,j,k,m
+  integer, parameter :: imax=100,jmax=100,kmax=100,mmax=100
   integer(kind=pio_offset) :: start(ndims), count(ndims)
-  integer :: iorank, numaiotasks
-
-  do iorank=0,num_io_procs-1
-     call Calcstartandcount(PIO_double, ndims, gdims, num_io_procs, iorank, start, count, numaiotasks)
-
-     psize=1
-     do n=1,ndims
-        psize=psize*count(n)
+  integer :: iorank, numaiotasks, tpsize
+!#ifdef DOTHIS
+  do i=1,imax
+     gdims(1)=i
+     do j=1,jmax
+        gdims(2)=j
+        do k=1,kmax
+           gdims(3)=k
+           do m=1,mmax
+              gdims(4)=m
+!#endif
+              tpsize = 0
+              numaiotasks=0
+              do while(tpsize==0)
+                 do iorank=0,num_io_procs-1
+                    call Calcstartandcount(PIO_double, ndims, gdims, num_io_procs, iorank, start, count, numaiotasks)
+                    
+                    psize=1
+                    do n=1,ndims
+                       psize=psize*count(n)
+                    end do
+                    tpsize = tpsize+psize
+                    !                 if(ndims==3) then
+!                                        write(*,'(i2,a,3i5,a,3i5,2i12)') iorank,' start =',start,' count=', count, product(gdims), psize
+                    !                 else if(ndims==4) then
+                    if(sum(count)>0) then
+                       write(*,'(i2,a,4i5,a,4i5,2i12)') iorank,' start =',start,' count=', count, product(gdims), psize
+                       if(any(start<0)) then
+                          print *, gdims
+                          stop 
+                       endif
+                    end if
+                    
+                 end do
+                 if(tpsize/=product(gdims)) then
+                    print *,'Failed to converge: ',tpsize,product(gdims),gdims
+                    stop
+                 end if
+              end do
+!#ifdef DOTHIS
+           end do
+        end do
      end do
-     if(ndims==3) then
-        write(*,'(i2,a,3i5,a,3i5,2i12)') iorank,' start =',start,' count=', count, product(gdims), psize
-     else if(ndims==4) then
-        write(*,'(i2,a,4i5,a,4i5,2i12)') iorank,' start =',start,' count=', count, product(gdims), psize
-     end if
   end do
+!#endif
 end program sandctest
 #endif
