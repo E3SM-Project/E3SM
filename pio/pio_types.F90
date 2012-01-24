369425ecb8d310792bc9a52d628914c0d84af6b7
Jim Edwards <edwards.jim@gmail.com>
2012-01-24 14:33:06 +0000

    update changelog

--- pio/pio_types.F90
+++ pio/pio_types.F90
@@ -159,7 +159,7 @@ module pio_types
 	integer(i4)         :: compsize   ! size of expected comp buffer
         integer(i4)         :: maxiobuflen   ! size of largest iobuffer
         integer(i4)         :: ndof
-        integer(14)         :: padding
+        integer(i4)         :: padding
     end type
 
 !>
