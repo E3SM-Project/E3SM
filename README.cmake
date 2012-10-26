aca5f4acefc205baa5328f1f2cd0dab436a64f9a
evanskj@ornl.gov <evanskj@ornl.gov@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2012-10-26 17:09:09 +0000

    update to cmake build instructions
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@2296 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 0652b8a3e8748eef1ef2f0ca9795f70d5e493f3e

--- README.cmake
+++ README.cmake
@@ -1,22 +1,22 @@
-9/2012 CGB and KJE 
+10/2012 CGB and KJE 
 
 HOMME now has a CMake build option for both sweqx and swim.
-It is in the BETA test mode
+It is in the BETA test mode, alert Chris Baker or Kate Evans of problems/unclear instructions
 (swim is the SW version of HOMME that uses trilinos in the implicit solve option)
 
-To use this:
-* right now, either build PIO and timing separately, then comment the build lines out of the build script OR
-* grab the changes needed to build those external libraries using cmake from the swim infrastructure developers
-
-1. mkdir BUILD_DIR somewhere
+1. mkdir BUILD_DIR somewhere, usually the main trunk directory
 2. cp /bld/cmake-script/$APPROPRIATE_BUILD_SCRIPT into $BUILD_DIR 
-(remember, right now comment out the PIO timing cmake build lines)
 3. ./$APPROPRIATE_BUILD_SCRIPT
 4. JAGUAR ONLY: export XTPE_LINK_TYPE='dynamic' needed right now TODO: put into script build process
+4. Linux box ONLY: export ZDIR='/usr/lib64' needed right now TODO: put into script build process
 4. Modify script as appropriate (DEBUG or not etc)
+ a. PLEV=# vertical levels
+ b. NUM_POINTS=np
+ c. -D CMAKE_INSTALL_PREFIX=where /bin/$EXE will sit 
 5. make -j4
-6. make install (if you want the executable installed in /opt/homme/trunk-$DATE)
+6. make install (where you told it to go)
 
-Sometimes you need to do a clean out to get a good build. If you are having problems, 
-try removing all the files (but the build script :)  ) from the build directory and start again.
+most build failures are do to residual build info. In the build directory:
+rm -rf CMakeCache.txt CMakeFiles src utils cmake_install.cmake Makefile bin install_manifest.txt *.h
+to get a fresh build starting point.
 
