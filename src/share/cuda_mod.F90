2ffc305404ff3a4f4c09c567c6d43234a7631236
normanmr@ornl.gov <normanmr@ornl.gov@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2012-12-18 23:59:22 +0000

    More of euler_step's beginning limiter_option == 8 or nu_p > 0 stuff has now been ported over to GPUs using openACC. Data transfers are a little more optimal. Not efficient yet, but getting better. The goal is to not transfer arrays that depend on Q over PCI-e if possible.
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@2480 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: c743dca2543ecbdf4d9a5562bb8b929b4f0c6df4

--- src/share/cuda_mod.F90
+++ src/share/cuda_mod.F90
@@ -530,60 +530,88 @@ contains
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   rhs_viss = 0
   if ( limiter_option == 8 .or. nu_p > 0 ) then
-  do ie = 1 , nelemd
-    dp_h(:,:,:,ie) = elem(ie)%derived%dp(:,:,:)
-    divdp_h(:,:,:,ie) = elem(ie)%derived%divdp_proj(:,:,:) 
-    Qdp_h(:,:,:,:,n0_qdp,ie) = elem(ie)%state%Qdp(:,:,:,:,n0_qdp)
-  enddo
-!$OMP BARRIER
-!$OMP MASTER
-!$acc data copyin(dp_h,divdp_h,qdp_h) copyout(qtens_biharmonic) present_or_create(dp)
-!$acc kernels
-    !$acc loop private(k,q,ie)
     do ie = 1 , nelemd
-      ! add hyperviscosity to RHS.  apply to Q at timelevel n0, Qdp(n0)/dp
-      do k = 1 , nlev    !  Loop index added with implicit inversion (AAM)
-        dp_h(:,:,k,ie) = dp_h(:,:,k,ie) - rhs_multiplier*dt*divdp_h(:,:,k,ie)
-        do q = 1 , qsize
-          Qtens_biharmonic(:,:,k,q,ie) = Qdp_h(:,:,k,q,n0_qdp,ie)/dp_h(:,:,k,ie)
+      dp_h(:,:,:,ie) = elem(ie)%derived%dp(:,:,:)
+      divdp_h(:,:,:,ie) = elem(ie)%derived%divdp_proj(:,:,:) 
+      Qdp_h(:,:,:,:,n0_qdp,ie) = elem(ie)%state%Qdp(:,:,:,:,n0_qdp)
+    enddo
+!$OMP BARRIER
+    if (hybrid%ithr == 0 ) then
+      !$acc data pcreate(dp,dp_h,divdp_h,qdp_h,qtens_biharmonic,qmin,qmax)
+      !$acc update device(dp_h,divdp_h,qdp_h)
+      !$acc kernels
+      !$acc loop private(k,q,ie)
+      do ie = 1 , nelemd
+        ! add hyperviscosity to RHS.  apply to Q at timelevel n0, Qdp(n0)/dp
+        do k = 1 , nlev    !  Loop index added with implicit inversion (AAM)
+          dp_h(:,:,k,ie) = dp_h(:,:,k,ie) - rhs_multiplier*dt*divdp_h(:,:,k,ie)
+          do q = 1 , qsize
+            Qtens_biharmonic(:,:,k,q,ie) = Qdp_h(:,:,k,q,n0_qdp,ie)/dp_h(:,:,k,ie)
+          enddo
         enddo
       enddo
-    enddo
-!$acc end kernels
-!$acc end data
-!$OMP END MASTER
+      !$acc end kernels
+      !$acc update host(qtens_biharmonic)
+
+      if ( rhs_multiplier == 0 .or. rhs_multiplier == 2 ) then
+        !$acc parallel loop gang vector_length(512)
+        do ie = 1 , nelemd
+          do q = 1 , qsize
+            !We had "worker" put here, but it doesn't work woth PGI
+            !$acc loop private(qmintmp,qmaxtmp) 
+            do k = 1 , nlev  
+              qmintmp = 1d9
+              qmaxtmp = -1d9
+              !$acc loop reduction(min:qmintmp) reduction(max:qmaxtmp) collapse(2) vector
+              do j = 1, np
+                do i = 1, np
+                  qmintmp=min(Qtens_biharmonic(i,j,k,q,ie),qmintmp)
+                  qmaxtmp=max(Qtens_biharmonic(i,j,k,q,ie),qmaxtmp)
+                enddo
+              enddo
+              qmin(k,q,ie) = max(qmintmp,0d0)
+              qmax(k,q,ie) = qmaxtmp
+            enddo
+          enddo
+        enddo
+        !$acc end parallel loop
+        !$acc update host(qmin,qmax)
+      endif
+
+      if ( rhs_multiplier == 1 ) then
+        !$acc update device(qmin,qmax)
+        !$acc parallel loop gang vector_length(512)
+        do ie = 1 , nelemd
+          do q = 1 , qsize
+            !We had "worker" put here, but it doesn't work woth PGI
+            !$acc loop private(qmintmp,qmaxtmp) 
+            do k = 1 , nlev  
+              qmintmp = 1d9
+              qmaxtmp = -1d9
+              !$acc loop reduction(min:qmintmp) reduction(max:qmaxtmp) collapse(2) vector
+              do j = 1, np
+                do i = 1, np
+                  qmintmp=min(Qtens_biharmonic(i,j,k,q,ie),qmintmp)
+                  qmaxtmp=max(Qtens_biharmonic(i,j,k,q,ie),qmaxtmp)
+                enddo
+              enddo
+              qmin(k,q,ie) = min(qmin(k,q,ie),qmintmp)
+              qmin(k,q,ie) = max(qmin(k,q,ie),0d0)
+              qmax(k,q,ie) = max(qmax(k,q,ie),qmaxtmp)
+            enddo
+          enddo
+        enddo
+        !$acc end parallel loop
+        !$acc update host(qmin,qmax)
+      endif
+
+      !$acc wait
+      !$acc end data
+    endif
 !$OMP BARRIER
 
     ! compute element qmin/qmax
     if ( rhs_multiplier == 0 ) then
-!$OMP BARRIER
-  if (hybrid%ithr == 0 ) then
-    !$acc data copyin(Qtens_biharmonic) copyout(qmin,qmax)
-    !$acc parallel loop gang vector_length(512)
-    do ie = 1 , nelemd
-      do q = 1 , qsize
-        !We had "worker" put here, but it doesn't work woth PGI
-        !$acc loop private(qmintmp,qmaxtmp) 
-        do k = 1 , nlev  
-          qmintmp = 1d9
-          qmaxtmp = -1d9
-          !$acc loop reduction(min:qmintmp) reduction(max:qmaxtmp) collapse(2) vector
-          do j = 1, np
-            do i = 1, np
-              qmintmp=min(Qtens_biharmonic(i,j,k,q,ie),qmintmp)
-              qmaxtmp=max(Qtens_biharmonic(i,j,k,q,ie),qmaxtmp)
-            enddo
-          enddo
-          qmin(k,q,ie) = max(qmintmp,0d0)
-          qmax(k,q,ie) = qmaxtmp
-        enddo
-      enddo
-    enddo
-    !$acc end parallel loop
-    !$acc wait
-    !$acc end data
-  endif
-!$OMP BARRIER
       ! update qmin/qmax based on neighbor data for lim8
       if ( limiter_option == 8 ) call neighbor_minmax(elem,hybrid,edgeAdvQ2,nets,nete,qmin(:,:,nets:nete),qmax(:,:,nets:nete))
 !$OMP BARRIER
@@ -595,15 +623,6 @@ contains
 
     ! lets just reuse the old neighbor min/max, but update based on local data
     if ( rhs_multiplier == 1 ) then
-      do ie = nets , nete
-        do k = 1 , nlev    !  Loop index added with implicit inversion (AAM)
-          do q = 1 , qsize
-            qmin(k,q,ie)=min(qmin(k,q,ie),minval(Qtens_biharmonic(:,:,k,q,ie)))
-            qmin(k,q,ie)=max(qmin(k,q,ie),0d0)
-            qmax(k,q,ie)=max(qmax(k,q,ie),maxval(Qtens_biharmonic(:,:,k,q,ie)))
-          enddo
-        enddo
-      enddo
 !$OMP BARRIER
 !$OMP MASTER
       ierr = cudaMemcpyAsync( qmin_d , qmin , size( qmin ) , cudaMemcpyHostToDevice , streams(2) )
@@ -614,16 +633,6 @@ contains
     ! get niew min/max values, and also compute biharmonic mixing term
     if ( rhs_multiplier == 2 ) then
       rhs_viss = 3
-      ! compute element qmin/qmax  
-      do ie = nets , nete
-        do k = 1  ,nlev    
-          do q = 1 , qsize
-            qmin(k,q,ie)=minval(Qtens_biharmonic(:,:,k,q,ie))
-            qmax(k,q,ie)=maxval(Qtens_biharmonic(:,:,k,q,ie))
-            qmin(k,q,ie)=max(qmin(k,q,ie),0d0)
-          enddo
-        enddo
-      enddo
       ! two scalings depending on nu_p:
       ! nu_p=0:    qtens_biharmonic *= dp0                   (apply viscsoity only to q)
       ! nu_p>0):   qtens_biharmonc *= elem()%psdiss_ave      (for consistency, if nu_p=nu_q)
