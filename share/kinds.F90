6996a60a58baaa7013f6089466ff09a4bb66589e
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-02-22 21:02:05 +0000

    Make quad precision default.
    git-svn-id: file:///Users/mataylo/repos/homme/branches/cmake-2527@2826 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 28f4368e8ff21ba2f968f118740114a0c4566cc9

--- share/kinds.F90
+++ share/kinds.F90
@@ -30,10 +30,10 @@ private
   log_kind     = 4,                      &
   real_kind    = 8,                      &
   iulog        = 6,                      & ! stderr file handle
-#if HOMME_QUAD_PREC
-  longdouble_kind    = 16
-#else 
+#if HOMME_NO_QUAD_PREC
   longdouble_kind    = 8
+#else 
+  longdouble_kind    = 16
 #endif
 #endif
 
