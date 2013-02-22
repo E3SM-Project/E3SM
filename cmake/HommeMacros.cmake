2277eb512a4a2517e2f24dd90c346bfffae4f55b
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-02-22 19:25:31 +0000

    Adding support for configure specialization. Now you can specify if you'd only like to build certain executables (eg. sweqx) with -DSWEQX_ONLY=TRUE etc. Moving the default options out of the root CMakeLists.txt file and into the swim CMakeLists.txt. Also namespacing them. Printing a summary of the values used for each executable. First pass at automatically supporting quad precision.
    git-svn-id: file:///Users/mataylo/repos/homme/branches/cmake-2527@2820 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 35dba239719b371719ec64c7acd714641a256d28

--- cmake/HommeMacros.cmake
+++ cmake/HommeMacros.cmake
@@ -274,4 +274,17 @@ macro(createTest testName)
 
 endmacro(createTest)
 
+macro(testQuadPrec HOMME_QUAD_PREC)
+  TRY_RUN(RUN_RESULT_VAR COMPILE_RESULT_VAR
+          ${CMAKE_BINARY_DIR}/compiler_test/ ${CMAKE_SOURCE_DIR}/cmake/quadTest.f90
+          COMPILE_OUTPUT_VARIABLE comp 
+          )
 
+  IF (${COMPILE_RESULT_VAR} AND ${RUN_RESULT_VAR} STREQUAL 0)
+    SET (${HOMME_QUAD_PREC} TRUE)
+    MESSAGE(STATUS "Quadruple-precision supported enabling")
+  ELSE ()
+    SET (HOMME_QUAD_PREC FALSE)
+    MESSAGE(STATUS "Quadruple-precision not supported")
+  ENDIF ()
+endmacro(testQuadPrec)
