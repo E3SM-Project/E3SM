d40c87594f4ddc85276cffa55f1297435973a9f3
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-03-05 18:06:40 +0000

    Reverting this spelt index.
    git-svn-id: file:///Users/mataylo/repos/homme/branches/cmake-2527@2860 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 68562719f69151e0e20bfd7921bb96331b39621d

--- share/spelt_mod.F90
+++ share/spelt_mod.F90
@@ -1347,7 +1347,7 @@ subroutine spelt_init3(elem,spelt,hybrid,nets,nete,tnp0)
     call ghostVpack2d(cellghostbuf,spelt(ie)%c,nipm, nep,nlev,ntrac,0, tnp0, timelevels,elem(ie)%desc)
   end do
   !-----------------------------------------------------------------------------------!  
-  call ghost_exchangeV(hybrid,cellghostbuf,nipm,nep,1)
+  call ghost_exchangeV(hybrid,cellghostbuf,nipm,nep,ntrac)
   !-----------------------------------------------------------------------------------!    
   do ie=nets,nete
     call ghostVunpack2d(cellghostbuf,spelt(ie)%c,nipm, nep,nlev,ntrac,0, tnp0, timelevels,elem(ie)%desc)
