8a16989b0f6fb0115a916473007a9954ba27d039
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-05-24 19:14:54 +0000

    Only add netcdf output files for openmp tests when compiling with openmp. Reorder the tests to get feedback sooner.
    git-svn-id: file:///Users/mataylo/repos/homme/branches/testing-3316@3325 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 761f25271f6a5d1083e37c2a36cd06019e91dd64

--- test/reg_test/run_tests/test-list.cmake
+++ test/reg_test/run_tests/test-list.cmake
@@ -1,14 +1,14 @@
 # Lists of test files for the HOMME regression tests
 SET(HOMME_TESTS 
+  swtc1.cmake
+  swtc5.cmake
+  swtc6.cmake
   baro1a.cmake
   baro1b.cmake
   baro2a.cmake
   baro2b.cmake
   baro2c.cmake
   baro2d.cmake
-  swtc1.cmake
-  swtc5.cmake
-  swtc6.cmake
   swtc1-dg.cmake
   swtc2-dg.cmake
   swtc5-dg.cmake
