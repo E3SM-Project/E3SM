3a3ca569a7f4f0bbd1efeb265ddbaa9ec0ab29b0
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-07-17 23:42:41 +0000

    Removing general yellowstone machine file - have to use compiler dependent files (yellowstoneGNU.cmake etc.) because of the differing compiler names.
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@3417 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 1bcc10d16598b80abb3b6234f5ccedf3e80cebcc

--- cmake/machineFiles/yellowstone.cmake
+++
@@ -1,9 +0,0 @@
-# CMake initial cache file for yellowstone intel
-SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
-SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
-SET (CMAKE_CXX_COMPILER mpiCC CACHE FILEPATH "")
-SET (NETCDF_DIR $ENV{NETCDF})
-SET (PNETCDF_DIR $ENV{PNETCDF})
-SET (HDF5_DIR /glade/apps/opt/hdf5/1.8.9/intel/default)
-SET (SZIP_DIR /glade/apps/opt/szlib/2.1/intel/12.1.4)
-SET (ZLIB_DIR /glade/apps/opt/zlib/1.2.7/intel/12.1.4)
