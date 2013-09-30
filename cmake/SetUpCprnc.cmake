4671db5b5ba5b9412e1565716c0eccb87c470d3a
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-09-30 19:16:04 +0000

    Creating a make clean-all target to remove CMake cache. Passing compiler flags to cprnc. Automatically delete the cprnc build directory at every configure.
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@3602 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 83af3d727f9633fab823a694f2de916d015a7ebe

--- cmake/SetUpCprnc.cmake
+++ cmake/SetUpCprnc.cmake
@@ -1,9 +1,14 @@
 
 INCLUDE(ExternalProject)
 
+
 # CPRNC locations
 SET (CPRNC_INSTALL_DIR ${CMAKE_BINARY_DIR}/utils/cprnc)
 SET (CPRNC_BINARY ${CMAKE_BINARY_DIR}/utils/cprnc/bin/cprnc)
+
+# Always remove this directory when configuring
+#   this is the only way to ensure that the cprnc cache is deleted
+FILE(REMOVE_RECURSE ${CPRNC_INSTALL_DIR})
 
 # The following builds cprnc 
 ExternalProject_Add(
@@ -12,6 +17,8 @@ ExternalProject_Add(
   CMAKE_ARGS
     -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
     -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+    -DCMAKE_Fortran_FLAGS="${CMAKE_Fortran_FLAGS}"
+    -DCMAKE_C_FLAGS="${CMAKE_C_FLAGS}"
     -DNETCDF_DIR=${CPRNC_NETCDF_DIR}
     -DHDF5_DIR=${CPRNC_HDF5_DIR}
     -DSZIP_DIR=${CPRNC_SZIP_DIR}
@@ -20,6 +27,7 @@ ExternalProject_Add(
     -DCMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}
     -DCMAKE_INSTALL_PREFIX=${CPRNC_INSTALL_DIR}
   INSTALL_DIR ${CPRNC_INSTALL_DIR}
+  PREFIX ${CPRNC_INSTALL_DIR}
   LOG_CONFIGURE ON)
 
 
