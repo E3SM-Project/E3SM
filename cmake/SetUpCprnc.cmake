42ecce6336657dfb762b15bf6017183e5471d4a4
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-10-04 22:16:10 +0000

    Fix a problem sending lists to the cprnc cmake build.
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@3612 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 070758d2ca65ca37cfebbe89e75c89b5576acd2f

--- cmake/SetUpCprnc.cmake
+++ cmake/SetUpCprnc.cmake
@@ -10,22 +10,29 @@ SET (CPRNC_BINARY ${CMAKE_BINARY_DIR}/utils/cprnc/bin/cprnc)
 #   this is the only way to ensure that the cprnc cache is deleted
 FILE(REMOVE_RECURSE ${CPRNC_INSTALL_DIR})
 
+# Deal with a strange problem passing lists to cmake
+FOREACH(NC_LIB ${Netcdf_LIBRARIES})
+  IF (NOT DEFINED NC_LIB_ARG)
+    SET(NC_LIB_ARG "${NC_LIB}")
+  ELSE ()
+    SET(NC_LIB_ARG "${NC_LIB_ARG}^^${NC_LIB}")
+  ENDIF ()
+ENDFOREACH()
+
 # The following builds cprnc 
 ExternalProject_Add(
   cprnc
   SOURCE_DIR ${CMAKE_SOURCE_DIR}/utils/cprnc_131004
+  LIST_SEPARATOR ^^
   CMAKE_ARGS
     -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
     -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
     -DCMAKE_Fortran_FLAGS='${CMAKE_Fortran_FLAGS}'
     -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS}'
-    -DNetcdf_LIBRARIES=${Netcdf_LIBRARIES}
+    -DNetcdf_LIBRARIES=${NC_LIB_ARG}
     -DNetcdf_INCLUDE_DIR=${Netcdf_INCLUDE_DIRS}
-    -DCMAKE_SYSTEM_NAME=${CMAKE_SYSTEM_NAME}
     -DCMAKE_INSTALL_PREFIX=${CPRNC_INSTALL_DIR}
   INSTALL_DIR ${CPRNC_INSTALL_DIR}
   PREFIX ${CPRNC_INSTALL_DIR}
   LOG_CONFIGURE ON)
-
-
 
