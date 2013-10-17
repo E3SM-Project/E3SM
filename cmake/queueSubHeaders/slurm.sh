b2cae9f3971ff07c3cae733304841f1bbe877c7c
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-10-17 15:27:52 +0000

    Normal queue for slurm. Remove empty directory.
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@3640 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: ed4907a10be6e1f324c6438ced39da4d4d710ec2

--- cmake/queueSubHeaders/slurm.sh
+++ cmake/queueSubHeaders/slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH -J ${testName}             # Job name
 #SBATCH -o ${testName}.stdout.%j   # Name of stdout output file(%j expands to jobId)
 #SBATCH -e ${testName}.stderr.%j   # Name of stderr output file(%j expands to jobId)
-#SBATCH -p development             # Submit to the 'normal' or 'development' queue
+#SBATCH -p normal                  # Submit to the 'normal' or 'development' queue
 #SBATCH -N 1                       # Total number of nodes requested (16 cores/node)
 #SBATCH -n 16                      # Total number of mpi tasks requested
 #SBATCH -t 0:40:00                 # Run time (hh:mm:ss) 
