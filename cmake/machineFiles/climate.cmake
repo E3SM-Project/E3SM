e75dada4e0f92d31d07b0ec384b31db51fa8812c
taylorm <taylorm@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2013-10-21 16:35:14 +0000

    change compiler flags on SNL machine
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@3649 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 886aee12b9776b54d936f232d364e4efcb5e7f1a

--- cmake/machineFiles/climate.cmake
+++ cmake/machineFiles/climate.cmake
@@ -15,10 +15,12 @@ SET (CMAKE_CXX_COMPILER mpicc CACHE FILEPATH "")
 #      -assume byterecl -fp-model precise -ftz -g -O3 
 # -O3 causes problems on redsky when openMP is enabled (even for 1 thread)
 #
-SET (FORCE_Fortran_FLAGS "-openmp -fp-model fast -ftz -g -O2" CACHE STRING "")
+#SET (FORCE_Fortran_FLAGS "-traceback -openmp -fp-model fast -ftz -g -O2" CACHE STRING "")
+SET (FORCE_Fortran_FLAGS "-traceback -openmp -fp-model precise -ftz -g -O2" CACHE STRING "")
 SET (ENABLE_OPENMP TRUE CACHE BOOL "")
 
-SET (ADD_Fortran_FLAGS "-traceback" CACHE STRING "")
+# ignored if FORCE_Fortran_FLAGS used above
+#SET (ADD_Fortran_FLAGS "-traceback" CACHE STRING "")
 
 SET (NETCDF_DIR $ENV{NETCDF_PATH} CACHE FILEPATH "")
 SET (PNETCDF_DIR $ENV{PNETCDF_PATH} CACHE FILEPATH "")
