7a4838afcc750ac80994ff421dac9dc410bd2922
Jim Edwards <edwards.jim@gmail.com>
2013-11-24 19:28:12 +0000

    improve pnetcdf lib detection, fix issues in put

--- pio/cmake/FindPnetcdf.cmake
+++ pio/cmake/FindPnetcdf.cmake
@@ -1,37 +1,24 @@
+include(FindPackageHandleStandardArgs)
 
-
-
-FIND_PATH(Pnetcdf_INCLUDE_DIR 
+FIND_PATH(PNETCDF_INCLUDE_DIR 
           pnetcdf.h
-          HINTS ${Homme_Hint_Paths}
-          PATHS ${PNETCDF_DIR} ${Homme_PNETCDF_DIR}
-          PATH_SUFFIXES include
-          NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
+          HINTS ${PNETCDF_DIR}/include)
+
 
 IF (${PREFER_SHARED})
-  FIND_LIBRARY(Pnetcdf_LIBRARY 
+  FIND_LIBRARY(PNETCDF_LIBRARY 
                NAMES pnetcdf
-               HINTS ${Pnetcdf_INCLUDE_DIR}/../lib
-               NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
+               HINTS ${PNETCDF_DIR}/lib)
+
 ELSE ()
-  FIND_LIBRARY(Pnetcdf_LIBRARY 
+  FIND_LIBRARY(PNETCDF_LIBRARY 
                NAMES libpnetcdf.a pnetcdf
-               HINTS ${Pnetcdf_INCLUDE_DIR}/../lib
-               NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
+               HINTS ${PNETCDF_DIR}/lib)
 ENDIF ()
 
-IF (Pnetcdf_INCLUDE_DIR AND Pnetcdf_LIBRARY)
-  SET(Pnetcdf_LIBRARIES ${Pnetcdf_LIBRARY} )
-  SET(Pnetcdf_INCLUDE_DIRS ${Pnetcdf_INCLUDE_DIR} )
-  SET(Pnetcdf_FOUND ON)
-  MESSAGE(STATUS "Found Pnetcdf:")
-  MESSAGE(STATUS "  Libraries: ${Pnetcdf_LIBRARIES}")
-  MESSAGE(STATUS "  Includes:  ${Pnetcdf_INCLUDE_DIRS}")
-ELSE()
-  SET(Pnetcdf_FOUND OFF)
-ENDIF()
+SET(PNETCDF_LIBRARIES ${PNETCDF_LIBRARY} )
+SET(PNETCDF_INCLUDE_DIRS ${PNETCDF_INCLUDE_DIR} )
 
-IF(Pnetcdf_FIND_REQUIRED AND NOT Pnetcdf_FOUND)
-  MESSAGE(FATAL_ERROR "Did not find required library Pnetcdf.\n"
-          "Please set location of Pnetcdf with -DPNETCDF_DIR")
-ENDIF()
+# Handle QUIETLY and REQUIRED.
+find_package_handle_standard_args(pnetcdf DEFAULT_MSG
+  PNETCDF_LIBRARY PNETCDF_INCLUDE_DIR  )
