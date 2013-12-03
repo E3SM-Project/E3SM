7f476e5f5a565ebfb2976bd55e74c20a7a1c1d1d
Jim Edwards <edwards.jim@gmail.com>
2013-12-03 15:14:56 +0000

    feedback from mira

--- pio/cmake/TryCSizeOf.f90
+++ pio/cmake/TryCSizeOf.f90
@@ -1,5 +1,6 @@
        program trycsizeof
          use iso_c_binding, only : c_sizeof
-         integer :: a,b
-         b = c_sizeof(a)
+         integer :: b
+         character(len=2) :: a(5)
+         b = c_sizeof(a(1))
       end program trycsizeof
