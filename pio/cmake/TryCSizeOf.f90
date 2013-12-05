2c71310f3aba80c80536f43e78a67d24337e2c04
Jim Edwards <edwards.jim@gmail.com>
2013-12-05 21:47:47 +0000

    fix for c_sizeof in xlf

--- pio/cmake/TryCSizeOf.f90
+++ pio/cmake/TryCSizeOf.f90
@@ -1,6 +1,6 @@
        program trycsizeof
          use iso_c_binding, only : c_sizeof
          integer :: b
-         character(len=2) :: a(5)
+         integer :: a(5)
          b = c_sizeof(a(1))
       end program trycsizeof
