450c035b965647b9e7cb112ce620fca1217f660f
jamroz@ucar.edu <jamroz@ucar.edu@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2014-01-29 20:05:51 +0000

    Moving cprnc to svn external - updating to latest.
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@3863 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: e49046ea8515da94b43fca3328426670d3309599

--- utils/cprnc_131004/MakeLynx.sh
+++
@@ -1,8 +0,0 @@
-#wrapper script to build cprnc on lynx
-#!/bin/bash
-
-. /opt/modules/${MODULE_VERSION}/init/bash
-
-module load PGI/netcdf4/4.1.3_seq
-
-gmake NETCDF=/contrib/netcdf/4.1.3/netcdf4-pgi
