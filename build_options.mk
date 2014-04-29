58b5ad91c616ea6dc11846fd80a8f9e13aeee4d9
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2014-04-29 08:36:38 -0600

    Adding cvmix to the list of FCINCLUDES
    This fixes a build error using the PGI complilers.

--- build_options.mk
+++ build_options.mk
@@ -2,11 +2,11 @@ PWD=$(shell pwd)
 ifeq "$(MODE)" "analysis"
 	EXE_NAME=ocean_analysis_model
 	NAMELIST_SUFFIX=ocean_analysis
-	FCINCLUDES += -I$(PWD)/src/core_ocean/mode_analysis -I$(PWD)/src/core_ocean/shared -I$(PWD)/src/core_ocean/analysis_members
+	FCINCLUDES += -I$(PWD)/src/core_ocean/mode_analysis -I$(PWD)/src/core_ocean/shared -I$(PWD)/src/core_ocean/analysis_members -I$(PWD)/src/core_ocean/cvmix
 else ifeq "$(MODE)" "forward"
 	EXE_NAME=ocean_forward_model
 	NAMELIST_SUFFIX=ocean_forward
-	FCINCLUDES += -I$(PWD)/src/core_ocean/mode_forward -I$(PWD)/src/core_ocean/shared -I$(PWD)/src/core_ocean/analysis_members
+	FCINCLUDES += -I$(PWD)/src/core_ocean/mode_forward -I$(PWD)/src/core_ocean/shared -I$(PWD)/src/core_ocean/analysis_members -I$(PWD)/src/core_ocean/cvmix
 else
 	EXE_NAME=ocean*_model
 	NAMELIST_SUFFIX=ocean*
