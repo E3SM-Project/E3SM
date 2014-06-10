2e3c768e43b8e967851c6272e94f2ad95b73a2e0
taylorm <taylorm@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2014-06-10 03:29:12 +0000

    update for updated redsky
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@4104 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: c4aaa93fce301b05562de99eb024cbce8551fbf9

--- cmake/machineFiles/redsky.cmake
+++ cmake/machineFiles/redsky.cmake
@@ -1,4 +1,7 @@
+# 
 # CMake initial cache file for Sandia's redsky
+# configurd for redsky's netcdf-intel/4.1 module 
+#
 SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
 SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
 SET (CMAKE_CXX_COMPILER mpicc CACHE FILEPATH "")
@@ -18,7 +21,8 @@ SET (FORCE_Fortran_FLAGS "-openmp -traceback -fp-model precise -ftz -g -O2" CACH
 
 
 SET (ENABLE_OPENMP TRUE CACHE BOOL "")
-SET (NETCDF_DIR "/projects/ccsm/netcdf4.2.1.1-intel" CACHE FILEPATH "")
+#SET (NETCDF_DIR "/projects/ccsm/netcdf4.2.1.1-intel" CACHE FILEPATH "")
+SET (NETCDF_DIR "/opt/netcdf-4.1-intel" CACHE FILEPATH "")
 SET (PNETCDF_DIR "/projects/ccsm/pnetcdf1.2.0-intel" CACHE FILEPATH "")
 SET (USE_QUEUING FALSE CACHE BOOL "")
 SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
