09bdc21c6352fc8cf1af6803b61768e032c7fb21
Jayesh Krishna <jayeshkrishna@gmail.com>
2014-06-20 19:39:57 +0000

    Adding capability to write generic template style functions. This feature will be used to autogenerate tests for the different predefined PIO data types.

--- test/util/pio_tf_f90gen.pl
+++ test/util/pio_tf_f90gen.pl
@@ -16,10 +16,25 @@ my($template_fname, $output_fname);
 my($template_has_auto_funcs, $template_has_test_driver);
 # Indicates whether auto funcs have already been inserted
 my($template_auto_funcs_inserted);
+# Used to disable write of template functions - the template
+# functions are read in the first pass and inserted just before
+# the driver
+# 0 => write is NOT disabled
+# 1 => write is disabled
+my($template_disable_wr);
 my($annotate_source);
 # The current test case number is modified by multiple funcs
 # * its logically easier to keep it a global
 my($cur_test_case_num);
+
+# template function name ==> template function body
+my (%template_funcs);
+# template function name ==> template function typenames
+my (%template_func_typenames);
+# template function typename ==> concrete types
+my (%template_typename_types);
+# Predefined typename ==> predefined types
+my (%template_predef_typename_types);
 
 # Initializing global vars
 $verbose = 0;
@@ -28,9 +43,190 @@ $template_fname = '';
 $output_fname = '';
 $template_has_auto_funcs = 0;
 $template_auto_funcs_inserted = 0;
+$template_disable_wr = 0;
 $template_has_test_driver = 0;
 $annotate_source = 0;
 $cur_test_case_num = 1;
+
+# Initialize predefined types
+sub init_predef_types
+{
+  $template_predef_typename_types{"PIO_TF_DATA_TYPE"} = [];
+  $template_predef_typename_types{"PIO_TF_FC_DATA_TYPE"} = [];
+  push(@{$template_predef_typename_types{"PIO_TF_DATA_TYPE"}}, "PIO_int");
+  push(@{$template_predef_typename_types{"PIO_TF_FC_DATA_TYPE"}}, "integer");
+  push(@{$template_predef_typename_types{"PIO_TF_DATA_TYPE"}}, "PIO_real");
+  push(@{$template_predef_typename_types{"PIO_TF_FC_DATA_TYPE"}}, "real(kind=4)");
+  push(@{$template_predef_typename_types{"PIO_TF_DATA_TYPE"}}, "PIO_double");
+  push(@{$template_predef_typename_types{"PIO_TF_FC_DATA_TYPE"}}, "real(kind=8)");
+}
+
+# Generates the generic template function bodies (or names)
+sub generate_gen_templates
+{
+  my ($gen_templ_func_name, $ref_modif_gen_templ_func_names,
+      $base_file_name, $ifline_num) = @_;
+  my ($gen_templ_func_bodies) = "";
+
+  # Find typenames for the template and corresponding concrete types
+  my (@typenames); 
+  my ($ntypenames);
+  my (@typename_counter, @typename_counter_max);
+  my ($typename);
+
+  if($verbose) {print "Going to generate functions from \"$gen_templ_func_name\"\n"; }
+  if(not exists $template_func_typenames{$gen_templ_func_name}){
+    print "ERROR: Error parsing template file ($base_file_name:$ifline_num)\n";
+    print "       Check the name of the function\n";
+    return "";
+  }
+  @typenames = @{$template_func_typenames{$gen_templ_func_name}};
+  $ntypenames = @typenames;
+  if($verbose) { print "TYPENAMES: " . join(",",@typenames) . " : $ntypenames\n"; }
+  if($ntypenames < 1){
+    if($verbose) {print "WARNING: No typenames to generate template func name ($gen_templ_func_name)\n";}
+    return "";
+  }
+  foreach $typename (@typenames){
+    if(exists $template_predef_typename_types{$typename}){
+      my $ntypes;
+      $ntypes = @{$template_predef_typename_types{$typename}};
+      push(@typename_counter, 0);
+      push(@typename_counter_max, $ntypes);
+    }
+    else{
+      print "ERROR: Only predefined types are allowed in template functions\n";
+      print "       $base_file_name:$ifline_num\n";
+      print "       \"$typename\" is NOT a predefined type\n";
+      return "";
+    }
+  }
+
+  # We are guaranteed there is atleast one typename and it has some (>1)
+  # predefined types
+  my ($cnt_max) = 1;
+  my ($i, $j, $k);
+  for($i=0; $i < $ntypenames; $i++){
+    if($cnt_max < $typename_counter_max[$i]){
+      $cnt_max = $typename_counter_max[$i];
+    }
+  }
+  my $gen_templ_func_body = "";
+  for($i=0; $i < $cnt_max; $i++){
+    my (@concrete_types);
+
+    $gen_templ_func_body = $template_funcs{$gen_templ_func_name};
+    for($j=0; $j < $ntypenames; $j++){
+      my ($ctype);
+      $ctype = @{$template_predef_typename_types{$typenames[$j]}}[$typename_counter[$j]];
+      $gen_templ_func_body =~ s/$typenames[$j]/$ctype/mg;
+      $ctype =~ s/[(]/_/g;
+      $ctype =~ s/[)]/_/g;
+      $ctype =~ s/[=]/_/g;
+      push(@concrete_types, $ctype);
+    }
+    $typename_counter[0] += 1;
+    for($k = $ntypenames - 1; $k > 0; $k--){
+      $typename_counter[$k] += 1;
+      if($typename_counter[$k] >= $typename_counter_max[$k]){
+        $typename_counter[$k] = 0;
+        $typename_counter[$k - 1] += 1;
+      }
+    }
+    my ($modif_gen_templ_func_name) = $gen_templ_func_name;
+    my ($type);
+    $modif_gen_templ_func_name .= "_" . join("_", @concrete_types) . "__";
+    push(@{$ref_modif_gen_templ_func_names}, $modif_gen_templ_func_name);
+    $gen_templ_func_body =~ s/$gen_templ_func_name/$modif_gen_templ_func_name/mg;
+
+    $gen_templ_func_bodies .= "\n\n" . $gen_templ_func_body;
+    $gen_templ_func_body = "";
+    undef(@concrete_types);
+  }
+  if($verbose) {print "GENERATED TEMPLATE FUNCTIONS: \n" . $gen_templ_func_bodies; }
+  return $gen_templ_func_bodies;
+}
+
+# FIXME - Not used, kept around so that the logic is not lost
+# Could be used to generate templates with all permutations of the typenames 
+sub generate_gen_templates_all_permutes
+{
+  my ($gen_templ_func_name, $ref_modif_gen_templ_func_names,
+      $base_file_name, $ifline_num) = @_;
+  my ($gen_templ_func_bodies) = "";
+
+  # Find typenames for the template and corresponding concrete types
+  my (@typenames); 
+  my ($ntypenames);
+  my (@typename_counter, @typename_counter_max);
+  my ($typename);
+
+  if($verbose) {print "Going to generate functions from \"$gen_templ_func_name\"\n"; }
+  if(not exists $template_func_typenames{$gen_templ_func_name}){
+    print "ERROR: Error parsing template file ($base_file_name:$ifline_num)\n";
+    print "       Check the name of the function\n";
+    return "";
+  }
+  @typenames = @{$template_func_typenames{$gen_templ_func_name}};
+  $ntypenames = @typenames;
+  if($verbose) { print "TYPENAMES: " . join(",",@typenames) . " : $ntypenames\n"; }
+  if($ntypenames < 1){
+    if($verbose) {print "WARNING: No typenames to generate template func name ($gen_templ_func_name)\n";}
+    return "";
+  }
+  foreach $typename (@typenames){
+    if(exists $template_predef_typename_types{$typename}){
+      my $ntypes;
+      $ntypes = @{$template_predef_typename_types{$typename}};
+      push(@typename_counter, 0);
+      push(@typename_counter_max, $ntypes);
+    }
+    else{
+      print "ERROR: Only predefined types are allowed in template functions\n";
+      print "       $base_file_name:$ifline_num\n";
+      print "       \"$typename\" is NOT a predefined type\n";
+      return "";
+    }
+  }
+
+  # We are guaranteed there is atleast one typename and it has some (>1)
+  # predefined types
+  my ($cnt_max) = 1;
+  my ($i, $j, $k);
+  for($i=0; $i < $ntypenames; $i++){
+    $cnt_max *= $typename_counter_max[$i];
+  }
+  my $gen_templ_func_body = "";
+  for($i=0; $i < $cnt_max; $i++){
+    my (@concrete_types);
+
+    $gen_templ_func_body = $template_funcs{$gen_templ_func_name};
+    for($j=0; $j < $ntypenames; $j++){
+      my ($ctype);
+      $ctype = @{$template_predef_typename_types{$typenames[$j]}}[$typename_counter[$j]];
+      push(@concrete_types, $ctype);
+      $gen_templ_func_body =~ s/$typenames[$j]/$ctype/mg;
+    }
+    $typename_counter[$ntypenames - 1] += 1;
+    for($k = $ntypenames - 1; $k > 0; $k--){
+      if($typename_counter[$k] >= $typename_counter_max[$k]){
+        $typename_counter[$k] = 0;
+        $typename_counter[$k - 1] += 1;
+      }
+    }
+    my ($modif_gen_templ_func_name) = $gen_templ_func_name;
+    my ($type);
+    $modif_gen_templ_func_name .= "_" . join("_", @concrete_types) . "__";
+    push(@{$ref_modif_gen_templ_func_names}, $modif_gen_templ_func_name);
+    $gen_templ_func_body =~ s/$gen_templ_func_name/$modif_gen_templ_func_name/mg;
+
+    $gen_templ_func_bodies .= "\n\n" . $gen_templ_func_body;
+    $gen_templ_func_body = "";
+    undef(@concrete_types);
+  }
+  if($verbose) {print "GENERATED TEMPLATE FUNCTIONS: \n" . $gen_templ_func_bodies; }
+  return $gen_templ_func_bodies;
+}
 
 # This function transforms the template source one line at a time
 # * Does the transformation of the template source
@@ -48,7 +244,13 @@ sub transform_src
     $out_line = $out_line . $1 . "  USE pio_tutil\n";
   }
   elsif(/^(\s*)(PIO_TF_TEST_SUB_END)(.*)$/s){
-    $out_line = $1 . "END SUBROUTINE" . $3;
+    if($template_disable_wr == 0){
+      $out_line = $1 . "END SUBROUTINE" . $3;
+    }
+    else{
+      $out_line = $1;
+      $template_disable_wr = 0;
+    }
   }
   elsif(/^(\s*)(PIO_TF_AUTO_TEST_SUB_BEGIN)(\s*)(.*)$/s){
     $out_line = $1 . "SUBROUTINE" . $3 . $4 . "\n";
@@ -56,10 +258,24 @@ sub transform_src
     $template_has_auto_funcs = 1;
 
     # Add auto test func into the auto func list
-    push @{$ref_auto_funcs_list}, $4;
+    if($template_disable_wr == 0){
+      push @{$ref_auto_funcs_list}, $4;
+    }
   }
-  elsif(/^(\s*)(PIO_TF_AUTO_TEST_SUB_END)(.*)$/s){
-    $out_line = $1 . "END SUBROUTINE" . $3;
+  elsif(/^(\s*)(PIO_TF_AUTO_TEST_SUB_END)(\s*)(.*)\s*$/s){
+    if($template_disable_wr == 0){
+      $out_line = $1 . "END SUBROUTINE" . $3 . $4;
+    }
+    else{
+      my (@gen_templ_func_names);
+      $out_line = generate_gen_templates($4, \@gen_templ_func_names,
+                    $template_fname, $template_line_no);
+      $template_disable_wr = 0;
+    }
+  }
+  elsif(/^(\s*)(PIO_TF_TEMPLATE)(.*)$/s){
+    $out_line = $1 . "";
+    $template_disable_wr = 1;
   }
   elsif(/^(\s*)(PIO_TF_TEST_DRIVER_BEGIN)(.*)$/s){
     $out_line = $1 . "SUBROUTINE PIO_TF_Test_driver_" . $3 . "\n";
@@ -220,9 +436,125 @@ sub transform_src
   }
   else{
     $out_line = $_;
+    # The line is modified
     $$ref_modif = 0;
   }
   return $out_line;
+}
+
+# Find the signature of generic template functions
+# Get the template typenames here, if we find a generic templ func
+# Returns 1 => if a generic template func is found, 0 otherwise
+sub find_gen_templ_funcs
+{
+  my($_, $ref_templ_typenames) = @_;
+  if(/^\s*PIO_TF_TEMPLATE\s*<(.*)>.*$/s){
+    if($1 eq ''){
+      print "Error parsing template code, Check syntax of PIO_TF_TEMPLATE...\n";
+      return 0;
+    }
+    my(@types) = split(/,/, $1);
+    if(scalar(@types) == 0){
+      print "Error parsing template code, Check syntax of PIO_TF_TEMPLATE...\n";
+      return 0;
+    }
+    my($type);
+    foreach $type (@types){
+      $type =~ s/^\s*//;
+      $type =~ s/\s*$//;
+      my(@type_typename) = split(/\s+/,$type);
+      if(scalar(@type_typename) != 2){
+        print "Error parsing template code, Check syntax of PIO_TF_TEMPLATE...\n";
+        return 0;
+      }
+      if($type_typename[0] ne "PIO_TF_PREDEF_TYPENAME"){
+        print "Error parsing template code. Only predefined typenames are supported\n";
+        return 0;
+      }
+      if($verbose){print "Found type : $type_typename[1] \n";}
+      push(@{$ref_templ_typenames}, $type_typename[1]);
+    }
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+# Parse and store the generic template function body
+# The generic template function body is parsed, transformed and stored
+sub parse_and_store_gen_templ_funcs
+{
+  my ($is_in_templ_func) = 1;
+  my($line, $ref_templ_funcname, $ref_templ_typenames,
+      $ref_gen_templ_func_list, $base_file_name, $ifline_num) = @_;
+  my ($out_line);
+
+  $out_line = "";
+  $_ = $line;
+  if(/^(\s*)(PIO_TF_TEST_SUB_BEGIN)(\s*)(.*)$/s){
+    ${$ref_templ_funcname} = $4;
+    $out_line = $1 . "SUBROUTINE" . $3 . $4 . "\n";
+    $out_line = $out_line . $1 . "USE pio_tutil\n";
+  }
+  elsif(/^(\s*)(PIO_TF_TEST_SUB_END)(.*)$/s){
+    $is_in_templ_func = 0;
+    $out_line = $1 . "END SUBROUTINE". $3;
+  }
+  elsif(/^(\s*)(PIO_TF_AUTO_TEST_SUB_BEGIN)(\s*)(.*)$/s){
+    ${$ref_templ_funcname} = $4;
+    push(@{$ref_gen_templ_func_list}, ${$ref_templ_funcname});
+    # Adding typenames to the function_name->typename map
+    $template_func_typenames{${$ref_templ_funcname}} = [];
+    my ($typename);
+    foreach $typename (@{$ref_templ_typenames}){
+      push(@{$template_func_typenames{${$ref_templ_funcname}}}, $typename);
+    }
+    $out_line = $1 . "SUBROUTINE" . $3 . $4 . "\n";
+    $out_line = $out_line . $1 . "USE pio_tutil\n";
+  }
+  elsif(/^(\s*)(PIO_TF_AUTO_TEST_SUB_END)(.*)$/s){
+    $is_in_templ_func = 0;
+    $out_line = $1 . "END SUBROUTINE". $3;
+  }
+  elsif(/^(\s*[!].*)$/){
+    $out_line = $1;
+  }
+  else{
+    my(@tmp_auto_funcs_list);
+    my($is_transformed);
+    $out_line = &transform_src(\@tmp_auto_funcs_list, $_, $base_file_name,
+                        $ifline_num, \$is_transformed);
+  }
+  if($annotate_source){
+    $out_line = $out_line . "   ! $base_file_name:$ifline_num" . "\n";
+  }
+  if($verbose) { print "Adding \"$out_line\" to ${$ref_templ_funcname}\n"; }
+  if(exists $template_funcs{${$ref_templ_funcname}}){
+    $template_funcs{${$ref_templ_funcname}} .= $out_line;
+  }
+  else{
+    $template_funcs{${$ref_templ_funcname}} = $out_line;
+  }
+  return $is_in_templ_func;
+}
+
+# Add generic auto template functions to the auto test function list
+sub update_auto_func_list_with_gen_templ
+{
+  my($ref_auto_funcs_list, $ref_gen_templ_funcs_list,
+      $base_file_name, $ifline_num) = @_;
+  my ($gen_templ_func_name);
+  my (@modif_gen_templ_func_names);
+  foreach $gen_templ_func_name (@{$ref_gen_templ_funcs_list}){
+    # Get the general template function names 
+    &generate_gen_templates($gen_templ_func_name, \@modif_gen_templ_func_names,
+        $base_file_name, $ifline_num);
+    my ($func_name);
+    foreach $func_name (@modif_gen_templ_func_names){
+      push(@{$ref_auto_funcs_list}, $func_name);
+    }
+  }
 }
 
 # Returns the default test main code
@@ -346,6 +678,7 @@ sub process_template_file
   my($ifline);
   my($header,$footer);
   my(@auto_funcs_list);
+  my(@gen_templ_funcs_list);
   my($TEMPLATE_FILE);
   my($base_file_name);
   my($in_line) = "";
@@ -355,11 +688,62 @@ sub process_template_file
 
   # Read template file
   open(TEMPLATE_FILE, "< $ifname") or die "Cannot open input template file: $ifname";
+
+  # FIXME: Move common parsing code in first and second pass to another function
+
+  ######################################################################
+  # FIRST PASS - find generic template functions and the calling instances
+  ######################################################################
+  if($verbose){ print "First pass\n"; }
+  my($is_in_template_func) = 0;
+  my($templ_funcname) = "UNDEF";
+  my(@templ_typenames) = ();
+  $ifline_num = 1;
+  while(<TEMPLATE_FILE>){
+    chomp;
+    if(/^(\s*[^!\s][^!]+)!(.*)$/s){
+      # Strip comments
+      $_ = $1;
+    }
+    if($is_in_template_func){
+      $is_in_template_func = parse_and_store_gen_templ_funcs($_,
+                              \$templ_funcname,
+                              \@templ_typenames,
+                              \@gen_templ_funcs_list,
+                              $base_file_name,
+                              $ifline_num);
+      if($is_in_template_func == 0){
+        if($verbose) { print "TEMPLATE_FUNCTION:\n$template_funcs{$templ_funcname}\n"; }
+        $templ_funcname = "UNDEF";
+        undef(@templ_typenames);
+      }
+    }
+    else{
+      $is_in_template_func = find_gen_templ_funcs($_, \@templ_typenames);
+    }
+    $ifline_num += 1;
+  }
+  # Add the appropriate generic template function names to the auto function list
+  # "PIO_TF_TEMPLATE<PIO_TF_PREDEF_TYPENAME PIO_TF_DATA_TYPE>
+  # PIO_TF_AUTO_TEST_SUB_BEGIN foo"
+  #       transformed to
+  # "Subroutine foo_PIO_int__" etc.
+  &update_auto_func_list_with_gen_templ(\@auto_funcs_list, \@gen_templ_funcs_list,
+    $base_file_name, -1);
+
+  # Reset the file ptr
+  seek(TEMPLATE_FILE, 0, 0);
+
+  ######################################################################
+  # SECOND PASS - process the template file and write the source file
+  ######################################################################
+  if($verbose){ print "Second pass\n"; }
   open(OUTPUT_FILE, "> $ofname") or die "Cannot open output file: $ofname";
 
   $header = get_header($ifname);
   print OUTPUT_FILE $header;
 
+  $template_disable_wr = 0;
   $ifline_num = 1;
   $in_line = "";
   # Unmodified (orig) line
@@ -370,7 +754,7 @@ sub process_template_file
     $orig_line = $orig_line . $_;
     chomp;
     $_ = $in_line . $_;
-    if(/^s*[!#].*$/s){
+    if(/^\s*[!#].*$/s){
       # Line starting with comment & preproc directives - print it as it is
       if($verbose){ print "Reading comment/preproc-directive: $_\n"; }
       $out_line = $_;
@@ -414,7 +798,10 @@ sub process_template_file
         }
       }
     }
-    print OUTPUT_FILE "$out_line\n";
+    # Note: template_disable_wr is being set in transform_src()
+    if($template_disable_wr == 0){
+      print OUTPUT_FILE "$out_line\n";
+    }
     $in_line = "";
     $orig_line = "";
     $ifline_num += 1;
@@ -455,6 +842,8 @@ if(!stat($template_fname)){
   print "Error: Cannot find input template file\n";
   &print_usage_and_exit();
 }
+
+&init_predef_types();
 
 if($verbose){ print "Reading input args complete\n" }
 &process_template_file($template_fname, $output_fname);
