6ce78e9b9f60000e64fd00f66eb2d4e5e891278e
Michael Duda <duda@ucar.edu>
2014-07-09 18:55:10 -0600

    For parallel execution, only read the run-time stream XML file on the root task and broadcast the file contents to other tasks; all tasks still parse the XML string independently.

--- src/framework/xml_stream_parser.c
+++ src/framework/xml_stream_parser.c
@@ -1,17 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ezxml/ezxml.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 
 /* 
  * Interface routines for building streams at run-time; defined in mpas_stream_manager.F
  */
-void stream_mgr_init_c(char *, int *);
+void stream_mgr_create_stream_c(void *, const char *, int *);
 
 
-void xml_stream_parser(char *fname, void *manager)
+int par_read(char *fname, int *mpi_comm, char **xml_buf, size_t *bufsize)
 {
-	FILE *iofd;
+	int iofd;
+	int rank;
+	struct stat s;
+	int err;
+
+#ifdef _MPI
+#include "mpi.h"
+	
+	MPI_Comm comm;
+
+	comm = MPI_Comm_f2c((MPI_Fint)(*mpi_comm));
+	err = MPI_Comm_rank(comm, &rank);
+#else
+	rank = 0;
+#endif
+
+	if (rank == 0) {
+		iofd = open(fname, O_RDONLY);
+		if (!iofd) {
+			fprintf(stderr, "Error: Could not open run-time I/O config file %s\n", fname);
+			return 1;
+		}
+
+		fstat(iofd, &s);
+		*bufsize = (size_t)s.st_size;
+#ifdef _MPI
+		err = MPI_Bcast((void *)bufsize, (int)sizeof(size_t), MPI_BYTE, 0, comm);
+#endif
+	
+		*xml_buf = (char *)malloc(*bufsize);
+		read(iofd, (void *)(*xml_buf), *bufsize);
+
+#ifdef _MPI
+		err = MPI_Bcast((void *)(*xml_buf), (int)(*bufsize), MPI_CHAR, 0, comm);
+#endif
+	}
+	else {
+#ifdef _MPI
+		err = MPI_Bcast((void *)bufsize, (int)sizeof(size_t), MPI_BYTE, 0, comm);
+#endif
+		*xml_buf = (char *)malloc(*bufsize);
+
+#ifdef _MPI
+		err = MPI_Bcast((void *)(*xml_buf), (int)(*bufsize), MPI_CHAR, 0, comm);
+#endif
+	}
+
+	return 0;
+}
+
+
+void xml_stream_parser(char *fname, void *manager, int *mpi_comm)
+{
+        char *xml_buf;
+	size_t bufsize;
 	ezxml_t streams;
 	ezxml_t foo_xml;
 	const char *attr;
@@ -19,13 +76,11 @@ void xml_stream_parser(char *fname, void *manager)
 
 	fprintf(stderr, "MGD DEV begin parsing run-time I/O from %s\n", fname);
 
-	iofd = fopen(fname, "r");
-	if (!iofd) {
-		fprintf(stderr, "Error: Could not open run-time I/O config file %s\n", fname);
+        if (par_read(fname, mpi_comm, &xml_buf, &bufsize) != 0) {
 		return;
 	}
 
-	streams = ezxml_parse_fp(iofd);
+	streams = ezxml_parse_str(xml_buf, bufsize);
 	if (!streams) {
 		fprintf(stderr, "Error: Problems encountered while parsing run-time I/O config file %s\n", fname);
 		return;
@@ -38,6 +93,8 @@ void xml_stream_parser(char *fname, void *manager)
 		stream_mgr_create_stream_c(manager, attr, &err);
 		err++;
 	}
+
+	free(xml_buf);
 
 	fprintf(stderr, "MGD DEV done parsing run-time I/O\n");
 }
