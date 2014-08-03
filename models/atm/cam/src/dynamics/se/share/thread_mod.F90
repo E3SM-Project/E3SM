c66461f1d51cfac0386e13070a2b3b8bccb997e3
Patrick Worley <worleyph@ornl.gov>
2014-08-03 14:42:29 -0400

    Introducing work around in the spectral element atmosphere dynamical core for compiler issue.
    This is imported from cesm1_3_beta11 (homme1_3_35_nag_tags/n02/src/share):
     - a rewrite of thread_mod.F90 to work around a PGI compiler bug

--- models/atm/cam/src/dynamics/se/share/thread_mod.F90
+++ models/atm/cam/src/dynamics/se/share/thread_mod.F90
@@ -1,50 +1,33 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-
+  
 module thread_mod
-  implicit none
-  private
-
-  integer, public :: NThreads
 
 #ifdef _OPENMP
-  interface omp_get_thread_num
-     integer function omp_get_thread_num()
-     end function omp_get_thread_num
-  end interface
-
-  interface omp_in_parallel
-     function omp_in_parallel() result(ans)
-       logical ans
-     end function omp_in_parallel
-  end interface
-
-  interface omp_set_num_threads
-     subroutine omp_set_num_threads(NThreads)
-       integer NThreads
-     end subroutine omp_set_num_threads
-  end interface
-
-  interface omp_get_num_threads
-     integer function omp_get_num_threads()
-       integer NThreads
-     end function omp_get_num_threads
-  end interface
-
-  interface omp_get_max_threads
-     integer function omp_get_max_threads()
-     end function omp_get_max_threads
-  end interface
+  use omp_lib, only: omp_get_thread_num, &
+       omp_in_parallel, &
+       omp_set_num_threads, &
+       omp_get_max_threads, &
+       omp_get_num_threads, &
+       omp_get_nested
 #endif
+  
+  implicit none
+  private
+  
+  integer, public :: NThreads, nthreads_accel
+  integer, public :: vert_num_threads
+  
   public :: omp_get_thread_num
   public :: omp_in_parallel
   public :: omp_set_num_threads
   public :: omp_get_max_threads
   public :: omp_get_num_threads
+  public :: omp_get_nested
 #ifndef _OPENMP
 contains
-
+    
   function omp_get_thread_num() result(ithr)
     integer ithr
     ithr=0
@@ -68,6 +51,11 @@ contains
   integer function omp_get_max_threads()
     omp_get_max_threads=1
   end function omp_get_max_threads
+
+  integer function omp_get_nested()
+    omp_get_nested=0
+  end function omp_get_nested
+
 #endif
 
 end module thread_mod
