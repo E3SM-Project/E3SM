6a2da16fabb67852147da95a3f4e26e0c59fd6af
Michael Duda <duda@ucar.edu>
2014-08-11 20:08:47 -0600

    Implement basic syntax checking for runtime I/O XML files.
    Perform a few basic syntax checks on the XML before we attempt to parse it with
    the ezxml library:
    
     1) Are the angle brackets balanced?
     2) Are the quotes balanced?
     3) Are all XML tags closed and nested properly?
    
    There are many syntax errors that this code will not catch, e.g., attribute values that
    contain no quotes at all; similarly, there are syntactically correct situations that
    this code will flag as bad, e.g., quoted strings that contain the '=' character.
    However, having the basic checking provided by the new routine xml_syntax_check() should
    be much better than nothing at all, and should at least lead users to more easily discover
    errors in their runtime files.
    
    Also, perform a few minor cleanup items in src/framework/xml_stream_parser.c.

--- src/framework/xml_stream_parser.c
+++ src/framework/xml_stream_parser.c
@@ -8,26 +8,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "ezxml/ezxml.h"
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include "ezxml/ezxml.h"
+
+#define MSGSIZE 128
 
 
 /* 
- * Interface routines for building streams at run-time; defined in mpas_stream_manager.F
+ *  Interface routines for building streams at run-time; defined in mpas_stream_manager.F
  */
 void stream_mgr_create_stream_c(void *, const char *, int *, const char *, int *, int *);
 void mpas_stream_mgr_add_field_c(void *, const char *, const char *, int *);
 
 
+/*
+ *  Stack node type used for basic syntax checking of XML
+ */
+struct stacknode {
+	int line;
+	char name[MSGSIZE];
+	struct stacknode *next;
+};
+
+struct stacknode *head = NULL;
+
+
 /* 
- * Global variables
+ *  Global variables
  */
 static char *global_file;
-
-#define MSGSIZE 128
 
 
 /*********************************************************************************
@@ -57,6 +69,137 @@ void fmt_warn(const char *mesg)
 	fprintf(stderr,"********************************************************************************\n");
 	fprintf(stderr,"Warning: In file %s, %s\n", global_file, mesg);
 	fprintf(stderr,"********************************************************************************\n");
+}
+
+
+/*********************************************************************************
+ *
+ *  Function: push_tag
+ *
+ *  Pushes a new node onto the stack.
+ *
+ *********************************************************************************/
+void push_tag(struct stacknode *node)
+{
+	if (node != NULL) {
+		node->next = head;
+		head = node;
+	}
+}
+
+
+/*********************************************************************************
+ *
+ *  Function: pop_tag
+ *
+ *  Pops a new node from the stack.
+ *
+ *********************************************************************************/
+struct stacknode * pop_tag(void)
+{
+	struct stacknode *retval;
+
+	retval = head;
+	if (head != NULL) {
+		head = head->next;
+	}
+
+	return retval;
+}
+
+
+/*********************************************************************************
+ *
+ *  Function: parse_xml_tag_name
+ *
+ *  Copies only the name of an XML tag from tag_buf into tag_name. For example,
+ *  the name of the tag
+ * 
+ *      <stream name="floop" interval="06:00:00"/>
+ *
+ *  is the string "stream".
+ *
+ *********************************************************************************/
+void parse_xml_tag_name(char *tag_buf, char *tag_name)
+{
+	size_t i;
+
+	/* Assume that a name ends with a space or null character */
+	i = 0;
+	while (tag_buf[i] != ' ' && tag_buf[i] != '\0') {
+		tag_name[i] = tag_buf[i];
+		i++;
+	}
+
+	tag_name[i] = '\0';
+}
+
+
+/*********************************************************************************
+ *
+ *  Function: parse_xml_tag
+ *
+ *  Parses the next XML tag into a string, plus other bookkeeping. All characters 
+ *  between the first '<' and immediately following '>' character from xml_buf
+ *  are copied into tag. The length of the buffer xml_buf is at least buf_len, and
+ *  the length of the buffer tag is also at least buf_len.
+ *
+ *  For providing useful error messages, this routine also counts line numbers, 
+ *  incrementing the line number each time a newline character is encountered.
+ *
+ *  The output argument start_line provides the line number on which the returned
+ *  tag began.
+ *
+ *  The output argument tag_len provides the number of characters in the tag that
+ *  were copied into the tag buffer. If no complete XML tag is found in the input 
+ *  buffer, the tag_len argument will be set to 0.
+ *
+ *********************************************************************************/
+size_t parse_xml_tag(char *xml_buf, size_t buf_len, char *tag, size_t *tag_len, int *line, int *start_line)
+{
+	size_t i, j;
+
+	/* Look for beginning of tag */
+	i = 0;
+	while (i < buf_len && xml_buf[i] != '<') {
+		if (xml_buf[i] == '\n')
+			(*line)++;
+		i++;
+	}
+
+	/* Ran out of characters... */
+	if (i == buf_len) {
+		*tag_len = 0;
+		return 0;
+	}
+
+
+	/* Move on to next character after opening '<' */
+	*start_line = *line;
+	i++;
+
+	/* Copy tag into string */
+	j = 0;
+	while (i < buf_len && xml_buf[i] != '>') {
+		if (xml_buf[i] == '\n')
+			(*line)++;
+		tag[j] = xml_buf[i];
+		i++;
+		j++;
+	}
+
+	/* Didn't find a closing '>' character */
+	if (i == buf_len) {
+		*tag_len = 0;
+		return 0;
+	}
+
+	tag[j] = '\0';
+	i++;
+
+	*tag_len = j;
+
+	return i;
 }
 
 
@@ -302,6 +445,7 @@ int check_streams(ezxml_t streams)
 		/* If fields are specified in a separate file, that file should exist */
 		for (test_xml = ezxml_child(stream_xml, "file"); test_xml; test_xml = ezxml_next(test_xml)) {
 			filename = ezxml_attr(test_xml, "name");
+/* TODO: should this also be done only on the master task? */
 			if (access(filename, F_OK|R_OK) == -1) {
 				snprintf(msgbuf, MSGSIZE, "definition of stream \"%s\" references file %s that cannot be opened for reading.", name, filename);
 				fmt_err(msgbuf);
@@ -335,6 +479,163 @@ int check_streams(ezxml_t streams)
 
 /*********************************************************************************
  *
+ *  Function: xml_syntax_check
+ *
+ *  Performs a few basic syntax checks on a buffer containing XML:
+ *  1) Are the angle brackets balanced?
+ *  2) Are the quotes balanced?
+ *  3) Are all XML tags closed and nested properly?
+ *
+ *  There are clearly many syntax errors that this code will not catch, e.g., attribute
+ *  values that contain no quotes at all; similarly, there are syntactically correct
+ *  situations that this code will flag as bad, e.g., quoted strings that contain the
+ *  '=' character. If we really wanted to be thorough, we should employ a proper parser
+ *  with a well-specified grammar.
+ *
+ *********************************************************************************/
+int xml_syntax_check(char *xml_buf, size_t bufsize)
+{
+	size_t i;
+	size_t len;
+	int nleft, nright, line, start_line;
+	char msgbuf[MSGSIZE];
+	char *tag_buf;
+	struct stacknode *node;
+	struct stacknode tmp_node;
+
+
+	/* 
+	 *  Check that we have balanced angle brackets 
+	 */
+	nleft = 0;
+	nright = 0;
+	line = 1;
+	for (i=0; i<bufsize; i++) {
+		if (xml_buf[i] == '<') {
+			nleft++;
+			if (nleft - nright > 1) {
+				snprintf(msgbuf, MSGSIZE, "line %i, unexpected \'<\' character. Is the previous XML tag missing a \'>\'?", line);
+				fmt_err(msgbuf);
+				return 1;
+			}
+		}
+		else if (xml_buf[i] == '>') {
+			nright++;
+			if (nleft != nright) {
+				snprintf(msgbuf, MSGSIZE, "line %i, unexpected \'>\' character. Is the XML tag missing a \'<\'?", line);
+				fmt_err(msgbuf);
+				return 1;
+			}
+		}
+		else if (xml_buf[i] == '\n') {
+			line++;
+		}
+	}
+	if (nleft != nright) {                                  /* Probably only triggered if no final '>' character? */
+		fmt_err("unbalanced angle brackets in XML. Is the file missing a final \'>\'?");
+		return 1;
+	}
+
+
+	/*
+	 *  Check that we have balanced quotes
+	 *  NB: This simple logic WILL NOT WORK if quoted strings are allowed to contain the '=' character!
+	 */
+	nleft = 0;
+	line = 1;
+	for (i=0; i<bufsize; i++) {
+		if (xml_buf[i] == '"') {
+			nleft = (nleft + 1) % 2;
+		}
+
+		/* 
+		 *  When we reach the end of a line or the beginning of a new attribute definition,
+		 *     the quotes should be balanced...
+		 */
+		if (xml_buf[i] == '=' || xml_buf[i] == '\n') {
+			if (nleft != 0) {
+				snprintf(msgbuf, MSGSIZE, "line %i, unterminated string. Is a closing quote not present?", line);
+				fmt_err(msgbuf);
+				return 1;
+			}
+			if (xml_buf[i] == '\n') {
+				line++;
+			}
+		}
+	}
+	if (nleft != 0) {
+		fmt_err("unbalanced quotes in XML.");
+		return 1;
+	}
+
+
+	/* 
+	 *  Check that each tag is closed
+	 */
+	i = 0;
+	tag_buf = (char *)malloc(bufsize);
+
+	line = 1;
+	do {
+		i += parse_xml_tag(&xml_buf[i], bufsize, tag_buf, &len, &line, &start_line);
+
+		if (len > 0) {
+
+			/* An opening tag. Push it onto the stack... */
+			if (tag_buf[0] != '/' && tag_buf[len-1] != '/') {
+				node = (struct stacknode *)malloc(sizeof(struct stacknode));
+				parse_xml_tag_name(tag_buf, node->name);
+				node->line = start_line;
+				push_tag(node);
+			}
+			/* A closing tag. Pop the stack... */
+			else if (tag_buf[0] == '/' && tag_buf[len-1] != '/') {
+				node = pop_tag();
+				parse_xml_tag_name(&tag_buf[1], tmp_node.name);    /* NB: &tag_buf[1] to skip over '/' character */
+				if (strncmp(tmp_node.name, node->name, (size_t)MSGSIZE) != 0) {
+					fprintf(stderr, "Found unexpected closing tag \"%s\" at line %i.\n", tmp_node.name, start_line);
+					snprintf(msgbuf, MSGSIZE, "line %i, unclosed or badly nested XML tag \"%s\".", node->line, node->name);
+					fmt_err(msgbuf);
+
+					while ((node = pop_tag()) != NULL) 
+						free(node);
+					return 1;	
+				}
+				free(node);
+			}
+			/* A singleton tag. Life is simple... */
+			else if (tag_buf[0] != '/' && tag_buf[len-1] == '/') {
+				parse_xml_tag_name(tag_buf, tmp_node.name);
+
+			}
+			/* Probable syntax error? */
+			else {
+				
+			}
+
+		}
+
+	} while (i <= bufsize && len > 0);
+
+	/* Pop the rest of the stack for any unclosed tags */
+	node = pop_tag();
+	if (node != NULL) {
+		snprintf(msgbuf, MSGSIZE, "line %i, unclosed or badly nested XML tag \"%s\".", node->line, node->name);
+		fmt_err(msgbuf);
+		
+		while ((node = pop_tag()) != NULL) 
+			free(node);
+		return 1;	
+	}
+
+	free(tag_buf);
+
+	return 0;
+}
+
+
+/*********************************************************************************
+ *
  *  Function: xml_stream_parser
  *
  *  Parses an XML file and builds streams using the MPAS_stream_manager module
@@ -362,7 +663,7 @@ void xml_stream_parser(char *fname, void *manager, int *mpi_comm, int *status)
 	int err;
 
 
-	fprintf(stderr, "MGD DEV begin parsing run-time I/O from %s\n", fname);
+	fprintf(stderr, "\nParsing run-time I/O configuration from %s ...\n", fname);
 	*status = 0;
 
 	global_file = fname;
@@ -380,10 +681,12 @@ void xml_stream_parser(char *fname, void *manager, int *mpi_comm, int *status)
 		return;
 	}	
 
+/*  check_streams now called from xml_stream_get_filename()
 	if (check_streams(streams) != 0) {
 		*status = 1;
 		return;
 	}
+*/
 
 	err = 0;
 
@@ -393,7 +696,8 @@ void xml_stream_parser(char *fname, void *manager, int *mpi_comm, int *status)
 		direction = ezxml_attr(stream_xml, "type");
 		filename_template = ezxml_attr(stream_xml, "filename_template");
 		records = ezxml_attr(stream_xml, "records_per_file");
-fprintf(stderr, "MGD DEV Found stream named %s\n", streamID);
+
+		fprintf(stderr, "   found immutable stream %s\n", streamID);
 
 		irecs = atoi(records);
 
@@ -420,7 +724,8 @@ fprintf(stderr, "MGD DEV Found stream named %s\n", streamID);
 		direction = ezxml_attr(stream_xml, "type");
 		filename_template = ezxml_attr(stream_xml, "filename_template");
 		records = ezxml_attr(stream_xml, "records_per_file");
-fprintf(stderr, "MGD DEV Found stream named %s\n", streamID);
+
+		fprintf(stderr, "   found mutable stream %s\n", streamID);
 
 		irecs = atoi(records);
 
@@ -446,7 +751,6 @@ fprintf(stderr, "MGD DEV Found stream named %s\n", streamID);
 			fd = fopen(varfile, "r");
 			if (fd != NULL) {
 				while (fscanf(fd, "%s", fieldname) != EOF) {
-fprintf(stderr, "Adding %s to stream %s\n", fieldname, streamID);
        			 		stream_mgr_add_field_c(manager, streamID, (const char *)fieldname, &err);
 					if (err != 0) {
 						*status = 1;
@@ -464,7 +768,6 @@ fprintf(stderr, "Adding %s to stream %s\n", fieldname, streamID);
 		}
 		for (var_xml = ezxml_child(stream_xml, "var"); var_xml; var_xml = ezxml_next(var_xml)) {
 			fieldname_const = ezxml_attr(var_xml, "name");
-fprintf(stderr, "Adding %s to stream %s\n", fieldname_const, streamID);
 			stream_mgr_add_field_c(manager, streamID, fieldname_const, &err);
 			if (err != 0) {
 				*status = 1;
@@ -475,7 +778,7 @@ fprintf(stderr, "Adding %s to stream %s\n", fieldname_const, streamID);
 
 	free(xml_buf);
 
-	fprintf(stderr, "MGD DEV done parsing run-time I/O\n");
+	fprintf(stderr, "... done parsing run-time I/O\n\n");
 }
 
 
@@ -509,6 +812,11 @@ void xml_stream_get_filename(char *fname, char *streamname, int *mpi_comm, char 
 		return;
 	}
 
+	if (xml_syntax_check(xml_buf, bufsize) != 0) {
+		*status = 1;
+		return;
+	}
+
 	streams = ezxml_parse_str(xml_buf, bufsize);
 	if (!streams) {
 		fprintf(stderr, "********************************************************************************\n\n");
@@ -518,7 +826,6 @@ void xml_stream_get_filename(char *fname, char *streamname, int *mpi_comm, char 
 		return;
 	}	
 
-/* TODO: is it better to check the streams here, or later when xml_stream_parser is called? */
 	if (check_streams(streams) != 0) {
 		*status = 1;
 		return;
