9defb633a3a3e08bcce9b339b6f29cc59b541558
Stefan Muszala <muszala@ucar.edu>
2014-10-15 18:23:33 +0000

    first attempt to hook example stubs into cmake build

--- scripts/python/buildPio.py
+++ scripts/python/buildPio.py
@@ -40,15 +40,10 @@ def main(argv):
     useMsg = ("Build PIO and unit tests outside of the CESM framework."
               "Mostly for development and testing for faster turn-around."
               " >> python scripts/python/buildPio.py <compiler>")
-    #
-    # instantiate class to get directory name from CLI
-    #
+
     cliP = parsers.cliParser()
     args = cliP.doCliParse(useMsg)
 
-    #
-    # cmake invocation and build take place in runBuild
-    #
     runBuild(args.compilerName)
 
 if __name__ == "__main__":
