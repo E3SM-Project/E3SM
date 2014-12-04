5d5cfd4d6c37c980f69fdaedb33ac9ed3ca5dc05
jroverf@sandia.gov <jroverf@sandia.gov@b670e762-8b1b-0410-9e79-e6a6fc92c689>
2014-12-04 14:14:54 +0000

    Add a function for subcell fluxes of the Laplace operator.
    This allows the computation of the weak integral form of the
    Laplace operator to be replaced by the boundary integral.
    The unit cells are the mesh elements chopped up into a regular
    checkerboard of subcells. Input is the field on the element.
    What is returned is an N by N by 4 array giving the fluxes
    for each sub-element. Note that this is redundant since
    neighboring elements have the same fluxes on shared sides.
    It is a thing Mark wanted.
    
    Also added a unit test that can be enabled by changing
    an #if 0 in sweq_mod.F90.
    
    
    
    
    git-svn-id: file:///Users/mataylo/repos/homme/trunk@4415 b670e762-8b1b-0410-9e79-e6a6fc92c689
    
    Former-commit-id: 4055dcc6a25127e60592498e95e79ac60f87e762

--- src/sweq_mod.F90
+++ src/sweq_mod.F90
@@ -80,8 +80,10 @@ contains
     use reduction_mod, only : parallelmax
     use mesh_mod, only : MeshUseMeshFile
     use unit_tests_mod, only : test_ibyp, test_edge_flux, &
-                              test_sub_integration, test_subcell_dss_fluxes, &
-                              test_subcell_div_fluxes  ! dont remove
+                              test_sub_integration,       &
+                              test_subcell_dss_fluxes,    &
+                              test_subcell_div_fluxes,    &
+                              test_subcell_Laplace_fluxes  ! dont remove
 
     
     implicit none
@@ -315,6 +317,7 @@ contains
     call test_sub_integration(elem,deriv,nets,nete)
     call test_subcell_dss_fluxes(elem,deriv,nets,nete)
     call test_subcell_div_fluxes(elem,deriv,nets,nete)
+    call test_subcell_Laplace_fluxes(elem,deriv,nets,nete)
     stop
 #endif
 
