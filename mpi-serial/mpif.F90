05b0b62348783f5631cbaf2b6e12015e4353fefc
Robert Jacob <jacob@mcs.anl.gov>
2014-12-05 17:32:28 -0600

    Use selected_int_kind(13) to get an 8-byte integer
    Needed for tests to compile with NAG compiler. See Issue #5.

--- mpi-serial/mpif.F90
+++ mpi-serial/mpif.F90
@@ -4,13 +4,8 @@
 
 Module mpi
 implicit none
-! MPI_ADDRESS_KIND: typekind 4 should be 4 byte integer,
-!                   and typekind 8 be 8-byte integer, etc.
-#ifdef SIZEOF_LONG
-        INTEGER, PARAMETER, PUBLIC :: MPI_ADDRESS_KIND=SIZEOF_LONG
-#else
-        INTEGER, PARAMETER, PUBLIC :: MPI_ADDRESS_KIND=8
-#endif
+! MPI_ADDRESS_KIND: need an 8-byte integer.
+        INTEGER, PARAMETER, PUBLIC :: MPI_ADDRESS_KIND=selected_int_kind(13)
 
 
         include "mpif.h"
