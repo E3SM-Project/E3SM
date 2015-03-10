406daf95e60da0cb65aed443947e77058696503b
Jim Edwards <edwards.jim@gmail.com>
2015-03-10 14:40:03 +0000

    mail results of cetus testing

--- scripts/cetus_test.sh
+++ scripts/cetus_test.sh
@@ -1,4 +1,4 @@
 #!/bin/csh -f
 setenv LOCARGS "--block ${COBALT_PARTNAME}"
 ctest --verbose
-mail -s'cetus pio2 tests' jedwards@ucar.edu -a pio2tests.out
+grep 'tests passed' ./pio2build.out | mail -s'cetus pio2 tests' jedwards@ucar.edu
