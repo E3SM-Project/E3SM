c1da7a79228b4db95af7f11821818cd730578da4
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2015-04-03 11:10:21 -0600

    Combining forward and analysis modes of the ocean core
    This commit combines the forward and analysis build modes of the ocean
    core into a single executable that is controllable via namelist options.
    
    To build the model now, you can use:
        make target CORE=ocean
    
    This creates ocean_model. In the namelist file, there is an option named
    "config_run_mode" that controls the run_mode for the executable (setting
    it to 'forward' will cause the executable to run in forward mode, while
    a value of 'analysis' will cause the executable to run in the analysis
    mode).

--- build_options.mk
+++ build_options.mk
@@ -1,17 +1,7 @@
 PWD=$(shell pwd)
-ifeq "$(MODE)" "analysis"
-	EXE_NAME=ocean_analysis_model
-	NAMELIST_SUFFIX=ocean_analysis
-	FCINCLUDES += -I$(PWD)/src/core_ocean/mode_analysis -I$(PWD)/src/core_ocean/shared -I$(PWD)/src/core_ocean/analysis_members -I$(PWD)/src/core_ocean/cvmix
-else ifeq "$(MODE)" "forward"
-	EXE_NAME=ocean_forward_model
-	NAMELIST_SUFFIX=ocean_forward
-	FCINCLUDES += -I$(PWD)/src/core_ocean/mode_forward -I$(PWD)/src/core_ocean/shared -I$(PWD)/src/core_ocean/analysis_members -I$(PWD)/src/core_ocean/cvmix
-else
-	EXE_NAME=ocean*_model
-	NAMELIST_SUFFIX=ocean*
-endif
+EXE_NAME=ocean_model
+NAMELIST_SUFFIX=ocean
+FCINCLUDES += -I$(PWD)/src/core_ocean/driver -I$(PWD)/src/core_ocean/mode_forward -I$(PWD)/src/core_ocean/mode_analysis -I$(PWD)/src/core_ocean/shared -I$(PWD)/src/core_ocean/analysis_members -I$(PWD)/src/core_ocean/cvmix
 
 report_builds:
-	@echo "CORE=ocean MODE=analysis"
-	@echo "CORE=ocean MODE=forward"
+	@echo "CORE=ocean"
