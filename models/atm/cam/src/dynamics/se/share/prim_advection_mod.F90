d99eb3fd79d89e516ee6535e249c8988af0f3386
Matt Norman <normanmr@ornl.gov>
2015-04-22 10:50:59 -0400

    Separating tracer precalculations for porting
    The precalculations of divdp and divdp_proj via divergence_sphere of vn0 is being separated into a subroutine so that it may be hijcked for CUDA implementation later.

--- models/atm/cam/src/dynamics/se/share/prim_advection_mod.F90
+++ models/atm/cam/src/dynamics/se/share/prim_advection_mod.F90
@@ -1158,9 +1158,6 @@ contains
     integer              , intent(in   ) :: nets
     integer              , intent(in   ) :: nete
 
-    real (kind=real_kind), dimension(np,np,2     ) :: gradQ
-    real (kind=real_kind), dimension(np,np  ,nlev) :: dp_star
-    real (kind=real_kind), dimension(np,np  ,nlev) :: dp_np1
     integer :: i,j,k,l,ie,q,nmin
     integer :: nfilt,rkstage,rhs_multiplier
     integer :: n0_qdp, np1_qdp
@@ -1181,18 +1178,7 @@ contains
     !                            it is not needed 
     ! Also: save a copy of div(U dp) in derived%div(:,:,:,1), which will be DSS'd 
     !       and a DSS'ed version stored in derived%div(:,:,:,2)
-    do ie=nets,nete
-#if (defined ELEMENT_OPENMP)
-!$omp parallel do private(k, gradQ)
-#endif
-      do k=1,nlev
-        ! div( U dp Q), 
-        gradQ(:,:,1)=elem(ie)%derived%vn0(:,:,1,k)
-        gradQ(:,:,2)=elem(ie)%derived%vn0(:,:,2,k)
-        elem(ie)%derived%divdp(:,:,k) = divergence_sphere(gradQ,deriv,elem(ie))
-      enddo
-      elem(ie)%derived%divdp_proj(:,:,:) = elem(ie)%derived%divdp(:,:,:)
-    enddo
+    call precompute_divdp( elem , deriv , nets , nete )
 
     !rhs_multiplier is for obtaining dp_tracers at each stage:
     !dp_tracers(stage) = dp - rhs_multiplier*dt*divdp_proj
@@ -1249,6 +1235,26 @@ contains
                      (rkstage-1)*elem(ie)%state%Qdp(:,:,:,1:qsize,np1_qdp) ) / rkstage
     enddo
   end subroutine qdp_time_avg
+
+!-----------------------------------------------------------------------------
+!-----------------------------------------------------------------------------
+
+  subroutine precompute_divdp( elem , deriv , nets , nete )
+    implicit none
+    type(element_t)      , intent(inout) :: elem(:)
+    type (derivative_t)  , intent(in   ) :: deriv
+    integer              , intent(in   ) :: nets , nete
+    integer :: ie , k
+    do ie = nets , nete
+#if (defined ELEMENT_OPENMP)
+!$omp parallel do private(k)
+#endif
+      do k = 1 , nlev   ! div( U dp Q), 
+        elem(ie)%derived%divdp(:,:,k) = divergence_sphere(elem(ie)%derived%vn0(:,:,:,k),deriv,elem(ie))
+      enddo
+      elem(ie)%derived%divdp_proj(:,:,:) = elem(ie)%derived%divdp(:,:,:)
+    enddo
+  end subroutine precompute_divdp
 
 !-----------------------------------------------------------------------------
 !-----------------------------------------------------------------------------
