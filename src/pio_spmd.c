d20bfe2d626010f5c35e567d53716ca76443ff0d
Jim Edwards <jedwards@ucar.edu>
2015-05-10 12:12:03 -0600

    acknowledge Worley contribution2

--- src/pio_spmd.c
+++ src/pio_spmd.c
@@ -1,6 +1,6 @@
 ////
 /// @file pio_spmd.c
-/// @author Original algorithms by Pat Worley; C translation by Jim Edwards.
+/// @author Algorithms modeled after spmd_utils in the Community Atmosphere Model; C translation Jim Edwards
 /// @date 2014
 /// @brief MPI_Gather, MPI_Gatherv, and MPI_Alltoallw with flow control options
 ///
