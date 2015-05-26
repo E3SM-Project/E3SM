73dde714ebfa219af77134bc43f4a9e6f2a80a25
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2015-05-26 15:24:35 -0600

    Initialize `shr_wv_sat_mod` in the driver.
    This commit adds namelist variables to control `shr_wv_sat_mod`, and
    initializes it from the driver, allowing this code to be used in flux
    calculations or by the various components.
    
    Note that this will not change answers until some code begins to
    actually use this module.
    
    Testing:
    
        Test suite: Unit tests and driver (CIME) tests.
        Baseline: cesm1_4_alpha05c
        Namelist changes:
            - Added variables `wv_sat_scheme`, `wv_sat_transition_start`,
              `wv_sat_use_tables`, and `wv_sat_table_spacing` to the
              infodata namelist.
    
        Status: bit for bit
    
    Code reviews:
    
         None.

--- driver-mct/driver/cesm_comp_mod.F90
+++ driver-mct/driver/cesm_comp_mod.F90
@@ -730,9 +730,24 @@ end subroutine cesm_pre_init1
 
 subroutine cesm_pre_init2()
    use pio, only : file_desc_t, pio_closefile, pio_file_is_open
+   use shr_const_mod, only: shr_const_tkfrz, shr_const_tktrip, &
+        shr_const_mwwv, shr_const_mwdair
+   use shr_wv_sat_mod, only: wv_sat_set_default, wv_sat_init, &
+        WVSatTableSpec, wv_sat_make_tables
    implicit none
    type(file_desc_t) :: pioid
    integer :: maxthreads
+
+   character(CS) :: wv_sat_scheme
+   real(r8) :: wv_sat_transition_start
+   logical :: wv_sat_use_tables
+   real(r8) :: wv_sat_table_spacing
+   character(CL) :: errstring
+
+   type(WVSatTableSpec) :: liquid_spec, ice_spec, mixed_spec
+
+   real(r8), parameter :: epsilo = shr_const_mwwv/shr_const_mwdair
+
    !----------------------------------------------------------
    ! Print Model heading and copyright message
    !----------------------------------------------------------
@@ -930,6 +945,39 @@ subroutine cesm_pre_init2()
            orb_lambm0=orb_lambm0,         &
            orb_mvelpp=orb_mvelpp)
    endif
+
+   call seq_infodata_getData(infodata,                   &
+        wv_sat_scheme=wv_sat_scheme,                     &
+        wv_sat_transition_start=wv_sat_transition_start, &
+        wv_sat_use_tables=wv_sat_use_tables,             &
+        wv_sat_table_spacing=wv_sat_table_spacing)
+
+   if (.not. wv_sat_set_default(wv_sat_scheme)) then
+      call shr_sys_abort('Invalid wv_sat_scheme.')
+   end if
+
+   call wv_sat_init(shr_const_tkfrz, shr_const_tktrip, &
+        wv_sat_transition_start, epsilo, errstring)
+
+   if (errstring /= "") then
+      call shr_sys_abort('wv_sat_init: '//trim(errstring))
+   end if
+
+   ! The below produces internal lookup tables in the range 175-374K for
+   ! liquid water, and 125-274K for ice, with a resolution set by the
+   ! option wv_sat_table_spacing.
+   ! In theory these ranges could be specified in the namelist, but in
+   ! practice users will want to change them *very* rarely if ever, which
+   ! is why only the spacing is in the namelist.
+   if (wv_sat_use_tables) then
+      liquid_spec = WVSatTableSpec(ceiling(200._r8/wv_sat_table_spacing), &
+           175._r8, wv_sat_table_spacing)
+      ice_spec = WVSatTableSpec(ceiling(150._r8/wv_sat_table_spacing), &
+           125._r8, wv_sat_table_spacing)
+      mixed_spec = WVSatTableSpec(ceiling(250._r8/wv_sat_table_spacing), &
+           125._r8, wv_sat_table_spacing)
+      call wv_sat_make_tables(liquid_spec, ice_spec, mixed_spec)
+   end if
 
    call seq_infodata_putData(infodata, &
         atm_phase=1,                   &
