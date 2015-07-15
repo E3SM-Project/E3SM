f23cc1285eceacc0bccbe0ddd621fdb0d0a75ca6
Matthew Hoffman <mhoffman@lanl.gov>
2015-07-15 11:53:52 -0600

    Update build_options.mk for use in standalone or ACME

--- build_options.mk
+++ build_options.mk
@@ -1,6 +1,9 @@
-PWD=$(shell pwd)
+ifeq "$(ROOT_DIR)" ""
+        ROOT_DIR=$(shell pwd)/src
+endif
 EXE_NAME=landice_model
 NAMELIST_SUFFIX=landice
+FCINCLUDES += -I$(ROOT_DIR)/core_landice
 override CPPFLAGS += -DCORE_LANDICE
 
 report_builds:
