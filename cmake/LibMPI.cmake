1bba202da8a112bf4876a8982422fe437610f830
Kevin Paul <kpaul@ucar.edu>
2015-08-18 14:31:11 -0600

    Simplifying UCAR/NCAR test runs.

--- cmake/LibMPI.cmake
+++ cmake/LibMPI.cmake
@@ -69,27 +69,9 @@ function (add_mpi_test TESTNAME)
     
     # UCAR LSF execution
     if (PLATFORM STREQUAL "ucar")
-        ###
-        ### note: no space between -n and num_procs for mpirun.lsf on
-        ### yellowstone
-        ###
-        set (EXE_CMD execca mpirun.lsf ${exe_cmds} -n${num_procs})
+
+        set (EXE_CMD mpirun.lsf ${exe_cmds})
         
-    # Argonne COBALT execution
-    elseif (PLATFORM STREQUAL "alcf" )
-        ###
-        ###
-        #set(PIO_RUNJOB ${CMAKE_BINARY_DIR}/scripts/pio_runjob.sh)
-        set (REQUIRED_OPTION --block \$ENV{COBALT_PARTNAME}) 
-        set (RUNJOB_NPF --np ${num_procs})
-        if (DEFINED ENV{BGQ_RUNJOB})
-          set (RUNJOB $ENV{BGQ_RUNJOB})
-        else()
-          set (RUNJOB runjob)
-        endif()
-        set (EXE_CMD ${RUNJOB} ${RUNJOB_NPF} ${REQUIRED_OPTION}
-                     ${MPIEXEC_PREFLAGS} : ${exe_cmds})
-    
     # All others (assume can run MPIEXEC directly)
     else()
         set(MPIEXEC_NPF ${MPIEXEC_NUMPROC_FLAG} ${num_procs})
