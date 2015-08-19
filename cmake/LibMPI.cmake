7976cc6b27681c41fcd2a1ce1b60d8d98122fa7f
Kevin Paul <kpaul@ucar.edu>
2015-08-19 10:46:07 -0600

    Removing debugging statements

--- cmake/LibMPI.cmake
+++ cmake/LibMPI.cmake
@@ -69,9 +69,6 @@ function (add_mpi_test TESTNAME)
     # Get the platform name
     platform_name (PLATFORM)
     
-    message ("exec_file = ${exec_file}")
-    message ("exec_args = ${exec_args}")
-    
     # Default ("unknown" platform) execution
     if (PLATFORM STREQUAL "unknown")
 
