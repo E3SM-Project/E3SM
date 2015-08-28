1d83ce03a3a25006fd8b22906c3b4ef376c7162c
Kevin Paul <kpaul@ucar.edu>
2015-08-28 14:33:23 -0600

    Adding ALCF test scripts

---
+++ ctest/runctest-alcf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#==============================================================================
+#
+#  This script defines how to run CTest on the Argonne Leadership Computing
+#  Facility systems (mira/cetus/vesta/cooley).
+#
+#  This assumes the CTest model name (e.g., "Nightly") is passed to it when
+#  run.
+#
+#==============================================================================
+
+# Get the CTest model name
+model=$1
+
+# Write QSUB submission script with the test execution command
+echo "#!/bin/sh" > runctest.sh
+echo "CTESTCMD=`which ctest`"
+echo "\$CTESTCMD -S ctest/CTestScript-Test.cmake,${model} -V"
+
+# Make the QSUB script executable
+chmod +x runctest.sh
+
+# Submit the job to the queue
+jobid=`qsub -t 20 -n 4 --proccount 4 --mode script runctest.sh`
+
+# Wait for the job to complete before exiting
+while true; do
+	status=`qstat $jobid`
+	if [ "$status" == "" ]; then
+		break
+	else
+		sleep 10
+	fi
+done
