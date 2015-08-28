a9e44463a92ad4bee0674c1b2b818553cb3966fa
Kevin Paul <kpaul@ucar.edu>
2015-08-28 09:14:41 -0600

    Adding environment setting script for default machines

---
+++ ctest/CTestEnvironment-unknown.cmake
@@ -0,0 +1,11 @@
+#==============================================================================
+#
+#  This file sets the environment variables needed to configure and run
+#  CTest on the default ("unknown") system.
+#
+#==============================================================================
+
+# Assume all package locations (NetCDF, PnetCDF, HDF5, etc) are already
+# set with existing environment variables: NETCDF, PNETCDF, HDF5, etc.
+
+set (ENV{ABC} 123)
