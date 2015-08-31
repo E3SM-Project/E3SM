6dc107a44798957e7f31f823d7b076fc3ca308d6
Kevin Paul <kpaul@ucar.edu>
2015-08-31 14:53:21 -0600

    Adding initial CDash run script for NERSC

---
+++ ctest/runcdash-nersc-intel.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Get/Generate the Dashboard Model
+if [ $# -eq 0 ]; then
+	model=Experimental
+else
+	model=$1
+fi
+
+module purge
+module load PrgEnv-intel
+module load git/2.4.6 
+module load cmake/3.0.0
+module load cray-hdf5-parallel/1.8.14
+module load cray-netcdf-hdf5parallel/4.3.3.1
+module load cray-parallel-netcdf/1.6.0
+
+export PIO_DASHBOARD_ROOT=`pwd`/dashboard
+export PIO_COMPILER_ID=Intel-15.0.1
+
+export CC=cc
+export FC=ftn
+
+if [ ! -d "$PIO_DASHBOARD_ROOT" ]; then
+  mkdir "$PIO_DASHBOARD_ROOT"
+fi
+cd "$PIO_DASHBOARD_ROOT"
+
+if [ ! -d src ]; then
+  git clone https://github.com/PARALLELIO/ParallelIO src
+fi
+cd src
+
+ctest -S CTestScript.cmake,${model} -VV
