94dcf85275403d23e2b7365ea32458c92e29f952
Kevin Paul <kpaul@ucar.edu>
2015-09-09 16:02:48 -0600

    Locking software versions and cleaning environment on ALCF

--- ctest/runcdash-alcf-ibm.sh
+++ ctest/runcdash-alcf-ibm.sh
@@ -11,13 +11,13 @@ fi
 GIT=/soft/versioning/git/2.3.0/bin/git
 CTEST=/soft/buildtools/cmake/3.3.0/bin/ctest
 
-export LIBZ=/soft/libraries/alcf/current/xl/ZLIB/
-export HDF5=/soft/libraries/hdf5/current/cnk-xl/current
-export NETCDF=/soft/libraries/netcdf/current/cnk-xl/current
-export PNETCDF=/soft/libraries/pnetcdf/current/cnk-xl/current
+export LIBZ=/soft/libraries/alcf/current/xl/ZLIB/lib
+export HDF5=/soft/libraries/hdf5/1.8.14/cnk-xl/V1R2M2-20150213
+export NETCDF=/soft/libraries/netcdf/4.3.3-f4.4.1/cnk-xl/V1R2M2-20150213
+export PNETCDF=/soft/libraries/pnetcdf/1.6.0/cnk-xl/V1R2M2-20150213
 
-export CC=/soft/compilers/wrappers/xl/mpicc
-export FC=/soft/compilers/wrappers/xl/mpif90
+export CC=/soft/compilers/wrappers/xl/mpixlc_r
+export FC=/soft/compilers/wrappers/xl/mpixlf90_r
 
 export PIO_DASHBOARD_ROOT=`pwd`/dashboard
 export PIO_COMPILER_ID=Cray-`$CC -qversion | head -n 2 | tail -n 1 | cut -d' ' -f2`
