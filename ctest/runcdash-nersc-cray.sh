0766adb82700ccf2313007ba7f0baf9ab980a5ee
Kevin Paul <kpaul@ucar.edu>
2015-09-10 11:30:41 -0600

    Moving back to default mpich and bumping down cce version to 8.3.12

--- ctest/runcdash-nersc-cray.sh
+++ ctest/runcdash-nersc-cray.sh
@@ -11,8 +11,8 @@ module purge
 module load PrgEnv-cray
 module load craype-ivybridge
 module load cray-shmem
-module load cray-mpich/7.2.2
-module swap cce cce/8.4.0.219
+module load cray-mpich
+module swap cce cce/8.3.12
 module load torque
 module load git/2.4.6 
 module load cmake/3.0.0
