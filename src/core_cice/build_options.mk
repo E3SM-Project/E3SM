aa392d7cff2ed73d776b289b18c652e69f9a61eb
“Adrian <akt@lanl.gov>
2015-09-22 11:14:49 +0100

    Added kinetic energy and speed to regional statistics. Analysis members need to access forward model code so rearranged code and build system to allow this. Created shared directory with most of the forward model code. Renamed forward_model to model_forward to avoid issues with ignored files.

--- src/core_cice/build_options.mk
+++ src/core_cice/build_options.mk
@@ -1,7 +1,7 @@
 PWD=$(shell pwd)
 EXE_NAME=cice_model
 NAMELIST_SUFFIX=cice
-FCINCLUDES += -I$(PWD)/src/core_cice/column -I$(PWD)/src/core_cice/forward_model -I$(PWD)/src/core_cice/analysis_members
+FCINCLUDES += -I$(PWD)/src/core_cice/column -I$(PWD)/src/core_cice/shared -I$(PWD)/src/core_cice/analysis_members -I$(PWD)/src/core_cice/model_forward
 override CPPFLAGS += -DCORE_CICE
 
 report_builds:
