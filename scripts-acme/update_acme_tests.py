62e704c1bd1949ee69b76d60cae3745d080b9ea7
Matthew Hoffman <wyeast@gmail.com>
2015-10-07 14:43:14 -0600

    Add SMS.f09_g16_a.IGCLM45_MLI test
    This runs CLM+MPASLI over AIS to test SMB.

--- scripts-acme/update_acme_tests.py
+++ scripts-acme/update_acme_tests.py
@@ -28,7 +28,8 @@ _TEST_SUITES = {
                          ("SMS.ne30_f19_g16_rx1.A", None),
                          ("SMS.f19_f19.I1850CLM45CN", None),
 			 ("SMS.f09_g16.I1850CLM45CN", None),
-			 ("SMS.hcru_hcru.I1850CRUCLM45CN", None)]
+			 ("SMS.hcru_hcru.I1850CRUCLM45CN", None),
+                         ("SMS.f09_g16_a.IGCLM45_MLI", None)]
                         ),
     "acme_integration" : ("acme_developer",
                           [("ERS.ne30_g16.B1850C5", None),
