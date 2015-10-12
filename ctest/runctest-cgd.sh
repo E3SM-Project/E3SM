81e85ec0180251e07fd2134189b6cc9b18398dc4
katetc <katec@ucar.edu>
2015-10-12 15:39:44 -0600

    Trying stuff for the hobart nightly tests

--- ctest/runctest-cgd.sh
+++ ctest/runctest-cgd.sh
@@ -17,6 +17,11 @@ model=$2
 
 # Write QSUB submission script with the test execution command
 echo "#!/bin/sh" > runctest.sh
+echo "export PIO_DASHBOARD_SITE=${PIO_DASHBOARD_BUILD_NAME}"
+echo "export PIO_DASHBOARD_BUILD_NAME=${PIO_DASHBOARD_BUILD_NAME}"
+echo "export PIO_DASHBOARD_SOURCE_DIR=${PIO_DASHB0ARD_SOURCE_DIR}"
+echo "export PIO_DASHBOARD_BINARY_DIR=${PIO_DASHBOARD_BINARY_DIR}"
+
 echo "CTESTCMD=`which ctest`" >> runctest.sh
 echo "\$CTESTCMD -S ${scrdir}/CTestScript-Test.cmake,${model} -V" >> runctest.sh
 
@@ -24,12 +29,7 @@ echo "\$CTESTCMD -S ${scrdir}/CTestScript-Test.cmake,${model} -V" >> runctest.sh
 chmod +x runctest.sh
 
 # Submit the job to the queue
-jobid=`qsub -l nodes=1:ppn=4 \
-	--env PIO_DASHBOARD_SITE=$PIO_DASHBOARD_SITE \
-	--env PIO_DASHBOARD_BUILD_NAME=$PIO_DASHBOARD_BUILD_NAME \
-	--env PIO_DASHBOARD_SOURCE_DIR=$PIO_DASHBOARD_SOURCE_DIR \
-	--env PIO_DASHBOARD_BINARY_DIR=$PIO_DASHBOARD_BINARY_DIR \
-	runctest.sh`
+jobid=`qsub -l nodes=1:ppn=4 runctest.sh`
 
 # Wait for the job to complete before exiting
 #while true; do
