b68f3051ce040ec940af795e84f500c3945355c0
Azamat Mametjanov <azamat@mcs.anl.gov>
2015-10-15 01:39:48 +0000

    Build standalone HOMME on Mira

--- components/homme/cmake/machineFiles/mira.cmake
+++ components/homme/cmake/machineFiles/mira.cmake
@@ -1,29 +1,20 @@
-# CMake initial cache file for Mac OSX 10.8
-# ANL mira/cestus machines 
-# 
-# note: you need to also set this before running CMAKE:
-#    setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/soft/libraries/alcf/current/xl/BLAS/lib:/soft/libraries/alcf/current/xl/LAPACK/lib:
-#
-#
+# ANL Mira/Cetus machines
 
-SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
-SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
-SET (CMAKE_CXX_COMPILER mpicc CACHE FILEPATH "")
+SET (CMAKE_Fortran_COMPILER mpixlf90_r CACHE FILEPATH "")
+SET (CMAKE_C_COMPILER mpixlc_r CACHE FILEPATH "")
+SET (CMAKE_CXX_COMPILER mpixlcxx_r CACHE FILEPATH "")
 
 SET (CMAKE_Fortran_FLAGS "-WF,-C! -O2 -Ipreqx_modules" CACHE STRING "")
 #SET (FORCE_Fortran_FLAGS "-WF,-C!" CACHE STRING "")
-SET (ENABLE_OPENMP FALSE CACHE BOOL "")
-
+SET (ENABLE_OPENMP TRUE CACHE BOOL "")
 
 #SET (WITH_PNETCDF FALSE CACHE FILEPATH "")
-SET (PNETCDF_DIR /soft/libraries/pnetcdf/1.3.1/cnk-xl/current CACHE FILEPATH "")
-SET (NETCDF_DIR /soft/libraries/netcdf/4.2.1.1/cnk-xl/V1R2M0-20130417 CACHE FILEPATH "")
-SET (HDF5_DIR /soft/libraries/hdf5/1.8.10/cnk-xl/current CACHE FILEPATH "")
+SET (PNETCDF_DIR /soft/libraries/pnetcdf/current/cnk-xl/current CACHE FILEPATH "")
+SET (NETCDF_DIR /soft/libraries/netcdf/current/cnk-xl/current CACHE FILEPATH "")
+SET (HDF5_DIR /soft/libraries/hdf5/current/cnk-xl/current CACHE FILEPATH "")
 SET (ZLIB_DIR /soft/libraries/alcf/current/xl/ZLIB CACHE FILEPATH "")
 
 SET (USE_QUEUING FALSE CACHE BOOL "")
 SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
+SET (ENV{LD_LIBRARY_PATH} "/soft/libraries/alcf/current/xl/BLAS/lib:/soft/libraries/alcf/current/xl/LAPACK/lib:$ENV{LD_LIBRARY_PATH}")
 
-# add these to LD_LIBRARY_PATH so cmake will find them:
-# /soft/libraries/alcf/current/xl/LAPACK/lib 
-# /soft/libraries/alcf/current/xl/BLAS/lib 
