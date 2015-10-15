fff902e0c4e63d0cce48e4c8596534f3615fd1d7
katetc <katec@ucar.edu>
2015-10-15 14:32:58 -0600

    More Hobart ctest debugging.

--- ctest/runcdash-cgd-nag.sh
+++ ctest/runcdash-cgd-nag.sh
@@ -16,6 +16,7 @@ export FC=mpif90
 export PIO_DASHBOARD_SITE="CGD"
 export PIO_DASHBOARD_ROOT=/scratch/cluster/katec/dashboard
 export CTEST_SCRIPT_DIRECTORY=${PIO_DASHBOARD_ROOT}/src
+export PIO_DASHBOARD_SOURCE_DIR=${CTEST_SCRIPT_DIRECTORY}
 export PIO_COMPILER_ID=Nag-6.0-gcc-`gcc --version | head -n 1 | cut -d' ' -f3`
 
 if [ ! -d "$PIO_DASHBOARD_ROOT" ]; then
@@ -24,6 +25,7 @@ fi
 cd "$PIO_DASHBOARD_ROOT"
 
 echo "CTEST_SCRIPT_DIRECTORY="${CTEST_SCRIPT_DIRECTORY}
+echo "PIO_DASHBOARD_SOURCE_DIR="${PIO_DASHBOARD_SOURCE_DIR}
 
 if [ ! -d src ]; then
   git clone https://github.com/Katetc/ParallelIO src
