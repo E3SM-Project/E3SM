18640ba64d6abe1299bd5c1042b8c032c47bc53b
Adrian Turner <akt@lanl.gov>
2015-10-22 14:29:39 -0600

    Added "coupled" preprocessor flag when in coupled mode.

--- src/core_cice/build_options.mk
+++ src/core_cice/build_options.mk
@@ -3,6 +3,9 @@ EXE_NAME=cice_model
 NAMELIST_SUFFIX=cice
 FCINCLUDES += -I$(PWD)/src/core_cice/column -I$(PWD)/src/core_cice/forward_model -I$(PWD)/src/core_cice/analysis_members
 override CPPFLAGS += -DCORE_CICE
+ifneq "$(ESM)" ""
+override CPPFLAGS += -Dcoupled
+endif
 
 report_builds:
 	@echo "CORE=cice"
