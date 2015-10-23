40ce88b57bf243fcc6b21bd7d86fbc86a79850c4
Adrian Turner <akt@lanl.gov>
2015-10-23 12:11:50 -0600

    Added CCSMCOUPLED preprocessor directive

--- src/core_cice/build_options.mk
+++ src/core_cice/build_options.mk
@@ -4,7 +4,7 @@ NAMELIST_SUFFIX=cice
 FCINCLUDES += -I$(PWD)/src/core_cice/column -I$(PWD)/src/core_cice/forward_model -I$(PWD)/src/core_cice/analysis_members
 override CPPFLAGS += -DCORE_CICE
 ifneq "$(ESM)" ""
-override CPPFLAGS += -Dcoupled
+override CPPFLAGS += -Dcoupled -DCCSMCOUPLED
 endif
 
 report_builds:
