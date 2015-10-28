20bc249dc6f23e269391caf4c8204860104e6392
Jayesh Krishna <jayesh@mcs.anl.gov>
2015-10-28 20:05:14 +0000

    Simplifying the initialization of PIO rearranger options
    Porting pre-CIME change in #76fc9f7 to PIO in CIME
    
    After discussions with Pat Worley we decided to remove unused
    compiler options to simplify the intialization of the PIO
    rearranger options. The following compiler flags are no longer
    used during initializing the rearranger options (i.e., choosing
    the defaults for the PIO rearranger options),
    
    1. _MPISERIAL : There is custom code in PIO to handle the rerranging
      of data when using the mpi-serial lib. The flow control options are
      never used in PIO when using the mpi-serial lib.
    
    2. _USE_[COMP2IO|IO2COMP]_FC : These flags are no longer used
       (experimental flags). The user can still manipulate these flow
       control options (flow control directions io2comp and comp2io)
       by passing rearranger options to pio_init()
    
    Also see PR #159, SEG-57
    SEG-180

--- externals/pio/pio/rearr_options.h
+++ externals/pio/pio/rearr_options.h
@@ -11,29 +11,4 @@
 #define DEF_P2P_ISEND .false.
 #define DEF_P2P_MAXREQ 64
 
-#ifndef _MPISERIAL
-#ifndef _NO_FLOW_CONTROL
-#define _USE_COMP2IO_FC 1
-#define _USE_IO2COMP_FC 1
-#endif  
-#endif
-!
-! The completely unreadable nature of the following lines is required by some compilers
-!
-#ifdef _USE_ALLTOALLW
-#define DEF_COMP2IO_OPTION 0
-#define DEF_IO2COMP_OPTION 0
-#else
-#ifdef _USE_COMP2IO_FC
-#define DEF_COMP2IO_OPTION 2
-#else
-#define DEF_COMP2IO_OPTION 1
-#endif
-#ifdef _USE_IO2COMP_FC
-#define DEF_IO2COMP_OPTION 2
-#else
-#define DEF_IO2COMP_OPTION 1
-#endif
-#endif
-
 #endif
