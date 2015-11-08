367f19912c375aa40976c04e31a9e607d7dcdb8e
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2015-11-07 23:50:03 -0700

    Make `shr_RandNum_mod` more object-oriented.
    Rather than using a case statement comparing strings to figure out which
    random number generator is being used, we now have a class for each
    generation method. This greatly reduces the input necessary to construct
    a generator object.
    
    Testing:
    
        Test suite: Intel benchmark for shr_RandNum code only.
        Baseline: N/A
        Namelist changes: No
        Status: bit for bit

--- share/shr_RandNum/test/bench/test_shr_RandNum.F90
+++ share/shr_RandNum/test/bench/test_shr_RandNum.F90
@@ -2,17 +2,18 @@ program test
 
 ! this program calls the available versions of the random generators
 
-use shr_RandNum_mod, only : shr_genRandNum, shr_RandNum_init, shr_RandNum_term, shr_rand_t
+use shr_RandNum_mod, only: ShrRandGen, ShrIntrinsicRandGen, ShrKissRandGen, &
+     ShrF95MtRandGen, ShrDsfmtRandGen, ShrMklMtRandGen
 
-INTEGER,parameter :: r8 = selected_real_kind(12)
+INTEGER, parameter :: r8 = selected_real_kind(12)
 
-type (shr_rand_t) :: randStream
+class(ShrRandGen), allocatable :: randStream
 
 integer, parameter :: nstream = 16   ! number of streams of random numbers
 integer, parameter :: length  = 1000 ! length of stream of random numbers
 integer            :: ntrials = 50000
 
-integer, dimension(nstream,1) :: seed = 7776578
+integer, dimension(nstream) :: seed = 7776578
 integer, dimension(nstream,4) :: kiss_seed
 integer, dimension(:,:), allocatable :: intrinsic_seed
 
@@ -23,19 +24,24 @@ integer :: c1, c2, cr, cm
 real(r8) :: dt, dt1,dt2
 
 #ifdef INTEL_MKL
-! intel math kernel library merseene twister
+! intel math kernel library mersenne twister
+
+  allocate(randStream, source=ShrMklMtRandGen(seed))
 
   call system_clock(c1, cr, cm)
   do m = 1,ntrials
-    call shr_RandNum_init( randStream, nstream, length, 'SFMT_MKL', seed=seed )
+     call randStream%finalize()
+     deallocate(randStream)
+     allocate(randStream, source=ShrMklMtRandGen(seed))
   enddo
   call system_clock(c2, cr, cm); dt1 = dble(c2-c1)/dble(cr)
 
   call system_clock(c1, cr, cm)
   do m = 1,ntrials
-    call shr_genRandNum( randStream, array )
+    call randStream%random(array)
   enddo
-  call shr_RandNum_term(randStream)
+  call randStream%finalize()
+  deallocate(randStream)
   call system_clock(c2, cr, cm); dt2 = dble(c2-c1)/dble(cr)
   dt = dt1+dt2
   print *, 'Init time   (SFMT_MKL): ',dt1
@@ -52,16 +58,17 @@ real(r8) :: dt, dt1,dt2
   do m = 1,ntrials
     do n = 1,nstream
        do i = 1, 4
-          kiss_seed(n,i) = seed(n,1)*i+n
+          kiss_seed(n,i) = seed(n)*i+n
        end do
     end do
 
-    call shr_RandNum_init( randStream, nstream, length, 'KISSVEC', &
-         seed=kiss_seed )
+    allocate(randStream, source=ShrKissRandGen(kiss_seed))
 
-    call shr_genRandNum  ( randStream, array )
+    call randStream%random(array)
+
+    call randStream%finalize()
+    deallocate(randStream)
   enddo
-  call shr_RandNum_term(randStream)
   call system_clock(c2, cr, cm); dt = dble(c2-c1)/dble(cr)
 
   print *, 'Total time   (KISSVEC): ',dt
@@ -73,11 +80,13 @@ real(r8) :: dt, dt1,dt2
 
   call system_clock(c1, cr, cm)
   do m = 1,ntrials
-    call shr_RandNum_init( randStream, nstream, length, 'MT19937', seed=seed )
+    allocate(randStream, source=ShrF95MtRandGen(seed))
 
-    call shr_genRandNum  ( randStream, array )
+    call randStream%random(array)
+
+    call randStream%finalize()
+    deallocate(randStream)
   enddo
-  call shr_RandNum_term(randStream)
   call system_clock(c2, cr, cm); dt = dble(c2-c1)/dble(cr)
 
   print *, 'Total time   (MT19937): ',dt
@@ -93,15 +102,16 @@ real(r8) :: dt, dt1,dt2
   do m = 1,ntrials
      do n = 1, nstream
         do i = 1, intrinsic_size
-           intrinsic_seed(n,i) = seed(n,1)*i+n
+           intrinsic_seed(n,i) = seed(n)*i+n
         end do
      end do
-     call shr_RandNum_init( randStream, nstream, length, 'F90_INTRINSIC', &
-          seed=intrinsic_seed )
+    allocate(randStream, source=ShrIntrinsicRandGen(intrinsic_seed))
 
-     call shr_genRandNum  ( randStream, array )
+    call randStream%random(array)
+
+    call randStream%finalize()
+    deallocate(randStream)
   enddo
-  call shr_RandNum_term(randStream)
   call system_clock(c2, cr, cm); dt = dble(c2-c1)/dble(cr)
 
   print *, 'Total time   (F90_INTRINSIC): ',dt
@@ -109,15 +119,17 @@ real(r8) :: dt, dt1,dt2
   print *, 'Summation of Random Numbers: ', SUM(array)
   print *, '--------'; print *, ''
 
-! SIMD-orientated merseene twister
+! SIMD-orientated mersenne twister
 
   call system_clock(c1, cr, cm)
   do m = 1,ntrials
-    call shr_RandNum_init( randStream, nstream, length, 'DSFMT_F03', seed=seed )
+    allocate(randStream, source=ShrDsfmtRandGen(seed, length))
 
-    call shr_genRandNum  ( randStream, array )
+    call randStream%random(array)
+
+    call randStream%finalize()
+    deallocate(randStream)
   enddo
-  call shr_RandNum_term(randStream)
   call system_clock(c2, cr, cm); dt = dble(c2-c1)/dble(cr)
 
   print *, 'Total time   (DSFMT_F03): ',dt
