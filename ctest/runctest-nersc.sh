685bf501398971c4af5bec47352edbcb4c0e16e0
Jim Edwards <jedwards@ucar.edu>
2016-01-01 08:06:21 -0800

    update nersc ctest

--- ctest/runctest-nersc.sh
+++ ctest/runctest-nersc.sh
@@ -17,16 +17,18 @@ model=$2
 
 # Write QSUB submission script with the test execution command
 echo "#!/bin/sh" > runctest.slurm
-echo "#SBATCH -q debug" >> runctest.slurm
+echo "#SBATCH --partition debug" >> runctest.slurm
 echo "#SBATCH --nodes=1" >> runctest.slurm
 echo "#SBATCH --ntasks-per-node=32" >> runctest.slurm
+echo "#SBATCH --time=00:15:00" >> runctest.slurm
+
 echo "#SBATCH --export PIO_DASHBOARD_SITE,PIO_DASHBOARD_BUILD_NAME,PIO_DASHBOARD_SOURCE_DIR,PIO_DASHBOARD_BINARY_DIR" >> runctest.slurm
 #echo "cd \$PBS_O_WORKDIR" >> runctest.pbs
 echo "CTEST_CMD=`which ctest`" >> runctest.slurm
 echo "\$CTEST_CMD -S ${scrdir}/CTestScript-Test.cmake,${model} -V" >> runctest.slurm
 
 # Submit the job to the queue
-jobid=`sbatch runctest.pbs`
+jobid=`sbatch runctest.slurm`
 
 # Wait for the job to complete before exiting
 while true; do
