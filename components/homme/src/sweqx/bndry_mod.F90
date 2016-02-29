83279bc33230ddcda7598fa4714007d0983343fd
Matt Norman <normanmr@ornl.gov>
2016-02-29 09:22:59 -0500

    Adding support for sweqx in HOMME cmake system
    -Added overloading modules to the src/sweqx director
    -Changed CMakeLists.txt to include the _base files and new overloading files.

---
+++ components/homme/src/sweqx/bndry_mod.F90
@@ -0,0 +1,8 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+module bndry_mod
+  use bndry_mod_base
+  implicit none
+end module bndry_mod
---
+++ components/homme/src/sweqx/derivative_mod.F90
@@ -0,0 +1,8 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+module derivative_mod
+  use derivative_mod_base
+  implicit none
+end module derivative_mod
