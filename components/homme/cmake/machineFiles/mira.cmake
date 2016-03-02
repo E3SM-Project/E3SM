070d074bf680e3a65d282df0930365ee6d1cfbb9
Azamat Mametjanov <azamat@mcs.anl.gov>
2016-03-02 19:35:16 +0000

    Revert "Merge branch 'azamat/tests/homme' into next (PR #706)"
    This reverts commit deb7c8cbcc0e7027d4c50275d1288095154cc8e7, reversing
    changes made to 7dddb6e4e2eb285cba38653d0dab7753aa4d5d47.

--- components/homme/cmake/machineFiles/mira.cmake
+++ components/homme/cmake/machineFiles/mira.cmake
@@ -15,6 +15,6 @@ SET (HDF5_DIR /soft/libraries/hdf5/current/cnk-xl/current CACHE FILEPATH "")
 SET (ZLIB_DIR /soft/libraries/alcf/current/xl/ZLIB CACHE FILEPATH "")
 
 SET (USE_QUEUING FALSE CACHE BOOL "")
-#SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
+SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
 SET (ENV{LD_LIBRARY_PATH} "/soft/libraries/alcf/current/xl/BLAS/lib:/soft/libraries/alcf/current/xl/LAPACK/lib:$ENV{LD_LIBRARY_PATH}")
 
