3a820d090a9b48f315bfdad1a26015aaea3cba8e
Azamat Mametjanov <azamat@mcs.anl.gov>
2016-03-03 00:13:32 +0000

    Let HOMME find BLAS/LAPACK from PATH
    [BFB] - Bit-For-Bit

--- components/homme/cmake/machineFiles/cetus.cmake
+++ components/homme/cmake/machineFiles/cetus.cmake
@@ -17,6 +17,7 @@ SET (ZLIB_DIR /soft/libraries/alcf/current/xl/ZLIB CACHE FILEPATH "")
 SET (USE_QUEUING FALSE CACHE BOOL "")
 SET (USE_MPIEXEC runjob CACHE FILEPATH "")
 
-#SET (HOMME_FIND_BLASLAPACK FALSE CACHE BOOL "")
+SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
+SET (ENV{PATH} "/soft/libraries/alcf/current/xl/BLAS/lib:/soft/libraries/alcf/current/xl/LAPACK/lib:$ENV{PATH}")
 SET (ENV{LD_LIBRARY_PATH} "/soft/libraries/alcf/current/xl/BLAS/lib:/soft/libraries/alcf/current/xl/LAPACK/lib:$ENV{LD_LIBRARY_PATH}")
 
