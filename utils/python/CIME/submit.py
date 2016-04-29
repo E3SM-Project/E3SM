9379cdd7ce6e6cca2729fd0a62eb4f69cf1de00b
Jim Edwards <jedwards@ucar.edu>
2016-04-28 20:04:10 -0600

    add pbs and --no-batch support

--- utils/python/CIME/submit.py
+++ utils/python/CIME/submit.py
@@ -14,9 +14,8 @@ def submit(caseroot, job="case.run", resubmit=None, no_batch=False):
     case = Case(caseroot)
 #    if resubmit is None:
 #        check_case(case, caseroot)
-        #Load Modules
-    if no_batch:
-        case.set_value("batch_system","none")
+
+    #Load Modules
     env_module = EnvModule(case.get_value("MACH"), case.get_value("COMPILER"),
                            case.get_value("CIMEROOT"),caseroot, case.get_value("MPILIB"),
                            case.get_value("DEBUG"))
@@ -24,7 +23,10 @@ def submit(caseroot, job="case.run", resubmit=None, no_batch=False):
     batchobj = BatchUtils(job, case)
     case.set_value("RUN_WITH_SUBMIT",True)
     case.flush()
-    batchobj.submit_single_job(job)
+
+    batchobj.dependancies.append(job)
+
+    batchobj.submit_jobs(no_batch=no_batch)
 
 
 
