19cf6b74b92c63fd4b0de7a979c236c169161b31
Jim Edwards <jedwards@ucar.edu>
2016-05-09 16:46:38 -0600

    log files should not be in git

--- utils/python/update_acme_tests.py
+++ utils/python/update_acme_tests.py
@@ -16,23 +16,23 @@ _TEST_SUITES = {
                    ),
 
     "acme_test_only_pass" : (None,
-                   ("TESTRUNPASS_P1.f19_g16_rx1.A",
-                    "TESTRUNPASS_P1.ne30_g16_rx1.A",
-                    "TESTRUNPASS_P1.f45_g37_rx1.A")
+                   ("TESTRUNPASS_Mmpi-serial.f19_g16_rx1.A",
+                    "TESTRUNPASS_Mmpi-serial.ne30_g16_rx1.A",
+                    "TESTRUNPASS_Mmpi-serial.f45_g37_rx1.A")
                    ),
 
     "acme_test_only_slow_pass" : (None,
-                   ("TESTRUNSLOWPASS_P1.f19_g16_rx1.A",
-                    "TESTRUNSLOWPASS_P1.ne30_g16_rx1.A",
-                    "TESTRUNSLOWPASS_P1.f45_g37_rx1.A")
+                   ("TESTRUNSLOWPASS_Mmpi-serial.f19_g16_rx1.A",
+                    "TESTRUNSLOWPASS_Mmpi-serial.ne30_g16_rx1.A",
+                    "TESTRUNSLOWPASS_Mmpi-serial.f45_g37_rx1.A")
                    ),
 
     "acme_test_only" : (None,
                    ("TESTBUILDFAIL.f19_g16_rx1.A",
-                    "TESTRUNFAIL_P1.f19_g16_rx1.A",
-                    "TESTRUNPASS_P1.f19_g16_rx1.A",
-                    "TESTMEMLEAKFAIL_P1.f19_g16.X",
-                    "TESTMEMLEAKPASS_P1.f19_g16.X")
+                    "TESTRUNFAIL_Mmpi-serial.f19_g16_rx1.A",
+                    "TESTRUNPASS_Mmpi-serial.f19_g16_rx1.A",
+                    "TESTMEMLEAKFAIL_Mmpi-serial.f19_g16.X",
+                    "TESTMEMLEAKPASS_Mmpi-serial.f19_g16.X")
                    ),
 
     "acme_runoff_developer" : (None,
