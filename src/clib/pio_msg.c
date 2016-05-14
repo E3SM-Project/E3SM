10ff1436da7f206c88b4b69261fe041b5e323694
Ed Hartnett <edwardjameshartnett@gmail.com>
2016-05-14 07:58:53 -0400

    got inq_attid working for async

--- src/clib/pio_msg.c
+++ src/clib/pio_msg.c
@@ -833,6 +833,48 @@ int def_dim_handler(iosystem_desc_t *ios)
     return PIO_NOERR;
 }
 
+/** This function is run on the IO tasks to rename a netCDF
+ * dimension. */
+int rename_dim_handler(iosystem_desc_t *ios)
+{
+    int ncid;
+    int len, namelen;
+    int iotype;
+    char *name;
+    int mode;
+    int mpierr;
+    int ret;
+    int dimid;
+
+    LOG((1, "rename_dim_handler"));
+
+    /* Get the parameters for this function that the he comp master
+     * task is broadcasting. */
+    if ((mpierr = MPI_Bcast(&ncid, 1, MPI_INT, 0, ios->intercomm)))
+	return PIO_EIO;
+    if ((mpierr = MPI_Bcast(&dimid, 1, MPI_INT, 0, ios->intercomm)))
+	return PIO_EIO;
+    if ((mpierr = MPI_Bcast(&namelen, 1, MPI_INT, 0, ios->intercomm)))
+	return PIO_EIO;
+    if (!(name = malloc(namelen + 1 * sizeof(char))))
+    	return PIO_ENOMEM;
+    if ((mpierr = MPI_Bcast((void *)name, namelen + 1, MPI_CHAR, 0,
+    			    ios->intercomm)))
+    	return PIO_EIO;
+    LOG((2, "rename_dim_handler got parameters namelen = %d "
+	 "name = %s ncid = %d varid = %d", namelen, name, ncid, varid));
+
+    /* Call the create file function. */
+    if ((ret = PIOc_rename_dim(ncid, dimid, name)))
+	return ret;
+
+    /* Free resources. */
+    free(name);
+
+    LOG((1, "%d rename_dim_handler succeeded!\n", my_rank));
+    return PIO_NOERR;
+}
+
 /** This function is run on the IO tasks. It reads or writes an array
  *  of data to a netCDF variable.
  *
@@ -1122,6 +1164,9 @@ int pio_msg_handler(int io_rank, int component_count, iosystem_desc_t *iosys)
 	    break;
 	case PIO_MSG_DELETE_FILE:
 	    delete_file_handler(my_iosys);
+	    break;
+	case PIO_MSG_RENAME_DIM:
+	    rename_dim_handler(my_iosys);
 	    break;
 	case PIO_MSG_DEF_DIM:
 	    def_dim_handler(my_iosys);
--- src/clib/pio_nc_async.c
+++ src/clib/pio_nc_async.c
@@ -1133,62 +1133,71 @@ int PIOc_inq_attid(int ncid, int varid, const char *name, int *idp)
  * PIOc_openfile() or PIOc_createfile().
  * @return PIO_NOERR for success, error code otherwise.  See PIOc_Set_File_Error_Handling
  */
-int PIOc_rename_dim (int ncid, int dimid, const char *name) 
+int PIOc_rename_dim(int ncid, int dimid, const char *name) 
 {
-    int ierr;
-    int msg;
+    iosystem_desc_t *ios;  /** Pointer to io system information. */
+    file_desc_t *file;     /** Pointer to file information. */
+    int ierr = PIO_NOERR;  /** Return code from function calls. */
     int mpierr = MPI_SUCCESS, mpierr2;  /** Return code from MPI function codes. */
-    iosystem_desc_t *ios;
-    file_desc_t *file;
-    char *errstr;
 
-    errstr = NULL;
-    ierr = PIO_NOERR;
+    LOG((1, "PIOc_inq ncid = %d", ncid));
 
-    file = pio_get_file_from_id(ncid);
-    if(file == NULL)
+    /* User must provide name of correct length. */
+    if (!name || strlen(name) > NC_MAX_NAME)
+	return PIO_EINVAL;
+
+    /* Find the info about this file. */
+    if (!(file = pio_get_file_from_id(ncid)))
 	return PIO_EBADID;
     ios = file->iosystem;
-    msg = PIO_MSG_RENAME_DIM;
 
-    if(ios->async_interface && ! ios->ioproc){
-	if(ios->compmaster) 
-	    mpierr = MPI_Send(&msg, 1,MPI_INT, ios->ioroot, 1, ios->union_comm);
-	mpierr = MPI_Bcast(&(file->fh),1, MPI_INT, ios->compmaster, ios->intercomm);
-    }
+    /* If async is in use, and this is not an IO task, bcast the parameters. */
+    if (ios->async_interface)
+    {
+	if (!ios->ioproc)
+	{
+	    int msg = PIO_MSG_RENAME_DIM; /** Message for async notification. */
+	    int namelen = strlen(name);
 
+	    if(ios->compmaster) 
+		mpierr = MPI_Send(&msg, 1,MPI_INT, ios->ioroot, 1, ios->union_comm);
 
-    if(ios->ioproc){
-	switch(file->iotype){
-#ifdef _NETCDF
-#ifdef _NETCDF4
-	case PIO_IOTYPE_NETCDF4P:
-	    ierr = nc_rename_dim(file->fh, dimid, name);;
-	    break;
-	case PIO_IOTYPE_NETCDF4C:
-#endif
-	case PIO_IOTYPE_NETCDF:
-	    if(ios->io_rank==0){
-		ierr = nc_rename_dim(file->fh, dimid, name);;
-	    }
-	    break;
-#endif
-#ifdef _PNETCDF
-	case PIO_IOTYPE_PNETCDF:
-	    ierr = ncmpi_rename_dim(file->fh, dimid, name);;
-	    break;
-#endif
-	default:
-	    ierr = iotype_error(file->iotype,__FILE__,__LINE__);
+	    if (!mpierr)
+		mpierr = MPI_Bcast(&file->fh, 1, MPI_INT, ios->compmaster, ios->intercomm);
+	    if (!mpierr)
+		mpierr = MPI_Bcast(&dimid, 1, MPI_INT, ios->compmaster, ios->intercomm);
+	    if (!mpierr)
+		mpierr = MPI_Bcast(&namelen, 1, MPI_INT, ios->compmaster, ios->intercomm);
+	    if (!mpierr)
+		mpierr = MPI_Bcast(&name, namelen + 1, MPI_CHAR, ios->compmaster, ios->intercomm);
 	}
+
+	/* Handle MPI errors. */
+	if ((mpierr2 = MPI_Bcast(&mpierr, 1, MPI_INT, ios->ioroot, ios->my_comm)))
+	    check_mpi(file, mpierr2, __FILE__, __LINE__);	    
+	check_mpi(file, mpierr, __FILE__, __LINE__);
     }
 
-    if(ierr != PIO_NOERR){
-	errstr = (char *) malloc((strlen(__FILE__) + 20)* sizeof(char));
-	sprintf(errstr,"in file %s",__FILE__);
+    
+    /* If this is an IO task, then call the netCDF function. */
+    if (ios->ioproc)
+    {
+#ifdef _PNETCDF
+	if (file->iotype == PIO_IOTYPE_PNETCDF)
+	    ierr = ncmpi_rename_dim(file->fh, dimid, name);
+#endif /* _PNETCDF */
+#ifdef _NETCDF
+	if (file->iotype != PIO_IOTYPE_PNETCDF && file->do_io)
+	    ierr = nc_rename_dim(file->fh, dimid, name);;
+#endif /* _NETCDF */
+	LOG((2, "PIOc_inq netcdf call returned %d", ierr));
     }
-    ierr = check_netcdf(file, ierr, errstr,__LINE__);
-    if(errstr != NULL) free(errstr);
+
+    /* Broadcast and check the return code. */
+    if ((mpierr = MPI_Bcast(&ierr, 1, MPI_INT, ios->ioroot, ios->my_comm)))
+	return PIO_EIO;
+    check_netcdf(file, ierr, __FILE__, __LINE__);
+    
     return ierr;
 }
 
