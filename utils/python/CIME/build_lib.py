eb486477d89e44a87c9d55fb9b2a70d4e9602d3b
Jason Sarich <sarich@mcs.anl.gov>
2016-06-08 09:38:00 -0500

    use stand-alone functions instead of classes for buildlib, buildnml

--- utils/python/CIME/build_lib.py
+++ utils/python/CIME/build_lib.py
@@ -1,45 +1,40 @@
+"""
+common implementation for building components
+
+These are used by components/<model_type>/<component>/cime_config/buildlib
+"""
 from CIME.XML.standard_module_setup import *
-from CIME.case import Case
-"""
-buildlibx - given the caseroot and component name(cmp), build the stub component
-"""
 
-class BuildLib(object):
-    def __init__(self, case, model, comp):
-        self.case = case
-        self.model = model
-        self.comp = comp
 
-class BuildXLib(buildlib):
-    def buildlib(self):
-        caseroot = self.case.get_value("CASEROOT")
-        casetools = self.self.case.get_value("CASETOOLS")
-        cimeroot = self.case.get_value("CIMEROOT")
-        mach = self.case.get_value("MACH")
-        libroot = self.case.get_value("LIBROOT")
-        gmake_j = self.case.get_value("GMAKE_J")
-        gmake = self.case.get_value("GMAKE")
+logger = logging.getLogger(__name__)
 
-        # Write directory list
-        with open('Filepath','w') as out:
-            out.write(os.path.join(caseroot, "SourceMods", "src.%s", self.comp)
-                      + "\n")
-            out.write(os.path.join(cimeroot, "components", "xcpl_comps",
-                                   "xshare") + "\n")
-            out.write(os.path.join(cimeroot, "components", "xcpl_comps",
-                                   self.comp, "cpl") + "\n")
+def build_x_lib(case, model, comp):
+    caseroot = case.get_value("CASEROOT")
+    casetools = case.get_value("CASETOOLS")
+    cimeroot = case.get_value("CIMEROOT")
+    mach = case.get_value("MACH")
+    libroot = case.get_value("LIBROOT")
+    gmake_j = case.get_value("GMAKE_J")
+    gmake = case.get_value("GMAKE")
 
-        # generate macro values
-        complib = os.path.join(libroot, "lib%s.a" % self.model)
-        makefile = os.path.join(casetools, "Makefile")
-        macfile = os.path.join(caseroot, "Macros.%s" % mach)
+    # Write directory list
+    with open('Filepath', 'w') as out:
+        out.write(os.path.join(caseroot, "SourceMods", "src.%s", comp)
+                  + "\n")
+        out.write(os.path.join(cimeroot, "components", "xcpl_comps",
+                               "xshare") + "\n")
+        out.write(os.path.join(cimeroot, "components", "xcpl_comps",
+                               comp, "cpl") + "\n")
 
-        # build
-        cmd = "%s complib -j %d MODEL=%s COMPLIB=%s -f %s MACFILE=%s" % (gmake,
-                    gmake_j, self.model, complib, makefile, macfile))
-        rc, out, err = run_cmd(cmd)
-        expect(rc == 0, "Command %s failed rc=%d\nout=%s\nerr=%s" % (cmd, rc,
-                                                                     out, err))
-        logger.info("Command %s completed with output %s\nerr %s"% (cmd, out,
-                                                                    err))
-        
+    # generate command line argument values
+    complib = os.path.join(libroot, "lib%s.a" % model)
+    makefile = os.path.join(casetools, "Makefile")
+    macfile = os.path.join(caseroot, "Macros.%s" % mach)
+
+    # build
+    cmd = "%s complib -j %d MODEL=%s COMPLIB=%s -f %s MACFILE=%s" % \
+           (gmake, gmake_j, model, complib, makefile, macfile)
+    rc, out, err = run_cmd(cmd)
+    expect(rc == 0, "Command %s failed rc=%d\nout=%s\nerr=%s" % (cmd, rc,
+                                                                 out, err))
+    logger.info("Command %s completed with output %s\nerr %s", cmd, out, err)
--- utils/python/CIME/build_nml.py
+++ utils/python/CIME/build_nml.py
@@ -1,62 +1,59 @@
+"""
+common implementation for building namelist commands
+
+These are used by components/<model_type>/<component>/cime_config/buildnml
+"""
 from CIME.XML.standard_module_setup import *
 from CIME.case import Case
 
-class BuildNml(object):
-    def __init__(self, case, model, comp):
-        self.case = case
-        self.model = model
-        self.comp = comp
+def build_x_nml(case, model, comp):
+    rundir = case.get_value("RUNDIR")
+    ninst = case.get_value("NINST_%s" % comp)
+    nx = case.get_value("%s_NX" % comp)
+    ny = case.get_value("%s_NY" % comp)
+    extras = []
+    dtype = 1
+    npes = 0
+    length = 0
 
-    
-class buildXNml(buildnml):
-    def build_namelist(self):
-        rundir = case.get_value("RUNDIR")
-        ninst = case.get_value("NINST_%s" % self.comp)
-        nx = case.get_value("%s_NX" % self.comp)
-        ny = case.get_value("%s_NY" % self.comp)
-        extras = []
-        dtype = 1
-        npes = 0
-        length = 0
-
-        if self.model == "xatm":
-            if ny == 1:
-                dtype = 2
-            extras = [["24",
-                       "ncpl  number of communications w/coupler per dat"],
-                      ["0.0",
-                       "simul time proxy (secs): time between cpl comms"]]
-        elif self.model == "xglc" or self.model == "xice":
+    if model == "xatm":
+        if ny == 1:
             dtype = 2
-        elif self.model == "xlnd":
-            dtype = 11
-        elif self.model == "xocn":
-            dtype = 4
-        elif self.model == "xrof":
-            dtype = 11
-            flood_mode = Case('XROF_FLOOD_MODE')
-            if flood_mode == "ACTIVE":
-                extras = [[".true.","flood flag"]]
-            else:
-                extras = [[".false.","flood flag"]]
+        extras = [["24",
+                   "ncpl  number of communications w/coupler per dat"],
+                  ["0.0",
+                   "simul time proxy (secs): time between cpl comms"]]
+    elif model == "xglc" or model == "xice":
+        dtype = 2
+    elif model == "xlnd":
+        dtype = 11
+    elif model == "xocn":
+        dtype = 4
+    elif model == "xrof":
+        dtype = 11
+        flood_mode = Case('XROF_FLOOD_MODE')
+        if flood_mode == "ACTIVE":
+            extras = [[".true.", "flood flag"]]
+        else:
+            extras = [[".false.", "flood flag"]]
 
-        for i in range(1, ninst + 1):
-            # If only 1 file, name is 'model_in'
-            # otherwise files are 'model_in0001', 'model_in0002', etc
-            if ninst == 1:
-                filename = os.path.join(rundir, "%s_in" % self.model)
-            else:
-                filename = os.path.join(rundir, "%s_in%4.4d" % (self.model,i))
+    for i in range(1, ninst + 1):
+        # If only 1 file, name is 'model_in'
+        # otherwise files are 'model_in0001', 'model_in0002', etc
+        if ninst == 1:
+            filename = os.path.join(rundir, "%s_in" % model)
+        else:
+            filename = os.path.join(rundir, "%s_in%4.4d" % (model, i))
 
 
-            with open(filename, 'w') as infile:
-                infile.write("%-20d ! i-direction global dimension\n" % nx)
-                infile.write("%-20d ! j-direction global dimension\n" % ny)
-                infile.write("%-20d ! decomp_type  1=1d-by-lat, 2=1d-by-lon,"
-                             " 3=2d, 4=2d evensquare, 11=segmented\n" % dtype)
-                infile.write("%-20d ! num of pes for i (type 3 only)\n" % npes)
-                infile.write("%-20d ! length of segments (type 4 only)\n" 
-                             % length)
-                for extra in extras:
-                    infile.write("%-20s ! %s\n" % (extra[0], extra[1]))
+        with open(filename, 'w') as infile:
+            infile.write("%-20d ! i-direction global dimension\n" % nx)
+            infile.write("%-20d ! j-direction global dimension\n" % ny)
+            infile.write("%-20d ! decomp_type  1=1d-by-lat, 2=1d-by-lon,"
+                         " 3=2d, 4=2d evensquare, 11=segmented\n" % dtype)
+            infile.write("%-20d ! num of pes for i (type 3 only)\n" % npes)
+            infile.write("%-20d ! length of segments (type 4 only)\n"
+                         % length)
+            for extra in extras:
+                infile.write("%-20s ! %s\n" % (extra[0], extra[1]))
 
