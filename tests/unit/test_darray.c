541a07037f88ffb1cc5ed801b8ba83809f33803b
Ed Hartnett <edwardjameshartnett@gmail.com>
2016-06-15 10:04:26 -0600

    cleanout of test_darray

--- tests/unit/test_darray.c
+++ tests/unit/test_darray.c
@@ -48,107 +48,7 @@ char dim_name[NDIM][NC_MAX_NAME + 1] = {"timestep", "x", "y"};
 /** Length of the dimensions in the sample data. */
 int dim_len[NDIM] = {NC_UNLIMITED, X_DIM_LEN, Y_DIM_LEN};
 
-/** Length of chunksizes to use in netCDF-4 files. */
-size_t chunksize[NDIM] = {2, X_DIM_LEN/2, Y_DIM_LEN/2};
-
-/** Check the dimension names.
- *
- * @param my_rank rank of process
- * @param ncid ncid of open netCDF file
- * 
- * @returns 0 for success, error code otherwise. */
-int
-check_dim_names(int my_rank, int ncid, int verbose)
-{
-    char dim_name[NC_MAX_NAME + 1];
-    char zero_dim_name[NC_MAX_NAME + 1];
-    int ret;
-
-    for (int d = 0; d < NDIM; d++)
-    {
-	strcpy(dim_name, "11111111111111111111111111111111");
-	if ((ret = PIOc_inq_dimname(ncid, d, dim_name)))
-	    return ret;
-	if (verbose)
-	    printf("my_rank %d dim %d name %s\n", my_rank, d, dim_name);
-
-	/* Did other ranks get the same name? */
-	if (!my_rank)
-	    strcpy(zero_dim_name, dim_name);
-	/* if (verbose) */
-	/*     printf("rank %d dim_name %s zero_dim_name %s\n", my_rank, dim_name, zero_dim_name); */
-	if ((ret = MPI_Bcast(&zero_dim_name, strlen(dim_name) + 1, MPI_CHAR, 0,
-				MPI_COMM_WORLD)))
-	    MPIERR(ret);
-	if (strcmp(dim_name, zero_dim_name))
-	    return ERR_AWFUL;
-    }
-    return 0;
-}
-
-/** Check the variable name.
- *
- * @param my_rank rank of process
- * @param ncid ncid of open netCDF file
- * 
- * @returns 0 for success, error code otherwise. */
-int
-check_var_name(int my_rank, int ncid, int verbose)
-{
-    char var_name[NC_MAX_NAME + 1];
-    char zero_var_name[NC_MAX_NAME + 1];
-    int ret;
-
-    strcpy(var_name, "11111111111111111111111111111111");
-    if ((ret = PIOc_inq_varname(ncid, 0, var_name)))
-	return ret;
-    if (verbose)
-	printf("my_rank %d var name %s\n", my_rank, var_name);
-
-    /* Did other ranks get the same name? */
-    if (!my_rank)
-	strcpy(zero_var_name, var_name);
-    if ((ret = MPI_Bcast(&zero_var_name, strlen(var_name) + 1, MPI_CHAR, 0,
-			 MPI_COMM_WORLD)))
-	MPIERR(ret);
-    if (strcmp(var_name, zero_var_name))
-	return ERR_AWFUL;
-    return 0;
-}
-
-/** Check the attribute name.
- *
- * @param my_rank rank of process
- * @param ncid ncid of open netCDF file
- * 
- * @returns 0 for success, error code otherwise. */
-int
-check_att_name(int my_rank, int ncid, int verbose)
-{
-    char att_name[NC_MAX_NAME + 1];
-    char zero_att_name[NC_MAX_NAME + 1];
-    int ret;
-
-    strcpy(att_name, "11111111111111111111111111111111");
-    if ((ret = PIOc_inq_attname(ncid, NC_GLOBAL, 0, att_name)))
-	return ret;
-    if (verbose)
-	printf("my_rank %d att name %s\n", my_rank, att_name);
-
-    /* Did everyone ranks get the same length name? */
-/*    if (strlen(att_name) != strlen(ATT_NAME))
-      return ERR_AWFUL;*/
-    if (!my_rank)
-    	strcpy(zero_att_name, att_name);
-    if ((ret = MPI_Bcast(&zero_att_name, strlen(att_name) + 1, MPI_CHAR, 0,
-    			 MPI_COMM_WORLD)))
-    	MPIERR(ret);
-    if (strcmp(att_name, zero_att_name))
-    	return ERR_AWFUL;
-    return 0;
-}
-
-/** Run Tests for NetCDF-4 Functions.
+/** Run Tests for darray Functions.
  *
  * @param argc argument count
  * @param argv array of arguments
@@ -174,10 +74,10 @@ main(int argc, char **argv)
 				      PIO_IOTYPE_NETCDF4P};
 
     /** Names for the output files. */
-    char filename[NUM_NETCDF_FLAVORS][NC_MAX_NAME + 1] = {"test_names_pnetcdf.nc",
-							  "test_names_classic.nc",
-							  "test_names_serial4.nc",
-							  "test_names_parallel4.nc"};
+    char filename[NUM_NETCDF_FLAVORS][NC_MAX_NAME + 1] = {"test_darray_pnetcdf.nc",
+							  "test_darray_classic.nc",
+							  "test_darray_serial4.nc",
+							  "test_darray_parallel4.nc"};
 	
     /** Number of processors that will do IO. In this test we
      * will do IO from all processors. */
@@ -203,50 +103,11 @@ main(int argc, char **argv)
     int iosysid;
 
     /** The ncid of the netCDF file. */
-    int ncid = 0;
+    int ncid;
 
     /** The ID of the netCDF varable. */
     int varid;
 
-    /** Storage of netCDF-4 files (contiguous vs. chunked). */
-    int storage;
-
-    /** Chunksizes set in the file. */
-    size_t my_chunksize[NDIM];
-    
-    /** The shuffle filter setting in the netCDF-4 test file. */
-    int shuffle;
-    
-    /** Non-zero if deflate set for the variable in the netCDF-4 test file. */
-    int deflate;
-
-    /** The deflate level set for the variable in the netCDF-4 test file. */
-    int deflate_level;
-
-    /** Non-zero if fletcher32 filter is used for variable. */
-    int fletcher32;
-
-    /** Endianness of variable. */
-    int endianness;
-
-    /* Size of the file chunk cache. */
-    size_t chunk_cache_size;
-
-    /* Number of elements in file cache. */
-    size_t nelems;
-
-    /* File cache preemption. */
-    float preemption;
-
-    /* Size of the var chunk cache. */
-    size_t var_cache_size;
-
-    /* Number of elements in var cache. */
-    size_t var_cache_nelems;
-
-    /* Var cache preemption. */    
-    float var_cache_preemption;
-    
     /** The I/O description ID. */
     int ioid;
 
@@ -354,25 +215,8 @@ main(int argc, char **argv)
 		ERR(ret);
 	}
 
-	/* Check the dimension names. */
-	if ((ret = check_dim_names(my_rank, ncid, verbose)))
-	    ERR(ret);
-
-	/* Define a global attribute. */
-	int att_val = 42;
-	if ((ret = PIOc_put_att_int(ncid, NC_GLOBAL, ATT_NAME, NC_INT, 1, &att_val)))
-	    ERR(ret);
-
-	/* Check the attribute name. */
-	if ((ret = check_att_name(my_rank, ncid, verbose)))
-	    ERR(ret);
-
 	/* Define a variable. */
 	if ((ret = PIOc_def_var(ncid, VAR_NAME, PIO_FLOAT, NDIM, dimids, &varid)))
-	    ERR(ret);
-
-	/* Check the variable name. */
-	if ((ret = check_var_name(my_rank, ncid, verbose)))
 	    ERR(ret);
 
 	if ((ret = PIOc_enddef(ncid)))
