ddb14a34aab3caa05c55e8c2342e91851e0e0896
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2016-07-05 16:18:27 -0600

    Fix errors when running `cs.status.*`.
    There were several errors related to baseline tests, including memory
    and throughput baselines, when running the `cs.status.*` script. (This
    is the script generated in the root directory for a test suite, for
    checking the current status of all tests in the suite.)
    
    Most of the necessary changes are in `system_tests_common`, where the
    output produced using `append_status` needs to use a format and phase
    identifiers that can be recognized by
    `wait_for_tests.parse_test_status`. `parse_test_status` also needs to
    recognize the memory and throughput comparisons as valid test phases, as
    well as recognizing "BFAIL" (the status resulting from a *missing*
    baseline directory) as a valid status.
    
    Testing:
    
        There are no formal regression tests for this functionality, but the
        CAM tests on yellowstone have been run with these changes, and
        cs.status runs without throwing an exception.

--- utils/python/wait_for_tests.py
+++ utils/python/wait_for_tests.py
@@ -13,6 +13,7 @@ TEST_PASS_STATUS          = "PASS"
 TEST_FAIL_STATUS          = "FAIL"
 TEST_DIFF_STATUS          = "DIFF"
 NAMELIST_FAIL_STATUS      = "NLFAIL"
+BASELINE_MISSING_STATUS   = "BFAIL"
 COMMENT_STATUS            = "COMMENT"
 SLEEP_INTERVAL_SEC        = .1
 THROUGHPUT_TEST_STR       = "tputcomp"
@@ -321,11 +322,13 @@ def parse_test_status(file_contents):
             else:
                 expect(test_name == curr_test_name, "inconsistent test name in parse_test_status: '%s' != '%s'"%(test_name, curr_test_name))
 
-            expect(status in [TEST_PENDING_STATUS ,TEST_PASS_STATUS, \
-                              TEST_FAIL_STATUS, TEST_DIFF_STATUS, NAMELIST_FAIL_STATUS],
+            expect(status in [TEST_PENDING_STATUS ,TEST_PASS_STATUS,
+                              TEST_FAIL_STATUS, TEST_DIFF_STATUS, NAMELIST_FAIL_STATUS,
+                              BASELINE_MISSING_STATUS],
                    "Unexpected status '%s' in parse_test_status" % status)
             expect(phase in ["INIT","CREATE_NEWCASE","XML","SETUP","SHAREDLIB_BUILD",
-                             "tputcomp","nlcomp","MODEL_BUILD", "compare", "generate", "memleak", "RUN"],
+                             "nlcomp","MODEL_BUILD", "compare", "generate",
+                             "memleak", "RUN", MEMORY_TEST_STR, THROUGHPUT_TEST_STR],
                    "phase '%s' not expected in parse_test_status" % phase)
 
             if (phase in rv):
