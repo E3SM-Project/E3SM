06ba72c6c3e1a23b2924b1c9df60d4df6ca72724
Matthew Hoffman <mhoffman@lanl.gov>
2016-07-18 11:54:27 -0600

    Correct SGH adaptive time stepper to correctly relate to model time
    Up until now, the SGH adaptive time stepper would not actually change
    the model time.  It worked correctly, but the time in, e.g, xtime, was
    not correct for how long elapsed in the SGH model.  This commit fixes
    that by forcing the SGH adaptive time stepper to hijack the primary
    model time variable, deltat.  In the future we probably want the SGH
    model time stepper to subcycle underneath the master model deltat, but
    this is fine for now.
    
    As part of this change, a new config has been introduced:
    config_SGH_max_adaptive_timestep
    Previously this functionality was coming from the master model deltat
    setting, but that no longer will work if the SGH model is changing that
    variable.

--- test_cases/ocean/landice/hydro-radial/setup_hydro-radial_initial_conditions.py
+++ test_cases/ocean/landice/hydro-radial/setup_hydro-radial_initial_conditions.py
@@ -84,7 +84,7 @@ gridfile.variables['uReconstructX'][0,thickness[0,:]==0.0,:] = 0.0
 
 # IC on thickness
 gridfile.variables['waterThickness'][0,:] = (r-5000.0)/r0 * 0.5 
-##gridfile.variables['waterThickness'][0,r<5000.0] = 0.0
+gridfile.variables['waterThickness'][0,gridfile.variables['waterThickness'][0,:]<0.0] = 0.0
 #gridfile.variables['waterThickness'][0,:] = 0.05
 
 gridfile.close()
