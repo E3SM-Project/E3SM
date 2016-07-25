d6c28b47a639a99847f4a829df9a902a8a6b0282
Jim Edwards <jedwards@ucar.edu>
2016-07-25 11:35:54 -0600

    fix memleak test giving error if baseline not found

--- utils/python/CIME/SystemTests/system_tests_common.py
+++ utils/python/CIME/SystemTests/system_tests_common.py
@@ -280,7 +280,7 @@ class SystemTestsCommon(object):
         if not os.path.isfile(baselog):
             # for backward compatibility
             baselog = os.path.join(basecmp_dir, "cpl.log")
-        if len(memlist) > 3:
+        if baselog is not None and len(memlist) > 3:
             blmem = self._get_mem_usage(baselog)[-1][1]
             curmem = memlist[-1][1]
             diff = (curmem-blmem)/blmem
