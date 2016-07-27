729837c8acb3dd48083f36b988e7c741ccfc7ab0
Matthew Hoffman <mhoffman@lanl.gov>
2016-07-27 16:26:52 -0600

    Create SHMIP test case - just test A for now
    Subglacial Hydrology Model Inter-comparison Project (SHMIP)
    These tests are described here: http://shmip.bitbucket.org/
    
    This commit begins to add these tests.  There are organized in a way
    where as many of the tests as possible get set up, but all the tests
    must be run manually.
    
    This commit adds test A, including a script to plot the results of test
    A3 against a benchmark from the GLADS model provided by the
    intercomparison.

---
+++ test_cases/ocean/landice/hydro-shmip/setup_hydro-shmip_initial_conditions.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# Generate initial conditions for hydro-SHMIP land ice test cases
+# Details here: http://shmip.bitbucket.org/
+
+from netCDF4 import Dataset as NetCDFFile
+import numpy as np
+
+# Parse options
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename", type='string', help="file to setup", metavar="FILE")
+parser.add_option("-t", "--test", dest="test", type='string', help="test type to set up, A-F", metavar="LETTER")
+parser.add_option("-n", "--number", dest="number", type='int', help="test variant to set up, 1-X, depending on test type", metavar="NUMBER")
+options, args = parser.parse_args()
+if not options.filename:
+   options.filename = 'landice_grid.nc'
+   print 'No file specified.  Attempting to use landice_grid.nc'
+
+# Setup dictionaries of parameter values for each experiment
+a_params = {1:7.93e-11,  2:1.59e-09, 3:5.79e-09, 4:2.5e-8, 5:4.5e-8, 6:5.79e-7}
+
+# Open the file, get needed dimensions
+gridfile = NetCDFFile(options.filename,'r+')
+nVertLevels = len(gridfile.dimensions['nVertLevels'])
+# Get variables
+xCell = gridfile.variables['xCell']
+yCell = gridfile.variables['yCell']
+xEdge = gridfile.variables['xEdge']
+yEdge = gridfile.variables['yEdge']
+xVertex = gridfile.variables['xVertex']
+yVertex = gridfile.variables['yVertex']
+thickness = gridfile.variables['thickness']
+bedTopography = gridfile.variables['bedTopography']
+layerThicknessFractions = gridfile.variables['layerThicknessFractions']
+SMB = gridfile.variables['sfcMassBal']
+
+
+if options.test in ('A','B','C','D'):  # set up sqrt geometry
+   unique_xs=np.array(sorted(list(set(xCell[:]))))
+   if min(unique_xs) >= 0.0:
+      shiftx = unique_xs[3]  # put origin in a few cells to the right of the left edge of mesh
+      xCell[:] = xCell[:] - shiftx
+      xEdge[:] = xEdge[:] - shiftx
+      xVertex[:] = xVertex[:] - shiftx
+
+
+   # thickness
+   x = 100.0e3 - np.absolute(xCell[:] - 100.0e3)
+   thickness[0,:] = 6.0 *( np.sqrt( x[:] + 5.0e3) - np.sqrt(5.0e3) ) + 1.0
+   thickness[0, x<0.0]=0.0  # make a margin
+
+   # flat bed
+   bedTopography[:] = 0.0
+
+# Setup layerThicknessFractions
+layerThicknessFractions[:] = 1.0 / nVertLevels
+
+if options.test == 'A':
+  gridfile.variables['externalWaterInput'][:] = a_params[options.number] * 1000.0  # Convert from m/s to kg/m2/s
+
+# melt
+gridfile.variables['basalMeltInput'][:] = 0.0 * 1000.0 # no basal melting
+
+# velocity
+gridfile.variables['uReconstructX'][:] = 1.0e-6 # all tests use this velocity
+
+# initial thickness
+gridfile.variables['waterThickness'][:] = 0.1 * thickness[:]/1500.0 # start with something to avoid weird adapative time steps initially
+
+# initial waterPressure
+gridfile.variables['waterPressure'][:] = 0.5 * 9.81 * 910.0 * thickness[:] # start with half of Pice
+
+gridfile.close()
+
+print 'Successfully added initial conditions to: ', options.filename
+
---
+++ test_cases/ocean/landice/hydro-shmip/visualize_output_shmip.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+'''
+Plots profiles for hydro-ship test case
+'''
+import numpy as np
+import netCDF4
+#import datetime
+# import math
+# from pylab import *
+from optparse import OptionParser
+import matplotlib.pyplot as plt
+from matplotlib import cm
+# from matplotlib.contour import QuadContourSet
+# import time
+
+secInYr = 3600.0 * 24.0 * 365.0  # Note: this may be slightly wrong for some calendar types!
+
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename", help="file to visualize", metavar="FILE")
+parser.add_option("-t", "--time", dest="time", help="time step to visualize (0 based)", metavar="TIME")
+parser.add_option("-s", "--save", action="store_true", dest="saveimages", help="include this flag to save plots as files")
+parser.add_option("-n", "--nodisp", action="store_true", dest="hidefigs", help="include this flag to not display plots (usually used with -s)")
+parser.add_option("-3", dest="A3", action="store_true", help="plot GLADS results for experiment 3")
+
+options, args = parser.parse_args()
+
+if not options.filename:
+	print "No filename provided. Using output.nc."
+        options.filename = "output.nc"
+
+if not options.time:
+	print "No time provided. Using time -1."
+        time_slice = -1
+else:
+        time_slice = int(options.time)
+
+
+
+f = netCDF4.Dataset(options.filename,'r')
+
+#xtime = f.variables['xtime'][:]
+xCell = f.variables['xCell'][:]
+yCell = f.variables['yCell'][:]
+yVertex = f.variables['yVertex'][:]
+#xEdge = f.variables['xEdge'][:]
+#yEdge = f.variables['yEdge'][:]
+h = f.variables['waterThickness'][time_slice,:]
+u = f.variables['waterVelocityCellX'][time_slice,:]
+N = f.variables['effectivePressure'][time_slice,:]
+days = f.variables['daysSinceStart'][:]
+basalmelt = f.variables['basalMeltInput'][time_slice,:]
+surfmelt = f.variables['externalWaterInput'][time_slice,:]
+xtime= f.variables['xtime']
+areaCell = f.variables['areaCell'][:]
+
+
+#print "attempting to get input data from landice_grid.nc!"
+#fin = netCDF4.Dataset('landice_grid.nc','r')
+#H = fin.variables['thickness'][0,:]
+
+print "Using time level ", time_slice, ", which is xtime=",''.join( xtime[time_slice,:])
+
+# Find center row  - currently files are set up to have central row at y=0
+unique_ys=np.unique(yCell[:])
+centerY=unique_ys[len(unique_ys)/2]
+print "number of ys, center y index, center Y value", len(unique_ys), len(unique_ys)/2, centerY
+ind = np.nonzero(yCell[:] == centerY)
+x = xCell[ind]/1000.0
+
+print "start plotting."
+
+fig = plt.figure(1, facecolor='w')
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312, sharex=ax1)
+ax3 = fig.add_subplot(313, sharex=ax1)
+
+if options.A3:
+   # plot GLADS data
+   G_x, G_Nmean, G_Nmin, G_Nmax, G_qmean, G_qmin, G_qmax, G_Qmax = np.loadtxt('tuning_A3.txt', skiprows=1, unpack=True, delimiter=",")
+   ax1.plot(G_x/1000.0, G_Nmin/1.0e6, 'r')
+   ax1.plot(G_x/1000.0, G_Nmean/1.0e6, 'b')
+   ax1.plot(G_x/1000.0, G_Nmax/1.0e6, 'y')
+
+   ax2.plot(G_x/1000.0, G_qmin, 'r')
+   ax2.plot(G_x/1000.0, G_qmean, 'b')
+   ax2.plot(G_x/1000.0, G_qmax, 'y')
+
+plt.sca(ax1)
+plt.plot(x, N[ind] / 1.0e6, '.-g')
+plt.xlabel('X-position (km)')
+plt.ylabel('effecive pressure (MPa)')
+plt.xlim( (0, 100.0) )
+plt.grid(True)
+
+plt.sca(ax2)
+plt.plot(x, np.absolute(h[ind] * u[ind]), '.-g')
+plt.xlabel('X-position (km)')
+plt.ylabel('water flux (m^2/s)')
+plt.grid(True)
+
+
+
+
+
+
+# plot how close to SS we are
+fig = plt.figure(2, facecolor='w')
+ax1 = fig.add_subplot(211)
+for i in ind:
+    plt.plot(days/365.0, f.variables['waterThickness'][:,i])
+plt.xlabel('Years since start')
+plt.ylabel('water thickness (m)')
+plt.grid(True)
+
+ax = fig.add_subplot(212, sharex=ax1)
+for i in ind:
+    plt.plot(days/365.0, f.variables['effectivePressure'][:,i]/1.0e6)
+plt.xlabel('Years since start')
+plt.ylabel('effective pressure (MPa)')
+plt.grid(True)
+
+
+
+print "plotting complete"
+
+plt.draw()
+if options.saveimages:
+        print "Saving figures to files."
+        plt.savefig('GL-position.png')
+
+
+
+
+if options.hidefigs:
+     print "Plot display disabled with -n argument."
+else:
+     plt.show()
+
