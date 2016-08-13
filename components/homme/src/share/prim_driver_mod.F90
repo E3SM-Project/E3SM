178ce96298293141caed40baaf3102ff07780865
Oksana Guba <oksana.guba@gmail.com>
2016-08-13 07:18:02 -0600

    Adding area correction based on 'epsilon bubble' for RRM meshes
    *********1*********2*********3*********4*********5*********6*********7**
    Numerical (based on integration weights) and geometrical areas do not coinside. Previously, in 3D code 'alpha' correction was used. For this, global difference over whole sphere between areas was spread among all DOFs. Now for RRM meshes which in homme use cubed_sphere_map=2 'epsilon bubble' correction is introduced. This correction fixes the difference between geometric and numerical areas for individual elements adjusting inner DOFs of the element. Uniform meshes (with cubed_sphere_map=0) still use 'alpha' correction.
    
    Affected files:
    
    components/homme/src/init_mod.F90 : Introducing 'epsilon bubble' for RRM meshes in SW code. SW code does not use 'alpha' correction.
    
    components/homme/src/share/cube_mod.F90 : Changing metric_atomic() for case cubed_sphere_map=2.
    
    components/homme/src/share/prim_driver_mod.F90 : Introducing 'epsilon bubble' correction in 3D code for RRM meshes.
    
    [Non-BFB] - Non Bit-For-Bit

--- components/homme/src/share/prim_driver_mod.F90
+++ components/homme/src/share/prim_driver_mod.F90
@@ -1,3 +1,18 @@
+! ------------------------------------------------------------------------------------------------
+! prim_driver_mod: 
+!
+! 08/2016: O. Guba Inserting code for area correction based on epsilon bubble:
+! Numerical area of the domain (sphere) is sum of integration weights. The sum
+! is not exactly equal geometric area (4\pi R). It is required that 
+! numerical area = geometric area.  
+! Previously, alpha correction was used. The alpha correction 'butters' 
+! the difference between numerical and geometrical areas evenly among DOFs. Then
+! geometric areas of individual elements do not equal their numerical areas,
+! only whole domain's areas coinside.
+! The 'epsilon bubble' approach modifies inner weights in each element so that
+! geometic and numerical areas of each element match.
+
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -59,7 +74,7 @@ contains
     use thread_mod, only : nthreads, omp_get_thread_num, vert_num_threads
     ! --------------------------------
     use control_mod, only : runtype, restartfreq, filter_counter, integration, topology, &
-         partmethod, while_iter, use_semi_lagrange_transport
+         partmethod, while_iter, use_semi_lagrange_transport, cubed_sphere_map
     ! --------------------------------
     use prim_state_mod, only : prim_printstate_init
     ! --------------------------------
@@ -77,6 +92,7 @@ contains
     ! --------------------------------
     use mesh_mod, only : MeshSetCoordinates, MeshUseMeshFile, MeshCubeTopology, &
          MeshCubeElemCount, MeshCubeEdgeCount
+! OG rotation_init_atomic is not used in this mod.
     use cube_mod, only : cube_init_atomic, rotation_init_atomic, set_corner_coordinates, assign_node_numbers_to_elem
     ! --------------------------------
     use metagraph_mod, only : metavertex_t, metaedge_t, localelemcount, initmetagraph, printmetavertex
@@ -118,6 +134,8 @@ contains
     ! --------------------------------
     use bndry_mod, only : sort_neighbor_buffer_mapping
     ! --------------------------------
+    use coordinate_systems_mod, only : sphere_tri_area
+    ! --------------------------------
 #ifndef CAM
     use repro_sum_mod, only: repro_sum, repro_sum_defaultopts, &
          repro_sum_setopts
@@ -147,8 +165,10 @@ contains
     logical, parameter :: Debug = .FALSE.
 
     real(kind=real_kind), allocatable :: aratio(:,:)
-    real(kind=real_kind) :: area(1),xtmp
-    character(len=80) rot_type   ! cube edge rotation type
+    ! xtmp var is used only for fvm_...
+    real(kind=real_kind) :: area(1), xtmp, area_sphere, area_num, area_dummy, sum_w, delta
+! OG Set but not used.
+!    character(len=80) rot_type   ! cube edge rotation type
 
     integer  :: i
     integer,allocatable :: TailPartition(:)
@@ -201,7 +221,8 @@ contains
     ! unnecessary complication here: all should
     ! be on the same footing. RDL
     ! =====================================
-    rot_type="contravariant"
+! OG Set but not used.
+!    rot_type="contravariant"
 
 #ifndef CAM
     if (par%masterproc) then
@@ -390,6 +411,7 @@ contains
            end do
            call assign_node_numbers_to_elem(elem, GridVertex)
        end if
+
        do ie=1,nelemd
           call cube_init_atomic(elem(ie),gp%points)
        enddo
@@ -398,29 +420,98 @@ contains
     ! =================================================================
     ! Initialize mass_matrix
     ! =================================================================
+    ! Let's not run mass matrix just yet?
     if(par%masterproc) write(iulog,*) 'running mass_matrix'
+    ! expensive
     call mass_matrix(par,elem)
     allocate(aratio(nelemd,1))
 
-    if (topology=="cube") then
-       area = 0
-       do ie=1,nelemd
-          aratio(ie,1) = sum(elem(ie)%mp(:,:)*elem(ie)%metdet(:,:))
-       enddo
-       call repro_sum(aratio, area, nelemd, nelemd, 1, commid=par%comm)
-       area(1) = 4*dd_pi/area(1)  ! ratio correction
-       deallocate(aratio)
-       if (par%masterproc) &
+    ! OG What is this? Find out!
+    if ( topology == "cube" ) then 
+       ! Alpha correction of area, works with uniform meshes.
+       if( cubed_sphere_map == 0 ) then
+          area = 0
+          do ie=1,nelemd
+             ! old
+             aratio(ie,1) = sum(elem(ie)%mp(:,:)*elem(ie)%metdet(:,:))
+             ! new
+             !aratio(ie,1) = 0.0d0
+             !do j = 1,np
+             !   do i = 1,np
+             !      aratio(ie,1) = aratio(ie,1) + gp%weights(i)*gp%weights(j)*elem(ie)%metdet(i,j)
+             !   enddo
+             !enddo
+          enddo
+          call repro_sum(aratio, area, nelemd, nelemd, 1, commid=par%comm)
+          area(1) = 4*dd_pi/area(1)  ! ratio correction
+          if (par%masterproc) &
             write(iulog,'(a,f20.17)') " re-initializing cube elements: area correction=",area(1)
 
-       do ie=1,nelemd
-          call cube_init_atomic(elem(ie),gp%points,area(1))
-          !call rotation_init_atomic(elem(ie),rot_type)
-       enddo
-    end if
+          do ie=1,nelemd
+             call cube_init_atomic(elem(ie),gp%points,area(1))
+             !call rotation_init_atomic(elem(ie),rot_type) ! why is this disabled?
+          enddo
+       ! Epsilon bubble correction for RRM meshes.
+       ! Note that this code is identical to the one in init_mod for SW
+       ! equations. 
+       elseif ( cubed_sphere_map == 2 ) then
+          do ie=1,nelemd
+             ! Obtain area of element = sum of areas of 2 triangles
+             call sphere_tri_area(elem(ie)%corners3D(1), elem(ie)%corners3D(2), &
+                                  elem(ie)%corners3D(3), area_sphere)
+             call sphere_tri_area(elem(ie)%corners3D(1), elem(ie)%corners3D(3), &
+                                  elem(ie)%corners3D(4), area_dummy)
+             ! Store element's area in area_sphere
+             area_sphere = area_sphere + area_dummy
 
+             ! Compute 'numerical area' of the element as sum of integration
+             ! weights.
+             area_num = 0.0
+             do i = 1,np
+                do j = 1,np          
+                   area_num = area_num + gp%weights(i)*gp%weights(j)*elem(ie)%metdet(i,j)
+                enddo
+             enddo
 
-    if(par%masterproc) write(iulog,*) 're-running mass_matrix'
+             ! Compute sum of inner integration weights for correction.
+             sum_w = 0 ! or sum_w = sum(elem(ie)%mp(2:np-1,2:np-1)*elem(ie)%metdet(2:np-1,2:np-1))
+             do j = 2, np-1
+                do i = 2, np-1
+                   sum_w = sum_w + gp%weights(i)*gp%weights(j)*elem(ie)%metdet(i,j)
+                enddo
+             enddo
+             !Which tol is to use here?
+             if ( sum_w > 1e-15 ) then
+                delta = (area_sphere - area_num)/sum_w
+                call cube_init_atomic(elem(ie),gp%points,1.0 + delta)
+             else
+                ! Abort since the denominator in correction is too small.
+             call abortmp('Area correction based on eps. bubble cannot be done, sum of inner integration weights is too small.') 
+             endif
+             !call rotation_init_atomic(elem(ie),rot_type)
+          enddo ! loop over elements
+
+          ! Temporary code for verification.
+          area = 0
+          do ie=1,nelemd
+             aratio(ie,1) = 0.0
+             do i = 1,np
+                do j=1,np
+                   aratio(ie,1) = aratio(ie,1) + gp%weights(i)*gp%weights(j)*elem(ie)%metdet(i,j)
+                enddo
+             enddo
+          enddo
+
+          call repro_sum(aratio, area, nelemd, nelemd, 1, commid=par%comm)
+          if (par%masterproc) &
+             write(iulog,'(a,f20.17)') "Epsilon bubble correction: Corrected area - 4\pi ",area(1) - 4.0d0*dd_pi
+
+       endif ! end of cubed_sphere_map==2
+    endif ! end of topology == 'cube'
+
+    deallocate(aratio)
+
+    if(par%masterproc) write(iulog,*) 'Running mass_matrix after area corrections.'
     call mass_matrix(par,elem)
 
 
