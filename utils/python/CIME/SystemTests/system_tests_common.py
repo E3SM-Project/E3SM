ea6ed623a767271b3d795cd0f19e55d95221554b
Jim Edwards <jedwards@ucar.edu>
2016-09-13 08:14:39 -0600

    remove debug code

--- utils/python/CIME/SystemTests/system_tests_common.py
+++ utils/python/CIME/SystemTests/system_tests_common.py
@@ -232,12 +232,10 @@ class SystemTestsCommon(object):
         run case needs indirection based on success.
         """
         success, comments = compare_test(self._case, suffix1, suffix2)
-        logger.warn("_component_compare_test 1 returns %s"%success)
         append_status(comments, sfile="TestStatus.log")
         status = TEST_PASS_STATUS if success else TEST_FAIL_STATUS
         with self._test_status:
             self._test_status.set_status("%s_%s_%s" % (COMPARE_PHASE, suffix1, suffix2), status)
-        logger.warn("_component_compare_test returns %s"%success)
         return success
 
     def _get_mem_usage(self, cpllog):
