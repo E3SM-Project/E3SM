302805b0a380c32f557cc9ff67139c9c848229df
Bill Sacks <sacks@ucar.edu>
2016-09-13 11:47:34 -0600

    Convert irrigation fraction back to a flux before sending to ROF
    Test suite: None
    Test baseline: N/A
    Test namelist changes: N/A
    Test status: N/A
    
    Fixes: None
    
    User interface changes?: No
    
    Code review: None

--- driver_cpl/shr/seq_flds_mod.F90
+++ driver_cpl/shr/seq_flds_mod.F90
@@ -1943,24 +1943,13 @@ module seq_flds_mod
      attname  = 'Flrl_rofi'
      call metadata_set(attname, longname, stdname, units)
 
-     ! TODO(wjs, 2016-09-09) Currently lnd sends an actual irrigation flux, but rof
-     ! receives a fractional amount (fraction of VOLR). We plan to change this so that
-     ! rof receives the irrigation flux.
-
      ! Irrigation flux (land/rof only)
      call seq_flds_add(l2x_fluxes,"Flrl_irrig")
+     call seq_flds_add(x2r_fluxes,"Flrl_irrig")
      longname = 'Irrigation flux (withdrawal from rivers)'
      stdname  = 'irrigation'
      units    = 'kg m-2 s-1'
      attname  = 'Flrl_irrig'
-     call metadata_set(attname, longname, stdname, units)
-
-     ! Irrigation fraction (land/rof only)
-     call seq_flds_add(x2r_fluxes,"Flrl_frac_irrig")
-     longname = 'Irrigation fraction'
-     stdname  = 'irrigation_fraction'
-     units    = '1'
-     attname  = 'Flrl_frac_irrig'
      call metadata_set(attname, longname, stdname, units)
 
      !-----------------------------
