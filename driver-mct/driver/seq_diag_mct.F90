33c98f97b575027f90aecb402ab41bf53386adc3
Bill Sacks <sacks@ucar.edu>
2016-09-28 19:54:55 -0600

    Avoid trying to handle the irrigation field in ACME
    Test suite: None
    Test baseline: N/A
    Test namelist changes: N/A
    Test status: N/A
    
    Fixes: None
    
    User interface changes?: No
    
    Code review: None

--- driver-mct/driver/seq_diag_mct.F90
+++ driver-mct/driver/seq_diag_mct.F90
@@ -852,7 +852,7 @@ subroutine seq_diag_lnd_mct( lnd, frac_l, infodata, do_l2x, do_x2l)
          index_l2x_Flrl_rofsub = mct_aVect_indexRA(l2x_l,'Flrl_rofsub')
          index_l2x_Flrl_rofdto = mct_aVect_indexRA(l2x_l,'Flrl_rofdto')
          index_l2x_Flrl_rofi   = mct_aVect_indexRA(l2x_l,'Flrl_rofi')
-         index_l2x_Flrl_irrig  = mct_aVect_indexRA(l2x_l,'Flrl_irrig')
+         index_l2x_Flrl_irrig  = mct_aVect_indexRA(l2x_l,'Flrl_irrig', perrWith='quiet')
 
          index_l2x_Fall_evap_16O    = mct_aVect_indexRA(l2x_l,'Fall_evap_16O',perrWith='quiet')
          if ( index_l2x_Fall_evap_16O /= 0 ) flds_wiso_lnd = .true.
@@ -882,8 +882,10 @@ subroutine seq_diag_lnd_mct( lnd, frac_l, infodata, do_l2x, do_x2l)
          if = f_wroff ; budg_dataL(if,ic,ip) = budg_dataL(if,ic,ip) - dl*l2x_l%rAttr(index_l2x_Flrl_rofsur,n) &
                                                                     - dl*l2x_l%rAttr(index_l2x_Flrl_rofgwl,n) &
                                                                     - dl*l2x_l%rAttr(index_l2x_Flrl_rofsub,n) &
-                                                                    - dl*l2x_l%rAttr(index_l2x_Flrl_irrig,n) &
                                                                     - dl*l2x_l%rAttr(index_l2x_Flrl_rofdto,n)
+         if (index_l2x_Flrl_irrig /= 0) then
+            if = f_wroff ; budg_dataL(if,ic,ip) = budg_dataL(if,ic,ip) - dl*l2x_l%rAttr(index_l2x_Flrl_irrig,n)
+         end if
          if = f_wioff ; budg_dataL(if,ic,ip) = budg_dataL(if,ic,ip) - dl*l2x_l%rAttr(index_l2x_Flrl_rofi,n)
 
          if ( flds_wiso_lnd )then
@@ -1060,7 +1062,7 @@ subroutine seq_diag_rof_mct( rof, frac_r, infodata)
       index_x2r_Flrl_rofgwl = mct_aVect_indexRA(x2r_r,'Flrl_rofgwl')
       index_x2r_Flrl_rofsub = mct_aVect_indexRA(x2r_r,'Flrl_rofsub')
       index_x2r_Flrl_rofdto = mct_aVect_indexRA(x2r_r,'Flrl_rofdto')
-      index_x2r_Flrl_irrig  = mct_aVect_indexRA(x2r_r,'Flrl_irrig')
+      index_x2r_Flrl_irrig  = mct_aVect_indexRA(x2r_r,'Flrl_irrig', perrWith='quiet')
       index_x2r_Flrl_rofi   = mct_aVect_indexRA(x2r_r,'Flrl_rofi')
 
       index_x2r_Flrl_rofl_16O = mct_aVect_indexRA(x2r_r,'Flrl_rofl_16O', perrWith='quiet')
@@ -1084,8 +1086,10 @@ subroutine seq_diag_rof_mct( rof, frac_r, infodata)
       if = f_wroff; budg_dataL(if,ic,ip) = budg_dataL(if,ic,ip) + dr*x2r_r%rAttr(index_x2r_Flrl_rofsur,n) &
                                                                 + dr*x2r_r%rAttr(index_x2r_Flrl_rofgwl,n) &
                                                                 + dr*x2r_r%rAttr(index_x2r_Flrl_rofsub,n) &
-                                                                + dr*x2r_r%rAttr(index_x2r_Flrl_rofdto,n) &
-                                                                + dr*x2r_r%rAttr(index_x2r_Flrl_irrig,n)
+                                                                + dr*x2r_r%rAttr(index_x2r_Flrl_rofdto,n)
+      if (index_x2r_Flrl_irrig /= 0) then
+         if = f_wroff; budg_dataL(if,ic,ip) = budg_dataL(if,ic,ip) + dr*x2r_r%rAttr(index_x2r_Flrl_irrig,n)
+      end if
 
       if = f_wioff; budg_dataL(if,ic,ip) = budg_dataL(if,ic,ip) + dr*x2r_r%rAttr(index_x2r_Flrl_rofi,n)
 
