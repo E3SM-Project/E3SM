9028e81362ce97082b907c16003b1de52de04cf6
James Foucar <jgfouca@sandia.gov>
2016-10-12 15:39:06 -0600

    Add cimeroot env setting back to standard_script_setup
    Will allow external tools to know where to find CIME.

--- scripts/Tools/standard_script_setup.py
+++ scripts/Tools/standard_script_setup.py
@@ -9,6 +9,9 @@ _CIMEROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..","..")
 _LIB_DIR = os.path.join(_CIMEROOT, "utils", "python")
 sys.path.append(_LIB_DIR)
 
+# Important: Allows external tools to link up with CIME
+os.environ["CIMEROOT"] = _CIMEROOT
+
 import CIME.utils
 CIME.utils.check_minimum_python_version(2, 7)
 CIME.utils.stop_buffering_output()
