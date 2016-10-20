e3344e62bc732ea43b8faf964da3446abe76fa16
Kris Brown <kris.brown3901@gmail.com>
2016-10-20 15:16:41 -0400

    Add inithistory to energyflux type

--- components/clm/src/main/ColumnEnergyFluxType.f90
+++ components/clm/src/main/ColumnEnergyFluxType.f90
@@ -16,9 +16,6 @@ module ColumnEnergyFluxType
   private
 
 
-
-
-
   type, public :: soilcol_energy_flux
    real(r8), pointer :: eflx_snomelt_col        (:)   ! col snow melt heat flux (W/m**2)
    real(r8), pointer :: eflx_snomelt_r_col      (:)   ! col rural snow melt heat flux (W/m**2)
@@ -42,6 +39,7 @@ module ColumnEnergyFluxType
   contains
     procedure, public :: Init => init_col_ef
     procedure, public :: InitAcclocate => initallocate_col_ef
+    procedure, public :: InitHistory => inithistory_col_ef
     procedure, public :: Clean => clean_col_ef
 
 end type soilcol_energy_flux
@@ -65,8 +63,6 @@ subroutine init_col_ef(this, bounds, t_grnd_col)
 end subroutine init_col_ef
 
 
-
-
 subroutine initallocate_col_ef(this, begc, endc)
   class(soilcol_energy_flux) :: this
   integer, intent(in) :: begc   ! beginning soil column index
@@ -88,6 +84,82 @@ subroutine initallocate_col_ef(this, begc, endc)
   allocate( this%errlon_col              (begc:endc))             ; this%errlon_col              (:)   = nan
 
 end subroutine init_col_ef
+
+
+subroutine inithistory_col_ef(this, bounds)
+  !
+  ! !DESCRIPTION:
+  ! Initialize module data structure
+  !
+  ! !USES:
+  use shr_infnan_mod , only : nan => shr_infnan_nan, assignment(=)
+  use clm_varpar     , only : nlevsno, nlevgrnd, crop_prog 
+  use clm_varctl     , only : use_cn
+  use histFileMod    , only : hist_addfld1d, hist_addfld2d, no_snow_normal
+  !
+  ! !ARGUMENTS:
+  class(energyflux_type) :: this
+  type(bounds_type), intent(in) :: bounds  
+  !
+  ! !LOCAL VARIABLES:
+  integer           :: begp, endp
+  integer           :: begc, endc
+  integer           :: begg, endg
+  character(10)     :: active
+  real(r8), pointer :: data2dptr(:,:), data1dptr(:) ! temp. pointers for slicing larger arrays
+  !------------------------------------------------------------------------
+
+  begp = bounds%begp; endp= bounds%endp
+  begc = bounds%begc; endc= bounds%endc
+  begg = bounds%begg; endg= bounds%endg
+  
+  this%eflx_snomelt_col(begc:endc) = spval
+  call hist_addfld1d (fname='FSM',  units='W/m^2',  &
+       avgflag='A', long_name='snow melt heat flux', &
+       ptr_col=this%eflx_snomelt_col, c2l_scale_type='urbanf')
+
+  this%eflx_snomelt_r_col(begc:endc) = spval
+  call hist_addfld1d (fname='FSM_R',  units='W/m^2',  &
+       avgflag='A', long_name='Rural snow melt heat flux', &
+       ptr_col=this%eflx_snomelt_r_col, set_spec=spval)
+
+  this%eflx_snomelt_u_col(begc:endc) = spval
+  call hist_addfld1d (fname='FSM_U',  units='W/m^2',  &
+       avgflag='A', long_name='Urban snow melt heat flux', &
+       ptr_col=this%eflx_snomelt_u_col, c2l_scale_type='urbanf', set_nourb=spval)
+
+  this%eflx_building_heat_col(begc:endc) = spval
+  call hist_addfld1d (fname='BUILDHEAT', units='W/m^2',  &
+       avgflag='A', long_name='heat flux from urban building interior to walls and roof', &
+       ptr_col=this%eflx_building_heat_col, set_nourb=0._r8, c2l_scale_type='urbanf')
+
+  this%eflx_urban_ac_col(begc:endc) = spval
+  call hist_addfld1d (fname='URBAN_AC', units='W/m^2',  &
+       avgflag='A', long_name='urban air conditioning flux', &
+       ptr_col=this%eflx_urban_ac_col, set_nourb=0._r8, c2l_scale_type='urbanf')
+
+  this%eflx_urban_heat_col(begc:endc) = spval
+  call hist_addfld1d (fname='URBAN_HEAT', units='W/m^2',  &
+       avgflag='A', long_name='urban heating flux', &
+       ptr_col=this%eflx_urban_heat_col, set_nourb=0._r8, c2l_scale_type='urbanf')
+
+  this%eflx_fgr12_col(begc:endc) = spval
+  call hist_addfld1d (fname='FGR12',  units='W/m^2',  &
+       avgflag='A', long_name='heat flux between soil layers 1 and 2', &
+       ptr_col=this%eflx_fgr12_col, set_lake=spval)
+
+  this%eflx_fgr_col(begc:endc,:) = spval
+  call hist_addfld2d (fname='FGR_SOIL_R', units='watt/m^2', type2d='levgrnd', &
+       avgflag='A', long_name='Rural downward heat flux at interface below each soil layer', &
+       ptr_col=this%eflx_fgr_col, set_spec=spval, default='inactive')
+
+  this%errsoi_col(begc:endc) = spval
+  call hist_addfld1d (fname='ERRSOI',  units='W/m^2',  &
+       avgflag='A', long_name='soil/lake energy conservation error', &
+       ptr_col=this%errsoi_col)
+
+end subroutine inithistory_col_ef
+
 
 subroutine clean_col_ef(this)
     class(soilcol_energy_flux) :: this
