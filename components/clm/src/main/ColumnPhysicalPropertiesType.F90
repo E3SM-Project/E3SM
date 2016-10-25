2d7c01d7092c800e1218bc908da23e24ae9a4dee
Dali Wang <wangd@ornl.gov>
2016-10-25 15:43:03 -0400

    Merged ChemStateType and CNDecompCascadeConType

--- components/clm/src/main/ColumnPhysicalPropertiesType.F90
+++ components/clm/src/main/ColumnPhysicalPropertiesType.F90
@@ -27,6 +27,21 @@ module ColumnPhysicalPropertiesType
   use clm_varcon     , only : ispval, spval
   use clm_varpar     , only : nlevsno, nlevgrnd, nlevlak
 
+
+! moved from ChemStateType
+  !------------------------------------------------------------------------------
+  ! !USES:
+  use shr_log_mod     , only : errMsg => shr_log_errMsg
+  use decompMod       , only : bounds_type
+  use abortutils      , only : endrun
+  use clm_varpar      , only : nlevsoi
+
+!  moved from CNDecompCascadeConType.F90
+
+  use decompMod      , only : bounds_type
+  use clm_varpar     , only : ndecomp_cascade_transitions, ndecomp_pools
+
+
   implicit none
   save
   private
@@ -63,7 +78,33 @@ module ColumnPhysicalPropertiesType
      real(r8), pointer :: zii                  (:)   ! convective boundary height [m]
      real(r8), pointer :: dz_lake              (:,:) ! lake layer thickness (m)  (1:nlevlak)
      real(r8), pointer :: z_lake               (:,:) ! layer depth for lake (m)
-     real(r8), pointer :: lakedepth            (:)   ! variable lake depth (m)                             
+     real(r8), pointer :: lakedepth            (:)   ! variable lake depth (m)          
+!  moved from chemStateType
+     real(r8), pointer :: soil_pH              (:,:) ! soil pH (-nlevsno+1:nlevgrnd)                   
+
+!  moved from CNDecompCascadeConType.F90
+     !-- properties of each pathway along decomposition cascade 
+     character(len=8)  , pointer :: cascade_step_name(:)               ! name of transition
+     integer           , pointer :: cascade_donor_pool(:)              ! which pool is C taken from for a given decomposition step
+     integer           , pointer :: cascade_receiver_pool(:)           ! which pool is C added to for a given decomposition step
+
+     !-- properties of each decomposing pool
+     logical           , pointer  :: floating_cn_ratio_decomp_pools(:) ! TRUE => pool has fixed C:N ratio
+     logical           , pointer  :: floating_cp_ratio_decomp_pools(:) ! TRUE => pool has fixed C:N ratio
+     character(len=8)  , pointer  :: decomp_pool_name_restart(:)       ! name of pool for restart files
+     character(len=8)  , pointer  :: decomp_pool_name_history(:)       ! name of pool for history files
+     character(len=20) , pointer  :: decomp_pool_name_long(:)          ! name of pool for netcdf long names
+     character(len=8)  , pointer  :: decomp_pool_name_short(:)         ! name of pool for netcdf short names
+     logical           , pointer  :: is_litter(:)                      ! TRUE => pool is a litter pool
+     logical           , pointer  :: is_soil(:)                        ! TRUE => pool is a soil pool
+     logical           , pointer  :: is_cwd(:)                         ! TRUE => pool is a cwd pool
+     real(r8)          , pointer  :: initial_cn_ratio(:)               ! c:n ratio for initialization of pools
+     real(r8)          , pointer  :: initial_cp_ratio(:)               ! c:n ratio for initialization of pools
+     real(r8)          , pointer  :: initial_stock(:)                  ! initial concentration for seeding at spinup
+     logical           , pointer  :: is_metabolic(:)                   ! TRUE => pool is metabolic material
+     logical           , pointer  :: is_cellulose(:)                   ! TRUE => pool is cellulose
+     logical           , pointer  :: is_lignin(:)                      ! TRUE => pool is lignin
+     real(r8)          , pointer  :: spinup_factor(:)                  ! factor by which to scale AD and relevant processes by
 
    contains
 
@@ -76,11 +117,32 @@ module ColumnPhysicalPropertiesType
   type(soilcol_properties) , public, target :: col_pp
 
   contains 
+!!!!***********DW***********change to (this, bounds) based on ChemStateType.F90
+!  subroutine init_col_pp(this, begc, endc)
+!    class(column_properties) :: this
+!    integer, intent(in) :: begc   ! beginning soil column index
+!    integer, intent(in) :: endc   ! ending soil column index
 
-  subroutine init_col_pp(this, begc, endc)
-    class(soilcol_properties) :: this
-    integer, intent(in) :: begc   ! beginning soil column index
-    integer, intent(in) :: endc   ! ending soil column index
+!  
+   subroutine init_col_pp(this, bounds)
+!   moved from ChemStateType.f90
+
+!    use clm_varpar            , only : nlevsoi
+
+    class(column_properties)         :: this
+    type(bounds_type), intent(in) :: bounds 
+
+
+    ! !LOCAL VARIABLES:
+    integer :: begp, endp
+    integer :: begc, endc
+    integer :: begg, endg
+    integer :: lbj,  ubj
+
+    begc = bounds%begc;
+    endc = bounds%endc
+    lbj  = 1;
+    ubj  = nlevsoi
 
     ! The following is set in initGridCellsMod  (***DW***: Need to considere Topounit?)
     allocate(this%gridcell    (begc:endc))                     ; this%gridcell    (:)   = ispval
@@ -108,6 +170,59 @@ module ColumnPhysicalPropertiesType
     allocate(this%n_melt      (begc:endc))                     ; this%n_melt      (:)   = nan 
     allocate(this%topo_slope  (begc:endc))                     ; this%topo_slope  (:)   = nan
     allocate(this%topo_std    (begc:endc))                     ; this%topo_std    (:)   = nan
+!   moved from ChemStateType.F90
+    allocate(this%soil_pH(begc:endc, lbj:ubj))
+
+!  moved from CNDecompCascadeConType.F90
+    ! !DESCRIPTION:
+    ! Initialize decomposition cascade state
+    !------------------------------------------------------------------------
+
+    !-- properties of each pathway along decomposition cascade 
+    allocate(decomp_cascade_con%cascade_step_name(1:ndecomp_cascade_transitions))
+    allocate(decomp_cascade_con%cascade_donor_pool(1:ndecomp_cascade_transitions))
+    allocate(decomp_cascade_con%cascade_receiver_pool(1:ndecomp_cascade_transitions))
+
+    !-- properties of each decomposing pool
+    allocate(decomp_cascade_con%floating_cn_ratio_decomp_pools(0:ndecomp_pools))
+    allocate(decomp_cascade_con%floating_cp_ratio_decomp_pools(0:ndecomp_pools))
+    allocate(decomp_cascade_con%decomp_pool_name_restart(0:ndecomp_pools))
+    allocate(decomp_cascade_con%decomp_pool_name_history(0:ndecomp_pools))
+    allocate(decomp_cascade_con%decomp_pool_name_long(0:ndecomp_pools))
+    allocate(decomp_cascade_con%decomp_pool_name_short(0:ndecomp_pools))
+    allocate(decomp_cascade_con%is_litter(0:ndecomp_pools))
+    allocate(decomp_cascade_con%is_soil(0:ndecomp_pools))
+    allocate(decomp_cascade_con%is_cwd(0:ndecomp_pools))
+    allocate(decomp_cascade_con%initial_cn_ratio(0:ndecomp_pools))
+    allocate(decomp_cascade_con%initial_cp_ratio(0:ndecomp_pools))
+    allocate(decomp_cascade_con%initial_stock(0:ndecomp_pools))
+    allocate(decomp_cascade_con%is_metabolic(0:ndecomp_pools))
+    allocate(decomp_cascade_con%is_cellulose(0:ndecomp_pools))
+    allocate(decomp_cascade_con%is_lignin(0:ndecomp_pools))
+    allocate(decomp_cascade_con%spinup_factor(0:ndecomp_pools))
+
+    !-- properties of each pathway along decomposition cascade 
+    decomp_cascade_con%cascade_step_name(1:ndecomp_cascade_transitions) = ''
+    decomp_cascade_con%cascade_donor_pool(1:ndecomp_cascade_transitions) = 0
+    decomp_cascade_con%cascade_receiver_pool(1:ndecomp_cascade_transitions) = 0
+
+    !-- first initialization of properties of each decomposing pool
+    decomp_cascade_con%floating_cn_ratio_decomp_pools(0:ndecomp_pools) = .false.
+    decomp_cascade_con%floating_cp_ratio_decomp_pools(0:ndecomp_pools) = .false.
+    decomp_cascade_con%decomp_pool_name_history(0:ndecomp_pools)       = ''
+    decomp_cascade_con%decomp_pool_name_restart(0:ndecomp_pools)       = ''
+    decomp_cascade_con%decomp_pool_name_long(0:ndecomp_pools)          = ''
+    decomp_cascade_con%decomp_pool_name_short(0:ndecomp_pools)         = ''
+    decomp_cascade_con%is_litter(0:ndecomp_pools)                      = .false.
+    decomp_cascade_con%is_soil(0:ndecomp_pools)                        = .false.
+    decomp_cascade_con%is_cwd(0:ndecomp_pools)                         = .false.
+    decomp_cascade_con%initial_cn_ratio(0:ndecomp_pools)               = nan
+    decomp_cascade_con%initial_cp_ratio(0:ndecomp_pools)               = nan
+    decomp_cascade_con%initial_stock(0:ndecomp_pools)                  = nan
+    decomp_cascade_con%is_metabolic(0:ndecomp_pools)                   = .false.
+    decomp_cascade_con%is_cellulose(0:ndecomp_pools)                   = .false.
+    decomp_cascade_con%is_lignin(0:ndecomp_pools)                      = .false.
+    decomp_cascade_con%spinup_factor(0:ndecomp_pools)                  = nan
   
   subroutine clean_col_pp(this)
     class(column_physical_properties) :: this
@@ -138,6 +253,7 @@ module ColumnPhysicalPropertiesType
     deallocate(this%n_melt     )
     deallocate(this%topo_slope )
     deallocate(this%topo_std   )
+    deallocate(this%soil_pH   )
 
   end subroutine clean_col_pp
 
