740f3aadb9fa2aced965bdb765fc1d7b270cf8ca
Bill Sacks <sacks@ucar.edu>
2016-11-02 09:27:25 -0600

    Fix scripts_regression_tests for new output format
    Test suite: None
    Test baseline: N/A
    Test namelist changes: N/A
    Test status: N/A
    
    Fixes: None
    
    User interface changes?: No
    
    Code review: None

--- utils/python/tests/scripts_regression_tests.py
+++ utils/python/tests/scripts_regression_tests.py
@@ -884,12 +884,12 @@ class Q_TestBlessTestResults(TestCreateTestCommon):
         test_id = "%s-%s" % (self._baseline_name, CIME.utils.get_timestamp())
         self.simple_test(False, "%s -t %s" % (comparg, test_id))
 
-        # compare_test_results should detect the fail
+        # compare_test_results should run to completion
         cpr_cmd = "%s/compare_test_results -b %s -t %s 2>&1" % (TOOLS_DIR, self._baseline_name, test_id)
-        output = run_cmd_assert_result(self, cpr_cmd, expected_stat=1)
+        output = run_cmd_no_fail(cpr_cmd)
 
         # use regex
-        expected_pattern = re.compile(r'COMPARE FAILED FOR TEST: %s[^\s]* reason Diff' % self._test_name)
+        expected_pattern = re.compile(r'FAIL %s[^\s]* BASELINE' % self._test_name)
         the_match = expected_pattern.search(output)
         self.assertNotEqual(the_match, None,
                             msg="Cmd '%s' failed to display failed test in output:\n%s" % (cpr_cmd, output))
