bfe9984ea04b0e47d1634a1990164a70e1cdf98c
Mark Petersen <mpetersen@lanl.gov>
2016-11-14 22:49:17 -0700

    bit-for-bit changes to ACME from MPAS-Ocean
    This includes:
    - changing MPAS-O submodule hash
    - updating namelist and streams to match

--- components/homme/dcmip_tests/dcmip2012_test1.3_thin_clouds_over_orography/README.txt
+++
@@ -1,2 +0,0 @@
-DCMIP2012 test 1-3
-https://www.earthsystemcog.org/projects/dcmip-2012/example_results/test-13
--- components/homme/dcmip_tests/dcmip2012_test1.3_thin_clouds_over_orography/jobscript-darwin-preqx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#   Jobscript for launching dcmip2012 test1-3 on a mac running Darwin
-#
-# usage: ./jobscript-...
-
-EXEC=../../test_execs/preqx-nlev60-interp/preqx-nlev60-interp     # set name of executable
-openmpiexec -n 6 $EXEC < ./namelist-lowres.nl       # launch simulation
