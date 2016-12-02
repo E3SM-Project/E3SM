d1510eda41b24ca8342aef09a114139572d2cfce
David J. Gardner <gardner48@llnl.gov>
2016-12-02 15:36:17 -0800

    Add #ifndef to skip vertical remap with implicit time stepping
    Add missing #ifndef TRILINOS around vertical remap, otherwise runs fail with errors for negative layer thickness

--- components/homme/src/share/prim_driver_mod.F90
+++ components/homme/src/share/prim_driver_mod.F90
@@ -1065,7 +1065,9 @@ contains
     !compute timelevels for tracers (no longer the same as dynamics)
     ! note: time level update for fvm tracers takes place in fvm_mod
     call TimeLevel_Qdp( tl, qsplit, n0_qdp, np1_qdp)
+#ifndef TRILINOS
     call vertical_remap(hybrid,elem,fvm,hvcoord,dt_remap,tl%np1,np1_qdp,n0_fvm,nets,nete)
+#endif
 
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     ! time step is complete.  update some diagnostic variables:
