398a1047cb1cbb5bb9040178e5b3de864161ee12
zshaheen <zshaheen93@gmail.com>
2016-12-14 13:03:19 -0800

    Moved main stuff to acme_diags folder and added conda and setup.py for when actual modules are made.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="acme_diags",
+    version="0.1",
+    author="Zeshawn Shaheen, Jill Zhang",
+    author_email="aims@llnl.gov",
+    description="ACME Diagnostics.",
+    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+    include_package_data=True
+)
