37d2fa9d127645c554558320908457831b6cf5df
Mariana Vertenstein <mvertens@ucar.edu>
2017-01-08 15:12:23 -0700

    added test to ensure that cesm and acme attributes were working correctly for X compset and CPL_SEQ_OPTION

--- utils/python/tests/scripts_regression_tests.py
+++ utils/python/tests/scripts_regression_tests.py
@@ -1141,6 +1141,12 @@ class K_TestCimeCase(TestCreateTestCommon):
                             msg="Build complete had wrong value '%s'" %
                             build_complete)
 
+            model_specific_val = case.get_value("CPL_SEQ_OPTION")
+            if CIME.utils.get_model() == 'cesm':
+                self.assertEqual(model_specific_val, "RASM_OPTION1")
+            else:
+                self.assertEqual(model_specific_val, "CESM1_MOD")
+
             # Test some test properties
             self.assertEqual(case.get_value("TESTCASE"), "TESTRUNPASS")
 
