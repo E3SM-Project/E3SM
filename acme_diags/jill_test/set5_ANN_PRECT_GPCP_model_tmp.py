a9e8360fc2f985a5bd1d7813bb663f7784dbff57
Zhang <zhang40@zhangml.llnl.gov>
2017-01-10 13:23:04 -0800

    add main title, output data name

--- acme_diags/jill_test/set5_ANN_PRECT_GPCP_model_tmp.py
+++ acme_diags/jill_test/set5_ANN_PRECT_GPCP_model_tmp.py
@@ -25,10 +25,14 @@ def compute_corr(model, obs):
         print err
     return corr
 
-reference_data_path='/space1/test_data/obs_for_diagnostics/'  # observation
-test_data_path='/space/golaz1/ACME_simulations/20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01/pp/clim_rgr/0070-0099/'  # model
-#reference_data_path='../'
-#test_data_path='../'
+#reference_data_path='/space1/test_data/obs_for_diagnostics/'  # observation
+#test_data_path='/space/golaz1/ACME_simulations/20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01/pp/clim_rgr/0070-0099/'  # model
+reference_data_path='../'
+test_data_path='../'
+
+#Pre defined variables
+var='PRECT'
+season='ANN'
 
 #Read in data
 reference_data_set='GPCP_v2.2_ANN_climo.nc'  # observation
@@ -38,7 +42,8 @@ f_obs=cdms2.open(reference_data_path + reference_data_set)
 f_mod=cdms2.open(test_data_path + test_data_set)
 
 obs_pr=f_obs('PRECT')
-mod_pr=(f_mod('PRECC')+f_mod('PRECL'))*3600.0*24.0*1000.0
+mod_pr=(f_mod('PRECC')+f_mod('PRECL'))*3600.0*24.0*1000.0 #this is derived variable convert m/s to mm/d
+mod_pr.units='mm/d'
 
 
 #For plotting, original grid is plotted for model observation, differece plot is regridded to coaser grid. Need if statement to evaluate grid size. aminusb_2ax from uvcmetrics takes care of this,which also considers complex corner cases.
@@ -53,8 +58,6 @@ else:
     obs_pr_reg=obs_pr
     mod_pr_reg=mod_pr.regrid(obs_grid,regridTool='esmf',regridMethod='linear')
 dif_pr=mod_pr_reg-obs_pr_reg
-print dir(dif_pr)
-quit(dif_pr)
 
 #calculate metrics and pass in as mv attribute,failed
 #obs_pr.mean=round(cdutil.averager(obs_pr, axis='xy', weights='generate'),2) #area-weighting
@@ -71,8 +74,8 @@ quit(dif_pr)
 
 
 #CORR and RMSE need to be calculated after reduction to ensure same array shapes.
-rmse= 'RMSE:'+'%.2f' %round(compute_rmse(obs_pr_reg, mod_pr_reg),2)
-corr= 'CORR:'+'%.2f' %round(compute_corr(obs_pr_reg, mod_pr_reg),2)
+rmse= 'RMSE:'+'%.2f' %compute_rmse(obs_pr_reg, mod_pr_reg)
+corr= 'CORR:'+'%.2f' %compute_corr(obs_pr_reg, mod_pr_reg)
 print rmse, corr
 
 #Plotting
@@ -88,9 +91,30 @@ template_2 = x.gettemplate('plotset5_0_x_2')
 mod_pr.long_name='model'
 obs_pr.long_name='observation'
 dif_pr.long_name='model-observation'
+
+#Or we can change title attribute, adding 3 more lines though...
+#mod_pr.title='model'
+#obs_pr.title='observation'
+#dif_pr.title='model-observation'
+#
+#del mod_pr.long_name
+#del obs_pr.long_name
+#del dif_pr.long_name
 template_0.title.priority=1
 template_1.title.priority=1
 template_2.title.priority=1
+
+template_0.units.priority=1
+template_1.units.priority=1
+template_2.units.priority=1
+
+#The dataname part should pass in basic model info: model_version...and basic obs info:obsname_version_years
+#Those information will be provided by metadata.
+template_0.dataname.priority=1
+template_1.dataname.priority=1
+
+mod_pr.dataname='test'
+
 
 isofill = x.createisofill()
 isofill.levels=[0,0.2, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 17]
@@ -100,6 +124,7 @@ x.plot(mod_pr, template_0, isofill)
 isofill = x.createisofill()
 isofill.levels=[0,0.2, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 17]
 
+#x.plot(obs_pr, template_1, isofill)
 x.plot(obs_pr, template_1, isofill)
 #x.setcolormap('bl_to_darkred')
 
@@ -119,6 +144,13 @@ x.plot(out_1)
 isofill = x.createisofill()
 isofill.levels=[-10,-8, -6, -4, -3, -2, -1,-0.5, 0, 0.5, 1, 2, 3, 4, 6, 8,10]
 x.plot(dif_pr, template_2, isofill)
+
+#Create main title for the 3 plots 
+main_title=vcs.createtext()
+main_title.string=' '.join([var,season])
+main_title.x=0.42
+main_title.y=0.98
+x.plot(main_title)
 
 x.png('test.png')
 
