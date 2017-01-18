8aaff27f40b02826c9c2fd473a6967979a6e315f
Jim Edwards <jedwards@ucar.edu>
2017-01-18 09:26:46 -0700

    add compsets schema to config_files.

--- utils/python/CIME/XML/compsets.py
+++ utils/python/CIME/XML/compsets.py
@@ -5,13 +5,18 @@ Common interface to XML files which follow the compsets format,
 from CIME.XML.standard_module_setup import *
 from CIME.XML.generic_xml import GenericXML
 from CIME.XML.entry_id import EntryID
+from CIME.XML.files import Files
+
 
 logger = logging.getLogger(__name__)
 
 class Compsets(GenericXML):
 
-    def __init__(self, infile=None):
-        GenericXML.__init__(self, infile)
+    def __init__(self, infile=None, files=None):
+        if files is None:
+            files = Files()
+        schema = files.get_schema("COMPSETS_SPEC_FILE")
+        GenericXML.__init__(self, infile, schema=schema)
         self.groups={}
 
     def get_compset_match(self, name):
