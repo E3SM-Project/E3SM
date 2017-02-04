E3SM-Project/E3SM

commit 6d493bc850e9bfa78cf35eeb0d5a86e8d9e2a4f2

cleanup, change args to da script

utils/python/CIME/case_run.py
@@ -177,9 +177,9 @@ def resubmit_check(case):
         submit(case, job=job, resubmit=True)
 
 ###############################################################################
-def do_data_assimilation(da_script, cycle, data_assimilation_cycles, lid):
+def do_data_assimilation(da_script, caseroot, cycle, lid):
 ###############################################################################
-    cmd = da_script + " 1> da.log.%s %d %d 2>&1" %(lid, cycle, data_assimilation_cycles)
+    cmd = da_script + " 1> da.log.%s %d %d 2>&1" %(lid, caseroot, cycle)
     logger.debug("running %s" %da_script)
     run_cmd_no_fail(cmd)
     # disposeLog(case, 'da', lid)  THIS IS UNDEFINED!
@@ -218,7 +218,7 @@ def case_run(case):
             get_timing(case, lid)     # Run the getTiming script
 
         if data_assimilation:
-            do_data_assimilation(data_assimilation_script, cycle, data_assimilation_cycles, lid)
+            do_data_assimilation(data_assimilation_script, case.get_value("CASEROOT"), cycle, lid)
 
         save_postrun_provenance(case)
 

utils/python/CIME/preview_namelists.py
@@ -72,6 +72,7 @@ def create_namelists(case):
         else:
             compname = case.get_value("COMP_%s" % model_str.upper())
         cmd = os.path.join(config_dir, "buildnml")
+        do_run_cmd = False
         try:
             with open(cmd, 'r') as f:
                 first_line = f.readline()
@@ -81,24 +82,22 @@ def create_namelists(case):
                 mod.buildnml(case, caseroot, compname)
             else:
                 raise SyntaxError
-
         except SyntaxError as detail:
             if 'python' in first_line:
                 expect(False, detail)
             else:
-                logger.info("   Running %s buildnml"%compname)
-                run_cmd_no_fail("%s %s" % (cmd, caseroot), verbose=False)
-                # refresh case xml object from file
-                case.read_xml()
+                do_run_cmd = True
         except AttributeError:
-            logger.info("   Running %s buildnml"%compname)
-            run_cmd_no_fail("%s %s" % (cmd, caseroot), verbose=False)
-            # refresh case xml object from file
-            case.read_xml()
+            do_run_cmd = True
         except:
             raise
 
-
+        if do_run_cmd:
+            logger.info("   Running %s buildnml"%compname)
+            case.flush()
+            run_cmd_no_fail("%s %s" % (cmd, caseroot), verbose=False)
+            # refresh case xml object from file
+            case.read_xml()            
     logger.info("Finished creating component namelists")
 
 

commit aa1c548d574b554528aa3a2ebe55ec56b938ca82

Changes requested in PR review
Added PRE and DAE tests to scripts_regression_tests
Added data-ESP data-change test to CESM prealpha testing
Fixed DAE errors introduced in 6d493bc8

cime_config/cesm/allactive/testlist_allactive.xml
@@ -371,4 +371,12 @@
       <option name="wallclock">  00:20 </option>
     </options>
   </test>
+  <test name="PRE" grid="f19_f19" compset="ADESP_TEST">
+    <machines>
+      <machine name="hobart" compiler="nag" category="prealpha"/>
+    </machines>
+    <options>
+      <option name="wallclock"> 00:20 </option>
+    </options>
+  </test>
 </testlist>

components/data_comps/desp/cime_config/user_nl_desp
@@ -6,8 +6,11 @@
 ! be modified below using the above syntax
 ! User preview_namelists to view (not modify) the output namelist in the
 ! directory $CASEROOT/CaseDocs
-! To modify the contents of a stream txt file, first use preview_namelists
-! to obtain the contents of the stream txt files in CaseDocs, and then
-! place a copy of the  modified stream txt file in $CASEROOT with the string
-! user_ prepended.
+!
+! Note that some namelist variables MAY NOT be changed in user_nl_desp - 
+! they are defined in a $CASEROOT xml file and must be changed with
+! xmlchange.
+!
+! For example, rather than set username to 'foo' in user_nl_desp, call
+! ./xmlchange USER=foo
 !------------------------------------------------------------------------

components/data_comps/desp/desp_comp_mod.F90
@@ -74,8 +74,9 @@ module desp_comp_mod
   integer,          parameter :: BAD_ID      = -1
   integer,          parameter :: NO_RPOINTER = -2
   integer,          parameter :: NO_RFILE    = -3
-  integer,          parameter :: NO_READ     = -4
-  integer,          parameter :: NO_WRITE    = -5
+  integer,          parameter :: NO_RPREAD   = -4
+  integer,          parameter :: NO_READ     = -5
+  integer,          parameter :: NO_WRITE    = -6
 
   type(shr_strdata_type)      :: SDESP
 
@@ -494,6 +495,8 @@ CONTAINS
         select case (errcode)
         case(NO_RPOINTER)
           call shr_sys_abort(subname//'Missing rpointer file for '//comp_names(ind))
+        case(NO_RPREAD)
+          call shr_sys_abort(subname//'Cannot read rpointer file for '//comp_names(ind))
         case(NO_RFILE)
           call shr_sys_abort(subname//'Missing restart file for '//comp_names(ind))
         case(NO_READ)
@@ -632,9 +635,10 @@ CONTAINS
     logical                          :: file_exists
     character(len=8)                 :: file_read
     character(len=CS)                :: rpointer_name
+    character(len=CL)                :: errmsg
     character(len=*), parameter      :: subname = "(desp_restart_filenames) "
 
-    retcode = noerr
+    retcode = NOERR
     filenames = ' '
     num_inst = size(filenames)
     allocate(ids(num_inst))
@@ -662,36 +666,38 @@ CONTAINS
 
     do ind = 1, num_inst
       rpointer_name = rpprefix//comp_names(comp_ind)//trim(seq_comm_suffix(ids(ind)))
-      inquire(file=rpointer_name, EXIST=file_exists, READ=file_read)
-      if (.not. file_exists) then
-        retcode = NO_RPOINTER
-      else if (trim(file_read) /= "YES") then
-        retcode = NO_READ
-      else
-        unitn = shr_file_getUnit()
-        if ((my_task == master_task) .and. (loglevel > 0)) then
-          write(logunit,"(3A)") subname,"read rpointer file ", rpointer_name
-        end if
-        open(unitn, file=rpointer_name, form='FORMATTED', status='old',iostat=ierr)
-        if (ierr < 0) then
-          call shr_sys_abort(subname//'rpointer file open returns an'// &
-               ' error condition')
-        end if
-        read(unitn,'(a)', iostat=ierr) filenames(ind)
-        if (ierr < 0) then
-          call shr_sys_abort(subname//'rpointer file read returns an'// &
-               ' error condition')
-        end if
-        close(unitn)
-        call shr_file_freeUnit(unitn)
-        inquire(file=filenames(ind), EXIST=file_exists, READ=file_read)
+      if (my_task == master_task) then
+        inquire(file=rpointer_name, EXIST=file_exists)
         if (.not. file_exists) then
-          retcode = NO_RFILE
-        else if (trim(file_read) /= "YES") then
-          retcode = NO_READ
-        ! No else
+          retcode = NO_RPOINTER
+        else
+          unitn = shr_file_getUnit()
+          if (loglevel > 0) then
+            write(logunit,"(3A)") subname,"read rpointer file ", rpointer_name
+          end if
+          open(unitn, file=rpointer_name, form='FORMATTED', status='old',     &
+               action='READ', iostat=ierr)
+          if (ierr /= 0) then
+            write(errmsg, '(a,i0)') 'rpointer file open returns error condition, ',ierr
+            call shr_sys_abort(subname//trim(errmsg))
+          end if
+          read(unitn,'(a)', iostat=ierr) filenames(ind)
+          if (ierr /= 0) then
+            write(errmsg, '(a,i0)') 'rpointer file read returns error condition, ',ierr
+            call shr_sys_abort(subname//trim(errmsg))
+          end if
+          close(unitn)
+          call shr_file_freeUnit(unitn)
+          inquire(file=filenames(ind), EXIST=file_exists)
+          if (.not. file_exists) then
+            retcode = NO_RFILE
+            ! No else
+          end if
         end if
       end if
+      ! Broadcast what we learned about files
+      call shr_mpi_bcast(retcode,  mpicom, 'rpointer status')
+      call shr_mpi_bcast(filenames(ind),  mpicom, 'filenames(ind)')
     end do
 
     if (allocated(ids)) then

utils/data_assimilation/da_no_data_mod.sh
@@ -13,18 +13,10 @@ if [ $# -ne 2 ]; then
   echo "ERROR: Wrong number of arguments, $# (should be 2)"
   errcode=$(( errcode + 1 ))
 else
-  cycle=$1
-  shift
-  total_cycles=$1
-  shift
-  if [ -n ${CASEROOT} ]; then
-    echo "caseroot: ${CASEROOT}"
-  else
-    echo "ERROR: CASEROOT not defined"
-    errcode=$(( errcode + 1 ))
-  fi
+  caseroot=$1
+  cycle=$2
+  echo "caseroot: ${caseroot}"
   echo "cycle: ${cycle}"
-  echo "total_cycles: ${total_cycles}"
 fi
 
 exit $errcode

utils/python/CIME/SystemTests/dae.py
@@ -80,7 +80,6 @@ class DAE(SystemTestsCompareTwo):
         for fname in da_files:
             found_caseroot = False
             found_cycle = False
-            found_total = False
             with open(fname) as dfile:
                 for line in dfile:
                     expect(line[0:5] != 'ERROR', "ERROR, error line found in %s"%fname)
@@ -91,17 +90,11 @@ class DAE(SystemTestsCompareTwo):
                         expect(int(line[7:]) == cycle_num,
                                "ERROR: Wrong cycle (%d) found in %s (expected %d)"%
                                (int(line[7:]), fname, cycle_num))
-                    elif line[0:12] == 'total_cycles':
-                        found_total = True
-                        expect(int(line[14:]) == da_cycles,
-                               "ERROR: Wrong total cycle count (%d) found in %s (expected %d)"%
-                               (int(line[14:]), fname, da_cycles))
                     else:
                         expect(False, "ERROR: Unrecognized line ('%s') found in %s"%(line, fname))
 
                 # End of for loop
                 expect(found_caseroot, "ERROR: No caseroot found in %s"%fname)
                 expect(found_cycle, "ERROR: No cycle found in %s"%fname)
-                expect(found_total, "ERROR: No total_cycles found in %s"%fname)
             # End of with
             cycle_num = cycle_num + 1

utils/python/CIME/case_run.py
@@ -184,7 +184,7 @@ def resubmit_check(case):
 ###############################################################################
 def do_data_assimilation(da_script, caseroot, cycle, lid):
 ###############################################################################
-    cmd = da_script + " 1> da.log.%s %d %d 2>&1" %(lid, caseroot, cycle)
+    cmd = da_script + " 1> da.log.%s %s %d 2>&1" %(lid, caseroot, cycle)
     logger.debug("running %s" %da_script)
     run_cmd_no_fail(cmd)
     # disposeLog(case, 'da', lid)  THIS IS UNDEFINED!

utils/python/update_acme_tests.py
@@ -47,7 +47,9 @@ _TEST_SUITES = {
                              "ERS_N2.f19_g16_rx1.A",
                              "ERR.f45_g37_rx1.A",
                              "ERP.f45_g37_rx1.A",
-                             "SMS_D_Ln9.f19_g16_rx1.A")
+                             "SMS_D_Ln9.f19_g16_rx1.A",
+                             "DAE.f19_f19.A",
+                             "PRE.f19_f19.ADESP")
                             ),
 
     #