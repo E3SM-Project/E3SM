852a1000956e09d32728c1850850f5894a0e1455
zshaheen <zshaheen93@gmail.com>
2017-02-16 13:23:36 -0800

    first version of conver_units() done

--- acme_diags/derivations/acme.py
+++ acme_diags/derivations/acme.py
@@ -1,3 +1,6 @@
+from numbers import Number
+from unidata import udunits
+
 def get_var_from_file_by_derived_var(nc_file, var_key):
     ''' Given a key to the derived_variables dict, compute and return
      whatever is described in derived_variables[var_key] '''
@@ -23,13 +26,37 @@ def _get_cdms_var_from_nc_file(inputs, nc_file):
     return args
 
 def rename(new_name):
-    ''' Given the new name, return it. '''
+    ''' Given the new name, just return it. '''
     return new_name
 
+def aplusb(var1, var2, target_units=None):
+    ''' Returns var1 + var2. If both of their units are not the same,
+    it tries to convert both of their units to target_units '''
+
+    if target_units is not None:
+        var1, var2 = _convert_units(var1, var2, target_units)
+
+    return var1 + var2
+
+def _convert_units(var1, var2, target_units):
+    ''' Converts units of var1 and var2 to target_units.
+    var1 and var2 are cdms.TransientVariable. '''
+    #return udunits(target_units, var1.units)
+    # temp is of type udunits
+    temp = udunits(1.0, var1.units)
+    coeff, offset= temp.how(target_units)
+
+    var1 = coeff*var1 + offset
+    var2 = coeff*var2 + offset
+
+    var1.units = target_units
+    var2.units = target_units
+
+    return var1, var2
 
 derived_variables = {
     'PRECT': [
         (['pr'],  rename),
-        (['PRECC','PRECL'], aplusb(a,b,units="mm/day"))
+        (['PRECC','PRECL'], lambda a, b: aplusb(a, b, target_units="mm/day"))
     ],
 }
