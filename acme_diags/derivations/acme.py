1356402f81e6ef8fe7707004e87eb40d4504a9de
zshaheen <zshaheen93@gmail.com>
2017-02-16 10:11:31 -0800

    cleanup

--- acme_diags/derivations/acme.py
+++ acme_diags/derivations/acme.py
@@ -1,27 +1,35 @@
-def get_something(var_key):
+def get_var_from_file_by_derived_var(nc_file, var_key):
     ''' Given a key to the derived_variables dict, compute and return
-     whatever is described in derived_variables[var_key]'''
+     whatever is described in derived_variables[var_key] '''
     # This function will interface with derived_variables and any other dict
     # in the derivations folder.
     if var_key in derived_variables.keys():
         derived_var_list = derived_variables[var_key]
         # What to do with multiple inputs???
         for inputs, func in derived_var_list:
-            return func(*inputs)
+            args = _get_cdms_var_from_nc_file(inputs, nc_file)
+            if args != []:
+                return func(*args)
     else:
         raise RuntimeError('The variable %s was not in the dictionary' % var)
+
+def _get_cdms_var_from_nc_file(inputs, nc_file):
+    ''' Given a list of string variable inputs, get
+    the actual cdms variables from the nc_file'''
+    args = []
+    for var in inputs:
+        if var in nc_file.variables.keys():
+            arg += [nc_file(var)(squeeze=1)]
+    return args
 
 def rename(new_name):
     ''' Given the new name, return it. '''
     return new_name
 
+
 derived_variables = {
     'PRECT': [
-        (['pr'],  lambda x: rename(x)),
-        (['pr2'],  lambda x: rename(x)),
-        #(['PRECC','PRECL'], (lambda a,b,units="mm/day": aplusb(a,b,units)) ))
+        (['pr'],  rename),
+        (['PRECC','PRECL'], aplusb(a,b,units="mm/day"))
     ],
 }
-
-
-print get_something('PRECT')
