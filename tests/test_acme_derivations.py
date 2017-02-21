c1faeb51a2825065ff9c9b5c30513eb397f4859c
zshaheen <zshaheen93@gmail.com>
2017-02-21 14:46:47 -0800

    two more unit tests

--- tests/test_acme_derivations.py
+++ tests/test_acme_derivations.py
@@ -14,5 +14,32 @@ class TestACMEDerivations(unittest.TestCase):
 
         self.assertEqual(new_var.units, 'mm/day')
 
+    def test_process_derived_var_passes(self):
+        derived_var = {
+            'PRECT': [
+                (['pr'],  lambda x: x),
+                (['PRECC'], lambda x: 'this is some function')
+            ],
+        }
+
+        precc_file = cdms2.open('precc.nc')
+        acme.process_derived_var('PRECT', derived_var, precc_file)
+        precc_file.close()
+
+    def test_process_derived_var_with_wrong_dict(self):
+        # pr, nothing, and nothing2 are not variables in the file we open
+        wrong_derived_var = {
+            'PRECT': [
+                (['pr'],  lambda x: x),
+                (['nothing','nothing2'], lambda x, y: 'this is some function')
+            ],
+        }
+
+        precc_file = cdms2.open('precc.nc')
+        with self.assertRaises(RuntimeError):
+            acme.process_derived_var('PRECT', wrong_derived_var, precc_file)
+        precc_file.close()
+
+
 if __name__ == '__main__':
     unittest.main()
