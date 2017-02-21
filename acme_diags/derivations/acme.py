c48506e905ea7828219046a44de043fc61ca2d7f
zshaheen <zshaheen93@gmail.com>
2017-02-21 14:05:29 -0800

    process_derived_var() refactored

--- acme_diags/derivations/acme.py
+++ acme_diags/derivations/acme.py
@@ -1,29 +1,27 @@
 from numbers import Number
 from unidata import udunits
 
-def get_var_from_file_by_derived_var(nc_file, var_key):
+def process_derived_var(var_key, nc_file):
     ''' Given a key to the derived_variables dict, compute and return
      whatever is described in derived_variables[var_key] '''
     # This function will interface with derived_variables and any other dict
     # in the derivations folder.
     if var_key in derived_variables.keys():
-        derived_var_list = derived_variables[var_key]
-        # What to do with multiple inputs???
-        for inputs, func in derived_var_list:
-            args = _get_cdms_var_from_nc_file(inputs, nc_file)
-            if args != []:
-                return func(*args)
+        inputs, func = _get_correct_derivation(var_key, nc_file)
+        # get all of the variables from nc_file
+        args = [nc_file(var)(squeeze=1) for var in inputs]
+        return func(*args)
     else:
         raise RuntimeError('The variable %s was not in the dictionary' % var)
 
-def _get_cdms_var_from_nc_file(inputs, nc_file):
-    ''' Given a list of string variable inputs, get
-    the actual cdms variables from the nc_file'''
-    args = []
-    for var in inputs:
-        if var in nc_file.variables.keys():
-            arg += [nc_file(var)(squeeze=1)]
-    return args
+def _get_correct_derivation(var_key, nc_file):
+    ''' Get the first valid derivation from the derived_variables dict. '''
+    derived_var_list = derived_variables[var_key]
+    # get the first function and args from
+    for inputs, func in derived_var_list:
+        # are all of the variables (inputs) in the nc_file?
+        if set(inputs).issubset(nc_file.variables.keys()):
+            return inputs, func
 
 def rename(new_name):
     ''' Given the new name, just return it. '''
