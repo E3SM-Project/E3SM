3df7b50af07674630a0ff485f60e6c5bf15d9861
Mark Taylor <mt@mp3dev.org>
2017-02-21 12:53:37 -0700

    dcmip4 namelists using rsplit=3

--- components/homme/dcmip_tests/dcmip2012_test4/theta/jobscript-snl.sh
+++ components/homme/dcmip_tests/dcmip2012_test4/theta/jobscript-snl.sh
@@ -18,8 +18,8 @@ endif
 # 2 min on 54 nodes on skybridge
 # NH model x1000 case
 set EXEC = ../../../test_execs/theta-nlev30/theta-nlev30
-#set namelist = nh-x1.nl           # 2592000 timesteps, 54 nodes - 320min?
-#set namelist = nh-x10.nl          #  648000 timsteps,  54 nodes - 80min ?
+#set namelist = nh-x1.nl           # 2592000 timesteps, 54 nodes - 7.4h
+#set namelist = nh-x10.nl          #  648000 timsteps,  54 nodes - 115min
 #set namelist = nh-x100.nl         #  64800  timesteps, 12 nodes - 51min 
 set namelist = nh-x1000.nl        #  6480 timesteps, 12 nodes - 5.9min
 #set namelist = h-x1.nl            #  4320 timesteps, 12 nodes, 3.3min
