4709b5ebc42c4fdab40ce4371ebf9f16eb370b73
zshaheen <zshaheen93@gmail.com>
2017-03-01 17:23:56 -0800

    Working with parameter

--- acme_diags/viewer/viewer.py
+++ acme_diags/viewer/viewer.py
@@ -3,39 +3,43 @@ import stat
 from output_viewer.build import build_viewer
 from output_viewer.utils import rechmod
 from output_viewer.index import OutputIndex, OutputPage, OutputFile, OutputRow, OutputGroup
+from acme_diags.acme_parameter import ACMEParameter
 
-path = '/Users/shaheen2/github/acme_diags/acme_diags/plotset5'
+parameter = ACMEParameter()
+parameter.case_id = 'set5_PRECT_GPCP'
+parameter.reference_name = 'GPCP (yrs1979-2009)'
+parameter.var = 'PRECT'
 
-ind = OutputIndex("Name of the model")
+# Use the commented code when this is called by the driver
+# path = os.path.abspath(parameter.case_id)
+path = os.path.join('/Users/shaheen2/github/acme_diags/acme_diags/plotset5', parameter.case_id)
+
+index = OutputIndex(parameter.reference_name)
 
 page1 = OutputPage("Set 5", ['Description', 'ANN', 'DJF', 'JJA', 'MAM', 'SON'])
 
 cols = []
-cols.append('some description')
-f1 = OutputFile(os.path.join(path, 'set5_PRECT_GPCP/GPCP_ANN.png'))
-cols.append(f1)
-f1 = OutputFile(os.path.join(path, 'set5_PRECT_GPCP/GPCP_DJF.png'))
-cols.append(f1)
-f1 = OutputFile(os.path.join(path, 'set5_PRECT_GPCP/GPCP_JJA.png'))
-cols.append(f1)
-f1 = OutputFile(os.path.join(path, 'set5_PRECT_GPCP/GPCP_MAM.png'))
-cols.append(f1)
-f1 = OutputFile(os.path.join(path, 'set5_PRECT_GPCP/GPCP_SON.png'))
-cols.append(f1)
+cols.append('Some description for %s' % parameter.case_id)
 
-r1 = OutputRow('PRECT', cols)
+files_in_dir = [fnm for fnm in os.listdir(path) if '.png' in fnm]
+print files_in_dir
+
+for f in files_in_dir:
+    cols.append(OutputFile(f))
+
+r1 = OutputRow(parameter.var, cols)
 
 g1 = OutputGroup('Variables for this obs')
-g2 = OutputGroup('Some Other Group')
+#g2 = OutputGroup('Some Other Group')
 
 page1.addGroup(g1)
-page1.addGroup(g2)
+#page1.addGroup(g2)
 page1.addRow(r1, 0)
 
 
-ind.addPage(page1)
-#ind.addPage(page2)
-ind.toJSON(os.path.join(path, "index.json"))
+index.addPage(page1)
+#index.addPage(page2)
+index.toJSON(os.path.join(path, "index.json"))
 
 
 
