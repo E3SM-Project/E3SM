92e61dc8f9c657e8473ab314bf4be5b6c3c9daa9
Mark Taylor <mt@mp3dev.org>
2017-03-18 17:06:48 -0600

    tweaks to dcmip3 jobscript

--- components/homme/dcmip_tests/dcmip2012_test2.2_nh_mountain_waves_with_shear/preqx/jobscript-snl.sh
+++ components/homme/dcmip_tests/dcmip2012_test2.2_nh_mountain_waves_with_shear/preqx/jobscript-snl.sh
@@ -24,13 +24,13 @@ endif
 #set EXEC = ../../../test_execs/theta-nlev60/theta-nlev60    
 #set namelist = namelist-default.nl
 
-
+###################################################################
 # hydrostatic preqx
+###################################################################
 set EXEC = ../../../test_execs/preqx-nlev60-interp/preqx-nlev60-interp        # set name of executable
 set namelist = namelist-default.nl
 
 \cp -f $namelist input.nl
-date
 mpirun -np $NCPU $EXEC < input.nl
-date
 ncl plot_lon_vs_z.ncl
+
