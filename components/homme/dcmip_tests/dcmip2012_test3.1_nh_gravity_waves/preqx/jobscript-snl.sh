51cd8828b4957bbd4a5abc7cd7057d01793374fb
Mark Taylor <mt@mp3dev.org>
2017-03-20 12:16:03 -0600

    adding snl jobscript for dcmip test 3

---
+++ components/homme/dcmip_tests/dcmip2012_test3.1_nh_gravity_waves/preqx/jobscript-snl.sh
@@ -0,0 +1,73 @@
+#!/bin/tcsh 
+#
+#   Jobscript for launching dcmip2012 test3-1 on the NERSC Edison machine
+#
+#SBATCH -J dcmip3-1           # job name
+#SBATCH -n 12                 # total number of mpi tasks requested
+#SBATCH -p ec                 # queue (partition) -- normal, development, etc.
+#SBATCH -t 00:10:00           # run time (hh:mm:ss)
+#SBATCH --account=FY150001
+
+set OMP_NUM_THREADS = 1
+set NCPU = 40 
+if ( ${?SLURM_NNODES} ) then   # skybridge
+    set NCPU = $SLURM_NNODES
+    @ NCPU *= 16
+    @ NCPU /= $OMP_NUM_THREADS
+endif
+
+#############################################################################
+# theta-nh
+#############################################################################
+set EXEC = ../../../test_execs/theta-nlev20/theta-nlev20
+set namelist = ./namelist-nonhydro.nl
+
+\rm -f input.nl
+cp $namelist input.nl
+mpirun -np $NCPU $EXEC < input.nl
+
+ncl plot_omega.ncl
+ncl plot_theta.ncl
+
+mv test31_omega.pdf                 nonhydro_test31_omega.pdf
+mv dcmip2012_test3_theta_diff.pdf   nonhydro_test3_theta_diff.pdf  
+mv movies/dcmip2012_test31.nc        movies/nonhydro_dcmip2012_test31.nc 
+
+#############################################################################
+# theta (hydrostatic
+#############################################################################
+set EXEC = ../../../test_execs/theta-nlev20/theta-nlev20
+set namelist = ./namelist-default.nl
+
+\rm -f input.nl
+cp $namelist input.nl
+mpirun -np $NCPU $EXEC < input.nl
+
+ncl plot_omega.ncl
+ncl plot_theta.ncl
+
+mv test31_omega.pdf                 hydro_test31_omega.pdf
+mv dcmip2012_test3_theta_diff.pdf   hydro_test3_theta_diff.pdf  
+mv movies/dcmip2012_test31.nc       movies/hydro_dcmip2012_test31.nc 
+
+
+
+#############################################################################
+# preqx (hydrostatic
+#############################################################################
+set EXEC = ../../../test_execs/preqx-nlev20-interp/preqx-nlev20-interp  
+set namelist = ./namelist-default.nl
+
+\rm -f input.nl
+cp $namelist input.nl
+mpirun -np $NCPU $EXEC < input.nl
+
+ncl plot_omega.ncl
+ncl plot_theta.ncl
+
+
+mv test31_omega.pdf                 preqx_test31_omega.pdf
+mv dcmip2012_test3_theta_diff.pdf   preqx_test3_theta_diff.pdf  
+mv movies/dcmip2012_test31.nc       movies/preqx_dcmip2012_test31.nc 
+
+
