2374c5654a1e019f5073ad9418dd4b9bbe5c550f
Azamat Mametjanov <azamat@mcs.anl.gov>
2017-03-22 15:51:44 -0500

    Update !CDIR COLLAPSE with !DIR$ COLLAPSE
    Also, the directive needs to be placed right before array assignment.

--- cime/externals/mct/mct/m_AttrVect.F90
+++ cime/externals/mct/mct/m_AttrVect.F90
@@ -844,15 +844,19 @@
 
   if(myZeroInts) then ! zero out INTEGER attributes
      if(List_allocated(aV%iList)) then
-!CDIR COLLAPSE
-        if(associated(aV%iAttr) .and. (nIAttr_(aV)>0)) aV%iAttr=0
+        if(associated(aV%iAttr) .and. (nIAttr_(aV)>0)) then
+!DIR$ COLLAPSE
+           aV%iAttr=0
+        endif
      endif
   endif
 
   if(myZeroReals) then ! zero out REAL attributes
      if(List_allocated(aV%rList)) then
-!CDIR COLLAPSE
-        if(associated(aV%rAttr) .and. (nRAttr_(aV)>0)) aV%rAttr=0._FP
+        if(associated(aV%rAttr) .and. (nRAttr_(aV)>0)) then
+!DIR$ COLLAPSE
+           aV%rAttr=0._FP
+        endif
      endif
   endif
 
