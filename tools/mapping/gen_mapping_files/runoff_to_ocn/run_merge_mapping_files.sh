9d004d76c0e9c86518922579bcb048407ddbaaae
Michael Levy <mike.levy.work@gmail.com>
2017-03-23 16:46:51 -0600

    Code clean-up after review
    NCL script
    ----------
    
    1) Error checking
       -- make sure all input files exist before reading any code
       -- all exits should be status_exit(1)
    
    2) Don't use NCL_ARGS_# for environment variable, use descriptive var name
    
    3) Add region_mask to global attributes
    
    Bash script
    -----------
    
    1) Clean up options (--map_out instead of --map-out-fname, etc)
    
    2) Add a main() function, so that meat of script is at top of file
    
    3) Update usage() to use cat << EOF instead of a bunch of echos
    
    4) Add line delimiters between subroutines to make it easier to read

--- tools/mapping/gen_mapping_files/runoff_to_ocn/run_merge_mapping_files.sh
+++ tools/mapping/gen_mapping_files/runoff_to_ocn/run_merge_mapping_files.sh
@@ -1,50 +1,69 @@
 #!/bin/bash
 #
-# $ ./run_merge_mapping_files --map-in-oo-fname file1_name   \
-#                             --map-in-ms-fname file2_name   \
-#                             --region-mask-fname file3_name \
-#                             --map-out-fname file4_name
+
+################################################################################
+
+main() {
+  parse_args $@
+  check_inputs
+  if [ ! -z $VERBOSE ]; then
+    print_summary
+    echo "Running ncl script..."
+    echo ""
+  fi
+
+  ncl ncl/merge_mapping_files.ncl
+}
+
+################################################################################
 
 usage() {
-  echo "$ ./run_merge_mapping_files --map-in-oo-fname file1_name   \\"
-  echo "                            --map-in-ms-fname file2_name   \\"
-  echo "                            --region-mask-fname file3_name \\"
-  echo "                            --map-out-fname file4_name     \\"
-  echo "                            [-h -v]"
-  echo ""
-  echo "  -h,--help     show this message"
-  echo "  -v,--verbose  show this message"
-  echo "  file1_name    entries into the open ocean"
-  echo "  file2_name    entries into marginal seas"
-  echo "  file3_name    marginal seas designation"
-  echo "  file4_name    output file"
+  cat << EOF
+$ ./run_merge_mapping_files --map_in_oo MAP_IN_OO_FNAME     \\
+                            --map_in_ms MAP_IN_MS_FNAME     \\
+                            --region_mask REGION_MASK_FNAME \\
+                            --map_out MAP_OUT_FNAME         \\
+                            [-h -v]
+
+  -h,--help     show this message
+  -v,--verbose  echo file names back to screen before running NCL script
+  --map_in_oo MAP_IN_OO_FNAME
+                mapping file containing map to open ocean points
+  --map_in_ms MAP_IN_MS_FNAME
+                mapping file containing map to marginal sea points
+  --region_mask REGION_MASK_FNAME
+                POP region mask file (to specify open ocean vs marginal sea)
+  --map_out MAP_OUT_FNAME
+                output file
+EOF
 }
+
+################################################################################
 
 parse_args() {
 
-  export NCL_N_ARGS=4
-  NCL_ARG_1=""
-  NCL_ARG_2=""
-  NCL_ARG_3=""
-  NCL_ARG_4=""
-  VERBOSE=""
+  # Clear pre-existing environment variables that will be set
+  export MAP_IN_OO_FNAME=""
+  export MAP_IN_MS_FNAME=""
+  export REGION_MASK_FNAME=""
+  export MAP_OUT_FNAME=""
 
   while [ $# -gt 0 ]; do
     case $1 in
-      --map-in-oo-fname )
-        export NCL_ARG_1=$2
+      --map_in_oo )
+        export MAP_IN_OO_FNAME=$2
         shift
       ;;
-      --map-in-ms-fname )
-        export NCL_ARG_2=$2
+      --map_in_ms )
+        export MAP_IN_MS_FNAME=$2
         shift
       ;;
-      --region-mask-fname )
-        export NCL_ARG_3=$2
+      --region_mask )
+        export REGION_MASK_FNAME=$2
         shift
       ;;
-      --map-out-fname )
-        export NCL_ARG_4=$2
+      --map_out )
+        export MAP_OUT_FNAME=$2
         shift
       ;;
       -v | --verbose )
@@ -62,30 +81,50 @@ parse_args() {
     esac
     shift
   done
+}
 
-  if [ -z ${NCL_ARG_1} ] || \
-     [ -z ${NCL_ARG_2} ] || \
-     [ -z ${NCL_ARG_3} ] || \
-     [ -z ${NCL_ARG_4} ]; then
-    echo "ERROR: Missing argument(s)!"
+################################################################################
+
+check_inputs() {
+
+  ERROR=""
+
+  if [ -z ${MAP_IN_OO_FNAME} ]; then
+    echo "ERROR: Missing --map_in_oo option"
+    ERROR="TRUE"
+  fi
+
+  if [ -z ${MAP_IN_MS_FNAME} ]; then
+    echo "ERROR: Missing --map_in_ms option"
+    ERROR="TRUE"
+  fi
+
+  if [ -z ${REGION_MASK_FNAME} ]; then
+    echo "ERROR: Missing --region_mask option"
+    ERROR="TRUE"
+  fi
+
+  if [ -z ${MAP_OUT_FNAME} ]; then
+    echo "ERROR: Missing --map_out option"
+    ERROR="TRUE"
+  fi
+
+  if [ ! -z ${ERROR} ]; then
     echo ""
     echo "Run $0 -h for help"
     exit 1
   fi
 }
 
+################################################################################
+
 print_summary() {
-  echo "Open ocean map: $NCL_ARG_1"
-  echo "Marginal seas map: $NCL_ARG_2"
-  echo "Region mask: $NCL_ARG_3"
-  echo "Output map: $NCL_ARG_4"
+  echo "Open ocean map: $MAP_IN_OO_FNAME"
+  echo "Marginal seas map: $MAP_IN_MS_FNAME"
+  echo "Region mask: $REGION_MASK_FNAME"
+  echo "Output map: $MAP_OUT_FNAME"
 }
 
-parse_args $@
-if [ ! -z $VERBOSE ]; then
-  print_summary
-  echo "Running ncl script..."
-  echo ""
-fi
+################################################################################
 
-ncl ncl/merge_mapping_files.ncl
+main $@
