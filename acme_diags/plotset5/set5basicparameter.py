f79612bf28273ccf770156c88690a037f228f6e6
chengzhuzhang <zhang40@llnl.gov>
2017-04-07 11:26:30 -0700

    revise doc

--- acme_diags/plotset5/set5basicparameter.py
+++ acme_diags/plotset5/set5basicparameter.py
@@ -7,7 +7,7 @@ test_name = '20160520.A_WCYCL1850.ne30'
 regrid_tool = 'esmf'
 regrid_method = 'linear'
 
-backend = 'cartopy'
+backend = 'vcs'
 
 diff_title = 'test - reference'
 diff_colormap = 'bl_to_darkred'
