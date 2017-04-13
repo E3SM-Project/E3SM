1c4563a864b2de3c1c1b1f281f9be8f928f398f1
Mariana Vertenstein <mvertens@ucar.edu>
2017-04-12 23:12:20 -0600

    fixed bugs

--- src/components/data_comps/drof/drof_comp_mod.F90
+++ src/components/data_comps/drof/drof_comp_mod.F90
@@ -255,8 +255,7 @@ subroutine drof_comp_init( EClock, cdata, x2r, r2x, NLFilename )
 
     ! check that we know how to handle the mode
 
-    if (trim(rof_mode) == 'NULL'            .or. &
-        trim(rof_mode) == 'COPYALL')
+    if (trim(rof_mode) == 'NULL' .or. trim(rof_mode) == 'COPYALL') then
         if (my_task == master_task) write(logunit,F00) 'rof mode = ',trim(rof_mode)
     else
       write(logunit,F00) ' ERROR illegal rof mode = ',trim(rof_mode)
