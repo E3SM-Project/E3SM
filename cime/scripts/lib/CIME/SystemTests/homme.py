e1fd40c7c0753cefd8ec599824a6882fb5deac7b
James Foucar <jgfouca@sandia.gov>
2017-04-14 12:37:58 -0600

    HOMME test typo fix

--- cime/scripts/lib/CIME/SystemTests/homme.py
+++ cime/scripts/lib/CIME/SystemTests/homme.py
@@ -67,7 +67,7 @@ class HOMME(SystemTestsCommon):
             stat = run_cmd("%s -j 4 check" % gmake, arg_stdout=log, combine_output=True, from_dir=exeroot)[0]
 
         else:
-            stat = run_cmd("%s -j 4 baseline" % gmake, arg_stdout=log, combine_output=True, from_dir=exeroot)
+            stat = run_cmd("%s -j 4 baseline" % gmake, arg_stdout=log, combine_output=True, from_dir=exeroot)[0]
 
         # Add homme.log output to TestStatus.log so that it can
         # appear on the dashboard. Otherwise, the TestStatus.log
