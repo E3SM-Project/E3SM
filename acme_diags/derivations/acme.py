65736846f3a302cf64da3416adc428534b60ef4c
zshaheen <zshaheen93@gmail.com>
2017-04-19 17:05:09 -0700

    finally using dicts, duplicates also work.

--- acme_diags/derivations/acme.py
+++ acme_diags/derivations/acme.py
@@ -9,43 +9,47 @@ def process_derived_var(var_key, derived_vars_dict, nc_file, parameter):
      whatever is described in derived_vars_dict[var_key] for the nc_file'''
     if hasattr(parameter, 'derived_variables'):
         _add_user_derived_vars(derived_vars_dict, parameter)
-    if var_key in derived_vars_dict.keys():
-        inputs, func = _get_correct_derivation(
-            var_key, derived_vars_dict, nc_file)
-        # get all of the variables from nc_file
-        args = [nc_file(var)(squeeze=1) for var in inputs]
-        return func(*args)
+    if var_key in derived_vars_dict:
+        return _compute_derived_var(var_key, derived_vars_dict, nc_file)
     else:
         raise RuntimeError(
             'The variable %s was not in the derived variables dictionary' % var_key)
 
 
 def _add_user_derived_vars(derived_vars_dict, parameter):
-    for key, derived_vars in parameter.derived_variables.iteritems():
-        # append the user defined vars to the already defined ones
+    ''' Append parameter.derived_variables to the correct part of derived_vars_dict '''
+    for key, user_derived_vars in parameter.derived_variables.iteritems():
+        # append the user-defined vars to the already defined ones
+        # add to an existing entry, otherwise create a new one
         if key in derived_vars_dict:
-            for v in derived_vars:
-                derived_vars_dict[key].append(v)
+            for v in user_derived_vars:
+                derived_vars_dict[key][v] = user_derived_vars[v]
         else:
-                derived_vars_dict[key] = derived_vars
+            derived_vars_dict[key] = user_derived_vars
 
 
-def _get_correct_derivation(var_key, derived_vars_dict, nc_file):
-    ''' Get the first valid derivation from the derived_vars_dict dict. '''
-    derived_var_list = derived_vars_dict[var_key]
+def _compute_derived_var(var_key, derived_vars_dict, nc_file):
+    ''' Call the first valid derivation from the derived_vars_dict dict. '''
+    derived_vars = derived_vars_dict[var_key]
     # store a list of all inputs visited, so if Exception, we get a good msg.
     derived_var_inputs = []
 
     # get the first function and inputs from the derived_vars_dict dict
-    for inputs, func in derived_var_list:
+    for inputs, func in derived_vars.iteritems():
         derived_var_inputs.append(inputs)
+        # tuples with a single string [ex: ('pr')] become just a string ['pr']
         # are all of the variables (inputs) in the nc_file?
-        if set(inputs).issubset(nc_file.variables.keys()):
-            return inputs, func
+        if isinstance(inputs, str) and inputs in nc_file.variables.keys():
+            args = [nc_file(inputs)(squeeze=1)]
+            return func(*args)
+
+        elif isinstance(inputs, tuple) and set(inputs).issubset(nc_file.variables.keys()):
+            args = [nc_file(var)(squeeze=1) for var in inputs]
+            return func(*args)
 
     # When nc_file is obs, there is var_key in nc_file, i.e. nc_file(var_key)
     if var_key in nc_file.variables.keys():
-        return [var_key], lambda x: x  # var_key needs to be in a list
+        return nc_file(var_key)(squeeze=1)
 
     raise RuntimeError('None of the variables (%s) are in the file: %s' % (
         derived_var_inputs, nc_file.id))
@@ -61,13 +65,13 @@ def aplusb(var1, var2, target_units=None):
     it tries to convert both of their units to target_units '''
 
     if target_units is not None:
-        var1 = _convert_units(var1, target_units)
-        var2 = _convert_units(var2, target_units)
+        var1 = convert_units(var1, target_units)
+        var2 = convert_units(var2, target_units)
 
     return var1 + var2
 
 
-def _convert_units(var, target_units):
+def convert_units(var, target_units):
     ''' Converts units of var to target_units.
     var is a cdms.TransientVariable. '''
 
@@ -113,11 +117,11 @@ def mask_by(input_var, maskvar, low_limit=None, high_limit=None):
     return var
 
 
-def qflx_convert_units(var):
+def qflxconvert_units(var):
     print var.units
     if var.units == 'kg/m2/s':
         # need to find a solution for units not included in udunits
-        #var = _convert_units( var, 'kg/m2/s' )
+        # var = convert_units( var, 'kg/m2/s' )
         var = var * 3600.0 * 24  # convert to mm/day
         var.units = 'mm/day'
     return var
@@ -125,10 +129,10 @@ def qflx_convert_units(var):
 def prect(precc, precl):
     """Total precipitation flux = convective + large-scal, """
     var = precc + precl
-    var = _convert_units(var, "mm/day") 
+    var = convert_units(var, "mm/day")
     var.long_name = "Total precipitation rate (convective + large-scale)"
     return var
-    
+
 def albedo(solin, fsntoa):
     """TOA (top-of-atmosphere) albedo, (solin - fsntoa) / solin, unit is nondimension"""
     var = (solin - fsntoa) / solin
@@ -192,166 +196,150 @@ def restoa(fsnt, flnt):
 # In derived_variable there is an entry for 'PRECT':
 # If 'PRECT' is not available, but both 'PRECC' and 'PRECT' are available in the netcdf variable keys,
 # PRECT is calculated using fuction prect() with precc and precl as inputs.
+
 derived_variables = {
-    'PRECT': [
-        (['pr'], rename),
-        (['PRECC', 'PRECL'], lambda precc, precl: prect(precc , precl))
-    ],
-}
-
-old_derived_variables = {
-    'PRECT': [
-        (['pr'], rename),
-        (['PRECC', 'PRECL'], lambda precc, precl: prect(precc , precl))
-    ],
-    'SST': [
-        (['SST'], lambda sst: _convert_units(sst, target_units="degC")),
-        (['TS', 'OCNFRAC'], lambda ts, ocnfrac: mask_by(
-            _convert_units(ts, target_units="degC"), ocnfrac, low_limit=0.9))
-    ],
-    'PREH2O': [
-        (['TMQ'], rename)
-    ],
-    'ALBEDO': [
-        (['ALBEDO'], rename),
-        (['SOLIN', 'FSNTOA'], lambda solin, fsntoa: albedo(solin, fsntoa))
-    ],
-    'ALBEDOC': [
-        (['ALBEDOC'], rename),
-        (['SOLIN', 'FSNTOAC'], lambda solin, fsntoac: albedoc(solin, fsntoac))
-    ],
-    'SWCF': [
-        (['SWCF'], rename),
-        (['FSNTOA', 'FSNTOAC'], lambda fsntoa, fsntoac: swcf(fsntoa, fsntoac))
-    ],
-    'SWCFSRF': [
-        (['SWCFSRF'], rename),
-        (['FSNS', 'FSNSC'], lambda fsns, fsnsc: swcfsrf(fsns, fsnsc))
-    ],
-    'LWCF': [
-        (['LWCF'], rename),
-        (['FLNTOA', 'FLNTOAC'], lambda flntoa, flntoac: lwcf(flntoa, flntoac))
-    ],
-    'LWCFSRF': [
-        (['LWCFSRF'], rename),
-        (['FLNSC', 'FLNS'], lambda flns, flnsc: lwcfsrf(flnsc, flns))
-    ],
-    'FLNS': [
-        (['FLNS'], rename)
-    ],
-    'FLNSC': [
-        (['FLNSC'], rename)
-    ],
-    'FLDS': [
-        (['FLDS'], rename)
-    ],
-    'FLDSC': [
-        (['FLDSC'], rename),
-        (['TS', 'FLNSC'], lambda ts, flnsc: fldsc(ts, flnsc))
-    ],
-    'FSNS': [
-        (['FSNS'], rename)
-    ],
-    'FSNSC': [
-        (['FSNSC'], rename)
-    ],
-    'FSDS': [
-        (['FSDS'], rename)
-    ],
-    'FSDSC': [
-        (['FSDSC'], rename)
-    ],
-    'FLUT': [
-        (['FLUT'], rename)
-    ],
-    'FLUTC': [
-        (['FLUTC'], rename)
-    ],
-    'FSNTOA': [
-        (['FSNTOA'], rename)
-    ],
-    
-    'FSNTOAC': [
-        (['FSNTOAC'], rename) #Note: CERES_EBAF data in amwg obs sets mis-spells "units" as "lunits"
-    ],
-    'RESTOM': [
-        (['RESTOA'], rename),
-        (['FSNT', 'FLNT'], lambda fsnt, flnt: restom(fsnt,flnt))
-    ],
-    'RESTOA': [
-        (['RESTOA'], rename),
-        (['FSNT', 'FLNT'], lambda fsnt, flnt: restoa(fsnt,flnt))
-    ],
-    'TREFHT_LAND': [
-        (['TREFHT', 'LANDFRAC'], lambda trefht, landfrac: mask_by(
-            _convert_units(trefht, target_units="K"), landfrac, low_limit=0.65)),
-        (['TREFHT_LAND'], rename)
-    ],
-    'PRECT_LAND': [
-        (['PRECC', 'PRECL', 'LANDFRAC'], lambda precc, precl, landfrac: mask_by(
-           prect(precc , precl), landfrac, low_limit=0.5)),  # 0.5 just to match amwg
-        (['PRECIP_LAND'], rename)
-    ],
-    'Z3': [
-        (['Z3'], lambda z3: _convert_units(z3, target_units="hectometer"))
-    ],
-    'PSL': [
-        (['PSL'], lambda psl: _convert_units(psl, target_units="mbar"))
-    ],
-    'T': [
-        (['T'], lambda t: _convert_units(t, target_units="K"))
-    ],
-    'U': [
-        (['U'], lambda u: _convert_units(u, target_units="m/s"))
-    ],
-    'TREFHT': [
-        (['TREFHT'], lambda t: _convert_units(t, target_units="K"))
-    ],
-    'TREFHT': [
-        (['TREFHT'], lambda t: _convert_units(t, target_units="K"))
-    ],
-    'QFLX': [
-        (['QFLX'], lambda qflx: qflx_convert_units(qflx))
-    ],
-
-    'LHFLX': [
-        (['LHFLX'], rename)
-    ],
-    'SHFLX': [
-        (['SHFLX'], rename)
-    ],
-    'TGCLDLWP_OCN': [(['TGCLDLWP_OCEAN'], (lambda x: _convert_units(x, target_units='g/m^2'))),
-                     (['TGCLDLWP', 'OCNFRAC'], lambda tgcldlwp, ocnfrac: mask_by(_convert_units(tgcldlwp, target_units="g/m^2"), ocnfrac, low_limit=0.65))],
-    'PRECT_OCN': [(['PRECT_OCEAN'], (lambda x: _convert_units(x, target_units='mm/day'))),
-                  (['PRECC', 'PRECL', 'OCNFRAC'], lambda a, b, ocnfrac: mask_by(aplusb(a, b, target_units="mm/day"), ocnfrac, low_limit=0.65))],
-    'PREH2O_OCN': [(['PREH2O_OCEAN'], (lambda x: _convert_units(x, target_units='mm'))),
-                   (['TMQ', 'OCNFRAC'], lambda preh2o, ocnfrac: mask_by(preh2o, ocnfrac, low_limit=0.65))],
-    'CLDHGH': [
-        (['CLDHGH'], lambda cldhgh: _convert_units(cldhgh, target_units="%")),
-    ],
-    'CLDLOW': [
-        (['CLDLOW'], lambda cldlow: _convert_units(cldlow, target_units="%")),
-    ],
-    'CLDMED': [
-        (['CLDMED'], lambda cldmed: _convert_units(cldmed, target_units="%")),
-    ],
-    'CLDTOT': [
-        (['CLDTOT'], lambda cldtot: _convert_units(cldtot, target_units="%")),
-    ],
-#    'CLDHGH_VISIR': [
-#        (['CLDHGH'], rename),
-#        (['CLDHGH_VISIR'], rename),
-#    ],
-#    'CLDLOW_VISIR': [
-#        (['CLDLOW'], rename),
-#        (['CLDLOW_VISIR'], rename),
-#    ],
-#    'CLDMED_VISIR': [
-#        (['CLDMED'], rename),
-#        (['CLDMED_VISIR'], rename),
-#    ],
-#    'CLDTOT_VISIR': [
-#        (['CLDTOT'], rename),
-#        (['CLDTOT_VISIR'], rename),
-#    ],
+    'PRECT': {
+        ('pr'): rename,
+        ('PRECC', 'PRECL'): lambda precc, precl: prect(precc, precl)
+    },
+    'SST': {
+        ('SST'): lambda sst: convert_units(sst, target_units="degC"),
+        ('TS', 'OCNFRAC'): lambda ts, ocnfrac: mask_by(
+            convert_units(ts, target_units="degC"), ocnfrac, low_limit=0.9)
+    },
+    'PREH2O': {
+        ('TMQ'): rename
+    },
+    'ALBEDO': {
+        ('ALBEDO'): rename,
+        ('SOLIN', 'FSNTOA'): lambda solin, fsntoa: albedo(solin, fsntoa)
+    },
+    'ALBEDOC': {
+        ('ALBEDOC'): rename,
+        ('SOLIN', 'FSNTOAC'): lambda solin, fsntoac: albedoc(solin, fsntoac)
+    },
+    'SWCF': {
+        ('SWCF'): rename,
+        ('FSNTOA', 'FSNTOAC'): lambda fsntoa, fsntoac: swcf(fsntoa, fsntoac)
+    },
+    'SWCFSRF': {
+        ('SWCFSRF'): rename,
+        ('FSNS', 'FSNSC'): lambda fsns, fsnsc: swcfsrf(fsns, fsnsc)
+    },
+    'LWCF': {
+        ('LWCF'): rename,
+        ('FLNTOA', 'FLNTOAC'): lambda flntoa, flntoac: lwcf(flntoa, flntoac)
+    },
+    'LWCFSRF': {
+        ('LWCFSRF'): rename,
+        ('FLNSC', 'FLNS'): lambda flns, flnsc: lwcfsrf(flnsc, flns)
+    },
+    'FLNS': {
+        ('FLNS'): rename
+    },
+    'FLNSC': {
+        ('FLNSC'): rename
+    },
+    'FLDS': {
+        ('FLDS'): rename
+    },
+    'FLDSC': {
+        ('FLDSC'): rename,
+        ('TS', 'FLNSC'): lambda ts, flnsc: fldsc(ts, flnsc)
+    },
+    'FSNS': {
+        ('FSNS'): rename
+    },
+    'FSNSC': {
+        ('FSNSC'): rename
+    },
+    'FSDS': {
+        ('FSDS'): rename
+    },
+    'FSDSC': {
+        ('FSDSC'): rename
+    },
+    'FLUT': {
+        ('FLUT'): rename
+    },
+    'FLUTC': {
+        ('FLUTC'): rename
+    },
+    'FSNTOA': {
+        ('FSNTOA'): rename
+    },
+    'FSNTOAC': {
+        # Note: CERES_EBAF data in amwg obs sets misspells "units" as "lunits"
+        ('FSNTOAC'): rename
+    },
+    'RESTOM': {
+        ('RESTOA'): rename,
+        ('FSNT', 'FLNT'): lambda fsnt, flnt: restom(fsnt,flnt)
+    },
+    'RESTOA': {
+        ('RESTOA'): rename,
+        ('FSNT', 'FLNT'): lambda fsnt, flnt: restoa(fsnt,flnt)
+    },
+    'TREFHT_LAND': {
+        ('TREFHT_LAND'): rename,
+        ('TREFHT', 'LANDFRAC'): lambda trefht, landfrac: mask_by(
+            convert_units(trefht, target_units="K"), landfrac, low_limit=0.65)
+    },
+    'PRECT_LAND': {
+        ('PRECIP_LAND'): rename,
+        # 0.5 just to match amwg
+        ('PRECC', 'PRECL', 'LANDFRAC'): lambda precc, precl, landfrac: mask_by(
+           prect(precc , precl), landfrac, low_limit=0.5)
+    },
+    'Z3': {
+        ('Z3'): lambda z3: convert_units(z3, target_units="hectometer")
+    },
+    'PSL': {
+        ('PSL'): lambda psl: convert_units(psl, target_units="mbar")
+    },
+    'T': {
+        ('T'): lambda t: convert_units(t, target_units="K")
+    },
+    'U': {
+        ('U'): lambda u: convert_units(u, target_units="m/s")
+    },
+    'TREFHT': {
+        ('TREFHT'): lambda t: convert_units(t, target_units="K")
+    },
+    'TREFHT': {
+        ('TREFHT'): lambda t: convert_units(t, target_units="K")
+    },
+    'QFLX': {
+        ('QFLX'): lambda qflx: qflxconvert_units(qflx)
+    },
+    'LHFLX': {
+        ('LHFLX'): rename
+    },
+    'SHFLX': {
+        ('SHFLX'): rename
+    },
+    'TGCLDLWP_OCN': {
+        ('TGCLDLWP_OCEAN'): lambda x: convert_units(x, target_units='g/m^2'),
+        ('TGCLDLWP', 'OCNFRAC'): lambda tgcldlwp, ocnfrac: mask_by(convert_units(tgcldlwp, target_units="g/m^2"), ocnfrac, low_limit=0.65)
+    },
+    'PRECT_OCN': {
+        ('PRECT_OCEAN'): lambda x: convert_units(x, target_units='mm/day'),
+        ('PRECC', 'PRECL', 'OCNFRAC'): lambda a, b, ocnfrac: mask_by(aplusb(a, b, target_units="mm/day"), ocnfrac, low_limit=0.65)
+    },
+    'PREH2O_OCN': {
+        ('PREH2O_OCEAN'): lambda x: convert_units(x, target_units='mm'),
+        ('TMQ', 'OCNFRAC'): lambda preh2o, ocnfrac: mask_by(preh2o, ocnfrac, low_limit=0.65)
+    },
+    'CLDHGH': {
+        ('CLDHGH'): lambda cldhgh: convert_units(cldhgh, target_units="%")
+    },
+    'CLDLOW': {
+        ('CLDLOW'), lambda cldlow: convert_units(cldlow, target_units="%")
+    },
+    'CLDMED': {
+        ('CLDMED'), lambda cldmed: convert_units(cldmed, target_units="%")
+    },
+    'CLDTOT': {
+        ('CLDTOT'), lambda cldtot: convert_units(cldtot, target_units="%")
+    }
 }
