5b5a2c5664e200a319ca32432d451bcc398a1850
Erich L Foster <elfost@sandia.gov>
2017-04-20 15:30:29 -0600

    Add Support of true and false in Namelist
    The Namelist.pm only allowed for .false. or .true. in namelist files. I have
    fixed the regex to allow for dots or no dots.
    
    Fixes #1283

--- utils/perl5lib/Build/Namelist.pm
+++ utils/perl5lib/Build/Namelist.pm
@@ -627,8 +627,8 @@ my $valint = "[+-]?[0-9]+";
 my $valint_repeat = "${valint}\\*$valint";
 
 # Logical data.
-my $vallogical1 = "\\.[Tt][Rr][Uu][Ee]\\.";
-my $vallogical2 = "\\.[Ff][Aa][Ll][Ss][Ee]\\.";
+my $vallogical1 = "\\.?[Tt][Rr][Uu][Ee]\\.?";
+my $vallogical2 = "\\.?[Ff][Aa][Ll][Ss][Ee]\\.?";
 my $vallogical = "$vallogical1|$vallogical2";
 my $vallogical_repeat = "${valint}\\*$vallogical1|${valint}\\*$vallogical2";
 
