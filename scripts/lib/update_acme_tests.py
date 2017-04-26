a5cfd299eb628ac406bc5ef3baaece6b13f0adc7
James Foucar <jgfouca@sandia.gov>
2017-04-26 13:03:28 -0600

    Change PET test to fit on desktops

--- scripts/lib/update_acme_tests.py
+++ scripts/lib/update_acme_tests.py
@@ -48,7 +48,7 @@ _TEST_SUITES = {
                              "ERP.f45_g37_rx1.A",
                              "SMS_D_Ln9.f19_g16_rx1.A",
                              "DAE.f19_f19.A",
-                             "PET.f19_f19.A",
+                             "PET_P32.f19_f19.A",
                              "SMS.T42_T42.S",
                              "PRE.f45_g37_rx1.ADESP")
                             ),
