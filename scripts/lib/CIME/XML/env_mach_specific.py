229bff71998555854f24cd2219d1d7c5260002af
Peter Andrew Bogenschutz <bogensch@cab688.llnl.gov>
2017-05-04 16:09:06 -0700

    change soft to generic for PR approval

--- scripts/lib/CIME/XML/env_mach_specific.py
+++ scripts/lib/CIME/XML/env_mach_specific.py
@@ -87,7 +87,7 @@ class EnvMachSpecific(EnvBase):
         if (module_system == "module"):
             self._load_module_modules(modules_to_load)
         elif (module_system == "soft"):
-            self._load_soft_modules(modules_to_load)
+            self._load_modules_generic(modules_to_load)
         elif (module_system == "generic"):
             self._load_modules_generic(modules_to_load)
         elif (module_system == "none"):
@@ -220,66 +220,6 @@ class EnvMachSpecific(EnvBase):
             expect(stat==0 and len(errout) == 0,
                    "module command %s failed with message:\n%s"%(cmd,errout))
             exec(py_module_code)
-
-    def _load_soft_modules(self, modules_to_load):
-        sh_init_cmd = self.get_module_system_init_path("sh")
-        sh_mod_cmd = self.get_module_system_cmd_path("sh")
-
-        # Some machines can set the environment
-        # variables using a script (such as /etc/profile.d/00softenv.sh
-        # on mira or /etc/profile.d/a_softenv.sh on blues)
-        # which load the new environment variables using softenv-load.
-
-        # Other machines need to run soft-dec.sh and evaluate the output,
-        # which may or may not have unresolved variables such as
-        # PATH=/soft/com/packages/intel/16/initial/bin:${PATH}
-
-        cmd = "source %s" % sh_init_cmd
-
-        if os.environ.has_key("SOFTENV_ALIASES"):
-            cmd += " && source $SOFTENV_ALIASES"
-        if os.environ.has_key("SOFTENV_LOAD"):
-            cmd += " && source $SOFTENV_LOAD"
-
-        for action,argument in modules_to_load:
-            cmd += " && %s %s %s" % (sh_mod_cmd, action, argument)
-
-        cmd += " && env"
-        output = run_cmd_no_fail(cmd)
-
-        ###################################################
-        # Parse the output to set the os.environ dictionary
-        ###################################################
-        newenv = {}
-        dolater = []
-        for line in output.splitlines():
-            if line.find('$')>0:
-                dolater.append(line)
-                continue
-
-            m=re.match(r'^(\S+)=(\S+)\s*;*\s*$',line)
-            if m:
-                key = m.groups()[0]
-                val = m.groups()[1]
-                newenv[key] = val
-
-        # Now that initial newenv has been set, resolve variables
-        for line in dolater:
-            m=re.match(r'^(\S+)=(\S+)\s*;*\s*$',line)
-            if m:
-                key = m.groups()[0]
-                valunresolved = m.groups()[1]
-                val = string.Template(valunresolved).safe_substitute(newenv)
-                expect(val is not None,
-                       'string value %s unable to be resolved' % valunresolved)
-                newenv[key] = val
-
-        # Set environment with new or updated values
-        for key in newenv:
-            if key in os.environ and key not in newenv:
-                del(os.environ[key])
-            else:
-                os.environ[key] = newenv[key]
 
     def _load_modules_generic(self, modules_to_load):
         sh_init_cmd = self.get_module_system_init_path("sh")
