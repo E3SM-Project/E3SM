75366d3232b7bbc08fef9d8077dcee8dee49d386
chengzhuzhang <zhang40@llnl.gov>
2017-05-08 18:12:47 -0700

    set3 plotting stuff

--- acme_diags/driver/set3_driver.py
+++ acme_diags/driver/set3_driver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import cdms2
 import MV2
+import cdutil
 from acme_diags.plot import plot
 from acme_diags.derivations import acme
 from acme_diags.metrics import rmse, corr, min_cdms, max_cdms, mean
@@ -147,27 +148,28 @@ def run_diag(parameter):
     
                     for region in regions:
                         print "selected region", region
+                         
 
-                        mv1_domain, mv2_domain = utils.select_region(region, mv1, mv2, land_frac,ocean_frac,parameter)
-    
-                        parameter.output_file = '-'.join(
-                            [ref_name, var, str(int(plev[ilev])), season, region])
-                        parameter.main_title = str(
-                            ' '.join([var, str(int(plev[ilev])), 'mb', season, region]))
-    
-                        # Regrid towards lower resolution of two variables for
-                        # calculating difference
-                        mv1_reg, mv2_reg = utils.regrid_to_lower_res(
-                            mv1_domain, mv2_domain, parameter.regrid_tool, parameter.regrid_method)
-    
-                        # Plotting
-                        diff = mv1_reg - mv2_reg
-                        metrics_dict = create_metrics(
-                            mv2_domain, mv1_domain, mv2_reg, mv1_reg, diff)
-
-                        parameter.var_region = region
-                        plot('5', mv2_domain, mv1_domain, diff, metrics_dict, parameter)
-                        utils.save_ncfiles('5', mv1_domain, mv2_domain, diff, parameter)
+#                        mv1_domain, mv2_domain = utils.select_region(region, mv1, mv2, land_frac,ocean_frac,parameter)
+#    
+#                        parameter.output_file = '-'.join(
+#                            [ref_name, var, str(int(plev[ilev])), season, region])
+#                        parameter.main_title = str(
+#                            ' '.join([var, str(int(plev[ilev])), 'mb', season, region]))
+#    
+#                        # Regrid towards lower resolution of two variables for
+#                        # calculating difference
+#                        mv1_reg, mv2_reg = utils.regrid_to_lower_res(
+#                            mv1_domain, mv2_domain, parameter.regrid_tool, parameter.regrid_method)
+#    
+#                        # Plotting
+#                        diff = mv1_reg - mv2_reg
+#                        metrics_dict = create_metrics(
+#                            mv2_domain, mv1_domain, mv2_reg, mv1_reg, diff)
+#
+#                        parameter.var_region = region
+#                        plot('5', mv2_domain, mv1_domain, diff, metrics_dict, parameter)
+#                        utils.save_ncfiles('5', mv1_domain, mv2_domain, diff, parameter)
 
                 f_in.close()
 
@@ -180,39 +182,48 @@ def run_diag(parameter):
 
                 for region in regions:
                     print "selected region", region
+                    mv1_zonal=cdutil.averager(mv1,axis='x')
+                    mv2_zonal=cdutil.averager(mv2,axis='x')
+                    print mv1_zonal.shape,mv2_zonal.shape
+                    import vcs
+                    x = vcs.init()
+                    line = x.createyxvsx()
+                    x.plot(mv1_zonal,line)
+                    x.plot(mv2_zonal,line)
+                    
 
-                    mv1_domain, mv2_domain = utils.select_region(region, mv1, mv2, land_frac,ocean_frac,parameter)
-    
-                    parameter.output_file = '-'.join(
-                        [ref_name, var, season, region])
-                    parameter.main_title = str(' '.join([var, season, region]))
-    
-                    # regrid towards lower resolution of two variables for
-                    # calculating difference
-                    mv1_reg, mv2_reg = utils.regrid_to_lower_res(
-                        mv1_domain, mv2_domain, parameter.regrid_tool, parameter.regrid_method)
-    
-                    # if var is 'SST' or var is 'TREFHT_LAND': #special case
-    
-                    if var == 'TREFHT_LAND'or var == 'SST':  # use "==" instead of "is"
-                        if ref_name == 'WILLMOTT':
-                            mv2_reg = MV2.masked_where(
-                                mv2_reg == mv2_reg.fill_value, mv2_reg)
-                            print ref_name
-    
-                            # if mv.mask is False:
-                            #    mv = MV2.masked_less_equal(mv, mv._FillValue)
-                            #    print "*************",mv.count()
-                        land_mask = MV2.logical_or(mv1_reg.mask, mv2_reg.mask)
-                        mv1_reg = MV2.masked_where(land_mask, mv1_reg)
-                        mv2_reg = MV2.masked_where(land_mask, mv2_reg)
-    
-                    diff = mv1_reg - mv2_reg
-                    metrics_dict = create_metrics(
-                        mv2_domain, mv1_domain, mv2_reg, mv1_reg, diff)
-                    parameter.var_region = region
-                    plot('5', mv2_domain, mv1_domain, diff, metrics_dict, parameter)
-                    utils.save_ncfiles('5', mv1_domain, mv2_domain, diff, parameter)
+#                    mv1_domain, mv2_domain = utils.select_region(region, mv1, mv2, land_frac,ocean_frac,parameter)
+#    
+#                    parameter.output_file = '-'.join(
+#                        [ref_name, var, season, region])
+#                    parameter.main_title = str(' '.join([var, season, region]))
+#    
+#                    # regrid towards lower resolution of two variables for
+#                    # calculating difference
+#                    mv1_reg, mv2_reg = utils.regrid_to_lower_res(
+#                        mv1_domain, mv2_domain, parameter.regrid_tool, parameter.regrid_method)
+#    
+#                    # if var is 'SST' or var is 'TREFHT_LAND': #special case
+#    
+#                    if var == 'TREFHT_LAND'or var == 'SST':  # use "==" instead of "is"
+#                        if ref_name == 'WILLMOTT':
+#                            mv2_reg = MV2.masked_where(
+#                                mv2_reg == mv2_reg.fill_value, mv2_reg)
+#                            print ref_name
+#    
+#                            # if mv.mask is False:
+#                            #    mv = MV2.masked_less_equal(mv, mv._FillValue)
+#                            #    print "*************",mv.count()
+#                        land_mask = MV2.logical_or(mv1_reg.mask, mv2_reg.mask)
+#                        mv1_reg = MV2.masked_where(land_mask, mv1_reg)
+#                        mv2_reg = MV2.masked_where(land_mask, mv2_reg)
+#    
+#                    diff = mv1_reg - mv2_reg
+#                    metrics_dict = create_metrics(
+#                        mv2_domain, mv1_domain, mv2_reg, mv1_reg, diff)
+#                    parameter.var_region = region
+#                    plot('5', mv2_domain, mv1_domain, diff, metrics_dict, parameter)
+#                    utils.save_ncfiles('5', mv1_domain, mv2_domain, diff, parameter)
     
             
             else:
