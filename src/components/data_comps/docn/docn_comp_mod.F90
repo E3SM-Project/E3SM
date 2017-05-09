008752abb3552294bce459a63f6394c3cfa3b4a5
Mariana Vertenstein <mvertens@ucar.edu>
2017-05-08 23:08:02 -0600

    additional fixes for aquaplanet som

--- src/components/data_comps/docn/docn_comp_mod.F90
+++ src/components/data_comps/docn/docn_comp_mod.F90
@@ -704,7 +704,7 @@ subroutine docn_comp_run( EClock, cdata,  x2o, o2x)
          o2x%rAttr(kswp ,n) = swp
       enddo
 
-   case('SOM','SOM_AQUAP')
+   case('SOM')
       lsize = mct_avect_lsize(o2x)
       do n = 1,SDOCN%nstreams
          call shr_dmodel_translateAV(SDOCN%avs(n),avstrm,avifld,avofld,rearr)
@@ -737,6 +737,43 @@ subroutine docn_comp_run( EClock, cdata,  x2o, o2x)
              !--- compute ice formed or melt potential ---
             o2x%rAttr(kq,n) = (tfreeze(n) - o2x%rAttr(kt,n))*(cpsw*rhosw*hn)/dt  ! ice formed q>0
             o2x%rAttr(kt,n) = max(tfreeze(n),o2x%rAttr(kt,n))                    ! reset temp
+            somtp(n) = o2x%rAttr(kt,n)                                        ! save temp
+         endif
+         enddo
+      endif   ! firstcall
+
+   case('SOM_AQUAP')
+      lsize = mct_avect_lsize(o2x)
+      do n = 1,SDOCN%nstreams
+         call shr_dmodel_translateAV(SDOCN%avs(n),avstrm,avifld,avofld,rearr)
+      enddo
+      if (firstcall) then
+         do n = 1,lsize
+            if (.not. read_restart) then
+               somtp(n) = o2x%rAttr(kt,n) + TkFrz
+            endif
+            o2x%rAttr(kt,n) = somtp(n)
+            o2x%rAttr(kq,n) = 0.0_R8
+         enddo
+      else   ! firstcall
+         tfreeze = shr_frz_freezetemp(o2x%rAttr(ks,:)) + TkFrz
+         do n = 1,lsize
+         if (imask(n) /= 0) then
+            !--- pull out h from av for resuse below ---
+            hn = avstrm%rAttr(kh,n)
+            !--- compute new temp ---
+            o2x%rAttr(kt,n) = somtp(n) + &
+               (x2o%rAttr(kswnet,n) + &  ! shortwave 
+                x2o%rAttr(klwup ,n) + &  ! longwave
+                x2o%rAttr(klwdn ,n) + &  ! longwave
+                x2o%rAttr(ksen  ,n) + &  ! sensible
+                x2o%rAttr(klat  ,n) + &  ! latent
+                x2o%rAttr(kmelth,n) - &  ! ice melt
+                avstrm%rAttr(kqbot ,n) - &  ! flux at bottom
+                (x2o%rAttr(ksnow,n)+x2o%rAttr(krofi,n))*latice) * &  ! latent by prec and roff
+                dt/(cpsw*rhosw*hn)
+             !--- compute ice formed or melt potential ---
+            o2x%rAttr(kq,n) = (tfreeze(n) - o2x%rAttr(kt,n))*(cpsw*rhosw*hn)/dt  ! ice formed q>0
             somtp(n) = o2x%rAttr(kt,n)                                        ! save temp
          endif
          enddo
