77e99cb8d752dc42ef34a58782b2b4b74b9d2756
Jim Edwards <jedwards@ucar.edu>
2017-05-11 08:26:22 -0600

    make sure var is valid for set_value

--- scripts/lib/CIME/case_submit.py
+++ scripts/lib/CIME/case_submit.py
@@ -42,7 +42,8 @@ def _submit(case, job=None, resubmit=False, no_batch=False, skip_pnl=False, batc
     # flag will stay in effect for the duration of the RESUBMITs
     env_batch = case.get_env("batch")
     if not resubmit:
-        case.set_value("IS_FIRST_RUN", True)
+        if case.get_value("TEST"):
+            case.set_value("IS_FIRST_RUN", True)
         if no_batch:
             batch_system = "none"
         else:
@@ -106,4 +107,3 @@ def check_DA_settings(case):
         script = case.get_value("DATA_ASSIMILATION_SCRIPT")
         cycles = case.get_value("DATA_ASSIMILATION_CYCLES")
         logger.info("Data Assimilation enabled using script {} with {:d} cycles".format(script,cycles))
-
