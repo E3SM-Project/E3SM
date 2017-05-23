76c4cac6e5eb6e0429651845dd20ae6bfda04206
James Foucar <jgfouca@sandia.gov>
2017-05-23 13:19:31 -0600

    jgfouca mods to this branch

--- scripts/lib/CIME/BuildTools/configure.py
+++ scripts/lib/CIME/BuildTools/configure.py
@@ -66,6 +66,14 @@ def _copy_depends_files(machine_name, machines_dir, output_dir, compiler):
             if os.path.isfile(dfile) and not os.path.isfile(outputdfile):
                 shutil.copyfile(dfile, outputdfile)
 
+class FakeCase(object):
+
+    def __init__(self, compiler, mpilib, debug):
+        self._vals = {"COMPILER":compiler, "MPILIB":mpilib, "DEBUG":debug}
+
+    def get_value(self, attrib):
+        expect(attrib in self._vals, "FakeCase does not support getting value of '%s'" % attrib)
+        return self._vals[attrib]
 
 def _generate_env_mach_specific(output_dir, machobj, compiler, mpilib, debug,
                                 sysos, unit_testing):
@@ -79,8 +87,9 @@ def _generate_env_mach_specific(output_dir, machobj, compiler, mpilib, debug,
     ems_file = EnvMachSpecific(output_dir, unit_testing=unit_testing)
     ems_file.populate(machobj)
     ems_file.write()
+    fake_case = FakeCase(compiler, mpilib, debug)
     for shell in ('sh', 'csh'):
-        ems_file.make_env_mach_specific_file(compiler, debug, mpilib, shell)
+        ems_file.make_env_mach_specific_file(shell, fake_case)
         shell_path = os.path.join(output_dir, ".env_mach_specific." + shell)
         with open(shell_path, 'a') as shell_file:
             if shell == 'sh':
