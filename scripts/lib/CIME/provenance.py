f725c8e28839dda97522d97b88e41baf1a019420
James Foucar <jgfouca@sandia.gov>
2017-05-28 14:53:42 -0600

    In ACME provenance code, handle failure to retrieve job_id
    Return None instead of raising an exception that kills the entire
    job.
    
    [BFB]

--- scripts/lib/CIME/provenance.py
+++ scripts/lib/CIME/provenance.py
@@ -16,14 +16,17 @@ def _get_batch_job_id_for_syslog(case):
     mach_syslog only works on certain machines
     """
     mach = case.get_value("MACH")
-    if mach in ['anvil', 'titan']:
-        return os.environ["PBS_JOBID"]
-    elif mach in ['edison', 'cori-haswell', 'cori-knl']:
-        return os.environ["SLURM_JOB_ID"]
-    elif mach == 'mira':
-        return os.environ["COBALT_JOBID"]
-    else:
-        return None
+    try:
+        if mach in ['anvil', 'titan']:
+            return os.environ["PBS_JOBID"]
+        elif mach in ['edison', 'cori-haswell', 'cori-knl']:
+            return os.environ["SLURM_JOB_ID"]
+        elif mach == 'mira':
+            return os.environ["COBALT_JOBID"]
+    except:
+        pass
+
+    return None
 
 def _save_build_provenance_acme(case, lid):
     cimeroot = case.get_value("CIMEROOT")
@@ -111,39 +114,40 @@ def _save_prerun_timing_acme(case, lid):
 
     # For some batch machines save queue info
     job_id = _get_batch_job_id_for_syslog(case)
-    if mach == "mira":
-        for cmd, filename in [("qstat -f", "qstatf"), ("qstat -lf %s" % job_id, "qstatf_jobid")]:
-            filename = "%s.%s" % (filename, lid)
-            run_cmd_no_fail(cmd, arg_stdout=filename, from_dir=full_timing_dir)
-            gzip_existing_file(os.path.join(full_timing_dir, filename))
-    elif mach in ["edison", "cori-haswell", "cori-knl"]:
-        for cmd, filename in [("sinfo -a -l", "sinfol"), ("sqs -f %s" % job_id, "sqsf_jobid"),
-                              # ("sqs -f", "sqsf"),
-                              ("squeue -o '%.10i %.15P %.20j %.10u %.7a %.2t %.6D %.8C %.10M %.10l %.20S %.20V'", "squeuef"),
-                              ("squeue -t R -o '%.10i %R'", "squeues")]:
-            filename = "%s.%s" % (filename, lid)
-            run_cmd_no_fail(cmd, arg_stdout=filename, from_dir=full_timing_dir)
-            gzip_existing_file(os.path.join(full_timing_dir, filename))
-    elif mach == "titan":
-        for cmd, filename in [("qstat -f %s >" % job_id, "qstatf_jobid"),
-                              ("xtnodestat >", "xtnodestat"),
-                              # ("qstat -f >", "qstatf"),
-                              # ("xtdb2proc -f", "xtdb2proc"),
-                              ("showq >", "showq")]:
-            full_cmd = cmd + " " + filename
-            run_cmd_no_fail(full_cmd + "." + lid, from_dir=full_timing_dir)
-            gzip_existing_file(os.path.join(full_timing_dir, filename + "." + lid))
+    if job_id is not None:
+        if mach == "mira":
+            for cmd, filename in [("qstat -f", "qstatf"), ("qstat -lf %s" % job_id, "qstatf_jobid")]:
+                filename = "%s.%s" % (filename, lid)
+                run_cmd_no_fail(cmd, arg_stdout=filename, from_dir=full_timing_dir)
+                gzip_existing_file(os.path.join(full_timing_dir, filename))
+        elif mach in ["edison", "cori-haswell", "cori-knl"]:
+            for cmd, filename in [("sinfo -a -l", "sinfol"), ("sqs -f %s" % job_id, "sqsf_jobid"),
+                                  # ("sqs -f", "sqsf"),
+                                  ("squeue -o '%.10i %.15P %.20j %.10u %.7a %.2t %.6D %.8C %.10M %.10l %.20S %.20V'", "squeuef"),
+                                  ("squeue -t R -o '%.10i %R'", "squeues")]:
+                filename = "%s.%s" % (filename, lid)
+                run_cmd_no_fail(cmd, arg_stdout=filename, from_dir=full_timing_dir)
+                gzip_existing_file(os.path.join(full_timing_dir, filename))
+        elif mach == "titan":
+            for cmd, filename in [("qstat -f %s >" % job_id, "qstatf_jobid"),
+                                  ("xtnodestat >", "xtnodestat"),
+                                  # ("qstat -f >", "qstatf"),
+                                  # ("xtdb2proc -f", "xtdb2proc"),
+                                  ("showq >", "showq")]:
+                full_cmd = cmd + " " + filename
+                run_cmd_no_fail(full_cmd + "." + lid, from_dir=full_timing_dir)
+                gzip_existing_file(os.path.join(full_timing_dir, filename + "." + lid))
 
-        # mdiag_reduce = os.path.join(full_timing_dir, "mdiag_reduce." + lid)
-        # run_cmd_no_fail("./mdiag_reduce.csh", arg_stdout=mdiag_reduce, from_dir=os.path.join(caseroot, "Tools"))
-        # gzip_existing_file(mdiag_reduce)
-    elif mach == "anvil":
-        for cmd, filename in [("qstat -f -1 acme >", "qstatf"),
-                              ("qstat -f %s >" % job_id, "qstatf_jobid"),
-                              ("qstat -r acme >", "qstatr")]:
-            full_cmd = cmd + " " + filename
-            run_cmd_no_fail(full_cmd + "." + lid, from_dir=full_timing_dir)
-            gzip_existing_file(os.path.join(full_timing_dir, filename + "." + lid))
+            # mdiag_reduce = os.path.join(full_timing_dir, "mdiag_reduce." + lid)
+            # run_cmd_no_fail("./mdiag_reduce.csh", arg_stdout=mdiag_reduce, from_dir=os.path.join(caseroot, "Tools"))
+            # gzip_existing_file(mdiag_reduce)
+        elif mach == "anvil":
+            for cmd, filename in [("qstat -f -1 acme >", "qstatf"),
+                                  ("qstat -f %s >" % job_id, "qstatf_jobid"),
+                                  ("qstat -r acme >", "qstatr")]:
+                full_cmd = cmd + " " + filename
+                run_cmd_no_fail(full_cmd + "." + lid, from_dir=full_timing_dir)
+                gzip_existing_file(os.path.join(full_timing_dir, filename + "." + lid))
 
     # copy/tar SourceModes
     source_mods_dir = os.path.join(caseroot, "SourceMods")
@@ -279,15 +283,16 @@ def _save_postrun_timing_acme(case, lid):
     # save output files and logs
     #
     globs_to_copy = []
-    if mach == "titan":
-        globs_to_copy.append("%s*OU" % job_id)
-    elif mach == "anvil":
-        globs_to_copy.append("/home/%s/%s*OU" % (getpass.getuser(), job_id))
-    elif mach == "mira":
-        globs_to_copy.append("%s*output" % job_id)
-        globs_to_copy.append("%s*cobaltlog" % job_id)
-    elif mach in ["edison", "cori-haswell", "cori-knl"]:
-        globs_to_copy.append("%s*run*%s" % (case.get_value("CASE"), job_id))
+    if job_id is not None:
+        if mach == "titan":
+            globs_to_copy.append("%s*OU" % job_id)
+        elif mach == "anvil":
+            globs_to_copy.append("/home/%s/%s*OU" % (getpass.getuser(), job_id))
+        elif mach == "mira":
+            globs_to_copy.append("%s*output" % job_id)
+            globs_to_copy.append("%s*cobaltlog" % job_id)
+        elif mach in ["edison", "cori-haswell", "cori-knl"]:
+            globs_to_copy.append("%s*run*%s" % (case.get_value("CASE"), job_id))
 
     globs_to_copy.append("logs/run_environment.txt.%s" % lid)
     globs_to_copy.append("logs/acme.log.%s.gz" % lid)
