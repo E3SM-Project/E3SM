3115d84d158360490372255f00a588dfdae6ca4f
Jim Edwards <jedwards@ucar.edu>
2017-06-08 16:33:14 -0600

    fix pylint issue

--- scripts/lib/CIME/build.py
+++ scripts/lib/CIME/build.py
@@ -121,6 +121,7 @@ def _build_checks(case, build_threaded, comp_interface, use_esmf_lib,
     ninst_value  = case.get_value("NINST_VALUE")
     smp_build    = case.get_value("SMP_BUILD")
     build_status = case.get_value("BUILD_STATUS")
+    expect(comp_interface == "mct", "Only supporting mct comp_interface at this time")
 
     smpstr = ""
     inststr = ""
