f06f100f0bf4b97f728ec0dd95434c6309410dea
zshaheen <zshaheen93@gmail.com>
2017-06-14 13:17:03 -0700

    added one ex

---
+++ docs/examples/temp-at-diff-levels-model-vs-obs/myparams.py
@@ -0,0 +1,23 @@
+reference_data_path = '/space1/test_data/reanalysis_data/ERA-Interim/ta/climos/'
+test_data_path = '/space/golaz1/ACME_simulations/20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01/pp/clim_rgr/0070-0099/'
+
+test_name = '20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01'
+
+ref_name = 'ERA-Interim_ta'
+
+backend = 'cartopy'
+diff_colormap = 'bl_to_darkred'
+results_dir = 'Jerry_ra_ta'
+
+sets = [5]
+
+def rename(new_name):
+    '''Given the new name, just return it.'''
+    return new_name
+
+derived_variables = {
+    'T': {
+        ('ta'): rename
+    }
+}
+
