71d6fa78a5312a249230410b320655827b70ee8a
zshaheen <zshaheen93@gmail.com>
2017-06-23 14:18:14 -0700

    matplotlib, mpl keywords added

--- acme_diags/plot/__init__.py
+++ acme_diags/plot/__init__.py
@@ -5,6 +5,8 @@ def _get_plot_fcn(backend, set_num):
     """Get the actual plot() function based on the backend and set_num."""
     try:
         import importlib
+        if backend == 'matplotlib' or backend == 'mpl':
+            backend = 'cartopy'
         mod_str = 'acme_diags.plot.{}.set{}'.format(backend, set_num)
         module = importlib.import_module(mod_str)
         return module.plot
@@ -20,8 +22,8 @@ def plot(set_num, ref, test, diff, metrics_dict, parameter):
     else:
         if not 1 <= int(set_num) <= 15:
             raise RuntimeError('set_num must be between 1 and 15')
-        if not parameter.backend == 'vcs' and not parameter.backend == 'cartopy':
-            raise RuntimeError('Invalid backend, choose either "vcs" or "cartopy"')
+        if parameter.backend not in ['vcs', 'cartopy', 'mpl', 'matplotlib']:
+            raise RuntimeError('Invalid backend, choose either "vcs" or "matplotlib"/"mpl"/"cartopy"')
 
         plot_fcn = _get_plot_fcn(parameter.backend, set_num)
         plot_fcn(ref, test, diff, metrics_dict, parameter)
