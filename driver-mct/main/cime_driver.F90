13d20433279aac752ec0b555a182f1cbe0eadee3
Robert Jacob <jacob@anl.gov>
2017-06-26 16:08:25 -0500

    De cesm-ify the cime driver
    renamed:    cesm_comp_mod.F90 -> cime_comp_mod.F90
    renamed:    cesm_driver.F90 -> cime_driver.F90
    
    Also renamed  seq_cesm_printlogheader to
    seq_cime_printlogheader and made message more generic

--- driver-mct/main/cime_driver.F90
+++ driver-mct/main/cime_driver.F90
@@ -1,8 +1,8 @@
-program cesm_driver
+program cime_driver
 
 !-------------------------------------------------------------------------------
 !
-! Purpose: Main program for CESM and ACME models.  Can have different
+! Purpose: Main program for a CIME-driven model.  Can have different
 !          land, sea-ice, and ocean models plugged in at compile-time.
 !          These models can be either: stub, dead, data, or active
 !          components or some combination of the above.
@@ -23,18 +23,18 @@ program cesm_driver
    use perf_mod,      only : t_startf, t_adj_detailf, t_stopf
    use ESMF,          only : ESMF_Initialize, ESMF_Finalize
    use seq_comm_mct,  only : esmf_logfile_kind
-   use cesm_comp_mod, only : cesm_pre_init1
-   use cesm_comp_mod, only : cesm_pre_init2
-   use cesm_comp_mod, only : cesm_init
-   use cesm_comp_mod, only : cesm_run
-   use cesm_comp_mod, only : cesm_final
+   use cime_comp_mod, only : cime_pre_init1
+   use cime_comp_mod, only : cime_pre_init2
+   use cime_comp_mod, only : cime_init
+   use cime_comp_mod, only : cime_run
+   use cime_comp_mod, only : cime_final
 
    implicit none
 
    !--------------------------------------------------------------------------
    ! Setup and initialize the communications and logging.
    !--------------------------------------------------------------------------
-   call cesm_pre_init1()
+   call cime_pre_init1()
 
    !--------------------------------------------------------------------------
    ! Initialize ESMF.  This is done outside of the ESMF_INTERFACE ifdef
@@ -48,10 +48,10 @@ program cesm_driver
    !--------------------------------------------------------------------------
    ! Timer initialization has to be after determination of the maximum number
    ! of threads used across all components, so called inside of
-   ! ccsm_pre_init2, as are t_startf and t_stopf for CPL:INIT and
-   ! cesm_pre_init2.
+   ! cime_pre_init2, as are t_startf and t_stopf for CPL:INIT and
+   ! cime_pre_init2.
    !--------------------------------------------------------------------------
-   call cesm_pre_init2()
+   call cime_pre_init2()
 
    !--------------------------------------------------------------------------
    ! Call the initialize, run and finalize routines.
@@ -60,17 +60,17 @@ program cesm_driver
    call t_startf('CPL:INIT')
    call t_adj_detailf(+1)
 
-   call cesm_init()
+   call cime_init()
 
    call t_adj_detailf(-1)
    call t_stopf('CPL:INIT')
 
-   call cesm_run()
-   call cesm_final()
+   call cime_run()
+   call cime_final()
 
    !--------------------------------------------------------------------------
    ! Clean-up
    !--------------------------------------------------------------------------
    call ESMF_Finalize( )
 
-end program cesm_driver
+end program cime_driver
