694c1fb69311c82b003a6bfd1c0ab1f9c122a1ce
Mark Taylor <mt@mp3dev.org>
2017-06-28 07:08:27 -0600

    more cmake cleanup: make cprnc an external target

--- components/homme/cmake/machineFiles/melvin.cmake
+++ components/homme/cmake/machineFiles/melvin.cmake
@@ -13,12 +13,6 @@ SET (ZLIB_DIR $ENV{SEMS_ZLIB_ROOT} CACHE FILEPATH "")
 
 SET (CPRNC_BINARY /sems-data-store/ACME/cprnc/build/cprnc CACHE FILEPAHT "")
 
-
-
-# hack until findnetcdf is updated to look for netcdf.mod
-# but this is ignored by cprnc
-# SET (ADD_Fortran_FLAGS "-I/usr/lib64/gfortran/modules" CACHE STRING "")
-
 SET (USE_QUEUING FALSE CACHE BOOL "")
 SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
 
