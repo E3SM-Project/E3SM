278baf7d3b3b51374b12e4f5185ed3a9c3baaafd
Michael Deakin <mdeakin@sandia.gov>
2017-07-13 12:23:00 -0600

    Initial work on converting NCR to use the SystemTestsCompareTwo object

--- scripts/lib/CIME/SystemTests/ncr.py
+++ scripts/lib/CIME/SystemTests/ncr.py
@@ -1,9 +1,10 @@
 """
 Implementation of the CIME NCR test.  This class inherits from SystemTestsCommon
 
-Build two exectuables for this test, the first is a default build the
-second halves the number of tasks and runs two instances for each component
-Lay all of the components out concurrently
+Build two exectuables for this test:
+The first is a default build
+The second runs two instances for each component with the same total number of tasks,
+and runs each of them concurrently
 """
 import shutil
 from CIME.XML.standard_module_setup import *
@@ -22,7 +23,7 @@ class NCR(SystemTestsCompareTwo):
         """
         SystemTestsCompareTwo.__init__(self, case,
                                        separate_builds = True,
-                                       run_two_suffix = "multiinst",
+                                       run_two_suffix = "singleinst",
                                        run_one_description = "default build",
                                        run_two_description = ("half the number of tasks, " +
                                                               "twice the number of instances"))
@@ -30,11 +31,15 @@ class NCR(SystemTestsCompareTwo):
     def _common_setup(self):
         pass
 
-    def _case_one_setup(self):
+    def _case_two_setup(self):
         # Set the number of instances, the ROOTPEs, and the number of tasks
         # The first case should have mostly default settings;
         # though we apparently halve the number of tasks if greater than 1
-        for comp in ['ATM','OCN','WAV','GLC','ICE','ROF','LND']:
+        comp_classes = self._case.get_values("COMP_CLASSES")
+        # Is this correct?
+        comp_classes.remove("CPL")
+
+        for comp in comp_classes:
             self._case.set_value("NINST_{}".format(comp), str(1))
             self._case.set_value("ROOTPE_{}".format(comp), 0)
             ntasks = self._case.get_value("NTASKS_{}".format(comp))
@@ -42,15 +47,20 @@ class NCR(SystemTestsCompareTwo):
                 self._case.set_value("NTASKS_{}".format(comp), ntasks // 2)
         case_setup(self._case, test_mode = True, reset = True)
 
-    def _case_two_setup(self):
+    def _case_one_setup(self):
         # Set the number of instances, the ROOTPEs, and the number of tasks
         # The second case should have twice the number of instances and half the number of tasks
         # All tasks should be running concurrently
+        comp_classes = self._case.get_values("COMP_CLASSES")
+        # Is this correct?
+        comp_classes.remove("CPL")
+
         ntasks_sum = 0
-        for comp in ['ATM','OCN','WAV','GLC','ICE','ROF','LND']:
+        # ['ATM','OCN','WAV','GLC','ICE','ROF','LND']
+        for comp in comp_classes:
             self._case.set_value("NINST_{}".format(comp), str(1))
             self._case.set_value("ROOTPE_{}".format(comp), ntasks_sum)
             ntasks = self._case.get_value("NTASKS_{}".format(comp))
             ntasks_sum += ntasks * 2
             self._case.set_value("NTASKS_{}".format(comp), ntasks * 2)
-        case_setup(self._case, test_mode = True, reset = True)
+        case_setup(self._case, test_mode = False, reset = True)
