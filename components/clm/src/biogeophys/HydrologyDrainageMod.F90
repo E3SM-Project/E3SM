sarats/ACME

Replace betr-v1 with betr-v2
Now betr-v1 is repalced wtih betr-v2. When betr is on,
the code run smoothly with intel compilers >15.x, and gnu compilers >5.x
on edison. The test suite will be run for edison in the
next commit and no answer change is expected for simulations with
betr off.

components/clm/src/biogeophys/HydrologyDrainageMod.F90
@@ -18,6 +18,7 @@ module HydrologyDrainageMod
   use WaterstateType    , only : waterstate_type
   use LandunitType      , only : lun                
   use ColumnType        , only : col                
+  use PatchType         , only : pft
   !
   ! !PUBLIC TYPES:
   implicit none
@@ -36,7 +37,7 @@ contains
        num_urbanc, filter_urbanc,         &
        num_do_smb_c, filter_do_smb_c,     &
        atm2lnd_vars, glc2lnd_vars, temperature_vars,    &
-       soilhydrology_vars, soilstate_vars, waterstate_vars, waterflux_vars)
+       soilhydrology_vars, soilstate_vars, waterstate_vars, waterflux_vars, ep_betr)
     !
     ! !DESCRIPTION:
     ! Calculates soil/snow hydrology with drainage (subsurface runoff)
@@ -49,8 +50,8 @@ contains
     use clm_varpar       , only : nlevgrnd, nlevurb, nlevsoi    
     use clm_time_manager , only : get_step_size, get_nstep
     use SoilHydrologyMod , only : CLMVICMap, Drainage
-    use TracerParamsMod  , only : pre_diagnose_soilcol_water_flux, diagnose_drainage_water_flux    
     use clm_varctl       , only : use_vsfm
+    use BeTRSimulationALM, only : betr_simulation_alm_type
     !
     ! !ARGUMENTS:
     type(bounds_type)        , intent(in)    :: bounds               
@@ -69,6 +70,7 @@ contains
     type(soilstate_type)     , intent(inout) :: soilstate_vars
     type(waterstate_type)    , intent(inout) :: waterstate_vars
     type(waterflux_type)     , intent(inout) :: waterflux_vars
+    class(betr_simulation_alm_type), intent(inout) :: ep_betr
     !
     ! !LOCAL VARIABLES:
     integer  :: g,l,c,j,fc                 ! indices
@@ -125,8 +127,8 @@ contains
       endif
 
       if (use_betr) then
-         call pre_diagnose_soilcol_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-              h2osoi_liq(bounds%begc:bounds%endc, 1:nlevsoi))
+        call ep_betr%BeTRSetBiophysForcing(bounds, col, pft, 1, nlevsoi, waterstate_vars=waterstate_vars)
+        call ep_betr%PreDiagSoilColWaterFlux(num_hydrologyc, filter_hydrologyc)
       endif
 
       if (.not. use_vsfm) then
@@ -137,8 +139,10 @@ contains
       endif
 
       if (use_betr) then
-         call diagnose_drainage_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-              h2osoi_liq(bounds%begc:bounds%endc, 1:nlevsoi), waterflux_vars)
+        call ep_betr%BeTRSetBiophysForcing(bounds, col, pft, 1, nlevsoi, waterstate_vars=waterstate_vars, &
+          waterflux_vars=waterflux_vars)
+        call ep_betr%DiagDrainWaterFlux(num_hydrologyc, filter_hydrologyc)
+        call ep_betr%RetrieveBiogeoFlux(bounds, 1, nlevsoi, waterflux_vars=waterflux_vars)
       endif
 
       do j = 1, nlevgrnd

components/clm/src/biogeophys/HydrologyNoDrainageMod.F90
@@ -19,6 +19,7 @@ Module HydrologyNoDrainageMod
   use WaterstateType    , only : waterstate_type
   use LandunitType      , only : lun                
   use ColumnType        , only : col                
+  use PatchType         , only : pft
   !
   ! !PUBLIC TYPES:
   implicit none
@@ -40,7 +41,7 @@ contains
        atm2lnd_vars, soilstate_vars, energyflux_vars, temperature_vars, &
        waterflux_vars, waterstate_vars, &
        soilhydrology_vars, aerosol_vars, &
-       soil_water_retention_curve, betrtracer_vars, tracerflux_vars, tracerstate_vars, &
+       soil_water_retention_curve, ep_betr, &
        alm_fates)
     !
     ! !DESCRIPTION:
@@ -70,15 +71,11 @@ contains
     use SoilHydrologyMod     , only : CLMVICMap, SurfaceRunoff, Infiltration, WaterTable
     use SoilWaterMovementMod , only : SoilWater 
     use SoilWaterRetentionCurveMod, only : soil_water_retention_curve_type
-    use TracerParamsMod      , only : pre_diagnose_soilcol_water_flux, diagnose_advect_water_flux, calc_smp_l
-    use BetrBGCMod           , only : calc_dew_sub_flux
-    use tracerfluxType       , only : tracerflux_type
-    use tracerstatetype      , only : tracerstate_type
-    use BeTRTracerType       , only : betrtracer_type        
     use clm_varctl           , only : use_vsfm
     use SoilHydrologyMod     , only : DrainageVSFM
-    use SoilWaterMovementMod, only : Compute_EffecRootFrac_And_VertTranSink_Default
-    use CLMFatesInterfaceMod  , only : hlm_fates_interface_type
+    use SoilWaterMovementMod , only : Compute_EffecRootFrac_And_VertTranSink_Default
+    use CLMFatesInterfaceMod , only : hlm_fates_interface_type
+    use BeTRSimulationALM    , only : betr_simulation_alm_type
     !
     ! !ARGUMENTS:
     type(bounds_type)        , intent(in)    :: bounds               
@@ -101,9 +98,7 @@ contains
     type(aerosol_type)       , intent(inout) :: aerosol_vars
     type(soilhydrology_type) , intent(inout) :: soilhydrology_vars
     class(soil_water_retention_curve_type), intent(in) :: soil_water_retention_curve
-    type(betrtracer_type)     , intent(in)    :: betrtracer_vars                    ! betr configuration information
-    type(tracerflux_type)     , intent(inout) :: tracerflux_vars                    ! tracer flux
-    type(tracerstate_type)    , intent(inout) :: tracerstate_vars                   ! tracer state variables data structure    
+    class(betr_simulation_alm_type), intent(inout) :: ep_betr
     type(hlm_fates_interface_type) , intent(inout) :: alm_fates
     !
     ! !LOCAL VARIABLES:
@@ -202,8 +197,8 @@ contains
            waterflux_vars, waterstate_vars)
 
       if (use_betr) then
-        call pre_diagnose_soilcol_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-          waterstate_vars%h2osoi_liq_col(bounds%begc:bounds%endc, 1:nlevsoi))
+        call ep_betr%BeTRSetBiophysForcing(bounds, col, pft, 1, nlevsoi, waterstate_vars=waterstate_vars)
+        call ep_betr%PreDiagSoilColWaterFlux(num_hydrologyc, filter_hydrologyc)
       endif
       
       if (use_vsfm) then
@@ -226,13 +221,12 @@ contains
             soil_water_retention_curve)
             
       if (use_betr) then
-        call diagnose_advect_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-             waterstate_vars%h2osoi_liq_col(bounds%begc:bounds%endc, 1:nlevsoi),                              &
-             soilhydrology_vars%qcharge_col(bounds%begc:bounds%endc), waterflux_vars)                
-        
-        call calc_smp_l(bounds, 1, nlevgrnd, num_hydrologyc, filter_hydrologyc, &
-             temperature_vars%t_soisno_col(bounds%begc:bounds%endc, 1:nlevgrnd), &
-             soilstate_vars, waterstate_vars, soil_water_retention_curve)
+        call ep_betr%BeTRSetBiophysForcing(bounds, col, pft, 1, nlevsoi, waterstate_vars=waterstate_vars, &
+           waterflux_vars=waterflux_vars, soilhydrology_vars = soilhydrology_vars)
+
+        call ep_betr%DiagAdvWaterFlux(num_hydrologyc, filter_hydrologyc)
+
+        call ep_betr%RetrieveBiogeoFlux(bounds, 1, nlevsoi, waterflux_vars=waterflux_vars)  
       endif
              
       if (use_vichydro) then
@@ -247,8 +241,7 @@ contains
       if (use_betr) then
          !apply dew and sublimation fluxes, this is a temporary work aroud for tracking water isotope
          !Jinyun Tang, Feb 4, 2015
-         call calc_dew_sub_flux(bounds, num_hydrologyc, filter_hydrologyc, &
-              waterstate_vars, waterflux_vars, betrtracer_vars, tracerflux_vars, tracerstate_vars)      
+         call ep_betr%CalcDewSubFlux(bounds, col, num_hydrologyc, filter_hydrologyc)
       endif           
       ! Natural compaction and metamorphosis.
       call SnowCompaction(bounds, num_snowc, filter_snowc, &

components/clm/src/biogeophys/SoilWaterMovementMod.F90
@@ -975,6 +975,7 @@ contains
                                                                         ! vegetation transpiration (mm H2O/s) (+ = to atm) 
           qflx_tran_veg_col   => waterflux_vars%qflx_tran_veg_col   , & ! Input:  [real(r8) (:)   ]  
                                                                         ! vegetation transpiration (mm H2O/s) (+ = to atm)
+          qflx_rootsoi_frac_patch  =>    waterflux_vars%qflx_rootsoi_frac_patch    , & ! Output: [real(r8) (:,:) ]  vegetation/soil water exchange (m H2O/s) (+ = to atm)
           rootr_patch         => soilstate_vars%rootr_patch         , & ! Input: [real(r8) (:,:) ]
                                                                         ! effective fraction of roots in each soil layer  
           rootr_col           => soilstate_vars%rootr_col             & ! Output: [real(r8) (:,:) ]  
@@ -1006,6 +1007,7 @@ contains
                   if (pft%active(p)) then
                      rootr_col(c,j) = rootr_col(c,j) + rootr_patch(p,j) * &
                            qflx_tran_veg_patch(p) * pft%wtcol(p)
+                     qflx_rootsoi_frac_patch(p,j) = rootr_patch(p,j) * qflx_tran_veg_patch(p) * pft%wtcol(p)
                   end if
                end if
             end do
@@ -1031,6 +1033,24 @@ contains
 
          end do
       end do
+
+      do pi = 1,max_patch_per_col
+         do j = 1,nlevsoi
+            do fc = 1, num_filterc
+               c = filterc(fc)
+               if (pi <= col%npfts(c)) then
+                  p = col%pfti(c) + pi - 1
+                  if (pft%active(p)) then
+                    if(rootr_col(c,j)==0._r8)then
+                      qflx_rootsoi_frac_patch(p,j) = 0._r8
+                    else
+                      qflx_rootsoi_frac_patch(p,j) = qflx_rootsoi_frac_patch(p,j)/(temp(c)*rootr_col(c,j))
+                    endif
+                  end if
+               end if
+            end do
+         end do
+       enddo
     end associate
     return
  end subroutine Compute_EffecRootFrac_And_VertTranSink_Default

E3SM-Project/E3SM

components/clm/src/biogeophys/HydrologyDrainageMod.F90
@@ -18,6 +18,7 @@ module HydrologyDrainageMod
   use WaterstateType    , only : waterstate_type
   use LandunitType      , only : lun_pp                
   use ColumnType        , only : col_pp                
+  use VegetationType    , only : veg_pp                
   !
   ! !PUBLIC TYPES:
   implicit none
@@ -36,7 +37,7 @@ contains
        num_urbanc, filter_urbanc,         &
        num_do_smb_c, filter_do_smb_c,     &
        atm2lnd_vars, glc2lnd_vars, temperature_vars,    &
-       soilhydrology_vars, soilstate_vars, waterstate_vars, waterflux_vars)
+       soilhydrology_vars, soilstate_vars, waterstate_vars, waterflux_vars, ep_betr)
     !
     ! !DESCRIPTION:
     ! Calculates soil/snow hydrology with drainage (subsurface runoff)
@@ -49,8 +50,8 @@ contains
     use clm_varpar       , only : nlevgrnd, nlevurb, nlevsoi    
     use clm_time_manager , only : get_step_size, get_nstep
     use SoilHydrologyMod , only : CLMVICMap, Drainage
-    use TracerParamsMod  , only : pre_diagnose_soilcol_water_flux, diagnose_drainage_water_flux    
     use clm_varctl       , only : use_vsfm
+    use BeTRSimulationALM, only : betr_simulation_alm_type
     !
     ! !ARGUMENTS:
     type(bounds_type)        , intent(in)    :: bounds               
@@ -69,6 +70,7 @@ contains
     type(soilstate_type)     , intent(inout) :: soilstate_vars
     type(waterstate_type)    , intent(inout) :: waterstate_vars
     type(waterflux_type)     , intent(inout) :: waterflux_vars
+    class(betr_simulation_alm_type), intent(inout) :: ep_betr
     !
     ! !LOCAL VARIABLES:
     integer  :: g,l,c,j,fc                 ! indices
@@ -125,8 +127,8 @@ contains
       endif
 
       if (use_betr) then
-         call pre_diagnose_soilcol_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-              h2osoi_liq(bounds%begc:bounds%endc, 1:nlevsoi))
+        call ep_betr%BeTRSetBiophysForcing(bounds, col_pp, veg_pp, 1, nlevsoi, waterstate_vars=waterstate_vars)
+        call ep_betr%PreDiagSoilColWaterFlux(num_hydrologyc, filter_hydrologyc)
       endif
 
       if (.not. use_vsfm) then
@@ -137,8 +139,10 @@ contains
       endif
 
       if (use_betr) then
-         call diagnose_drainage_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-              h2osoi_liq(bounds%begc:bounds%endc, 1:nlevsoi), waterflux_vars)
+        call ep_betr%BeTRSetBiophysForcing(bounds, col_pp, veg_pp, 1, nlevsoi, waterstate_vars=waterstate_vars, &
+          waterflux_vars=waterflux_vars)
+        call ep_betr%DiagDrainWaterFlux(num_hydrologyc, filter_hydrologyc)
+        call ep_betr%RetrieveBiogeoFlux(bounds, 1, nlevsoi, waterflux_vars=waterflux_vars)
       endif
 
       do j = 1, nlevgrnd

components/clm/src/biogeophys/HydrologyNoDrainageMod.F90
@@ -19,6 +19,7 @@ Module HydrologyNoDrainageMod
   use WaterstateType    , only : waterstate_type
   use LandunitType      , only : lun_pp                
   use ColumnType        , only : col_pp                
+  use VegetationType      , only : veg_pp                
   !
   ! !PUBLIC TYPES:
   implicit none
@@ -40,7 +41,7 @@ contains
        atm2lnd_vars, soilstate_vars, energyflux_vars, temperature_vars, &
        waterflux_vars, waterstate_vars, &
        soilhydrology_vars, aerosol_vars, &
-       soil_water_retention_curve, betrtracer_vars, tracerflux_vars, tracerstate_vars, &
+       soil_water_retention_curve, ep_betr, &
        alm_fates)
     !
     ! !DESCRIPTION:
@@ -70,15 +71,11 @@ contains
     use SoilHydrologyMod     , only : CLMVICMap, SurfaceRunoff, Infiltration, WaterTable
     use SoilWaterMovementMod , only : SoilWater 
     use SoilWaterRetentionCurveMod, only : soil_water_retention_curve_type
-    use TracerParamsMod      , only : pre_diagnose_soilcol_water_flux, diagnose_advect_water_flux, calc_smp_l
-    use BetrBGCMod           , only : calc_dew_sub_flux
-    use tracerfluxType       , only : tracerflux_type
-    use tracerstatetype      , only : tracerstate_type
-    use BeTRTracerType       , only : betrtracer_type        
     use clm_varctl           , only : use_vsfm
     use SoilHydrologyMod     , only : DrainageVSFM
-    use SoilWaterMovementMod, only : Compute_EffecRootFrac_And_VertTranSink_Default
-    use CLMFatesInterfaceMod  , only : hlm_fates_interface_type
+    use SoilWaterMovementMod , only : Compute_EffecRootFrac_And_VertTranSink_Default
+    use CLMFatesInterfaceMod , only : hlm_fates_interface_type
+    use BeTRSimulationALM    , only : betr_simulation_alm_type
     !
     ! !ARGUMENTS:
     type(bounds_type)        , intent(in)    :: bounds               
@@ -101,9 +98,7 @@ contains
     type(aerosol_type)       , intent(inout) :: aerosol_vars
     type(soilhydrology_type) , intent(inout) :: soilhydrology_vars
     class(soil_water_retention_curve_type), intent(in) :: soil_water_retention_curve
-    type(betrtracer_type)     , intent(in)    :: betrtracer_vars                    ! betr configuration information
-    type(tracerflux_type)     , intent(inout) :: tracerflux_vars                    ! tracer flux
-    type(tracerstate_type)    , intent(inout) :: tracerstate_vars                   ! tracer state variables data structure    
+    class(betr_simulation_alm_type), intent(inout) :: ep_betr
     type(hlm_fates_interface_type) , intent(inout) :: alm_fates
     !
     ! !LOCAL VARIABLES:
@@ -204,8 +199,8 @@ contains
            waterflux_vars, waterstate_vars)
 
       if (use_betr) then
-        call pre_diagnose_soilcol_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-          waterstate_vars%h2osoi_liq_col(bounds%begc:bounds%endc, 1:nlevsoi))
+        call ep_betr%BeTRSetBiophysForcing(bounds, col_pp, veg_pp, 1, nlevsoi, waterstate_vars=waterstate_vars)
+        call ep_betr%PreDiagSoilColWaterFlux(num_hydrologyc, filter_hydrologyc)
       endif
       
       if (use_vsfm) then
@@ -228,13 +223,12 @@ contains
             soil_water_retention_curve)
             
       if (use_betr) then
-        call diagnose_advect_water_flux(bounds, num_hydrologyc, filter_hydrologyc, num_urbanc, filter_urbanc, &
-             waterstate_vars%h2osoi_liq_col(bounds%begc:bounds%endc, 1:nlevsoi),                              &
-             soilhydrology_vars%qcharge_col(bounds%begc:bounds%endc), waterflux_vars)                
-        
-        call calc_smp_l(bounds, 1, nlevgrnd, num_hydrologyc, filter_hydrologyc, &
-             temperature_vars%t_soisno_col(bounds%begc:bounds%endc, 1:nlevgrnd), &
-             soilstate_vars, waterstate_vars, soil_water_retention_curve)
+        call ep_betr%BeTRSetBiophysForcing(bounds, col_pp, veg_pp, 1, nlevsoi, waterstate_vars=waterstate_vars, &
+           waterflux_vars=waterflux_vars, soilhydrology_vars = soilhydrology_vars)
+
+        call ep_betr%DiagAdvWaterFlux(num_hydrologyc, filter_hydrologyc)
+
+        call ep_betr%RetrieveBiogeoFlux(bounds, 1, nlevsoi, waterflux_vars=waterflux_vars)  
       endif
              
       if (use_vichydro) then
@@ -249,8 +243,7 @@ contains
       if (use_betr) then
          !apply dew and sublimation fluxes, this is a temporary work aroud for tracking water isotope
          !Jinyun Tang, Feb 4, 2015
-         call calc_dew_sub_flux(bounds, num_hydrologyc, filter_hydrologyc, &
-              waterstate_vars, waterflux_vars, betrtracer_vars, tracerflux_vars, tracerstate_vars)      
+         call ep_betr%CalcDewSubFlux(bounds, col_pp, num_hydrologyc, filter_hydrologyc)
       endif           
       ! Natural compaction and metamorphosis.
       call SnowCompaction(bounds, num_snowc, filter_snowc, &

components/clm/src/biogeophys/SoilWaterMovementMod.F90
@@ -1086,6 +1086,7 @@ contains
                                                                         ! vegetation transpiration (mm H2O/s) (+ = to atm) 
           qflx_tran_veg_col   => waterflux_vars%qflx_tran_veg_col   , & ! Input:  [real(r8) (:)   ]  
                                                                         ! vegetation transpiration (mm H2O/s) (+ = to atm)
+          qflx_rootsoi_frac_patch  =>    waterflux_vars%qflx_rootsoi_frac_patch    , & ! Output: [real(r8) (:,:) ]  vegetation/soil water exchange (m H2O/s) (+ = to atm)
           rootr_patch         => soilstate_vars%rootr_patch         , & ! Input: [real(r8) (:,:) ]
                                                                         ! effective fraction of roots in each soil layer  
           rootr_col           => soilstate_vars%rootr_col             & ! Output: [real(r8) (:,:) ]  
@@ -1118,7 +1119,8 @@ contains
                   p = col_pp%pfti(c) + pi - 1
                   if (veg_pp%active(p)) then
                      rootr_col(c,j) = rootr_col(c,j) + rootr_patch(p,j) * &
-                           qflx_tran_veg_patch(p) * veg_pp%wtcol(p)
+                          qflx_tran_veg_patch(p) * veg_pp%wtcol(p)
+                     qflx_rootsoi_frac_patch(p,j) = rootr_patch(p,j) * qflx_tran_veg_patch(p) * veg_pp%wtcol(p)
                   end if
                end if
             end do
@@ -1145,6 +1147,24 @@ contains
 
          end do
       end do
+
+      do pi = 1,max_patch_per_col
+         do j = 1,nlevsoi
+            do fc = 1, num_filterc
+               c = filterc(fc)
+               if (pi <= col_pp%npfts(c)) then
+                  p = col_pp%pfti(c) + pi - 1
+                  if (veg_pp%active(p)) then
+                    if(rootr_col(c,j)==0._r8)then
+                      qflx_rootsoi_frac_patch(p,j) = 0._r8
+                    else
+                      qflx_rootsoi_frac_patch(p,j) = qflx_rootsoi_frac_patch(p,j)/(temp(c)*rootr_col(c,j))
+                    endif
+                  end if
+               end if
+            end do
+         end do
+       enddo
     end associate
     return
  end subroutine Compute_EffecRootFrac_And_VertTranSink_Default