112c4c36cc5f139b1fe06a1354e59dfcb7cd97dd
James Foucar <jgfouca@sandia.gov>
2017-07-19 16:15:28 -0600

    Be sure to only do the inst setuo once

--- scripts/lib/CIME/SystemTests/slr.py
+++ scripts/lib/CIME/SystemTests/slr.py
@@ -35,18 +35,21 @@ class SLR(SystemTestsCommon):
         # (in the development phase we use 3)
         # Lay all of the components out concurrently
 
-        ninst = 3
-        logging.warn("Starting to build multi-instance exe")
-        for comp in ['ATM','OCN','WAV','GLC','ICE','ROF','LND']:
-            ntasks = self._case.get_value("NTASKS_%s"%comp)
-            self._case.set_value("ROOTPE_%s"%comp, 0)
-            self._case.set_value("NINST_%s"%comp,  ninst)
-            self._case.set_value("NTASKS_%s"%comp, ntasks*ninst)
+        # Only want this to happen once. It will impact the sharedlib build
+        # so it has to happen there.
+        if not model_only:
+            ninst = 3
+            logging.warn("Starting to build multi-instance exe")
+            for comp in ['ATM','OCN','WAV','GLC','ICE','ROF','LND']:
+                ntasks = self._case.get_value("NTASKS_%s"%comp)
+                self._case.set_value("ROOTPE_%s"%comp, 0)
+                self._case.set_value("NINST_%s"%comp,  ninst)
+                self._case.set_value("NTASKS_%s"%comp, ntasks*ninst)
 
-        self._case.set_value("ROOTPE_CPL",0)
-        self._case.set_value("NTASKS_CPL",ntasks*ninst)
-        self._case.flush()
+            self._case.set_value("ROOTPE_CPL",0)
+            self._case.set_value("NTASKS_CPL",ntasks*ninst)
+            self._case.flush()
 
-        case_setup(self._case, test_mode=True, reset=True)
+            case_setup(self._case, test_mode=True, reset=True)
 
         self.build_indv(sharedlib_only=sharedlib_only, model_only=model_only)
