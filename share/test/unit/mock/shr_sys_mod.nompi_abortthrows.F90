e4ee345053126ed32b3e6823825df2e9f361ceff
Bill Sacks <sacks@ucar.edu>
2017-07-27 16:13:00 -0600

    Use fake shr_abort_mod from fake shr_sys_mod
    Rather than defining the fake abort routine directly in the fake
    shr_sys_mod

--- share/test/unit/mock/shr_sys_mod.nompi_abortthrows.F90
+++ share/test/unit/mock/shr_sys_mod.nompi_abortthrows.F90
@@ -4,18 +4,18 @@ module shr_sys_mod
 ! It contains only a few routines that are needed, and an abort method that throws a pFUnit
 ! exception instead of actually aborting.
 
-  ! TODO(wjs, 2017-07-27) I have now separated shr_sys_abort into its own module:
-  ! shr_abort_mod:shr_abort_abort. This mock should be revised or removed in light of
-  ! that.
-
 use shr_kind_mod, only: &
      shr_kind_in, shr_kind_r8
+
+! This used to be in shr_sys_mod; we provide this rename for backwards compatibility
+use shr_abort_mod, only : shr_sys_abort => shr_abort_abort
 
 implicit none
 private
 save
 
 ! Fake abort
+! Imported from shr_abort_mod and republished with renames for backwards compatibility
 public :: shr_sys_abort
 
 ! Fake sleep
@@ -25,19 +25,6 @@ public :: shr_sys_sleep
 public :: shr_sys_flush
 
 contains
-
-subroutine shr_sys_abort(string, rc)
-  use pfunit_mod, only: throw
-
-  character(*), optional :: string
-  integer(shr_kind_in), optional :: rc
-
-  ! Prevent compiler spam about unused variables.
-  if (.false.) rc = rc
-
-  call throw("ABORTED: "//trim(string))
-
-end subroutine shr_sys_abort
 
 subroutine shr_sys_sleep(sec)
   real(shr_kind_r8), intent(in) :: sec
