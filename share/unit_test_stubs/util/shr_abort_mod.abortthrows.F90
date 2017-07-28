e6eecf44f030aaa9bc375e467c593be6aa26cfaf
Bill Sacks <sacks@ucar.edu>
2017-07-27 20:28:04 -0600

    Add a unit test of the pfunit version of shr_abort_abort

--- share/unit_test_stubs/util/shr_abort_mod.abortthrows.F90
+++ share/unit_test_stubs/util/shr_abort_mod.abortthrows.F90
@@ -17,6 +17,28 @@ contains
 
   subroutine shr_abort_abort(string,rc)
     ! Replacement for shr_abort_abort that throws a pfunit exception rather than aborting
+    !
+    ! This can be used to test expected errors (i.e., failure testing).
+    !
+    ! If this occurs within a pFUnit-based test:
+    !
+    ! - If you have code like:
+    !
+    !   @assertExceptionRaised(expected_message)
+    !
+    !   then your test will pass if the actual message in the 'throw' call (including the
+    !   'ABORTED: ' prefix) matches expected_message; it will fail if the actual message
+    !   doesn't match the expected message
+    !
+    ! - If you don't have
+    !
+    !   @assertExceptionRaised
+    !
+    !   or
+    !
+    !   call assertExceptionRaised
+    !
+    !   then this will result in the given pFUnit test failing.
 
     !----- arguments -----
     character(len=*)    , intent(in), optional :: string  ! error message string
