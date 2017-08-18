ac387d53e6219fbe5681a3b1bdc3a3474325046b
Qing Zhu <qzhu@n0002.scs00>
2017-08-18 16:15:51 -0700

    Initialization of P pools during AD->RG spinup transition

--- components/clm/src/data_types/CNStateType.F90
+++ components/clm/src/data_types/CNStateType.F90
@@ -158,6 +158,13 @@ module CNStateType
 
      real(r8), pointer :: frac_loss_lit_to_fire_col        (:)
      real(r8), pointer :: frac_loss_cwd_to_fire_col        (:)
+
+     ! soil phosphorus pools Qing Z. 2017
+     real(r8), pointer :: labp_col             (:)   ! labile phosphorus g/m2
+     real(r8), pointer :: secp_col             (:)   ! secondary phosphorus g/m2
+     real(r8), pointer :: occp_col             (:)   ! occluded phosphorus g/m2
+     real(r8), pointer :: prip_col             (:)   ! parent material phosphorus g/m2
+     logical           :: pdatasets_present          ! surface dataset has p pools info
    contains
 
      procedure, public  :: Init         
@@ -333,6 +340,12 @@ contains
     allocate(fert_type                        (begc:endc))                   ; fert_type     (:) = 0
     allocate(fert_continue                    (begc:endc))                   ; fert_continue (:) =0
     allocate(fert_dose                        (begc:endc,1:12))              ; fert_dose     (:,:) = nan
+
+    ! soil phosphorus pools Qing Z. 2017
+    allocate(this%labp_col                    (begc:endc))                   ; this%labp_col(:) = nan
+    allocate(this%secp_col                    (begc:endc))                   ; this%secp_col(:) = nan
+    allocate(this%occp_col                    (begc:endc))                   ; this%occp_col(:) = nan
+    allocate(this%prip_col                    (begc:endc))                   ; this%prip_col(:) = nan
   end subroutine InitAllocate
 
   !------------------------------------------------------------------------
@@ -672,6 +685,11 @@ contains
     integer     ,pointer     :: fert_type_rdin (:)
     integer     ,pointer     :: fert_continue_rdin (:)
     real(r8)    ,pointer     :: fert_dose_rdin (:,:)
+    ! soil phosphorus pool Qing Z. 2017
+    real(r8) ,pointer  :: labp_g (:)                       ! read in - LABILE_P
+    real(r8) ,pointer  :: secp_g (:)                       ! read in - SECONDARY_P
+    real(r8) ,pointer  :: occp_g (:)                       ! read in - OCCLUDED_P
+    real(r8) ,pointer  :: prip_g (:)                       ! read in - APATITE_P
     !-----------------------------------------------------------------------
 
     begc = bounds%begc; endc= bounds%endc
@@ -811,6 +829,63 @@ contains
        write(iulog,*)
     endif
     
+    if (masterproc) then
+       write(iulog,*) 'Attempting to read initial phosphorus pools data .....'
+    end if
+
+    call getfil (fsurdat, locfn, 0)
+    call ncd_pio_openfile (ncid, locfn, 0)
+
+    ! Read soil phosphorus pool Qing Z. 2017 
+    this%pdatasets_present = .true.
+    allocate(labp_g(bounds%begg:bounds%endg))
+    call ncd_io(ncid=ncid, varname='LABILE_P', flag='read', data=labp_g, dim1name=grlnd, readvar=readvar)
+    if (.not. readvar) then
+       this%pdatasets_present = .false.
+    else
+       do c = bounds%begc, bounds%endc
+          g = col_pp%gridcell(c)
+          this%labp_col(c) = labp_g(g)
+       end do
+    end if
+    deallocate(labp_g)
+
+    allocate(secp_g(bounds%begg:bounds%endg))
+    call ncd_io(ncid=ncid, varname='SECONDARY_P', flag='read', data=secp_g, dim1name=grlnd, readvar=readvar)
+    if (.not. readvar) then
+       this%pdatasets_present = .false.
+    else
+       do c = bounds%begc, bounds%endc
+          g = col_pp%gridcell(c)
+          this%secp_col(c) = secp_g(g)
+       end do
+    end if
+    deallocate(secp_g)
+
+    allocate(occp_g(bounds%begg:bounds%endg))
+    call ncd_io(ncid=ncid, varname='OCCLUDED_P', flag='read', data=occp_g, dim1name=grlnd, readvar=readvar)
+    if (.not. readvar) then
+       this%pdatasets_present = .false.
+    else
+       do c = bounds%begc, bounds%endc
+          g = col_pp%gridcell(c)
+          this%occp_col(c) = occp_g(g)
+       end do
+    end if
+    deallocate(occp_g)
+
+    allocate(prip_g(bounds%begg:bounds%endg))
+    call ncd_io(ncid=ncid, varname='APATITE_P', flag='read', data=prip_g, dim1name=grlnd, readvar=readvar)
+    if (.not. readvar) then
+       this%pdatasets_present = .false.
+    else
+       do c = bounds%begc, bounds%endc
+          g = col_pp%gridcell(c)
+          this%prip_col(c) = prip_g(g)
+       end do
+    end if
+    deallocate(prip_g)  
+
     ! --------------------------------------------------------------------
     ! Initialize terms needed for dust model
     ! TODO - move these terms to DUSTMod module variables 
@@ -967,6 +1042,7 @@ contains
     use abortutils , only : endrun
     use restUtilMod
     use ncdio_pio
+    use fileutils  , only : getfil
     !
     ! !ARGUMENTS:
     class(cnstate_type) :: this
@@ -976,7 +1052,7 @@ contains
     !
     ! !LOCAL VARIABLES:
     integer, pointer :: temp1d(:) ! temporary
-    integer          :: p,j,c,i   ! indices
+    integer          :: p,j,c,i,g   ! indices
     logical          :: readvar   ! determine if variable is on initial file
     real(r8), pointer :: ptr2d(:,:) ! temp. pointers for slicing larger arrays
     real(r8), pointer :: ptr1d(:)   ! temp. pointers for slicing larger arrays
