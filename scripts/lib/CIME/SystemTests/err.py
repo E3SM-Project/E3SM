3fdbbecee039c5b0a89bf4c36bcac7285399f904
Jim Edwards <jedwards@ucar.edu>
2017-08-30 18:31:33 -0600

    convert erri

--- scripts/lib/CIME/SystemTests/err.py
+++ scripts/lib/CIME/SystemTests/err.py
@@ -5,8 +5,8 @@ ERR tests short term archiving and restart capabilities
 from CIME.XML.standard_module_setup import *
 from CIME.SystemTests.system_tests_compare_two import SystemTestsCompareTwo
 from CIME.SystemTests.irt import IRT
-
-import shutil, glob
+from CIME.case_st_archive import restore_from_archive
+from CIME.utils import ls_sorted_by_mtime
 
 logger = logging.getLogger(__name__)
 
@@ -22,21 +22,14 @@ class ERR(IRT):
                                        run_one_description = 'initial',
                                        run_two_description = 'restart',
                                        multisubmit = True)
+
     def _case_one_custom_postrun_action(self):
         pass
 
     def _case_two_custom_prerun_action(self):
         dout_s_root = self._case1.get_value("DOUT_S_ROOT")
-        rundir = self._case.get_value("RUNDIR")
-        case = self._case.get_value("CASE")
-        # First remove restart, history and rpointer files from the run directory
-        for item in glob.iglob(os.path.join(rundir, "{}.*".format(case))):
-            if not item.endswith("base"):
-                os.remove(item)
-
-        for item in glob.iglob(os.path.join(rundir, "rpointer.*")):
-            os.remove(item)
-
-        # Then replace them from the restart directory
-        for item in glob.iglob(os.path.join(dout_s_root,"rest","*","*")):
-            shutil.copy(item, rundir)
+        rest_root = os.path.abspath(os.path.join(dout_s_root,"rest"))
+        restart_list = ls_sorted_by_mtime(rest_root)
+        expect(len(restart_list) >= 1, "No restart files found in {}".format(rest_root))
+        restore_from_archive(self._case, rest_dir=
+            os.path.join(rest_root, restart_list[0]))
