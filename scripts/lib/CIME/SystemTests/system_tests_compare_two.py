810f4febac60b6ec605d8012c5c1d183418c5b84
Bill Sacks <sacks@ucar.edu>
2017-08-31 12:42:58 -0600

    Add more unit tests of system_tests_compare_two, fix multisubmit bug
    The multisubmit bug related to the indentation of a block: this was
    mistakenly being done for both phases of a multi-submit, whereas it only
    should have been done after the second
    phase. https://github.com/ESMCI/cime/pull/1830 had this correct, but the
    indentation was incorrect in https://github.com/ESMCI/cime/pull/1837,
    which is what came to master.

--- scripts/lib/CIME/SystemTests/system_tests_compare_two.py
+++ scripts/lib/CIME/SystemTests/system_tests_compare_two.py
@@ -228,12 +228,12 @@ class SystemTestsCompareTwo(SystemTestsCommon):
                 self.run_indv(suffix = self._run_two_suffix)
                 self._case_two_custom_postrun_action()
 
-        # Compare results
-        # Case1 is the "main" case, and we need to do the comparisons from there
-        self._activate_case1()
-        self._link_to_case2_output()
+            # Compare results
+            # Case1 is the "main" case, and we need to do the comparisons from there
+            self._activate_case1()
+            self._link_to_case2_output()
 
-        self._component_compare_test(self._run_one_suffix, self._run_two_suffix, success_change=success_change)
+            self._component_compare_test(self._run_one_suffix, self._run_two_suffix, success_change=success_change)
 
     # ========================================================================
     # Private methods
