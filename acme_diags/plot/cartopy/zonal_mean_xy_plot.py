c26bb7b25747ff820137ad09a32f851fc5299d69
Chris Golaz <golaz1@llnl.gov>
2017-09-10 17:16:36 -0700

    Customized ACME zonal_mean_xy plots. Added horizontal line for difference plot.

--- acme_diags/plot/cartopy/zonal_mean_xy_plot.py
+++ acme_diags/plot/cartopy/zonal_mean_xy_plot.py
@@ -43,6 +43,7 @@ def plot(reference, test, diff, metrics_dict, parameter):
     ax.plot(reference.getLatitude()[:],ma.squeeze( reference.asma()),'r',linewidth=2)
     ax1 = fig.add_axes(panel[1])
     ax1.plot(diff.getLatitude()[:],ma.squeeze( diff.asma() ),'k', linewidth=2) 
+    ax1.axhline(y=0, color='0.5')
     fig.text(panel[0][0],panel[0][2]+0.095,"Test: " + parameter.test_name,ha='left',fontdict=plotSideTitle,color = 'black')
     fig.text(panel[0][0],panel[0][2]+0.07,"Reference: " +parameter.reference_name,ha='left',fontdict=plotSideTitle,color='red')
     fig.text(panel[1][0],panel[0][2]-0.3,"Test-Reference",ha='left',fontdict=plotSideTitle)
