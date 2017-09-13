20e2eb622c0bc3494e0aa844c6f07c31ebbd8662
Jill Chengzhu Zhang <zhang40@llnl.gov>
2017-09-13 14:59:24 -0700

    change data paths

--- examples/model-to-model/myparams.py
+++ examples/model-to-model/myparams.py
@@ -1,8 +1,8 @@
-reference_data_path = '/space/golaz1/ACME_simulations/20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01/pp/clim_rgr/0070-0099/'
-ref_name = '20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01'
+reference_data_path = '/p/cscratch/acme/data/test_model_data_for_acme_diags/'
+ref_name = '20161118.beta0.F1850COSP.ne30_ne30.edison'
 
-test_data_path = '/space/golaz1/ACME_simulations/20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01/pp/clim_rgr/0070-0099/'
-test_name = '20160520.A_WCYCL1850.ne30_oEC.edison.alpha6_01'
+test_data_path = '/p/cscratch/acme/data/test_model_data_for_acme_diags/'
+test_name = '20161118.beta0.FC5COSP.ne30_ne30.edison'
 
 backend = 'vcs'
 results_dir = 'model_to_model'
