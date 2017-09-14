eb8baedd804c7ba782c9a2d5d7d6ed605032e8bc
chengzhuzhang <zhang40@llnl.gov>
2017-09-13 17:42:38 -0700

    add an example for obs vs obs

---
+++ examples/obs-vs-obs/myparams.py
@@ -0,0 +1,11 @@
+reference_data_path = '/p/cscratch/acme/data/obs_for_acme_diags/'
+ref_name = 'ceres_ebaf_toa_v2.8'
+
+test_data_path = '/p/cscratch/acme/data/obs_for_acme_diags/'
+test_name = 'ceres_ebaf_toa_v4.0'
+
+backend = 'matplotlib'
+results_dir = 'obs_vs_obs'
+
+sets = ['lat_lon']
+seasons = ['ANN']
