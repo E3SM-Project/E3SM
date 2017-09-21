beb9ee3b4cc20cbf8497548c2c2e0a25d57a2bff
Jim Edwards <jedwards@ucar.edu>
2017-09-21 14:14:02 -0600

    rework fix for err test

--- scripts/lib/CIME/SystemTests/err.py
+++ scripts/lib/CIME/SystemTests/err.py
@@ -2,7 +2,7 @@
 CIME ERR test  This class inherits from ERS
 ERR tests short term archiving and restart capabilities
 """
-import glob, os
+import glob, os, shutil
 from CIME.XML.standard_module_setup import *
 from CIME.SystemTests.restart_tests import RestartTest
 from CIME.case_st_archive import restore_from_archive
@@ -11,17 +11,25 @@ from CIME.utils import ls_sorted_by_mtime
 logger = logging.getLogger(__name__)
 
 class ERR(RestartTest):
-
     def __init__(self, case): # pylint: disable=super-init-not-called
         """
         initialize an object interface to the ERR system test
         """
-        RestartTest.__init__(self, case, # pylint: disable=non-parent-init-called
+        super(ERR, self).__init__(case,
                              separate_builds = False,
                              run_two_suffix = 'rest',
                              run_one_description = 'initial',
                              run_two_description = 'restart',
                              multisubmit = True)
+
+    def _case_one_setup(self):
+        super(ERR, self)._case_one_setup()
+        self._case.set_value("DOUT_S", True)
+
+    def _case_two_setup(self):
+        super(ERR, self)._case_two_setup()
+        self._case.set_value("DOUT_S", False)
+
 
     def _case_two_custom_prerun_action(self):
         dout_s_root = self._case1.get_value("DOUT_S_ROOT")
@@ -33,9 +41,12 @@ class ERR(RestartTest):
 
     def _case_two_custom_postrun_action(self):
         # Link back to original case1 name
+        # This is needed so that the necessary files are present for
+        # baseline comparison and generation,
+        # since some of them may have been moved to the archive directory
         for case_file in glob.iglob(os.path.join(self._case1.get_value("RUNDIR"),
                                                  "*.nc.{}".format(self._run_one_suffix))):
             orig_file = case_file[:-(1+len(self._run_one_suffix))]
             if not os.path.isfile(orig_file):
-                os.symlink(case_file, orig_file)
+                shutil.copyfile(case_file, orig_file)
 
