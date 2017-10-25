3cfba66210b60cfd5cfcd172d8f75ef14630166b
James Foucar <jgfouca@sandia.gov>
2017-10-25 15:01:22 -0600

    Move the save pre/post run provenance to immediately around the model.exe invocation
    The background progress montoring script is spawned in
    save_prerun_provenance, and killed, if necessary, in
    save_postrun_prevenance.
    
    [BFB]

--- scripts/lib/CIME/case_run.py
+++ scripts/lib/CIME/case_run.py
@@ -102,7 +102,11 @@ def _run_model_impl(case, lid, skip_pnl=False):
 
     while loop:
         loop = False
+
+        save_prerun_provenance(case)
         stat = run_cmd(cmd, from_dir=rundir)[0]
+        save_postrun_provenance(case)
+
         model_logfile = os.path.join(rundir, model + ".log." + lid)
         # Determine if failure was due to a failed node, if so, try to restart
         if stat != 0:
@@ -285,7 +289,6 @@ def case_run(case, skip_pnl=False):
                         lid, prefix="prerun")
             case.read_xml()
 
-        save_prerun_provenance(case)
         lid = run_model(case, lid, skip_pnl)
 
         save_logs(case, lid)       # Copy log files back to caseroot
@@ -303,8 +306,6 @@ def case_run(case, skip_pnl=False):
             do_external(postrun_script, case.get_value("CASEROOT"), case.get_value("RUNDIR"),
                         lid, prefix="postrun")
             case.read_xml()
-
-        save_postrun_provenance(case)
 
     logger.warn("check for resubmit")
     resubmit_check(case)
