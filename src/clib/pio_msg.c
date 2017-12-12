a920f03930074e4f4e359f8b692111f5a40349c9
Jim Edwards <jedwards@ucar.edu>
2017-12-12 16:06:22 -0700

    Squashed 'src/externals/pio2/' changes from c84f74c..0f241db
    0f241db response to comments
    1007a7a cannot predetermin ndims here
    99ef07d Merge pull request #1241 from NCAR/free_new_allocs
    29ed162 free recently allocated vars
    fbc3584 Merge pull request #1239 from NCAR/dontuse_nc_max
    63dee3d Merge pull request #1240 from NCAR/limitto2GiB
    64f2492 limit to 2GiB due to romio bug
    29aee05 dont use NC_MAX values
    d831ad3 Merge pull request #1231 from mgduda/mpi_type_fix
    e996bdb Merge pull request #1222 from NCAR/ejh_autoconf_logging
    426af22 Partial fix for incorrect type of 'mpi_type' in pioc_support.c
    7eb724f added enable-logging option to autotools build
    
    git-subtree-dir: src/externals/pio2
    git-subtree-split: 0f241db88cfee1912a2769a052dba0d2d79f83d5

--- src/clib/pio_msg.c
+++ src/clib/pio_msg.c
@@ -190,7 +190,8 @@ int create_file_handler(iosystem_desc_t *ios)
 
     /* Call the create file function. */
     PIOc_createfile(ios->iosysid, &ncid, &iotype, filename, mode);
-    
+
+
     LOG((1, "create_file_handler succeeded!"));
     return PIO_NOERR;
 }
@@ -276,7 +277,7 @@ int inq_handler(iosystem_desc_t *ios)
 
     /* Call the inq function to get the values. */
     PIOc_inq(ncid, ndimsp, nvarsp, ngattsp, unlimdimidp);
-    
+
     return PIO_NOERR;
 }
 
@@ -1005,8 +1006,10 @@ int inq_var_handler(iosystem_desc_t *ios)
     char name_present, xtype_present, ndims_present, dimids_present, natts_present;
     char name[NC_MAX_NAME + 1], *namep = NULL;
     nc_type xtype, *xtypep = NULL;
+    /* no easy way not to hard code this array length */
+    int dimids[8];
     int *ndimsp = NULL, *dimidsp = NULL, *nattsp = NULL;
-    int ndims, dimids[NC_MAX_DIMS], natts;
+    int ndims, natts;
     int mpierr;
 
     LOG((1, "inq_var_handler"));
@@ -1040,7 +1043,7 @@ int inq_var_handler(iosystem_desc_t *ios)
     if (ndims_present)
         ndimsp = &ndims;
     if (dimids_present)
-        dimidsp = dimids;
+	dimidsp = dimids;
     if (natts_present)
         nattsp = &natts;
 
@@ -1064,9 +1067,10 @@ int inq_var_chunking_handler(iosystem_desc_t *ios)
 {
     int ncid;
     int varid;
+    int ndims;
     char storage_present, chunksizes_present;
     int storage, *storagep = NULL;
-    PIO_Offset chunksizes[NC_MAX_DIMS], *chunksizesp = NULL;
+    PIO_Offset *chunksizesp = NULL;
     int mpierr;
 
     assert(ios);
@@ -1080,6 +1084,8 @@ int inq_var_chunking_handler(iosystem_desc_t *ios)
         return check_mpi2(ios, NULL, mpierr, __FILE__, __LINE__);
     if ((mpierr = MPI_Bcast(&storage_present, 1, MPI_CHAR, 0, ios->intercomm)))
         return check_mpi2(ios, NULL, mpierr, __FILE__, __LINE__);
+    if ((mpierr = MPI_Bcast(&ndims, 1, MPI_INT, 0, ios->intercomm)))
+        return check_mpi2(ios, NULL, mpierr, __FILE__, __LINE__);
     if ((mpierr = MPI_Bcast(&chunksizes_present, 1, MPI_CHAR, 0, ios->intercomm)))
         return check_mpi2(ios, NULL, mpierr, __FILE__, __LINE__);
     LOG((2,"inq_var_handler ncid = %d varid = %d storage_present = %d chunksizes_present = %d",
@@ -1089,10 +1095,14 @@ int inq_var_chunking_handler(iosystem_desc_t *ios)
     if (storage_present)
         storagep = &storage;
     if (chunksizes_present)
-        chunksizesp = chunksizes;
+        if (!(chunksizesp = malloc(ndims *sizeof(PIO_Offset))))
+            return pio_err(ios, NULL, PIO_ENOMEM, __FILE__, __LINE__);
 
     /* Call the inq function to get the values. */
     PIOc_inq_var_chunking(ncid, varid, storagep, chunksizesp);
+
+    if(chunksizes_present)
+	free(chunksizesp);
 
     return PIO_NOERR;
 }
@@ -1492,7 +1502,7 @@ int def_var_chunking_handler(iosystem_desc_t *ios)
     int ndims;
     int storage;
     char chunksizes_present;
-    PIO_Offset chunksizes[NC_MAX_DIMS], *chunksizesp = NULL;
+    PIO_Offset *chunksizesp = NULL;
     int mpierr;
 
     assert(ios);
@@ -1510,18 +1520,20 @@ int def_var_chunking_handler(iosystem_desc_t *ios)
         return check_mpi2(ios, NULL, mpierr, __FILE__, __LINE__);
     if ((mpierr = MPI_Bcast(&chunksizes_present, 1, MPI_CHAR, 0, ios->intercomm)))
         return check_mpi2(ios, NULL, mpierr, __FILE__, __LINE__);
-    if (chunksizes_present)
-        if ((mpierr = MPI_Bcast(chunksizes, ndims, MPI_OFFSET, 0, ios->intercomm)))
+    if (chunksizes_present){
+        if (!(chunksizesp = malloc(ndims* sizeof(PIO_Offset))))
+            return pio_err(ios, NULL, PIO_ENOMEM, __FILE__, __LINE__);
+        if ((mpierr = MPI_Bcast(chunksizesp, ndims, MPI_OFFSET, 0, ios->intercomm)))
             return check_mpi2(ios, NULL, mpierr, __FILE__, __LINE__);
+    }
     LOG((1, "def_var_chunking_handler got parameters ncid = %d varid = %d storage = %d "
          "ndims = %d chunksizes_present = %d", ncid, varid, storage, ndims, chunksizes_present));
 
-    /* Set the non-NULL pointers. */
-    if (chunksizes_present)
-        chunksizesp = chunksizes;
-
     /* Call the function. */
     PIOc_def_var_chunking(ncid, varid, storage, chunksizesp);
+
+    if(chunksizes_present)
+	free(chunksizesp);
 
     LOG((1, "def_var_chunking_handler succeeded!"));
     return PIO_NOERR;
@@ -2703,7 +2715,7 @@ int pio_msg_handler2(int io_rank, int component_count, iosystem_desc_t **iosys,
         /* If an error was returned by the handler, exit. */
         LOG((3, "pio_msg_handler2 ret %d msg %d index %d io_rank %d", ret, msg, index, io_rank));
         if (ret)
-            return pio_err(my_iosys, NULL, ret, __FILE__, __LINE__);            
+            return pio_err(my_iosys, NULL, ret, __FILE__, __LINE__);
 
         /* Listen for another msg from the component whose message we
          * just handled. */
