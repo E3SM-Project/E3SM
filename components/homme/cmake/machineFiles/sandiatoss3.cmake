b78895fc1b518f3e7a891a1fb368cd6e17af66bf
Mark Taylor <mt@mp3dev.org>
2017-12-15 12:40:46 -0700

    remove forced fortran options from sandiatoss3.cmake
    Test system (sandiatoss3) was overriding all the compiler flags, meaning that it
    was not testing the changes in this PR.
    
    tweak machine file so sandiatoss3 systems will use default compiler flags.

--- components/homme/cmake/machineFiles/sandiatoss3.cmake
+++ components/homme/cmake/machineFiles/sandiatoss3.cmake
@@ -15,19 +15,9 @@ SET (USE_MPI_OPTIONS "--map-by node:SPAN" CACHE FILEPATH "")
 # this is ignored if we use FORCE_Fortran_FLAGS
 SET (ADD_Fortran_FLAGS "-traceback" CACHE STRING "")
 
-# override cmake's intel defaults:
-# default cmake options for Intel: 
-#      -assume byterecl -fp-model precise -ftz -g -O3 
-# -O3 causes problems on redsky when openMP is enabled (even for 1 thread)
-#
-#SET (FORCE_Fortran_FLAGS "-openmp -fp-model fast -ftz -g -O2" CACHE STRING "")
-SET (FORCE_Fortran_FLAGS "-openmp -traceback -fp-model precise -ftz -g -O2" CACHE STRING "")
-
 # redsky upgrade 8/2017, need to load sems-netcdf module:
 SET (WITH_PNETCDF FALSE CACHE FILEPATH "")
 SET (NETCDF_DIR $ENV{SEMS_NETCDF_ROOT} CACHE FILEPATH "")
-SET (HDF5_DIR $ENV{SEMS_HDF5_ROOT} CACHE FILEPATH "")
-SET (ZLIB_DIR $ENV{SEMS_ZLIB_ROOT} CACHE FILEPATH "")
 
 
 SET (USE_QUEUING FALSE CACHE BOOL "")
