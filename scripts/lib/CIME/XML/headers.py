90b1eecd53fdc82e7a6bb1ee4aae08d880552fd4
Jim Edwards <jedwards@ucar.edu>
2017-12-20 11:26:43 -0700

    headers should be Generic not Entryid

--- scripts/lib/CIME/XML/headers.py
+++ scripts/lib/CIME/XML/headers.py
@@ -3,12 +3,12 @@ Interface to the config_headers.xml file.  This class inherits from EntryID.py
 """
 from CIME.XML.standard_module_setup import *
 
-from CIME.XML.entry_id import EntryID
+from CIME.XML.generic_xml import GenericXML
 from CIME.XML.files import Files
 
 logger = logging.getLogger(__name__)
 
-class Headers(EntryID):
+class Headers(GenericXML):
     def __init__(self,infile=None):
         """
         initialize an object
@@ -20,7 +20,7 @@ class Headers(EntryID):
         if infile is None:
             files = Files()
             infile = files.get_value('CASEFILE_HEADERS', resolved=True)
-        EntryID.__init__(self, infile)
+        super(Headers, self).__init__(infile)
 
     def get_header_node(self, fname):
         fnode = self.get_child("file", attributes={"name" : fname})
