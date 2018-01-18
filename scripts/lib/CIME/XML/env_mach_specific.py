f3ef4a72c373ab9004bc4ee2cc3a16777bc7331e
Jim Edwards <jedwards@ucar.edu>
2018-01-17 17:43:57 -0700

    set root for toplevel machines object

--- scripts/lib/CIME/XML/env_mach_specific.py
+++ scripts/lib/CIME/XML/env_mach_specific.py
@@ -26,7 +26,7 @@ class EnvMachSpecific(EnvBase):
     def populate(self, machobj):
         """Add entries to the file using information from a Machines object."""
         items = ("module_system", "environment_variables", "resource_limits", "mpirun", "run_exe","run_misc_suffix")
-        default_run_suffix = machobj.get_child("default_run_suffix")
+        default_run_suffix = machobj.get_child("default_run_suffix", root=machobj.root)
         default_run_exe_node = machobj.get_child("default_run_exe", root=default_run_suffix)
         default_run_misc_suffix_node = machobj.get_child("default_run_misc_suffix", root=default_run_suffix)
 
