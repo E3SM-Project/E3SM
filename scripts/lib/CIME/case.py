f46f1f20b5b78cafece22f8974577cbd3af93266
James Foucar <jgfouca@sandia.gov>
2018-02-01 16:36:26 -0700

    Remove case.st_archive

--- scripts/lib/CIME/case.py
+++ scripts/lib/CIME/case.py
@@ -1008,7 +1008,6 @@ class Case(object):
                     os.path.join(toolsdir, "case.build"),
                     os.path.join(toolsdir, "case.submit"),
                     os.path.join(toolsdir, "case.qstatus"),
-                    os.path.join(toolsdir, "case.st_archive"),
                     os.path.join(toolsdir, "case.cmpgen_namelists"),
                     os.path.join(toolsdir, "preview_namelists"),
                     os.path.join(toolsdir, "preview_run"),
