1ac2fb69d981e6f275df4538d05ad477075c0fb5
Jim Edwards <jedwards@ucar.edu>
2018-02-23 14:18:02 -0700

    fix pylint issue

--- scripts/lib/CIME/XML/env_mach_pes.py
+++ scripts/lib/CIME/XML/env_mach_pes.py
@@ -3,7 +3,6 @@ Interface to the env_mach_pes.xml file.  This class inherits from EntryID
 """
 from CIME.XML.standard_module_setup import *
 from CIME.XML.env_base import EnvBase
-from CIME.XML.generic_xml import GenericXML
 import math
 
 logger = logging.getLogger(__name__)
