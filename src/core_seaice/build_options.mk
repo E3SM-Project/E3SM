fa7844282edeb2a777d9c3cc88f58a78578c912e
Adrian K. Turner <akt@pn1707938.lanl.gov>
2018-03-12 14:50:36 -0600

    Change executable name to seaice_model

--- src/core_seaice/build_options.mk
+++ src/core_seaice/build_options.mk
@@ -1,7 +1,7 @@
 ifeq "$(ROOT_DIR)" ""
    ROOT_DIR=$(shell pwd)/src
 endif
-EXE_NAME=cice_model
+EXE_NAME=seaice_model
 NAMELIST_SUFFIX=seaice
 FCINCLUDES += -I$(ROOT_DIR)/core_seaice/column -I$(ROOT_DIR)/core_seaice/shared -I$(ROOT_DIR)/core_seaice/analysis_members -I$(ROOT_DIR)/core_seaice/model_forward
 override CPPFLAGS += -DCORE_SEAICE
