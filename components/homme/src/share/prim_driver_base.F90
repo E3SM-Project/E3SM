61d83d7f50b75c4b01e8dde2f6799fcbb5439370
Mark Taylor <mt@mp3dev.org>
2018-03-14 16:08:46 -0400

    another memory leak

--- components/homme/src/share/prim_driver_base.F90
+++ components/homme/src/share/prim_driver_base.F90
@@ -487,6 +487,12 @@ contains
     do j = 1, MetaVertex(1)%nmembers
        call deallocate_gridvertex_nbrs(MetaVertex(1)%members(j))
     end do
+    do j = 1, MetaVertex(1)%nedges
+       deallocate(MetaVertex(1)%edges(j)%members
+       deallocate(MetaVertex(1)%edges(j)%edgeptrP
+       deallocate(MetaVertex(1)%edges(j)%edgeptrS
+       deallocate(MetaVertex(1)%edges(j)%edgeptrP_ghost
+    end do
     deallocate(MetaVertex(1)%edges)
     deallocate(MetaVertex(1)%members)
     deallocate(MetaVertex)
