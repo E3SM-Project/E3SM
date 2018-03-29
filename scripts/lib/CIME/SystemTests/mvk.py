0cf862b2e2717236a31958ef7244a96fea94a0a0
Joseph H Kennedy <kennedyjh@ornl.gov>
2018-03-29 11:29:20 -0400

    Update MVK system test build and namelist settings

--- scripts/lib/CIME/SystemTests/mvk.py
+++ scripts/lib/CIME/SystemTests/mvk.py
@@ -29,34 +29,29 @@ class MVK(SystemTestsCommon):
 
         # Build executable with multiple instances
         ninst = 30
-
-        # BSINGH: For debugging only - Building the model can take
-        # significant amount of time. Setting fake_bld to True can save
-        # that time
-        fake_bld = False
+        # The default NTASKS seem to be much lower than we'd need, so scale it
+        scale_ntasks = 1
 
         # Only want this to happen once. It will impact the sharedlib build
         # so it has to happen there.
         if not model_only:
             logging.warn('Starting to build multi-instance exe')
-            for comp in ['ATM', 'OCN', 'WAV', 'GLC', 'ICE', 'ROF', 'LND']:
-                ntasks = self._case.get_value("NTASKS_{}".format(comp))
+            for comp in self._case.get_values("COMP_CLASSES"):
                 self._case.set_value('ROOTPE_{}'.format(comp), 0)
-                self._case.set_value('NINST_{}'.format(comp), ninst)
-                self._case.set_value('NTASKS_{}'.format(comp), ntasks*ninst)
+                self._case.set_value('NTHRDS_{}'.format(comp), 1)
 
-            self._case.set_value('ROOTPE_CPL', 0)
-            self._case.set_value('NTASKS_CPL', ntasks*ninst)
+                ntasks = self._case.get_value("NTASKS_{}".format(comp))
+                print('MVK:NTASKS_{}: {}'.format(comp, ntasks))
+                self._case.set_value('NTASKS_{}'.format(comp), ntasks*scale_ntasks*ninst)
+                if comp != 'CPL':
+                    self._case.set_value('NINST_{}'.format(comp), ninst)
+
             self._case.flush()
 
             case_setup(self._case, test_mode=False, reset=True)
 
-        # BSINGH: Faking a bld can save the time code spend in building the model components
-        if fake_bld:
-            if not sharedlib_only:
-                post_build(self._case, [])
-        else:
-            self.build_indv(sharedlib_only=sharedlib_only, model_only=model_only)
+
+        self.build_indv(sharedlib_only=sharedlib_only, model_only=model_only)
 
         # =================================================================
         # Run-time settings.
@@ -68,48 +63,27 @@ class MVK(SystemTestsCommon):
         data_root = self._case.get_value("DIN_LOC_ROOT")
         for iinst in range(1, ninst+1):
             with open('user_nl_cam_{:04d}'.format(iinst), 'w') as nl_atm_file:
-                nl_atm_file.write("fincl2 = 'TS:A','TSMN:M','TSMX:X','PRECT:A','PRECTMX:X'")
-                nl_atm_file.write('mfilt = 1,30')
-                nl_atm_file.write('nhtfrq = 0,-24')
+                nl_atm_file.write("fincl2 = 'TS:A','TSMN:M','TSMX:X','PRECT:A','PRECTMX:X'\n")
+                nl_atm_file.write('mfilt = 1,30\n')
+                nl_atm_file.write('nhtfrq = 0,-24\n')
 
-                nl_atm_file.write('cldfrc_rhminl = 0.9100')
-                nl_atm_file.write('zmconv_c0_ocn = 0.0110')
+                # nl_atm_file.write('cldfrc_rhminl = 0.9100\n')
+                nl_atm_file.write('zmconv_c0_ocn = 0.0074D0\n')
+                nl_atm_file.write('dtime = 5400\n')
 
-                nl_atm_file.write("rsf_file = '{}/atm/waccm/phot/RSF_GT200nm_v3.0_c080416.nc'".format(data_root))
-                nl_atm_file.write("bnd_topo = '{}/atm/cam/topo/USGS-gtopo30_1.9x2.5_smooth500-50_ne16np4_c050602.nc'".format(data_root))
+                # nl_atm_file.write("rsf_file = '{}/atm/waccm/phot/RSF_GT200nm_v3.0_c080416.nc'\n".format(data_root))
+                # nl_atm_file.write("bnd_topo = '{}/atm/cam/topo/USGS-gtopo30_1.9x2.5_smooth500-50_ne16np4_c050602.nc'\n".format(data_root))
 
-                nl_atm_file.write('phys_loadbalance = 0')
-                nl_atm_file.write('new_random = .true.')
-                nl_atm_file.write('pertlim = 1.0e-10')
-                nl_atm_file.write('seed_custom = {}'.format(iinst))
+                # nl_atm_file.write('phys_loadbalance = 0\n')
+                nl_atm_file.write('new_random = .true.\n')
+                nl_atm_file.write('pertlim = 1.0e-10\n')
+                nl_atm_file.write('seed_custom = {}\n'.format(iinst))
 
-            with open('user_nl_clm_{:04d}'.format(iinst), 'w') as nl_clm_file:
-                nl_clm_file.write("finidat = '/lustre/atlas/proj-shared/cli106/salil/archive/A_F.1850c5.ne16_g37.l2/rest/0100-11-01-00000/A_F.1850c5.ne16_g37.l2.clm2.r.0100-11-01-00000.nc'")
+            # with open('user_nl_clm_{:04d}'.format(iinst), 'w') as nl_clm_file:
+            #     # nl_clm_file.write("finidat = '{}/lnd/clm2/initdata_map/clmi.I1850CLM45.ne30_g16.1155ba0.clm2.r.nc'\n".format(data_root))
+            #     nl_clm_file.write("finidat = '{}/lnd/clm2/initdata_map/clmi.I1850CLM45.ne16_qu240.d02c96d.clm2.r.0021-01-01-00000.nc'\n".format(data_root))
 
 
-# ------
-# Notes:
-# ------
-# Follow setup of cases in: /autofs/nccs-svm1_home1/salil/acme_cases/ne16_F1850C5_master/
-# User_nl_* changes:
-#   CAM:
-#       fincl2 = 'TS:A','TSMN:M','TSMX:X','PRECT:A','PRECTMX:X'
-#       mfilt = 1,30
-#       nhtfrq = 0,-24
-#
-#       cldfrc_rhminl = 0.9100
-#       zmconv_c0_ocn = 0.0110
-#
-#       rsf_file = '/lustre/atlas1/cli900/world-shared/cesm/inputdata/atm/waccm/phot/RSF_GT200nm_v3.0_c080416.nc'
-#       bnd_topo = '/lustre/atlas1/cli900/world-shared/cesm/inputdata/atm/cam/topo/USGS-gtopo30_1.9x2.5_smooth500-50_ne16np4_c050602.nc'
-#
-#       phys_loadbalance = 0
-#       new_random = .true.
-#       pertlim = 1.e-10
-#       seed_custom = 2
-#
-#   CLM:
-#       finidat = '/lustre/atlas/proj-shared/cli106/salil/archive/A_F.1850c5.ne16_g37.l2/rest/0100-11-01-00000/A_F.1850c5.ne16_g37.l2.clm2.r.0100-11-01-00000.nc'
+    def run_phase(self):
 
-# Original case setup:
-# ./create_newcase -mach titan -compiler pgi -res ne16_g37 -compset F1850C5 -case /ccs/home/salil/acme_cases/F1850C5_08162017_master -project cli106
+        self.run_indv()
