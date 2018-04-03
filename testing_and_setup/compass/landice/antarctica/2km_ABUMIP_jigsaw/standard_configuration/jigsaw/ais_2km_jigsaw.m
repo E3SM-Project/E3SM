ed187ad22ec7c09f8bce52dd62be850332b601bd
Matthew Hoffman <mhoffman@lanl.gov>
2018-04-03 14:52:39 -0600

    Make mesh gen script a Matlab script rather than a function
    Makes debugging and analysis easier.

--- testing_and_setup/compass/landice/antarctica/2km_ABUMIP_jigsaw/standard_configuration/jigsaw/ais_2km_jigsaw.m
+++ testing_and_setup/compass/landice/antarctica/2km_ABUMIP_jigsaw/standard_configuration/jigsaw/ais_2km_jigsaw.m
@@ -1,4 +1,4 @@
-function ais_2km_jigsaw()
+%function ais_2km_jigsaw()
 % 2km ais mesh
 
     name = 'ais2km';
@@ -347,6 +347,7 @@ axis equal
         'vertices',mesh.point.coord(:,1:2), ...
         'facecolor','w', ...
         'edgecolor',[.2,.2,.2]) ;
+
     hold on; axis image;
     patch ('faces',mesh.tria3.index(t_90,1:3), ...
         'vertices',mesh.point.coord(:,1:2), ...
@@ -363,7 +364,7 @@ axis equal
         'linewidth',1.5) ;
     axis equal
     title('JIGSAW TRIA mesh') ;
-
+%%
     drawscr(mesh.point.coord (:,1:2), ...
             mesh.edge2.index (:,1:2), ...
             mesh.tria3.index (:,1:3)) ;
@@ -379,29 +380,4 @@ axis equal
 %         'position',[.05,.10,.30,.35]) ;
     drawnow ;
 
-end
-function d = point_to_line(pt, v1, v2)
-
-%       a = v1 - v2;
-%       b = pt - v2;
-%       d = norm(cross([a(1), a(2), 0], [b(1), b(2), 0])) / norm(a);
-       
-m=(v2(2)-v1(2))/(v2(1)-v1(1));
-b=v1(2)-m*v1(1);
-d=abs(m*pt(1) + -1*pt(2) + b)/sqrt(m^2+(-1)^2);
-
-%       
-% a=v1; b=v2; x=pt;
-% d_ab = norm(a-b);
-% d_ax = norm(a-x);
-% d_bx = norm(b-x);
-% 
-% if dot(a-b,x-b)*dot(b-a,x-a)>=0
-%     A = [a,1;b,1;x,1];
-%     dist = abs(det(A))/d_ab;        
-% else
-%     dist = min(d_ax, d_bx);
-% end      
-%     d=dist;  
-    
-end
+%end
