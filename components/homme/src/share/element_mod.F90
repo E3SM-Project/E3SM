96c210dfa6b9896aea77202c4f62cc78be88be52
Matt Norman <normanmr@ornl.gov>
2018-04-04 10:07:43 -0400

    Revert "Revert "derived_dp now flattened. I got rid of the gap between euler_step calls, finally.""
    This reverts commit d934b51832e576a3b7fa4fc01591847d183b6730.

--- components/homme/src/share/element_mod.F90
+++ components/homme/src/share/element_mod.F90
@@ -272,7 +272,7 @@ contains
   subroutine setup_element_pointers(elem)
     use dimensions_mod, only: nelemd, qsize
 #if USE_OPENACC
-    use element_state, only : state_Qdp, derived_vn0, derived_divdp, derived_divdp_proj, derived_dpdiss_ave
+    use element_state, only : state_Qdp, derived_vn0, derived_divdp, derived_divdp_proj, derived_dpdiss_ave, derived_dp
 #endif
     implicit none
     type(element_t), intent(inout) :: elem(:)
@@ -283,12 +283,14 @@ contains
     allocate( derived_divdp            (np,np,nlev,nelemd)                    )
     allocate( derived_divdp_proj       (np,np,nlev,nelemd)                    )
     allocate( derived_dpdiss_ave       (np,np,nlev,nelemd)                    )
+    allocate( derived_dp               (np,np,nlev,nelemd)                    )
     do ie = 1 , nelemd
       elem(ie)%state%Qdp                 => state_Qdp                (:,:,:,:,:,ie)
       elem(ie)%derived%vn0               => derived_vn0              (:,:,:,:,ie)  
       elem(ie)%derived%divdp             => derived_divdp            (:,:,:,ie)    
       elem(ie)%derived%divdp_proj        => derived_divdp_proj       (:,:,:,ie)    
       elem(ie)%derived%dpdiss_ave        => derived_dpdiss_ave       (:,:,:,ie)    
+      elem(ie)%derived%dp                => derived_dp               (:,:,:,ie)    
     enddo
 #endif
   end subroutine setup_element_pointers
