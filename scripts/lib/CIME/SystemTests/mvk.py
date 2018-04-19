23efdae586776f4fcbc6f27c709b8569aa0741d3
Joseph H Kennedy <kennedyjh@ornl.gov>
2018-04-19 12:56:12 -0400

    Setup to try and debug #2212

--- scripts/lib/CIME/SystemTests/mvk.py
+++ scripts/lib/CIME/SystemTests/mvk.py
@@ -9,9 +9,25 @@ This class inherits from SystemTestsCommon.
 from CIME.XML.standard_module_setup import *
 from CIME.SystemTests.system_tests_common import SystemTestsCommon
 from CIME.case_setup import case_setup
-from CIME.build import post_build
-# import CIME.utils
-# from CIME.check_lockedfiles import *
+
+import CIME.test_status
+
+from livvkit.util import functions as lfn
+# FIXME: this is python2 only. Python 2 and 3 compatible is much hard, esp. if
+#        python 3.0--3.4 need to be supported
+# try:
+#     import livvkit
+# except ImportError:
+#     import pip
+#     pip.main(['install', '--user', 'livvkit'])
+#     import site
+#     reload(site)
+#     import livvkit
+
+# FIXME: remove once eve is a package, or inc. eve into CIME/SystemTest/Test_utils
+eve_lib_dir = '/ccs/home/kennedy/EVE/eve'
+sys.path.append(eve_lib_dir)
+import eve
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +39,8 @@ class MVK(SystemTestsCommon):
         initialize an object interface to the MVK test
         """
         SystemTestsCommon.__init__(self, case)
+
+        logger.info('MVK:INIT: {}'.format(self._compare_baseline()))
 
 
     def build_phase(self, sharedlib_only=False, model_only=False):
@@ -41,7 +59,7 @@ class MVK(SystemTestsCommon):
                 self._case.set_value('NTHRDS_{}'.format(comp), 1)
 
                 ntasks = self._case.get_value("NTASKS_{}".format(comp))
-                print('MVK:NTASKS_{}: {}'.format(comp, ntasks))
+                logger.info('MVK:NTASKS_{}: {}'.format(comp, ntasks))
                 self._case.set_value('NTASKS_{}'.format(comp), ntasks*scale_ntasks*ninst)
                 if comp != 'CPL':
                     self._case.set_value('NINST_{}'.format(comp), ninst)
@@ -49,7 +67,6 @@ class MVK(SystemTestsCommon):
             self._case.flush()
 
             case_setup(self._case, test_mode=False, reset=True)
-
 
         self.build_indv(sharedlib_only=sharedlib_only, model_only=model_only)
 
@@ -60,7 +77,6 @@ class MVK(SystemTestsCommon):
         # =================================================================
 
         # namelist specifications for each instance
-        data_root = self._case.get_value("DIN_LOC_ROOT")
         for iinst in range(1, ninst+1):
             with open('user_nl_cam_{:04d}'.format(iinst), 'w') as nl_atm_file:
                 nl_atm_file.write("fincl2 = 'TS:A','TSMN:M','TSMX:X','PRECT:A','PRECTMX:X'\n")
@@ -71,19 +87,47 @@ class MVK(SystemTestsCommon):
                 nl_atm_file.write('zmconv_c0_ocn = 0.0074D0\n')
                 nl_atm_file.write('dtime = 5400\n')
 
-                # nl_atm_file.write("rsf_file = '{}/atm/waccm/phot/RSF_GT200nm_v3.0_c080416.nc'\n".format(data_root))
-                # nl_atm_file.write("bnd_topo = '{}/atm/cam/topo/USGS-gtopo30_1.9x2.5_smooth500-50_ne16np4_c050602.nc'\n".format(data_root))
-
                 # nl_atm_file.write('phys_loadbalance = 0\n')
                 nl_atm_file.write('new_random = .true.\n')
                 nl_atm_file.write('pertlim = 1.0e-10\n')
                 nl_atm_file.write('seed_custom = {}\n'.format(iinst))
 
-            # with open('user_nl_clm_{:04d}'.format(iinst), 'w') as nl_clm_file:
-            #     # nl_clm_file.write("finidat = '{}/lnd/clm2/initdata_map/clmi.I1850CLM45.ne30_g16.1155ba0.clm2.r.nc'\n".format(data_root))
-            #     nl_clm_file.write("finidat = '{}/lnd/clm2/initdata_map/clmi.I1850CLM45.ne16_qu240.d02c96d.clm2.r.0021-01-01-00000.nc'\n".format(data_root))
-
 
     def run_phase(self):
 
         self.run_indv()
+
+
+    # def _generate_baseline(self):
+    #     run_dir = self._case.get_value("RUNDIR")
+    #     basegen_dir = os.path.join(self._case.get_value("BASELINE_ROOT"), self._case.get_value("BASEGEN_CASE"))
+    #     logger.info('MVK:GENDIR: {}'.format(basegen_dir))
+    #
+    #     with self._test_status:
+    #         self._test_status.set_status(CIME.test_status.GENERATE_PHASE, CIME.test_status.TEST_PASS_STATUS)
+
+
+    def _compare_baseline(self):
+        logger.info('MVK:CMPR: {}'.format(self._case.get_value("COMPARE_BASELINE")))
+        logger.info('MVK:CMPR: {}'.format(type(self._case.get_value("COMPARE_BASELINE"))))
+
+        run_dir = self._case.get_value("RUNDIR")
+        case_name = self._case.get_value("CASE")
+        basegen_case = self._case.get_value("BASEGEN_CASE")
+        basegen_dir = os.path.join(self._case.get_value("BASELINE_ROOT"), self._case.get_value("BASEGEN_CASE"))
+
+        eve_config = {
+            "MVK_{}".format(case_name): {
+                "module": os.path.join(eve_lib_dir, "extensions/ks.py"),
+                "case1": case_name,
+                "dir1": run_dir,
+                "case2": basegen_case,
+                "dir2": basegen_dir,
+                "ninst": 30,
+                "critical": 13
+            }
+        }
+        logger.info('MVK:EVECONFIG: {}'.format(eve_config))
+
+        with self._test_status:
+            self._test_status.set_status(CIME.test_status.BASELINE_PHASE, CIME.test_status.TEST_PASS_STATUS)
