c3c3bffd0ec2890bd4b64437b9b7c4395bf3dfa0
Balwinder Singh <balwindersingh@gmail.com>
2018-04-22 19:01:39 -0700

    some more formatting chaged to icol

--- components/cam/src/physics/cam/co2_data_flux.F90
+++ components/cam/src/physics/cam/co2_data_flux.F90
@@ -7,7 +7,6 @@ module co2_data_flux
 
   use shr_kind_mod,   only : r8 => shr_kind_r8
   use ppgrid,         only : begchunk, endchunk, pcols
-  use phys_grid,      only : get_ncols_p
   use error_messages, only : alloc_err
   use tracer_data,  only : trfld, trfile, trcdata_init, advance_trcdata
 
@@ -19,14 +18,12 @@ module co2_data_flux
 
 ! private
   private
-
 !--------------------------------------------------------------------------------------------------
 TYPE :: read_interp          
- 
   real(r8), pointer, dimension(:,:)   :: co2flx
 END TYPE read_interp
 !-------------------------------------------------------------------------------------------------
-                       
+ 
 contains
  
 !===============================================================================
@@ -44,12 +41,11 @@ subroutine read_data_flux (fields, file, input_file, xin, state, pbuf2d)
  
 !---------------------------Common blocks-------------------------------
 ! Dummy arguments
-  type(trfld), pointer :: fields(:)
-  type(trfile), intent(out)       :: file
-
-  character(len=*),   intent(in)    :: input_file
+  type(trfld),               pointer :: fields(:)
+  type(trfile),    intent(out)       :: file
+  character(len=*),intent(in)        :: input_file
   TYPE(read_interp),  intent(inout) :: xin   
-  type(physics_state), pointer       :: state(:)
+  type(physics_state),       pointer :: state(:)
   type(physics_buffer_desc), pointer :: pbuf2d(:,:)
  
   integer istat                 ! error return
@@ -61,8 +57,6 @@ subroutine read_data_flux (fields, file, input_file, xin, state, pbuf2d)
   allocate( xin%co2flx(pcols,begchunk:endchunk), stat=istat )
   call alloc_err( istat, 'CO2FLUX_READ', 'co2flx', &
        pcols*(endchunk-begchunk+1) )
- 
-
   allocate (file%in_pbuf(1))
   file%in_pbuf(1) = .false.
   specifier(1)    = 'CO2_flux' !name of variable to read from file
@@ -77,32 +71,33 @@ end subroutine read_data_flux
  
 !===============================================================================
 
-subroutine interp_time_flux (fields,file,xin, prev_timestep)
+subroutine interp_time_flux (fields, file,xin)
  
 !-----------------------------------------------------------------------
 ! Time interpolate data to current time.
 ! Reading in new monthly data if necessary.
 !
 !-----------------------------------------------------------------------
- 
-  type(trfld), pointer :: fields(:)
-  type(trfile)         :: file
+  use phys_grid,      only : get_ncols_p
+  use ppgrid,         only : begchunk, endchunk
 
-  logical, intent(in), optional     :: prev_timestep ! If using previous timestep, set to true
+  type(trfld),               pointer :: fields(:)
+  type(trfile), intent(inout)        :: file
   TYPE(read_interp),  intent(inout) :: xin
 
 !---------------------------Local variables-----------------------------
-  integer i,j,lchnk      ! indices
-  integer ncol           ! number of columns in current chunk
+  integer :: icol, lchnk      ! indices
+  integer :: ncol             ! number of columns in current chunk
 !-----------------------------------------------------------------------
 
   !Read next data if needed and interpolate (space and time)
   call advance_trcdata( fields, file)
 
-  do lchnk=begchunk,endchunk
-     ncol = get_ncols_p(lchnk)
-     do i=1,ncol
-        xin%co2flx(i,lchnk) = fields(1)%data(i,1,lchnk)
+  !Assign 
+  do lchnk   = begchunk, endchunk
+     ncol    = get_ncols_p(lchnk)
+     do icol = 1, ncol
+        xin%co2flx(icol,lchnk) = fields(1)%data(icol, 1,lchnk)
      end do
   end do
 
