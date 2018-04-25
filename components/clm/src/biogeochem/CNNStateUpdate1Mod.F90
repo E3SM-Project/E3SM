28c3d4b8e4a591628afda81283a8a158ea2a668b
Gautam Bisht <gbisht@lbl.gov>
2018-04-24 20:23:02 -0700

    Revert "Merge branch 'qzhu-lbl/lnd/ECA_parameter_clean' into next (PR #2220)"
    This reverts commit 2706c1a49fec9cb9ee48cbafef89366473542c55, reversing
    changes made to 411402c237c2933ad9dd2233c9c46669835d87aa.

--- components/clm/src/biogeochem/CNNStateUpdate1Mod.F90
+++ components/clm/src/biogeochem/CNNStateUpdate1Mod.F90
@@ -365,8 +365,7 @@ contains
          ! uptake from soil mineral N pool
          ns%npool_patch(p) = &
               ns%npool_patch(p) + nf%sminn_to_npool_patch(p)*dt
-         if (nu_com .ne. 'RD') ns%npool_patch(p) = ns%npool_patch(p) + nf%supplement_to_plantn(p)*dt + &
-                                                   nf%nfix_to_plantn_patch(p)*dt
+         if (nu_com .ne. 'RD') ns%npool_patch(p) = ns%npool_patch(p) + nf%supplement_to_plantn(p)*dt
 
          ! deployment from retranslocation pool
          ns%npool_patch(p)    = ns%npool_patch(p)    + nf%retransn_to_npool_patch(p)*dt
