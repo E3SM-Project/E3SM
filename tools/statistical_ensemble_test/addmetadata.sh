1d38871e52a7f6ca6d16e96dac78e2eaaa06c799
Jim Edwards <jedwards@ucar.edu>
2018-05-12 08:04:05 -0600

    a little flesh

--- tools/statistical_ensemble_test/addmetadata.sh
+++ tools/statistical_ensemble_test/addmetadata.sh
@@ -1,10 +1,39 @@
 #!/usr/bin/env bash
-caseroot=$1
-file=$2
+#
+# Adds metadata to netcdf statistical ensemble test files.
+#
+Args=("$@")
+i=0
+while [ $i -le ${#Args[@]} ]; do
+  case ${Args[$i]} in
+    --caseroot )
+      i=$((i+1))
+      caseroot=${Args[$i]}
+      if [ ! -d ${caseroot} ]; then
+        echo "ERROR: caseroot not found: $caseroot"
+        exit 2
+      fi
+      if [ ! -f ${caseroot}/xmlquery ]; then
+	echo "ERROR: Directory $caseroot does not appear to be a cesm case directory"
+	exit 3
+      fi
+    ;;
+    --histfile )
+      i=$((i+1))
+      histfile=${Args[$i]}
+      if [ ! -f ${histfile} ]; then
+	echo "ERROR: file not found $histfile"
+        exit 4
+      fi
+    ;;
+  esac
+  i=$((i+1))
+done
+
 cd $caseroot
 if hash ncks 2>/dev/null; then
- ncks --glb compset=`./xmlquery --value COMPSET` --glb grid=`./xmlquery --value GRID` --glb testtype="uf" --glb compiler=`./xmlquery --value COMPILER` --glb machineid=`./xmlquery --value MACH`  $file $file.tmp
-  mv $file.tmp $file
+ ncks --glb compset=`./xmlquery --value COMPSET` --glb grid=`./xmlquery --value GRID` --glb testtype="uf" --glb compiler=`./xmlquery --value COMPILER` --glb machineid=`./xmlquery --value MACH`  $histfile $histfile.tmp
+  mv $histfile.tmp $histfile
 else
   echo "This script requires the ncks tool"
 fi
