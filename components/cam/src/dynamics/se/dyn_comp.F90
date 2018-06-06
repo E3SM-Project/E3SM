3cd16ddd66ccc0b955c680dcb768e45b47472b15
Jason Sarich <sarich@mcs.anl.gov>
2018-06-06 15:39:16 -0500

    add in se changes to homme and cam that use moab driver

--- components/cam/src/dynamics/se/dyn_comp.F90
+++ components/cam/src/dynamics/se/dyn_comp.F90
@@ -101,6 +101,9 @@ CONTAINS
     use physics_buffer,   only: pbuf_add_field, dtype_r8
     use ppgrid,           only: pcols, pver
     use cam_abortutils,   only : endrun
+#ifdef HAVE_MOAB
+    use seq_comm_mct,      only: MHID, MHFID  ! id of homme moab coarse and fine applications
+#endif
 
     ! PARAMETERS:
     type(file_desc_t),   intent(in)  :: fh       ! PIO file handle for initial or restart file
@@ -111,6 +114,12 @@ CONTAINS
     integer :: neltmp(3)
     integer :: npes_se
     integer :: npes_se_stride
+
+#ifdef HAVE_MOAB
+    integer, external :: iMOAB_RegisterFortranApplication
+    integer :: ierr, ATM_ID1
+    character*32  appname
+#endif
 
     !----------------------------------------------------------------------
 
@@ -150,6 +159,28 @@ CONTAINS
 #endif
 
     if(par%dynproc) then
+#ifdef HAVE_MOAB
+       appname="HM_COARSE"//CHAR(0)
+       ATM_ID1 = 19
+       ierr = iMOAB_RegisterFortranApplication(appname, par%comm, ATM_ID1, MHID)
+       if (ierr > 0 )  &
+           call endrun('Error: cannot register moab app')
+       if(par%masterproc) then
+           write(iulog,*) " "
+           write(iulog,*) "register MOAB app:", trim(appname), "  MHID=", MHID
+           write(iulog,*) " "
+       endif
+       appname="HM_FINE"//CHAR(0)
+       ATM_ID1 = 119
+       ierr = iMOAB_RegisterFortranApplication(appname, par%comm, ATM_ID1, MHFID)
+       if (ierr > 0 )  &
+           call endrun('Error: cannot register moab app for fine mesh')
+       if(par%masterproc) then
+           write(iulog,*) " "
+           write(iulog,*) "register MOAB app:", trim(appname), "  MHFID=", MHFID
+           write(iulog,*) " "
+       endif
+#endif
        call prim_init1(elem,par,dom_mt,TimeLevel)
 
        dyn_in%elem => elem
