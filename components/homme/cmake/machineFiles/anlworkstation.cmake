18e88886882adfaebac199ee7c4abfb0e8785886
jayeshkrishna <jayesh@mcs.anl.gov>
2018-06-07 16:12:22 -0500

    Adding cmake build script for anlworkstation
    Adding the HOMME cmake build script for anlworkstation.

---
+++ components/homme/cmake/machineFiles/anlworkstation.cmake
@@ -0,0 +1,25 @@
+# anlworkstation
+
+SET (CMAKE_Fortran_COMPILER mpif90 CACHE FILEPATH "")
+SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
+SET (CMAKE_CXX_COMPILER mpicxx CACHE FILEPATH "")
+
+#SET (CMAKE_Fortran_FLAGS "-WF,-C! -O2 -Ipreqx_modules" CACHE STRING "")
+#SET (FORCE_Fortran_FLAGS "-WF,-C!" CACHE STRING "")
+SET (ENABLE_OPENMP TRUE CACHE BOOL "")
+
+SET (PNETCDF_DIR /soft/apps/packages/climate/pnetcdf/1.8.1/gcc-6.2.0 CACHE FILEPATH "")
+SET (NETCDF_DIR /soft/apps/packages/climate/netcdf/4.3.3.1c-4.2cxx-4.4.2f-parallel/gcc-6.2.0 CACHE FILEPATH "")
+SET (ENV{HDF5} /soft/apps/packages/climate/hdf5/1.8.16-parallel/gcc-6.2.0 CACHE FILEPATH "")
+#SET (ENV{LIBZ} /soft/libraries/alcf/current/xl/ZLIB CACHE FILEPATH "")
+SET (CPRNC_DIR /home/climate1/acme/cprnc/build/cprnc CACHE FILEPATH "")
+
+SET (USE_QUEUING FALSE CACHE BOOL "")
+SET (USE_MPIEXEC mpiexec CACHE FILEPATH "")
+
+SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
+#SET (ENV{PATH} "/soft/libraries/alcf/current/xl/BLAS/lib:/soft/libraries/alcf/current/xl/LAPACK/lib:$ENV{PATH}")
+SET (ENV{PATH} "/soft/apps/packages/climate/netcdf/4.3.3.1c-4.2cxx-4.4.2f-parallel:$ENV{PATH}")
+SET (ENV{LD_LIBRARY_PATH} "/soft/apps/packages/climate/hdf5/1.8.16-serial/gcc-6.2.0/lib:$ENV{LD_LIBRARY_PATH}")
+
+SET (BUILD_HOMME_SWEQX FALSE CACHE BOOL "")
