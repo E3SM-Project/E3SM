8338b248abaafa6bf84cfea09c0b26725fa01eda
Oksana Guba <oksana.guba@gmail.com>
2018-07-16 12:28:03 -0600

    more fixes for the prev. commit for tendencies

--- components/homme/src/share/namelist_mod.F90
+++ components/homme/src/share/namelist_mod.F90
@@ -809,7 +809,7 @@ module namelist_mod
 #ifndef CAM
 !standalone homme does not support ftype=1 (cause it is identical to ftype=0).
 !also, standalone ftype=0 is the same as standalone ftype=2.
-    if ((ftype == 0).or.(ftype == 2).or.(ftype == 3).or.(ftype == 4)) then
+    if ((ftype == 0).or.(ftype == 2).or.(ftype == 3).or.(ftype == 4).or.(ftype==5)) then
     else
        call abortmp('Standalone homme supports only se_ftype=0,2,3,4')
     endif
