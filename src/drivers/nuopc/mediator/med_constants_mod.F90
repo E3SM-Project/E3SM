acd68c398aaface55c3519351cc943df3decb2b7
Jim Edwards <jedwards@ucar.edu>
2018-07-18 09:24:12 -0600

    cleanup constants

--- src/drivers/nuopc/mediator/med_constants_mod.F90
+++ src/drivers/nuopc/mediator/med_constants_mod.F90
@@ -4,21 +4,18 @@ module med_constants_mod
   ! Mediator Internal State Datatype.
   !-----------------------------------------------------------------------------
 
-  use ESMF
+  use shr_kind_mod, only : R8=>shr_kind_r8
 
   implicit none
 
-  public
+  private
 
-  integer           , parameter :: med_constants_dbug_flag = 6
- !integer           , parameter :: med_constants_dbug_flag = 0
- !logical           , parameter :: med_constants_statewrite_flag = .true.
-  logical           , parameter :: med_constants_statewrite_flag = .false.
-  real(ESMF_KIND_R8), parameter :: med_constants_spval_init = 0.0_ESMF_KIND_R8  ! spval for initialization
-  real(ESMF_KIND_R8), parameter :: med_constants_spval = 0.0_ESMF_KIND_R8  ! spval
-  real(ESMF_KIND_R8), parameter :: med_constants_czero = 0.0_ESMF_KIND_R8  ! spval
-  integer           , parameter :: med_constants_ispval_mask = -987987     ! spval for RH mask values
-  character(len=*)  , parameter :: med_constants_spval_rhfile = 'SkiP_FilE'
+  integer,          public, parameter :: med_constants_dbug_flag = 0
+  logical,          public, parameter :: med_constants_statewrite_flag = .false.
+  real(R8),         public, parameter :: med_constants_spval_init = 0.0_R8  ! spval for initialization
+  real(R8),         public, parameter :: med_constants_spval = 0.0_R8  ! spval
+  real(R8),         public, parameter :: med_constants_czero = 0.0_R8  ! spval
+  integer,          public, parameter :: med_constants_ispval_mask = -987987     ! spval for RH mask values
 
   !-----------------------------------------------------------------------------
 
