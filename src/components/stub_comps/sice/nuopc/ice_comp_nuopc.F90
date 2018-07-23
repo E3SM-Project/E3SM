b43a3c7c0c959876f0a5a071dc5d5a09309d9095
Jim Edwards <jedwards@ucar.edu>
2018-07-23 10:46:59 -0600

    fix sice comp

--- src/components/stub_comps/sice/nuopc/ice_comp_nuopc.F90
+++ src/components/stub_comps/sice/nuopc/ice_comp_nuopc.F90
@@ -1,5 +1,15 @@
-module sice_comp_nuopc
+module ice_comp_nuopc
 
   ! This is only needed here to satisfy the current cime build requirements
+  public :: ICESetServices
 
-end module sice_comp_nuopc
+  contains
+
+  subroutine ICESetServices(gcomp, rc)
+      use ESMF, only : ESMF_GridComp
+      implicit none
+      type(ESMF_GridComp)  :: gcomp
+      integer, intent(out) :: rc
+    end subroutine ICESetServices
+
+end module ice_comp_nuopc
