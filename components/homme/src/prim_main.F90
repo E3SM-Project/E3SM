4d9ccbb59665dc999f17dba16b88d8eab13a9951
Luca Bertagna <lbertag@sandia.gov>
2018-08-08 17:39:27 -0600

    HOMME: Minor modifications to a few shared files
    Mainly 2 mods:
    
    - add a 'nextoutputstep' integer variable to the time_mod: this is useful
      for targets that need to perform some work to prepare for output (like
      copying from device to host).
    - split prim_init1 into several subroutines: this is a preqrequisite for
      kokkos targets (see upcoming commits) to work. In fact, kokkos targets
      will still rely on Fortran to perform most of the setup, and grab what
      they need from it. In particular, the kokkos targets:
        *) need to get the connectivity info from Fortran. Up to now, the
           structures GridEdge and MetaVertex were local to prim_init1, so
           I had to split the routine and expose those variables in the module
        *) does not need fortran to allocate buffers for the boundary exchange.
           the BE process is fully handled in C++.
    
    Also, the subroutine setup_element_pointers in element_mod should be moved.
    The openacc target should define its own prim_init1 routine, which uses
    most of the default subroutines prim_init1_xyz defined in prim_driver_base,
    but it would stick the pointers allocation/binding in between. You can find
    an example of this in the upcoming commit with the preqx_kokkos target.

--- components/homme/src/prim_main.F90
+++ components/homme/src/prim_main.F90
@@ -13,7 +13,7 @@ program prim_main
   use thread_mod,       only: nthreads, hthreads, vthreads, omp_get_thread_num, &
                               omp_set_num_threads, omp_get_nested, &
                               omp_get_num_threads, omp_get_max_threads
-  use time_mod,         only: tstep, nendstep, timelevel_t, TimeLevel_init
+  use time_mod,         only: tstep, nendstep, timelevel_t, TimeLevel_init, nstep=>nextOutputStep
   use dimensions_mod,   only: nelemd, qsize
   use control_mod,      only: restartfreq, vfile_mid, vfile_int, runtype, integration, statefreq, tstep_type
   use domain_mod,       only: domain1d_t, decompose
@@ -49,7 +49,6 @@ program prim_main
   integer nets,nete
   integer ithr
   integer ierr
-  integer nstep
   
   character (len=20) :: numproc_char
   character (len=20) :: numtrac_char
@@ -66,7 +65,7 @@ program prim_main
   ! =====================================
   if(par%masterproc) print *,"Primitive Equation Init1..."
   call t_initf('input.nl',LogPrint=par%masterproc, &
-	Mpicom=par%comm, MasterTask=par%masterproc)
+               Mpicom=par%comm, MasterTask=par%masterproc)
   call t_startf('Total')
   call t_startf('prim_init1')
   call prim_init1(elem,  par,dom_mt,tl)
