3e2b95e6035af2a34747abaf7b8ca62f88792fcf
zshaheen <zshaheen93@gmail.com>
2018-08-14 17:40:48 -0700

    Climo function converting to cdms2.TransientVariable correctly.

--- acme_diags/driver/utils/climo.py
+++ acme_diags/driver/utils/climo.py
@@ -1,4 +1,3 @@
-import copy
 import numpy as np
 import numpy.ma as ma
 import cdms2
@@ -10,6 +9,7 @@ def climo(var, season):
     Compute the climatology for var for the given season.
     The returned variable must be 2 dimensional.
     """
+    print('in climo.py')
     season_idx = {
         '01': [1,0,0,0,0,0,0,0,0,0,0,0],
         '02': [0,1,0,0,0,0,0,0,0,0,0,0],
@@ -32,6 +32,10 @@ def climo(var, season):
 
     # Redefine time to be in the middle of the time interval
     var_time = var.getTime()
+    if var_time is None:
+        # Climo cannot be ran on this variable.
+        return var
+
     tbounds = var_time.getBounds()
     var_time[:] = 0.5*(tbounds[:,0]+tbounds[:,1])
     var_time_absolute = var_time.asComponentTime()
@@ -48,7 +52,7 @@ def climo(var, season):
     elif season == 'SEASONALCYCLE':
         cycle = ['DJF','MAM','JJA', 'SON']
     else:
-        cycle = [ season ]
+        cycle = [season]
 
     ncycle = len(cycle)
     climo = ma.zeros([ncycle]+list(np.shape(v))[1:])
@@ -57,53 +61,26 @@ def climo(var, season):
                           for i in range(len(var_time_absolute)) ], dtype=np.int).nonzero()
         climo[n] = ma.average(v[idx], axis=0, weights=dt[idx])
 
-    print('in climo.py')
 
-    temp = cdms2.asVariable(climo)(squeeze=1)
-
-    temp.attributes = var.attributes
-    temp.units = var.units
-    temp.setGrid(var.getGrid())
-    temp.setAxis(0, var.getAxis(1))
-    temp.setAxis(1, var.getAxis(2))
-    return temp
-
+    trans_var = cdms2.createVariable(climo)(squeeze=1)
     """
-    t_var = copy.deepcopy(var)
-    t_var = t_var(squeeze=1)  # Doesn't do anything b/c shape is (6000, _, _)
-    #climo = climo(squeeze=1)
-    #t_var.data = climo.data
-    stuff = cdms2.asVariable(climo)(squeeze=1)
-    print(stuff.shape)
-    print(t_var._data.shape)
-    print(stuff._data.shape)
-    t_var._data = stuff._data
-    return t_var
-
-    #print(type(var))
-    #print('dir(climo)', dir(climo))
-    print(var.data.shape)
-    print(climo.data.shape)
-    var[:,] = climo
-    var = var[:1]
-    #var.data = climo.data
-    #quit()
-    return var
+    temp = cdms2.createVariable(
+                climo, grid=var.getGrid(),
+                axis=[var.getAxis(0), var.getAxis(1), var.getAxis(2), var.getAxis(3)],
+                id=var.id)(squeeze=1)
     """
-    '''
-    t_var = cdms2.asVariable(climo)
-    print(dir(t_var))
-    quit()
-    t_var = t_var(squeeze=1)
-    #print(type(t_var))
-    #print(dir(t_var))
-    t_var.attributes = var.attributes
-    t_var.units = var.units
-    print('t_var.getGrid()', t_var.getGrid())
-    t_var.setGrid(var.getGrid())
-    print('t_var.getGrid()', t_var.getGrid())
-    #print(type(t_var))
-    #print(type(t_var(squeeze=1)))
-    # return t_var(squeeze=1)
-    return t_var
-    '''
+    # Losing the grid after a squeeze is normal, we need to set it again.
+    trans_var.setGrid(var.getGrid())
+    # Set the correct axis as well.
+    trans_var.setAxis(0, var.getAxis(1))
+    trans_var.setAxis(1, var.getAxis(2))
+    if var.getLevel():
+        # If it's a 3D variable, set the last axis.
+        trans_var.setAxis(2, var.getAxis(3))
+    # Copy any missing attributes.
+    trans_var.attributes = var.attributes
+    trans_var.units = var.units
+    trans_var.id = var.id
+
+    return trans_var
+
