5d9522f14860ff3b31e5eb36da0d574ac44ded4b
Bill Sacks <sacks@ucar.edu>
2018-08-16 13:52:27 -0600

    Extract cs_status implementation into its own file
    This will make it possible to unit test this implementation

---
+++ scripts/lib/CIME/cs_status.py
@@ -0,0 +1,32 @@
+"""
+Implementation of the cs.status script, which prints the status of all
+of the tests in one or more test suites
+"""
+
+from CIME.XML.standard_module_setup import *
+from CIME.test_status import TestStatus
+import os
+
+def cs_status(test_paths, summary=False):
+    test_id_output = {}
+    for test_path in test_paths:
+        test_dir=os.path.dirname(test_path)
+        ts = TestStatus(test_dir=test_dir)
+        test_id = os.path.basename(test_dir).split(".")[-1]
+        test_name = ts.get_name()
+        status = ts.get_overall_test_status()
+        if not summary:
+            output = "  %s (Overall: %s) details:\n" % (test_name, status)
+            output += ts.phase_statuses_dump(prefix="    ")
+        else:
+            output = "  %s %s\n" % (status, test_name)
+
+        if test_id in test_id_output:
+            test_id_output[test_id] += output
+        else:
+            test_id_output[test_id] = output
+
+    for test_id in sorted(test_id_output):
+        print(test_id)
+        print(test_id_output[test_id])
+        print(' ')
