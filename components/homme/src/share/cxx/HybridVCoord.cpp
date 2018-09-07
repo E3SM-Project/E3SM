81d40cfd224d24f301d444f8ea8bf9da50fcb804
Luca Bertagna <lbertag@sandia.gov>
2018-09-07 13:11:58 -0600

    HOMME: change in how HybridVCoord is randomly initialized in C++
    Before we initialized ai, then kept trying to initialize bi checking that
    the constraint 'ai+bi be increasing' was satisfied. This could cause some
    hanging, in case the samples were not 'lucky'.
    Now we create ai and bi deterministically, and add some random noise (small
    enough so that the constraint 'ai+bi be increasing' is still satisfied).

--- components/homme/src/share/cxx/HybridVCoord.cpp
+++ components/homme/src/share/cxx/HybridVCoord.cpp
@@ -5,6 +5,7 @@
  *******************************************************************************/
 
 #include "HybridVCoord.hpp"
+#include "ErrorDefs.hpp"
 #include "utilities/TestUtils.hpp"
 
 #include <random>
@@ -53,8 +54,6 @@ void HybridVCoord::init(const Real ps0_in,
 }
 
 void HybridVCoord::random_init(int seed) {
-  const int min_value = std::numeric_limits<Real>::epsilon();
-  const int max_value = 1.0 - min_value;
   hybrid_ai = ExecViewManaged<Real[NUM_INTERFACE_LEV]>(
       "Hybrid a_interface coefs");
   hybrid_bi = ExecViewManaged<Real[NUM_INTERFACE_LEV]>(
@@ -63,48 +62,80 @@ void HybridVCoord::random_init(int seed) {
   std::mt19937_64 engine(seed);
   ps0 = 1.0;
 
-  // hybrid_a can technically range from 0 to 1 like hybrid_b,
-  // but doing so makes enforcing the monotonicity of p = a + b difficult
-  // So only go to 0.25
-  genRandArray(hybrid_ai, engine, std::uniform_real_distribution<Real>(
-                                      min_value, max_value / 4.0));
+  using urd = std::uniform_real_distribution<Real>;
 
-  HostViewManaged<Real[NUM_INTERFACE_LEV]> host_hybrid_ai(
-      "Host hybrid ai coefs");
-  Kokkos::deep_copy(host_hybrid_ai, hybrid_ai);
+  // eta=hybrid_a+hybrid_b increasing ensures p is monotonic (sufficient, not necessary cond).
+  // Also, hybrid_a and hybrid_b must be between 0 and 1.
+  // So easy way: generate uniform eta=a+b in [eta_min,1], with eta_min>0.
+  // That means eta(i) = i*delta_eta, with delta_eta = (1-eta_min)/NUM_PHYSICAL_LEV.
+  // Then pick a = c*b, with c<1 (say c=0.1). Then eta = b+c*b=(c+1)b,
+  // so b(i) = i*delta_eta/(c+1) and a(i) = c*i*delta_eta/(c+1).
+  // To add randomness, perturb sligthly: a(i)=a(i)+da and b(i)=b(i)+db,
+  // without violating eta(i+1)>eta(i). For instance, let da+db<delta_eta/2,
+  // which is true if da<delta_eta/4 and db<delta_eta/4.
+
+  decltype(hybrid_ai)::HostMirror host_hybrid_ai("Host hybrid ai coefs");
+  decltype(hybrid_bi)::HostMirror host_hybrid_bi("Host hybrid bi coefs");
+  const Real eta_min = 0.001;
+
+  // The proportionality constant between a and b.
+  Real c;
+  genRandArray(&c,1,engine,urd(0.0001,0.25));
+
+  // The eta uniform grid spacing
+  Real deta = (1-eta_min)/NUM_PHYSICAL_LEV;
+  Real perturbation = deta/5;
+
+  for (int i=0; i<NUM_INTERFACE_LEV; ++i) {
+    // Compute eta
+    Real eta_i = eta_min+deta*i;
+
+    // Create b and a
+    host_hybrid_bi(i) = eta_i/(c+1);
+    host_hybrid_ai(i) = c*host_hybrid_bi(i);
+
+    // Add some randomness to a and b
+    Real dab[2];
+    Real perturbation_adjusted = std::min(std::min(perturbation,eta_i),std::min(perturbation,1-eta_i));;
+
+    auto perturbation_dist = urd(-perturbation_adjusted,perturbation_adjusted);
+    genRandArray(dab,2,engine,perturbation_dist);
+    host_hybrid_ai(i) += dab[0];
+    host_hybrid_bi(i) += dab[1];
+
+    // Enforce 0<a,b<1
+    if (host_hybrid_ai(i)<0) {
+      host_hybrid_ai(i) = 0;
+    }
+    if (host_hybrid_bi(i)<0) {
+      host_hybrid_bi(i) = 0;
+    }
+    if (host_hybrid_ai(i)>1) {
+      host_hybrid_ai(i) = 1;
+    }
+    if (host_hybrid_bi(i)>1) {
+      host_hybrid_bi(i) = 1;
+    }
+  }
+
+  // Enforce b(0)=0, a(0)=eta_min, and b(end)=1, a(end)=0;
+  host_hybrid_ai(0) = eta_min;
+  host_hybrid_bi(0) = 0;
+  host_hybrid_ai(NUM_PHYSICAL_LEV) = 0;
+  host_hybrid_bi(NUM_PHYSICAL_LEV) = 1;
+  
+  // Safety check: a+b should be monotone here
+  for (int i=1; i<NUM_INTERFACE_LEV; ++i) {
+    Real curr = host_hybrid_ai(i) + host_hybrid_bi(i);
+    Real prev = host_hybrid_ai(i-1) + host_hybrid_bi(i-1);   
+
+    Errors::runtime_check(curr>prev,"Error! hybrid_a+hybrid_b is not increasing.\n", -1);
+  }
+
+  Kokkos::deep_copy(hybrid_ai, host_hybrid_ai);
+  Kokkos::deep_copy(hybrid_bi, host_hybrid_bi);
 
   hybrid_ai0 = host_hybrid_ai(0);
-
-  // p = a + b must be monotonically increasing
-  // OG: what is this for? does a test require it?
-  // (not critisizm, but i don't understand)
-  const auto check_coords = [=](
-      HostViewUnmanaged<Real[NUM_INTERFACE_LEV]> coords) {
-    // Enforce the boundaries
-    coords(0) = 0.0;
-    coords(1) = 1.0;
-    // Put them in order
-    std::sort(coords.data(), coords.data() + coords.size());
-    Real p_prev = hybrid_ai0 + coords(0);
-    for (int i = 1; i < NUM_INTERFACE_LEV; ++i) {
-      // Make certain they're all distinct
-      if (coords(i) <=
-          coords(i - 1) * (1.0 + std::numeric_limits<Real>::epsilon())) {
-        return false;
-      }
-      // Make certain p = a + b is increasing
-      Real p_cur = coords(i) + host_hybrid_ai(i);
-      if (p_cur <= p_prev) {
-        return false;
-      }
-      p_prev = p_cur;
-    }
-    // All good!
-    return true;
-  };
-  genRandArray(hybrid_bi, engine,
-               std::uniform_real_distribution<Real>(min_value, max_value),
-               check_coords);
 
   compute_deltas();
 }
