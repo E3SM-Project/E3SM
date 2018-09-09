c00bacdddde6908631d6b4d203faafdfcfdb220c
Jim Edwards <jedwards@ucar.edu>
2018-09-09 15:11:14 -0600

    fix issues with mosart and mom

--- src/components/data_comps/drof/nuopc/rof_comp_nuopc.F90
+++ src/components/data_comps/drof/nuopc/rof_comp_nuopc.F90
@@ -414,6 +414,7 @@ contains
   !===============================================================================
 
   subroutine ModelAdvance(gcomp, rc)
+    use shr_nuopc_utils_mod, only : shr_nuopc_memcheck
     type(ESMF_GridComp)  :: gcomp
     integer, intent(out) :: rc
 
