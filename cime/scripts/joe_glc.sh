5532686c5977994cd8ab5a6a41ada7c1bff8d9f9
Joseph H Kennedy <kennedyjh@ornl.gov>
2018-09-21 13:06:44 -0400

    Working glacier MEC script for NERSC

--- cime/scripts/joe_glc.sh
+++ cime/scripts/joe_glc.sh
@@ -20,6 +20,10 @@
 #
 # ./case.setup
 #
+# echo "hist_fincl1 = 'QICE_FORC', 'QSNOFRZ', 'SNO_BW'" >> user_nl_clm
+#
+# ./case.setup --reset
+#
 # ./case.build
 #
 # ./xmlchange STOP_N=32
@@ -31,7 +35,7 @@
 # ------
 # EDISON
 # ------
-case=$CSCRATCH/kennedyj/snow_joe_0
+case=$CSCRATCH/$USER/snow_joe_0
 
 ./create_newcase -case $case -compset IGCLM45_MLI -res f09_g16_a -project m1795
 
@@ -43,9 +47,17 @@ cd $case
 
 ./case.setup
 
+echo "hist_fincl1 = 'QICE_FORC', 'QSNOFRZ', 'SNO_BW'" >> user_nl_clm
+
+./case.setup --reset
+
 ./case.build
 
 ./xmlchange STOP_N=32
 ./xmlchange JOB_WALLCLOCK_TIME=02:30:00
 
-./case.submit
+./case.submit --skip-preview-namelist
+# --skip-preview-namelist b/c of NERSC perl issues:
+#    https://github.com/E3SM-Project/E3SM/issues/2539
+
+
