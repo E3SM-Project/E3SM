476477d495f5b0e921ce2f41aa36a7caae33c517
Balwinder Singh <balwindersingh@gmail.com>
2018-09-25 12:00:53 -0700

    Removes some debug statements from perturb module
    [BFB] - Bit-For-Bit
    
    See confluence for a more detailed description about these tags.

--- components/cam/src/physics/cam/module_perturb.F90
+++ components/cam/src/physics/cam/module_perturb.F90
@@ -5,14 +5,7 @@ module module_perturb
   implicit none
   
   integer :: kprnt = 61
-  integer :: fstream 
 contains
-
-  subroutine module_perturb_init
-    !stream for writing files for multi-instance 
-    fstream = 200 + inst_index
-
-  end subroutine module_perturb_init
 
   function icolprnt(lchnk) result(icol)
     integer, intent(in)  :: lchnk
@@ -20,6 +13,6 @@ contains
     integer :: icol
     icol = 0
     icol = phys_debug_col(lchnk)
-    !if(icol>0)write(*,*)'BALLI::icol,lchnk',icol,lchnk
+    !if(icol>0)print*,'BALLI::icol,lchnk',icol,lchnk
   end function icolprnt
 end module module_perturb
