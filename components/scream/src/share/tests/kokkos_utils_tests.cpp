214be138ff82c2c7e8b98b8f8fd4ac0a2a1eada5
Luca Bertagna <lbertag@sandia.gov>
2018-10-02 16:46:43 -0600

    Added utility to reshape views
    This functionality will be needed due to the fact that the
    field repository will only store 1d views. It will be up
    to the individual atm processes to store an unmanaged copy
    of the field with the proper layout (for ease and performance).

---
+++ components/scream/src/share/tests/kokkos_utils_tests.cpp
@@ -0,0 +1,32 @@
+#include "catch2/catch.hpp"
+
+#include "share/util/scream_kokkos_utils.hpp"
+
+namespace {
+
+TEST_CASE("data_type", "") {
+  using namespace scream::util;
+
+  // Check meta-util to get underlying scalar type of a raw MD array
+  REQUIRE(std::is_same<ScalarType<double**&>::type,double>::value);
+  REQUIRE(std::is_same<ScalarType<double*[3]>::type,double>::value);
+  REQUIRE(std::is_same<ScalarType<double[2][3]>::type,double>::value);
+
+  // Check meta-util to get rank and dynamic rank of a raw MD array
+  REQUIRE(GetRanks<double[2][3]>::rank==2);
+  REQUIRE(GetRanks<double[2][3]>::rank_dynamic==0);
+  REQUIRE(GetRanks<double*[2][3]>::rank==3);
+  REQUIRE(GetRanks<double*[2][3]>::rank_dynamic==1);
+  REQUIRE(GetRanks<double**[2][3]>::rank==4);
+  REQUIRE(GetRanks<double**[2][3]>::rank_dynamic==2);
+
+  // Check meta-util that allows to reshape a view
+  Kokkos::View<double*> v1d("",100);
+  auto v2d = reshape<double[2][50]>(v1d);
+  REQUIRE(v2d.size()==100);
+
+  auto v3d = reshape<double*[5][5]>(v2d,4);
+  REQUIRE (v3d.size()==100);
+}
+
+} // anonymous namespace
---
+++ components/scream/src/share/util/scream_kokkos_utils.hpp
@@ -0,0 +1,84 @@
+#ifndef SCREAM_KOKKOS_UTILS_HPP
+#define SCREAM_KOKKOS_UTILS_HPP
+
+#include <share/scream_kokkos_meta.hpp>
+
+#include <Kokkos_Core.hpp>
+
+#include <cassert>
+#include <type_traits>
+
+// This file should not be merged with share/scream_kokkos_meta.hpp.
+// That file contains functionalities that *ideally* should be in kokkos
+// itself, and we can foresee being in kokkos in the future. Hence, that
+// file may be gone at some point (in fact, it may be gone by the time
+// you read this comment). This file, instead, contains functionalities
+// that are probably not generic enough to appear in kokkos any time soon
+// (or ever), and are more app-specific.
+
+namespace scream {
+namespace util {
+
+// Tiny helper structures, since type_traits has remove_all_extents
+// but not remove_all_pointers, and lacks of something that remove
+// all pointers, extents and possibly reference
+template<typename T>
+struct remove_all_pointers {
+  using type = T;
+};
+template<typename T>
+struct remove_all_pointers<T*> {
+  using type = typename remove_all_pointers<T>::type;
+};
+
+template<typename MDArrayType>
+struct ScalarType {
+private:
+  using no_ref = typename std::remove_reference<MDArrayType>::type;
+  using no_arr = typename std::remove_all_extents<no_ref>::type;
+public:
+  using type = typename remove_all_pointers<no_arr>::type;
+};
+
+template<typename DataType>
+struct GetRanks {
+private:
+  using dimension = typename Kokkos::View<DataType>::dimension;
+public:
+  enum : int { rank = dimension::rank };
+  enum : int { rank_dynamic = dimension::rank_dynamic };
+};
+
+template<typename DataTypeOut, typename DataTypeIn, typename... Props>
+typename std::enable_if<GetRanks<DataTypeOut>::rank_dynamic==0,
+                        typename ko::Unmanaged<Kokkos::View<DataTypeOut,Props...>>>::type
+reshape (Kokkos::View<DataTypeIn,Props...> view_in) {
+  typename ko::Unmanaged<Kokkos::View<DataTypeOut,Props...>> view_out(view_in.data());
+  assert (view_in.size()==view_out.size());
+  return view_out;
+}
+
+template<typename DataTypeOut, typename DataTypeIn, typename... Props>
+typename std::enable_if<GetRanks<DataTypeOut>::rank_dynamic==1,
+                        typename ko::Unmanaged<Kokkos::View<DataTypeOut,Props...>>>::type
+reshape (Kokkos::View<DataTypeIn,Props...> view_in,
+         const int dim0) {
+  typename ko::Unmanaged<Kokkos::View<DataTypeOut,Props...>> view_out(view_in.data(),dim0);
+  assert (view_in.size()==view_out.size());
+  return view_out;
+}
+
+template<typename DataTypeOut, typename DataTypeIn, typename... Props>
+typename std::enable_if<GetRanks<DataTypeOut>::rank_dynamic==2,
+                        typename ko::Unmanaged<Kokkos::View<DataTypeOut,Props...>>>::type
+reshape (Kokkos::View<DataTypeIn,Props...> view_in,
+         const int dim0, const int dim1) {
+  typename ko::Unmanaged<Kokkos::View<DataTypeOut,Props...>> view_out(view_in.data(),dim0,dim1);
+  assert (view_in.size()==view_out.size());
+  return view_out;
+}
+
+} // namespace util
+} // namespace scream
+
+#endif // SCREAM_KOKKOS_UTILS_HPP
