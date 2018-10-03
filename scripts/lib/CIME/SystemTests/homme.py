c2e63ee52435c972abd1d21e08aa040e5fac9529
Luca Bertagna <lbertag@sandia.gov>
2018-10-03 14:29:43 -0600

    CIME: modifications to homme tests script
    Fixed handling of baselines: on machines that support strict
    (as of today, only sandiatoss and anvil), we need to have two
    separate trees for the baselines for 'fast' and 'strict' fp models.

--- scripts/lib/CIME/SystemTests/homme.py
+++ scripts/lib/CIME/SystemTests/homme.py
@@ -39,34 +39,36 @@ class HOMME(SystemTestsCommon):
                 basename = ""
                 baselinedir = exeroot
 
-            if (mach in ["sandiatoss3"]):
-                preqx_kokkos_args = "-DBUILD_HOMME_PREQX_KOKKOS=ON -DKOKKOS_PATH=/home/onguba/kokkos/build-omp-nodebug"
-            elif (mach in ["anvil"]):
-                preqx_kokkos_args = "-DBUILD_HOMME_PREQX_KOKKOS=ON -DKOKKOS_PATH=/home/onguba/kokkos/build-serial-omp-nodebug"
-            else :
-                preqx_kokkos_args = ""
+            if (mach in ["sandiatoss3", "anvil"]):
+                exeroot_fast = os.path.join(exeroot, "fast")
+                exeroot_strict = os.path.join(exeroot, "strict")
+
+                if (mach in ["sandiatoss3"]):
+                    preqx_kokkos_args = "-DBUILD_HOMME_PREQX_KOKKOS=ON -DKOKKOS_PATH=/home/onguba/kokkos/build-omp-nodebug"
+                else:
+                    preqx_kokkos_args = "-DBUILD_HOMME_PREQX_KOKKOS=ON -DKOKKOS_PATH=/home/onguba/kokkos/build-serial-omp-nodebug"
+
+                os.mkdir(exeroot_fast)
+                os.mkdir(exeroot_strict)
+
+                cmake_cmd_fast = "cmake -C {}/components/homme/cmake/machineFiles/{}.cmake -DUSE_NUM_PROCS={} {} {}/components/homme -DHOMME_TESTING_PROFILE=dev -DHOMME_BASELINE_DIR={}/{}/fast -DCPRNC_DIR={}/..".format(srcroot, mach, procs, preqx_kokkos_args, srcroot, baselinedir, basename, cprnc)
+                cmake_cmd_strict = "cmake -C {}/components/homme/cmake/machineFiles/{}-strict.cmake -DUSE_NUM_PROCS={} {} {}/components/homme -DHOMME_TESTING_PROFILE=dev -DHOMME_BASELINE_DIR={}/{}/strict -DCPRNC_DIR={}/..".format(srcroot, mach, procs, preqx_kokkos_args, srcroot, baselinedir, basename, cprnc)
 
 
-            cmake_cmd_fast = "cmake -C {}/components/homme/cmake/machineFiles/{}.cmake -DUSE_NUM_PROCS={} {} {}/components/homme -DHOMME_TESTING_PROFILE=dev -DHOMME_BASELINE_DIR={}/{} -DCPRNC_DIR={}/..".format(srcroot, mach, procs, preqx_kokkos_args, srcroot, baselinedir, basename, cprnc)
-            cmake_cmd_strict = "cmake -C {}/components/homme/cmake/machineFiles/{}-strict.cmake -DUSE_NUM_PROCS={} {} {}/components/homme -DHOMME_TESTING_PROFILE=dev -DHOMME_BASELINE_DIR={}/{} -DCPRNC_DIR={}/..".format(srcroot, mach, procs, preqx_kokkos_args, srcroot, baselinedir, basename, cprnc)
+                run_cmd_no_fail(cmake_cmd_fast, arg_stdout=os.path.join(exeroot, "homme.bldlog"), combine_output=True, from_dir=exeroot_fast)
+                run_cmd_no_fail(cmake_cmd_strict, arg_stdout=os.path.join(exeroot, "homme.bldlog"), combine_output=True, from_dir=exeroot_strict)
 
-            exeroot_fast = os.path.join(exeroot, "fast")
-            exeroot_strict = os.path.join(exeroot, "strict")
+                run_cmd_no_fail("{} -j8".format(gmake), arg_stdout=os.path.join(exeroot, "homme.bldlog"), combine_output=True, from_dir=exeroot_fast)
+                post_build(self._case, [os.path.join(exeroot, "homme.bldlog")], build_complete=True)
 
-            os.mkdir(exeroot_fast)
-            os.mkdir(exeroot_strict)
-            run_cmd_no_fail(cmake_cmd_fast, arg_stdout=os.path.join(exeroot_fast, "homme.bldlog"), combine_output=True, from_dir=exeroot_fast)
-            if (mach in ["anvil", "sandiatoss3"]):
-                run_cmd_no_fail(cmake_cmd_strict, arg_stdout=os.path.join(exeroot_strict, "homme.bldlog"), combine_output=True, from_dir=exeroot_strict)
+                run_cmd_no_fail("{} -j8".format(gmake), arg_stdout=os.path.join(exeroot, "homme.bldlog"), combine_output=True, from_dir=exeroot_strict)
+                post_build(self._case, [os.path.join(exeroot, "homme.bldlog")], build_complete=True)
+            else:
+                cmake_cmd = "cmake -C {}/components/homme/cmake/machineFiles/{}.cmake -DUSE_NUM_PROCS={} {}/components/homme -DHOMME_BASELINE_DIR={}/{} -DCPRNC_DIR={}/..".format(srcroot, mach, procs, srcroot, baselinedir, basename, cprnc)
 
-            run_cmd_no_fail("{} -j8".format(gmake), arg_stdout=os.path.join(exeroot_fast, "homme.bldlog"), combine_output=True, from_dir=exeroot_fast)
-            if (mach in ["anvil", "sandiatoss3"]):
-                run_cmd_no_fail("{} -j8".format(gmake), arg_stdout=os.path.join(exeroot_strict, "homme.bldlog"), combine_output=True, from_dir=exeroot_strict)
-
-            post_build(self._case, [os.path.join(exeroot_fast, "homme.bldlog")], build_complete=True)
-            if (mach in ["anvil", "sandiatoss3"]):
-                post_build(self._case, [os.path.join(exeroot_strict, "homme.bldlog")], build_complete=True)
-
+                run_cmd_no_fail(cmake_cmd, arg_stdout=os.path.join(exeroot, "homme.bldlog"), combine_output=True, from_dir=exeroot)
+                run_cmd_no_fail("{} -j8".format(gmake), arg_stdout=os.path.join(exeroot, "homme.bldlog"), combine_output=True, from_dir=exeroot)
+                post_build(self._case, [os.path.join(exeroot, "homme.bldlog")], build_complete=True)
     def run_phase(self):
 
         rundir   = self._case.get_value("RUNDIR")
@@ -82,37 +84,52 @@ class HOMME(SystemTestsCommon):
         if os.path.exists(log):
             os.remove(log)
 
-        exeroot_fast = os.path.join(exeroot, "fast")
-        exeroot_strict = os.path.join(exeroot, "strict")
+        if (mach in ["anvil", "sandiatoss3"]):
+            exeroot_fast = os.path.join(exeroot, "fast")
+            exeroot_strict = os.path.join(exeroot, "strict")
 
-        if generate:
-            full_baseline_dir = os.path.join(baseline, basegen, "tests", "baseline")
-            stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
-            if (mach in ["anvil", "sandiatoss3"]):
-                stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
-            if stat == 0:
-                if os.path.isdir(full_baseline_dir):
-                    shutil.rmtree(full_baseline_dir)
+            if generate:
+                stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
+                stat = stat + run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
+                if stat == 0:
+                    full_baseline_fast_dir = os.path.join(baseline, basegen, "fast", "tests", "baseline")
+                    full_baseline_strict_dir = os.path.join(baseline, basegen, "strict", "tests", "baseline")
+                    if os.path.isdir(full_baseline_fast_dir):
+                        shutil.rmtree(full_baseline_fast_dir)
+                    if os.path.isdir(full_baseline_strict_dir):
+                        shutil.rmtree(full_baseline_strict_dir)
 
-                shutil.copytree(os.path.join(exeroot_fast, "tests", "baseline"), full_baseline_dir)
-                if (mach in ["anvil", "sandiatoss3"]):
-                    shutil.copytree(os.path.join(exeroot_strict, "tests", "baseline"), full_baseline_dir)
+                    shutil.copytree(os.path.join(exeroot_fast, "tests", "baseline"), full_baseline_fast_dir)
+                    shutil.copytree(os.path.join(exeroot_strict, "tests", "baseline"), full_baseline_strict_dir)
 
-        elif compare:
-            stat = run_cmd("ctest -E cxx", arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
-            if (mach in ["anvil", "sandiatoss3"]):
-                stat = run_cmd("ctest", arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
+            elif compare:
+                stat = run_cmd("ctest -E cxx", arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
+                stat = stat + run_cmd("ctest", arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
 
-        else:
-            stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
-            if (mach in ["anvil", "sandiatoss3"]):
-                stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
+            else:
+                stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
+                stat = stat + run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
 
-            stat = run_cmd("ctest -E cxx", arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
-            if (mach in ["anvil", "sandiatoss3"]):
+                stat = run_cmd("ctest -E cxx", arg_stdout=log, combine_output=True, from_dir=exeroot_fast)[0]
                 # Only run the dev and unit profiles. This excludes all theta/sweqx/baro tests.
                 # That's what we want: we only want to test cxx vs f90 impls, so preqx vs preqx_kokkos and unit tests.
-                stat = run_cmd("ctest -L 'dev|unit'", arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
+                stat = stat + run_cmd("ctest -L 'dev|unit'", arg_stdout=log, combine_output=True, from_dir=exeroot_strict)[0]
+        else:
+            if generate:
+                full_baseline_dir = os.path.join(baseline, basegen, "tests", "baseline")
+                stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot)[0]
+                if stat == 0:
+                    if os.path.isdir(full_baseline_dir):
+                        shutil.rmtree(full_baseline_dir)
+
+                    shutil.copytree(os.path.join(exeroot_fast, "tests", "baseline"), full_baseline_dir)
+
+            elif compare:
+                stat = run_cmd("ctest -E cxx", arg_stdout=log, combine_output=True, from_dir=exeroot)[0]
+
+            else:
+                stat = run_cmd("{} -j 4 baseline".format(gmake), arg_stdout=log, combine_output=True, from_dir=exeroot)[0]
+                stat = run_cmd("ctest -E cxx", arg_stdout=log, combine_output=True, from_dir=exeroot)[0]
 
         # Add homme.log output to TestStatus.log so that it can
         # appear on the dashboard. Otherwise, the TestStatus.log
