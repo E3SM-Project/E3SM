c2ecd1792cfe142ac3bce8ddfda387ff6aa9c2f3
Luca Bertagna <lbertag@sandia.gov>
2018-10-05 11:55:45 -0600

    Fixing dynamics and coupling processes.
    They missed the impl of some pure virtual methods.

--- components/scream/src/control/surface_coupling.hpp
+++ components/scream/src/control/surface_coupling.hpp
@@ -16,7 +16,7 @@ public:
   // The type of the block (dynamics or physics)
   AtmosphereProcessType type () const { return AtmosphereProcessType::Coupling; }
 
-  std::string name () const { return "scream::surface_coupling"; }
+  std::string name () const { return "surface_coupling"; }
 
   // The communicator associated with this atm process
   const Comm& get_comm () const { return m_surface_coupling_comm; }
@@ -32,7 +32,14 @@ public:
   // Clean up
   void finalize ( /* inputs */ );
 
+  // Providing a list of required and computed fields
+  const std::list<std::string>&  get_required_fields () const { return m_fields_to_import; }
+  const std::list<std::string>&  get_computed_fields () const { return m_fields_to_export; }
+
 protected:
+
+  std::list<std::string> m_fields_to_export;
+  std::list<std::string> m_fields_to_import;
 
   field_repo_type<ExecViewManaged<Real*>>   m_device_field_repo;
   field_repo_type<HostViewManaged<Real*>>   m_host_field_repo;
--- components/scream/src/dynamics/atmosphere_dynamics.hpp
+++ components/scream/src/dynamics/atmosphere_dynamics.hpp
@@ -22,13 +22,12 @@ namespace scream
 class AtmosphereDynamics : public AtmosphereProcess
 {
 public:
-  AtmosphereDynamics () = default;
 
   // The type of the subcomponent (dynamics or physics)
   AtmosphereProcessType type () const { return AtmosphereProcessType::Dynamics; }
 
   // The name of the subcomponent 
-  std::string name () const { return "scream::dynamics"; }
+  std::string name () const { return "dynamics"; }
 
   // The communicator used by the dynamics
   const Comm& get_comm () const { return m_dynamics_comm; }
@@ -40,10 +39,13 @@ public:
 
   // These two methods allow the driver to figure out what subcomponent need
   // a given field and what subcomponent updates a given field.
-  const std::list<std::string>&  get_required_fields () const;
-  const std::list<std::string>&  get_computed_fields () const;
+  const std::list<std::string>&  get_required_fields () const { return m_input_fields;  }
+  const std::list<std::string>&  get_computed_fields () const { return m_output_fields; }
 
 protected:
+
+  std::list<std::string> m_input_fields;
+  std::list<std::string> m_output_fields;
 
   Comm      m_dynamics_comm;
 
