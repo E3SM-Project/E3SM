a41acc3f8749dfcdb2236a37b81d81d3f0bbc0e6
kdraeder <raeder@ucar.edu>
2018-10-06 17:58:34 -0600

    Add and fix description of write_hist_alarm + hr2x write schedule.

--- driver-mct/main/cime_comp_mod.F90
+++ driver-mct/main/cime_comp_mod.F90
@@ -269,7 +269,7 @@ module cime_comp_mod
   logical  :: t24hr_alarm            ! alarm every twentyfour hours
   logical  :: t1yr_alarm             ! alarm every year, at start of year
   logical  :: pause_alarm            ! pause alarm
-  logical  :: write_hist_alarm       ! seq_timemgr index for driver
+  logical  :: write_hist_alarm       ! alarm to write a history file under multiple conditions
   integer  :: drv_index              ! seq_timemgr index for driver
 
   real(r8) :: days_per_year = 365.0  ! days per year
@@ -3088,6 +3088,8 @@ contains
              if (drv_threading) call seq_comm_setnthreads(nthreads_CPLID)
              if (do_hist_r2x) then
                 call t_drvstartf ('driver_rofpost_histaux', barrier=mpicom_CPLID)
+                ! Write coupler's hr2x file at 24 hour marks, 
+                ! and at the end of the forecast, even if that's not at a 24 hour mark.
                 write_hist_alarm = t24hr_alarm .or. stop_alarm
                 do eri = 1,num_inst_rof
                    inst_suffix =  component_get_suffix(rof(eri))
