77e93fda8583abbdb8c5c15941509facd9108fb2
Mark Taylor <mt@mp3dev.org>
2018-10-12 10:39:29 -0500

    add PS contour plots to TC test case

--- components/homme/dcmip_tests/dcmip2016_test2_tropical_cyclone/theta-l/jobscript-snl.sh
+++ components/homme/dcmip_tests/dcmip2016_test2_tropical_cyclone/theta-l/jobscript-snl.sh
@@ -42,14 +42,14 @@ date
 ncl plot-tropical-cyclone-init.ncl  # u,t,th,q,pnh,geo,ps, time=0
 ncl plot-horiz-crossx.ncl     # contour plot, time=10d, U,V,T,ps,precl,Q,geo
 ncl plot-intensity-trace.ncl
-
+ncl plot-horis-ps.ncl
 # save output
 \mv -f movies/dcmip2016_test21.nc   movies/${prefix}_dcmip2016_test21.nc
 
 \mv -f init.pdf ${prefix}_init.pdf
 \mv -f x-sections.pdf ${prefix}_x-sections.pdf
 \mv -f wind.pdf ${prefix}_wind.pdf
-\mv -f ps.pdf ${prefix}_ps.pdf
+\mv -f psmap.pdf ${prefix}_psmap.pdf
 }
 
 
