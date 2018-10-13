9a356c9664f5c80756dc937ea39cba4aed253f5a
Mark Taylor <mt@mp3dev.org>
2018-10-12 20:30:05 -0500

    add ps contour plots to theta model, dcmip2016 test2

--- components/homme/dcmip_tests/dcmip2016_test2_tropical_cyclone/preqx/jobscript-snl.sh
+++ components/homme/dcmip_tests/dcmip2016_test2_tropical_cyclone/preqx/jobscript-snl.sh
@@ -42,6 +42,7 @@ date
 ncl plot-tropical-cyclone-init.ncl  # u,t,th,q,pnh,geo,ps, time=0
 ncl plot-horiz-crossx.ncl     # contour plot, time=10d, U,V,T,ps,precl,Q,geo
 ncl plot-intensity-trace.ncl
+ncl plot-horiz-ps.ncl
 
 # save output
 \mv -f movies/dcmip2016_test21.nc   movies/${prefix}_dcmip2016_test21.nc
@@ -50,6 +51,7 @@ ncl plot-intensity-trace.ncl
 \mv -f x-sections.pdf ${prefix}_x-sections.pdf
 \mv -f wind.pdf ${prefix}_wind.pdf
 \mv -f ps.pdf ${prefix}_ps.pdf
+\mv -f psmap.pdf ${prefix}_psmap.pdf
 }
 
 
