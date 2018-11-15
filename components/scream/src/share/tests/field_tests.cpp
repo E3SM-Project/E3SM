eb89e895abd6736385e922acf07eb3b32c9e5bca
Luca Bertagna <lbertag@sandia.gov>
2018-11-14 18:12:49 -0700

    Minor work on field-related stuff unit testing.

--- components/scream/src/share/tests/field_tests.cpp
+++ components/scream/src/share/tests/field_tests.cpp
@@ -1,34 +1,26 @@
 #include "catch2/catch.hpp"
 
-#include "share/field_header.hpp"
-#include "share/field.hpp"
-#include "share/field_repository.hpp"
+#include <share/field/field_identifier.hpp>
+#include <share/field/field_header.hpp>
+#include <share/field/field.hpp>
+#include <share/field/field_repository.hpp>
 
 namespace {
 
-TEST_CASE("field_header", "") {
+TEST_CASE("field_identifier", "") {
   using namespace scream;
 
   std::vector<FieldTag> tags1 = {FieldTag::Element, FieldTag::GaussPoint, FieldTag::GaussPoint};
   std::vector<FieldTag> tags2 = {FieldTag::Element, FieldTag::Component, FieldTag::Level};
-  std::vector<FieldTag> tags3 = {FieldTag::Column, FieldTag::Level};
 
-  std::vector<int> dims1 = {2, 3, 4};
-  std::vector<int> dims2 = {2, 3, 3};
-  std::vector<int> dims3 = {100, 72};
+  FieldIdentifier fid1 ("field_1", tags1);
+  FieldIdentifier fid2 ("field_1", tags1);
+  FieldIdentifier fid3 ("field_1", tags2);
+  FieldIdentifier fid4 ("field_2", tags2);
 
-  FieldHeader fh1 ("field_1", tags1, {2, 3, 4});
-  FieldHeader fh2 ("field_1", tags1);
-  fh2.set_dimensions(dims1);
-  FieldHeader fh3 ("field_1", tags2, dims1);
-  FieldHeader fh4 ("field_2", tags2, dims1);
-  FieldHeader fh5 ("field_2", tags2);
-  fh5.set_dimensions({2,3,3});
-
-  REQUIRE (fh1==fh2);
-  REQUIRE (fh2!=fh3);
-  REQUIRE (fh3!=fh4);
-  REQUIRE (fh4!=fh5);
+  REQUIRE (fid1==fid2);
+  REQUIRE (fid2!=fid3);
+  REQUIRE (fid3!=fid4);
 }
 
 TEST_CASE("field_repo", "") {
@@ -36,31 +28,26 @@ TEST_CASE("field_repo", "") {
 
   std::vector<FieldTag> tags1 = {FieldTag::Element, FieldTag::GaussPoint, FieldTag::GaussPoint};
   std::vector<FieldTag> tags2 = {FieldTag::Element, FieldTag::Component, FieldTag::Level};
-  std::vector<FieldTag> tags3 = {FieldTag::Column, FieldTag::Level};
+
+  FieldIdentifier fid1("field_1", tags1);
+  FieldIdentifier fid2("field_2", tags1);
 
   std::vector<int> dims1 = {2, 3, 4};
   std::vector<int> dims2 = {2, 3, 3};
-  std::vector<int> dims3 = {100, 72};
 
-  std::shared_ptr<FieldHeader> fh1 = std::make_shared<FieldHeader>("field_1", tags1, dims1);
-  std::shared_ptr<FieldHeader> fh2 = std::make_shared<FieldHeader>("field_1", tags1, dims2);
-  std::shared_ptr<FieldHeader> fh3 = std::make_shared<FieldHeader>("field_2", tags1, dims1);
-  std::shared_ptr<FieldHeader> fh4 = std::make_shared<FieldHeader>("field_2", tags2, dims1);
+  fid1.set_dimensions(dims1);
+  fid2.set_dimensions(dims2);
 
   FieldRepository<ExecMemSpace>  repo_dev;
-  auto f1 = repo_dev.register_field(fh1);
-  auto f2 = repo_dev.register_field(fh2);
+  auto f1 = repo_dev.register_field(fid1);
+  auto f2 = repo_dev.register_field(fid2);
   repo_dev.registration_complete();
 
-  // Check the two fields headers are different
-  REQUIRE (f1.header()!=f2.header());
+  // Check the two fields identifiers are indeed different
+  REQUIRE (f1.get_header().get_identifier()!=f2.get_header().get_identifier());
 
-  // Reshape a field, and check it went well;
-  Field<Real[2][3][4],ExecMemSpace,false> f1_shaped(f1);
-  REQUIRE (f1_shaped.header()==f1.header());
-  REQUIRE (decltype(f1)::view_type::Rank==1);
-  REQUIRE (decltype(f1_shaped)::view_type::Rank==3);
-  REQUIRE (f1.view().size()==f1_shaped.view().size());
+  // Check registration is indeed closed
+  REQUIRE (!repo_dev.is_registration_open());
 }
 
 } // anonymous namespace
