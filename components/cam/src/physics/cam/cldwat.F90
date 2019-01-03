e5ef0f66d7642bd3e163104ecdeb43d338b58419
Shixuan Zhang <zhan391@constance04.pnl.gov>
2019-01-03 13:46:50 -0800

    Add a new option in the condensation:
    when ltend <0 then termB =-(1-f)*ltend = 0

--- components/cam/src/physics/cam/cldwat.F90
+++ components/cam/src/physics/cam/cldwat.F90
@@ -582,8 +582,15 @@ subroutine pcond (lchnk   ,ncol    , &
 
                   ! Q=C-E=-C1*Al + C2*Aq - C3* At + C4*Er
   
-                  cme(i,k) = -cmec1(i)*lctend(i,k) + cmec2(i)*qtend(i,k)  &
-                             -cmec3(i)*ttend(i,k) + cmec4(i)*evapprec(i,k)
+                  if( ql_incld_opt >= 1 ) then
+
+                   cme(i,k) = -cmec1(i)*max(0._r8,lctend(i,k)) + cmec2(i)*qtend(i,k)  &
+                              -cmec3(i)*ttend(i,k) + cmec4(i)*evapprec(i,k)
+                  else
+                   cme(i,k) = -cmec1(i)*lctend(i,k) + cmec2(i)*qtend(i,k)  &
+                              -cmec3(i)*ttend(i,k) + cmec4(i)*evapprec(i,k)
+                  end if 
+
                endif
 
            ! 3. when rh < rhu00, evaporate existing cloud water
