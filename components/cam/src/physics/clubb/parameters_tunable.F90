3b91a61ca8f5baed376fe126d1d69a40b1fd4f77
Balwinder Singh <balwindersingh@gmail.com>
2019-01-08 11:15:40 -0600

    Adds missing block for CLUBB namelist variables

--- components/cam/src/physics/clubb/parameters_tunable.F90
+++ components/cam/src/physics/clubb/parameters_tunable.F90
@@ -1076,6 +1076,45 @@ module parameters_tunable
 
     end if
 
+    if (clubb_C1 /= init_value) then
+       C1 = clubb_C1
+       C1b = C1
+    end if
+    ! if clubb_C2thl and clubb_C2rtthl not specified, continue to use C2thl=C2rt, C2rtthl = 1.3*C2rt
+    ! to preserve existing compsets that have assumed so and only vary C2rt
+    if (clubb_C2rt /= init_value) then
+       C2rt = clubb_C2rt
+       if (clubb_C2thl == init_value) C2thl = C2rt
+       if (clubb_C2rtthl == init_value) C2rtthl = C2rt*1.3_core_rknd
+    end if
+    ! Allows C2thl and C2rtthl to vary separately
+    if (clubb_C2thl /= init_value) C2thl = clubb_C2thl
+    if (clubb_C2rtthl /= init_value) C2rtthl = clubb_C2rtthl
+    if (clubb_C6rt /= init_value) then
+       C6rt = clubb_C6rt
+       C6thl = C6rt
+    end if
+    if (clubb_C6rtb /= init_value) C6rtb = clubb_C6rtb
+    if (clubb_C7 /= init_value) C7 = clubb_C7
+    if (clubb_C7b /= init_value) C7b = clubb_C7b
+    if (clubb_C8 /= init_value) C8 = clubb_C8
+    if (clubb_C11 /= init_value) C11 = clubb_C11
+    if (clubb_C11b /= init_value) C11b = clubb_C11b
+    if (clubb_C14 /= init_value) C14 = clubb_C14
+    if (clubb_beta /= init_value) beta = clubb_beta
+    ! if clubb_gamma_coefb not specified, continue to use gamma_coefb=gamma_coef
+    ! to preserve existing compsets that have assumed so  and only vary gamma_coef
+    if (clubb_gamma_coef /= init_value) then
+       gamma_coef = clubb_gamma_coef
+       if (clubb_gamma_coefb == init_value) gamma_coefb = gamma_coef
+    end if
+    ! Allows gamma_coefb to vary separately
+    if (clubb_gamma_coefb /= init_value) gamma_coefb = clubb_gamma_coefb
+    if (clubb_mu /= init_value) mu = clubb_mu
+    if (clubb_nu1 /= init_value) nu1 = clubb_nu1
+    if (clubb_c_K10 /= init_value) c_K10 = clubb_c_K10
+    if (clubb_wpxp_L_thresh /= init_value)wpxp_L_thresh = clubb_wpxp_L_thresh
+
     ! Put the variables in the output array
     call pack_parameters &
              ( C1, C1b, C1c, C2, C2b, C2c, C2rt, C2thl, C2rtthl, &
