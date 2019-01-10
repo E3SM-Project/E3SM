f1e23ad6841edb22cbde9638d96e3c85f017b191
Michael Brunke <brunke@atmo.arizona.edu>
2019-01-10 17:15:32 -0500

    Re-calculating downscaled potential temp to fix non-BFB

--- components/clm/src/cpl/lnd_disagg_forc.F90
+++ components/clm/src/cpl/lnd_disagg_forc.F90
@@ -135,7 +135,8 @@ contains
          !         = tbot_c * (exp(zbot_c/Hbot))^(rair/cpair)
          !         = tbot_c * exp((zbot_c/Hbot) * (rair/cpair))
 
-         thbot_t= tbot_t*exp((zbot_t/Hbot)*(rair/cpair))  ! pot temp calc
+!         thbot_t= tbot_t*exp((zbot_t/Hbot)*(rair/cpair))  ! pot temp calc
+	 thbot_t = tbot_t*(100000./pbot_t)**(rair/cpair)
 
          call Qsat(tbot_g,pbot_g,es_g,dum1,qs_g,dum2)
          call Qsat(tbot_t,pbot_t,es_t,dum1,qs_t,dum2)
@@ -145,7 +146,7 @@ contains
          rhos_t = (pbot_t-0.378*egcm_t) / (rair*tbot_t)
 
        top_as%tbot(t) = tbot_t
-!       top_as%thbot(t) = thbot_t
+       top_as%thbot(t) = thbot_t
 !	 top_as%vp_atm(t) = es_t
        top_as%qbot(t) = qbot_t
        top_as%pbot(t) = pbot_t
