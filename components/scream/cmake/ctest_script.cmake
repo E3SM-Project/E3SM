f4ef8aab8b84f53e281160922bfed8644fcb5f1b
James Foucar <jgfouca@sandia.gov>
2019-02-08 16:33:50 -0700

    Minor mods to facilitate interaction with scream-test-all

--- components/scream/cmake/ctest_script.cmake
+++ components/scream/cmake/ctest_script.cmake
@@ -1,5 +1,7 @@
 cmake_minimum_required(VERSION 3.10)
 
+set(CTEST_BUILD_NAME "scream_unit_tests${BUILD_NAME_MOD}")
+
 get_filename_component(working_dir ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 set(CTEST_SOURCE_DIRECTORY "${working_dir}")
 set(CTEST_BINARY_DIRECTORY "${working_dir}/ctest-build")
@@ -8,7 +10,7 @@ if(NOT DEFINED dashboard_model)
   set(dashboard_model Experimental)
 endif()
 If(NOT DEFINED dashboard_track)
-  set(dashboard_track Experimental)
+  set(dashboard_track E3SM_SCREAM)
 endif()
 
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
@@ -24,12 +26,16 @@ ctest_build(CAPTURE_CMAKE_ERROR build_ctest_error RETURN_VALUE build_error FLAGS
 message("build error           = ${build_error}")
 message("build ctest error     = ${build_ctest_error}")
 
-execute_process(COMMAND make baseline WORKING_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
+if (NOT BUILD_ONLY)
+  execute_process(COMMAND make baseline WORKING_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
 
-ctest_test(PARALLEL_LEVEL ${PARALLEL_LEVEL} CAPTURE_CMAKE_ERROR test_ctest_error RETURN_VALUE test_error)
-message("test error            = ${test_error}")
-message("test ctest error      = ${test_ctest_error}")
+  ctest_test(PARALLEL_LEVEL ${PARALLEL_LEVEL} CAPTURE_CMAKE_ERROR test_ctest_error RETURN_VALUE test_error)
+  message("test error            = ${test_error}")
+  message("test ctest error      = ${test_ctest_error}")
+endif()
 
-ctest_submit(RETRY_COUNT 10 RETRY_DELAY 60 CAPTURE_CMAKE_ERROR submit_ctest_error RETURN_VALUE submit_error)
-message("submit error            = ${submit_error}")
-message("submit ctest error      = ${submit_ctest_error}")
+if (NOT NO_SUBMIT)
+  ctest_submit(RETRY_COUNT 10 RETRY_DELAY 60 CAPTURE_CMAKE_ERROR submit_ctest_error RETURN_VALUE submit_error)
+  message("submit error            = ${submit_error}")
+  message("submit ctest error      = ${submit_ctest_error}")
+endif()
