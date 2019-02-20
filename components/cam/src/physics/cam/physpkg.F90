3382111c0852e593dc147a82a20dbfc851f70369
Balwinder Singh <balwindersingh@gmail.com>
2019-02-20 17:41:54 -0600

    Adds vmag_gust to infld calls in physpkg.F90
    [BFB] - Bit-For-Bit

--- components/cam/src/physics/cam/physpkg.F90
+++ components/cam/src/physics/cam/physpkg.F90
@@ -430,6 +430,17 @@ subroutine phys_inidat( cam_out, pbuf2d )
     tpert_idx = pbuf_get_index( 'tpert')
     call pbuf_set_field(pbuf2d, tpert_idx, tptr)
 
+
+    call infld('vmag_gust', fh_ini, dim1name, dim2name, 1, pcols, begchunk, endchunk, &
+         tptr(:,:), found, gridname='physgrid')
+    if(.not. found) then
+       tptr(:,:) = 1._r8
+       if (masterproc) write(iulog,*) 'vmag_gust initialized to 1.'
+    end if
+    vmag_gust_idx = pbuf_get_index( 'vmag_gust')
+    call pbuf_set_field(pbuf2d, vmag_gust_idx, tptr)
+
+
     fieldname='QPERT'  
     qpert_idx = pbuf_get_index( 'qpert',ierr)
     if (qpert_idx > 0) then
