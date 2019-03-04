16593f05b3c3e1953f44a2549e48bde40b07e4ba
James Foucar <jgfouca@sandia.gov>
2019-03-04 11:45:42 -0700

    Refactor to use add_subdirectory for Kokkos

--- components/scream/cmake/Kokkos.cmake
+++ components/scream/cmake/Kokkos.cmake
@@ -4,22 +4,33 @@ if (Kokkos_DIR)
 elseif (KOKKOS_DIR)
   SET (KOKKOS_INSTALL_DIR ${KOKKOS_DIR})
 else ()
-  message (FATAL_ERROR "SCREAM requires Kokkos. Set one of Kokkos_DIR or KOKKOS_DIR to point to the base directory of the Kokkos installation.")
+  # Build kokkos submodule if user did not specify Kokkos_DIR.
+  set(KOKKOS_SRC    ${CMAKE_SOURCE_DIR}/../../externals/kokkos)
+  set(KOKKOS_BINARY ${CMAKE_BINARY_DIR}/kokkos/install)
+
+  add_subdirectory(${KOKKOS_SRC} ${KOKKOS_BINARY})
+
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I${KOKKOS_SRC}/core/src -I${KOKKOS_BINARY}")
+  set(KOKKOS_LDFLAGS_STR "-L${KOKKOS_BINARY}")
 endif ()
 
-# Set some kokkos variables
-include (${KOKKOS_INSTALL_DIR}/kokkos_generated_settings.cmake)
-set (Kokkos_INCLUDE_DIR ${KOKKOS_INSTALL_DIR}/include)
-set (Kokkos_LIBRARY_DIR ${KOKKOS_INSTALL_DIR}/lib)
-set (Kokkos_LINK_FLAGS ${KOKKOS_LINK_FLAGS})
+if (KOKKOS_INSTALL_DIR)
+  # Set some kokkos variables
+  include (${KOKKOS_INSTALL_DIR}/kokkos_generated_settings.cmake)
+  set (Kokkos_INCLUDE_DIR ${KOKKOS_INSTALL_DIR}/include)
+  set (Kokkos_LIBRARY_DIR ${KOKKOS_INSTALL_DIR}/lib)
+  set (Kokkos_LINK_FLAGS ${KOKKOS_LINK_FLAGS})
 
-# LB: is this really needed?
-string (REPLACE ";" " " KOKKOS_CXXFLAGS_STR "${KOKKOS_CXXFLAGS}")
-set(KOKKOS_LDFLAGS_STR "")
-foreach(LDFLAG ${KOKKOS_LDFLAGS})
-  if (${LDFLAG} IN_LIST KOKKOS_CXXFLAGS)
-    message("Skipping repeat flag ${LDFLAG}")
-  else()
-    set(KOKKOS_LDFLAGS_STR "${KOKKOS_LDFLAGS_STR} ${LDFLAG}")
-  endif()
-endforeach()
+  # LB: is this really needed?
+  string (REPLACE ";" " " KOKKOS_CXXFLAGS_STR "${KOKKOS_CXXFLAGS}")
+  set(KOKKOS_LDFLAGS_STR "")
+  foreach(LDFLAG ${KOKKOS_LDFLAGS})
+    if (${LDFLAG} IN_LIST KOKKOS_CXXFLAGS)
+      message("Skipping repeat flag ${LDFLAG}")
+    else()
+      set(KOKKOS_LDFLAGS_STR "${KOKKOS_LDFLAGS_STR} ${LDFLAG}")
+    endif()
+  endforeach()
+
+  SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${KOKKOS_CXXFLAGS_STR}")
+endif()
