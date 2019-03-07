9ac358575d178ca6ae7aba76d9815a17832145eb
Luca Bertagna <lbertag@sandia.gov>
2019-03-07 11:01:46 -0700

    Removed dependency of control on dynamics.
    This stems from the fact that, when relying on homme, dynamics needs some
    configure time parameters in order to build as a library. This forces
    dynamics to be created just-in-time for new executables. On the other hand,
    control would like to implement some tests, and would need to link against
    dynamics. To solve this, we make control dynamics-agnostics. Control should
    only bother about processes organization (init, ordering, run, clean-up),
    fields book-keeping (through a field repository), and handling surface coupling.
    In this context, there's no need to know about how dymamics works.
    Note: the driver can still check the type of each atm process, so it can
          still check, for instance, that there is a dynamics process in the
          processes dag.

--- components/scream/src/control/tests/control_tests.cpp
+++ components/scream/src/control/tests/control_tests.cpp
@@ -1,7 +1,58 @@
 #include <catch2/catch.hpp>
-#include <control/atmosphere_driver.hpp>
+#include "share/atmosphere_process.hpp"
+#include "control/atmosphere_driver.hpp"
+#include "control/surface_coupling.hpp"
 
-namespace {
+namespace scream {
+
+template<AtmosphereProcessType PType>
+class DummyProcess : public scream::AtmosphereProcess {
+public:
+
+  explicit DummyProcess (const ParameterList& /* params */) {}
+
+  // The type of the block (dynamics or physics)
+  AtmosphereProcessType type () const { return PType; }
+
+  // Return some sort of name, linked to PType
+  std::string name () const { return e2str(PType); }
+
+  // The communicator associated with this atm process
+  const Comm& get_comm () const { return m_comm; }
+
+  // The initialization method should prepare all stuff needed to import/export from/to
+  // f90 structures.
+  void initialize (const Comm& comm) { m_comm = comm; }
+
+  // The run method is responsible for exporting atm states to the e3sm coupler, and
+  // import surface states from the e3sm coupler.
+  void run ( /* inputs? */ ) {}
+
+  // Clean up
+  void finalize ( /* inputs */ ) {}
+
+  // Register all fields in the given repo
+  void register_fields (FieldRepository<Real, device_type>& /* field_repo */) const {}
+
+  // Providing a list of required and computed fields
+  const std::set<FieldIdentifier>&  get_required_fields () const { return m_dummy_fids; }
+  const std::set<FieldIdentifier>&  get_computed_fields () const { return m_dummy_fids; }
+
+protected:
+
+  // Setting the field in the atmosphere process
+  void set_required_field_impl (const Field<const Real, device_type>& /* f */) {}
+  void set_computed_field_impl (const Field<      Real, device_type>& /* f */) {}
+
+  std::set<FieldIdentifier> m_dummy_fids;
+
+  Comm    m_comm;
+};
+
+template<AtmosphereProcessType PType>
+AtmosphereProcess* create_dummy_process (const ParameterList& p) {
+  return new DummyProcess<PType>(p);
+}
 
 TEST_CASE("process_factory", "") {
   using namespace scream;
@@ -15,10 +66,18 @@ TEST_CASE("process_factory", "") {
   params.set<std::string>("Schedule Type","Sequential");
 
   auto& p0 = params.sublist("Process 0");
-  p0.set<std::string>("Process Name", "Surface Coupling");
+  p0.set<std::string>("Process Name", "Dummy Physics");
 
   auto& p1 = params.sublist("Process 1");
-  p1.set<std::string>("Process Name", "Dynamics");
+  p1.set<std::string>("Process Name", "Dummy Dynamics");
+
+  // Need to register products in the factory *before* we create any AtmosphereProcessGroup,
+  // which rely on factory for process creation. The initialize method of the AD does that.
+  // While we're at it, check that the case insensitive key of the factory works.
+  auto& factory = AtmosphereProcessFactory::instance();
+  factory.register_product("duMmy pHySics",&create_dummy_process<AtmosphereProcessType::Physics>);
+  factory.register_product("dummY dynAmics",&create_dummy_process<AtmosphereProcessType::Dynamics>);
+  factory.register_product("Surface Coupling",&create_surface_coupling);
 
   // Create a comm
   Comm atm_comm (MPI_COMM_WORLD);
