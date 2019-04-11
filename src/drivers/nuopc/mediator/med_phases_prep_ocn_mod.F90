c6f859745be591bfa55ffba7919b10a9fa28b8ea
Ufuk Turuncoglu <turuncu@ucar.edu>
2019-04-11 17:09:52 -0600

    fix for merging preciptiation with ice melt

--- src/drivers/nuopc/mediator/med_phases_prep_ocn_mod.F90
+++ src/drivers/nuopc/mediator/med_phases_prep_ocn_mod.F90
@@ -542,7 +542,6 @@ contains
                FBinB=is_local%wrap%FBImp(compatm,compocn), fnameB='Faxa_lwdn' , wgtB=ocnwgt1, &
                FBinC=is_local%wrap%FBImp(compatm,compocn), fnameC='Faxa_lwnet' , wgtc=wgtp01, rc=rc)
 
-          ! Merge rain with melted ice before sending liquid precipitation to ocean
           call med_merge_field(is_local%wrap%FBExp(compocn),      'Faxa_rain' , &
                FBInA=is_local%wrap%FBImp(compatm,compocn), fnameA='Faxa_rain' , wgtA=ofrac, &
                FBInB=is_local%wrap%FBImp(compice,compocn), fnameB='Fioi_meltw', wgtB=ifrac, rc=rc)
