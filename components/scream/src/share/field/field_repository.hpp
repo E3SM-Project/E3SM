34e3bb9486d25ca7fac142333e82f4423b189d15
Luca Bertagna <lbertag@sandia.gov>
2019-04-15 16:56:19 -0600

    Change in the internal storage of the FieldRepository
    The repo now stores a map of maps. The outer map has as a key the bare
    name of the field (e.g., "temperature"). The inner map uses the field
    identifier as a key. This means that two fields with the same name but
    different layouts (or grids, or dimensions) will be stored in the same
    entry of the outer map, but in different entries of the inner map.
    
    This distinction will be used by the Atmosphere Driver (AD) to build
    remapping objects to remap corresponding fields with different layouts.

--- components/scream/src/share/field/field_repository.hpp
+++ components/scream/src/share/field/field_repository.hpp
@@ -26,8 +26,8 @@ public:
   using field_type      = Field<scalar_type,device_type>;
   using header_type     = typename field_type::header_type;
   using identifier_type = typename header_type::identifier_type;
-  using requests_type   = std::map<std::string,std::vector<identifier_type>>;
   using map_type        = std::map<identifier_type,field_type>;
+  using repo_type       = std::map<std::string,map_type>;
 
   // Constructor(s)
   FieldRepository ();
@@ -48,20 +48,20 @@ public:
 
   // Methods to query the database
   int size () const { return m_fields.size(); }
-  bool has_field (const identifier_type& identifier) const { return m_fields.find(identifier)!=m_fields.end(); }
+  bool has_field (const identifier_type& identifier) const;
   field_type get_field (const identifier_type& identifier) const;
   RepoState repository_state () const { return m_state; }
+
+  typename repo_type::const_iterator begin() const { return m_fields.begin(); }
+  typename repo_type::const_iterator end()   const { return m_fields.begin(); }
 
 protected:
 
   // The state of the repository
   RepoState       m_state;
 
-  // The requests
-  requests_type   m_requests;
-
   // The actual repo.
-  map_type        m_fields;
+  repo_type       m_fields;
 };
 
 // ============================== IMPLEMENTATION ============================= //
@@ -85,11 +85,21 @@ void FieldRepository<ScalarType,Device>::register_field (const identifier_type& 
   // Sanity checks
   error::runtime_check(m_state==RepoState::Open,"Error! Registration of new fields not started or no longer allowed.\n");
 
+  // Get the map of all fields with this name
+  auto& map = m_fields[id.name()];
+
   // Try to create the field. Allow case where it is already existing.
-  auto it_bool = m_fields.emplace(id,field_type(id));
+  auto it_bool = map.emplace(id,field_type(id));
 
   // Make sure the field can accommodate the requested value type
   it_bool.first->second.get_header().get_alloc_properties().template request_value_type_allocation<RequestedValueType>();
+}
+
+template<typename ScalarType, typename Device>
+bool FieldRepository<ScalarType,Device>::
+has_field (const identifier_type& identifier) const {
+  auto it = m_fields.find(identifier.name());
+  return it!=m_fields.end() && it->second.find(identifier)!=it->second.end();
 }
 
 template<typename ScalarType, typename Device>
@@ -97,8 +107,10 @@ typename FieldRepository<ScalarType,Device>::field_type
 FieldRepository<ScalarType,Device>::get_field (const identifier_type& id) const {
   error::runtime_check(m_state==RepoState::Closed,"Error! You are not allowed to grab fields from the repo until after the registration phase is completed.\n");
 
-  auto it = m_fields.find(id);
-  error::runtime_check(it!=m_fields.end(), "Error! Field not found.\n");
+  const auto& map = m_fields.find(id.name());
+  error::runtime_check(map!=m_fields.end(), "Error! Field not found.\n");
+  auto it = map->second.find(id);
+  error::runtime_check(it!=map->second.end(), "Error! Field not found.\n");
   return it->second;
 }
 
@@ -111,8 +123,10 @@ void FieldRepository<ScalarType,Device>::registration_begins () {
 template<typename ScalarType, typename Device>
 void FieldRepository<ScalarType,Device>::registration_ends () {
   // Proceed to allocate fields
-  for (auto& it : m_fields) {
-    it.second.allocate_view();
+  for (auto& map : m_fields) {
+    for (auto& it : map.second) {
+      it.second.allocate_view();
+    }
   }
 
   // Prohibit further registration of fields
