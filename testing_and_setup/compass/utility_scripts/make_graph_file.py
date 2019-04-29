2feb378d14b6b95164c7de293d3d8a6044c17658
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-04-29 13:57:45 +0200

    Update utility_scripts to py3

--- testing_and_setup/compass/utility_scripts/make_graph_file.py
+++ testing_and_setup/compass/utility_scripts/make_graph_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
 import os
 import numpy as np
 
@@ -14,10 +17,10 @@ parser.add_argument("-w", "--weights", dest="weight_field", help="Field to weigh
 args = parser.parse_args()
 
 if not args.weight_field:
-	print "Weight field missing. Defaulting to unweighted graphs."
-	weighted_parts = False
+    print("Weight field missing. Defaulting to unweighted graphs.")
+    weighted_parts = False
 else:
-	weighted_parts = True
+    weighted_parts = True
 
 dev_null = open(os.devnull, 'w')
 grid = NetCDFFile(args.filename, 'r')
@@ -28,34 +31,34 @@ nEdges = len(grid.dimensions['nEdges'])
 nEdgesOnCell = grid.variables['nEdgesOnCell'][:]
 cellsOnCell = grid.variables['cellsOnCell'][:] - 1
 if weighted_parts:
-	try:
-		weights = grid.variables[args.weight_field][:]
-	except:
-		print args.weight_field, ' not found in file. Defaulting to un-weighted partitions.'
-		weighted_parts = False
+    try:
+        weights = grid.variables[args.weight_field][:]
+    except:
+        print(args.weight_field, ' not found in file. Defaulting to un-weighted partitions.')
+        weighted_parts = False
 grid.close()
 
 nEdges = 0
 for i in np.arange(0, nCells):
-	for j in np.arange(0,nEdgesOnCell[i]):
-		if cellsOnCell[i][j] != -1:
-			nEdges = nEdges + 1
+    for j in np.arange(0,nEdgesOnCell[i]):
+        if cellsOnCell[i][j] != -1:
+            nEdges = nEdges + 1
 
 nEdges = nEdges/2
 
 graph = open('graph.info', 'w+')
 if weighted_parts:
-	graph.write('%s %s 010\n'%(nCells, nEdges))
+    graph.write('%s %s 010\n'%(nCells, nEdges))
 else:
-	graph.write('%s %s\n'%(nCells, nEdges))
+    graph.write('%s %s\n'%(nCells, nEdges))
 
 for i in np.arange(0, nCells):
-	if weighted_parts:
-		graph.write('%s '%int(weights[i]))
+    if weighted_parts:
+        graph.write('%s '%int(weights[i]))
 
-	for j in np.arange(0,nEdgesOnCell[i]):
-		if(cellsOnCell[i][j] >= 0):
-			graph.write('%s '%(cellsOnCell[i][j]+1))
-	graph.write('\n')
+    for j in np.arange(0,nEdgesOnCell[i]):
+        if(cellsOnCell[i][j] >= 0):
+            graph.write('%s '%(cellsOnCell[i][j]+1))
+    graph.write('\n')
 graph.close()
 
