82661004095088f4371325a3911f2f190470fa5f
Xylar Asay-Davis <xylarstorm@gmail.com>
2019-04-30 13:59:26 +0200

    Partially update ISOMIP+ for py3
    Viz and parameter-study scripts will be updated separately
    at a later date.

--- testing_and_setup/compass/ocean/isomip_plus/update_evaporationFlux.py
+++ testing_and_setup/compass/ocean/isomip_plus/update_evaporationFlux.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
 import numpy
 from netCDF4 import Dataset
 
 from optparse import OptionParser
-
-import os
-import os.path
 
 import subprocess
 
@@ -13,15 +13,20 @@ import glob
 
 parser = OptionParser()
 
-parser.add_option("--in_forcing_file", type="string", default="forcing_data_init.nc", dest="in_forcing_file")
-parser.add_option("--out_forcing_file", type="string", default="forcing_data_updated.nc", dest="out_forcing_file")
-parser.add_option("--out_forcing_link", type="string", default="forcing_data.nc", dest="out_forcing_link")
+parser.add_option("--in_forcing_file", type="string",
+                  default="forcing_data_init.nc", dest="in_forcing_file")
+parser.add_option("--out_forcing_file", type="string",
+                  default="forcing_data_updated.nc", dest="out_forcing_file")
+parser.add_option("--out_forcing_link", type="string",
+                  default="forcing_data.nc", dest="out_forcing_link")
 parser.add_option("--avg_months", type="int", default=3, dest="avg_months")
 
 options, args = parser.parse_args()
 
-subprocess.check_call(['cp', options.in_forcing_file, options.out_forcing_file])
-subprocess.check_call(['ln', '-sfn', options.out_forcing_file, options.out_forcing_link])
+subprocess.check_call(['cp',
+                       options.in_forcing_file, options.out_forcing_file])
+subprocess.check_call(['ln', '-sfn',
+                       options.out_forcing_file, options.out_forcing_link])
 
 fluxFiles = sorted(glob.glob('timeSeriesStatsMonthly*.nc'))
 index = max(0, len(fluxFiles)-options.avg_months)
@@ -50,10 +55,11 @@ Ssurf = 33.8
 
 meanMeltFlux = 0.0
 for fileName in fluxFiles:
-  inFile = Dataset(fileName, 'r')
-  freshwaterFlux = inFile.variables['timeMonthly_avg_landIceFreshwaterFlux'][0, :]
-  meanMeltFlux += numpy.sum(freshwaterFlux*areaCell)
-  inFile.close()
+    inFile = Dataset(fileName, 'r')
+    freshwaterFlux = \
+        inFile.variables['timeMonthly_avg_landIceFreshwaterFlux'][0, :]
+    meanMeltFlux += numpy.sum(freshwaterFlux*areaCell)
+    inFile.close()
 
 meanMeltFlux /= len(fluxFiles)
 
@@ -61,17 +67,17 @@ meanMeltFlux /= len(fluxFiles)
 meanMeltFlux /= rho_sw
 
 meanMeltRate = meanMeltFlux*secPerYear/meanIceArea
-print 'mean melt rate:', meanMeltRate, 'm/yr'
+print('mean melt rate: {} m/yr'.format(meanMeltRate))
 
 area = numpy.sum(areaCell)
 
 meanSeaLevelRiseRate = meanMeltFlux*secPerYear/area
-print 'mean rate of sea-level change:', meanSeaLevelRiseRate, 'm/yr'
+print('mean rate of sea-level change: {} m/yr'.format(meanSeaLevelRiseRate))
 
 evaporationFluxVar = outFile.variables['evaporationFlux']
 seaIceSalinityFluxVar = outFile.variables['seaIceSalinityFlux']
 seaIceHeatFluxVar = outFile.variables['seaIceHeatFlux']
-evaporationFlux = evaporationFluxVar[0,:]
+evaporationFlux = evaporationFluxVar[0, :]
 
 evapMask = evaporationFlux != 0.
 
@@ -79,17 +85,17 @@ evapArea = numpy.sum(areaCell*evapMask)
 
 evapRate = -meanMeltFlux/evapArea
 
-print 'evaporation rate:', evapRate*secPerYear, 'm/yr'
+print('evaporation rate: {} m/yr'.format(evapRate*secPerYear))
 
 evaporationFlux[evapMask] = evapRate*rho_sw
-evaporationFluxVar[0,:] = evaporationFlux
+evaporationFluxVar[0, :] = evaporationFlux
 
-flux = seaIceSalinityFluxVar[0,:]
+flux = seaIceSalinityFluxVar[0, :]
 flux[evapMask] = evapRate*Ssurf/sflux_factor
-seaIceSalinityFluxVar[0,:] = flux
+seaIceSalinityFluxVar[0, :] = flux
 
-flux = seaIceHeatFluxVar[0,:]
+flux = seaIceHeatFluxVar[0, :]
 flux[evapMask] = evapRate*Tsurf/hflux_factor
-seaIceHeatFluxVar[0,:] = flux
+seaIceHeatFluxVar[0, :] = flux
 
 outFile.close()
