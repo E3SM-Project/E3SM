d0a6bc3e5a1c0bd0a692e15bd9b1853f0f62cedd
James Foucar <jgfouca@sandia.gov>
2019-04-30 10:23:27 -0600

    Seperate interface from impl

--- components/scream/src/share/util/scream_lin_interp.hpp
+++ components/scream/src/share/util/scream_lin_interp.hpp
@@ -8,7 +8,6 @@
 #include "share/scream_pack.hpp"
 #include "share/scream_pack_kokkos.hpp"
 
-
 namespace scream {
 namespace util {
 
@@ -43,19 +42,7 @@ struct LinInterp
   // ------ public API -------
   //
 
-  LinInterp(int ncol, int km1, int km2, Scalar minthresh) :
-    m_ncol(ncol),
-    m_km1(km1),
-    m_km2(km2),
-    m_km1_pack(scream::pack::npack<Pack>(km1)),
-    m_km2_pack(scream::pack::npack<Pack>(km2)),
-    m_minthresh(minthresh),
-    m_policy(util::ExeSpaceUtils<ExeSpace>::get_default_team_policy(ncol, m_km2_pack)),
-    m_indx_map("m_indx_map", ncol, scream::pack::npack<IntPack>(km2))
-#ifndef NDEBUG
-    , m_indx_map_dbg("m_indx_map_dbg", ncol, scream::pack::npack<IntPack>(km2))
-#endif
-  {}
+  LinInterp(int ncol, int km1, int km2, Scalar minthresh);
 
   int km1_pack() const { return m_km1_pack; }
   int km2_pack() const { return m_km2_pack; }
@@ -64,13 +51,7 @@ struct LinInterp
   KOKKOS_INLINE_FUNCTION
   void setup(const MemberType& team,
              const V& x1,
-             const V& x2) const
-  {
-    setup_nlogn(team, *this, scream::pack::repack<Pack::n>(x1), scream::pack::repack<Pack::n>(x2));
-#ifndef NDEBUG
-    setup_n2(team, *this, scream::pack::repack<Pack::n>(x1), scream::pack::repack<Pack::n>(x2));
-#endif
-  }
+             const V& x2) const;
 
   // Linearly interpolate y(x1) onto coordinates x2
   template <typename V>
@@ -79,110 +60,23 @@ struct LinInterp
                   const V& x1,
                   const V& x2,
                   const V& y1,
-                  const V& y2) const
-  {
-    lin_interp_impl(team,
-                    *this,
-                    scream::pack::repack<Pack::n>(x1),
-                    scream::pack::repack<Pack::n>(x2),
-                    scream::pack::repack<Pack::n>(y1),
-                    scream::pack::repack<Pack::n>(y2));
-  }
+                  const V& y2) const;
+
+  //
+  // -------- Internal API, data ------
+  //
 
   KOKKOS_INLINE_FUNCTION
-  static void lin_interp_impl(const MemberType& team,
-                              const LinInterp& liv,
-                              const view_1d<const Pack>& x1, const view_1d<const Pack>& x2, const view_1d<const Pack>& y1,
-                              const view_1d<Pack>& y2)
-  {
-    auto x1s = scream::pack::scalarize(x1);
-    auto y1s = scream::pack::scalarize(y1);
-
-    const int i = team.league_rank();
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team, liv.m_km2_pack), [&] (Int k2) {
-      const auto indx_pk = liv.m_indx_map(i, k2);
-#ifndef NDEBUG
-      const auto indx_pk_dbg = liv.m_indx_map_dbg(i, k2);
-      for (int s = 0; s < Pack::n; ++s) {
-        if (k2*Pack::n + s < liv.m_km2) {
-          scream_kassert(indx_pk[s] == indx_pk_dbg[s]);
-        }
-      }
-#endif
-      const auto end_mask = indx_pk == liv.m_km1 - 1;
-      if (end_mask.any()) {
-        const auto not_end = !end_mask;
-        scream_masked_loop(end_mask, s) {
-          int k1 = indx_pk[s];
-          y2(k2)[s] = y1s(k1) + (y1s(k1)-y1s(k1-1))*(x2(k2)[s]-x1s(k1))/(x1s(k1)-x1s(k1-1));
-        }
-        scream_masked_loop(not_end, s) {
-          int k1 = indx_pk[s];
-          y2(k2)[s] = y1s(k1) + (y1s(k1+1)-y1s(k1))*(x2(k2)[s]-x1s(k1))/(x1s(k1+1)-x1s(k1));
-        }
-      }
-      else {
-        Pack x1p, x1p1, y1p, y1p1;
-        scream::pack::index_and_shift<1>(x1s, indx_pk, x1p, x1p1);
-        scream::pack::index_and_shift<1>(y1s, indx_pk, y1p, y1p1);
-        const auto& x2p = x2(k2);
-
-        y2(k2) = y1p + (y1p1-y1p)*(x2p-x1p)/(x1p1-x1p);
-      }
-
-      y2(k2).set(y2(k2) < liv.m_minthresh, liv.m_minthresh);
-    });
-  }
-
-#ifndef NDEBUG
-  KOKKOS_INLINE_FUNCTION
-  static void setup_n2(const MemberType& team, const LinInterp& liv, const view_1d<const Pack>& x1, const view_1d<const Pack>& x2)
-  {
-    auto x1s = scream::pack::scalarize(x1);
-    auto idxs = liv.m_indx_map_dbg;
-
-    const int i = team.league_rank();
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team, liv.m_km2_pack), [&] (Int k2) {
-      for (int s = 0; s < Pack::n; ++s) {
-        if( x2(k2)[s] <= x1s(0) ) { // x2[k2] comes before x1[0]
-          idxs(i, k2)[s] = 0;
-        }
-        else if( x2(k2)[s] >= x1s(liv.m_km1-1) ) { // x2[k2] comes after x1[-1]
-          idxs(i, k2)[s] = liv.m_km1-1;
-        }
-        else {
-          for (int k1 = 1; k1 < liv.m_km1; ++k1) { // scan over x1
-            if( (x2(k2)[s]>=x1s(k1-1)) && (x2(k2)[s]<x1s(k1)) ) { // check if x2[k2] lies within x1[k1-1] and x1[k1]
-              idxs(i, k2)[s] = k1-1;
-            }
-          }
-        }
-      }
-    });
-  }
-#endif
+  static void setup_impl(
+    const MemberType& team, const LinInterp& liv, const view_1d<const Pack>& x1, const view_1d<const Pack>& x2);
 
   KOKKOS_INLINE_FUNCTION
-  static void setup_nlogn(const MemberType& team, const LinInterp& liv, const view_1d<const Pack>& x1, const view_1d<const Pack>& x2)
-  {
-    auto x1s = scream::pack::scalarize(x1);
+  static void lin_interp_impl(
+    const MemberType& team,
+    const LinInterp& liv,
+    const view_1d<const Pack>& x1, const view_1d<const Pack>& x2, const view_1d<const Pack>& y1,
+    const view_1d<Pack>& y2);
 
-    const int i = team.league_rank();
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team, liv.m_km2_pack), [&] (Int k2) {
-      for (int s = 0; s < Pack::n; ++s) {
-        const Scalar x1_indv = x2(k2)[s];
-        auto begin = x1s.data();
-        auto upper = begin + liv.m_km1;
-
-        auto ub = util::upper_bound(begin, upper, x1_indv);
-        int x1_idx = ub - begin;
-        if (x1_idx > 0) {
-          --x1_idx;
-        }
-        liv.m_indx_map(i, k2)[s] = x1_idx;
-      }
-    });
-  }
 
   int m_ncol;
   int m_km1;
@@ -192,10 +86,9 @@ struct LinInterp
   Scalar m_minthresh;
   TeamPolicy m_policy;
   view_2d<IntPack> m_indx_map; // [x2-idx] -> x1-idx
-#ifndef NDEBUG
-  view_2d<IntPack> m_indx_map_dbg; // [x2-idx] -> x1-idx
-#endif
 };
+
+#include "scream_lin_interp_impl.hpp"
 
 } //namespace util
 } //namespace scream
