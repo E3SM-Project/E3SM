f4726b7f1631d2dc3e42d39ca7b19aae5e0d9939
zshaheen <zshaheen93@gmail.com>
2019-06-12 18:31:10 -0700

    Added new env stuff.

--- tests/system/all_sets_api.py
+++ tests/system/all_sets_api.py
@@ -1,7 +1,6 @@
 # Running the software with the API:
 #    python all_sets_api.py -d all_sets.py
 from acme_diags.parameter.core_parameter import CoreParameter
-from acme_diags.parameter.zonal_mean_2d_parameter import ZonalMean2dParameter
 from acme_diags.run import Run
 
 thing = Run()
