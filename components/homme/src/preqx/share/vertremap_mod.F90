46b3c79fd42e85d88ca7ce8db86da0fa9b97d485
hsiangheleellnl <lee1061@llnl.gov>
2019-06-20 14:05:06 -0700

    remap scheme modification for FIVE

--- components/homme/src/preqx/share/vertremap_mod.F90
+++ components/homme/src/preqx/share/vertremap_mod.F90
@@ -38,11 +38,11 @@ contains
   use hybvcoord_mod,  only: hvcoord_t
   use control_mod,    only: rsplit
   use hybrid_mod,     only: hybrid_t
+  use ppgrid,         only: pver, pverp
 ! HHLEE 20190521
 #ifdef FIVE
   use shr_kind_mod,   only: r8=>shr_kind_r8
   use physconst,      only: rair, gravit
-  use ppgrid,         only: pver, pverp
   use five_intr,      only: pver_five, pverp_five, &
                             masswgt_vert_avg, linear_interp, &
                             hyai_five_toshare, hyam_five_toshare, &
@@ -67,24 +67,27 @@ contains
   real (kind=real_kind), dimension(np,np,pver_five)   :: dp_five
   real (kind=real_kind), dimension(np,np,pver_five)   :: dp_star_five
   real (kind=real_kind), dimension(np,np,pverp_five)  :: eta_dot_dpdn_five
+  real (kind=real_kind), dimension(np,np,pverp)       :: eta_dot_dpdn
   real (kind=real_kind), dimension(np,np,pver_five)   :: dp3d_five
+  real (kind=real_kind), dimension(np,np,pver,qsize)  :: state_Q
 
   real (kind=real_kind), dimension(np,np,pver_five)  :: ttmp_five
   real (kind=real_kind), dimension(np,np,pver_five)  :: utmp_five
   real (kind=real_kind), dimension(np,np,pver_five)  :: vtmp_five
-  real (kind=real_kind), dimension(np,np,pver_five,qsize)  :: Qdptmp_five
+  real (kind=real_kind), dimension(np,np,pver_five,qsize)  :: Qtmp_five
   real (kind=real_kind), dimension(np,np,pver_five)  :: ttmp_five_old
   real (kind=real_kind), dimension(np,np,pver_five)  :: utmp_five_old
   real (kind=real_kind), dimension(np,np,pver_five)  :: vtmp_five_old
-  real (kind=real_kind), dimension(np,np,pver_five,qsize)  :: Qdptmp_five_old
+  real (kind=real_kind), dimension(np,np,pver_five,qsize)  :: Qtmp_five_old
   real (kind=real_kind), dimension(np,np,pver_five)  :: ttmp_five_tend
   real (kind=real_kind), dimension(np,np,pver_five)  :: utmp_five_tend
   real (kind=real_kind), dimension(np,np,pver_five)  :: vtmp_five_tend
-  real (kind=real_kind), dimension(np,np,pver_five,qsize)  :: Qdptmp_five_tend
+  real (kind=real_kind), dimension(np,np,pver_five,qsize)  :: Qtmp_five_tend
   real (kind=real_kind), dimension(np,np,pver)       :: ttmp_host
   real (kind=real_kind), dimension(np,np,pver)       :: utmp_host
   real (kind=real_kind), dimension(np,np,pver)       :: vtmp_host
-  real (kind=real_kind), dimension(np,np,pver,qsize) :: Qdptmp_host
+  real (kind=real_kind), dimension(np,np,pver,qsize) :: Qtmp_host
+  real (kind=real_kind), dimension(np,np,pver,qsize) :: Qdp_test
 
   real (kind=real_kind), dimension(np,np,pver_five)  :: rho_five
   real (kind=real_kind), dimension(np,np,pver_five)  :: dz_five
@@ -119,29 +122,41 @@ contains
   ttmp_five = 0.
   utmp_five = 0.
   vtmp_five = 0.
-  Qdptmp_five = 0.
+  Qtmp_five = 0.
   ttmp_host = 0.
   utmp_host = 0.
   vtmp_host = 0.
-  Qdptmp_host = 0.
+  Qtmp_host = 0.
 #endif
  
    do ie=nets,nete
      ! update final ps_v
      elem(ie)%state%ps_v(:,:,np1) = hvcoord%hyai(1)*hvcoord%ps0 + &
           sum(elem(ie)%state%dp3d(:,:,:,np1),3)
+#ifdef FIVE
+! For SCM only
+    do k=1,nlev
+      eta_dot_dpdn(:,:,k)=elem(1)%derived%omega_p(1,1,k)
+    enddo
+      eta_dot_dpdn(:,:,nlev+1) = eta_dot_dpdn(:,:,nlev)
+#endif
+
      do k=1,nlev
         dp(:,:,k) = ( hvcoord%hyai(k+1) - hvcoord%hyai(k) )*hvcoord%ps0 + &
              ( hvcoord%hybi(k+1) - hvcoord%hybi(k) )*elem(ie)%state%ps_v(:,:,np1)
-        if (rsplit==0) then
-           dp_star(:,:,k) = dp(:,:,k) + dt*(elem(ie)%derived%eta_dot_dpdn(:,:,k+1) -&
-                elem(ie)%derived%eta_dot_dpdn(:,:,k))
-        else
-           dp_star(:,:,k) = elem(ie)%state%dp3d(:,:,k,np1)
-        endif
+
+!        if (rsplit==0) then
+!           dp_star(:,:,k) = dp(:,:,k) + dt*(elem(ie)%derived%eta_dot_dpdn(:,:,k+1) -&
+!                elem(ie)%derived%eta_dot_dpdn(:,:,k))
+!        else
+!           dp_star(:,:,k) = elem(ie)%state%dp3d(:,:,k,np1)
+!        endif
 #ifdef FIVE
+        dp_star(:,:,k) = dp(:,:,k) + dt*(eta_dot_dpdn(:,:,k+1) - eta_dot_dpdn(:,:,k))
+
         pint_host(:,:,k) = hvcoord%hyai(k)*hvcoord%ps0+hvcoord%hybi(k)*elem(ie)%state%ps_v(:,:,np1)
         pmid_host(:,:,k) = hvcoord%hyam(k)*hvcoord%ps0+hvcoord%hybm(k)*elem(ie)%state%ps_v(:,:,np1)
+
 #endif
      enddo
 #ifdef FIVE
@@ -172,7 +187,7 @@ contains
 
      do i=1,np
      do j=1,np
-        call linear_interp(pint_host(i,j,:),pint_five(i,j,:),elem(ie)%derived%eta_dot_dpdn(i,j,1:pverp),&
+        call linear_interp(pint_host(i,j,:),pint_five(i,j,:),eta_dot_dpdn(i,j,1:pverp),&
                      eta_dot_dpdn_five(i,j,1:pverp_five),pverp,pverp_five)  
      enddo
      enddo
@@ -180,6 +195,7 @@ contains
      do k=1,pver_five
         dp_star_five(:,:,k) = dp_five(:,:,k) + dt*(eta_dot_dpdn_five(:,:,k+1) - eta_dot_dpdn_five(:,:,k))
      enddo
+
 #endif
 
 #ifdef FIVE
@@ -191,9 +207,11 @@ contains
         call linear_interp(pmid_host(i,j,:),pmid_five(i,j,:),elem(ie)%state%t(i,j,:,np1),ttmp_five(i,j,:),pver,pver_five)
         call linear_interp(pmid_host(i,j,:),pmid_five(i,j,:),elem(ie)%state%v(i,j,1,:,np1),utmp_five(i,j,:),pver,pver_five)
         call linear_interp(pmid_host(i,j,:),pmid_five(i,j,:),elem(ie)%state%v(i,j,2,:,np1),vtmp_five(i,j,:),pver,pver_five)
-       do q = 1, qsize 
-         call linear_interp(pmid_host(i,j,:),pmid_five(i,j,:),elem(ie)%state%Qdp(i,j,:,q,np1_qdp),Qdptmp_five(i,j,:,q),pver,pver_five)
-         Qdptmp_five_old(i,j,:,q) = Qdptmp_five(i,j,:,q)
+       do q = 1, qsize
+         state_Q(i,j,:,q) = elem(ie)%state%Qdp(i,j,:,q,np1_qdp) / dp_star(i,j,:) 
+         call linear_interp(pmid_host(i,j,:),pmid_five(i,j,:),state_Q(i,j,:,q),Qtmp_five(i,j,:,q),pver,pver_five)
+         Qtmp_five_old(i,j,:,q) = Qtmp_five(i,j,:,q)
+         Qtmp_five(i,j,:,q) = Qtmp_five(i,j,:,q)*dp_star_five(i,j,:)
        enddo 
      enddo 
      enddo
@@ -201,7 +219,7 @@ contains
         ttmp_five_old = ttmp_five 
         ttmp_five = ttmp_five*dp_star_five
         call t_startf('vertical_remap1_FIVE_1')
-        call remap1(ttmp_five,np,1,dp_star_five,dp_five)
+        call remap1(ttmp_five,np,pver_five,1,dp_star_five,dp_five)
         call t_stopf('vertical_remap1_FIVE_1')
         ttmp_five = ttmp_five/dp_five
         ttmp_five_tend = (ttmp_five - ttmp_five_old)/dt
@@ -209,35 +227,33 @@ contains
         utmp_five_old = utmp_five 
         utmp_five = utmp_five*dp_star_five
         call t_startf('vertical_remap1_FIVE_2')
-        call remap1(utmp_five,np,1,dp_star_five,dp_five)
+        call remap1(utmp_five,np,pver_five,1,dp_star_five,dp_five)
         call t_stopf('vertical_remap1_FIVE_2')
-        utmp_five = utmp_five/dp_five
         utmp_five = utmp_five/dp_five
         utmp_five_tend = (utmp_five - utmp_five_old)/dt
 
         vtmp_five_old = vtmp_five 
         vtmp_five = vtmp_five*dp_star_five
         call t_startf('vertical_remap1_FIVE_3')
-        call remap1(vtmp_five,np,1,dp_star_five,dp_five)
+        call remap1(vtmp_five,np,pver_five,1,dp_star_five,dp_five)
         call t_stopf('vertical_remap1_FIVE_3')
-        vtmp_five = vtmp_five/dp_five
         vtmp_five = vtmp_five/dp_five
         vtmp_five_tend = (vtmp_five - vtmp_five_old)/dt
 
      if (qsize>0) then
-
+       
        call t_startf('vertical_remap1_FIVE_4')
-       call remap1(Qdptmp_five,np,qsize,dp_star_five,dp_five)
+       call remap1(Qtmp_five,np,pver_five,qsize,dp_star_five,dp_five)
        call t_stopf('vertical_remap1_FIVE_4')
 
      endif
-! ----- For rho_host esimate on the reference levels
-        ttmp(:,:,:,1)=elem(ie)%state%t(:,:,:,np1)
-        ttmp(:,:,:,1)=ttmp(:,:,:,1)*dp_star
-
-        call t_startf('vertical_remap1_1')
-        call remap1(ttmp,np,1,dp_star,dp)
-        call t_stopf('vertical_remap1_1')
+!! ----- For rho_host esimate on the reference levels
+!        ttmp(:,:,:,1)=elem(ie)%state%t(:,:,:,np1)
+!        ttmp(:,:,:,1)=ttmp(:,:,:,1)*dp_star
+!
+!        call t_startf('vertical_remap1_1')
+!        call remap1(ttmp,np,1,dp_star,dp)
+!        call t_stopf('vertical_remap1_1')
 
 !-------------------
 ! should calculate rho_five before or after remap??
@@ -264,11 +280,12 @@ contains
                             vtmp_five_tend(i,j,:),vtmp_host(i,j,:))
 
        do q = 1, qsize 
-        Qdptmp_five_tend(i,j,:,q) = (Qdptmp_five(i,j,:,q) - Qdptmp_five_old(i,j,:,q))/dt
+        Qtmp_five(i,j,:,q) = Qtmp_five(i,j,:,q)/dp_five(i,j,:)
+        Qtmp_five_tend(i,j,:,q) = (Qtmp_five(i,j,:,q) - Qtmp_five_old(i,j,:,q))/dt
 
         call masswgt_vert_avg(rho_host(i,j,:),rho_five(i,j,:),dz_host(i,j,:),dz_five(i,j,:),&
                             pint_host(i,j,:),pmid_five(i,j,:),pmid_host(i,j,:),&
-                            Qdptmp_five_tend(i,j,:,q),Qdptmp_host(i,j,:,q))
+                            Qtmp_five_tend(i,j,:,q),Qtmp_host(i,j,:,q))
        enddo 
      enddo 
      enddo
@@ -294,7 +311,7 @@ contains
         ttmp(:,:,:,1)=ttmp(:,:,:,1)*dp_star
 
         call t_startf('vertical_remap1_1')
-        call remap1(ttmp,np,1,dp_star,dp)
+        call remap1(ttmp,np,pver,1,dp_star,dp)
         call t_stopf('vertical_remap1_1')
 
         elem(ie)%state%t(:,:,:,np1)=ttmp(:,:,:,1)/dp
@@ -303,7 +320,7 @@ contains
         ttmp(:,:,:,2)=elem(ie)%state%v(:,:,2,:,np1)*dp_star
 
         call t_startf('vertical_remap1_2')
-        call remap1(ttmp,np,2,dp_star,dp)
+        call remap1(ttmp,np,pver,2,dp_star,dp)
         call t_stopf('vertical_remap1_2')
 
         elem(ie)%state%v(:,:,1,:,np1)=ttmp(:,:,:,1)/dp
@@ -324,11 +341,15 @@ contains
 #ifdef FIVE
       do q = 1, qsize
          elem(ie)%state%Qdp(:,:,:,q,np1_qdp) = elem(ie)%state%Qdp(:,:,:,q,np1_qdp) + &
-                   Qdptmp_host(:,:,:,q) *dt
+                   Qtmp_host(:,:,:,q) *dp(:,:,:) * dt
+         !Qdp_test(:,:,:,q) = elem(ie)%state%Qdp(:,:,:,q,np1_qdp) + &
+         !          Qtmp_host(:,:,:,q) *dp(:,:,:) * dt
       enddo
+      elem(ie)%state%Qdp(:,:,:,:,np1_qdp) = max(elem(ie)%state%Qdp(:,:,:,:,np1_qdp),0.) 
+      !Qdp_test = max(Qdp_test,0.)
 #else
        call t_startf('vertical_remap1_3')
-       call remap1(elem(ie)%state%Qdp(:,:,:,:,np1_qdp),np,qsize,dp_star,dp)
+       call remap1(elem(ie)%state%Qdp(:,:,:,:,np1_qdp),np,pver,qsize,dp_star,dp)
        call t_stopf('vertical_remap1_3')
 #endif
 
@@ -340,7 +361,4 @@ contains
 
 
 end module 
-
-
-
 
