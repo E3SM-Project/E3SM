6cdeb8c7b95f93d0691334206497c13353e5df96
Steven Brus <sbrus@lanl.gov>
2019-06-24 11:15:23 -0600

    Make concatenate_cfsr.py python 3 compatible

--- testing_and_setup/compass/ocean/hurricane/concatenate_cfsr.py
+++ testing_and_setup/compass/ocean/hurricane/concatenate_cfsr.py
@@ -9,13 +9,13 @@ filetypes = ['prmsl','wnd10m']
 for ft in filetypes:
 
   files = sorted(glob.glob(pwd+ft+'*.nc'))
-  print files 
+  print(files) 
 
   filenames = [f.split('/')[-1] for f in files]
-  print filenames
+  print(filenames)
 
   command = ['ncrcat']
   command.extend(filenames)
   command.append(ft+'.nc')
-  print command
+  print(command)
   subprocess.call(command)
