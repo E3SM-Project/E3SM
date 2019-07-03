6486c2851704066a503585cbfd74fac9e27fd2b3
Sulman B N <sulmanbn@ornl.gov>
2019-07-03 16:58:26 -0400

    Added capability to specify parameters of N fixation (NPP) equation by PFT. Now column N fixation is calculated as a weighted average over PFTs.

--- components/clm/src/biogeochem/NitrogenDynamicsMod.F90
+++ components/clm/src/biogeochem/NitrogenDynamicsMod.F90
@@ -164,6 +164,7 @@ contains
     use clm_time_manager , only : get_days_per_year, get_step_size
     use shr_sys_mod      , only : shr_sys_flush
     use clm_varcon       , only : secspday, spval
+    use pftvarcon        , only : noveg
     !
     ! !ARGUMENTS:
     integer                 , intent(in)    :: num_soilc       ! number of soil columns in filter
@@ -173,10 +174,11 @@ contains
     type(nitrogenflux_type) , intent(inout) :: nitrogenflux_vars 
     !
     ! !LOCAL VARIABLES:
-    integer  :: c,fc                  ! indices
+    integer  :: c,fc,p                ! indices
     real(r8) :: t                     ! temporary
     real(r8) :: dayspyr               ! days per year
     real(r8) :: secspyr              ! seconds per yr
+    real(r8) :: total_weight
     logical  :: do_et_bnf = .false.
     !-----------------------------------------------------------------------
 
@@ -184,10 +186,13 @@ contains
          cannsum_npp    => col_cf%annsum_npp      , & ! Input:  [real(r8) (:)]  nitrogen deposition rate (gN/m2/s)                
          col_lag_npp    => col_cf%lag_npp         , & ! Input: [real(r8) (:)]  (gC/m2/s) lagged net primary production           
 
-         qflx_tran_veg  => col_wf%qflx_tran_veg    , & ! col vegetation transpiration (mm H2O/s) (+ = to atm)
+         qflx_tran_veg  => col_wf%qflx_tran_veg   , & ! col vegetation transpiration (mm H2O/s) (+ = to atm)
          
-         qflx_evap_veg  => col_wf%qflx_evap_veg    , & ! col vegetation evaporation (mm H2O/s) (+ = to atm)
-         nfix_to_sminn  => col_nf%nfix_to_sminn   & ! Output: [real(r8) (:)]  symbiotic/asymbiotic N fixation to soil mineral N (gN/m2/s)
+         qflx_evap_veg  => col_wf%qflx_evap_veg   , & ! col vegetation evaporation (mm H2O/s) (+ = to atm)
+         nfix_to_sminn  => col_nf%nfix_to_sminn   , & ! Output: [real(r8) (:)]  symbiotic/asymbiotic N fixation to soil mineral N (gN/m2/s)
+         
+         Nfix_NPP_c1    => veg_vp%Nfix_NPP_c1     , & ! Pre-exponential parameter for NPP_based N fixation
+         Nfix_NPP_c2    => veg_vp%Nfix_NPP_c2       & ! Exponential parameter for NPP_based N fixation
          )
 
       dayspyr = get_days_per_year()
@@ -206,22 +211,47 @@ contains
             ! Loop through columns
             do fc = 1,num_soilc
                c = filter_soilc(fc)         
-               
+               ! B. Sulman: Loop through patches. Nfix is weighted average of value for each PFT's parameters
+               t = 0.0_r8
                if (col_lag_npp(c) /= spval) then
-                  ! need to put npp in units of gC/m^2/year here first
-                  t = (1.8_r8 * (1._r8 - exp(-0.003_r8 * col_lag_npp(c)*(secspday * dayspyr))))/(secspday * dayspyr)  
-                  nfix_to_sminn(c) = max(0._r8,t)
+                  total_weight = 0.0_r8  ! To correct for inactive and unveg cells
+                  do p = col_pp%pfti(c), col_pp%pftf(c)
+                      ! need to put npp in units of gC/m^2/year here first
+                      ! B. Sulman: calculate Nfix value for each patch's parameters, and add to weighted average
+                      if (veg_pp%active(p) .and. (veg_pp%itype(p) .ne. noveg)) then
+                          t = t + max(0._r8,veg_pp%wtcol(p)*(Nfix_NPP_c1(veg_pp%itype(p)) * &
+                                 (1._r8 - exp(-Nfix_NPP_c2(veg_pp%itype(p)) * col_lag_npp(c)*(secspday * dayspyr))))/(secspday * dayspyr))  
+                          total_weight = total_weight + veg_pp%wtcol(p)
+                      endif
+                   enddo
+                   if(total_weight>0) then
+                       nfix_to_sminn(c) = max(0._r8,t/total_weight)
+                   else
+                       nfix_to_sminn(c) = 0._r8
+                   endif
                else
-                  nfix_to_sminn(c) = 0._r8
+                   nfix_to_sminn(c) = 0._r8
                endif
             end do
          else
             ! use annual-mean values for NPP-NFIX relation
             do fc = 1,num_soilc
                c = filter_soilc(fc)
-               
-               t = (1.8_r8 * (1._r8 - exp(-0.003_r8 * cannsum_npp(c))))/(secspday * dayspyr)
-               nfix_to_sminn(c) = max(0._r8,t)
+               ! B. Sulman: Loop through patches. Nfix is weighted average of value for each PFT's parameters
+               t = 0.0_r8
+               total_weight = 0.0_r8
+               do p = col_pp%pfti(c), col_pp%pftf(c)
+                  if (veg_pp%active(p) .and. (veg_pp%itype(p) .ne. noveg)) then
+                       t = t + max(0._r8,veg_pp%wtcol(p)*(Nfix_NPP_c1(veg_pp%itype(p)) * &
+                             (1._r8 - exp(-Nfix_NPP_c2(veg_pp%itype(p)) * cannsum_npp(c))))/(secspday * dayspyr))
+                       total_weight = total_weight + veg_pp%wtcol(p)
+                  endif
+               enddo
+               if (total_weight>0) then
+                   nfix_to_sminn(c) = max(0._r8,t/total_weight)
+               else
+                   nfix_to_sminn(c) = 0.0_r8
+               endif
             end do
          endif
       endif
--- components/clm/src/data_types/VegetationPropertiesType.F90
+++ components/clm/src/data_types/VegetationPropertiesType.F90
@@ -90,7 +90,10 @@ module VegetationPropertiesType
      real(r8), allocatable :: livewdcp      (:)   ! live wood (phloem and ray parenchyma) C:P (gC/gP)
      real(r8), allocatable :: deadwdcp      (:)   ! dead wood (xylem and heartwood) C:P (gC/gP)
      real(r8), allocatable :: graincp       (:)   ! grain C:P (gC/gP) for prognostic crop model
-     
+    
+     real(r8), allocatable :: Nfix_NPP_c1   (:)   ! Pre-exponential parameter in NPP-based N fixation eqn
+     real(r8), allocatable :: Nfix_NPP_c2   (:)   ! Exponential parameter in NPP-based N fixation eqn
+
      ! pft dependent parameters for phosphorus for nutrient competition
      real(r8), allocatable :: vmax_plant_nh4(:)        ! vmax for plant nh4 uptake
      real(r8), allocatable :: vmax_plant_no3(:)        ! vmax for plant no3 uptake
@@ -168,6 +171,7 @@ contains
     use pftvarcon , only : fertnitro, graincn, fleafcn, ffrootcn, fstemcn, dwood
     use pftvarcon , only : presharv, convfact, fyield
     use pftvarcon , only : leafcp,lflitcp, frootcp, livewdcp, deadwdcp,graincp
+    use pftvarcon , only : Nfix_NPP_c1, Nfix_NPP_c2 
     use pftvarcon , only : vmax_plant_nh4, vmax_plant_no3, vmax_plant_p, vmax_minsurf_p_vr
     use pftvarcon , only : km_plant_nh4, km_plant_no3, km_plant_p, km_minsurf_p_vr
     use pftvarcon , only : km_decomp_nh4, km_decomp_no3, km_decomp_p, km_nit, km_den
@@ -252,7 +256,10 @@ contains
     allocate(this%livewdcp      (0:numpft))        ; this%livewdcp     (:)   =nan
     allocate(this%deadwdcp      (0:numpft))        ; this%deadwdcp     (:)   =nan
     allocate(this%graincp       (0:numpft))        ; this%graincp      (:)   =nan
-    
+   
+    allocate(this%Nfix_NPP_c1   (0:numpft))        ; this%Nfix_NPP_c1   (:)   =nan
+    allocate(this%Nfix_NPP_c2   (0:numpft))        ; this%Nfix_NPP_c2   (:)   =nan
+
     allocate( this%alpha_nfix    (0:numpft))                     ; this%alpha_nfix    (:)        =nan
     allocate( this%alpha_ptase   (0:numpft))                     ; this%alpha_ptase   (:)        =nan
     allocate( this%ccost_nfix    (0:numpft))                     ; this%ccost_nfix    (:)        =nan
@@ -381,7 +388,10 @@ contains
        this%mbbopt(m)       = mbbopt(m)
        this%nstor(m)        = nstor(m)
        this%br_xr(m)        = br_xr(m)
- 
+
+       this%Nfix_NPP_c1(m)  = Nfix_NPP_c1(m)
+       this%Nfix_NPP_c2(m)  = Nfix_NPP_c2(m)
+
     !----------------------F.-M. Yuan (2018-03-23): user-defined parameter file ---------------------------------------------------------------------
        this%nonvascular(m)  = nonvascular(m)
        this%nfixer(m)       = nfixer(m)
