367d24ddf18d76301b4c7c3e5b610b8162c98759
JIAN SUN <jian.sun@pnnl.gov>
2019-07-08 16:05:46 -0700

    add _r8 to float numbers and use more digits for Gauss-Hermite quadratures in modal_aero_amicphys.F90

--- components/cam/src/chemistry/modal_aero/modal_aero_amicphys.F90
+++ components/cam/src/chemistry/modal_aero/modal_aero_amicphys.F90
@@ -5030,8 +5030,13 @@ agepair_loop1: &
       real(r8) :: tmpa
       real(r8), save :: xghq(nghq), wghq(nghq) ! quadrature abscissae and weights
 
-      data xghq / 0.70710678_r8, -0.70710678_r8 /
-      data wghq / 0.88622693_r8,  0.88622693_r8 /
+! Dick Easter's version
+!      data xghq / 0.70710678_r8, -0.70710678_r8 /
+!      data wghq / 0.88622693_r8,  0.88622693_r8 /
+
+! JS updates on 07/08/2019: ghq with more digits
+      data xghq / 7.0710678118654746e-01_r8, -7.0710678118654746e-01_r8 /
+      data wghq / 8.8622692545275794e-01_r8,  8.8622692545275794e-01_r8 /
 
       accomxp283 = accom * 0.283_r8
       accomxp75  = accom * 0.75_r8
