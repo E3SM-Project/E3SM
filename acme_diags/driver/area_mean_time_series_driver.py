7ac3974364206bd250c07faa5bd478130f9cc2ab
ChengzhuZhang <zhang40@llnl.gov>
2019-07-11 21:21:03 -0700

    adjust time as a function in dataset clasee

--- acme_diags/driver/area_mean_time_series_driver.py
+++ acme_diags/driver/area_mean_time_series_driver.py
@@ -59,25 +59,8 @@ def run_diag(parameter):
             print("Selected region: {}".format(region))
             test_data = utils.dataset.Dataset(parameter, test=True)
             test = test_data.get_timeseries_variable(var)
+            print('Start and end time for selected time slices for test data: ', test.getTime().asComponentTime()[0],test.getTime().asComponentTime()[-1])
             
-            # Redefine time to be in the middle of the time interval
-            # And rewrite the time axis
-            #These steps are important for data where the absolute time doesn't full in the middle, such as E3SM, the time was recorded at the end of each time Bounds.
-            var_time = test.getTime()
-            tbounds = var_time.getBounds()
-            var_time[:] = 0.5*(tbounds[:,0]+tbounds[:,1])
-            var_time_absolute = var_time.asComponentTime()
-            time2 = cdms2.createAxis(var_time)
-            time2.designateTime()
-            #.designateTime() needs to be set before attributes changes.
-            time2.units = var_time.units
-            time2.calendar = var_time.calendar
-            #time2.calendar = cdtime.NoLeapCalendar
-            time2.id = 'time'
-            test.setAxis(0,time2)
-            
-            # Make sure data have correct montly Bounds
-            cdutil.setTimeBoundsMonthly(test)
             print('test shape',test.shape, test.units)
 
             parameter.viewer_descr[var] = getattr(test, 'long_name', var)
@@ -93,15 +76,13 @@ def run_diag(parameter):
                 parameter.ref_name_yrs = utils.general.get_name_and_yrs(parameter, ref_data)
 
                 ref = ref_data.get_timeseries_variable(var)
+                print('Start and end time for selected time slices for ref data: ', ref.getTime().asComponentTime()[0],ref.getTime().asComponentTime()[-1])
 
-                cdutil.setTimeBoundsMonthly(ref)
- 
                 
                 # TODO: Will this work if ref and test are timeseries data,
                 # but land_frac and ocean_frac are climo'ed.
                 test_domain, ref_domain = utils.general.select_region(
                     region, test, ref, land_frac, ocean_frac, parameter)
-                print(test_domain.getTime().asComponentTime())
 
                 # Average over selected region, and average
                 # over months to get the yearly mean.
