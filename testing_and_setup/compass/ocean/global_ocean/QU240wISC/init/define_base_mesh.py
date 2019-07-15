990701eca290a32dbabb0526b8641b8c52909a9f
Mark Petersen <mpetersen@lanl.gov>
2019-07-15 11:24:06 -0600

    Add QU240wISC, EC60to30wISC, SO60to10wISC

--- testing_and_setup/compass/ocean/global_ocean/QU240wISC/init/define_base_mesh.py
+++ testing_and_setup/compass/ocean/global_ocean/QU240wISC/init/define_base_mesh.py
@@ -7,30 +7,15 @@
 %    lat - latitude, vector of length n, with entries between -90 and 90, degrees
 """
 import numpy as np
-import jigsaw_to_MPAS.mesh_definition_tools as mdt
 
 
 def cellWidthVsLatLon():
 
-    ddeg = 0.1
+    ddeg = 10
+    constantCellWidth = 240
 
     lat = np.arange(-90, 90.01, ddeg)
     lon = np.arange(-180, 180.01, ddeg)
 
-    cellWidthSouth = 15 * np.ones(lat.size)
-    cellWidthNorth = 60 * np.ones(lat.size)
-    latTransition = -30
-    latWidthTransition = 5
-
-    cellWidthVsLat = mdt.mergeCellWidthVsLat(
-        lat,
-        cellWidthSouth,
-        cellWidthNorth,
-        latTransition,
-        latWidthTransition)
-
-    cellWidth = np.ones((lat.size, lon.size))
-    for i in range(lon.size):
-        cellWidth[:, i] = cellWidthVsLat
-
+    cellWidth = constantCellWidth * np.ones((lat.size, lon.size))
     return cellWidth, lon, lat
