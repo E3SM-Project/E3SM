09d52ebacea8124feb56a5b7b114d126c4756be3
James Foucar <jgfouca@sandia.gov>
2019-07-16 11:11:51 -0600

    Remove error capturing from core ctest script
    It was preventing a non-zero return code when things failed.

--- components/scream/cmake/ctest_script.cmake
+++ components/scream/cmake/ctest_script.cmake
@@ -18,22 +18,13 @@ set(CTEST_CONFIGURE_COMMAND "${CMAKE_COMMAND} ${CTEST_SOURCE_DIRECTORY}")
 
 ctest_start(${dashboard_model} TRACK ${dashboard_track})
 
-ctest_configure(CAPTURE_CMAKE_ERROR configure_ctest_error RETURN_VALUE configure_error)
-message("configure error       = ${configure_error}")
-message("configure ctest error = ${configure_ctest_error}")
-
-ctest_build(CAPTURE_CMAKE_ERROR build_ctest_error RETURN_VALUE build_error FLAGS "-j8")
-message("build error           = ${build_error}")
-message("build ctest error     = ${build_ctest_error}")
+ctest_configure()
+ctest_build(FLAGS "-j8")
 
 if (NOT BUILD_ONLY)
-  ctest_test(PARALLEL_LEVEL ${PARALLEL_LEVEL} CAPTURE_CMAKE_ERROR test_ctest_error RETURN_VALUE test_error)
-  message("test error            = ${test_error}")
-  message("test ctest error      = ${test_ctest_error}")
+  ctest_test(PARALLEL_LEVEL ${PARALLEL_LEVEL})
 endif()
 
 if (NOT NO_SUBMIT)
-  ctest_submit(RETRY_COUNT 10 RETRY_DELAY 60 CAPTURE_CMAKE_ERROR submit_ctest_error RETURN_VALUE submit_error)
-  message("submit error            = ${submit_error}")
-  message("submit ctest error      = ${submit_ctest_error}")
+  ctest_submit(RETRY_COUNT 10 RETRY_DELAY 60)
 endif()
