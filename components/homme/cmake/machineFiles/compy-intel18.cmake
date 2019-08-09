6bcc6008a8778f8269cb404b1b7320a25b8326db
Oksana Guba <oksanaguba@gmail.com>
2019-08-09 13:32:16 -0700

    splitting cache file into intel18 and intel19 versions. intel19 build could not read homme nl file from stream, intel18 build is ok.

---
+++ components/homme/cmake/machineFiles/compy-intel18.cmake
@@ -0,0 +1,25 @@
+# O.Guba 08/2019
+# module load cmake intel/18.0.0 intelmpi/2018 mkl netcdf
+# however, NETCDF_ROOT won't be set
+
+SET(CMAKE_C_COMPILER "mpiicc" CACHE STRING "")
+SET(CMAKE_CXX_COMPILER "mpiicpc" CACHE STRING "")
+SET(CMAKE_Fortran_COMPILER "mpiifort" CACHE STRING "")
+
+SET (WITH_PNETCDF FALSE CACHE FILEPATH "")
+
+#intel18
+SET (NETCDF_DIR "/share/apps/netcdf/4.6.3/intel/18.0.0/" CACHE STRING "")
+
+#duplicated path
+#is there a better way? without this linker does not see C netcdf library
+SET (ADD_LINKER_FLAGS "-L//share/apps/netcdf/4.6.3/intel/18.0.0//lib -lnetcdff -lnetcdf" CACHE STRING "")
+#SET (ADD_Fortran_FLAGS " -traceback " CACHE STRING "")
+
+#SET (USE_MPIEXEC "srun" CACHE STRING "")
+#SET (USE_MPI_OPTIONS "--cpu_bind=cores" CACHE STRING "")
+SET (HOMME_USE_MKL "TRUE" CACHE FILEPATH "") # for Intel
+SET (USE_QUEUING FALSE CACHE BOOL "")
+# for standalone HOMME builds:
+#SET (CPRNC_DIR /lcrc/group/acme/tools/cprnc CACHE FILEPATH "")
+
