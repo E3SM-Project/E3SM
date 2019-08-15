e6bfbd615a0a8b3b6f0394503bdf164817a0c16a
Oksana Guba <oksanaguba@gmail.com>
2019-08-15 08:43:06 -0700

    added srun options. tested baro2b on 16 compy ranks (60 sec) vs baro2b on 24 skybridge ranks (75 sec) for intel18. i assume no more binding options are needed.

--- components/homme/cmake/machineFiles/compy-intel18.cmake
+++ components/homme/cmake/machineFiles/compy-intel18.cmake
@@ -16,8 +16,8 @@ SET (NETCDF_DIR "/share/apps/netcdf/4.6.3/intel/18.0.0/" CACHE STRING "")
 SET (ADD_LINKER_FLAGS "-L//share/apps/netcdf/4.6.3/intel/18.0.0//lib -lnetcdff -lnetcdf" CACHE STRING "")
 #SET (ADD_Fortran_FLAGS " -traceback " CACHE STRING "")
 
-#SET (USE_MPIEXEC "srun" CACHE STRING "")
-#SET (USE_MPI_OPTIONS "--cpu_bind=cores" CACHE STRING "")
+SET (USE_MPIEXEC "srun" CACHE STRING "")
+SET (USE_MPI_OPTIONS " --mpi=pmi2 --kill-on-bad-exit --cpu_bind=cores" CACHE STRING "")
 SET (HOMME_USE_MKL "TRUE" CACHE FILEPATH "") # for Intel
 SET (USE_QUEUING FALSE CACHE BOOL "")
 # for standalone HOMME builds:
--- components/homme/cmake/machineFiles/compy-intel19.cmake
+++ components/homme/cmake/machineFiles/compy-intel19.cmake
@@ -19,8 +19,8 @@ SET (NETCDF_DIR "$ENV{NETCDF_ROOT}" CACHE STRING "")
 SET (ADD_LINKER_FLAGS "-L/$ENV{NETCDF_ROOT}/lib -lnetcdff -lnetcdf" CACHE STRING "")
 SET (ADD_Fortran_FLAGS " -traceback " CACHE STRING "")
 
-#SET (USE_MPIEXEC "srun" CACHE STRING "")
-#SET (USE_MPI_OPTIONS "--cpu_bind=cores" CACHE STRING "")
+SET (USE_MPIEXEC "srun" CACHE STRING "")
+SET (USE_MPI_OPTIONS " --mpi=pmi2 --kill-on-bad-exit --cpu_bind=cores" CACHE STRING "")
 SET (HOMME_USE_MKL "TRUE" CACHE FILEPATH "") # for Intel
 SET (USE_QUEUING FALSE CACHE BOOL "")
 # for standalone HOMME builds:
