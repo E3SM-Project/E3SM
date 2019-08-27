90eadaae917b2bf5ce7e15a015cbe340bb2949da
James Foucar <jgfouca@sandia.gov>
2019-08-27 16:35:15 -0600

    Fix comment typo

--- components/cam/src/physics/cam/micro_p3.F90
+++ components/cam/src/physics/cam/micro_p3.F90
@@ -187,7 +187,7 @@ contains
   END SUBROUTINE p3_init_a
 
   subroutine p3_get_tables(mu_r_user, revap_user, vn_user, vm_user)
-    ! This can be called after of p3_init_b.
+    ! This can be called after p3_init_b.
     implicit none
     real(rtype), dimension(150), intent(out) :: mu_r_user
     real(rtype), dimension(300,10), intent(out) :: vn_user, vm_user, revap_user
