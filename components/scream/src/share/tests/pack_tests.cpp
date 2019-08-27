e71760175ae94e789c471c6582d902258cb3613d
Kyle Pressel <pressel@gmail.com>
2019-08-27 08:07:35 -0700

    Add != operator to Spack and test it.

--- components/scream/src/share/tests/pack_tests.cpp
+++ components/scream/src/share/tests/pack_tests.cpp
@@ -248,6 +248,7 @@ struct TestPack {
     test_pack_gen_unary_stdfn(tgamma);
 
     test_mask_gen_bin_op_all(==);
+    test_mask_gen_bin_op_all(!=); 
     test_mask_gen_bin_op_all(>=);
     test_mask_gen_bin_op_all(<=);
     test_mask_gen_bin_op_all(>);
