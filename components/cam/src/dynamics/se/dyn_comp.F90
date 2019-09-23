c5d91deb8f4150c0906f08efc5c9e43199a3e297
Peter Andrew Bogenschutz <bogensch@syrah256.llnl.gov>
2019-09-23 16:02:20 -0700

    prep IOP scream with dynamics

--- components/cam/src/dynamics/se/dyn_comp.F90
+++ components/cam/src/dynamics/se/dyn_comp.F90
@@ -332,7 +332,7 @@ CONTAINS
   subroutine dyn_run( dyn_state, rc )
 
     ! !USES:
-    use scamMod,          only: single_column, use_3dfrc
+    use scamMod,          only: single_column, iop_scream, use_3dfrc
     use se_single_column_mod, only: apply_SC_forcing
     use parallel_mod,     only : par
     use prim_driver_mod,  only: prim_run_subcycle
@@ -350,6 +350,7 @@ CONTAINS
     integer ::  n
     integer :: nets, nete, ithr
     integer :: ie
+    logical :: single_column_in
 
     ! !DESCRIPTION:
     !
@@ -368,15 +369,18 @@ CONTAINS
        nete=dom_mt(ithr)%end
        hybrid = hybrid_create(par,ithr,hthreads)
 
-!       if (.not. use_3dfrc) then
-!         do n=1,se_nsplit
+       single_column_in = single_column
+       if (single_column .and. iop_scream) single_column_in = .false.
+
+       if (.not. use_3dfrc) then
+         do n=1,se_nsplit
            ! forward-in-time RK, with subcycling
-!           call t_startf("prim_run_sybcycle")
-!           call prim_run_subcycle(dyn_state%elem,hybrid,nets,nete,&
-!               tstep, single_column, TimeLevel, hvcoord, n)
-!           call t_stopf("prim_run_sybcycle")
-!         end do
-!       endif
+           call t_startf("prim_run_sybcycle")
+           call prim_run_subcycle(dyn_state%elem,hybrid,nets,nete,&
+               tstep, single_column_in, TimeLevel, hvcoord, n)
+           call t_stopf("prim_run_sybcycle")
+         end do
+       endif
 
        if (single_column) then
          call apply_SC_forcing(dyn_state%elem,hvcoord,TimeLevel,3,.false.,nets,nete)
