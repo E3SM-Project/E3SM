55cb63827684d3d98d4fb4ce2972046a96ac0b80
Chengzhu Zhang <zhang40@llnl.gov>
2019-10-01 15:03:12 -0700

    convert unit

--- analysis_data_preprocess/process_ISLSCPII_COMP_RUNOFF.py
+++ analysis_data_preprocess/process_ISLSCPII_COMP_RUNOFF.py
@@ -9,6 +9,7 @@ import MV2
 import glob
 import numpy
 import cdutil
+from calendar import monthrange
 
 
 basedir = '/p/user_pub/e3sm/zhang40/analysis_data_e3sm_diags/ISLSCPII_GRDC/'
@@ -20,6 +21,7 @@ end_time = '199512'
 #Missing data cells are given the value of -99 over water, and -88 over land 
 #(mainly Greenland and Antarctica).
 missing = -99
+missing2 = -88
 
 nlat = 360
 nlon = 720
@@ -38,22 +40,26 @@ time.designateTime()
 time.id = "time"
 data = MV2.zeros([nTimes, nlat, nlon])
 
-
 print(data.shape)
 
 count = 0
 for year in range(1986,1996):
     for month in range(12):
+        print(year,month)
+        days_per_month = monthrange(year, month+1)[1]
+        print(days_per_month)
         imon = '{0:02d}'.format(month+1)
         fname = datapath + file_name +'_' + str(year)+imon+'.asc'
-        
-        data[count,:,:] = numpy.loadtxt(fname, skiprows=6)
+        data[count,:,:] = numpy.loadtxt(fname, skiprows=6) / days_per_month * 24. * 3600.  #converted from mm/month to kg/m2/s
         count = count+1
 
 print(count)
-data = MV2.masked_where(data == missing, data)
+#data = MV2.masked_where(data == missing, data)
+#data = MV2.masked_where(data == missing2, data)
+data = MV2.masked_where(data < 0 , data)
 out_data = cdms2.createVariable(data, axes =[time,lat,lon], id = "mrro")
-out_data.units = "mm/month"   #converted from mm/month
+#out_data.units = "mm/month"   
+out_data.units = "kg/m2/s"   #converted from mm/month to kg/m2/s
 out_data.id = "QRUNOFF" #runoff flux for cmip
 cdutil.setTimeBoundsMonthly(out_data)
 out_file.dataname = 'ISLSCP-GRDC'
