6853ac5872a3b58d1c28853a650bf74eb76088ad
chengzhuzhang <zhang40@llnl.gov>
2019-10-04 12:42:19 -0700

    rename files

--- tests/system/all_sets.py
+++ tests/system/all_sets.py
@@ -1,4 +1,12 @@
-reference_data_path = '.'
-test_data_path = '.'
-results_dir = 'all_sets_results'
+# Running the software with the API:
+#    python all_sets_api.py -d all_sets.py
+from acme_diags.parameter.core_parameter import CoreParameter
+from acme_diags.parameter.area_mean_time_series_parameter import AreaMeanTimeSeriesParameter
+from acme_diags.run import Run
+
+thing = Run()
+param = CoreParameter()
+ts_param = AreaMeanTimeSeriesParameter()
+
+thing.run_diags([param, ts_param])
 
