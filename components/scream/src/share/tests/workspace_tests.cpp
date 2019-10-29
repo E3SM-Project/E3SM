324d43fe0fc6ed559d8c419740dc9eb249928d43
Aaron Donahue <donahue.aaron@gmail.com>
2019-10-29 13:29:42 -0700

    Left out == 2 in fix.

--- components/scream/src/share/tests/workspace_tests.cpp
+++ components/scream/src/share/tests/workspace_tests.cpp
@@ -171,7 +171,7 @@ static void unittest_workspace()
 #ifdef WS_EXPENSIVE_TEST
     if (true)
 #else
-    if ( ExeSpace::concurrency() ) // the test below is expensive, we don't want all threads sweeps to run it
+    if ( ExeSpace::concurrency() == 2 ) // the test below is expensive, we don't want all threads sweeps to run it
 #endif
     {
       // Test weird take/release permutations.
