5f73364a2424fe7314f967caca1f467923eb8ff5
James Foucar <jgfouca@sandia.gov>
2019-11-16 15:06:47 -0600

    Switch back to gmake as default backend

--- cime/scripts/lib/CIME/SystemTests/system_tests_common.py
+++ cime/scripts/lib/CIME/SystemTests/system_tests_common.py
@@ -34,7 +34,7 @@ class SystemTestsCommon(object):
         self._skip_pnl = False
         self._cpllog = "med" if self._case.get_value("COMP_INTERFACE")=="nuopc" else "cpl"
         self._old_build = False
-        self._use_gmake = False
+        self._ninja     = False
         self._dry_run   = False
 
     def _init_environment(self, caseroot):
@@ -69,7 +69,7 @@ class SystemTestsCommon(object):
 
             self._case.case_setup(reset=True, test_mode=True)
 
-    def build(self, sharedlib_only=False, model_only=False, old_build=False, use_gmake=False, dry_run=False):
+    def build(self, sharedlib_only=False, model_only=False, old_build=False, ninja=False, dry_run=False):
         """
         Do NOT override this method, this method is the framework that
         controls the build phase. build_phase is the extension point
@@ -77,7 +77,7 @@ class SystemTestsCommon(object):
         """
         success = True
         self._old_build = old_build
-        self._use_gmake = use_gmake
+        self._ninja     = ninja
         self._dry_run   = dry_run
         for phase_name, phase_bool in [(SHAREDLIB_BUILD_PHASE, not model_only),
                                        (MODEL_BUILD_PHASE, not sharedlib_only)]:
@@ -127,7 +127,7 @@ class SystemTestsCommon(object):
         build.case_build(self._caseroot, case=self._case,
                          sharedlib_only=sharedlib_only, model_only=model_only,
                          save_build_provenance=not model=='cesm',
-                         use_old=self._old_build, use_gmake=self._use_gmake, dry_run=self._dry_run)
+                         use_old=self._old_build, ninja=self._ninja, dry_run=self._dry_run)
 
     def clean_build(self, comps=None):
         if comps is None:
