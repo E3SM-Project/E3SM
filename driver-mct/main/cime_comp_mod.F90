00c3931ce26b4bc1c218652e3b489c5b042732f7
Qi Tang <tang30@llnl.gov>
2019-11-29 13:16:42 -0800

    Turn off atm_c2_rof

--- driver-mct/main/cime_comp_mod.F90
+++ driver-mct/main/cime_comp_mod.F90
@@ -1588,7 +1588,7 @@ contains
 
     if (atm_present) then
        if (lnd_prognostic) atm_c2_lnd = .true.
-       if (rof_prognostic) atm_c2_rof = .true.
+       if (rof_prognostic .and. (.not. atm_prognostic)) atm_c2_rof = .true.
        if (ocn_prognostic) atm_c2_ocn = .true.
        if (ocn_present   ) atm_c2_ocn = .true. ! needed for aoflux calc if aoflux=ocn
        if (ice_prognostic) atm_c2_ice = .true.
