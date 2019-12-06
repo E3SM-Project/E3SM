ac6982fcd912b133a048b11bead517a9e7e05427
James Foucar <jgfouca@sandia.gov>
2019-12-06 14:22:53 -0700

    sp fix for lookup_ice

--- components/scream/src/physics/p3/p3_functions_table_ice_impl.hpp
+++ components/scream/src/physics/p3/p3_functions_table_ice_impl.hpp
@@ -117,8 +117,8 @@ void Functions<S,D>
   // (account for uneven spacing in lookup table for density)
   const auto rhop_le_650 = qiti_gt_small && (rhop <= 650);
   const auto rhop_gt_650 = qiti_gt_small && (rhop > 650);
-  t.dum5.set(rhop_le_650, (rhop-50)*0.005 + 1);
-  t.dum5.set(rhop_gt_650, (rhop-650)*0.004 + 4);
+  t.dum5.set(rhop_le_650, (rhop-50)*sp(0.005) + 1);
+  t.dum5.set(rhop_gt_650, (rhop-650)*sp(0.004) + 4);
 
   // set limits
   t.dumjj = IntSmallPack(t.dum5);
