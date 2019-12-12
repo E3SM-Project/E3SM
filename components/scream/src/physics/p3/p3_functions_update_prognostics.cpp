977ea9f345c2284aa1898fa6c1509507defa3bc8
Balwinder Singh <balwindersingh@gmail.com>
2019-12-12 15:41:10 -0800

    minor changes (changed a comment and deleted extra lines white spaces)

--- components/scream/src/physics/p3/p3_functions_update_prognostics.cpp
+++ components/scream/src/physics/p3/p3_functions_update_prognostics.cpp
@@ -5,7 +5,7 @@ namespace scream {
   namespace p3 {
 
     /*
-     * Explicit instantiation for doing p3 conservation functions on Reals using the
+     * Explicit instantiation for doing p3 update prognostics functions on Reals using the
 * default device.
 */
 
