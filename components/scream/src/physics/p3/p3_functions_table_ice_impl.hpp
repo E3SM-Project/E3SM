4f93d0acb172be08c7f29eb883419098af76bee9
James Foucar <jgfouca@sandia.gov>
2019-12-18 14:16:00 -0700

    fix typo

--- components/scream/src/physics/p3/p3_functions_table_ice_impl.hpp
+++ components/scream/src/physics/p3/p3_functions_table_ice_impl.hpp
@@ -1,7 +1,7 @@
 #ifndef P3_FUNCTIONS_TABLE_ICE_IMPL_HPP
 #define P3_FUNCTIONS_TABLE_ICE_IMPL_HPP
 
-#include "p3_function.hpp" // for ETI only but harmless for GPU
+#include "p3_functions.hpp" // for ETI only but harmless for GPU
 
 #include <fstream>
 
--- components/scream/src/physics/p3/p3_functions_upwind_impl.hpp
+++ components/scream/src/physics/p3/p3_functions_upwind_impl.hpp
@@ -1,7 +1,7 @@
 #ifndef P3_FUNCTIONS_UPWIND_IMPL_HPP
 #define P3_FUNCTIONS_UPWIND_IMPL_HPP
 
-#include "p3_function.hpp" // for ETI only but harmless for GPU
+#include "p3_functions.hpp" // for ETI only but harmless for GPU
 
 namespace scream {
 namespace p3 {
