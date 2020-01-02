b7c3c5e2781c0c064776b46a203bdea4c05cc708
Jon Wolfe <jonbob@lanl.gov>
2020-01-02 10:41:37 -0800

    Fix issues with adding atm fields to rof coupling

--- cime/src/drivers/mct/main/prep_rof_mod.F90
+++ cime/src/drivers/mct/main/prep_rof_mod.F90
@@ -169,7 +169,7 @@ contains
 
        allocate(l2r_rx(num_inst_rof))
        do eri = 1,num_inst_rof
-          call mct_avect_init(l2r_rx(eri), rList=seq_flds_x2r_fields, lsize=lsize_r)
+          call mct_avect_init(l2r_rx(eri), rList=seq_flds_l2x_fluxes_to_rof, lsize=lsize_r)
           call mct_avect_zero(l2r_rx(eri))
        end do
 
@@ -191,7 +191,7 @@ contains
           !
           ! (This listDiff works even if have_irrig_field is false.)
           call shr_string_listDiff( &
-               list1 = seq_flds_x2r_fluxes, &
+               list1 = seq_flds_l2x_fluxes_to_rof, &
                list2 = irrig_flux_field, &
                listout = lnd2rof_normal_fluxes)
        endif
@@ -218,7 +218,7 @@ contains
 
        allocate(a2r_rx(num_inst_rof))
        do eri = 1,num_inst_rof
-          call mct_avect_init(a2r_rx(eri), rList=seq_flds_x2r_fields, lsize=lsize_r)
+          call mct_avect_init(a2r_rx(eri), rList=seq_flds_a2x_fields_to_rof, lsize=lsize_r)
           call mct_avect_zero(a2r_rx(eri))
        end do
 
@@ -718,8 +718,8 @@ contains
     do eri = 1,num_inst_rof
        eai = mod((eri-1),num_inst_atm) + 1
        r2x_rx => component_get_c2x_cx(rof(eri))
-       call seq_map_map(mapper_Sa2r, a2racc_ax(eai), a2r_rx(eri), fldlist=seq_flds_a2x_states, norm=.true.)
-       call seq_map_map(mapper_Fa2r, a2racc_ax(eai), a2r_rx(eri), fldlist=seq_flds_a2x_fluxes, norm=.true.)
+       call seq_map_map(mapper_Sa2r, a2racc_ax(eai), a2r_rx(eri), fldlist=seq_flds_a2x_states_to_rof, norm=.true.)
+       call seq_map_map(mapper_Fa2r, a2racc_ax(eai), a2r_rx(eri), fldlist=seq_flds_a2x_fluxes_to_rof, norm=.true.)
     end do
     call t_drvstopf  (trim(timer))
 
