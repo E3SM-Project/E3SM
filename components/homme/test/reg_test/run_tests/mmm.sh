120df8f24cf1c707e8dad6ecb0d6c15bf9db958f
Oksana Guba <oksana.guba@gmail.com>
2020-01-10 17:36:34 -0600

    fix naming for CvsF tests

--- components/homme/test/reg_test/run_tests/mmm.sh
+++
@@ -1,6 +0,0 @@
-
-name=${AANAME}
-ttype=${AATTYPE}
-
-
-
--- components/homme/test/reg_test/run_tests/nnn.sh
+++
@@ -1,8 +0,0 @@
-
-source mmm2.sh
-
-sed -e s/BBNAME/${name}/ -e s/BBTTYPE/${ttype}/  \
-     theta-xx-test-template.cmake > ${name}.cmake
-
-
-
