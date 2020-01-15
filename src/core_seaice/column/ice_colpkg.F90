b5958a1dde8eeb3d4a31646e694556eded08556a
Adrian K. Turner <akt@pn1707938.lanl.gov>
2020-01-15 10:07:47 -0700

    Added a prescribed mode

--- src/core_seaice/column/ice_colpkg.F90
+++ src/core_seaice/column/ice_colpkg.F90
@@ -56,7 +56,9 @@
            colpkg_snow_temperature, &
            colpkg_liquidus_temperature, &
            colpkg_sea_freezing_temperature, &
-           colpkg_enthalpy_snow
+           colpkg_enthalpy_ice, &
+           colpkg_enthalpy_snow, &
+           colpkg_salinity_profile
 
       ! warning messages
       public :: &
@@ -379,7 +381,31 @@
       end subroutine colpkg_init_thermo
 
 !=======================================================================
+! Initial salinity profile
+!
+! authors: C. M. Bitz, UW
+!          William H. Lipscomb, LANL
 
+      function colpkg_salinity_profile(zn) result(salinity)
+
+        use ice_colpkg_shared, only: saltmax
+        use ice_constants_colpkg, only: c1, c2, pi
+
+        real(kind=dbl_kind), intent(in) :: &
+             zn ! depth
+
+        real(kind=dbl_kind) :: &
+             salinity ! initial salinity profile
+
+        real (kind=dbl_kind), parameter :: &
+             nsal    = 0.407_dbl_kind, &
+             msal    = 0.573_dbl_kind
+
+        salinity = (saltmax/c2)*(c1-cos(pi*zn**(nsal/(msal+zn))))
+
+      end function colpkg_salinity_profile
+
+!=======================================================================
 ! Compute orbital parameters for the specified date.
 !
 ! author:  Bruce P. Briegleb, NCAR 
@@ -1692,6 +1718,33 @@
         endif
 
       end function colpkg_snow_temperature
+
+!=======================================================================
+
+      function colpkg_enthalpy_ice(zTin, zSin) result(qin)
+
+        use ice_colpkg_shared, only: ktherm
+        use ice_mushy_physics, only: enthalpy_mush
+        use ice_constants_colpkg, only: depressT, rhoi, cp_ice, Lfresh, cp_ocn, c1
+
+        real(kind=dbl_kind), intent(in) :: zTin
+        real(kind=dbl_kind), intent(in) :: zSin
+        real(kind=dbl_kind) :: qin
+
+        real(kind=dbl_kind) :: Tmlt
+
+        if (ktherm == 2) then
+
+           qin = enthalpy_mush(zTin, zSin)
+
+        else
+
+           Tmlt = -zSin*depressT
+           qin = -(rhoi * (cp_ice*(Tmlt-zTin) + Lfresh*(c1-Tmlt/zTin) - cp_ocn*Tmlt))
+
+        endif
+
+      end function colpkg_enthalpy_ice
 
 !=======================================================================
 
