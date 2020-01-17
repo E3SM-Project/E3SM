003987d03b639b47b9f2bb48439f68b87ee4027c
Luca Bertagna <lbertag@sandia.gov>
2020-01-16 18:05:52 -0700

    Homme: bug fix in preqx_kokkos

--- components/homme/src/preqx_kokkos/cxx/CaarFunctorImpl.hpp
+++ components/homme/src/preqx_kokkos/cxx/CaarFunctorImpl.hpp
@@ -114,7 +114,7 @@ struct CaarFunctorImpl {
   }
 
   void init_buffers (const FunctorsBuffersManager& fbm) {
-    Errors::runtime_check(fbm.allocated_size()>requested_buffer_size(), "Error! Buffers size not sufficient.\n");
+    Errors::runtime_check(fbm.allocated_size()>=requested_buffer_size(), "Error! Buffers size not sufficient.\n");
 
     const int nteams = get_num_concurrent_teams(m_policy);
 
