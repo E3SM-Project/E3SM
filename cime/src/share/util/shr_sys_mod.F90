64eaf4bbb32ae50e65bf6650946f305eb0abb7f7
Patrick Worley <worleyph@gmail.com>
2020-01-18 17:31:05 -0800

    Document when shr_sys_irtc can be used in multi-threaded region
    The addition of the threadprivate declaration to shr_sys_irtc is
    sufficient to ensure thread safety as shr_sys_irtc is used within
    E3SM currently. It is not sufficient for all possible usage scenarios,
    e.g., between multi-threaded regions when the two regions have
    different numbers of active threads. Comments are added to
    shr_sys_irtc to document this.
    
    [BFB]

--- cime/src/share/util/shr_sys_mod.F90
+++ cime/src/share/util/shr_sys_mod.F90
@@ -224,6 +224,14 @@ integer(SHR_KIND_I8) FUNCTION shr_sys_irtc( rate )
 
 !-------------------------------------------------------------------------------
 ! emulates Cray/SGI irtc function (returns clock tick since last reboot)
+!
+! This function is not intended to measure elapsed time between
+! multi-threaded regions with different numbers of threads. However,
+! use of the threadprivate declaration does guarantee accurate
+! measurement per thread within a single multi-threaded region as
+! long as the number of threads is not changed dynamically during
+! execution within the multi-threaded region.
+!
 !-------------------------------------------------------------------------------
 
    call system_clock(count=count,count_rate=count_rate, count_max=count_max)
