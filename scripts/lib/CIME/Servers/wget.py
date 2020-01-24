4cab9d2dff58334aee18c63835a052239dbf1214
Jim Edwards <jedwards@ucar.edu>
2020-01-24 09:33:07 -0700

    add timeouts for ftp and wget

--- scripts/lib/CIME/Servers/wget.py
+++ scripts/lib/CIME/Servers/wget.py
@@ -6,6 +6,24 @@ from CIME.XML.standard_module_setup import *
 from CIME.Servers.generic_server import GenericServer
 logger = logging.getLogger(__name__)
 
+import signal
+from contextlib import contextmanager
+
+class TimeoutException(Exception): pass
+
+@contextmanager
+def time_limit(seconds):
+    #pylint: disable=unused-argument
+    def signal_handler(signum, frame):
+        raise TimeoutException("Timed out!")
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+
 class WGET(GenericServer):
     def __init__(self, address, user='', passwd=''):
         self._args = ''
@@ -15,24 +33,38 @@ class WGET(GenericServer):
             self._args += "--password {} ".format(passwd)
         self._server_loc = address
 
-        cmd = "wget {} --no-check-certificate --spider {}".format(self._args, address)
-        err, output, _ = run_cmd(cmd, combine_output=True)
-        expect(err == 0,"Could not connect to repo via '{}'\nThis is most likely either a proxy, or network issue.\nOutput:\n{}".format(cmd, output))
+    @classmethod
+    def wget_login(cls, address, user='', passwd=''):
+        args = ''
+        if user:
+            args += "--user {} ".format(user)
+        if passwd:
+            args += "--password {} ".format(passwd)
+
+        try:
+            with time_limit(30):
+                err,_,_ = run_cmd("wget {} --spider {}".format(args, address), verbose=True)
+                expect(err == 0,"Could not connect to repo '{0}'\nThis is most likely either a proxy, or network issue .")
+            return cls(address,user=user,passwd=passwd)
+        except TimeoutException as e:
+            logger.warning("wget login timeout! {}".format(e))
+            return None
+
 
     def fileexists(self, rel_path):
         full_url = os.path.join(self._server_loc, rel_path)
-        stat, out, err = run_cmd("wget {} --no-check-certificate --spider {}".format(self._args, full_url))
+        stat, out, err = run_cmd("wget {} --spider {}".format(self._args, full_url))
         if (stat != 0):
-            logging.warning("FAIL: Repo '{}' does not have file '{}'\nReason:{}\n{}\n".format(self._server_loc, full_url, out, err))
+            logging.warning("FAIL: Repo '{}' does not have file '{}'\nReason:{}\n{}\n".format(self._server_loc, full_url, out.encode('utf-8'), err.encode('utf-8')))
             return False
         return True
 
     def getfile(self, rel_path, full_path):
         full_url = os.path.join(self._server_loc, rel_path)
         stat, output, errput = \
-                run_cmd("wget {} {} -nc --no-check-certificate --output-document {}".format(self._args, full_url, full_path))
+                run_cmd("wget {} {} -nc --output-document {}".format(self._args, full_url, full_path))
         if (stat != 0):
-            logging.warning("wget failed with output: {} and errput {}\n".format(output, errput))
+            logging.warning("wget failed with output: {} and errput {}\n".format(output.encode('utf-8'), errput.encode('utf-8')))
             # wget puts an empty file if it fails.
             try:
                 os.remove(full_path)
@@ -46,11 +78,11 @@ class WGET(GenericServer):
     def getdirectory(self, rel_path, full_path):
         full_url = os.path.join(self._server_loc, rel_path)
         stat, output, errput = \
-            run_cmd("wget  {} {} -r -N --no-check-certificate --no-directories ".format(self._args, full_url+os.sep), from_dir=full_path)
+            run_cmd("wget  {} {} -r -N --no-directories ".format(self._args, full_url+os.sep), from_dir=full_path)
         logger.debug(output)
         logger.debug(errput)
         if (stat != 0):
-            logging.warning("wget failed with output: {} and errput {}\n".format(output, errput))
+            logging.warning("wget failed with output: {} and errput {}\n".format(output.encode('utf-8'), errput.encode('utf-8')))
             # wget puts an empty file if it fails.
             try:
                 os.remove(full_path)
