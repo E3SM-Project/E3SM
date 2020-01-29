62241e2c5d1ec26af2b856b24d2dc59b2880d348
James Foucar <jgfouca@sandia.gov>
2020-01-29 09:57:04 -0700

    Update comment

--- components/scream/src/share/scream_kokkos.hpp
+++ components/scream/src/share/scream_kokkos.hpp
@@ -3,8 +3,11 @@
 
 // Funnel all Kokkos includes through this file.
 
-// We do not want to see warnings coming from Kokkos
-#pragma GCC system_header
+// We do not want to see warnings coming from Kokkos. This is currently
+// handled through CMAKE, using the SYSTEM qualifier when setting up
+// target_include_directories. Future Kokkos changes may complicate
+// this approach.
+//#pragma GCC system_header
 
 #include <Kokkos_Core.hpp>
 
