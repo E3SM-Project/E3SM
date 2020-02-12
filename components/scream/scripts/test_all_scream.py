f99f7a6a8cc32a57d3621ff5bd3f2bf9c9b8168a
James Foucar <jgfouca@sandia.gov>
2020-02-12 15:03:12 -0700

    Fix

--- components/scream/scripts/test_all_scream.py
+++ components/scream/scripts/test_all_scream.py
@@ -54,7 +54,9 @@ class TestAllScream(object):
             expect(self._machine, "If dashboard submit request, must provide machine name")
 
         # Compute baseline info
-        expect(not (self._baseline_ref and self._baseline_ref),
+        import pdb
+        pdb.set_trace()
+        expect(not (self._baseline_ref and self._baseline_dir),
                "Makes no sense to specify a baseline generation commit if using pre-existing baselines ")
         if self._baseline_dir is None:
             if self._baseline_ref is None:
