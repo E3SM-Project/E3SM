c292a4ed2c74b05f69207d27047c655e26d08f1a
James Foucar <jgfouca@sandia.gov>
2020-02-21 13:58:57 -0700

    Fixes for waterman.cmake

--- components/homme/cmake/machineFiles/waterman.cmake
+++ components/homme/cmake/machineFiles/waterman.cmake
@@ -4,14 +4,16 @@
 SET(BUILD_HOMME_PREQX_KOKKOS TRUE CACHE BOOL "")
 SET (WITH_PNETCDF FALSE CACHE FILEPATH "")
 
+set (USE_NUM_PROCS 1 CACHE STRING "Num mpiprocs to use")
 set (NETCDF_DIR $ENV{NETCDF_ROOT} CACHE FILEPATH "")
+set (NetCDF_Fortran_PATH /ascldap/users/lbertag/workdir/libs/netcdf/netcdf-f/netcdf-f-install/waterman/gcc CACHE FILEPATH "")
 set (HDF5_DIR $ENV{HDF5_ROOT} CACHE FILEPATH "")
 set (ZLIB_DIR $ENV{ZLIB_ROOT} CACHE FILEPATH "")
 set (CURL_ROOT $ENV{CURL_ROOT} CACHE FILEPATH "")
 set (CURL_LIBRARY -L$ENV{CURL_ROOT}/lib -lcurl CACHE LIST "")
 
 SET (HAVE_EXTRAE TRUE CACHE BOOL "")
-SET (Extrae_LIBRARY "-L${NETCDF_DIR}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -ldl -lz" CACHE STRING "")
+SET (Extrae_LIBRARY "-L${NETCDF_DIR}/lib -L${NetCDF_Fortran_PATH}/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -ldl -lz" CACHE STRING "")
 
 SET(HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
 SET(USE_QUEUING FALSE CACHE BOOL "")
