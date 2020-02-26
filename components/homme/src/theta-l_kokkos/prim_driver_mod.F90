11d3d75456809815b3debadb3a06bbe6ddba5e63
Luca Bertagna <lbertag@sandia.gov>
2020-02-26 16:02:23 -0700

    Hommexx: added HOMMEXX_GB_CONFIG guard
    It replaces a hard-coded 'if 0'.

--- components/homme/src/theta-l_kokkos/prim_driver_mod.F90
+++ components/homme/src/theta-l_kokkos/prim_driver_mod.F90
@@ -368,8 +368,7 @@ contains
       compute_diagnostics = .false.
     endif
 
-#if 0
-!!!!!!!!!!!! TODO: uncomment for CAM runs
+#ifndef HOMMEXX_GB_CONFIG
     call t_startf('push_to_cxx')
     call push_forcing_to_c(elem_derived_FM,   elem_derived_FVTheta, elem_derived_FT, &
                            elem_derived_FPHI, elem_derived_FQ)
