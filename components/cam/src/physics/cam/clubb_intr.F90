defd14413b597f3a2dbc0b11dd4e9ef58de49f1b
Wuyin Lin <wlin@bnl.gov>
2020-03-06 11:36:23 -0800

    Revert "Merge singhbalwinder/atm/clubbv2_rebased_1_for_remrgings (PR #3109)"
    This reverts commit 863ea9f1396cf49f85a24ff4e37b83716dca2281, reversing
    changes made to 55c832035c99194b7ddaa03b28a0e1c6bf3c1323.
    
      modified:   components/cam/src/physics/cam/clubb_intr.F90

--- components/cam/src/physics/cam/clubb_intr.F90
+++ components/cam/src/physics/cam/clubb_intr.F90
@@ -579,9 +579,7 @@ end subroutine clubb_init_cnst
 
 
     !----- Begin Code -----
-    !$OMP PARALLEL
     l_do_expldiff_rtm_thlm = do_expldiff
-    !$OMP END PARALLEL
     
     allocate( &
        pdf_params_chnk(pcols,begchunk:endchunk),   &
