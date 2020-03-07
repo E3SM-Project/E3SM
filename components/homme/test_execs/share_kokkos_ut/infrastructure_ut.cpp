3d2e2e1c80fabe0dcfa0fc8637ccbc4128fc4619
Andrew M. Bradley <ambradl@sandia.gov>
2020-03-06 17:11:23 -0700

    Hommexx: Update unit test.

--- components/homme/test_execs/share_kokkos_ut/infrastructure_ut.cpp
+++ components/homme/test_execs/share_kokkos_ut/infrastructure_ut.cpp
@@ -86,42 +86,46 @@ TEST_CASE("ExecSpaceDefs",
     for (int num_elem = 0; num_elem <= 10000; num_elem += 10) {
       for (int qsize = 1; qsize <= 30; ++qsize) {
         for (bool prefer_threads : {true, false}) {
-          Homme::ThreadPreferences tp;
-          tp.prefer_threads = prefer_threads;
-          tp.max_vectors_usable = plev;
-          const int npi = num_elem*qsize;
-          const auto tv = Homme::Parallel::team_num_threads_vectors_for_gpu(
-            num_device_warps, num_threads_per_warp,
-            min_warps_per_team, max_warps_per_team,
-            npi, tp);
-          // Correctness tests.
-          test_basics(tp, tv);
-          REQUIRE(Homme::prevpow2(tv.second) == tv.second);
-          REQUIRE(tv.first * tv.second >= num_threads_per_warp*min_warps_per_team);
-          REQUIRE(tv.first * tv.second <= num_threads_per_warp*max_warps_per_team);
-          // Tests for good behavior.
-          REQUIRE(tv.first * tv.second >= min_warps_per_team * num_threads_per_warp);
-          if (npi >= num_device_warps*num_threads_per_warp)
-            REQUIRE(tv.first * tv.second == min_warps_per_team * num_threads_per_warp);
+          for (bool prefer_larger_team : {true, false}) {
+            Homme::ThreadPreferences tp;
+            tp.prefer_threads = prefer_threads;
+            tp.max_vectors_usable = plev;
+            tp.prefer_larger_team = prefer_larger_team;
+            const int npi = num_elem*qsize;
+            const auto tv = Homme::Parallel::team_num_threads_vectors_for_gpu(
+              num_device_warps, num_threads_per_warp,
+              min_warps_per_team, max_warps_per_team,
+              npi, tp);
+            // Correctness tests.
+            test_basics(tp, tv);
+            REQUIRE(Homme::prevpow2(tv.second) == tv.second);
+            REQUIRE(tv.first * tv.second >= num_threads_per_warp*min_warps_per_team);
+            REQUIRE(tv.first * tv.second <= num_threads_per_warp*max_warps_per_team);
+            // Tests for good behavior.
+            REQUIRE(tv.first * tv.second >= min_warps_per_team * num_threads_per_warp);
+            if ( ! prefer_larger_team && npi >= num_device_warps*num_threads_per_warp)
+              REQUIRE(tv.first * tv.second == min_warps_per_team * num_threads_per_warp);
+          }
         }
       }
     }
     // Numbers are
-    //     {#elem, qsize, prefer_threads, #thread, #vector}.
-    static const int cases[][5] = {{1, 1, 1, 16, 32},
-                                   {96, 30, 1, 16, 8},
-                                   {96, 30, 0, 4, 32}};
+    //     {#elem, qsize, prefer_threads, prefer_larger_team, #thread, #vector}.
+    static const int cases[][6] = {{1, 1, 1, 0, 16, 32},
+                                   {96, 30, 1, 0, 16, 8},
+                                   {96, 30, 0, 0, 4, 32}};
     for (unsigned int i = 0; i < sizeof(cases)/sizeof(*cases); ++i) {
       const auto& c = cases[i];
       Homme::ThreadPreferences tp;
       tp.max_vectors_usable = plev/2;
       tp.prefer_threads = c[2];
+      tp.prefer_larger_team = c[3];
       const auto tv = Homme::Parallel::team_num_threads_vectors_for_gpu(
         num_device_warps, num_threads_per_warp,
         min_warps_per_team, max_warps_per_team,
         c[0]*c[1], tp);
-      REQUIRE(tv.first == c[3]);
-      REQUIRE(tv.second == c[4]);
+      REQUIRE(tv.first == c[4]);
+      REQUIRE(tv.second == c[5]);
     }
   }
 }
