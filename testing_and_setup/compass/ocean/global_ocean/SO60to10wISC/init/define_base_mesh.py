a3b819a47b9ec2937ab89ce24214cdfa8a67a059
Xylar Asay-Davis <xylarstorm@gmail.com>
2020-03-16 16:57:11 -0500

    Fix sign of transition offset
    Write out the signed distance for debugging purposes.

--- testing_and_setup/compass/ocean/global_ocean/SO60to10wISC/init/define_base_mesh.py
+++ testing_and_setup/compass/ocean/global_ocean/SO60to10wISC/init/define_base_mesh.py
@@ -3,6 +3,7 @@ import jigsaw_to_MPAS.mesh_definition_tools as mdt
 from jigsaw_to_MPAS.coastal_tools import signed_distance_from_geojson, \
     compute_cell_width
 from geometric_features import read_feature_collection
+import xarray
 
 # Uncomment to plot the cell size distribution.
 # import matplotlib
@@ -48,15 +49,22 @@ def cellWidthVsLatLon():
     signed_distance = signed_distance_from_geojson(fc, lon, lat,
                                                    max_length=0.25)
 
+    da = xarray.DataArray(signed_distance,
+                          dims=['y', 'x'],
+                          coords={'y': lat, 'x': lon},
+                          name='signed_distance')
+    cw_filename = 'signed_distance.nc'
+    da.to_netcdf(cw_filename)
+
     # multiply by 5 because transition_width gets multiplied by 0.2 in
     # compute_cell_width
     # Equivalent to 10 degrees latitude
     trans_width = 5*1100e3
-    # the last term compensates for the offset in compute_cell_width
+    # The last term compensates for the offset in compute_cell_width.
     # The middle of the transition is ~2.5 degrees (300 km) south of the
     # region boundary to best match previous transition at 48 S. (The mean lat
     # of the boundary is 45.5 S.)
-    trans_start = 300e3 - 0.5 * trans_width
+    trans_start = -300e3 - 0.5 * trans_width
     dx_min = 10.
 
     cellWidth = compute_cell_width(signed_distance, cellWidth, lon,
