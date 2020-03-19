c67488a283a584cbd3d288123bbfacce961e256b
James Foucar <jgfouca@sandia.gov>
2020-03-19 14:51:14 -0600

    Forgot to add this to recent PR

--- components/scream/src/physics/p3/p3_functions_evaporate_sublimate_precip.cpp
+++ components/scream/src/physics/p3/p3_functions_evaporate_sublimate_precip.cpp
@@ -2,14 +2,14 @@
 #include "share/scream_types.hpp"
 
 namespace scream {
-  namespace p3 {
+namespace p3 {
 
-    /*
-     * Explicit instantiation for doing p3 update prognostics functions on Reals using the
-     * default device.
-     */
+/*
+ * Explicit instantiation for doing p3 update prognostics functions on Reals using the
+ * default device.
+ */
 
-    template struct Functions<Real,DefaultDevice>;
+template struct Functions<Real,DefaultDevice>;
 
-  } // namespace p3
+} // namespace p3
 } // namespace scream
