cef71a9d31b71555080cf650b063ec0381d24b18
Aaron Donahue <donahue.aaron@gmail.com>
2020-03-20 13:48:41 -0700

    Update Livermore Computing configuration files to support running E3SM testing suite.
    Fix issue with YAML CMAKE file in SCREAM.

--- components/scream/cmake/YamlCpp.cmake
+++ components/scream/cmake/YamlCpp.cmake
@@ -9,9 +9,9 @@ define_property(GLOBAL
 get_property(IS_YAMLCPP_ALREADY_BUILT GLOBAL PROPERTY E3SM_YAMLCPP_ALREADY_BUILT SET)
 
 if (NOT IS_YAMLCPP_ALREADY_BUILT)
-  set (YAMLCPP_SRC_DIR ${SCREAM_SOURCE_DIR}/../../externals/yaml-cpp)
+  set (YAMLCPP_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../../externals/yaml-cpp)
   set (YAMLCPP_BIN_DIR ${CMAKE_BINARY_DIR}/externals/yaml-cpp)
-
+  message("YAML SRC DIR: ${YAMLCPP_SRC_DIR}")
   set (BUILD_TESTING FALSE)
   add_subdirectory (${YAMLCPP_SRC_DIR} ${YAMLCPP_BIN_DIR})
 
