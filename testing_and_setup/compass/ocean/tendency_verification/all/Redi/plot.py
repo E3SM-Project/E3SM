788d41e226dac0131ce20c8445f0302fe2d21558
Mark Petersen <mpetersen@lanl.gov>
2020-03-25 12:56:52 -0600

    Update COMPASS visualization script

--- testing_and_setup/compass/ocean/tendency_verification/all/Redi/plot.py
+++ testing_and_setup/compass/ocean/tendency_verification/all/Redi/plot.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
 
-fig = plt.gcf()
-fig.set_size_inches(20.0, 15.0)
 
 # mrp: read from file mesh after nx,ny attributes are added:
 nx = 10  # ncfileMesh.getncattr('nx')
@@ -17,21 +15,27 @@ ny = 10  # ncfileMesh.getncattr('ny')
 iz = 5
 
 tests = ['test1', 'test2', 'test3']
-ntests = len(tests)
+nTests = len(tests)
 grids = ['res1', 'res2', 'res3', 'res4']
 dx = [10, 20, 50, 100]
-ngrids = len(grids)
+nGrids = len(grids)
 tracers = ['tracer1', 'tracer2', 'tracer3']
+nTracers = len(tracers)
 
-difL2 = np.zeros([ntests, ngrids, 3])
-errL2 = np.zeros([ntests, ngrids, 3])
-for i in range(ntests):
+difL2 = np.zeros([nTests, nGrids, nTracers])
+errL2 = np.zeros([nTests, nGrids, nTracers])
+for i in range(nTests):
     test = tests[i]
-    for j in range(len(grids)):
+
+    for j in range(nGrids):
         grid = grids[j]
         ncfileIC = Dataset('../' + test + '_' + grid + '/init.nc', 'r')
         ncfile = Dataset('../' + test + '_' + grid + '/output.nc', 'r')
-        for k in range(len(tracers)):
+        fig = plt.gcf()
+        plt.clf()
+        fig.set_size_inches(20.0, 15.0)
+
+        for k in range(nTracers):
             tracer = tracers[k]
             sol = np.reshape(
                 ncfileIC.variables[tracer + 'Tend'][0, :, iz], [ny, nx])
@@ -40,15 +44,46 @@ for i in range(ntests):
             dif = abs(var[1:ny - 1, 1:nx - 1] - sol[1:ny - 1, 1:nx - 1])
             err = abs((var[1:ny - 1, 1:nx - 1] - sol[1:ny - \
                       1, 1:nx - 1]) / sol[1:ny - 1, 1:nx - 1])
+            print(i,j,k,np.size(difL2))
             difL2[i, j, k] = np.sqrt(np.mean(dif[:]**2))
             errL2[i, j, k] = np.sqrt(np.mean(err[:]**2))
             #errL2[i,j,k] = np.max(err[:])
+
+            # --- Every other row in y needs to average two neighbors in x on planar hex mesh
+            var_avg = var
+            sol_avg = sol
+            for iy in range(0, ny, 2):
+                for ix in range(1, nx - 2):
+                    var_avg[iy, ix] = (var[iy, ix + 1] + var[iy, ix]) / 2.0
+                    sol_avg[iy, ix] = (sol[iy, ix + 1] + sol[iy, ix]) / 2.0
+
+            ax1 = plt.subplot(nTracers, 3, 3 * k + 1)
+            plt.imshow(var_avg[1:ny - 1, 1:nx - 1])
+            plt.colorbar()
+            plt.title(test + ' ' + grid + ' computed')
+            
+            ax2 = plt.subplot(nTracers, 3, 3 * k + 2)
+            plt.imshow(sol_avg[1:ny - 1, 1:nx - 1])
+            plt.colorbar()
+            plt.title(test + ' ' + grid + ' solution')
+
+            ax3 = plt.subplot(nTracers, 3, 3 * k + 3)
+            plt.imshow(var_avg[1:ny - 1, 1:nx - 1] - sol_avg[1:ny - 1, 1:nx - 1])
+            plt.colorbar()
+            plt.title('error')
+
+        plt.savefig(test + '_' + grid + '_sections.png')
+
         ncfileIC.close()
         ncfile.close()
 
-for i in range(ntests):
+fig = plt.gcf()
+plt.clf()
+fig.set_size_inches(20.0, 15.0)
+
+for i in range(nTests):
     test = tests[i]
-    plt.subplot(ntests, 3, 3 * i + 1)
+    plt.subplot(nTests, 3, 3 * i + 1)
     for k in range(len(tracers)):
         tracer = tracers[k]
         plt.loglog(dx, difL2[i, :, k], '-x', label=tracer)
@@ -58,9 +93,9 @@ for i in range(ntests):
     plt.grid()
 plt.xlabel('cell width, km')
 
-for i in range(ntests):
+for i in range(nTests):
     test = tests[i]
-    plt.subplot(ntests, 3, 3 * i + 2)
+    plt.subplot(nTests, 3, 3 * i + 2)
     for k in range(len(tracers)):
         tracer = tracers[k]
         plt.loglog(dx, errL2[i, :, k], '-x', label=tracer)
@@ -71,9 +106,9 @@ for i in range(ntests):
     plt.grid()
 plt.xlabel('cell width, km')
 
-for i in range(ntests):
+for i in range(nTests):
     test = tests[i]
-    plt.subplot(ntests, 3, 3 * i + 3)
+    plt.subplot(nTests, 3, 3 * i + 3)
     for k in range(len(tracers)):
         tracer = tracers[k]
         plt.loglog(dx, difL2[i, :, k] /
@@ -83,4 +118,4 @@ for i in range(ntests):
     plt.grid()
 plt.xlabel('cell width, km')
 
-plt.savefig('output.png')
+plt.savefig('convergence.png')
