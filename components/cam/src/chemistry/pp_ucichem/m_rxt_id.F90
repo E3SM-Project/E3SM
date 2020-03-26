b869fe14f0cdb560558b38c115b9c0a9abae9c84
Juno Hsu <junoh@uci.edu>
2020-03-26 15:00:52 -0700

    UCIchem Fortran source code are added

---
+++ components/cam/src/chemistry/pp_ucichem/m_rxt_id.F90
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                
+      module m_rxt_id                                                           
+                                                                                
+      implicit none                                                             
+                                                                                
+      integer, parameter :: rid_jo2 =    1                                      
+      integer, parameter :: rid_jh2o2 =    2                                    
+      integer, parameter :: rid_jch2o_a =    3                                  
+      integer, parameter :: rid_jch2o_b =    4                                  
+      integer, parameter :: rid_jch3ooh =    5                                  
+      integer, parameter :: rid_jc2h5ooh =    6                                 
+      integer, parameter :: rid_jno2 =    7                                     
+      integer, parameter :: rid_jno3 =    8                                     
+      integer, parameter :: rid_jn2o5 =    9                                    
+      integer, parameter :: rid_jhno3 =   10                                    
+      integer, parameter :: rid_jhno4 =   11                                    
+      integer, parameter :: rid_jch3cho =   12                                  
+      integer, parameter :: rid_jpan =   13                                     
+      integer, parameter :: rid_jacet_a =   14                                  
+      integer, parameter :: rid_jacet_b =   15                                  
+      integer, parameter :: rid_jmvkmacr =   16                                 
+      integer, parameter :: rid_usr_jo3_a =   17                                
+      integer, parameter :: rid_usr_jo3_b =   18                                
+      integer, parameter :: rid_usr_jo3_c =   19                                
+      integer, parameter :: rid_3b_H2O2 =   24                                  
+      integer, parameter :: rid_2b_H2O2 =   25                                  
+      integer, parameter :: rid_3b_HNO3 =   33                                  
+      integer, parameter :: rid_3b_HNO4 =   34                                  
+      integer, parameter :: rid_3b_N2O5 =   35                                  
+      integer, parameter :: rid_3b_PAN =   36                                   
+      integer, parameter :: rid_Keq_HNO4 =   37                                 
+      integer, parameter :: rid_Keq_N2O5 =   38                                 
+      integer, parameter :: rid_Keq_PAN =   39                                  
+      integer, parameter :: rid_het_N2O5 =   40                                 
+      integer, parameter :: rid_het_NO3 =   41                                  
+      integer, parameter :: rid_het_HO2 =   42                                  
+      integer, parameter :: rid_3b_OHHNO3 =   44                                
+      integer, parameter :: rid_3b_OH_CO =   48                                 
+      integer, parameter :: rid_usr_DMS_OH =   85                               
+      integer, parameter :: rid_usr_SO2_OH =   86                               
+                                                                                
+      integer, parameter :: rid_r0020 =   20                                    
+      integer, parameter :: rid_r0021 =   21                                    
+      integer, parameter :: rid_r0022 =   22                                    
+      integer, parameter :: rid_r0023 =   23                                    
+      integer, parameter :: rid_r0026 =   26                                    
+      integer, parameter :: rid_r0027 =   27                                    
+      integer, parameter :: rid_r0028 =   28                                    
+      integer, parameter :: rid_r0029 =   29                                    
+      integer, parameter :: rid_r0030 =   30                                    
+      integer, parameter :: rid_r0031 =   31                                    
+      integer, parameter :: rid_r0032 =   32                                    
+      integer, parameter :: rid_r0043 =   43                                    
+      integer, parameter :: rid_r0045 =   45                                    
+      integer, parameter :: rid_r0046 =   46                                    
+      integer, parameter :: rid_r0047 =   47                                    
+      integer, parameter :: rid_r0049 =   49                                    
+      integer, parameter :: rid_r0050 =   50                                    
+      integer, parameter :: rid_r0051 =   51                                    
+      integer, parameter :: rid_r0052 =   52                                    
+      integer, parameter :: rid_r0053 =   53                                    
+      integer, parameter :: rid_r0054 =   54                                    
+      integer, parameter :: rid_r0055 =   55                                    
+      integer, parameter :: rid_r0056 =   56                                    
+      integer, parameter :: rid_r0057 =   57                                    
+      integer, parameter :: rid_r0058 =   58                                    
+      integer, parameter :: rid_r0059 =   59                                    
+      integer, parameter :: rid_r0060 =   60                                    
+      integer, parameter :: rid_r0061 =   61                                    
+      integer, parameter :: rid_r0062 =   62                                    
+      integer, parameter :: rid_r0063 =   63                                    
+      integer, parameter :: rid_r0064 =   64                                    
+      integer, parameter :: rid_r0065 =   65                                    
+      integer, parameter :: rid_r0066 =   66                                    
+      integer, parameter :: rid_r0067 =   67                                    
+      integer, parameter :: rid_r0068 =   68                                    
+      integer, parameter :: rid_r0069 =   69                                    
+      integer, parameter :: rid_r0070 =   70                                    
+      integer, parameter :: rid_r0071 =   71                                    
+      integer, parameter :: rid_r0072 =   72                                    
+      integer, parameter :: rid_r0073 =   73                                    
+      integer, parameter :: rid_r0074 =   74                                    
+      integer, parameter :: rid_r0075 =   75                                    
+      integer, parameter :: rid_r0076 =   76                                    
+      integer, parameter :: rid_r0077 =   77                                    
+      integer, parameter :: rid_r0078 =   78                                    
+      integer, parameter :: rid_r0079 =   79                                    
+      integer, parameter :: rid_r0080 =   80                                    
+      integer, parameter :: rid_r0081 =   81                                    
+      integer, parameter :: rid_r0082 =   82                                    
+      integer, parameter :: rid_r0083 =   83                                    
+      integer, parameter :: rid_r0084 =   84                                    
+                                                                                
+      end module m_rxt_id                                                       
