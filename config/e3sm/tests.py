9ad35e0ddc89214520588fee815347e481da9980
Robert Jacob <jacob@anl.gov>
2020-03-27 17:50:17 -0700

    Change name on 1850 BGCEXP_BCRC tests in e3sm_integration
    2 tests in e3sm_integration need to have "BCRC" changed to "CNTL".

--- config/e3sm/tests.py
+++ config/e3sm/tests.py
@@ -149,8 +149,8 @@ _TESTS = {
             "ERP_Ld3.ne30_oECv3_ICG.A_WCYCL1850S",
             "SMS.f09_g16_a.MALI",
             "SMS_D_Ln5.conusx4v1_conusx4v1.FC5AV1C-L",
-            "SMS.ne30_oECv3.BGCEXP_BCRC_CNPECACNT_1850.clm-bgcexp",
-            "SMS.ne30_oECv3.BGCEXP_BCRC_CNPRDCTC_1850.clm-bgcexp",
+            "SMS.ne30_oECv3.BGCEXP_CNTL_CNPECACNT_1850.clm-bgcexp",
+            "SMS.ne30_oECv3.BGCEXP_CNTL_CNPRDCTC_1850.clm-bgcexp",
             "SMS_D_Ld1.T62_oEC60to30v3.DTESTM",
             "SMS_D_Ld1.ne30_r05_oECv3.A_WCYCL1850",
             )
