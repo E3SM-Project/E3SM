9ab54aeb549c4a6371b674b5fede2adfc75381ae
Xingqiu Yuan <xyuan@anl.gov>
2020-03-29 18:47:24 -0400

    remove the redundant codes from the implementation

--- components/scream/src/physics/p3/p3_functions_incloud_mixingratios_impl.hpp
+++ components/scream/src/physics/p3/p3_functions_incloud_mixingratios_impl.hpp
@@ -42,15 +42,6 @@ void Functions<S,D>
    nc_incld.set(not_qc_ge_qsmall,
                 sp(0));
 
-//       if (qc.ge.qsmall) then
-//          qc_incld = qc*inv_lcldm
-//          nc_incld = max(nc*inv_lcldm,0._rtype)
-//          !AaronDonahue, kai has something about if nccons then nc=ncnst/rho
-//       else
-//          qc_incld = 0._rtype
-//          nc_incld = 0._rtype
-//       end if 
-
    qitot_incld.set(qitot_ge_qsmall,
                    qitot*inv_icldm);
    nitot_incld.set(qitot_ge_qsmall,
@@ -60,15 +51,6 @@ void Functions<S,D>
                   sp(0));
    nitot_incld.set(not_qitot_ge_qsmall,
                   sp(0));
-
-//       if (qitot.ge.qsmall) then
-//          qitot_incld = qitot*inv_icldm
-//          nitot_incld = max(nitot*inv_icldm,0._rtype)
-//          !AaronDonahue, kai has something about if nicons then ni=ninst/rho
-//       else
-//          qitot_incld = 0._rtype
-//          nitot_incld = 0._rtype
-//       end if 
 
    qirim_incld.set(qitot_and_qirim_ge_qsmall,
                    qirim*inv_icldm);
@@ -80,14 +62,6 @@ void Functions<S,D>
    birim_incld.set(not_qitot_and_qirim_ge_qsmall,
                    sp(0));
 
-//       if (qirim.ge.qsmall.and.qitot.ge.qsmall) then
-//          qirim_incld = qirim*inv_icldm
-//          birim_incld = max(birim*inv_lcldm,0._rtype)
-//       else
-//          qirim_incld = 0._rtype
-//          birim_incld = 0._rtype
-//       end if 
-
    qr_incld.set(qr_ge_qsmall,
                 qr*inv_rcldm);
    nr_incld.set(qr_ge_qsmall,
@@ -97,15 +71,6 @@ void Functions<S,D>
                 sp(0));
    nr_incld.set(not_qr_ge_qsmall,
                 sp(0));
-
-//       if (qr.ge.qsmall) then
-//          qr_incld = qr*inv_rcldm
-//          nr_incld = max(nr*inv_rcldm,0._rtype)
-//          !AaronDonahue, kai has something about if nccons then nc=ncnst/rho
-//       else
-//          qr_incld = 0._rtype
-//          nr_incld = 0._rtype
-//       end if
 
    const auto any_gt_limit = (qc_incld > incloud_limit) || (qitot_incld > incloud_limit) || (qr_incld > precip_limit)
                           || (birim_incld > incloud_limit);
@@ -118,19 +83,6 @@ void Functions<S,D>
                    pack::max(birim_incld, incloud_limit));
    qr_incld.set(any_gt_limit,
                 pack::max(qr_incld, precip_limit));
-
-
-//       if (qc_incld.gt.incloud_limit .or.qitot_incld.gt.incloud_limit .or. qr_incld.gt.precip_limit .or.birim_incld.gt.incloud_limit) then
-//!          write(errmsg,'(a3,i4,3(a5,1x,e16.8,1x))') 'k: ', k, ', qc:',qc_incld, ', qi:',qitot_incld,', qr:',qr_incld
-//          qc_incld    = max(qc_incld,incloud_limit)
-//          qitot_incld = max(qitot_incld,incloud_limit)
-//          birim_incld = max(birim_incld,incloud_limit)
-//          qr_incld    = max(qr_incld,precip_limit)
-//!          if (masterproc) write(iulog,*)  errmsg
-//!          call handle_errmsg('Micro-P3 (Init)',subname='In-cloud mixing
-//!          ratio too large',extra_msg=errmsg)
-//       end if
-
 }
 
 } // namespace p3
