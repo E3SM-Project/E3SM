a89aea33bd203e65155e5c088c4d96445858a32f
James Foucar <jgfouca@sandia.gov>
2020-04-04 14:25:42 -0500

    case.setup: Fix case reset on some machines
    Removing env_mach_specific.xml triggered the FakeCase path in
    BuildConf.configure. This path doesn't work if the machine is using
    advanced attribute selectors (e.g. SMP_PRESENT) in their environment
    block in config_machines.xml.
    
    case_setup only needs BuildConf.configure to recreate the env_mach_specific.xml
    file and the Macro files.

--- scripts/lib/CIME/BuildTools/configure.py
+++ scripts/lib/CIME/BuildTools/configure.py
@@ -23,7 +23,7 @@ from CIME.XML.env_mach_specific import EnvMachSpecific
 logger = logging.getLogger(__name__)
 
 def configure(machobj, output_dir, macros_format, compiler, mpilib, debug,
-              comp_interface, sysos, unit_testing=False):
+              comp_interface, sysos, unit_testing=False, noenv=False):
     """Add Macros, Depends, and env_mach_specific files to a directory.
 
     Arguments:
@@ -46,7 +46,7 @@ def configure(machobj, output_dir, macros_format, compiler, mpilib, debug,
 
     _copy_depends_files(machobj.get_machine_name(), machobj.machines_dir, output_dir, compiler)
     _generate_env_mach_specific(output_dir, machobj, compiler, mpilib,
-                                debug, comp_interface, sysos, unit_testing)
+                                debug, comp_interface, sysos, unit_testing, noenv=noenv)
 
 def _copy_depends_files(machine_name, machines_dir, output_dir, compiler):
     """
@@ -74,7 +74,7 @@ class FakeCase(object):
         return self._vals[attrib]
 
 def _generate_env_mach_specific(output_dir, machobj, compiler, mpilib, debug,
-                                comp_interface, sysos, unit_testing):
+                                comp_interface, sysos, unit_testing, noenv=False):
     """
     env_mach_specific generation.
     """
@@ -82,9 +82,14 @@ def _generate_env_mach_specific(output_dir, machobj, compiler, mpilib, debug,
     if os.path.exists(ems_path):
         logger.warning("{} already exists, delete to replace".format(ems_path))
         return
+
     ems_file = EnvMachSpecific(output_dir, unit_testing=unit_testing)
     ems_file.populate(machobj)
     ems_file.write()
+
+    if noenv:
+        return
+
     fake_case = FakeCase(compiler, mpilib, debug, comp_interface)
     ems_file.load_env(fake_case)
     for shell in ('sh', 'csh'):
