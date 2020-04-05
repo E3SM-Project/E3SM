5fda94dcea9f2369383ebc1488ed2c689ddedfd3
Xingqiu Yuan <xyuan@anl.gov>
2020-04-05 16:34:29 -0400

    add fortran interface to cxx code

--- components/cam/src/physics/cam/micro_p3_utils.F90
+++ components/cam/src/physics/cam/micro_p3_utils.F90
@@ -78,6 +78,8 @@ module micro_p3_utils
     real(rtype), parameter, public :: rhows = 917._rtype  ! bulk density water solid
     real(rtype), parameter, public :: dropmass = 5.2e-7_rtype
 
+    logical, public :: use_cxx = .true.
+
 ! particle mass-diameter relationship
 ! currently we assume spherical particles for cloud ice/snow
 ! m = cD^d
@@ -233,9 +235,19 @@ real(rtype), parameter :: precip_limit  = 1.0E-2
 !                                                                                          !
 !__________________________________________________________________________________________!
     subroutine get_latent_heat(its,ite,kts,kte,v,s,f)
+#ifdef SCREAM_CONFIG_IS_CMAKE
+    use micro_p3_iso_f, only: get_latent_heat_f
+#endif
 
        integer,intent(in) :: its,ite,kts,kte
        real(rtype),dimension(its:ite,kts:kte),intent(out) :: v,s,f
+
+#ifdef SCREAM_CONFIG_IS_CMAKE
+    if (use_cxx) then
+       call get_latent_heat_f(its,ite,kts,kte,v,s,f)
+       return
+    endif
+#endif
 
 !       integer i,k
 
