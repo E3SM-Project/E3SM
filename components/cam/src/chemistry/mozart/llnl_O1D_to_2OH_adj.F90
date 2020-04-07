01395a7be71fe3332b4ee425efae7427ca434c71
pratherUCI <mprather@uci.edu>
2020-04-07 14:52:34 -0700

    cleaned up LLNL_O1d, added jo1dU

--- components/cam/src/chemistry/mozart/llnl_O1D_to_2OH_adj.F90
+++ components/cam/src/chemistry/mozart/llnl_O1D_to_2OH_adj.F90
@@ -1,6 +1,6 @@
 !===========================================================================
 ! Now used (also) for chemUCI usrrxt's because it has access to the photolysis rates
-!   Michael Prather 4/4/2020
+!   Michael Prather 4/7/2020
 !
 ! Originally for LLNL SuperFast chemistry:
 ! Combine several reactions into one pseudo reaction to correct the
@@ -20,9 +20,11 @@ module llnl_O1D_to_2OH_adj
 
   save
 
+!-LLNL-superfast
   integer :: jo1d_ndx
 
 !-chemUCI
+  integer :: jo1dU_ndx
   integer :: uci1_ndx, uci2_ndx, uci3_ndx, uci4_ndx, uci5_ndx, uci6_ndx, uci7_ndx, uci8_ndx, uci9_ndx
   integer :: HNO4f_ndx, N2O5f_ndx, PANf_ndx, ucih1_ndx, ucih2_ndx, ucih3_ndx
 
@@ -39,10 +41,12 @@ contains
 
     implicit none
 
+!-LLNL-superfast
     jo1d_ndx  = get_rxt_ndx( 'j2oh' )
 !
 !-chemUCI
 !
+    jo1dU_ndx        = get_rxt_ndx( 'jo1dU' ) ! O3 + hv -> O(1D) + O2
     uci1_ndx         = get_rxt_ndx( 'uci1' )  ! O3(O1D) + H2O -> 2 OH
     uci2_ndx         = get_rxt_ndx( 'uci2' )  ! O3(O1D) + H2 -> OH _ HO2
     uci3_ndx         = get_rxt_ndx( 'uci3' )  ! O3(O1D) + CH4 -> OH + CH3OO
@@ -52,14 +56,12 @@ contains
     uci7_ndx         = get_rxt_ndx( 'uci7' )  ! HNO4r thermal decomp
     uci8_ndx         = get_rxt_ndx( 'uci8' )  ! N2O5r thermal
     uci9_ndx         = get_rxt_ndx( 'uci9' )  ! PANr thermal
-    HNO4f_ndx         = get_rxt_ndx( 'HNO4f' )  ! HNO4 formation
-    N2O5f_ndx         = get_rxt_ndx( 'N2O5f' )  ! N2O5 formation
-    PANf_ndx          = get_rxt_ndx( 'PANf' )   ! PAN formation
+    HNO4f_ndx        = get_rxt_ndx( 'HNO4f' ) ! HNO4 formation
+    N2O5f_ndx        = get_rxt_ndx( 'N2O5f' ) ! N2O5 formation
+    PANf_ndx         = get_rxt_ndx( 'PANf' )  ! PAN formation
     ucih1_ndx        = get_rxt_ndx( 'ucih1' ) ! N2O5 + ASAD (het chem)
     ucih2_ndx        = get_rxt_ndx( 'ucih2' ) ! NO3 + ASAD
     ucih3_ndx        = get_rxt_ndx( 'ucih3' ) ! HO2 + ASAD
-
-
 
     if (masterproc) then
        write (iulog,*) 'O1D_to_2OH_adj_init: Found j2oh index in O1D_to_2OH_adj_init of   ', jo1d_ndx
@@ -68,7 +70,7 @@ contains
 !chemUCI
        write(iulog,*) 'O1D_to_2OH_adj_init: chemUCI diagnostics '
        write(iulog,'(10i5)') uci1_ndx, uci2_ndx, uci3_ndx, uci4_ndx, uci5_ndx, uci6_ndx, uci7_ndx, uci8_ndx, uci9_ndx
-       write(iulog,'(10i5)') jo1d_ndx, ucih1_ndx, ucih2_ndx, ucih3_ndx
+       write(iulog,'(10i5)') jo1dU_ndx, ucih1_ndx, ucih2_ndx, ucih3_ndx
     endif
 
   end subroutine O1D_to_2OH_adj_init
@@ -80,9 +82,6 @@ contains
     use chem_mods,    only : nfs, phtcnt, rxntot, nfs !PJC added rxntot, nfs
     use ppgrid,       only : pcols, pver              !PJC added pcols
     use mo_setinv,    only : n2_ndx, o2_ndx, h2o_ndx  !PJC + MJP
-!    use carma_flags_mod, only : carma_do_hetchem
-!    use aero_model,      only : aero_model_surfarea
-!    use rad_constituents,only : rad_cnst_get_info
 
     implicit none
 
@@ -98,8 +97,6 @@ contains
     !--------------------------------------------------------------------
     ! ... local variables
     !--------------------------------------------------------------------
-    real(r8), parameter :: dg = 0.1_r8            ! mole diffusion =0.1 cm2/s (Dentener, 1993)
-
     integer  ::  i, k
     real(r8) ::  tp(ncol)                       ! 300/t
     real(r8) ::  tinv(ncol)                     ! 1/t
@@ -109,8 +106,8 @@ contains
     real(r8) ::  exp_fac(ncol)                  ! vector exponential
     real(r8) ::  fc(ncol)
     real(r8) ::  prod_O1D(ncol)
-    real(r8) ::  c_n2o5, c_ho2, c_no3
-    real(r8) ::  gamma_n2o5, gamma_ho2, gamma_no3
+!    real(r8) ::  c_n2o5, c_ho2, c_no3
+!    real(r8) ::  gamma_n2o5, gamma_ho2, gamma_no3
 
     real(r8) :: n2_rate(ncol,pver)
     real(r8) :: o2_rate(ncol,pver)
@@ -132,45 +129,18 @@ contains
     real(r8), parameter :: y2 =  55.0_r8
     real(r8), parameter :: y3 =  60.0_r8
 
-
-             sfc    => sfc_array(i,k,:)
-             dm_aer => dm_array(i,k,:)
-             rxt(i,k,ucih2_ndx) = hetrxtrate( sfc, dm_aer, dg, c_no3 , gamma_no3  )
-
 !
 !-chemUCI reset of all user rates, including those with the O(1D) fix
 !     remember that the rxt()'s heref have been calculated rxt =k[X&Y] * [X] * [Y]
 !
-!   if( uci1_ndx > 0 ) then
-!
-!      if( ucih1_ndx > 0 .or. ucih2_ndx > 0 .or. ucih3_ndx > 0 ) then
-!!----all this is to get reactive aerosol surface area for std trop het-chem---may not quite work
-!         call rad_cnst_get_info(0, nmodes=ntot_amode)
-!         if (ntot_amode>0) then
-!            allocate(sfc_array(pcols,pver,ntot_amode), dm_array(pcols,pver,ntot_amode) )
-!         else
-!            allocate(sfc_array(pcols,pver,5), dm_array(pcols,pver,5) )
-!         endif
-!         sfc_array(:,:,:) = 0._r8
-!         dm_array(:,:,:) = 0._r8
-!         sad_total(:,:) = 0._r8
-!         if( carma_do_hetchem ) then
-!            sad_total(:ncol,:pver)=strato_sad(:ncol,:pver)
-!         else
-!            call aero_model_surfarea( &
-!               mmr, rm1, relhum, pmid, temp, strato_sad, &
-!               sulfate, m, ltrop, het1_ndx, pbuf, ncol, sfc_array, dm_array, sad_total )
-!         endif
-!!-------------------------not sure if the above will work---------------------
-!      endif
 
     level_loop : do k = 1,pver
        tinv(:)           = 1._r8 / temp(:ncol,k)
        tp(:)             = 300._r8 * tinv(:)
        sqrt_t(:)         = sqrt( temp(:ncol,k) )
 
-       if( jo1d_ndx > 0 ) then
-          prod_O1D(:) = rxt(:,k,jo1d_ndx)   ! 1/sec, = production of O(1D) when *[O3]
+       if( jo1dU_ndx > 0 ) then
+          prod_O1D(:) = rxt(:,k,jo1dU_ndx)   ! 1/sec, = production of O(1D) when *[O3]
              call comp_exp( exp_fac, 110._r8*tinv, ncol )
           fc(:) =         2.15e-11_r8 * exp_fac(:) * invariants(:,k,n2_ndx)
              call comp_exp( exp_fac, 55._r8*tinv, ncol )
@@ -234,32 +204,50 @@ contains
           endif
        endif
 
+       if( ucih1_ndx > 0 ) then
+           rxt(:,k,ucih1_ndx) = 0._r8
+       else
+           rxt(:,k,ucih1_ndx) = 0._r8
+       endif
+
+       if( ucih2_ndx > 0 ) then
+           rxt(:,k,ucih2_ndx) = 0._r8
+       else
+           rxt(:,k,ucih2_ndx) = 0._r8
+       endif
+
+       if( ucih3_ndx > 0 ) then
+           rxt(:,k,ucih3_ndx) = 0._r8
+       else
+           rxt(:,k,ucih3_ndx) = 0._r8
+       endif
+
+
 !  hetrxtrate sums over all the aerosol types: hydrolysis reactions on wetted aerosols
 !    NEEDS surface area(sfc) & diameter(dm_aer) for as many aerosols as there are.
 !    SUM  rxt(i) = sfc(i) / (0.5_r8*dm_aer(i)/dg_gas + (4._r8/(c_gas*gamma_gas)))
 !         rxt = sfc / ( (rad_aer/Dg_gas) + (4/(c_gas*gamma_gas)))
-
-       if( ucih1_ndx > 0 .or. ucih2_ndx > 0 .or. ucih3_ndx > 0 ) then
-          uci_loop : do i = 1,ncol
-             sfc    => sfc_array(i,k,:)
-             dm_aer => dm_array(i,k,:)
-             c_n2o5 = 1.40e3_r8 * sqrt_t(i)         ! mean molecular speed of n2o5
-             c_no3  = 1.85e3_r8 * sqrt_t(i)         ! mean molecular speed of no3
-             c_ho2  = 2.53e3_r8 * sqrt_t(i)         ! mean molecular speed of ho2
-             gamma_n2o5 = rxt(i,k,ucih1_ndx)
-             gamma_no3  = rxt(i,k,ucih2_ndx)
-             gamma_ho2  = rxt(i,k,ucih3_ndx)
-             if( ucih1_ndx > 0 ) then
-                rxt(i,k,ucih1_ndx) = hetrxtrate( sfc, dm_aer, dg, c_n2o5, gamma_n2o5 )
-             end if
-             if( ucih2_ndx > 0 ) then
-                rxt(i,k,ucih2_ndx) = hetrxtrate( sfc, dm_aer, dg, c_no3 , gamma_no3  )
-             end if
-             if( ucih3_ndx > 0 ) then
-                rxt(i,k,ucih3_ndx) = hetrxtrate( sfc, dm_aer, dg, c_ho2 , gamma_ho2  )
-             end if
-          enddo uci_loop
-       endif
+!       if( ucih1_ndx > 0 .or. ucih2_ndx > 0 .or. ucih3_ndx > 0 ) then
+!          uci_loop : do i = 1,ncol
+!             sfc    => sfc_array(i,k,:)
+!             dm_aer => dm_array(i,k,:)
+!             c_n2o5 = 1.40e3_r8 * sqrt_t(i)         ! mean molecular speed of n2o5
+!             c_no3  = 1.85e3_r8 * sqrt_t(i)         ! mean molecular speed of no3
+!             c_ho2  = 2.53e3_r8 * sqrt_t(i)         ! mean molecular speed of ho2
+!             gamma_n2o5 = rxt(i,k,ucih1_ndx)
+!             gamma_no3  = rxt(i,k,ucih2_ndx)
+!             gamma_ho2  = rxt(i,k,ucih3_ndx)
+!             if( ucih1_ndx > 0 ) then
+!                rxt(i,k,ucih1_ndx) = hetrxtrate( sfc, dm_aer, dg, c_n2o5, gamma_n2o5 )
+!             end if
+!             if( ucih2_ndx > 0 ) then
+!                rxt(i,k,ucih2_ndx) = hetrxtrate( sfc, dm_aer, dg, c_no3 , gamma_no3  )
+!             end if
+!             if( ucih3_ndx > 0 ) then
+!                rxt(i,k,ucih3_ndx) = hetrxtrate( sfc, dm_aer, dg, c_ho2 , gamma_ho2  )
+!             end if
+!          enddo uci_loop
+!      endif
 
     end do level_loop
 !
@@ -279,51 +267,3 @@ contains
 
 
   end subroutine O1D_to_2OH_adj
-
-!-------------------------------------------------------------------------
-  subroutine comp_exp( x, y, n )
-
-      implicit none
-
-      real(r8), intent(out) :: x(:)
-      real(r8), intent(in)  :: y(:)
-      integer,  intent(in)  :: n
-
-!  #ifdef IBM
-!      call vexp( x, y, n )
-!  #else
-      x(:n) = exp( y(:n) )
-!  #endif
-
-  end subroutine comp_exp
-
-    !-------------------------------------------------------------------------
-    !  Heterogeneous reaction rates for uptake of a gas on an aerosol:
-    !-------------------------------------------------------------------------
-  function hetrxtrate( sfc, dm_aer, dg_gas, c_gas, gamma_gas ) result(rate)
-
-      real(r8), intent(in) :: sfc(:)
-      real(r8), intent(in) :: dm_aer(:)
-      real(r8), intent(in) :: dg_gas
-      real(r8), intent(in) :: c_gas
-      real(r8), intent(in) :: gamma_gas
-      real(r8) :: rate
-
-      real(r8),allocatable :: rxt(:)
-      integer :: n, i
-
-      n = size(sfc)
-
-      allocate(rxt(n))
-      do i=1,n
-         rxt(i) = sfc(i) / (0.5_r8*dm_aer(i)/dg_gas + (4._r8/(c_gas*gamma_gas)))
-      enddo
-
-      rate = sum(rxt)
-
-      deallocate(rxt)
-
-  endfunction hetrxtrate
-
-
-end module llnl_O1D_to_2OH_adj
