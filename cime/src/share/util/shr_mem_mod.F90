7f211eb3f3ed6fb3d4f064d5b8a79c1235fb202b
Azamat Mametjanov <azamat@mcs.anl.gov>
2020-04-30 23:43:52 +0000

    Add more error-checking

--- cime/src/share/util/shr_mem_mod.F90
+++ cime/src/share/util/shr_mem_mod.F90
@@ -2,6 +2,7 @@ MODULE shr_mem_mod
 
   use shr_kind_mod, only : shr_kind_r8
   use shr_log_mod, only: s_logunit => shr_log_Unit
+  use shr_sys_mod, only: shr_sys_abort
 
   implicit none
   private
@@ -40,6 +41,7 @@ CONTAINS
 
     real(shr_kind_r8),allocatable :: mem_tmp(:)
 
+    character(*),parameter :: subname = "(shr_mem_init)"
     !---------------------------------------------------
 
     lprt = .false.
@@ -48,11 +50,21 @@ CONTAINS
     endif
 
     ierr = GPTLget_memusage (msize, mrss0, mshare, mtext, mdatastack)
-    allocate(mem_tmp(1024*1024))    ! 1 MWord, 8 MB
+    if (ierr .ne. 0) call shr_sys_abort(trim(subname)//': GPTLget_memusage mrss0 failed')
+
+    allocate(mem_tmp(1024*1024), stat=ierr)    ! 1 MWord, 8 MB
+    if (ierr .ne. 0) call shr_sys_abort(trim(subname)//': allocate failed')
+
     mem_tmp = -1.0
     ierr = GPTLget_memusage (msize, mrss1, mshare, mtext, mdatastack)
-    deallocate(mem_tmp)
+    if (ierr .ne. 0) call shr_sys_abort(trim(subname)//': GPTLget_memusage mrss1 failed')
+
+    deallocate(mem_tmp, stat=ierr)
+    if (ierr .ne. 0) call shr_sys_abort(trim(subname)//': deallocate failed')
+
     ierr = GPTLget_memusage (msize, mrss2, mshare, mtext, mdatastack)
+    if (ierr .ne. 0) call shr_sys_abort(trim(subname)//': GPTLget_memusage mrss2 failed')
+
     mb_blk = 0.0_shr_kind_r8
     if (mrss1 - mrss0 > 0) then
        mb_blk = (8.0_shr_kind_r8)/((mrss1-mrss0)*1.0_shr_kind_r8)
@@ -61,12 +73,12 @@ CONTAINS
     if (lprt) then
        write(s_logunit,'(A,f16.2)') '8 MB memory   alloc in MB is ',(mrss1-mrss0)*mb_blk
        write(s_logunit,'(A,f16.2)') '8 MB memory dealloc in MB is ',(mrss1-mrss2)*mb_blk
-       write(s_logunit,'(A,f16.2)') 'Memory block size conversion in bytes is ',mb_blk*1024_shr_kind_r8*1024.0_shr_kind_r8
+       write(s_logunit,'(A,f16.2)') 'Memory block size conversion in bytes is ',mb_blk*1024.0_shr_kind_r8*1024.0_shr_kind_r8
     endif
     if (present(strbuf)) then
        write(strbuf,'(3(A,f16.2))') '8 MB memory   alloc in MB is ',(mrss1-mrss0)*mb_blk, &
             '\n8 MB memory dealloc in MB is ',(mrss1-mrss2)*mb_blk, &
-            '\nMemory block size conversion in bytes is ',mb_blk*1024_shr_kind_r8*1024.0_shr_kind_r8
+            '\nMemory block size conversion in bytes is ',mb_blk*1024.0_shr_kind_r8*1024.0_shr_kind_r8
     endif
 
 
