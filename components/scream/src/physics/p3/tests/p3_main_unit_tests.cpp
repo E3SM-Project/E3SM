0d9b71567b9f17ed82098c26151c6fe6b30cf611
James Foucar <jgfouca@sandia.gov>
2020-05-04 15:45:19 -0600

    progress

--- components/scream/src/physics/p3/tests/p3_main_unit_tests.cpp
+++ components/scream/src/physics/p3/tests/p3_main_unit_tests.cpp
@@ -34,55 +34,60 @@ static void run_phys()
 
 static void run_bfb_p3_main_pre_loop()
 {
+  constexpr Scalar qsmall     = C::QSMALL;
+  constexpr Scalar zerodegc   = C::ZeroDegC;
+  constexpr Scalar sup_upper = -0.05;
+  constexpr Scalar sup_lower = -0.1;
+
   const std::array< std::pair<Real, Real>, P3MainPreLoopData::NUM_ARRAYS > ranges = {
-    std::make_pair(0, 1) // pres
-    std::make_pair(0, 1) // pdel
-    std::make_pair(0, 1) // dzq
-    std::make_pair(0, 1) // npccn
-    std::make_pair(0, 1) // exner
-    std::make_pair(0, 1) // inv_exner
-    std::make_pair(0, 1) // inv_lcldm
-    std::make_pair(0, 1) // inv_icldm
-    std::make_pair(0, 1) // inv_rcldm
-    std::make_pair(0, 1) // t
-    std::make_pair(0, 1) // rho
-    std::make_pair(0, 1) // inv_rho
-    std::make_pair(0, 1) // qvs
-    std::make_pair(0, 1) // qvi
-    std::make_pair(0, 1) // sup
-    std::make_pair(0, 1) // supi
-    std::make_pair(0, 1) // rhofacr
-    std::make_pair(0, 1) // rhofaci
-    std::make_pair(0, 1) // acn
-    std::make_pair(0, 1) // qv
-    std::make_pair(0, 1) // th
-    std::make_pair(0, 1) // qc
-    std::make_pair(0, 1) // nc
-    std::make_pair(0, 1) // qr
-    std::make_pair(0, 1) // nr
-    std::make_pair(0, 1) // qitot
-    std::make_pair(0, 1) // nitot
-    std::make_pair(0, 1) // qirim
-    std::make_pair(0, 1) // birim
-    std::make_pair(0, 1) // xxlv
-    std::make_pair(0, 1) // xxls
-    std::make_pair(0, 1) // xlf
-    std::make_pair(0, 1) // qc_incld
-    std::make_pair(0, 1) // qr_incld
-    std::make_pair(0, 1) // qitot_incld
-    std::make_pair(0, 1) // qirim_incld
-    std::make_pair(0, 1) // nc_incld
-    std::make_pair(0, 1) // nr_incld
-    std::make_pair(0, 1) // nitot_incld
-    std::make_pair(0, 1) // birim_incld
+    std::make_pair(0, 1), // pres
+    std::make_pair(0, 1), // pdel
+    std::make_pair(0, 1), // dzq
+    std::make_pair(0, 1), // npccn
+    std::make_pair(0, 1), // exner
+    std::make_pair(0, 1), // inv_exner
+    std::make_pair(0, 1), // inv_lcldm
+    std::make_pair(0, 1), // inv_icldm
+    std::make_pair(0, 1), // inv_rcldm
+    std::make_pair(zerodegc - 10, zerodegc + 10), // t
+    std::make_pair(0, 1), // rho
+    std::make_pair(0, 1), // inv_rho
+    std::make_pair(0, 1), // qvs
+    std::make_pair(0, 1), // qvi
+    std::make_pair(sup_lower -.05, sup_upper + .05), // sup
+    std::make_pair(sup_lower -.05, sup_upper + .05), // supi
+    std::make_pair(0, 1), // rhofacr
+    std::make_pair(0, 1), // rhofaci
+    std::make_pair(0, 1), // acn
+    std::make_pair(0, 1), // qv
+    std::make_pair(0, 1), // th
+    std::make_pair(0, qsmall * 2), // qc
+    std::make_pair(0, 1), // nc
+    std::make_pair(0, qsmall * 2), // qr
+    std::make_pair(0, 1), // nr
+    std::make_pair(0, qsmall * 2), // qitot
+    std::make_pair(0, 1), // nitot
+    std::make_pair(0, 1), // qirim
+    std::make_pair(0, 1), // birim
+    std::make_pair(0, 1), // xxlv
+    std::make_pair(0, 1), // xxls
+    std::make_pair(0, 1), // xlf
+    std::make_pair(0, 1), // qc_incld
+    std::make_pair(0, 1), // qr_incld
+    std::make_pair(0, 1), // qitot_incld
+    std::make_pair(0, 1), // qirim_incld
+    std::make_pair(0, 1), // nc_incld
+    std::make_pair(0, 1), // nr_incld
+    std::make_pair(0, 1), // nitot_incld
+    std::make_pair(0, 1), // birim_incld
   };
 
   P3MainPreLoopData isds_fortran[] = {
-    //       kts, kte, ktop, kbot, kdir,        dt,       odt, prt_sol, ranges
-    P3MainPreLoopData(1,  72,   27,   72,   -1, 1.800E+03, 5.556E-04,     0.0, ranges),
-    P3MainPreLoopData(1,  72,   72,   27,    1, 1.800E+03, 5.556E-04,     1.0, ranges),
-    P3MainPreLoopData(1,  72,   27,   27,   -1, 1.800E+03, 5.556E-04,     0.0, ranges),
-    P3MainPreLoopData(1,  72,   27,   27,    1, 1.800E+03, 5.556E-04,     2.0, ranges),
+    //              kts, kte, ktop, kbot, kdir, log_predictNc,        dt, ranges
+    P3MainPreLoopData(1,  72,    1,   72,   -1, false,         1.800E+03, ranges),
+    P3MainPreLoopData(1,  72,    1,   72,   -1, true,          1.800E+03, ranges),
+    P3MainPreLoopData(1,  72,   72,    1,    1, false,         1.800E+03, ranges),
+    P3MainPreLoopData(1,  72,   72,    1,    1, true,          1.800E+03, ranges),
   };
 
   static constexpr Int num_runs = sizeof(isds_fortran) / sizeof(P3MainPreLoopData);
@@ -98,36 +103,58 @@ static void run_bfb_p3_main_pre_loop()
 
     // Get data from fortran
   for (Int i = 0; i < num_runs; ++i) {
-    ice_sedimentation(isds_fortran[i]);
+    p3_main_pre_main_loop(isds_fortran[i]);
   }
 
   // Get data from cxx
   for (Int i = 0; i < num_runs; ++i) {
     P3MainPreLoopData& d = isds_cxx[i];
-    ice_sedimentation_f(d.kts, d.kte, d.ktop, d.kbot, d.kdir,
-                        d.rho, d.inv_rho, d.rhofaci, d.icldm, d.inv_dzq,
-                        d.dt, d.odt,
-                        d.qitot, d.qitot_incld, d.nitot, d.qirim, d.qirim_incld, d.birim, d.birim_incld,
-                        d.nitot_incld, &d.prt_sol, d.qi_tend, d.ni_tend);
+    p3_main_pre_main_loop_f(d.kts, d.kte, d.ktop, d.kbot, d.kdir, d.log_predictNc, d.dt,
+                            d.pres, d.pdel, d.dzq, d.npccn, d.exner, d.inv_exner, d.inv_lcldm, d.inv_icldm, d.inv_rcldm,
+                            d.t, d.rho, d.inv_rho, d.qvs, d.qvi, d.sup, d.supi, d.rhofacr, d.rhofaci,
+                            d.acn, d.qv, d.th, d.qc, d.nc, d.qr, d.nr, d.qitot, d.nitot, d.qirim, d.birim, d.xxlv, d.xxls, d.xlf, d.qc_incld, d.qr_incld, d.qitot_incld,
+                            d.qirim_incld, d.nc_incld, d.nr_incld, d.nitot_incld, d.birim_incld,
+                            &d.log_nucleationPossible, &d.log_hydrometeorsPresent);
   }
 
   for (Int i = 0; i < num_runs; ++i) {
-    // Due to pack issues, we must restrict checks to the active k space
     Int start = std::min(isds_fortran[i].kbot, isds_fortran[i].ktop) - 1; // 0-based indx
     Int end   = std::max(isds_fortran[i].kbot, isds_fortran[i].ktop);     // 0-based indx
     for (Int k = start; k < end; ++k) {
+      REQUIRE(isds_fortran[i].t[k]           == isds_cxx[i].t[k]);
+      REQUIRE(isds_fortran[i].rho[k]         == isds_cxx[i].rho[k]);
+      REQUIRE(isds_fortran[i].inv_rho[k]     == isds_cxx[i].inv_rho[k]);
+      REQUIRE(isds_fortran[i].qvs[k]         == isds_cxx[i].qvs[k]);
+      REQUIRE(isds_fortran[i].qvi[k]         == isds_cxx[i].qvi[k]);
+      REQUIRE(isds_fortran[i].sup[k]         == isds_cxx[i].sup[k]);
+      REQUIRE(isds_fortran[i].supi[k]        == isds_cxx[i].supi[k]);
+      REQUIRE(isds_fortran[i].rhofacr[k]     == isds_cxx[i].rhofacr[k]);
+      REQUIRE(isds_fortran[i].rhofaci[k]     == isds_cxx[i].rhofaci[k]);
+      REQUIRE(isds_fortran[i].acn[k]         == isds_cxx[i].acn[k]);
+      REQUIRE(isds_fortran[i].qv[k]          == isds_cxx[i].qv[k]);
+      REQUIRE(isds_fortran[i].th[k]          == isds_cxx[i].th[k]);
+      REQUIRE(isds_fortran[i].qc[k]          == isds_cxx[i].qc[k]);
+      REQUIRE(isds_fortran[i].nc[k]          == isds_cxx[i].nc[k]);
+      REQUIRE(isds_fortran[i].qr[k]          == isds_cxx[i].qr[k]);
+      REQUIRE(isds_fortran[i].nr[k]          == isds_cxx[i].nr[k]);
       REQUIRE(isds_fortran[i].qitot[k]       == isds_cxx[i].qitot[k]);
-      REQUIRE(isds_fortran[i].qitot_incld[k] == isds_cxx[i].qitot_incld[k]);
       REQUIRE(isds_fortran[i].nitot[k]       == isds_cxx[i].nitot[k]);
-      REQUIRE(isds_fortran[i].nitot_incld[k] == isds_cxx[i].nitot_incld[k]);
       REQUIRE(isds_fortran[i].qirim[k]       == isds_cxx[i].qirim[k]);
-      REQUIRE(isds_fortran[i].qirim_incld[k] == isds_cxx[i].qirim_incld[k]);
       REQUIRE(isds_fortran[i].birim[k]       == isds_cxx[i].birim[k]);
+      REQUIRE(isds_fortran[i].xxlv[k]        == isds_cxx[i].xxlv[k]);
+      REQUIRE(isds_fortran[i].xxls[k]        == isds_cxx[i].xxls[k]);
+      REQUIRE(isds_fortran[i].xlf[k]         == isds_cxx[i].xlf[k]);
+      REQUIRE(isds_fortran[i].qc_incld[k]    == isds_cxx[i].qc_incld[k]);
+      REQUIRE(isds_fortran[i].qr_incld[k]    == isds_cxx[i].qr_incld[k]);
+      REQUIRE(isds_fortran[i].qitot_incld[k] == isds_cxx[i].qitot_incld[k]);
+      REQUIRE(isds_fortran[i].qirim_incld[k] == isds_cxx[i].qirim_incld[k]);
+      REQUIRE(isds_fortran[i].nc_incld[k]    == isds_cxx[i].nc_incld[k]);
+      REQUIRE(isds_fortran[i].nr_incld[k]    == isds_cxx[i].nr_incld[k]);
+      REQUIRE(isds_fortran[i].nitot_incld[k] == isds_cxx[i].nitot_incld[k]);
       REQUIRE(isds_fortran[i].birim_incld[k] == isds_cxx[i].birim_incld[k]);
-      REQUIRE(isds_fortran[i].qi_tend[k]     == isds_cxx[i].qi_tend[k]);
-      REQUIRE(isds_fortran[i].ni_tend[k]     == isds_cxx[i].ni_tend[k]);
     }
-    REQUIRE(isds_fortran[i].prt_sol == isds_cxx[i].prt_sol);
+    REQUIRE(isds_fortran[i].log_hydrometeorsPresent == isds_cxx[i].log_hydrometeorsPresent);
+    REQUIRE(isds_fortran[i].log_nucleationPossible  == isds_cxx[i].log_nucleationPossible);
   }
 }
 
