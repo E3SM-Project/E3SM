c51bc5c00f771a918ce16f0fb556e880395735a0
Shixuan Zhang <zhan391@compy01.pnl.gov>
2020-05-09 11:02:13 -0700

    Generate the domain files with 0.25x0.25 resolution

--- cime/tools/mapping/gen_domain_files/run_gen_dom.sh
+++ cime/tools/mapping/gen_domain_files/run_gen_dom.sh
@@ -3,5 +3,6 @@
 ./gen_domain -m  ../gen_mapping_files/gen_ESMF_mapping_file/map_0.25x0.25_TO_fv1.9x2.5_aave.200430.nc \
              -o 0.25x0.25 \
              -l fv1.9x2.5 \
+             -p 2         \
              -c "domain file for a 1/4-degree ocean + 2-degree FV"
   
