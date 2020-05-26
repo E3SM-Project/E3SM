5c29eefc08de6241bd011199fbdff62e41c809ef
Luca Bertagna <lbertag@sandia.gov>
2020-05-26 17:46:25 -0600

    Setting the kind for reals in lin interpolation
    The kind is set depending on EKAT precision.
    
    Note: setting the flag for default real and default double would also
    fix this, but we should not rely on that flag. Ideally, EKAT should not
    need that flag, and fully rely on EKAT_DOUBLE_PRECISION to establish
    the kind of every real.

--- components/scream/ekat/tests/algorithm/lin_interp_ref.f90
+++ components/scream/ekat/tests/algorithm/lin_interp_ref.f90
@@ -2,19 +2,19 @@
 
 module scream_ut
 
-#ifdef EKAT_DOUBLE_PRECISION
-use iso_c_binding, only: c_real => c_double
-#else
-use iso_c_binding, only: c_real => c_float
-#endif
+  use iso_c_binding, only: c_int, c_double, c_float
 
   implicit none
+
+#ifdef EKAT_DOUBLE_PRECISION
+  integer, parameter :: c_real = c_double
+#else
+  integer, parameter :: c_real = c_float
+#endif
 
 contains
 
   subroutine linear_interp_c(x1,x2,y1,y2,km1,km2,ncol,minthresh) bind(c)
-
-    use iso_c_binding
 
     integer(kind=c_int), value, intent(in) :: km1, km2, ncol
     real(kind=c_real), value, intent(in) :: minthresh
@@ -30,14 +30,15 @@ contains
   ! Linear interpolation to get values on various grids
 
   subroutine linear_interp(x1,x2,y1,y2,km1,km2,ncol,minthresh)
+    use iso_c_binding
     implicit none
 
-    integer, intent(in) :: km1, km2
-    integer, intent(in) :: ncol
-    real, intent(in) :: x1(ncol,km1), y1(ncol,km1)
-    real, intent(in) :: x2(ncol,km2)
-    real, intent(in) :: minthresh
-    real, intent(out) :: y2(ncol,km2)
+    integer(kind=c_int), intent(in) :: km1, km2
+    integer(kind=c_int), intent(in) :: ncol
+    real(kind=c_real), intent(in) :: x1(ncol,km1), y1(ncol,km1)
+    real(kind=c_real), intent(in) :: x2(ncol,km2)
+    real(kind=c_real), intent(in) :: minthresh
+    real(kind=c_real), intent(out) :: y2(ncol,km2)
 
     integer :: k1, k2, i
 
