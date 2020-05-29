36400dc09474791ed3aec126a3f4bcc92b8b93a9
Xingqiu Yuan <xyuan@anl.gov>
2020-05-29 10:45:38 -0400

    add config changes for IBM Openmp 4.5

--- cime/config/e3sm/machines/Depends.summit.ibm.cmake
+++ cime/config/e3sm/machines/Depends.summit.ibm.cmake
@@ -1,3 +1,3 @@
 foreach(ITEM IN LISTS FILES_NEED_OPENMP_FLAGS)
-  set_property(SOURCE ${ITEM} APPEND_STRING PROPERTY COMPILE_FLAGS " -g -O2 -WF,-D_OPENMP -qsmp=omp -qoffload ")
+  e3sm_add_flags("${ITEM}" " -g -O2 -WF,-D_OPENMP -qsmp=omp -qoffload ")
 endforeach()
