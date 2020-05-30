07a55c8d60c104a79675d61eb5540ea54738f832
Andrew M. Bradley <ambradl@sandia.gov>
2020-05-30 01:50:24 -0700

    EAM: Add a mass-conservation test to CIME tests.
    Set up a theta-sl-pg2 ne4pg2 test with -co2_cycle. Use POSTRUN_SCRIPT to run a
    python-2/3-compatible script to read "qv( 42)" and "dry M" output from Homme's
    prim_state_mod. Tracer 42 is CO2_FFF in this test. Check mass conservation for
    tracer 42 and dry air in the script. If they are fine, exit with 0; if bad, 1. A
    non-0 exit in a POSTRUN_SCRIPT makes the RUN phase fail.
    
    The new test is SMS_Ld9.ne4pg2_ne4pg2.FC5AV1C-04P2.cam-thetahy_sl_pg2_mass

--- cime/config/e3sm/tests.py
+++ cime/config/e3sm/tests.py
@@ -271,7 +271,8 @@ _TESTS = {
         "tests"    : (
                  "SMS_Ln5.ne4pg2_ne4pg2.FC5AV1C-L.cam-thetahy_pg2",
                  "SMS_Ln5.ne4pg2_ne4pg2.FC5AV1C-L.cam-thetahy_sl_pg2",
-                 "ERS_Ln5.ne4pg2_ne4pg2.FC5AV1C-L.cam-thetahy_sl_pg2"
+                 "ERS_Ln5.ne4pg2_ne4pg2.FC5AV1C-L.cam-thetahy_sl_pg2",
+                 "SMS_Ld9.ne4pg2_ne4pg2.FC5AV1C-04P2.cam-thetahy_sl_pg2_mass",
                  )
     },
     "e3sm_bench_hires_g" : {
