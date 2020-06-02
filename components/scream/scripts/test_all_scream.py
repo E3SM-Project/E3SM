3efc485e81a0337ee26c9ba5740f97db8bea4482
Luca Bertagna <lbertag@sandia.gov>
2020-06-02 17:56:43 -0600

    SCREAM: small test-all-scream mod.
    Printing out the full branch name (if available) rather than just 'HEAD'
    at TestAllScream construction time.

--- components/scream/scripts/test_all_scream.py
+++ components/scream/scripts/test_all_scream.py
@@ -71,7 +71,7 @@ class TestAllScream(object):
         if self._submit:
             expect(self._machine, "If dashboard submit request, must provide machine name")
 
-        print_last_commit(git_ref="HEAD")
+        print_last_commit(git_ref=self._original_branch)
 
         # Compute baseline info
         expect(not (self._baseline_ref and self._baseline_dir),
