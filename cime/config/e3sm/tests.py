abe614303ddb3b7516b3cb91be2188be75fb2219
Michael A. Brunke <brunke@email.arizona.edu>
2020-06-04 09:10:37 -0700

    Adds test for variable soil with CTC-RD

--- cime/config/e3sm/tests.py
+++ cime/config/e3sm/tests.py
@@ -32,6 +32,7 @@ _TESTS = {
             "ERS.f19_g16.I1850GSWCNPRDCTCBC.clm-ctc_f19_g16_I1850GSWCNPRDCTCBC",
             "ERS.f19_g16.I20TRGSWCNPRDCTCBC.clm-ctc_f19_g16_I20TRGSWCNPRDCTCBC",
             "ERS.f09_g16.ICLM45BC",
+            "ERS.f09_g16.I1850GSWCNPRDCTCBC.clm-vstrd",
             "SMS.r05_r05.I1850CLM45CN",
             )
         },
