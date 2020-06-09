04764ec3ab1442ff6dc1247900574b6a4e7f258f
Luca Bertagna <lbertag@sandia.gov>
2020-06-08 23:42:53 -0600

    SCREAM: first stab at using new AT feature
    As a test, we decide that 'AT: SKIP TESTING' can be used to bypass tests.

--- components/scream/scripts/jenkins/jenkins_common.sh
+++ components/scream/scripts/jenkins/jenkins_common.sh
@@ -1,13 +1,33 @@
 #!/bin/bash -xe
 
-source ./scream/components/scream/scripts/jenkins/${NODE_NAME}_setup
+# Check if the env var PR_LABELS is defined, and contains something meaningful
+IFS=';' read -r -a labels <<< "$PR_LABELS";
 
-git config --global user.email "jenkins@ignore.com"
-git config --global user.name "Jenkins Jenkins"
-
-SUBMIT="--submit"
-if [ -n "$PULLREQUESTNUM" ]; then
-    SUBMIT="" # We don't submit AT runs
+# default values
+skip_testing=0
+if [ ${#labels[@]} -gt 0 ]; then
+  # We do have some labels. Look for some supported ones.
+  for label in "${labels[@]}"
+  do
+    if [ "$label" == "AT: SKIP TESTING" ]; then
+      skip_testing=1
+    fi
+  done
 fi
 
-./scream/components/scream/scripts/gather-all-data "./scripts/test-all-scream \$compiler -p -i -m \$machine $SUBMIT" -l -m $SCREAM_MACHINE
+if [ $skip_testing -eq 0 ]; then
+  # User did not request to skip tests. Proceed with testing.
+  source ./scream/components/scream/scripts/jenkins/${NODE_NAME}_setup
+
+  git config --global user.email "jenkins@ignore.com"
+  git config --global user.name "Jenkins Jenkins"
+
+  SUBMIT="--submit"
+  if [ -n "$PULLREQUESTNUM" ]; then
+      SUBMIT="" # We don't submit AT runs
+  fi
+
+  ./scream/components/scream/scripts/gather-all-data "./scripts/test-all-scream \$compiler -p -i -m \$machine $SUBMIT" -l -m $SCREAM_MACHINE
+else
+  echo "Tests were skipped, since the Github label 'AT: SKIP TESTING' was found.\n"
+fi
