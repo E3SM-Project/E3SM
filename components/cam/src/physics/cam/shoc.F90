d78376581cdf835ad229e6f5e49d3f9b42de858c
Balwinder Singh <balwindersingh@gmail.com>
2020-06-17 18:23:59 -0700

    all pass:12bdg: ck is param

--- components/cam/src/physics/cam/shoc.F90
+++ components/cam/src/physics/cam/shoc.F90
@@ -2402,7 +2402,7 @@ subroutine adv_sgs_tke(nlev, shcol, dtime, shoc_mix, wthv_sec, &
 
   !real(rtype) :: lambda_low,lambda_high,lambda_slope, brunt_low
   !real(rtype) :: zL_crit_val, pbl_trans
-  real(rtype) :: Ck
+  !real(rtype) :: Ck
   real(rtype) :: Cs
   !real(rtype) :: Ckh
   !real(rtype) :: Ckm
@@ -2428,7 +2428,7 @@ subroutine adv_sgs_tke(nlev, shcol, dtime, shoc_mix, wthv_sec, &
 
   ! Turbulent coefficients
   !real(rtype), parameter :: Cs  = 0.15_rtype
-  !real(rtype), parameter :: Ck  = 0.1_rtype
+  real(rtype), parameter :: Ck  = 0.1_rtype
   real(rtype), parameter :: Ckh = 0.1_rtype
   real(rtype), parameter :: Ckm = 0.1_rtype
   real(rtype), parameter :: Ckh_s = 1.0_rtype
@@ -2454,7 +2454,7 @@ subroutine adv_sgs_tke(nlev, shcol, dtime, shoc_mix, wthv_sec, &
 
   ! Turbulent coefficients
   Cs=0.15_rtype
-  Ck=0.1_rtype
+  !Ck=0.1_rtype
   ! eddy coefficients for diffusivities
   !Ckh=0.1_rtype
   !Ckm=0.1_rtype
