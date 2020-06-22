18af62f437a2cda019bfed31412a773772b4ee9e
Luca Bertagna <lbertag@sandia.gov>
2020-06-22 14:27:54 -0600

    Fixes to ScalarTraits meta-struct
    - a copy/paste had left a 'float' where a 'double' was needed
    - type checks should be made on the type name stripped od any
      cv qualifier and reference.

--- components/scream/ekat/src/ekat/ekat_scalar_traits.hpp
+++ components/scream/ekat/src/ekat/ekat_scalar_traits.hpp
@@ -6,6 +6,7 @@
 #include <limits>
 #include <climits>
 #include <type_traits>
+#include <typeinfo>
 
 #ifdef KOKKOS_ENABLE_CUDA
 #include <math_constants.h>
@@ -16,15 +17,18 @@ namespace scream {
 template<typename T>
 struct ScalarTraits {
 
-  static_assert (std::is_arithmetic<T>::value, "Error! Template parameter 'T' in ScalarTraits must be a numeric type.\n");
+  using raw_type = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+
+  static_assert (std::is_arithmetic<raw_type>::value, "Error! Template parameter 'T' in ScalarTraits must be a numeric type.\n");
 
   KOKKOS_INLINE_FUNCTION
-  static const T quiet_NaN () {
-    scream_kassert_msg(std::is_floating_point<T>::value, "Error! NaN is only available for floating point types.\n");
+  static const raw_type quiet_NaN () {
+    scream_kassert_msg(std::is_floating_point<raw_type>::value,
+                       "Error! NaN is only available for floating point types.\n");
 #ifdef __CUDA_ARCH__
-    if (std::is_same<T,float>::value) {
+    if (std::is_same<raw_type,float>::value) {
       return CUDART_NAN_F;
-    } else if (std::is_same<T,float>::value) {
+    } else if (std::is_same<raw_type,double>::value) {
       return CUDART_NAN;
     } else {
       scream_kerror_msg ("Error! No NaN provided for this floating point type.\n");
@@ -32,30 +36,31 @@ struct ScalarTraits {
       return 0;
     }
 #else
-    return std::numeric_limits<T>::quiet_NaN();
+    return std::numeric_limits<raw_type>::quiet_NaN();
 #endif
   }
 
   KOKKOS_INLINE_FUNCTION
-  static const T invalid () {
+  static const raw_type invalid () {
     // For a floating point, return NaN. For an integer, return largest possible number
-    T val(0);
-    if (std::is_floating_point<T>::value) {
-      val = quiet_NaN();
+    raw_type val(0);
+
+    if (std::is_floating_point<raw_type>::value) {
+      val = ScalarTraits<raw_type>::quiet_NaN();
     } else {
       // If cuda supported numeric_limits, we would not need all these ifs
-      if (std::is_same<T,int>::value) {
-        val = static_cast<T>(INT_MAX);
-      } else if (std::is_same<T,unsigned int>::value) {
-        val = static_cast<T>(UINT_MAX);
-      } else if (std::is_same<T,long>::value) {
-        val = static_cast<T>(LONG_MAX);
+      if (std::is_same<raw_type,int>::value) {
+        val = static_cast<raw_type>(INT_MAX);
+      } else if (std::is_same<raw_type,unsigned int>::value) {
+        val = static_cast<raw_type>(UINT_MAX);
+      } else if (std::is_same<raw_type,long>::value) {
+        val = static_cast<raw_type>(LONG_MAX);
       } else if (std::is_same<T,unsigned long>::value) {
-        val = static_cast<T>(ULONG_MAX);
-      } else if (std::is_same<T,long long>::value) {
-        val = static_cast<T>(LLONG_MAX);
-      } else if (std::is_same<T,unsigned long long>::value) {
-        val = static_cast<T>(ULLONG_MAX);
+        val = static_cast<raw_type>(ULONG_MAX);
+      } else if (std::is_same<raw_type,long long>::value) {
+        val = static_cast<raw_type>(LLONG_MAX);
+      } else if (std::is_same<raw_type,unsigned long long>::value) {
+        val = static_cast<raw_type>(ULLONG_MAX);
       }
     }
     return val;
