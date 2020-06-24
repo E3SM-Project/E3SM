272652e5b30fbf08fa234c43ad163d828aade556
Luca Bertagna <lbertag@sandia.gov>
2020-06-24 17:58:10 -0600

    Fixed cmake warning when processing yaml-cpp

--- components/scream/ekat/cmake/YamlCpp.cmake
+++ components/scream/ekat/cmake/YamlCpp.cmake
@@ -19,8 +19,8 @@ if (NOT IS_YAMLCPP_ALREADY_BUILT)
   set (YAMLCPP_BINARY_DIR ${CMAKE_BINARY_DIR}/externals/yaml-cpp)
 
   # We don't want testing or any yaml-cpp executable at all
-  set (YAML_CPP_BUILD_TESTS FALSE)
-  set (YAML_CPP_BUILD_TOOLS FALSE)
+  option (YAML_CPP_BUILD_TOOLS "Enable parse tools" OFF)
+  option (YAML_CPP_BUILD_TESTS "Enable yaml-cpp tests" OFF)
   add_subdirectory (${YAMLCPP_SOURCE_DIR} ${YAMLCPP_BINARY_DIR})
 
   set (YAMLCPP_INCLUDE_DIRS ${YAMLCPP_SOURCE_DIR}/include)
