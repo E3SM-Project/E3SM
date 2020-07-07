aa17f85046b028a33870ec84f141ae22a12a4b9b
Patrick Worley <worleyph@gmail.com>
2020-07-06 17:51:14 -0700

    Remove performance archiving support for Edison
    The system edison is not longer operational. Removed performance
    archiving support for edison in provenance.py and removed
    the job progress monitoring script syslog.edison .
    
    [BFB]

--- cime/scripts/lib/CIME/provenance.py
+++ cime/scripts/lib/CIME/provenance.py
@@ -17,7 +17,7 @@ def _get_batch_job_id_for_syslog(case):
     """
     mach = case.get_value("MACH")
     try:
-        if mach in ['anvil', 'compy', 'edison', 'cori-haswell', 'cori-knl']:
+        if mach in ['anvil', 'compy', 'cori-haswell', 'cori-knl']:
             return os.environ["SLURM_JOB_ID"]
         elif mach in ['theta']:
             return os.environ["COBALT_JOBID"]
@@ -142,7 +142,7 @@ def _save_prerun_timing_e3sm(case, lid):
                 filename = "%s.%s" % (filename, lid)
                 run_cmd_no_fail(cmd, arg_stdout=filename, from_dir=full_timing_dir)
                 gzip_existing_file(os.path.join(full_timing_dir, filename))
-        elif mach in ["edison", "cori-haswell", "cori-knl"]:
+        elif mach in ["cori-haswell", "cori-knl"]:
             for cmd, filename in [("sinfo -a -l", "sinfol"), ("sqs -f %s" % job_id, "sqsf_jobid"),
                                   # ("sqs -f", "sqsf"),
                                   ("squeue -o '%.10i %.15P %.20j %.10u %.7a %.2t %.6D %.8C %.10M %.10l %.20S %.20V'", "squeuef"),
@@ -329,7 +329,7 @@ def _save_postrun_timing_e3sm(case, lid):
             globs_to_copy.append("%s*error" % job_id)
             globs_to_copy.append("%s*output" % job_id)
             globs_to_copy.append("%s*cobaltlog" % job_id)
-        elif mach in ["edison", "cori-haswell", "cori-knl"]:
+        elif mach in ["cori-haswell", "cori-knl"]:
             globs_to_copy.append("%s*run*%s" % (case.get_value("CASE"), job_id))
         elif mach == "summit":
             globs_to_copy.append("e3sm.stderr.%s" % job_id)
