d9c9c3138b93195f5801fe44f6cf56a2ea55e49c
Balwinder Singh <balwindersingh@gmail.com>
2020-07-10 10:24:34 -0700

    Added SPMD CPP def to hide CAM infrastructure routines

--- components/cam/src/physics/cam/debug_info.F90
+++ components/cam/src/physics/cam/debug_info.F90
@@ -63,11 +63,17 @@ contains
   subroutine report_error_info(cause, calling_func, icol, klev, prnt_macmic)
 
     !Reports simulation parameters
-    use cam_logfile,    only: iulog
+
+    use micro_p3_utils, only: iulog_e3sm
+    use physics_utils,  only: rtype
+
+#ifdef SPMD
+    !Modules to use when SCREAM run with MPI under E3SM
+    use spmd_utils,     only: iam
     use phys_grid,      only: get_rlat_p, get_rlon_p
     use time_manager,   only: get_nstep
-    use physics_utils,  only: rtype
-    use spmd_utils,     only: iam
+#endif
+
     implicit none
 
     !intent-ins
@@ -80,6 +86,8 @@ contains
 
     !local variables
     real(rtype), parameter :: radian2deg = 180.0_rtype/(4.0_rtype*atan(1.0_rtype)) !180/pi = 57.296
+
+    integer                :: col_to_prnt, proc_num
     real(rtype)            :: lat, lon
     character(len=1000)    :: klevstr, macmicstr, icolstr
 
@@ -98,27 +106,37 @@ contains
     endif
 
     !update icolstr to add info about the column used and compute lat lon
+    lat = huge(1.0_rtype)
+    lon = huge(1.0_rtype)
     if(present(icol)) then
-       icolstr = '*NOTE*: Using user specified column index for computing lat-lons'
-       lat = get_rlat_p(chunk,icol)*radian2deg
-       lon = get_rlon_p(chunk,icol)*radian2deg
+       icolstr     = '*NOTE*: Using user specified column index for computing lat-lons'
+       col_to_prnt = icol
     else
        icolstr = '*NOTE*: Using auto updated column index from P3 for computing lat-lons'
-       lat = get_rlat_p(chunk,icol_p3)*radian2deg
-       lon = get_rlon_p(chunk,icol_p3)*radian2deg
+       col_to_prnt = icol_p3
     endif
 
+    !initialize proc_num to a negative value
+    proc_num = -1
+#ifdef SPMD
+    lat = get_rlat_p(chunk, col_to_prnt) * radian2deg
+    lon = get_rlon_p(chunk, col_to_prnt) * radian2deg
+    proc_num = iam
+#endif
+
     !print proc info in the message and issue only one write statement
-    write(iulog,'(a,/,a,i5,a,/,a,/,a,/,a,f12.8,a,f12.8,a,/,a,/,a,i5,/,a,/,a,i5,a,/,a)') &
+    write(iulog_e3sm,'(a,/,a,i5,a,/,a,/,a,/,a,f12.8,a,f12.8,a,/,a,/,a,i5,/,a,/,a,i5,a,/,a)') &
          '', &
-         '********************** Proc #', iam,' output *******************************************', &
+         '********************** Proc #', proc_num,' output *******************************************', &
          'Fail due to '//trim(adjustl(cause))//' in subroutine/function:'//trim(adjustl(calling_func)), &
+#ifdef SPMD
          trim(adjustl(icolstr)), &
          'Column location in degrees(lat,lon) is:(',lat,',',lon,')', &
          trim(adjustl(macmicstr)), &
          'timestep:',get_nstep(), &
+#endif
          trim(adjustl(klevstr)), &
-         '********************** Proc #', iam,' output Ends **************************************', &
+         '********************** Proc #', proc_num,' output Ends **************************************', &
          ''
   end subroutine report_error_info
 
