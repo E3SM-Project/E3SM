e2576d44f8113b869de240987d7094397029fc6b
James Foucar <jgfouca@sandia.gov>
2020-07-13 13:59:01 -0600

    These files should not have been added back

--- cime/config/e3sm/machines/Depends.cray.cmake
+++
@@ -1,1 +0,0 @@
-list(NOOPT_FILES APPEND cice/src/mpi/ice_boundary.F90 cice/src/serial/ice_boundary.F90)
--- cime/config/e3sm/machines/Depends.nag.cmake
+++
@@ -1,1 +0,0 @@
-list(NOOPT_FILES APPEND cam/src/control/wrap_mpi.F90 cam/src/utils/fft99.F90)
