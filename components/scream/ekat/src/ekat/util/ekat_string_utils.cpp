c292ae5880d481437bb6f21eeca00c6af8606a72
Luca Bertagna <lbertag@sandia.gov>
2020-07-22 16:43:44 -0600

    Added string utilities, and tested them.
    - Utility to split a string with a given delimiter.
    - Utility to strip a given char from a string.
    
    Also, moved string_utils.hpp to ekat_string_utils.hpp, and
    moved impl of some non-templated method to a cpp file.

---
+++ components/scream/ekat/src/ekat/util/ekat_string_utils.cpp
@@ -0,0 +1,92 @@
+#include <ekat/util/ekat_string_utils.hpp>
+#include <algorithm>
+#include <sstream>
+
+namespace scream {
+namespace util {
+
+void strip (std::string& str, const char c) {
+  auto new_end = std::remove(str.begin(),str.end(),c);
+  str.erase(new_end,str.end());
+}
+
+std::vector<std::string> split(const std::string& str, const char del) {
+  std::vector<std::string> blocks;
+
+  auto start = 0;
+  auto pos = str.find(del);
+  while (pos!=std::string::npos) {
+    blocks.push_back(str.substr(start,pos-start));
+    start = pos + 1;
+    pos = str.find(del,start);
+  }
+  return blocks;
+}
+
+std::string trim (const std::string& s) {
+  if (s=="") {
+    return s;
+  }
+  int nl = 0;
+  while (s[nl] == ' ') {
+    ++nl;
+  }
+  int size = s.size();
+  int nt = 0;
+  while (nt<size && s[size-nt-1] == ' ') {
+    ++nt;
+  }
+
+  return s.substr(nl,size-nl-nt);
+}
+
+std::string strint (const std::string& s, const int i) {
+  std::stringstream ss;
+  ss << s << " " << i;
+  return ss.str();
+}
+
+bool caseInsensitiveEqualString (const std::string& s1, const std::string& s2) {
+  auto charComp = [](const char c1, const char c2)->bool{
+    return c1==c2 || std::toupper(c1)==std::toupper(c2);
+  };
+  return s1.size()==s2.size() &&
+         std::equal(s1.begin(),s1.end(),s2.begin(),charComp);
+}
+
+bool caseInsensitiveLessString (const std::string& s1, const std::string& s2) {
+  auto charCompLess = [](const char c1, const char c2)->bool{
+    return std::toupper(c1)<std::toupper(c2);
+  };
+  auto charCompEq = [](const char c1, const char c2)->bool{
+    return std::toupper(c1)==std::toupper(c2);
+  };
+  for (auto it1=s1.begin(),it2=s2.begin(); it1!=s1.end() && it2!=s2.end(); ++it1,++it2) {
+    if (charCompLess(*it1,*it2)) {
+      return true;
+    } else if (!charCompEq(*it1,*it2)) {
+      return false;
+    }
+  }
+  return s1.size()<s2.size();
+}
+
+bool caseInsensitiveLessEqualString (const std::string& s1, const std::string& s2) {
+  auto charCompLess = [](const char c1, const char c2)->bool{
+    return std::toupper(c1)<std::toupper(c2);
+  };
+  auto charCompEq = [](const char c1, const char c2)->bool{
+    return std::toupper(c1)==std::toupper(c2);
+  };
+  for (auto it1=s1.begin(),it2=s2.begin(); it1!=s1.end() && it2!=s2.end(); ++it1,++it2) {
+    if (charCompLess(*it1,*it2)) {
+      return true;
+    } else if (!charCompEq(*it1,*it2)) {
+      return false;
+    }
+  }
+  return s1.size()<=s2.size();
+}
+
+} // namespace util
+} // namespace scream
---
+++ components/scream/ekat/src/ekat/util/ekat_string_utils.hpp
@@ -0,0 +1,81 @@
+#ifndef EKAT_STRING_UTILS_HPP
+#define EKAT_STRING_UTILS_HPP
+
+#include <string>
+#include <vector>
+
+namespace scream {
+namespace util {
+
+// Strip character c from input string
+void strip (std::string& str, const char c);
+
+// Split a string at every occurrence of given delimiter char
+std::vector<std::string> split(const std::string& str, const char del);
+
+// Trim leading/trailing whitespaces.
+std::string trim (const std::string& s);
+
+// Small utility that cats a space and an integer to an input string.
+std::string strint (const std::string& s, const int i);
+
+// A no-overhead class that inherits from std::string, which we only
+// use to trigger different behavior in the ==,!=,<,<= operators.
+class CaseInsensitiveString final : public std::string
+{
+public:
+  template<typename... Args>
+  CaseInsensitiveString (Args... args)
+   : std::string(args...)
+  {}
+
+  virtual ~CaseInsensitiveString () = default;
+};
+
+// Case-insensitive comparison functions
+bool caseInsensitiveEqualString (const std::string& s1, const std::string& s2);
+bool caseInsensitiveLessString (const std::string& s1, const std::string& s2);
+bool caseInsensitiveLessEqualString (const std::string& s1, const std::string& s2);
+
+// Overloads of comparison operators, which use the routines above if at least one
+// of the two inputs is indeed a CaseInsensitiveString
+template<typename S1, typename S2>
+typename std::enable_if<
+      std::is_same<S1,CaseInsensitiveString>::value ||
+      std::is_same<S2,CaseInsensitiveString>::value,
+      bool>::type
+operator== (const S1& s1, const S2& s2) {
+  return caseInsensitiveEqualString(s1,s2);
+}
+
+template<typename S1, typename S2>
+typename std::enable_if<
+      std::is_same<S1,CaseInsensitiveString>::value ||
+      std::is_same<S2,CaseInsensitiveString>::value,
+      bool>::type
+operator!= (const S1& s1, const S2& s2) {
+  return ! (s1==s2);
+}
+
+template<typename S1, typename S2>
+typename std::enable_if<
+      std::is_same<S1,CaseInsensitiveString>::value ||
+      std::is_same<S2,CaseInsensitiveString>::value,
+      bool>::type
+operator< (const S1& s1, const S2& s2) {
+  return caseInsensitiveLessString(s1,s2);
+}
+
+template<typename S1, typename S2>
+typename std::enable_if<
+      std::is_same<S1,CaseInsensitiveString>::value ||
+      std::is_same<S2,CaseInsensitiveString>::value,
+      bool>::type
+operator<= (const S1& s1, const S2& s2) {
+  return caseInsensitiveLessEqualString(s1,s2);
+}
+
+} // namespace util
+} // namespace scream
+
+#endif // EKAT_STRING_UTILS_HPP
