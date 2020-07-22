e9f0aa8430225f695bbbaa4051ee541b58e25bb0
Steven Brus <sbrus@lanl.gov>
2020-07-22 08:33:17 -0600

    Add vegetation drag to hurricane cases
      - This includes the following resolutions
           USDEQU240at60cr20rr4WD_veg
           USDEQU120at30cr10rr2WD_veg
           USDEQU60at15cr5rr1WD_veg

--- testing_and_setup/compass/ocean/hurricane/scripts/vegetation_tif_to_mpas.py
+++ testing_and_setup/compass/ocean/hurricane/scripts/vegetation_tif_to_mpas.py
@@ -35,8 +35,36 @@ def read_tif(filename):
 
     return data, lat, lon
 
+def mesh_tri(grid_file):
+
+  ncmesh = Dataset(grid_file,'r')
+  lon_mesh = np.rad2deg(ncmesh.variables['lonCell'][:])-360.0
+  #lon_mesh = np.rad2deg(np.mod(ncmesh.variables['lonCell'][:] + np.pi, 2.0*np.pi) - np.pi)
+  lat_mesh = np.rad2deg(ncmesh.variables['latCell'][:])
+  nEdgesOnCell = ncmesh.variables['nEdgesOnCell'][:]
+  cellsOnCell = ncmesh.variables['cellsOnCell'][:,:]
+ 
+  # Triangulate cells 
+  triangles = Triangulation(lon_mesh,lat_mesh)
+
+  # Compute triangulation mask (needs to be vectorized)
+  mask = np.array(np.zeros((triangles.triangles.shape[0],)),dtype=bool)
+  ntri = triangles.neighbors.shape[0]
+  for i in range(ntri):
+    k = 0
+    for j in range(3):
+      n = triangles.triangles[i,j]
+      if nEdgesOnCell[n] != np.where(cellsOnCell[n,:] != 0)[0].size:  # Mask triangles
+        k = k +1                                                      # containing
+    if k == 3:                                                        # 3 boundary
+      mask[i] = True                                                  # cells
+  triangles.set_mask(mask)
+
+
+  return triangles
+
 ######################## function to interpolate tif data to MPAS-O grid ###########################
-def interpolate_data_to_grid(data_file,grid_file):
+def interpolate_data_to_grid(data_file,grid_file,fill=0):
 
     # get tif data info
     data, lat_data, lon_data = read_tif(data_file)
@@ -51,14 +79,14 @@ def interpolate_data_to_grid(data_file,grid_file):
 
     # interpolate tif data onto new grid
     interpolator = grid_interpolator((lon_data,lat_data[::-1]),np.flipud(data).T,
-                                     method='linear',
-                                     bounds_error=False,fill_value=0.0)
+                                     method='nearest',
+                                     bounds_error=False,fill_value=fill)
     interp_data = interpolator(grid_points)
 
     return lon_data,lat_data,data,lon_grid,lat_grid,interp_data
 
 ############################### function to plot interpolated data #################################
-def plot_interp_data(lon_data,lat_data,data,lon_grid,lat_grid,interp_data):
+def plot_interp_data(lon_data,lat_data,data,lon_grid,lat_grid,interp_data,tri):
 
     # plot original data
     fig, ax = plt.subplots(1,2, figsize=(12,5))
@@ -69,15 +97,14 @@ def plot_interp_data(lon_data,lat_data,data,lon_grid,lat_grid,interp_data):
     cbar = fig.colorbar(cf,ax=ax[0])
 
     # plot interpolated data
-    lon_max = max(lon_data)
-    lon_min = min(lon_data)
-    lat_max = max(lat_data)
-    lat_min = min(lat_data)
+    lon_max = np.max(lon_data) + 5.0
+    lon_min = np.min(lon_data) - 5.0
+    lat_max = np.max(lat_data) + 5.0
+    lat_min = np.min(lat_data) - 5.0
     levels = np.linspace(np.amin(interp_data),np.amax(interp_data),10)
-    cf = ax[1].tricontourf(lon_grid[(lon_grid>lon_min) & (lon_grid<lon_max) & (lat_grid>lat_min) & (lat_grid<lat_max)],
-                         lat_grid[(lon_grid>lon_min) & (lon_grid<lon_max) & (lat_grid>lat_min) & (lat_grid<lat_max)],
-                         interp_data[(lon_grid>lon_min) & (lon_grid<lon_max) & (lat_grid>lat_min) & (lat_grid<lat_max)],
-                         levels=levels)
+    cf = ax[1].tricontourf(tri, interp_data, levels=levels)
+    ax[1].set_xlim([lon_min,lon_max])
+    ax[1].set_ylim([lat_min,lat_max])
     ax[1].set_title('interpolated data')
     cbar = fig.colorbar(cf,ax=ax[1])
 
@@ -91,6 +118,7 @@ if __name__ == '__main__':
   import pprint
   import numpy as np
   import matplotlib.pyplot as plt
+  from matplotlib.tri import Triangulation
   from scipy.interpolate import RegularGridInterpolator as grid_interpolator
   from netCDF4 import Dataset
 
@@ -106,9 +134,11 @@ if __name__ == '__main__':
   vegDiameter_file = cfg['input_location'] + cfg['vegetation_diameter_file']
   Manning_file = cfg['input_location'] + cfg['Delaware_Manning_file']
   grid_file = cfg['mpas_grid_file']
+  bottomDrag_file = cfg['bottomDrag_file']
   figplot = cfg['figplot']
   output_file = cfg['output_file']
 
+  tri = mesh_tri(grid_file)
 ############################### data interpolation and plot ########################################
   ##vegetation Mask
   lon_veg, lat_veg, vegMask, lon_grid, lat_grid, interp_vegMask = interpolate_data_to_grid(vegMask_file, grid_file)
@@ -118,70 +148,69 @@ if __name__ == '__main__':
   interp_vegMask[interp_vegMask>0]=1
   interp_vegMask = interp_vegMask.astype(int)
   if (figplot):
-    plot_interp_data(lon_veg, lat_veg, vegMask, lon_grid, lat_grid, interp_vegMask)
+    plot_interp_data(lon_veg, lat_veg, vegMask, lon_grid, lat_grid, interp_vegMask, tri)
     plt.savefig('vegetation_mask.png')
 
   ##vegHeight
   lon_veg, lat_veg, vegHght, lon_grid, lat_grid, interp_vegHght = interpolate_data_to_grid(vegHeight_file, grid_file)
   if (figplot):
-    plot_interp_data(lon_veg, lat_veg, vegHght, lon_grid, lat_grid, interp_vegHght)
+    plot_interp_data(lon_veg, lat_veg, vegHght, lon_grid, lat_grid, interp_vegHght, tri)
+    plt.savefig('vegetation_height.png')
 
   ##vegDensity
   lon_veg, lat_veg, vegDens, lon_grid, lat_grid, interp_vegDens = interpolate_data_to_grid(vegDensity_file, grid_file)
   if (figplot):
-    plot_interp_data(lon_veg, lat_veg, vegDens, lon_grid, lat_grid, interp_vegDens)
+    plot_interp_data(lon_veg, lat_veg, vegDens, lon_grid, lat_grid, interp_vegDens, tri)
+    plt.savefig('vegetation_density.png')
 
   ##vegDiameter
   lon_veg, lat_veg, vegDiam, lon_grid, lat_grid, interp_vegDiam = interpolate_data_to_grid(vegDiameter_file, grid_file)
   if (figplot):
-    plot_interp_data(lon_veg, lat_veg, vegDiam, lon_grid, lat_grid, interp_vegDiam)
+    plot_interp_data(lon_veg, lat_veg, vegDiam, lon_grid, lat_grid, interp_vegDiam, tri)
+    plt.savefig('vegetation_diameter.png')
 
   ##Manning
-  lon_manning, lat_manning, manning, lon_grid, lat_grid, interp_manning = interpolate_data_to_grid(Manning_file, grid_file)
+  forcing_nc = Dataset(bottomDrag_file,'r')
+  manning_init = forcing_nc.variables['bottomDrag'][:]
+  lon_manning, lat_manning, manning, lon_grid, lat_grid, interp_manning = interpolate_data_to_grid(Manning_file, grid_file, fill=-9999)
+  interp_manning[interp_manning < 1e-5] = manning_init[interp_manning < 1e-5]
   if (figplot):
-    plot_interp_data(lon_manning, lat_manning, manning, lon_grid, lat_grid, interp_manning)
+    plot_interp_data(lon_manning, lat_manning, manning, lon_grid, lat_grid, manning_init, tri)
+    plt.savefig('manning_init.png')
+    plot_interp_data(lon_manning, lat_manning, manning, lon_grid, lat_grid, interp_manning, tri)
+    plt.savefig('vegetation_manning.png')
 
 ################################# output file generation ###########################################
   if os.path.exists(output_file):
     os.remove(output_file)
 
-  newfile = Dataset(output_file,'w', format='NETCDF4')
+  newfile = Dataset(output_file,'w', format='NETCDF3_64BIT_OFFSET')
 
   # define dimension
-  newfile.createDimension('nCell',len(lat_grid))
+  newfile.createDimension('nCells',len(lat_grid))
 
   # create variables
-  latitude = newfile.createVariable('latCell', np.float32, ('nCell'))
-  latitude.standard_name='latitude'
-  latitude.units ='degree'
-
-  longitude = newfile.createVariable('lonCell', np.float32, ('nCell'))
-  longitude.standard_name='longitude'
-  longitude.units ='degree'
-
-  veg_mask = newfile.createVariable('vegetationMask', np.int8, ('nCell'))
+  veg_mask = newfile.createVariable('vegetationMask', np.int32, ('nCells'))
   veg_mask.standard_name='vegetation_mask'
   veg_mask.units =''
 
-  veg_diam = newfile.createVariable('vegetationDiameter', np.float32, ('nCell'))
+  veg_diam = newfile.createVariable('vegetationDiameter', np.float64, ('nCells'))
   veg_diam.standard_name='stem diameter of each vegetation shoot'
   veg_diam.units ='m'
 
-  veg_hght = newfile.createVariable('vegetationHeight', np.float32, ('nCell'))
+  veg_hght = newfile.createVariable('vegetationHeight', np.float64, ('nCells'))
   veg_hght.standard_name='stem height of each vegetation shoot'
   veg_hght.units ='m'
 
-  veg_dens = newfile.createVariable('vegetationDensity', np.float32, ('nCell'))
+  veg_dens = newfile.createVariable('vegetationDensity', np.float64, ('nCells'))
   veg_dens.standard_name='stem numbers per unit area'
   veg_dens.units ='m^{-2}'
 
-  Delaware_manning = newfile.createVariable('bottomDrag', np.float32, ('nCell'))
+  Delaware_manning = newfile.createVariable('bottomDrag', np.float64, ('nCells'))
   Delaware_manning.standard_name='bottom Manning roughness coefficient'
   Delaware_manning.units ='s/m^{1/3}'
 
   # assign variable values
-  latitude[:] = lat_grid[:]
-  longitude[:] = lon_grid[:]+360
   veg_mask[:] = interp_vegMask[:]
   veg_diam[:] = interp_vegDiam[:]
   veg_hght[:] = interp_vegHght[:]
