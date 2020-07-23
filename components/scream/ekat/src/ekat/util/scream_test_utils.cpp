2c9de0ed484b9c31ab021294e920bcdd4a6ecc16
Luca Bertagna <lbertag@sandia.gov>
2020-07-23 12:12:12 -0600

    Fix compilation errors on GPU builds.

--- components/scream/ekat/src/ekat/util/scream_test_utils.cpp
+++ components/scream/ekat/src/ekat/util/scream_test_utils.cpp
@@ -35,35 +35,34 @@ int get_test_device (const int mpi_rank)
                         "       Res group id for this rank: " + std::to_string(my_res_group) + "\n");
 
     key += "_" + std::string(res_type);
-    scream_require_msg(getenv(key)!=nullptr,
+    scream_require_msg(getenv(key.c_str())!=nullptr,
                         "Error! Missing '" + key + "' env var. Something might be off with res group detection,\n"
                         "       or with the properties set for the test.\n"
                         "       CTEST_RESOURCE_COUNT: " + std::to_string(res_group_count) + "\n"
                         "       Res group id for this rank: " + std::to_string(my_res_group) + "\n"
-                        "       Res group type for this rank: " + std::to_string(res_type) + "\n");
+                        "       Res group type for this rank: " + std::string(res_type) + "\n");
 
-      auto res = std::string(getenv(key));
+    auto res = std::string(getenv(key.c_str()));
 
-      // res should look like 'id:N,slots:M'. If multiple resources are assigned to a group,
-      // there would be multiple strings like that, separated by ';'. We don't support that,
-      // so we check that there's no ';' in res.
-      // Note: We should always have M=1, since ekat only asks for 1 slot per resource
-      scream_require_msg(res.find(';')==res.end(), "Error! Multiple resources specified for group " + std::to_string(my_res_group) + "\n");
+    // res should look like 'id:N,slots:M'. If multiple resources are assigned to a group,
+    // there would be multiple strings like that, separated by ';'. We don't support that,
+    // so we check that there's no ';' in res.
+    // Note: We should always have M=1, since ekat only asks for 1 slot per resource
+    scream_require_msg(res.find(';')==std::string::npos, "Error! Multiple resources specified for group " + std::to_string(my_res_group) + "\n");
 
-      auto id_N_slots_M = split(res,',');
-      scream_require_msg(id_N_slots_M.size()==2, "Error! Something seems wrong with resource spec '" + res + "'\n");
+    auto id_N_slots_M = split(res,',');
+    scream_require_msg(id_N_slots_M.size()==2, "Error! Something seems wrong with resource spec '" + res + "'\n");
 
-      auto slots_M = split(id_N_slots_M[1],':');
-      scream_require_msg(slots_M.size()==2, "Error! Something seems wrong with resource spec '" + res + "'\n");
-      scream_require_msg(slots_M[0]=="slots", "Error! Something seems wrong with resource spec '" + res + "'\n");
-      scream_require_msg(slots_M[1]=="1", "Error! Something seems wrong with resource spec '" + res + "'\n");
+    auto slots_M = split(id_N_slots_M[1],':');
+    scream_require_msg(slots_M.size()==2, "Error! Something seems wrong with resource spec '" + res + "'\n");
+    scream_require_msg(slots_M[0]=="slots", "Error! Something seems wrong with resource spec '" + res + "'\n");
+    scream_require_msg(slots_M[1]=="1", "Error! Something seems wrong with resource spec '" + res + "'\n");
 
-      auto id_N = split(id_N_slots_M[0],':');
-      scream_require_msg(id_N.size()==2, "Error! Something seems wrong with resource spec '" + res + "'\n";
-      scream_require_msg(id_N[0]=="id", "Error! Something seems wrong with resource spec '" + res + "'\n");
+    auto id_N = split(id_N_slots_M[0],':');
+    scream_require_msg(id_N.size()==2, "Error! Something seems wrong with resource spec '" + res + "'\n");
+    scream_require_msg(id_N[0]=="id", "Error! Something seems wrong with resource spec '" + res + "'\n");
 
-      dev_id = std::atoi(id_N[1]);
-    }
+    dev_id = std::atoi(id_N[1]);
   }
 #else
   (void) mpi_rank;
