088387c44c098874ebba406efd47ebb7e3ce45f2
Luca Bertagna <lbertag@sandia.gov>
2020-08-20 00:07:08 -0600

    SCREAM: added pack size info to scream config string

--- components/scream/src/share/scream_config.cpp
+++ components/scream/src/share/scream_config.cpp
@@ -8,6 +8,7 @@ std::string scream_config_string() {
   auto config = ekat::util::ekat_config_string();
 
   config += "sizeof(Real) = " + std::to_string(sizeof(Real)) + "\n";
+  config += "default pack size = " + std::to_string(SCREAM_PACK_SIZE) + "\n";
 
   return config;
 }
