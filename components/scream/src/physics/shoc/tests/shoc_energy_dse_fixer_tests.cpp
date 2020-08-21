e8e08b061b2224c44486baf36df96622a04cacf8
Peter Andrew Bogenschutz <bogensch@quartz2306.llnl.gov>
2020-08-21 14:34:20 -0700

    finish up test for dse fixer

--- components/scream/src/physics/shoc/tests/shoc_energy_dse_fixer_tests.cpp
+++ components/scream/src/physics/shoc/tests/shoc_energy_dse_fixer_tests.cpp
@@ -26,14 +26,14 @@ struct UnitWrap::UnitTest<D>::TestShocEnergyDseFixer {
 
   static void run_property()
   {
-    static constexpr Int shcol    = 3;
+    static constexpr Int shcol    = 6;
     static constexpr Int nlev     = 5;
     static constexpr auto nlevi   = nlev + 1;
 
     // Tests for the SHOC function
     //     shoc_energy_dse_fixer
 
-    // TEST ONE
+    // TEST
     // For columns that are identical EXCEPT for the shoctop indicee,
     //  verify that given a positive value of energy imbalance that
     //  columns with a higher SHOC top were subject to more energy removal.
@@ -46,14 +46,14 @@ struct UnitWrap::UnitTest<D>::TestShocEnergyDseFixer {
     static constexpr Real se_dis = 0.1;
 
     // level indicee of SHOC top layer
-    static constexpr Int shoctop[shcol] = {5, 3, 1};
+    static constexpr Int shoctop[shcol] = {5, 3, 1, 2, 4, 4};
 
     // Initialzie data structure for bridgeing to F90
     SHOCEnergydsefixerData SDS(shcol, nlev);
 
     // Test that the inputs are reasonable.
-    // for this test we need exactly three columns
-    REQUIRE(SDS.shcol == 3);
+    // for this test we need exactly six columns
+    REQUIRE(SDS.shcol == 6);
 
     // Fill in test data on zt_grid.
     for(Int s = 0; s < SDS.shcol; ++s) {
@@ -82,6 +82,32 @@ struct UnitWrap::UnitTest<D>::TestShocEnergyDseFixer {
 
     // Call the fortran implementation
     shoc_energy_dse_fixer(SDS);
+    
+    // Check the results
+    Real temp_sum[shcol];
+    for(Int s = 0; s < shcol; ++s) {
+      temp_sum[s] = 0.0;
+      for(Int n = 0; n < SDS.nlev; ++n) {
+        const auto offset = n + s * SDS.nlev;
+        temp_sum[s] += SDS.host_dse[offset];
+      }
+    }
+    
+    // Verify that as shoctop values get lower that the
+    // summation of temperatures also gets lower.  This is proportionally
+    // to the amount of energy we expect to be removed from a column. 
+    for (Int s = 0; s < shcol-1; ++s) {
+      if (shoctop[s] < shoctop[s+1]){
+        REQUIRE(temp_sum[s] < temp_sum[s+1]);
+      }
+      else if (shoctop[s] > shoctop[s+1]){
+        REQUIRE(temp_sum[s] > temp_sum[s+1]);
+      }
+      else{
+        REQUIRE(temp_sum[s] == temp_sum[s+1]);
+      }
+    }
+    
   }
 
 };
