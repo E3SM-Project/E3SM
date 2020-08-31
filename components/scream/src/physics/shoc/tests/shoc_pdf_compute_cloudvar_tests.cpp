09ead14a25ea48a2b683b95bada40c1db0cc9aba
Peter Andrew Bogenschutz <bogensch@quartz1154.llnl.gov>
2020-08-31 11:57:03 -0700

    finish test for the cloud water variance

--- components/scream/src/physics/shoc/tests/shoc_pdf_compute_cloudvar_tests.cpp
+++ components/scream/src/physics/shoc/tests/shoc_pdf_compute_cloudvar_tests.cpp
@@ -31,10 +31,119 @@ struct UnitWrap::UnitTest<D>::TestShocPdfCompCldVar {
     //  shoc_assumed_pdf_compute_cloud_liquid_variance
 
     // TEST ONE
-
+    // standard deviation test.  Given two tests with identical inputs, 
+    //  but one where s1 and s2 are larger, verify that SGS cloud water
+    //  variance is larger
     
+    // Define gaussian fraction [-]
+    static constexpr Real a = 0.3;
+    // Define s1
+    static constexpr Real s1 = 0.03;
+    // Define cloud liquid water for gaussian one [kg/kg]
+    static constexpr Real ql1 = 0.01;
+    // define cloud fraction for gaussian one [-]
+    static constexpr Real C1 = 0.2;
+    // Define s2
+    static constexpr Real s2 = 0.003;
+    // Define cloud liquid water for gaussian two [kg/kg]
+    static constexpr Real ql2 = 0.001;
+    // define cloud fraction for gaussian one [-]
+    static constexpr Real C2 = 0.1; 
+    // define grid mean shoc_ql [kg/kg]
+    static constexpr Real shoc_ql = 0.005;
+    
+    // Now define standard deviations of the gaussians.
+    //    Here we define two sets for the different tests
+    static constexpr Real std_s1_small = 0.01;
+    static constexpr Real std_s2_small = 0.001;
+    
+    // Define larger quantities for the second part of the test
+    static constexpr Real std_s1_large = 0.02;
+    static constexpr Real std_s2_large = 0.002; 
+    
+    // Verify these are in fact larger
+    REQUIRE(std_s1_large > std_s1_small);
+    REQUIRE(std_s2_large > std_s2_small);
+        
     // Initialize data structure for bridging to F90
     SHOCPDFcompcloudvarData SDS;
+    
+    // load the data for the first part of the test
+    SDS.a = a;
+    SDS.s1 = s1;
+    SDS.ql1 = ql1;
+    SDS.C1 = C1;
+    SDS.s2 = s2;
+    SDS.ql2 = ql2;
+    SDS.C2 = C2;
+    SDS.shoc_ql = shoc_ql;
+    
+    SDS.std_s1 = std_s1_small;
+    SDS.std_s2 = std_s2_small;
+    
+    // Verify inputs
+    REQUIRE(SDS.a > 0);
+    REQUIRE(SDS.a < 1);
+    REQUIRE(SDS.s1 >= 0);
+    REQUIRE(SDS.C1 >= 0);
+    REQUIRE(SDS.ql1 >= 0);
+    REQUIRE(SDS.s2 >= 0);
+    REQUIRE(SDS.C2 >= 0);
+    REQUIRE(SDS.ql2 >= 0);
+    REQUIRE(SDS.shoc_ql >= 0);
+    REQUIRE(SDS.std_s1 >= 0);
+    REQUIRE(SDS.std_s2 >= 0);    
+    
+    // Call the fortran implementation
+    shoc_assumed_pdf_compute_cloud_liquid_variance(SDS);
+    
+    // Save the result for the first part of the test
+    Real shoc_ql2_small = SDS.shoc_ql2;
+    
+    // Now load the data needed for the second part of the test 
+    SDS.std_s1 = std_s1_large;
+    SDS.std_s2 = std_s2_large;
+    
+    // Call the fortran implementation
+    shoc_assumed_pdf_compute_cloud_liquid_variance(SDS);
+    
+    // Verify that the liquid water variance is larger when the standard
+    //   deviations of s are larger
+    
+    REQUIRE(SDS.shoc_ql2 > shoc_ql2_small);
+    
+    // TEST TWO
+    // Grid mean ql test.  Given identical inputs with the exception of 
+    //  that in one set of inputs the grid mean of cloud water is larger, verify
+    //  that shoc_ql2 is small in this case
+    
+    // For inputs we will recycle the inputs from the last test used.
+    //  The exception being the grid mean cloud water 
+    
+    static constexpr Real shoc_ql_small = 0.005;
+    static constexpr Real shoc_ql_large = 0.006;    
+
+    // Verify that values are expected
+    REQUIRE(shoc_ql_large > shoc_ql_small); 
+    
+    // Load input data
+    SDS.shoc_ql = shoc_ql_small;
+    
+    // Call the fortran implementation
+    shoc_assumed_pdf_compute_cloud_liquid_variance(SDS);
+    
+    // save the output for this test
+    shoc_ql2_small = SDS.shoc_ql2;
+    
+    // Load data for second part of this test
+    SDS.shoc_ql = shoc_ql_large;
+    
+    // Call the fortran implementation
+    shoc_assumed_pdf_compute_cloud_liquid_variance(SDS);
+    
+    // Check the result.  The cloud water variance should be SMALLER
+    //  if the grid mean is larger for this case
+    REQUIRE(SDS.shoc_ql2 < shoc_ql2_small);    
 
   }
   
