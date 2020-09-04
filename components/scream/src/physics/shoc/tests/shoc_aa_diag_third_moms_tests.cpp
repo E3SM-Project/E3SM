bad448e3b361f4d894d10bb1aa7acc4ee4ca7f7e
Peter Andrew Bogenschutz <bogensch@quartz1532.llnl.gov>
2020-09-04 16:07:13 -0700

    add tests for third moments

--- components/scream/src/physics/shoc/tests/shoc_aa_diag_third_moms_tests.cpp
+++ components/scream/src/physics/shoc/tests/shoc_aa_diag_third_moms_tests.cpp
@@ -27,17 +27,55 @@ struct UnitWrap::UnitTest<D>::TestAAdiagThirdMoms {
 
     // Tests for the SHOC function:
     //   aa_terms_diag_third_shoc_moment
+    
+    // Run test two times.  One where 0 parameters stay the same
+    //  and the other where 1 parameters vary.  Verify that the aa0
+    //  term remains constant between the two tests.  
   
-    //  Test to be sure that very high values of w3
-    //    are reduced but still the same sign   
-  
+    // omega0 term
+    constexpr static Real omega0 = 7.54e-2;
+    // omega1 term
+    constexpr static Real omega1 = 5.37e-3;
+    // omega2 term
+    constexpr static Real omega2 = 0.54;
+    // x0 term
+    constexpr static Real x0 = -4.31;
+    // y0 term
+    constexpr static Real y0 = 22.72;
+    // x1 term
+    constexpr static Real x1_test1a = 41.05;
+    // y1 term
+    constexpr static Real y1_test1a = 375.69;            
 
     // Initialize data structure for bridging to F90
     SHOCAAdiagthirdmomsData SDS;
 
+    // Load up the data
+    SDS.omega0 = omega0;
+    SDS.x0 = x0;
+    SDS.y0 = y0;
+    SDS.omega1 = omega1;
+    SDS.x1 = x1_test1a;
+    SDS.y1 = y1_test1a;
+    SDS.omega2 = omega2;
+      
+    // Call the fortran implementation    
+    aa_terms_diag_third_shoc_moment(SDS);  
+    
+    // Save the output
+    Real aa0_test1a = SDS.aa0;
+    Real aa1_test1a = SDS.aa1;
+    
+    // Load up data where only the 1 terms have varies
+    SDS.x1 = 0.3*x1_test1a;
+    SDS.y1 = 0.3*y1_test1a;   
     
     // Call the fortran implementation    
-    aa_terms_diag_third_shoc_moment(SDS);    
+    aa_terms_diag_third_shoc_moment(SDS); 
+    
+    // Check the result
+    REQUIRE(SDS.aa0 == aa0_test1a);
+    REQUIRE(SDS.aa1 < aa1_test1a);       
     
   }
 
--- components/scream/src/physics/shoc/tests/shoc_xy_diag_third_moms_tests.cpp
+++ components/scream/src/physics/shoc/tests/shoc_xy_diag_third_moms_tests.cpp
@@ -28,16 +28,57 @@ struct UnitWrap::UnitTest<D>::TestXYdiagThirdMoms {
     // Tests for the SHOC function:
     //   x_y_terms_diag_third_shoc_moment
   
-    //  Test to be sure that very high values of w3
-    //    are reduced but still the same sign   
-  
+    // TEST ONE
+    // Call the function twice, with two different sets of terms.  
+    //  In the second test the fterms should be larger.  Verify 
+    //  that the x0 and y0 terms are the same, but that the
+    //  x1 and y1 terms have increased. 
+    
+    // buoyancy term (isotropy squared * brunt vaisalla frequency)
+    constexpr static Real buoy_sgs2 = -100;
+    // f0 term
+    constexpr static Real f0_test1a = 13000;
+    // f3 term
+    constexpr static Real f1_test1a = 8500;
+    // f4 term
+    constexpr static Real f2_test1a = 30;    
 
     // Initialize data structure for bridging to F90
     SHOCXYdiagthirdmomsData SDS;
-
+    
+    // Load up the data
+    SDS.buoy_sgs2 = buoy_sgs2;
+    SDS.f0 = f0_test1a;
+    SDS.f1 = f1_test1a;
+    SDS.f2 = f2_test1a;    
     
     // Call the fortran implementation    
-    x_y_terms_diag_third_shoc_moment(SDS);    
+    x_y_terms_diag_third_shoc_moment(SDS);
+    
+    // Save test results
+    Real x0_test1a = SDS.x0;
+    Real y0_test1a = SDS.y0;
+    Real x1_test1a = SDS.x1; 
+    Real y1_test1a = SDS.y1;
+    
+    // Now load up data for second part of test
+    // Feed in LARGER values
+    SDS.f0 = 1.2*f0_test1a;
+    SDS.f1 = 1.2*f1_test1a;
+    SDS.f2 = 1.2*f2_test1a;         
+
+    // Call the fortran implementation    
+    x_y_terms_diag_third_shoc_moment(SDS);
+    
+    // Now check the result
+    
+    // x0 and y0 terms should NOT have changed
+    REQUIRE(SDS.x0 == x0_test1a);
+    REQUIRE(SDS.y0 == y0_test1a);  
+    
+    // x1 and y1 terms should have increased
+    REQUIRE(SDS.x1 > x1_test1a);
+    REQUIRE(SDS.y1 > y1_test1a);      
     
   }
 
