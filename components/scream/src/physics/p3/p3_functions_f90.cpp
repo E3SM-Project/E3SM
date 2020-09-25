rfiorella/test-E3SM

P3 Bugfixes: p3_main_part3 ice should be incld and calc_bulkRhoRime should update cell-aves (#595)
Makes p3_main_part3 use in-cloud values for ice phase. Also ensures consistency between in-cloud and cell ave values after calcbulkRhoRime. This PR is the second part of #584 .

Co-authored-by: Peter Caldwell <caldwell19@llnl.gov> 
Co-authored-by: Balwinder Singh <balwindersingh@gmail.com>
Co-authored-by: Xingqiu Yuan <xyuan@anl.gov>

components/scream/src/physics/p3/p3_functions_f90.cpp
@@ -214,7 +214,7 @@ void p3_main_part2_c(
 
 void p3_main_part3_c(
   Int kts, Int kte, Int kbot, Int ktop, Int kdir,
-  Real* exner, Real* cld_frac_l, Real* cld_frac_r,
+  Real* exner, Real* cld_frac_l, Real* cld_frac_r, Real* cld_frac_i,
   Real* rho, Real* inv_rho, Real* rhofaci, Real* qv, Real* th, Real* qc, Real* nc, Real* qr, Real* nr,
   Real* qi, Real* ni, Real* qm, Real* bm, Real* latent_heat_vapor, Real* latent_heat_sublim,
   Real* mu_c, Real* nu, Real* lamc, Real* mu_r, Real* lamr, Real* vap_liq_exchange,
@@ -773,7 +773,7 @@ void p3_main_part2(P3MainPart2Data& d)
 P3MainPart3Data::P3MainPart3Data(
   Int kts_, Int kte_, Int kbot_, Int ktop_, Int kdir_) :
   PhysicsTestData((kte_ - kts_) + 1, {
-    &exner, &cld_frac_l, &cld_frac_r,
+    &exner, &cld_frac_l, &cld_frac_r, &cld_frac_i,
     &rho, &inv_rho, &rhofaci,
     &qv, &th, &qc, &nc, &qr, &nr, &qi, &ni, &qm, &bm, &latent_heat_vapor, &latent_heat_sublim,
     &mu_c, &nu, &lamc, &mu_r,
@@ -787,7 +787,7 @@ void p3_main_part3(P3MainPart3Data& d)
   p3_init();
   p3_main_part3_c(
     d.kts, d.kte, d.kbot, d.ktop, d.kdir,
-    d.exner, d.cld_frac_l, d.cld_frac_r,
+    d.exner, d.cld_frac_l, d.cld_frac_r, d.cld_frac_i,
     d.rho, d.inv_rho, d.rhofaci, d.qv, d.th, d.qc, d.nc, d.qr, d.nr, d.qi, d.ni, d.qm, d.bm, d.latent_heat_vapor, d.latent_heat_sublim,
     d.mu_c, d.nu, d.lamc, d.mu_r, d.lamr, d.vap_liq_exchange,
     d. ze_rain, d.ze_ice, d.diag_vmi, d.diag_effi, d.diag_di, d.rho_qi, d.diag_ze, d.diag_effc);
@@ -2961,7 +2961,7 @@ void p3_main_part2_f(
 
 void p3_main_part3_f(
   Int kts, Int kte, Int kbot, Int ktop, Int kdir,
-  Real* exner, Real* cld_frac_l, Real* cld_frac_r,
+  Real* exner, Real* cld_frac_l, Real* cld_frac_r, Real* cld_frac_i,
   Real* rho, Real* inv_rho, Real* rhofaci, Real* qv, Real* th, Real* qc,
   Real* nc, Real* qr, Real* nr, Real* qi, Real* ni, Real* qm,
   Real* bm, Real* latent_heat_vapor, Real* latent_heat_sublim, Real* mu_c, Real* nu, Real* lamc,
@@ -2992,7 +2992,8 @@ void p3_main_part3_f(
   Kokkos::Array<view_1d, P3MainPart3Data::NUM_ARRAYS> temp_d;
 
   ekat::host_to_device({
-      exner, cld_frac_l, cld_frac_r, rho, inv_rho, rhofaci, qv, th, qc,
+      exner, cld_frac_l, cld_frac_r, cld_frac_i, rho, inv_rho, rhofaci, qv, th, qc,
+
       nc, qr, nr, qi, ni, qm, bm, latent_heat_vapor, latent_heat_sublim, mu_c, nu, lamc, mu_r,
       lamr, vap_liq_exchange, ze_rain, ze_ice, diag_vmi, diag_effi, diag_di,
       rho_qi, diag_ze, diag_effc},
@@ -3002,35 +3003,36 @@ void p3_main_part3_f(
     exner_d              (temp_d[0]),
     cld_frac_l_d         (temp_d[1]),
     cld_frac_r_d         (temp_d[2]),
-    rho_d                (temp_d[3]),
-    inv_rho_d            (temp_d[4]),
-    rhofaci_d            (temp_d[5]),
-    qv_d                 (temp_d[6]),
-    th_d                 (temp_d[7]),
-    qc_d                 (temp_d[8]),
-    nc_d                 (temp_d[9]),
-    qr_d                 (temp_d[10]),
-    nr_d                 (temp_d[11]),
-    qi_d                 (temp_d[12]),
-    ni_d                 (temp_d[13]),
-    qm_d                 (temp_d[14]),
-    bm_d                 (temp_d[15]),
-    latent_heat_vapor_d  (temp_d[16]),
-    latent_heat_sublim_d (temp_d[17]),
-    mu_c_d               (temp_d[18]),
-    nu_d                 (temp_d[19]),
-    lamc_d               (temp_d[20]),
-    mu_r_d               (temp_d[21]),
-    lamr_d               (temp_d[22]),
-    vap_liq_exchange_d   (temp_d[23]),
-    ze_rain_d            (temp_d[24]),
-    ze_ice_d             (temp_d[25]),
-    diag_vmi_d           (temp_d[26]),
-    diag_effi_d          (temp_d[27]),
-    diag_di_d            (temp_d[28]),
-    rho_qi_d             (temp_d[29]),
-    diag_ze_d            (temp_d[30]),
-    diag_effc_d          (temp_d[31]);
+    cld_frac_i_d         (temp_d[3]),
+    rho_d                (temp_d[4]),
+    inv_rho_d            (temp_d[5]),
+    rhofaci_d            (temp_d[6]),
+    qv_d                 (temp_d[7]),
+    th_d                 (temp_d[8]),
+    qc_d                 (temp_d[9]),
+    nc_d                 (temp_d[10]),
+    qr_d                 (temp_d[11]),
+    nr_d                 (temp_d[12]),
+    qi_d                 (temp_d[13]),
+    ni_d                 (temp_d[14]),
+    qm_d                 (temp_d[15]),
+    bm_d                 (temp_d[16]),
+    latent_heat_vapor_d  (temp_d[17]),
+    latent_heat_sublim_d (temp_d[18]),
+    mu_c_d               (temp_d[19]),
+    nu_d                 (temp_d[20]),
+    lamc_d               (temp_d[21]),
+    mu_r_d               (temp_d[22]),
+    lamr_d               (temp_d[23]),
+    vap_liq_exchange_d   (temp_d[24]),
+    ze_rain_d            (temp_d[25]),
+    ze_ice_d             (temp_d[26]),
+    diag_vmi_d           (temp_d[27]),
+    diag_effi_d          (temp_d[28]),
+    diag_di_d            (temp_d[29]),
+    rho_qi_d             (temp_d[30]),
+    diag_ze_d            (temp_d[31]),
+    diag_effc_d          (temp_d[32]);
 
   // Call core function from kernel
   const auto dnu         = P3GlobalForFortran::dnu();
@@ -3039,7 +3041,7 @@ void p3_main_part3_f(
   Kokkos::parallel_for(policy, KOKKOS_LAMBDA(const MemberType& team) {
 
     P3F::p3_main_part3(team, nk_pack, dnu, itab,
-                       exner_d, cld_frac_l_d, cld_frac_r_d, rho_d, inv_rho_d,
+                       exner_d, cld_frac_l_d, cld_frac_r_d, cld_frac_i_d, rho_d, inv_rho_d,
                        rhofaci_d, qv_d, th_d, qc_d, nc_d, qr_d, nr_d,
                        qi_d, ni_d, qm_d, bm_d, latent_heat_vapor_d,
                        latent_heat_sublim_d, mu_c_d, nu_d, lamc_d, mu_r_d, lamr_d,

components/scream/src/physics/p3/p3_main_impl.hpp
@@ -434,6 +434,9 @@ void Functions<S,D>
         nr_incld(k).set(qi_gt_small, max(nr_incld(k), nsmall));
 
         const auto rhop = calc_bulk_rho_rime(qi_incld(k), qm_incld(k), bm_incld(k), qi_gt_small);
+        qi(k).set(qi_gt_small, qi_incld(k)*cld_frac_i(k) );
+        qm(k).set(qi_gt_small, qm_incld(k)*cld_frac_i(k) );
+        bm(k).set(qi_gt_small, bm_incld(k)*cld_frac_i(k) );
 
         TableIce table_ice;
         lookup_ice(qi_incld(k), ni_incld(k), qm_incld(k), rhop, table_ice, qi_gt_small);
@@ -712,6 +715,7 @@ void Functions<S,D>
   const uview_1d<const Spack>& exner,
   const uview_1d<const Spack>& cld_frac_l,
   const uview_1d<const Spack>& cld_frac_r,
+  const uview_1d<const Spack>& cld_frac_i,
   const uview_1d<Spack>& rho,
   const uview_1d<Spack>& inv_rho,
   const uview_1d<Spack>& rhofaci,
@@ -802,12 +806,19 @@ void Functions<S,D>
 
       // impose lower limits to prevent taking log of # < 0
       ni(k) = max(ni(k), nsmall);
-      nr(k)    = max(nr(k), nsmall);
 
-      const auto rhop = calc_bulk_rho_rime(qi(k), qm(k), bm(k), qi_gt_small);
+      auto qi_incld = qi(k)/cld_frac_i(k);
+      auto ni_incld = ni(k)/cld_frac_i(k);
+      auto qm_incld = qm(k)/cld_frac_i(k);
+      auto bm_incld = bm(k)/cld_frac_i(k);
+
+      const auto rhop = calc_bulk_rho_rime(qi_incld, qm_incld, bm_incld, qi_gt_small);
+      qi(k).set(qi_gt_small, qi_incld*cld_frac_i(k) );
+      qm(k).set(qi_gt_small, qm_incld*cld_frac_i(k) );
+      bm(k).set(qi_gt_small, bm_incld*cld_frac_i(k) );
 
       TableIce table_ice;
-      lookup_ice(qi(k), ni(k), qm(k), rhop, table_ice, qi_gt_small);
+      lookup_ice(qi_incld, ni_incld, qm_incld, rhop, table_ice, qi_gt_small);
 
       table_val_qi_fallspd.set(qi_gt_small, apply_table_ice(1,  itab, table_ice, qi_gt_small));
       table_val_ice_eff_rad.set(qi_gt_small, apply_table_ice(5,  itab, table_ice, qi_gt_small));
@@ -819,8 +830,8 @@ void Functions<S,D>
 
       // impose mean ice size bounds (i.e. apply lambda limiters)
       // note that the Nmax and Nmin are normalized and thus need to be multiplied by existing N
-      ni(k).set(qi_gt_small, min(ni(k), table_val_ni_lammax * ni(k)));
-      ni(k).set(qi_gt_small, max(ni(k), table_val_ni_lammin * ni(k)));
+      ni_incld.set(qi_gt_small, min(ni_incld, table_val_ni_lammax * ni_incld));
+      ni_incld.set(qi_gt_small, max(ni_incld, table_val_ni_lammin * ni_incld));
 
       // --this should already be done in s/r 'calc_bulkRhoRime'
       const auto qm_small = qm(k) < qsmall && qi_gt_small;
@@ -834,15 +845,18 @@ void Functions<S,D>
       rho_qi(k).set(qi_gt_small, table_val_ice_bulk_dens);
 
       // note factor of air density below is to convert from m^6/kg to m^6/m^3
-      ze_ice(k).set(qi_gt_small, ze_ice(k) + sp(0.1892)*table_val_ice_reflectivity*ni(k)*rho(k)); // sum contribution from each ice category (note: 0.1892 = 0.176/0.93);
+      ze_ice(k).set(qi_gt_small, ze_ice(k) + sp(0.1892)*table_val_ice_reflectivity*ni_incld*rho(k));   // sum contribution from each ice category (note: 0.1892 = 0.176/0.93);
       ze_ice(k).set(qi_gt_small, max(ze_ice(k), sp(1.e-22)));
 
-      qv(k)     .set(qi_small, qv(k) + qi(k));
-      th(k)     .set(qi_small, th(k) - exner(k)*qi(k)*latent_heat_sublim(k)*inv_cp);
-      qi(k)  .set(qi_small, 0);
-      ni(k)  .set(qi_small, 0);
-      qm(k)  .set(qi_small, 0);
-      bm(k)  .set(qi_small, 0);
+      //above formula for ze only makes sense for in-cloud vals, but users expect cell-ave output.
+      ze_ice(k).set(qi_gt_small, ze_ice(k)*cld_frac_i(k));
+
+      qv(k).set(qi_small, qv(k) + qi(k));
+      th(k).set(qi_small, th(k) - exner(k)*qi(k)*latent_heat_sublim(k)*inv_cp);
+      qi(k).set(qi_small, 0);
+      ni(k).set(qi_small, 0);
+      qm(k).set(qi_small, 0);
+      bm(k).set(qi_small, 0);
       diag_di(k).set(qi_small, 0);
     }
 
@@ -1083,7 +1097,7 @@ void Functions<S,D>
     // and compute diagnostic fields for output
     //
     p3_main_part3(
-      team, nk_pack, dnu, itab, oexner, ocld_frac_l, ocld_frac_r,
+      team, nk_pack, dnu, itab, oexner, ocld_frac_l, ocld_frac_r, ocld_frac_i,
       rho, inv_rho, rhofaci, oqv, oth, oqc, onc, oqr, onr, oqi, oni,
       oqm, obm, olatent_heat_vapor, olatent_heat_sublim, omu_c, nu, olamc, mu_r, lamr,
       ovap_liq_exchange, ze_rain, ze_ice, diag_vmi, odiag_effi, diag_di,

E3SM-Project/E3SM

Co-authored-by: Peter Caldwell <caldwell19@llnl.gov>

components/scream/src/physics/p3/p3_functions_f90.cpp
@@ -214,7 +214,7 @@ void p3_main_part2_c(
 
 void p3_main_part3_c(
   Int kts, Int kte, Int kbot, Int ktop, Int kdir,
-  Real* exner, Real* cld_frac_l, Real* cld_frac_r,
+  Real* exner, Real* cld_frac_l, Real* cld_frac_r, Real* cld_frac_i,
   Real* rho, Real* inv_rho, Real* rhofaci, Real* qv, Real* th_atm, Real* qc, Real* nc, Real* qr, Real* nr,
   Real* qi, Real* ni, Real* qm, Real* bm, Real* latent_heat_vapor, Real* latent_heat_sublim,
   Real* mu_c, Real* nu, Real* lamc, Real* mu_r, Real* lamr, Real* vap_liq_exchange,
@@ -773,7 +773,7 @@ void p3_main_part2(P3MainPart2Data& d)
 P3MainPart3Data::P3MainPart3Data(
   Int kts_, Int kte_, Int kbot_, Int ktop_, Int kdir_) :
   PhysicsTestData((kte_ - kts_) + 1, {
-    &exner, &cld_frac_l, &cld_frac_r,
+    &exner, &cld_frac_l, &cld_frac_r, &cld_frac_i,
     &rho, &inv_rho, &rhofaci,
     &qv, &th_atm, &qc, &nc, &qr, &nr, &qi, &ni, &qm, &bm, &latent_heat_vapor, &latent_heat_sublim,
     &mu_c, &nu, &lamc, &mu_r,
@@ -787,7 +787,7 @@ void p3_main_part3(P3MainPart3Data& d)
   p3_init();
   p3_main_part3_c(
     d.kts, d.kte, d.kbot, d.ktop, d.kdir,
-    d.exner, d.cld_frac_l, d.cld_frac_r,
+    d.exner, d.cld_frac_l, d.cld_frac_r, d.cld_frac_i, 
     d.rho, d.inv_rho, d.rhofaci, d.qv, d.th_atm, d.qc, d.nc, d.qr, d.nr, d.qi, d.ni, d.qm, d.bm, d.latent_heat_vapor, d.latent_heat_sublim,
     d.mu_c, d.nu, d.lamc, d.mu_r, d.lamr, d.vap_liq_exchange,
     d. ze_rain, d.ze_ice, d.diag_vm_qi, d.diag_eff_rad_qi, d.diag_diam_qi, d.rho_qi, d.diag_equiv_reflectivity, d.diag_eff_rad_qc);
@@ -2964,7 +2964,7 @@ void p3_main_part2_f(
 
 void p3_main_part3_f(
   Int kts, Int kte, Int kbot, Int ktop, Int kdir,
-  Real* exner, Real* cld_frac_l, Real* cld_frac_r,
+  Real* exner, Real* cld_frac_l, Real* cld_frac_r, Real* cld_frac_i,
   Real* rho, Real* inv_rho, Real* rhofaci, Real* qv, Real* th_atm, Real* qc,
   Real* nc, Real* qr, Real* nr, Real* qi, Real* ni, Real* qm,
   Real* bm, Real* latent_heat_vapor, Real* latent_heat_sublim, Real* mu_c, Real* nu, Real* lamc,
@@ -2995,45 +2995,46 @@ void p3_main_part3_f(
   Kokkos::Array<view_1d, P3MainPart3Data::NUM_ARRAYS> temp_d;
 
   ekat::host_to_device({
-      exner, cld_frac_l, cld_frac_r, rho, inv_rho, rhofaci, qv, th_atm, qc,
+      exner, cld_frac_l, cld_frac_r, cld_frac_i, rho, inv_rho, rhofaci, qv, th_atm, qc,
       nc, qr, nr, qi, ni, qm, bm, latent_heat_vapor, latent_heat_sublim, mu_c, nu, lamc, mu_r,
       lamr, vap_liq_exchange, ze_rain, ze_ice, diag_vm_qi, diag_eff_rad_qi, diag_diam_qi,
       rho_qi, diag_equiv_reflectivity, diag_eff_rad_qc},
     nk, temp_d);
 
   view_1d
-    exner_d              (temp_d[0]),
-    cld_frac_l_d         (temp_d[1]),
-    cld_frac_r_d         (temp_d[2]),
-    rho_d                (temp_d[3]),
-    inv_rho_d            (temp_d[4]),
-    rhofaci_d            (temp_d[5]),
-    qv_d                 (temp_d[6]),
-    th_atm_d             (temp_d[7]),
-    qc_d                 (temp_d[8]),
-    nc_d                 (temp_d[9]),
-    qr_d                 (temp_d[10]),
-    nr_d                 (temp_d[11]),
-    qi_d                 (temp_d[12]),
-    ni_d                 (temp_d[13]),
-    qm_d                 (temp_d[14]),
-    bm_d                 (temp_d[15]),
-    latent_heat_vapor_d  (temp_d[16]),
-    latent_heat_sublim_d (temp_d[17]),
-    mu_c_d               (temp_d[18]),
-    nu_d                 (temp_d[19]),
-    lamc_d               (temp_d[20]),
-    mu_r_d               (temp_d[21]),
-    lamr_d               (temp_d[22]),
-    vap_liq_exchange_d   (temp_d[23]),
-    ze_rain_d            (temp_d[24]),
-    ze_ice_d             (temp_d[25]),
-    diag_vm_qi_d           (temp_d[26]),
-    diag_eff_rad_qi_d          (temp_d[27]),
-    diag_diam_qi_d            (temp_d[28]),
-    rho_qi_d             (temp_d[29]),
-    diag_equiv_reflectivity_d            (temp_d[30]),
-    diag_eff_rad_qc_d          (temp_d[31]);
+    exner_d                    (temp_d[0]),
+    cld_frac_l_d               (temp_d[1]),
+    cld_frac_r_d               (temp_d[2]),
+    cld_frac_i_d               (temp_d[3]),
+    rho_d                      (temp_d[4]),
+    inv_rho_d                  (temp_d[5]),
+    rhofaci_d                  (temp_d[6]),
+    qv_d                       (temp_d[7]),
+    th_atm_d                   (temp_d[8]),
+    qc_d                       (temp_d[9]),
+    nc_d                       (temp_d[10]),
+    qr_d                       (temp_d[11]),
+    nr_d                       (temp_d[12]),
+    qi_d                       (temp_d[13]),
+    ni_d                       (temp_d[14]),
+    qm_d                       (temp_d[15]),
+    bm_d                       (temp_d[16]),
+    latent_heat_vapor_d        (temp_d[17]),
+    latent_heat_sublim_d       (temp_d[18]),
+    mu_c_d                     (temp_d[19]),
+    nu_d                       (temp_d[20]),
+    lamc_d                     (temp_d[21]),
+    mu_r_d                     (temp_d[22]),
+    lamr_d                     (temp_d[23]),
+    vap_liq_exchange_d         (temp_d[24]),
+    ze_rain_d                  (temp_d[25]),
+    ze_ice_d                   (temp_d[26]),
+    diag_vm_qi_d               (temp_d[27]),
+    diag_eff_rad_qi_d          (temp_d[28]),
+    diag_diam_qi_d             (temp_d[29]),
+    rho_qi_d                   (temp_d[30]),
+    diag_equiv_reflectivity_d  (temp_d[31]),
+    diag_eff_rad_qc_d          (temp_d[32]);
 
   // Call core function from kernel
   const auto dnu            = P3GlobalForFortran::dnu();
@@ -3042,7 +3043,7 @@ void p3_main_part3_f(
   Kokkos::parallel_for(policy, KOKKOS_LAMBDA(const MemberType& team) {
 
     P3F::p3_main_part3(team, nk_pack, dnu, ice_table_vals,
-                       exner_d, cld_frac_l_d, cld_frac_r_d, rho_d, inv_rho_d,
+                       exner_d, cld_frac_l_d, cld_frac_r_d, cld_frac_i_d, rho_d, inv_rho_d,
                        rhofaci_d, qv_d, th_atm_d, qc_d, nc_d, qr_d, nr_d,
                        qi_d, ni_d, qm_d, bm_d, latent_heat_vapor_d,
                        latent_heat_sublim_d, mu_c_d, nu_d, lamc_d, mu_r_d, lamr_d,

components/scream/src/physics/p3/p3_main_impl.hpp
@@ -434,6 +434,9 @@ void Functions<S,D>
         nr_incld(k).set(qi_gt_small, max(nr_incld(k), nsmall));
 
         const auto rhop = calc_bulk_rho_rime(qi_incld(k), qm_incld(k), bm_incld(k), qi_gt_small);
+        qi(k).set(qi_gt_small, qi_incld(k)*cld_frac_i(k) );
+        qm(k).set(qi_gt_small, qm_incld(k)*cld_frac_i(k) );
+        bm(k).set(qi_gt_small, bm_incld(k)*cld_frac_i(k) );
 
         TableIce table_ice;
         lookup_ice(qi_incld(k), ni_incld(k), qm_incld(k), rhop, table_ice, qi_gt_small);
@@ -712,6 +715,7 @@ void Functions<S,D>
   const uview_1d<const Spack>& exner,
   const uview_1d<const Spack>& cld_frac_l,
   const uview_1d<const Spack>& cld_frac_r,
+  const uview_1d<const Spack>& cld_frac_i,
   const uview_1d<Spack>& rho,
   const uview_1d<Spack>& inv_rho,
   const uview_1d<Spack>& rhofaci,
@@ -802,12 +806,19 @@ void Functions<S,D>
 
       // impose lower limits to prevent taking log of # < 0
       ni(k) = max(ni(k), nsmall);
-      nr(k)    = max(nr(k), nsmall);
 
-      const auto rhop = calc_bulk_rho_rime(qi(k), qm(k), bm(k), qi_gt_small);
+      auto qi_incld = qi(k)/cld_frac_i(k);
+      auto ni_incld = ni(k)/cld_frac_i(k);
+      auto qm_incld = qm(k)/cld_frac_i(k);
+      auto bm_incld = bm(k)/cld_frac_i(k);
+
+      const auto rhop = calc_bulk_rho_rime(qi_incld, qm_incld, bm_incld, qi_gt_small);
+      qi(k).set(qi_gt_small, qi_incld*cld_frac_i(k) );
+      qm(k).set(qi_gt_small, qm_incld*cld_frac_i(k) );
+      bm(k).set(qi_gt_small, bm_incld*cld_frac_i(k) );
 
       TableIce table_ice;
-      lookup_ice(qi(k), ni(k), qm(k), rhop, table_ice, qi_gt_small);
+      lookup_ice(qi_incld, ni_incld, qm_incld, rhop, table_ice, qi_gt_small);
 
       table_val_qi_fallspd.set(qi_gt_small, apply_table_ice(1,  ice_table_vals, table_ice, qi_gt_small));
       table_val_ice_eff_rad.set(qi_gt_small, apply_table_ice(5,  ice_table_vals, table_ice, qi_gt_small));
@@ -819,8 +830,8 @@ void Functions<S,D>
 
       // impose mean ice size bounds (i.e. apply lambda limiters)
       // note that the Nmax and Nmin are normalized and thus need to be multiplied by existing N
-      ni(k).set(qi_gt_small, min(ni(k), table_val_ni_lammax * ni(k)));
-      ni(k).set(qi_gt_small, max(ni(k), table_val_ni_lammin * ni(k)));
+      ni_incld.set(qi_gt_small, min(ni_incld, table_val_ni_lammax * ni_incld));
+      ni_incld.set(qi_gt_small, max(ni_incld, table_val_ni_lammin * ni_incld));
 
       // --this should already be done in s/r 'calc_bulkRhoRime'
       const auto qm_small = qm(k) < qsmall && qi_gt_small;
@@ -834,15 +845,18 @@ void Functions<S,D>
       rho_qi(k).set(qi_gt_small, table_val_ice_bulk_dens);
 
       // note factor of air density below is to convert from m^6/kg to m^6/m^3
-      ze_ice(k).set(qi_gt_small, ze_ice(k) + sp(0.1892)*table_val_ice_reflectivity*ni(k)*rho(k)); // sum contribution from each ice category (note: 0.1892 = 0.176/0.93);
+      ze_ice(k).set(qi_gt_small, ze_ice(k) + sp(0.1892)*table_val_ice_reflectivity*ni_incld*rho(k));   // sum contribution from each ice category (note: 0.1892 = 0.176/0.93);
       ze_ice(k).set(qi_gt_small, max(ze_ice(k), sp(1.e-22)));
 
-      qv(k)     .set(qi_small, qv(k) + qi(k));
-      th_atm(k)     .set(qi_small, th_atm(k) - exner(k)*qi(k)*latent_heat_sublim(k)*inv_cp);
-      qi(k)  .set(qi_small, 0);
-      ni(k)  .set(qi_small, 0);
-      qm(k)  .set(qi_small, 0);
-      bm(k)  .set(qi_small, 0);
+      //above formula for ze only makes sense for in-cloud vals, but users expect cell-ave output.
+      ze_ice(k).set(qi_gt_small, ze_ice(k)*cld_frac_i(k));
+
+      qv(k).set(qi_small, qv(k) + qi(k));
+      th_atm(k).set(qi_small, th_atm(k) - exner(k)*qi(k)*latent_heat_sublim(k)*inv_cp);
+      qi(k).set(qi_small, 0);
+      ni(k).set(qi_small, 0);
+      qm(k).set(qi_small, 0);
+      bm(k).set(qi_small, 0);
       diag_diam_qi(k).set(qi_small, 0);
     }
 
@@ -1083,7 +1097,7 @@ void Functions<S,D>
     // and compute diagnostic fields for output
     //
     p3_main_part3(
-      team, nk_pack, dnu, ice_table_vals, oexner, ocld_frac_l, ocld_frac_r,
+      team, nk_pack, dnu, ice_table_vals, oexner, ocld_frac_l, ocld_frac_r, ocld_frac_i, 
       rho, inv_rho, rhofaci, oqv, oth, oqc, onc, oqr, onr, oqi, oni,
       oqm, obm, olatent_heat_vapor, olatent_heat_sublim, omu_c, nu, olamc, mu_r, lamr,
       ovap_liq_exchange, ze_rain, ze_ice, diag_vm_qi, odiag_eff_rad_qi, diag_diam_qi,