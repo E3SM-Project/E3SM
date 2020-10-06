f17b5abc9b284f7a7e256b582539866ba0c68181
tcclevenger <tcleven@clemson.edu>
2020-10-06 15:54:27 -0400

    remove extra constant

--- components/scream/src/physics/shoc/shoc_constants.hpp
+++ components/scream/src/physics/shoc/shoc_constants.hpp
@@ -15,7 +15,6 @@ struct Constants
       static constexpr Scalar maxtke         = 50.0;    // Maximum TKE [m2/s2]
       static constexpr Scalar minlen         = 20.0;    // Lower limit for mixing length [m]
       static constexpr Scalar maxlen         = 20000.0; // Upper limit for mixing length [m]
-      static constexpr Scalar minlen         = 20.0;    // Lower limit for mixing length [m]
       static constexpr Scalar maxiso         = 20000.0; // Upper limit for isotropy time scale [s]
       static constexpr Scalar length_fac     = 0.5;     // Mixing length scaling parameter
       static constexpr Scalar c_diag_3rd_mom = 7.0;     // Coefficient for diag third moment parameters
