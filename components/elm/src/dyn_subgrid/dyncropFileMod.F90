fcfabed2a494cad178cd703018269cfa3ce129c3
hydrotian <tian.zhou@pnnl.gov>
2020-10-22 10:15:44 -0700

    activating dynamic crop fraction when prognostic crop model is turned off

--- components/elm/src/dyn_subgrid/dyncropFileMod.F90
+++ components/elm/src/dyn_subgrid/dyncropFileMod.F90
@@ -12,7 +12,7 @@ module dyncropFileMod
   use decompMod             , only : bounds_type, BOUNDS_LEVEL_PROC
   use dynFileMod            , only : dyn_file_type
   use dynVarTimeUninterpMod , only : dyn_var_time_uninterp_type
-  use clm_varctl            , only : iulog
+  use clm_varctl            , only : iulog, use_crop
   use clm_varcon            , only : grlnd, namec
   use abortutils            , only : endrun
   use spmdMod               , only : masterproc, mpicom
@@ -188,7 +188,9 @@ contains
           end if
           
           col_pp%wtlunit(c) = wtcft_cur(g,m)
-          crop_inst%fertnitro_patch(p) = fertcft_cur(g,m)
+          if (use_crop) then                  
+            crop_inst%fertnitro_patch(p) = fertcft_cur(g,m)
+          end if   
           col_set(c) = .true.
        end if
     end do
