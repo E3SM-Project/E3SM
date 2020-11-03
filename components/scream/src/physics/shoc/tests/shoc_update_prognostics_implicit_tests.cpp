d1038fe3332aba425278050090ab9e67e8cfb47c
Peter Andrew Bogenschutz <bogensch@syrah144.llnl.gov>
2020-11-03 11:27:14 -0800

    test for tracers

--- components/scream/src/physics/shoc/tests/shoc_update_prognostics_implicit_tests.cpp
+++ components/scream/src/physics/shoc/tests/shoc_update_prognostics_implicit_tests.cpp
@@ -72,9 +72,9 @@ struct UnitWrap::UnitTest<D>::TestUpdatePrognosticsImplicit {
     static constexpr Real wind_bounds = 5; // [m/s]
 
     // establish spurious threshold bounds
-    static const auto qwspur_thresh = Approx(0.0).margin(1e-10);
-    static const auto thlspur_thresh = Approx(0.0).margin(1e-10);
-    static const auto trcspur_thresh = Approx(0.0).margin(1e-15);
+    static const auto qwspur_thresh = Approx(0.0).margin(1e-4);
+    static const auto thlspur_thresh = Approx(0.0).margin(1e-4);
+    static const auto trcspur_thresh = Approx(0.0).margin(1e-4);
 
     // Input for tracer (no units)
     Real tracer_in[shcol][nlev][num_tracer];
@@ -329,7 +329,7 @@ struct UnitWrap::UnitTest<D>::TestUpdatePrognosticsImplicit {
         spurious = (trc_int_a[s][t] - trc_int_b[s][t])/dtime
                    - rho_zi_srf*SDS.wtracer_sfc[t_offset];
         // Spurious source should be sufficiently small
-//        REQUIRE(spurious == trcspur_thresh);
+        REQUIRE(spurious == trcspur_thresh);
       }
 
     }
