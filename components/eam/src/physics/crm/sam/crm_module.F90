d13db34a59007418c50cef98560a35c32e59fccd
Matt Norman <normanmr@ornl.gov>
2020-11-04 12:32:28 -0500

    C++ code now gives correct results in standalone on the CPU

--- components/eam/src/physics/crm/sam/crm_module.F90
+++ components/eam/src/physics/crm/sam/crm_module.F90
@@ -1,8 +1,4 @@
 
-#define DEBUG(x) write(*,fmt='(A,ES24.16)') 'x: ', real( sum(x) , 8 )
-#define DEBUG_LOG(x) write(*,fmt='(A,ES24.16)') 'x: ', real( count(x) , 8 )
-#define abort() stop
-
 module crm_module
   use openacc_utils, only: prefetch
   use perf_mod
@@ -406,14 +402,6 @@ subroutine crm(lchnk, ncrms, dt_gl, plev,       &
       enddo
     enddo
   endif
-  
-  DEBUG(crm_clear_rh);
-  DEBUG(crm_clear_rh_cnt);
-  DEBUG(u);
-  DEBUG(v);
-  DEBUG(w);
-  DEBUG(tabs);
-  abort();
 
   ! Populate microphysics array from crm_state
   !$acc parallel loop collapse(4) async(asyncid)
@@ -442,7 +430,7 @@ subroutine crm(lchnk, ncrms, dt_gl, plev,       &
       enddo
     enddo
   enddo
-
+  
 #ifdef m2005
   do icrm = 1 , ncrms
     do k=1, nzm
@@ -579,7 +567,7 @@ subroutine crm(lchnk, ncrms, dt_gl, plev,       &
     ! estimate roughness length assuming logarithmic profile of velocity near the surface:
     ustar(icrm) = sqrt(crm_input%tau00(icrm)/rho(icrm,1))
     z0(icrm) = z0_est(z(icrm,1),bflx(icrm),wnd(icrm),ustar(icrm))
-    z0(icrm) = max(real(0.00001,crm_rknd),min(real(1.,crm_rknd),z0(icrm)))
+    z0(icrm) = max(real(0.00001D0,crm_rknd),min(real(1.,crm_rknd),z0(icrm)))
     crm_output%subcycle_factor(icrm) = 0.
     crm_output%prectend (icrm)=colprec (icrm)
     crm_output%precstend(icrm)=colprecs(icrm)
