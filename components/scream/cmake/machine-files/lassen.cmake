4603365f21387499b7113d4342377d9e4cbad113
Aaron Donahue <donahue.aaron@gmail.com>
2020-11-09 13:06:10 -0800

    Make changes needed to get tests working on LC machines.
    This commit adjusts the cmake flags to extend the generality in how
    mpirun is used in testing.  The immediate impact is that the two LC
    machine syrah and quartz will be able to run tests using srun, since
    mpirun is not available.
    
    There are also adjustments to fix lassen machine configurations.

--- components/scream/cmake/machine-files/lassen.cmake
+++ components/scream/cmake/machine-files/lassen.cmake
@@ -2,5 +2,6 @@
 set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
 include (${EKAT_MACH_FILES_PATH}/kokkos/nvidia-v100.cmake)
 include (${EKAT_MACH_FILES_PATH}/kokkos/cuda.cmake)
-
-set(CMAKE_CXX_FLAGS "-std=c++11" CACHE STRING "")
+set(NetCDF_C_PATHS /usr/gdata/climdat/lassen_builds/libs/netcdf/netcdf-c/netcdf-c-install/ CACHE STRING "")
+set(NetCDF_Fortran_PATHS /usr/gdata/climdat/lassen_builds/libs/netcdf/netcdf-f/netcdf-f-install/ CACHE STRING "")
+set(CMAKE_C_COMPILER /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-gcc-7.3.1/bin/mpicc CACHE STRING "")
