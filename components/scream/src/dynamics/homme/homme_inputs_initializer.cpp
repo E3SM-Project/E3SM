a1f5bc9376d19efd8cefd90b253cf03ec4fd0b18
Luca Bertagna <lbertag@sandia.gov>
2020-11-10 15:50:05 -0700

    SCREAM: some fixes to address PR review
    - typos
    - fix call to CreateDynamicsLib for homme-p3 test (not compiled, yet)
    - removed commented code

--- components/scream/src/dynamics/homme/homme_inputs_initializer.cpp
+++ components/scream/src/dynamics/homme/homme_inputs_initializer.cpp
@@ -1,13 +1,6 @@
 #include "dynamics/homme/homme_inputs_initializer.hpp"
 
 #include "dynamics/homme//interface/scream_homme_interface.hpp"
-#include "dynamics/homme/homme_dimensions.hpp"
-
-#include "Hommexx_Session.hpp"
-#include "ElementsState.hpp"
-#include "Tracers.hpp"
-#include "ElementsForcing.hpp"
-#include "Types.hpp"
 
 namespace scream {
 
@@ -19,29 +12,16 @@ void HommeInputsInitializer::initialize_fields () {
   // ctor and initialize method. The AD attempts to init the
   // inputs *after* all atm process have been inited.
   // So here's what we do: if the user asks to init inputs,
-  // we simply go ahead and to it, by calling this method inside
+  // we simply go ahead and do it, by calling this method inside
   // HommeDynamics' initialize call. When later the AD calls this
   // initializer, we simply skip everything, since we already inited
   // all the fields.
   // Note: if some other atm proc is trying to init any of the fields
-  //       inited by this class, we the AD should throw an error anyways.
+  //       inited by this class, the AD should throw an error anyways.
 
   if (!m_fields_inited) {
-    // Sanity check. This class should init *all* homme's inputs:
-    // FM, FVTheta, FPHI, FQ, v, dp, w, phi, vtheta, q, qdp
-    // auto count = m_names.count("v")
-    //            + m_names.count("w")
-    //            + m_names.count("dp")
-    //            + m_names.count("vtheta")
-    //            + m_names.count("phi")
-    //            + m_names.count("qdp")
-    //            + m_names.count("q")
-    //            + m_names.count("fm")
-    //            + m_names.count("fq")
-    //            + m_names.count("fphi")
-    //            + m_names.count("ft")
-    //            + m_names.count("fvtheta");
-    // EKAT_REQUIRE_MSG (count==12, "Error! Homme should init *all* input, not just a few.\n");
+    // TODO: add checks to ensure we're in charge of initialization for *all*
+    //       the dynamics inputs (otherwise there may be some inconsistencies).
 
     // Compute initial conditions in f90, then copy all data to the
     // C++ elements views. Notice that this does *not( reallocate
