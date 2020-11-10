fe7f7728ec1349dbb1c013f833b30a1c6af479a6
James Foucar <jgfouca@sandia.gov>
2020-11-10 13:50:39 -0800

    Add a working cori.cmake file with correct name

---
+++ components/scream/cmake/machine-files/cori.cmake
@@ -0,0 +1,5 @@
+set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
+
+# Load knl arch and openmp backend for kokkos
+include (${EKAT_MACH_FILES_PATH}/kokkos/intel-knl.cmake)
+include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
