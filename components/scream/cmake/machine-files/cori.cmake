8b8ea30f46522620318592b2f30329f4c28cd861
James Foucar <jgfouca@sandia.gov>
2020-11-11 15:22:45 -0700

    Add comment explaining cori hack

--- components/scream/cmake/machine-files/cori.cmake
+++ components/scream/cmake/machine-files/cori.cmake
@@ -4,4 +4,6 @@ set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/c
 include (${EKAT_MACH_FILES_PATH}/kokkos/intel-knl.cmake)
 include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
 
+# Fixes some openmpi link problems we observed on cori. This hack is
+# not necessary if CRAYPE_LINK_TYPE=dynamic is in the environment.
 set(SCREAM_CORI_HACK True CACHE BOOL "")
