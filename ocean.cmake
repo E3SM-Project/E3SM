6702bc1a61963bfa9c3d08ac06b4988d7b3ef94d
Mark Petersen <mpetersen@lanl.gov>
2020-11-18 18:42:02 -0700

    Remove mpas_ocn_diagnostics_routines.F

--- ocean.cmake
+++ ocean.cmake
@@ -49,7 +49,6 @@ list(APPEND RAW_SOURCES
   core_ocean/shared/mpas_ocn_gm.F
   core_ocean/shared/mpas_ocn_diagnostics.F
   core_ocean/shared/mpas_ocn_diagnostics_variables.F
-  core_ocean/shared/mpas_ocn_diagnostics_routines.F
   core_ocean/shared/mpas_ocn_mesh.F
   core_ocean/shared/mpas_ocn_thick_ale.F
   core_ocean/shared/mpas_ocn_equation_of_state.F
