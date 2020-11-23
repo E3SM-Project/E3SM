6ba1a8c05dbf08d6058d4c7ea250e897533faf90
James Foucar <jgfouca@sandia.gov>
2020-11-23 16:08:59 -0700

    gen-boiler: newline after Data struct

--- components/scream/scripts/gen_boiler.py
+++ components/scream/scripts/gen_boiler.py
@@ -1507,6 +1507,7 @@ class GenBoiler(object):
         <BLANKLINE>
           PTD_STD_DEF(FakeSubData, 8, shcol, nlev, nlevi, ntracers, gag, bab1, bab2, val);
         };
+        <BLANKLINE>
         """
         arg_data         = force_arg_data if force_arg_data else self._get_arg_data(phys, sub)
         struct_members   = "\n  ".join(gen_struct_members(arg_data))
@@ -1518,7 +1519,8 @@ class GenBoiler(object):
         result = \
 """struct {struct_name}{inheritance} {{
   {struct_members}{api}
-}};""".format(struct_name=struct_name, inheritance=inheritance, struct_members=struct_members, api=api)
+}};
+""".format(struct_name=struct_name, inheritance=inheritance, struct_members=struct_members, api=api)
         return result
 
     ###########################################################################
