567a877a65a9274b1519b2b5debbee875acf4883
Balwinder <balwindersingh@gmail.com>
2020-11-25 02:27:43 -0500

    remove Spacks from constant declarations

--- components/scream/src/physics/shoc/shoc_isotropic_ts_impl.hpp
+++ components/scream/src/physics/shoc/shoc_isotropic_ts_impl.hpp
@@ -26,14 +26,14 @@ void Functions<S,D>
 {
 
   //constants from physics/share
-  static constexpr  auto ggr = C::gravit;
+  static constexpr  Scalar ggr = C::gravit;
 
-  //Declate constants as Spacks to use ekat's min and max functions
-  static const Spack lambda_low  (sp(0.001));
-  static const Spack lambda_high (sp(0.04));
-  static const Spack lambda_slope(sp(0.65));
-  static const Spack brunt_low   (sp(0.02));
-  static const Spack maxiso      (20000); // Return to isotropic timescale [s]
+  //Declare constants
+  static constexpr Scalar lambda_low   = sp(0.001);
+  static constexpr Scalar lambda_high  = sp(0.04);
+  static constexpr Scalar lambda_slope = sp(0.65);
+  static constexpr Scalar brunt_low    = sp(0.02);
+  static constexpr Scalar maxiso       = 20000; // Return to isotropic timescale [s]
 
   const Int nlev_pack = ekat::npack<Spack>(nlev);
 
@@ -43,14 +43,14 @@ void Functions<S,D>
       const Spack tscale = 2*tke(k)/a_diss(k);
 
       // define a damping term "lambda" based on column stability
-      Spack lambda = lambda_low + ((brunt_int/ggr)-brunt_low)*lambda_slope;
-      lambda       = ekat::max(lambda_low,ekat::min(lambda_high,lambda));
+      Spack lambda(lambda_low + ((brunt_int/ggr)-brunt_low)*lambda_slope);
+      lambda = ekat::max(Spack(lambda_low),ekat::min(Spack(lambda_high),lambda));
 
       const Spack buoy_sgs_save = brunt(k);
       lambda.set(buoy_sgs_save <=0, 0); //set lambda to zero where buoy_sgs_save <=0
 
       // Compute the return to isotropic timescale
-      isotropy(k)= ekat::min(maxiso,tscale/(1+lambda*buoy_sgs_save*ekat::square(tscale)));
+      isotropy(k)= ekat::min(Spack(maxiso),tscale/(1+lambda*buoy_sgs_save*ekat::square(tscale)));
 
     });
 }
