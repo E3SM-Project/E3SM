5d8152c824942132f2341d7911bdede2e0a9bc6e
Balwinder Singh <balwindersingh@gmail.com>
2020-11-30 14:09:34 -0800

    adds bfb squares

--- components/eam/src/physics/cam/shoc.F90
+++ components/eam/src/physics/cam/shoc.F90
@@ -2993,7 +2993,7 @@ subroutine compute_shr_prod(nlevi, nlev, shcol, dz_zi, u_wind, v_wind, sterm)
         ! calculate vertical gradient of u&v wind
         u_grad = grid_dz*(u_wind(i,km1)-u_wind(i,k))
         v_grad = grid_dz*(v_wind(i,km1)-v_wind(i,k))
-        sterm(i,k) = u_grad**2+v_grad**2
+        sterm(i,k) = bfb_square(u_grad)+bfb_square(v_grad)
      enddo
   enddo
 
