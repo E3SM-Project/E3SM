cc76b4692b1dc093fb24d522b6224bf38cb1a894
Balwinder Singh <balwindersingh@gmail.com>
2020-11-30 13:52:20 -0800

    added bfb unit test

--- components/scream/src/physics/shoc/shoc_compute_shr_prod_impl.hpp
+++ components/scream/src/physics/shoc/shoc_compute_shr_prod_impl.hpp
@@ -26,27 +26,26 @@ void Functions<S,D>
 {
   const Int nlev_pack = ekat::npack<Spack>(nlev);
 
-  const auto sclr_uwind = scalarize(u_wind);//
-  const auto sclr_vwind = scalarize(v_wind);
+  //scalarize so that we can use shift to compute the differece  ( x(k-1) - x )
+  const auto sclr_uwind = scalarize(u_wind); //for u_wind
+  const auto sclr_vwind = scalarize(v_wind); //for v_wind
 
   //compute shear production term
   Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nlev_pack), [&] (const Int& k) {
 
+    const Spack grid_dz = 1/dz_zi(k);
+
+    // calculate vertical gradient of u&v wind
     auto range_pack1 = ekat::range<IntSmallPack>(k*Spack::n);
     auto range_pack2 = range_pack1;
     range_pack2.set(range_pack1 < 1, 1); // don't want the shift to go below zero. we mask out that result anyway
-
-    const Spack grid_dz = 1/dz_zi(k);
-    // calculate vertical gradient of u&v wind
     Spack u_up_grid, u_grid, v_up_grid, v_grid;
-    ekat::index_and_shift<-1>(sclr_uwind, range_pack2, u_grid, u_up_grid);
-    ekat::index_and_shift<-1>(sclr_vwind, range_pack2, v_grid, v_up_grid);
-    //const Spcak u_grad = grid_dz*(u_wind(km1)-u_wind(k));
-    //const Spack v_grad = grid_dz*(v_wind(km1)-v_wind(k));
-    //const Spack u_grad = grid_dz*(u_wind(k)-u_wind(k)+1);
-    //const Spack v_grad = grid_dz*(v_wind(k)-v_wind(k)+1);
+    ekat::index_and_shift<-1>(sclr_uwind, range_pack2, u_grid, u_up_grid); //for u_wind
+    ekat::index_and_shift<-1>(sclr_vwind, range_pack2, v_grid, v_up_grid); //for v_wind
     const Spack u_grad(range_pack1 > 0 && range_pack1 < nlev, grid_dz*(u_up_grid - u_grid));
     const Spack v_grad(range_pack1 > 0 && range_pack1 < nlev, grid_dz*(v_up_grid - v_grid));
+
+    //compute shear production
     sterm(k)           = u_grad*u_grad+v_grad*v_grad;
   });
   /*
@@ -59,7 +58,6 @@ void Functions<S,D>
   const Int nlevi_pack = ekat::npack<Spack>(nlevi);
   const Int last_pack_entry = (nlevi%Spack::n == 0 ? Spack::n-1 : nlevi%Spack::n-1);
   sterm(nlevi_pack-1)[last_pack_entry] = 0;
-
 }
 
 } // namespace shoc
