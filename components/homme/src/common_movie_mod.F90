770ef7745a540a94b4817123e21436853145f0fd
Christopher Eldred <celdred@sandia.gov>
2020-12-11 15:46:56 -0700

    does this work?

--- components/homme/src/common_movie_mod.F90
+++ components/homme/src/common_movie_mod.F90
@@ -100,8 +100,8 @@ module common_movie_mod
                                                                1,2,5,0,0,0, & ! geo
                                                                1,2,5,0,0,0, & !omega
                                                                1,2,5,0,0,0, & !dp3d
-                                                               1,0,0,0,0,0, &  ! lat
-                                                               1,0,0,0,0,0, &  ! lon
+                                                               1,0,0,0,0,0, &  ! lat (y for planar)
+                                                               1,0,0,0,0,0, &  ! lon (x for planar)
                                                                2,0,0,0,0,0, &  ! lev
                                                                3,0,0,0,0,0, &  ! ilev
                                                                2,0,0,0,0,0, &  !hyam
@@ -135,28 +135,30 @@ module common_movie_mod
                                                  'time        ',&
                                                  'nsubelements'/)  
 #else
-  integer, parameter :: varcnt = 10
+  integer, parameter :: varcnt = 12
   integer, parameter :: maxdims=4
   character*(*),parameter::dimnames(maxdims)=(/'ncol ','nlev ','nelem','time '/)  
   integer, parameter :: vardims(maxdims,varcnt) =  reshape( (/ 1,4,0,0,  &  !ps
                                                                1,2,4,0,  &  !geop
                                                                1,2,4,0,  &  !u
                                                                1,2,4,0,  &  !v
-                                                               1,0,0,0,  &  !lon
-                                                               1,0,0,0,  &  !lat
+                                                               1,0,0,0,  &  !lon (x for planar)
+                                                               1,0,0,0,  &  !lat (y for planar)
                                                                4,0,0,0,  &  ! time
                                                                1,2,4,0,  &  ! zeta
                                                                1,2,4,0,  &  ! div
+                                                               1,2,4,0,  &  ! eta = absolute vorticity = zeta + coriolis
+                                                               1,2,4,0,  &  ! pv
                                                                1,0,0,0/),&  ! area
                                                                shape=(/maxdims,varcnt/))
   character*(*),parameter::varnames(varcnt)=(/'ps       ','geop     ','u        ','v        ',&
                                               'lon      ','lat      ','time     ',&
-                                              'zeta     ','div      ','area     '/)
+                                              'zeta     ','div      ','eta      ','pv       ','area     '/)
   integer, parameter :: vartype(varcnt)=(/nf_double,nf_double,nf_double,nf_double,nf_double,&
-       nf_double,nf_double,nf_double,nf_double,nf_double/)
+         nf_double,nf_double,nf_double,nf_double,nf_double,nf_double,nf_double/)
   logical, parameter :: varrequired(varcnt)=(/.false.,.false.,.false.,.false.,&
                                               .true.,.true.,.true.,&
-                                              .false.,.false.,.true./)
+                                              .false.,.false.,.false.,.false.,.true./)
 #endif
 
   ! end of analysis_nl namelist variables
