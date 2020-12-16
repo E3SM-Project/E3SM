d1d2690ee7beff7f5c3040d1ec5670a74c7b185d
Ryan Forsyth <forsyth2@llnl.gov>
2020-12-15 17:24:47 -0800

    Image Comparison Tests

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,17 @@
 # Run from top level directory of repository.
 
-# Copy over test data [takes about two minutes]
-# TODO: How will an automated CI tool access the E3SM data server? We could set up a public key for the CI server.
+# Uncomment if you want to remove the results directory from the previous test run.
+#rm -rf tests/system/all_sets_results_test/
+
+# Copy over test data and images [takes about two minutes]
+# TODO: CI tool will access data from the web
 # Change <username>
 scp -r <username>@blues.lcrc.anl.gov:/lcrc/group/e3sm/public_html/e3sm_diags_test_data/unit_test_data tests/unit_test_data
+scp -r <username>@blues.lcrc.anl.gov:/lcrc/group/e3sm/public_html/e3sm_diags_test_data/unit_test_images tests/unit_test_images
 
 # Use "&&" so the test will fail if either python call fails.
 cd tests/system && python -m unittest && cd .. && python -m unittest && cd ..
 
-# Uncomment if you want to delete the test data copied from blues after running the tests.
+# Uncomment if you want to delete the test data/images copied from Blues after running the tests.
 #rm -rf tests/unit_test_data
+#rm -rf tests/unit_test_images
