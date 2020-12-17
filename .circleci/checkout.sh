bcb54a3ad81923eeb29205ef582b780fff7e5991
Lukasz Lacinski <lukasz@uchicago.edu>
2020-12-17 14:54:00 -0600

    Add Circle CI test

---
+++ .circleci/checkout.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Replace all ssh URLs to submodules with HTTP URLs
+sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
+git submodule update --init
+cd components/mpas-source
+sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
+git submodule update --init --recursive
