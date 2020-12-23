1fbe059ee3623b7e85755829be6860300b7626d3
Qi Tang <tang30@llnl.gov>
2020-12-22 18:45:30 -0800

    revert mo_exp_sol.F90 to only handle E90 decay

--- components/eam/src/chemistry/mozart/mo_exp_sol.F90
+++ components/eam/src/chemistry/mozart/mo_exp_sol.F90
@@ -1,16 +1,9 @@
 
 module mo_exp_sol
 
-  use shr_kind_mod, only : r8 => shr_kind_r8
-
   private
   public :: exp_sol
   public :: exp_sol_inti
-
-  save
-
-  real(r8), parameter :: r30days = 3.858e-7_r8 ! e-folding decay rate of 30 days
-  integer :: uci1_ndx
 
 contains
 
@@ -20,9 +13,6 @@ contains
     use chem_mods,   only : clscnt1, clsmap
     use ppgrid,      only : pver
     use cam_history, only : addfld, add_default
-    use mo_chem_utls, only : get_rxt_ndx
-    use cam_logfile,  only : iulog
-    use spmd_utils,   only : masterproc
 
     implicit none
 
@@ -35,18 +25,6 @@ contains
        call addfld( trim(solsym(j))//'_CHML', (/ 'lev' /), 'I', '/cm3/s', 'chemical loss rate' )
 
     enddo
-
-    !-----------------------------------------------------------------------
-    !    	... Reaction rate indices
-    !-----------------------------------------------------------------------
-    uci1_ndx     = get_rxt_ndx( 'uci1' )
-
-    if (masterproc) then
-       write(iulog,*) ' '
-       write(iulog,*) 'exp_sol_inti: diagnostics uci rates indices'
-       write(iulog,'(10i5)') uci1_ndx
-    end if
-
   end subroutine exp_sol_inti
 
 
@@ -114,35 +92,11 @@ contains
              end do
           end do
        else
-          !
-          ! tropospheric solver for non-E90 species
-          !
           do i = 1,ncol
              do k = ltrop(i)+1,pver
                 base_sol(i,k,l)  = base_sol(i,k,l) + delt * (prod(i,k,m) + ind_prd(i,k,m) - loss(i,k,m))
              end do
           end do
-       end if
-       !
-       ! stratospheric decay for chemUCI
-       !
-       if (uci1_ndx > 0) then
-          if (trim(solsym(l))=='CH2O' .or. trim(solsym(l))=='CH3O2' &
-            .or. trim(solsym(l))=='CH3OOH' .or. trim(solsym(l))=='PAN' &
-            .or. trim(solsym(l))=='CO' .or. trim(solsym(l))=='C2H6' &
-            .or. trim(solsym(l))=='C3H8' .or. trim(solsym(l))=='C2H4' &
-            .or. trim(solsym(l))=='ROHO2'.or.trim(solsym(l))=='CH3COCH3' &
-            .or. trim(solsym(l))=='C2H5O2'.or.trim(solsym(l))=='C2H5OOH' &
-            .or. trim(solsym(l))=='CH3CHO'.or.trim(solsym(l))=='CH3CO3' &
-            .or. trim(solsym(l))=='ISOP' .or. trim(solsym(l))=='ISOPO2' &
-            .or. trim(solsym(l))=='MVKMACR'.or.trim(solsym(l))=='MVKO2' &
-            ) then
-             do i = 1,ncol
-                do k = 1,ltrop(i)
-                   base_sol(i,k,l)  = base_sol(i,k,l) * (1 - delt * r30days)
-                end do
-             end do
-          end if
        end if
 
        wrk(:,:) = (prod(:,:,m) + ind_prd(:,:,m))*xhnm
