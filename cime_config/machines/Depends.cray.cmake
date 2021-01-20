806361e87be397678be26a7af29e6e209299efa8
Azamat Mametjanov <azamat@mcs.anl.gov>
2021-01-20 00:19:55 +0000

    Update Cray compiler to cce/10.0.3 on Theta

--- cime_config/machines/Depends.cray.cmake
+++ cime_config/machines/Depends.cray.cmake
@@ -1,1 +1,1 @@
-list(APPEND NOOPT_FILES cice/src/mpi/ice_boundary.F90 cice/src/serial/ice_boundary.F90 cice/src/source/ice_domain.F90 cam/src/physics/cam/gw_common.F90)
+list(APPEND NOOPT_FILES cice/src/mpi/ice_boundary.F90 cice/src/serial/ice_boundary.F90 cice/src/source/ice_domain.F90 eam/src/physics/cam/gw_common.F90)
