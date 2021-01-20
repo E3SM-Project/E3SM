c3ea5ef3245854d5c0eea4118fb30c65ca8163db
Adrian K. Turner <akt@lanl.gov>
2021-01-20 17:19:08 -0600

    Improvements to square test case

--- testing_and_setup/seaice/testcases/square/run_testcase.py
+++ testing_and_setup/seaice/testcases/square/run_testcase.py
@@ -5,104 +5,66 @@ except ImportError:
     print("Module f90nml needed and not available")
     raise
 
-# quad
-print("Quad")
-os.system("rm grid.nc")
-os.system("ln -s square_mesh_80x80_culled.nc grid.nc")
-os.system("rm ic.nc")
-os.system("ln -s ic_quad.nc ic.nc")
+operatorMethods = ["wachspress","pwl","weak"]
 
-# Wachspress
-print("  Wachspress")
-nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"variational",
-                                "config_variational_basis":"wachspress"}}
+gridTypes = ["hex","quad"]
+#gridTypes = ["quad"]
 
-f90nml.patch("namelist.seaice.square", nmlPatch, "namelist.seaice.wachspress")
+grids = {"hex" :["0082x0094",
+                 "0164x0188",
+                 "0328x0376",
+                 "0656x0752"],
+         "quad":["0080x0080",
+                 "0160x0160",
+                 "0320x0320",
+                 "0640x0640"]}
+#grids = {"hex" :["0082x0094"],
+#         "quad":["0080x0080"]}
 
-os.system("rm -rf namelist.seaice streams.seaice output_quad_wachspress")
-os.system("ln -s namelist.seaice.wachspress namelist.seaice")
-os.system("ln -s streams.seaice.square streams.seaice")
 
-os.system("../../../../seaice_model")
+#subcycleNumbers = [120,240,480,960,1920,3840,7680,15360,30720]
+subcycleNumbers = [120,240,480,960,1920,3840,7680]
+#subcycleNumbers = [120,240]
 
-os.system("mv output output_quad_wachspress")
+for gridType in gridTypes:
 
-# PWL
-print("  PWL")
-nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"variational",
-                                "config_variational_basis":"pwl"}}
+    print("Grid type: ", gridType)
 
-f90nml.patch("namelist.seaice.square", nmlPatch, "namelist.seaice.pwl")
+    for operatorMethod in operatorMethods:
 
-os.system("rm -rf namelist.seaice streams.seaice output_quad_pwl")
-os.system("ln -s namelist.seaice.pwl namelist.seaice")
-os.system("ln -s streams.seaice.square streams.seaice")
+        print("  Operator Method: ", operatorMethod)
 
-os.system("../../../../seaice_model")
+        for grid in grids[gridType]:
 
-os.system("mv output output_quad_pwl")
+            print("    Grid: ", grid)
 
-# Weak
-print("  Weak")
-nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"weak"}}
+            os.system("rm grid.nc")
+            os.system("rm ic.nc")
+            os.system("ln -s grid_%s_%s.nc grid.nc" %(gridType,grid))
+            os.system("ln -s ic_%s_%s.nc ic.nc" %(gridType,grid))
 
-f90nml.patch("namelist.seaice.square", nmlPatch, "namelist.seaice.weak")
+            for subcycleNumber in subcycleNumbers:
 
-os.system("rm -rf namelist.seaice streams.seaice output_quad_weak")
-os.system("ln -s namelist.seaice.weak namelist.seaice")
-os.system("ln -s streams.seaice.square streams.seaice")
+                print("      Subcycle number: ", subcycleNumber)
 
-os.system("../../../../seaice_model")
+                if (operatorMethod == "wachspress"):
+                    nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"variational",
+                                                    "config_variational_basis":"wachspress",
+                                                    "config_elastic_subcycle_number":subcycleNumber}}
+                elif (operatorMethod == "pwl"):
+                    nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"variational",
+                                                    "config_variational_basis":"pwl",
+                                                    "config_elastic_subcycle_number":subcycleNumber}}
+                elif (operatorMethod == "weak"):
+                    nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"weak",
+                                                    "config_elastic_subcycle_number":subcycleNumber}}
 
-os.system("mv output output_quad_weak")
+                f90nml.patch("namelist.seaice.square", nmlPatch, "namelist.seaice.%s.%i" %(operatorMethod, subcycleNumber))
 
-# hex
-print("Hex")
-os.system("rm grid.nc")
-os.system("ln -s square_mesh_82x94_culled.nc grid.nc")
-os.system("rm ic.nc")
-os.system("ln -s ic_hex.nc ic.nc")
+                os.system("rm -rf namelist.seaice streams.seaice output_%s_%s_%s_%i" %(gridType, operatorMethod, grid, subcycleNumber))
+                os.system("ln -s namelist.seaice.%s.%i namelist.seaice" %(operatorMethod, subcycleNumber))
+                os.system("ln -s streams.seaice.square streams.seaice")
 
-# Wachspress
-print("  Wachspress")
-nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"variational",
-                                "config_variational_basis":"wachspress"}}
+                os.system("../../../../seaice_model")
 
-f90nml.patch("namelist.seaice.square", nmlPatch, "namelist.seaice.wachspress")
-
-os.system("rm -rf namelist.seaice streams.seaice output_hex_wachspress")
-os.system("ln -s namelist.seaice.wachspress namelist.seaice")
-os.system("ln -s streams.seaice.square streams.seaice")
-
-os.system("../../../../seaice_model")
-
-os.system("mv output output_hex_wachspress")
-
-# PWL
-print("  PWL")
-nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"variational",
-                                "config_variational_basis":"pwl"}}
-
-f90nml.patch("namelist.seaice.square", nmlPatch, "namelist.seaice.pwl")
-
-os.system("rm -rf namelist.seaice streams.seaice output_hex_pwl")
-os.system("ln -s namelist.seaice.pwl namelist.seaice")
-os.system("ln -s streams.seaice.square streams.seaice")
-
-os.system("../../../../seaice_model")
-
-os.system("mv output output_hex_pwl")
-
-# Weak
-print("  Weak")
-nmlPatch = {"velocity_solver": {"config_stress_divergence_scheme":"weak"}}
-
-f90nml.patch("namelist.seaice.square", nmlPatch, "namelist.seaice.weak")
-
-os.system("rm -rf namelist.seaice streams.seaice output_hex_weak")
-os.system("ln -s namelist.seaice.weak namelist.seaice")
-os.system("ln -s streams.seaice.square streams.seaice")
-
-os.system("../../../../seaice_model")
-
-os.system("mv output output_hex_weak")
+                os.system("mv output output_%s_%s_%s_%i" %(gridType, operatorMethod, grid, subcycleNumber))
