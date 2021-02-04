009ed223b19ec7586c7249cb6a3c7c7746b41505
Luca Bertagna <lbertag@sandia.gov>
2021-02-04 15:58:08 -0700

    SCREAM: fix surface coupling to work with subviewed fields

--- components/scream/src/control/surface_coupling.cpp
+++ components/scream/src/control/surface_coupling.cpp
@@ -1,5 +1,7 @@
 #include "control/surface_coupling.hpp"
 
+#include "share/field/field_utils.hpp"
+
 namespace scream {
 namespace control {
 
@@ -74,39 +76,8 @@ register_import(const std::string& fname,
   // Set cpl index
   info.cpl_idx = cpl_idx;
 
-  const auto& layout = field.get_header().get_identifier().get_layout();
-  const auto& alloc_prop = field.get_header().get_alloc_properties();
-  const auto& tags = layout.tags();
-
-  using namespace ShortFieldTagsNames;
-
-  switch (layout.rank()) {
-    case 1:
-      EKAT_REQUIRE_MSG(vecComp==-1, "Error! Vector component specified, but field '" + fname + "' is a 2d scalar.\n");
-      info.col_stride = 1;
-      info.col_offset = 0;
-      break;
-    case 2:
-      EKAT_REQUIRE_MSG(tags[1]==VL || tags[1]==VAR || tags[1]==CMP,
-                         "Error! Unexpected tag '" + e2str(tags[1]) + "' for second dimension of export field '" + fname + "'.\n");
-      EKAT_REQUIRE_MSG(tags.back()!=VL || vecComp==-1, "Error! Vector component specified, but field '" + fname + "' is a 3d scalar.\n");
-
-      info.col_stride = alloc_prop.get_last_extent();
-      info.col_offset = vecComp;
-      break;
-    case 3:
-      EKAT_REQUIRE_MSG(tags[1]==VAR || tags[1]==CMP,
-                         "Error! Unexpected tag '" + e2str(tags[1]) + "' for second dimension of export field '" + fname + "'.\n");
-      EKAT_REQUIRE_MSG(tags[2]==VL,
-                         "Error! Unexpected tag '" + e2str(tags[2]) + "' for third dimension of export field '" + fname + "'.\n");
-      EKAT_REQUIRE_MSG(vecComp>=0, "Error! Vector component not specified for 3d vector field '" + fname + "/.\n");
-
-      info.col_stride = layout.dim(1)*alloc_prop.get_last_extent();
-      info.col_offset = tags[1]==VL ? 0 : vecComp;
-      break;
-    default:
-      EKAT_ERROR_MSG("Error! Unsupported field rank for import field '" + fname + "'.\n");
-  }
+  // Get column offset and stride
+  get_col_info (field.get_header_ptr(), vecComp, info.col_offset, info.col_stride);
 
   // Store the identifier of this field, for debug purposes
   m_imports_fids.insert(field.get_header().get_identifier());
@@ -156,41 +127,8 @@ register_export (const std::string& fname,
   // Set cpl index
   info.cpl_idx = cpl_idx;
 
-  // To figure out col_stride and col_offset, we need to inspect the field layout,
-  // as well as its allocation properties (to handle the case od padding)
-  const auto& layout = field.get_header().get_identifier().get_layout();
-  const auto& alloc_prop = field.get_header().get_alloc_properties();
-  const auto& tags = layout.tags();
-
-  using namespace ShortFieldTagsNames;
-
-  switch (layout.rank()) {
-    case 1:
-      EKAT_REQUIRE_MSG(vecComp==-1, "Error! Vector component specified, but field '" + fname + "' is a 2d scalar.\n");
-      info.col_stride = 1;
-      info.col_offset = 0;
-      break;
-    case 2:
-      EKAT_REQUIRE_MSG(tags[1]==VL || tags[1]==VAR || tags[1]==CMP,
-                         "Error! Unexpected tag '" + e2str(tags[1]) + "' for second dimension of import field '" + fname + "'.\n");
-      EKAT_REQUIRE_MSG(tags.back()!=VL || vecComp==-1, "Error! Vector component specified, but field '" + fname + "' is a 3d scalar.\n");
-
-      info.col_stride = alloc_prop.get_last_extent();
-      info.col_offset = tags[1]==VL ? 0 : vecComp;
-      break;
-    case 3:
-      EKAT_REQUIRE_MSG(tags[1]==VAR || tags[1]==CMP,
-                         "Error! Unexpected tag '" + e2str(tags[1]) + "' for second dimension of import field '" + fname + "'.\n");
-      EKAT_REQUIRE_MSG(tags[2]==VL,
-                         "Error! Unexpected tag '" + e2str(tags[2]) + "' for third dimension of import field '" + fname + "'.\n");
-      EKAT_REQUIRE_MSG(vecComp>=0, "Error! Vector component not specified for 3d vector field '" + fname + "/.\n");
-
-      info.col_stride = layout.dim(1)*alloc_prop.get_last_extent();
-      info.col_offset = vecComp;
-      break;
-    default:
-      EKAT_ERROR_MSG("Error! Unsupported field rank for import field '" + fname + "'.\n");
-  }
+  // Get column offset and stride
+  get_col_info (field.get_header_ptr(), vecComp, info.col_offset, info.col_stride);
 
   // Store the identifier of this field, for debug purposes
   m_exports_fids.insert(field.get_header().get_identifier());
@@ -311,6 +249,89 @@ void SurfaceCoupling::do_export ()
 
   // Deep copy fields from device to cpl host array
   Kokkos::deep_copy(m_cpl_exports_view_h,m_cpl_exports_view_d);
+}
+
+void SurfaceCoupling::
+get_col_info(const std::shared_ptr<const FieldHeader>& fh,
+             int vecComp, int& col_offset, int& col_stride) const
+{
+  // Each field is seen as a certain number of columns worth of data (CWD)
+  // at every column point. E.g., a field with layout (ncols,nlevs) is 1 CWD,
+  // while a field with layout (ncols,3,nlevs) is 3 CWD.
+  // The col_offset encodes the offset of this colum *within a single mesh
+  // point icol". So for a vector layout (ncols,3,nlevs), to import/export
+  // the icomp-th component of the field, we'd have col_offset=icomp. For scalar
+  // layouts, the offset is always 0 (only 1 CWD per mesh point).
+  // HOWEVER: if this field was a subview of another field, things are a bit
+  // more involved. Say the field F1 (ncols,3,nlevs) is a subview of a larger
+  // field F2 with layout (ncols,nvars,3,nlevs), corresponding to ivar=2.
+  // Then, F2 has 3*nvars CWD per mesh point. Since the underlying data ptr
+  // is the same, to get the correct col_offset, we need to figure out the
+  // offset inside F2. In this case, it would be col_offset = ivar*3+icomp.
+
+  using namespace ShortFieldTagsNames;
+
+  // Get this field layout info
+  const auto& layout = fh->get_identifier().get_layout();
+  const auto& dims = layout.dims();
+
+  auto lt = get_layout_type(layout.tags());
+  const bool scalar = lt==LayoutType::Scalar2D || lt==LayoutType::Scalar3D;
+  const bool vector = lt==LayoutType::Vector2D || lt==LayoutType::Vector3D;
+  EKAT_REQUIRE_MSG(scalar || vector,
+      "Error! Support for tensor fields not yet implemented in surface coupling.\n");
+  EKAT_REQUIRE_MSG( (vecComp<0) == scalar,
+      "Error! You can and must specify a vector component only for vector fields.\n");
+
+  vecComp = std::max(0,vecComp);
+
+  // Product of dimensions of field, without counting space dims, that is,
+  // the product of dimensions of the "analytical" field.
+  int dims_prod = 1;
+
+  // Compute initial offset based on this field rank and requested component
+  col_offset = vecComp;
+  if (vector) {
+    dims_prod = dims[1];
+  }
+
+  // If rank>1, there always is some stride
+  col_stride = 1;
+  if (layout.rank()>1) {
+    // Note: use alloc prop's last extent, cause it includes padding (if any);
+    col_stride = fh->get_alloc_properties().get_last_extent();
+    if (layout.rank()>2) {
+      col_stride *= dims[1];
+    }
+  }
+
+  // Recursively go through parent field, adding offset depending
+  // on the location of the slice.
+  // Note: as of now, I don't expect there to be "a grandparent" for this field,
+  //       so we could avoid the while loop. However, the logic is so simple, that
+  //       I may as well add it, and be safe down the road if it happens.
+  std::shared_ptr<const FieldHeader> me = fh;
+  std::shared_ptr<const FieldHeader> p  = me->get_parent().lock();
+  while (p!=nullptr) {
+    const auto& idx = me->get_alloc_properties().get_subview_idx();
+
+    // Recall: idx = (idim,k) = (dimension where slice happened, index along said dimension).
+    // Field class only allows idim=0,1. But we should never be in the case of idim=0, here.
+    // If we have idim=0, it means that the parent field did not have CL has tag[0]...
+    EKAT_REQUIRE_MSG(idx.first==1, "Error! Bizarre scenario discovered. Contact developers.\n");
+
+    // Update the offset
+    col_offset += idx.second*dims_prod;
+
+    // Update stride.
+    col_stride *= p->get_identifier().get_layout().dim(1);
+
+    // Update trail_dims, in case there's another parent.
+    dims_prod *= p->get_identifier().get_layout().dim(1);
+
+    me = p;
+    p = me->get_parent().lock();
+  }
 }
 
 } // namespace control
