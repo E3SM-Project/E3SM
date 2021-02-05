03ad31423e62e1a44a52f48ed92e65c459f6630e
Ryan Forsyth <forsyth2@llnl.gov>
2021-02-05 08:13:09 -0800

    Add diagnostics sets to tutorial

---
+++ tests/complete_run_v2_3_0.py
@@ -0,0 +1,41 @@
+import unittest
+import os
+from PIL import Image, ImageChops
+from system.test_diags import compare_images
+import sys
+sys.path.insert(0, '../examples')
+from tutorials.tutorial_v2_3_0_all_sets_E3SM_machines import run_lcrc
+
+# Due to the large amount of data required to run, this test will be run manually on Anvil
+# (rather than as part of the CI tests).
+
+
+class TestCompleteRun(unittest.TestCase):
+    def test_complete_run(self):
+        actual_images_dir = run_lcrc('.')
+
+        # The expected_images_file lists all 475 images we expect to compare.
+        # It was generated with the following steps:
+        # cd /lcrc/group/e3sm/public_html/e3sm_diags_test_data/unit_test_complete_run/v2_3_0_all_sets_2021_02_09
+        # find . -type f -name '*.png' > ../expected_images_v2_3_0_all_sets_2021_02_09.txt
+        expected_images_file = '/lcrc/group/e3sm/public_html/e3sm_diags_test_data/unit_test_complete_run/expected_images_v2_3_0_all_sets_2021_02_09.txt'
+        expected_images_dir = '/lcrc/group/e3sm/public_html/e3sm_diags_test_data/unit_test_complete_run/v2_3_0_all_sets_2021_02_09'
+
+        mismatched_images = []
+
+        with open(expected_images_file) as f:
+            for line in f:
+                image_name = line.strip('./').strip('\n')
+                path_to_actual_png = os.path.join(actual_images_dir, image_name)
+                path_to_expected_png = os.path.join(expected_images_dir, image_name)
+
+                compare_images(self, mismatched_images, image_name, path_to_actual_png, path_to_expected_png)
+
+        self.assertEqual(mismatched_images, [])
+
+
+if __name__ == '__main__':
+    # Run the following first:
+    # srun --pty --nodes=1 --time=01:00:00 /bin/bash
+    # source /lcrc/soft/climate/e3sm-unified/load_latest_e3sm_unified.sh
+    unittest.main()
