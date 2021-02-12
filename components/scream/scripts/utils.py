d644eba86fbd9612cd5da60c6b9a6a26fb422357
James Foucar <jgfouca@sandia.gov>
2021-02-11 17:27:10 -0700

    Add ability to present whitespace and comments in case's yaml

--- components/scream/scripts/utils.py
+++ components/scream/scripts/utils.py
@@ -501,13 +501,18 @@ def multilevel_dict_change(ml_dict, keys, value):
 ###############################################################################
     """
     Change an existing value in a multi-level dict (expects all keys to already
-    be in dict).
+    be in dict). Returns True if dict was modified
     """
     num_keys = len(keys)
+    modified = False
     for idx, key in enumerate(keys):
         expect(key in ml_dict, "No key {} in {}-th level of ml_dict {}".format(key, idx, ml_dict))
         if idx == num_keys-1:
             # Last key
-            ml_dict[key] = value
+            if ml_dict[key] != value:
+                ml_dict[key] = value
+                modified = True
         else:
             ml_dict = ml_dict[key]
+
+    return modified
