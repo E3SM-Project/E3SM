6e19d69955d02e951011843f22b7844feaefccd3
Juno.Hsu <junoh@uci.edu>
2021-02-26 03:35:43 -0800

    No need to prescribe ubc for NO,NO2,HNO3 if Linoz_v3 is active
    	modified:   mo_gas_phase_chemdr.F90

--- components/eam/src/chemistry/mozart/mo_gas_phase_chemdr.F90
+++ components/eam/src/chemistry/mozart/mo_gas_phase_chemdr.F90
@@ -870,7 +870,7 @@ contains
     !-----------------------------------------------------------------------      
     !         ... Set upper boundary mmr values
     !-----------------------------------------------------------------------      
-    call set_fstrat_vals( vmr, pmid, pint, troplev, calday, ncol,lchnk )
+    if(.not. linoz_v3) call set_fstrat_vals( vmr, pmid, pint, troplev, calday, ncol,lchnk )
 
     !-----------------------------------------------------------------------      
     !         ... Set fixed lower boundary mmr values
