845f6ec470f4ef959825290f36de647856e17b78
Charles D Koven <cdkoven@lbl.gov>
2021-04-12 10:17:21 -0700

    added elm-side changes to allow snow to occlude lai in fates

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -135,6 +135,7 @@ module ELMFatesInterfaceMod
    use EDBtranMod            , only : btran_ed, &
                                       get_active_suction_layers
    use EDCanopyStructureMod  , only : canopy_summarization, update_hlm_dynamics
+   use EDCanopyStructureMod  , only : UpdateFatesAvgSnowDepth
    use FatesPlantRespPhotosynthMod, only : FatesPlantRespPhotosynthDrive
    use EDAccumulateFluxesMod , only : AccumulateFluxes_ED
    use FatesSoilBGCFluxMod   , only : UnPackNutrientAquisitionBCs
@@ -821,7 +822,7 @@ contains
       call this%wrap_update_hlmfates_dyn(nc,               &
                                          bounds_clump,     &
                                          canopystate_inst, &
-                                         frictionvel_inst)
+                                         frictionvel_inst, .false.)
       
       ! ---------------------------------------------------------------------------------
       ! Part IV: 
@@ -945,7 +946,7 @@ contains
    !--------------------------------------------------------------------------------------
 
    subroutine wrap_update_hlmfates_dyn(this, nc, bounds_clump,      &
-         canopystate_inst, frictionvel_inst )
+         canopystate_inst, frictionvel_inst, is_initing_from_restart)
 
       ! ---------------------------------------------------------------------------------
       ! This routine handles the updating of vegetation canopy diagnostics, (such as lai)
@@ -959,6 +960,10 @@ contains
      integer                 , intent(in)           :: nc
      type(canopystate_type)  , intent(inout)        :: canopystate_inst
      type(frictionvel_type)  , intent(inout)        :: frictionvel_inst
+
+     ! is this being called during a read from restart sequence (if so then use the restarted fates
+     ! snow depth variable rather than the CLM variable).
+     logical                 , intent(in)           :: is_initing_from_restart
      
      integer :: npatch  ! number of patches in each site
      integer :: ifp     ! index FATES patch 
@@ -988,7 +993,12 @@ contains
           this%fates(nc)%bc_in(s)%snow_depth_si   = snow_depth(c)
           this%fates(nc)%bc_in(s)%frac_sno_eff_si = frac_sno_eff(c)
        end do
-       
+
+       ! Only update the fates internal snow burial if this is not a restart
+       if (.not. is_initing_from_restart) then
+          call UpdateFatesAvgSnowDepth(this%fates(nc)%sites,this%fates(nc)%bc_in)
+       end if
+
        ! Canopy diagnostics for FATES
        call canopy_summarization(this%fates(nc)%nsites, &
             this%fates(nc)%sites,  &
@@ -1391,7 +1401,7 @@ contains
                ! Update diagnostics of FATES ecosystem structure used in HLM.
                ! ------------------------------------------------------------------------
                call this%wrap_update_hlmfates_dyn(nc,bounds_clump, &
-                     canopystate_inst,frictionvel_inst)
+                     canopystate_inst,frictionvel_inst, .true.)
                
                ! ------------------------------------------------------------------------
                ! Update the 3D patch level radiation absorption fractions
@@ -1530,7 +1540,7 @@ contains
            ! Update diagnostics of FATES ecosystem structure used in HLM.
            ! ------------------------------------------------------------------------
            call this%wrap_update_hlmfates_dyn(nc,bounds_clump, &
-                canopystate_inst,frictionvel_inst)
+                canopystate_inst,frictionvel_inst, .false.)
 
            ! ------------------------------------------------------------------------
            ! Update history IO fields that depend on ecosystem dynamics
