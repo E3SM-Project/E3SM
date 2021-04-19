16297c8b262a2ff2a4c6e9b70543406fe387ddb6
James Foucar <jgfouca@sandia.gov>
2021-04-19 14:12:57 -0600

    Major upgrades to scripts and testing
    1) Enable scripts-tests in the AT if the 'scripts' label is on. Ensure_pylint
    is called prior to the pylint tests and so this should work regardless of
    preexisting pylint availability.
    2) Add a full test suite for git_utils
    3) Add testing to verify that test-all-scream baselines are being handled correctly.
    Testing this thoroughly required the ability to spoof git states.
    4) Add test-all-scream ability to know which specific test-types are missing
    baselines or have expired baselines.

--- components/scream/scripts/jenkins/jenkins_common_impl.sh
+++ components/scream/scripts/jenkins/jenkins_common_impl.sh
@@ -20,7 +20,10 @@ fi
 
 if [ $skip_testing -eq 0 ]; then
   # User did not request to skip tests. Proceed with testing.
-  source ./scream/components/scream/scripts/jenkins/${NODE_NAME}_setup
+
+  cd scream/components/scream
+
+  source scripts/jenkins/${NODE_NAME}_setup
 
   git config --global user.email "jenkins@ignore.com"
   git config --global user.name "Jenkins Jenkins"
@@ -34,29 +37,23 @@ if [ $skip_testing -eq 0 ]; then
   # IF such dir is not found, then the default (ctest-build/baselines) is used
   BASELINES_DIR=AUTO
 
-  ./scream/components/scream/scripts/gather-all-data "./scripts/test-all-scream --baseline-dir $BASELINES_DIR \$compiler -c EKAT_DISABLE_TPL_WARNINGS=ON -p -i -m \$machine $SUBMIT" -l -m $SCREAM_MACHINE
+  ./scripts/gather-all-data "./scripts/test-all-scream --baseline-dir $BASELINES_DIR \$compiler -c EKAT_DISABLE_TPL_WARNINGS=ON -p -i -m \$machine $SUBMIT" -l -m $SCREAM_MACHINE
 
   # Add a valgrind test for mappy for nightlies
   if [[ -n "$SUBMIT" && "$SCREAM_MACHINE" == "mappy" ]]; then
-      ./scream/components/scream/scripts/gather-all-data "./scripts/test-all-scream -t valg --baseline-dir $BASELINES_DIR \$compiler -c EKAT_DISABLE_TPL_WARNINGS=ON -p -i -m \$machine $SUBMIT" -l -m $SCREAM_MACHINE
+    ./scripts/gather-all-data "./scripts/test-all-scream -t valg --baseline-dir $BASELINES_DIR \$compiler -c EKAT_DISABLE_TPL_WARNINGS=ON -p -i -m \$machine $SUBMIT" -l -m $SCREAM_MACHINE
   fi
 
   # Uncomment this once pylint is available on target machines
-  # if [ $test_scripts -eq 1 ]; then
-  #     cd scream/components/scream/scripts
-  #     ./scripts-tests -g
-  #     if [ $? -ne 0 ]; then
-  #       exit 1
-  #     fi
-  #     ./scripts-tests -c
-  #     if [ $? -ne 0 ]; then
-  #       exit 1
-  #     fi
-  #     ./scripts-tests -f -m $SCREAM_MACHINE
-  #     if [ $? -ne 0 ]; then
-  #       exit 1
-  #     fi
-  # fi
+  if [ $test_scripts -eq 1 ]; then
+    # JGF: I'm not sure there's much value in these dry-run comparisons
+    # since we aren't changing HEADs
+    ./scripts/scripts-tests -g
+    ./scripts/scripts-tests -c
+
+    ./scripts/scripts-tests -f -m $SCREAM_MACHINE
+  fi
+
 else
   echo "Tests were skipped, since the Github label 'CI: Integrate Without Testing' was found.\n"
 fi
