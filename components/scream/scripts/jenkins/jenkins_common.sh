49a5b36f50e91a6a7fbe028ac9a402ee9ee70b38
James Foucar <jgfouca@sandia.gov>
2021-04-19 10:30:14 -0600

    tee output

--- components/scream/scripts/jenkins/jenkins_common.sh
+++ components/scream/scripts/jenkins/jenkins_common.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
 DATE_STAMP=$(date "+%Y-%m-%d_%H%M%S")
 
-$SCRIPT_DIR/jenkins_common_impl.sh >& JENKINS_$DATE_STAMP
+set -o pipefail
+$SCRIPT_DIR/jenkins_common_impl.sh 2>&1 | tee JENKINS_$DATE_STAMP
