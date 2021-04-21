6adea3de077aa2e52d0ae19ea4e1286d53779aa2
James Foucar <jgfouca@sandia.gov>
2021-04-21 15:50:20 -0600

    Fix unintended push of debug stuff

--- components/scream/scripts/jenkins/jenkins_common.sh
+++ components/scream/scripts/jenkins/jenkins_common.sh
@@ -3,6 +3,5 @@
 export JENKINS_SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
 DATE_STAMP=$(date "+%Y-%m-%d_%H%M%S")
 
-echo $JENKINS_SCRIPT_DIR
-# set -o pipefail
-# $JENKINS_SCRIPT_DIR/jenkins_common_impl.sh 2>&1 | tee JENKINS_$DATE_STAMP
+set -o pipefail
+$JENKINS_SCRIPT_DIR/jenkins_common_impl.sh 2>&1 | tee JENKINS_$DATE_STAMP
