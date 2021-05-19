dead62e7c8c12e58175e86d5403be3f80d68587c
Luca Bertagna <lbertag@sandia.gov>
2021-05-19 17:32:28 -0600

    SCREAM: improve comments in scream_common_physics_functions.hpp

--- components/scream/src/share/util/scream_common_physics_functions_impl.hpp
+++ components/scream/src/share/util/scream_common_physics_functions_impl.hpp
@@ -1,15 +1,11 @@
 #ifndef SCREAM_COMMON_PHYSICS_IMPL_HPP
 #define SCREAM_COMMON_PHYSICS_IMPL_HPP
 
-#include "share/util/scream_common_physics_functions.hpp"
 #include "physics/share/physics_constants.hpp"
+#include "share/util/scream_column_ops.hpp"
 
 namespace scream {
 
-//-----------------------------------------------------------------------------------------------//
-// Computes exner function
-// The result is exners formula, and is [dimensionless]
-// The input is mid-level pressure, and has units of [Pa]
 template<typename DeviceT>
 template<typename ScalarT>
 KOKKOS_INLINE_FUNCTION
@@ -23,7 +19,7 @@ ScalarT PhysicsFunctions<DeviceT>::exner_function(const ScalarT& pressure)
 
   return pow( pressure/p0, rd*inv_cp );
 }
-// For operations on a full column at a time:
+
 template<typename DeviceT>
 template<typename ScalarT, typename InputProviderP>
 KOKKOS_INLINE_FUNCTION
@@ -37,22 +33,14 @@ void PhysicsFunctions<DeviceT>::exner_function(const MemberType& team,
   });
 }
 
-//-----------------------------------------------------------------------------------------------//
-// Converts temperature into potential temperature
-// The result is the potential temperature, units in [K]
-// The inputs are
-//   temperature is the atmospheric temperature, units in [K]
-//   pressure is the atmospheric pressure, units in [Pa].  pressure is used in Exners function using `exner` defined above.
 template<typename DeviceT>
 template<typename ScalarT>
 KOKKOS_INLINE_FUNCTION
 ScalarT PhysicsFunctions<DeviceT>::calculate_theta_from_T(const ScalarT& temperature, const ScalarT& pressure)
 {
-  // Theta = T/exner;
   return temperature/exner_function(pressure);
 }
 
-// For operations on a full column at a time:
 template<typename DeviceT>
 template<typename ScalarT, typename InputProviderT, typename InputProviderP>
 KOKKOS_INLINE_FUNCTION
@@ -67,22 +55,14 @@ void PhysicsFunctions<DeviceT>::calculate_theta_from_T(const MemberType& team,
   });
 }
 
-//-----------------------------------------------------------------------------------------------//
-// Converts potential temperature into temperature
-// The result is the temperature, units in [K]
-// The inputs are
-//   theta is the potential temperature, units in [K]
-//   pressure is the atmospheric pressure, units in [Pa].  pressure is used in Exners function using `exner` defined above.
 template<typename DeviceT>
 template<typename ScalarT>
 KOKKOS_INLINE_FUNCTION
 ScalarT PhysicsFunctions<DeviceT>::calculate_T_from_theta(const ScalarT& theta, const ScalarT& pressure)
 {
-  // T = Theta*exner
   return theta*exner_function(pressure);
 }
 
-// For operations on a full column at a time:
 template<typename DeviceT>
 template<typename ScalarT, typename InputProviderT, typename InputProviderP>
 KOKKOS_INLINE_FUNCTION
@@ -96,12 +76,7 @@ void PhysicsFunctions<DeviceT>::calculate_T_from_theta(const MemberType& team,
     temperature(k) = calculate_T_from_theta(theta(k),pressure(k));
   });
 }
-//-----------------------------------------------------------------------------------------------//
-// Compute temperature from virtual temperature
-// The result unit is in [K]
-// The inputs are
-//   T_virtual is the virtual temperature.  Units in [K].
-//   qv        is the water vapor mass mixing ratio.  Units in [kg/kg]
+
 template<typename DeviceT>
 template<typename ScalarT>
 KOKKOS_INLINE_FUNCTION
@@ -111,6 +86,7 @@ calculate_temperature_from_virtual_temperature(const ScalarT& T_virtual, const S
   using C = scream::physics::Constants<Real>;
 
   static constexpr auto ep_2 = C::ep_2;
+
   return T_virtual*((ep_2*(1.0+qv))/(qv+ep_2));
 }
 
@@ -129,13 +105,6 @@ calculate_temperature_from_virtual_temperature(const MemberType& team,
   });
 }
 
-
-//-----------------------------------------------------------------------------------------------//
-// Compute virtual temperature
-// The result unit is in [K]
-// The inputs are
-//   temperature is the atmospheric temperature.  Units in [K].
-//   qv    is the water vapor mass mixing ratio.  Units in [kg/kg]
 template<typename DeviceT>
 template<typename ScalarT>
 KOKKOS_INLINE_FUNCTION
@@ -162,13 +131,6 @@ calculate_virtual_temperature(const MemberType& team,
   });
 }
 
-//-----------------------------------------------------------------------------------------------//
-// Compute dry static energy (DSE).
-// The result unit is in [J/kg]
-// The inputs are
-//   temperature       is the atmospheric temperature. Units in [K].
-//   z                 is the geopotential height above surface at midpoints. Units in [m].
-//   surf_geopotential is the surface geopotential height. Units in [m].
 template<typename DeviceT>
 template<typename ScalarT>
 KOKKOS_INLINE_FUNCTION
@@ -197,21 +159,13 @@ void PhysicsFunctions<DeviceT>::calculate_dse(const MemberType& team,
     dse(k) = calculate_dse(temperature(k),z(k),surf_geopotential);
   });
 }
-//-----------------------------------------------------------------------------------------------//
-// Determine the physical thickness of a vertical layer
-// The result is dz, units in [m]
-// The inputs are
-//   pseudo_density is the pressure level thickness, [Pa]
-//   p_mid          is the average atmosphere pressure over the level, [Pa]
-//   T_mid          is the average atmospheric temperature over the level, [K] - needed for T_virtual
-//   qv             is the water vapor mass mixing ratio, [kg/kg] - needed for T_virtual
+
 template<typename DeviceT>
 template<typename ScalarT>
 KOKKOS_INLINE_FUNCTION
 ScalarT PhysicsFunctions<DeviceT>::
 calculate_dz(const ScalarT& pseudo_density, const ScalarT& p_mid, const ScalarT& T_mid, const ScalarT& qv)
 {
-  // dz = pseudo_density*T_v*R/(p*g)
   using C = scream::physics::Constants<Real>;
 
   const ScalarT& T_virtual = calculate_virtual_temperature(T_mid,qv);
@@ -238,13 +192,7 @@ void PhysicsFunctions<DeviceT>::calculate_dz(const MemberType& team,
     dz(k) = calculate_dz(pseudo_density(k),p_mid(k),T_mid(k),qv(k));
   });
 }
-//-----------------------------------------------------------------------------------------------//
-// Determine the geopotential height of level interfaces
-// The result is z_int, units in [m]
-// The inputs are
-//   dz the vertical level thickness, [m]
-//   z_surf: the surface elevation [m]
-// Note: Only applicable over an entire column due to the need to integrate over dz.
+
 template<typename DeviceT>
 template<typename ScalarT, typename InputProviderZ>
 KOKKOS_INLINE_FUNCTION
@@ -259,7 +207,7 @@ void PhysicsFunctions<DeviceT>::calculate_z_int(const MemberType& team,
   constexpr bool FromTop = false;
   column_ops::template column_scan<FromTop>(team,num_levs,dz,z_int,z_surf);
 }
-//-----------------------------------------------------------------------------------------------//
+
 } // namespace scream
 
 #endif // SCREAM_COMMON_PHYSICS_IMPL_HPP
