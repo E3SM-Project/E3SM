fb43c1d920175cd0bedfbaf5a0c5654929d7dbf0
Walter Hannah <whannah1@gmail.com>
2021-06-01 18:22:05 -0400

    fix rebase bug
    	modified:   components/eam/src/physics/crm/crm_physics.F90

--- components/eam/src/physics/crm/crm_physics.F90
+++ components/eam/src/physics/crm/crm_physics.F90
@@ -654,7 +654,7 @@ subroutine crm_physics_tend(ztodt, state, tend, ptend, pbuf, cam_in, cam_out, &
    ! Initialize CRM state (nullify pointers, allocate memory, etc)
    !------------------------------------------------------------------------------------------------
    call crm_state_initialize(crm_state, pcols, crm_nx, crm_ny, crm_nz)
-   call crm_rad_initialize(crm_rad)
+   call crm_rad_initialize(crm_rad, pcols, crm_nx_rad, crm_ny_rad, crm_nz)
    call crm_input_initialize(crm_input, pcols, pver)
    call crm_output_initialize(crm_output, pcols, pver, crm_nx, crm_ny, crm_nz)
 
