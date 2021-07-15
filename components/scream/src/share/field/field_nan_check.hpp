3c40a5038f1e34130581d6c79213ea111444920c
Aaron Donahue <donahue.aaron@gmail.com>
2021-07-15 06:53:53 -0700

    Previous branch wasn't actually running rrtmgp.  This commit fixes that. It updates the CMakeLists.txt to include files needed by rrtmgp, and switches the input.yaml file for that test to run all 5 processes. It also updates scorpio_output to accept "Physics" and "Physics GLL" as acceptable grid names for output.
    Additionally, this commit introduces a field property check for NaN in
    a field and adds a test for that check in the field test.
    
    Finally a cosmetic change to some white space that was off.

---
+++ components/scream/src/share/field/field_nan_check.hpp
@@ -0,0 +1,50 @@
+#ifndef SCREAM_FIELD_NAN_CHECK_HPP
+#define SCREAM_FIELD_NAN_CHECK_HPP
+
+#include "share/field/field.hpp"
+#include "ekat/util/ekat_math_utils.hpp"
+
+namespace scream
+{
+
+// This field property check returns true if all data in a given field are
+// not a NaN.
+// Default to no repair allowed, we don't really want to be repairing NaN's
+// in our fields.
+template<typename RealType>
+class FieldNaNCheck: public FieldPropertyCheck<RealType> {
+public:
+  using non_const_RT = typename FieldPropertyCheck<RealType>::non_const_RT;
+  using const_RT     = typename FieldPropertyCheck<RealType>::const_RT;
+
+  // Default constructor -- cannot repair fields that fail the check.
+  FieldNaNCheck () = default; 
+
+  // Overrides.
+
+  bool check(const Field<const_RT>& field) const override {
+    auto view = field.get_view();
+    Int num_nans = 0; 
+    Kokkos::parallel_reduce(view.extent(0), KOKKOS_LAMBDA(Int i, Int& m) {
+      if (isnan(view(i))) {
+        m += 1;
+      }
+    }, num_nans);
+    return (num_nans == 0);
+  }
+
+  bool can_repair() const override {
+    return false;
+  }
+
+  void repair(Field<non_const_RT>& field) const override {
+    // Do Nothing
+  }
+
+protected:
+
+};
+
+} // namespace scream
+
+#endif // SCREAM_FIELD_NEN_CHECK_HPP
