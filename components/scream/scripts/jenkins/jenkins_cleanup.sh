38bd42f205f4cfb639868a9d165e92bc130672ed
James Foucar <jgfouca@sandia.gov>
2021-07-19 16:25:11 -0600

    more fixes

--- components/scream/scripts/jenkins/jenkins_cleanup.sh
+++ components/scream/scripts/jenkins/jenkins_cleanup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -xe
 
 export JENKINS_SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
-cd ${WORKSPACE}
 DATE_STAMP=$(date "+%Y-%m-%d_%H%M%S")
 
 set -o pipefail
-$JENKINS_SCRIPT_DIR/jenkins_cleanup_impl.sh 2>&1 | tee JENKINS_CLEAN_$DATE_STAMP
+$JENKINS_SCRIPT_DIR/jenkins_cleanup_impl.sh 2>&1 | tee ${WORKSPACE}/${BUILD_ID}/JENKINS_CLEAN_$DATE_STAMP
--- components/scream/scripts/jenkins/jenkins_cleanup_impl.sh
+++ components/scream/scripts/jenkins/jenkins_cleanup_impl.sh
@@ -2,6 +2,9 @@
 
 # Adjust this number to keep more/less builds
 echo "WORKSPACE: ${WORKSPACE}, BUILD_ID: ${BUILD_ID}"
+
+cd ${WORKSPACE}
+
 NUM_KEEP=30
 KEEP_LAST=${BUILD_ID}
 KEEP_FIRST=$((${BUILD_ID}-${NUM_KEEP}))
