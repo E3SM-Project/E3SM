dc8a0a978228977c2334866b6d3f585a081ace97
Aaron Donahue <donahue.aaron@gmail.com>
2021-07-26 16:28:10 -0700

    update gcc version for lassen

--- components/scream/scripts/machines_specs.py
+++ components/scream/scripts/machines_specs.py
@@ -48,7 +48,7 @@ MACHINE_METADATA = {
                   46,
                   46,
                   "/sems-data-store/ACME/baselines/scream/master-baselines"),
-    "lassen" : (["module --force purge", "module load git gcc/8.3.1 cuda/10.1.243 cmake/3.16.8 spectrum-mpi lapack python/3.7.2", "export LLNL_USE_OMPI_VARS='y'"],
+    "lassen" : (["module --force purge", "module load git gcc/7.3.1 cuda/10.1.243 cmake/3.16.8 spectrum-mpi lapack python/3.7.2", "export LLNL_USE_OMPI_VARS='y'"],
                  ["mpicxx","mpifort","mpicc"],
                   "bsub -Ip -qpdebug",
                   44,
