33de4195005893ddd8abf22965a93b15a7360f19
Luca Bertagna <lbertag@sandia.gov>
2021-08-16 18:04:21 -0600

    SCREAM: minor mods to BuildCprnc cmake script
    * Make sure the cprnc subdir is only added once
    * Set a project-wide cache var with the path/to/cprnc exec.

--- components/scream/cmake/BuildCprnc.cmake
+++ components/scream/cmake/BuildCprnc.cmake
@@ -5,18 +5,23 @@
 #
 # to your CMakeLists.txt.
 
-function(BuildCprnc)
+macro(BuildCprnc)
 
-  set(BLDROOT ${PROJECT_BINARY_DIR}/externals/cprnc)
-  file(WRITE ${BLDROOT}/Macros.cmake
-"
-set(SCC ${CMAKE_C_COMPILER})
-set(SFC ${CMAKE_Fortran_COMPILER})
-set(FFLAGS \"${CMAKE_Fortran_FLAGS}\")
-set(NETCDF_PATH ${NetCDF_Fortran_PATHS})
-"
-)
-  set(SRC_ROOT ${PROJECT_SOURCE_DIR}/../..)
-  set(ENV{CIMEROOT} ${SRC_ROOT}/cime)
-  add_subdirectory($ENV{CIMEROOT}/tools/cprnc ${BLDROOT})
-endfunction()
+  # Make sure this is built only once
+  if (NOT TARGET cprnc)
+    set(BLDROOT ${PROJECT_BINARY_DIR}/externals/cprnc)
+    file(WRITE ${BLDROOT}/Macros.cmake
+      "
+      set(SCC ${CMAKE_C_COMPILER})
+      set(SFC ${CMAKE_Fortran_COMPILER})
+      set(FFLAGS \"${CMAKE_Fortran_FLAGS}\")
+      set(NETCDF_PATH ${NetCDF_Fortran_PATHS})
+      "
+    )
+    set(SRC_ROOT ${PROJECT_SOURCE_DIR}/../..)
+    set(ENV{CIMEROOT} ${SRC_ROOT}/cime)
+    add_subdirectory($ENV{CIMEROOT}/tools/cprnc ${BLDROOT})
+
+    set(CPRNC_BINARY ${BLDROOT}/cprnc CACHE INTERNAL "")
+  endif()
+endmacro()
