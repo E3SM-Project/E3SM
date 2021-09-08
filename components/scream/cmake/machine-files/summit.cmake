9ada5e5642ff06f2118231bba5f2204a27a1f445
Benjamin Hillman <bhillma@sandia.gov>
2021-09-08 18:35:19 -0400

    Fixes for summit

--- components/scream/cmake/machine-files/summit.cmake
+++ components/scream/cmake/machine-files/summit.cmake
@@ -4,3 +4,4 @@ include (${EKAT_MACH_FILES_PATH}/kokkos/nvidia-v100.cmake)
 include (${EKAT_MACH_FILES_PATH}/kokkos/cuda.cmake)
 
 set(SCREAM_MPIRUN_EXE "jsrun" CACHE STRING "")
+set(SCREAM_MPI_NP_FLAG "-n" CACHE STRING "")
