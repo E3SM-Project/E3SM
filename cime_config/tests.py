fb436045bd6af20599b263f9486d1f14a6413b93
Azamat Mametjanov <azamat@mcs.anl.gov>
2021-09-23 13:54:44 -0700

    Update high-res F-compset and low-res G-compset grids

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -339,13 +339,13 @@ _TESTS = {
     "e3sm_bench_hires_f" : {
         "time"     : "01:00:00",
         "tests"    : (
-                 "PFS_P7200.ne120pg2_r05_oECv3.F2010.eam-bench-noio",
-                 "PFS_P8640.ne120pg2_r05_oECv3.F2010.eam-bench-noio",
-                 "PFS_P10800.ne120pg2_r05_oECv3.F2010.eam-bench-noio",
-                 "PFS_P14400.ne120pg2_r05_oECv3.F2010.eam-bench-noio",
-                 "PFS_P21600.ne120pg2_r05_oECv3.F2010.eam-bench-noio",
-                 "PFS_P43200.ne120pg2_r05_oECv3.F2010.eam-bench-noio",
-                 "PFS_P86400.ne120pg2_r05_oECv3.F2010.eam-bench-noio",
+                 "PFS_P7200.ne120pg2_r05_EC30to60E2r2.F2010.eam-bench-noio",
+                 "PFS_P8640.ne120pg2_r05_EC30to60E2r2.F2010.eam-bench-noio",
+                 "PFS_P10800.ne120pg2_r05_EC30to60E2r2.F2010.eam-bench-noio",
+                 "PFS_P14400.ne120pg2_r05_EC30to60E2r2.F2010.eam-bench-noio",
+                 "PFS_P21600.ne120pg2_r05_EC30to60E2r2.F2010.eam-bench-noio",
+                 "PFS_P43200.ne120pg2_r05_EC30to60E2r2.F2010.eam-bench-noio",
+                 "PFS_P86400.ne120pg2_r05_EC30to60E2r2.F2010.eam-bench-noio",
                  )
     },
     "e3sm_bench_hires" : {
@@ -361,11 +361,11 @@ _TESTS = {
         "share"    : True,
         "time"     : "01:00:00",
         "tests"    : (
-                 "PFS_P320.T62_oEC60to30v3.GMPAS-IAF.bench-gmpas_noio",
-                 "PFS_P480.T62_oEC60to30v3.GMPAS-IAF.bench-gmpas_noio",
-                 "PFS_P640.T62_oEC60to30v3.GMPAS-IAF.bench-gmpas_noio",
-                 "PFS_P960.T62_oEC60to30v3.GMPAS-IAF.bench-gmpas_noio",
-                 "PFS_P1280.T62_oEC60to30v3.GMPAS-IAF.bench-gmpas_noio",
+                 "PFS_P320.T62_EC30to60E2r2.GMPAS-IAF.bench-gmpas_noio",
+                 "PFS_P480.T62_EC30to60E2r2.GMPAS-IAF.bench-gmpas_noio",
+                 "PFS_P640.T62_EC30to60E2r2.GMPAS-IAF.bench-gmpas_noio",
+                 "PFS_P960.T62_EC30to60E2r2.GMPAS-IAF.bench-gmpas_noio",
+                 "PFS_P1280.T62_EC30to60E2r2.GMPAS-IAF.bench-gmpas_noio",
                  )
     },
     "e3sm_bench_lores_f" : {
