2765aa97ce2c1d0613859e0f9bdb033b7faf15dd
James Foucar <jgfouca@sandia.gov>
2021-10-07 13:10:27 -0600

    Add missing file

---
+++ components/scream/tests/generic/infra/resource_spread.cpp
@@ -0,0 +1,15 @@
+#include "ekat/kokkos/ekat_kokkos_types.hpp"
+
+#include <catch2/catch.hpp>
+
+#include <iostream>
+
+namespace scream {
+
+TEST_CASE("rank_and_thread_spread", "[fake_infra_test]")
+{
+  // TODO
+  REQUIRE(true);
+}
+
+} // empty namespace
