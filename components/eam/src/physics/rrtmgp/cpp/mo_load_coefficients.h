e77b3dd686cf290a0ff5ea6da290ee02d5d9ba11
Benjamin R. Hillman <bhillma@sandia.gov>
2021-10-07 14:47:29 -0600

    Revert to RRTMGP I/O

--- components/eam/src/physics/rrtmgp/cpp/mo_load_coefficients.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "rrtmgp_const.h"
-#include "mo_gas_concentrations.h"
-#include "mo_gas_optics_rrtmgp.h"
-#include <string>
-
-void load_and_init(GasOpticsRRTMGP &kdist, std::string filename, GasConcs const &available_gases);
-
