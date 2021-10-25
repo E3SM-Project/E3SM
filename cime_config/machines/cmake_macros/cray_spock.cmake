273206b033c8a90bf77ff1f6e773e4fa1b04dd56
Youngsung Kim <kimy@ornl.gov>
2021-10-25 15:02:07 -0400

    added cray_spock.cmake

---
+++ cime_config/machines/cmake_macros/cray_spock.cmake
@@ -0,0 +1,11 @@
+if (compile_threaded)
+  string(APPEND CFLAGS " -fopenmp")
+  string(APPEND FFLAGS " -fopenmp")
+  string(APPEND CXXFLAGS " -fopenmp")
+  string(APPEND LDFLAGS " -fopenmp")
+endif()
+
+string(APPEND SLIBS " -L$ENV{PNETCDF_PATH}/lib -lpnetcdf")
+set(PNETCDF_PATH "$ENV{PNETCDF_PATH}")
+set(PIO_FILESYSTEM_HINTS "gpfs")
+
