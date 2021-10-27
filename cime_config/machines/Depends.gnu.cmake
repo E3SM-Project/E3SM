62ab0ddea7dcb019513cd1989b01dd5d0b631974
noel <ndkeen@lbl.gov>
2021-10-27 15:22:06 -0700

    Add a flag to remove a certain optimization for one MPAS file -- only for GNU compilers. Only needed for gnu9 and higher, but is safe with lower versions.

--- cime_config/machines/Depends.gnu.cmake
+++ cime_config/machines/Depends.gnu.cmake
@@ -1,1 +1,11 @@
 e3sm_add_flags("eam/src/dynamics/fv/geopk.F90" "-fcray-pointer")
+
+list(APPEND MPAS_ICE_SHORTWAVE
+  ${CMAKE_BINARY_DIR}/core_seaice/column/ice_shortwave.f90
+)
+
+if (NOT DEBUG)
+  foreach(ITEM IN LISTS MPAS_ICE_SHORTWAVE)
+    e3sm_add_flags("${ITEM}" "-fno-tree-pta") # avoids an error that shows up in solver with gnu9 and higher
+  endforeach()
+endif()
