7a49c9e412aa6d6820286220e6cf8a34d01cbd86
Aaron Sheffield Donahue <asdonah@blake.sandia.gov>
2021-11-02 17:30:01 -0600

    Restructure the spa unit tests.  Now they are all individual tests rather than a single common unit test that runs multiple structures.
    This commit is incomplete.  The remap test needs to be fixed given that
    when mpi_ranks>1 only part of the remap data is stored.

--- components/scream/src/physics/spa/tests/spa_read_remap_from_file_test.cpp
+++ components/scream/src/physics/spa/tests/spa_read_remap_from_file_test.cpp
@@ -1,7 +1,6 @@
 #include "catch2/catch.hpp"
 
 #include "share/io/scream_scorpio_interface.hpp"
-#include "spa_unit_tests_common.hpp"
 #include "physics/spa/spa_functions.hpp"
 
 #include "ekat/ekat_pack.hpp"
@@ -13,78 +12,74 @@
 #include <random>
 #include <thread>
 
-namespace scream {
-namespace spa {
-namespace unit_test {
-
-template <typename D>
-struct UnitWrap::UnitTest<D>::TestReadRemapData {
-
-  template <typename S>
-  using view_1d = typename KokkosTypes<D>::template view_1d<S>;
-
-  static void run_property()
-  {
-//    // Set up the mpi communicator and init the pio subsystem
-//    ekat::Comm io_comm(MPI_COMM_WORLD);  // MPI communicator group used for I/O set as ekat object.
-//    MPI_Fint fcomm = MPI_Comm_c2f(io_comm.mpi_comm());  // MPI communicator group used for I/O.  In our simple test we use MPI_COMM_WORLD, however a subset could be used.
-//    scorpio::eam_init_pio_subsystem(fcomm);   // Gather the initial PIO subsystem data creater by component coupler
-
-    // Establish the SPA function object
-    using SPAFunc         = spa::SPAFunctions<Real, DefaultDevice>;
-    SPAFunc::SPAHorizInterp spa_horiz_interp;
-
-//    const std::string remap_file_name = "map_ne2np4_to_ne4np4_mono.nc";
-    const std::string remap_file_name = "spa_data_for_testing.nc";
-
-    Int target_grid_ncols = 2;
-    view_1d<int> dofs_gids("",target_grid_ncols);
-    for (int ii=0;ii<ncols;ii++) {
-      dofs_gids(ii) = ii;
-    }
-    SPAFunc::get_remap_weights_from_file(remap_file_name,target_grid_ncols,dofs_gids,spa_horiz_interp);
-
-    REQUIRE(spa_horiz_interp.length==6); // The test file has 17,498 points in it
-    REQUIRE(spa_horiz_interp.source_grid_ncols==3); // The test file maps ne2 (218 cols) to ne4 (866 cols)
-
-    // We have a few metrics to ensure that the data read from file matches the data in the file.
-    Real tol = 1e5*std::numeric_limits<Real>::epsilon();
-    Int col_sum = 0;
-    Int row_sum = 0;
-    Real wgt_sum = 0.0;
-    view_1d<Real> wgts("",target_grid_ncols);
-    Kokkos::deep_copy(wgts,0.0);
-    for (int i=0; i<spa_horiz_interp.length; i++) {
-      col_sum += spa_horiz_interp.target_grid_loc[i];
-      row_sum += spa_horiz_interp.source_grid_loc[i];
-      wgt_sum += spa_horiz_interp.weights[i];
-      wgts(spa_horiz_interp.target_grid_loc[i]) += spa_horiz_interp.weights[i];
-    }
-    REQUIRE(col_sum == 9);
-    REQUIRE(row_sum == 12);
-    REQUIRE(std::abs(wgt_sum - 2.0) < tol);
-    // The sum of remap weights should always be 1.0
-    for (int i=0; i<target_grid_ncols; i++) {
-      REQUIRE(wgts[i]==1.0);
-    }
-    
-//    // All Done 
-//    scorpio::eam_pio_finalize();
-  }
-
-}; // struct TestReadRemapData
-
-} // namespace unit_test
-} // namespace spa
-} //namespace scream
-
 namespace {
 
-TEST_CASE("spa_read_remap_test", "spa")
+using namespace scream;
+using namespace spa;
+
+template <typename S>
+using view_1d = typename KokkosTypes<DefaultDevice>::template view_1d<S>;
+
+TEST_CASE("spa_read_remap_data","spa")
 {
-  using TestStruct = scream::spa::unit_test::UnitWrap::UnitTest<scream::DefaultDevice>::TestReadRemapData;
+  // Set up the mpi communicator and init the pio subsystem
+  ekat::Comm spa_comm(MPI_COMM_WORLD);  // MPI communicator group used for I/O set as ekat object.
+  MPI_Fint fcomm = MPI_Comm_c2f(spa_comm.mpi_comm());  // MPI communicator group used for I/O.  In our simple test we use MPI_COMM_WORLD, however a subset could be used.
+  scorpio::eam_init_pio_subsystem(fcomm);   // Gather the initial PIO subsystem data creater by component coupler
 
-  TestStruct::run_property();
+  // Establish the SPA function object
+  using SPAFunc         = spa::SPAFunctions<Real, DefaultDevice>;
+  SPAFunc::SPAHorizInterp spa_horiz_interp;
+
+  const std::string remap_file_name = "spa_data_for_testing.nc";
+
+  Int tgt_grid_ncols = 48;
+  Int src_grid_ncols = 20;
+  // Break the test set of columns into local degrees of freedom per mpi rank
+  auto comm_size = spa_comm.size();
+  auto comm_rank = spa_comm.rank();
+  std::vector<int> my_dofs;
+  for (int ii=comm_rank;ii<tgt_grid_ncols;ii+=comm_size) {
+    my_dofs.push_back(ii);
+  }
+  view_1d<int> dofs_gids("",my_dofs.size());
+  auto dofs_gids_h = Kokkos::create_mirror_view(dofs_gids);
+  Kokkos::deep_copy(dofs_gids_h,dofs_gids);
+  for (int ii=0;ii<my_dofs.size();ii++) {
+    dofs_gids_h(ii) = my_dofs[ii];
+  }
+  Kokkos::deep_copy(dofs_gids,dofs_gids_h);
+  SPAFunc::get_remap_weights_from_file(remap_file_name,tgt_grid_ncols,dofs_gids,spa_horiz_interp);
+
+  REQUIRE(spa_horiz_interp.length==tgt_grid_ncols*src_grid_ncols);
+  REQUIRE(spa_horiz_interp.source_grid_ncols==src_grid_ncols);
+
+  // We have a few metrics to ensure that the data read from file matches the data in the file.
+  Real tol = 1e5*std::numeric_limits<Real>::epsilon();
+  Int col_sum = 0;
+  Int row_sum = 0;
+  Real wgt_sum = 0.0;
+  view_1d<Real> wgts("",tgt_grid_ncols);
+  Kokkos::deep_copy(wgts,0.0);
+  for (int i=0; i<spa_horiz_interp.length; i++) {
+    col_sum += spa_horiz_interp.target_grid_loc[i];
+    row_sum += spa_horiz_interp.source_grid_loc[i];
+    wgt_sum += spa_horiz_interp.weights[i];
+    wgts(spa_horiz_interp.target_grid_loc[i]) += spa_horiz_interp.weights[i];
+  }
+  // Note, for our test problem the column sum is sum(0...tgt_grid_ncols-1)*src_grid_ncols,
+  //       and the                     row sum is sum(0...src_grid_ncols-1)*tgt_grid_ncols. 
+  //       Each set of weights should add up to 1.0 for each ncol, so total weights=tgt_grid_ncols
+  REQUIRE(col_sum == (tgt_grid_ncols*(tgt_grid_ncols-1))/2*src_grid_ncols);
+  REQUIRE(row_sum == (src_grid_ncols*(src_grid_ncols-1)/2*tgt_grid_ncols));
+  REQUIRE(std::abs(wgt_sum - 1.0*tgt_grid_ncols) < tol);
+  // The sum of remap weights should always be 1.0
+  for (int i=0; i<tgt_grid_ncols; i++) {
+    REQUIRE(wgts[i]==1.0);
+  }
+  
+  // All Done 
+  scorpio::eam_pio_finalize();
 }
 
-}
+} //namespace
