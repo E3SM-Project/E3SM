588dcea85267870eaec437950878b4780758be27
Oksana Guba <oksana.guba@gmail.com>
2021-12-07 09:20:01 -0700

    fixing random fails

--- components/homme/test_execs/thetal_kokkos_ut/gllfvremap_ut.cpp
+++ components/homme/test_execs/thetal_kokkos_ut/gllfvremap_ut.cpp
@@ -142,6 +142,7 @@ struct Session {
     p.transport_alg = 0;
     p.moisture = MoistDry::MOIST;
     p.theta_hydrostatic_mode = false;
+    p.rearth = PhysicalConstants::rearth0;
     p.params_set = true;
 
     const auto hyai = cmvdc(h.hybrid_ai);
