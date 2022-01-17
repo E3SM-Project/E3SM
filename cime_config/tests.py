771bd912b0503af2554bcacf4acb6a91a92dc266
Oksana Guba <oksana.guba@gmail.com>
2022-01-16 20:31:12 -0600

    adding ers test, no threads, passes

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -55,7 +55,8 @@ _TESTS = {
             "SMS.ne4_oQU240.F2010.eam-cosplite",
             "SMS_R_Ld5.ne4_ne4.FSCM5A97.eam-scm",
             "SMS_D_Ln5.ne4_oQU240.F2010",
-            "SMS_Ln5.ne4pg2_oQU480.F2010"
+            "SMS_Ln5.ne4pg2_oQU480.F2010",
+            "ERS.ne4_oQU240.F2010.eam-hommexx"
             )
         },
 
