9915ad6fce7f47fd283807bf992e867c49201327
Walter Hannah <whannah1@gmail.com>
2022-01-27 10:22:34 -0500

    fix CRM_QV output for P3

--- components/eam/src/physics/crm/crm_history.F90
+++ components/eam/src/physics/crm/crm_history.F90
@@ -470,6 +470,10 @@ subroutine crm_history_out(state, ptend, crm_state, crm_rad, crm_output, &
       call outfld('CRM_QV  ',(crm_state%qt(icol_beg:icol_end,:,:,:)    &
                               -crm_output%qcl(icol_beg:icol_end,:,:,:) &
                               -crm_output%qci(icol_beg:icol_end,:,:,:)), ncol, lchnk )
+   else if (MMF_microphysics_scheme .eq. 'p3') then 
+      call outfld('CRM_QV  ',(crm_state%qt(icol_beg:icol_end,:,:,:)    &
+                              -crm_output%qcl(icol_beg:icol_end,:,:,:) &
+                              -crm_output%qci(icol_beg:icol_end,:,:,:)), ncol, lchnk )
    else if (MMF_microphysics_scheme .eq. 'm2005') then 
       call outfld('CRM_QV  ', crm_state%qt(icol_beg:icol_end,:,:,:)    &
                               -crm_output%qcl(icol_beg:icol_end,:,:,:), ncol, lchnk )
