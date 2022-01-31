93536cb5720d10dd49257fb34a23441f9b473f7f
Xingqiu Yuan <xyuan@anl.gov>
2022-01-31 12:01:54 -0500

    move tracer enum to samxx_const

--- components/eam/src/physics/crm/samxx/micro_p3.h
+++ components/eam/src/physics/crm/samxx/micro_p3.h
@@ -8,18 +8,6 @@
 #include "p3_functions_f90.hpp"
 #include "p3_f90.hpp"
 
-enum {
-   idx_qt = 0,  // total water (qv + qc)
-   // idx_qc,      // cloud liq amount
-   idx_qi,      // cloud ice amount
-   idx_qr,      // rain amount
-   idx_nc,      // cloud liq number
-   idx_ni,      // cloud ice number
-   idx_nr,      // rain number
-   idx_qm,      // ice rime amount
-   idx_bm       // ice rime volume
-};
-
 void micro_p3_init();
 void micro_p3_proc();
 
