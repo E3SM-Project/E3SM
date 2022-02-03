6aa094c115dde5d603d4600751820efd9b081183
Youngsung Kim <kimy@ornl.gov>
2022-02-03 08:31:15 -0500

    applied cce/13.0.1 to crayclang config.

--- cime_config/machines/cmake_macros/gnu_crusher.cmake
+++ cime_config/machines/cmake_macros/gnu_crusher.cmake
@@ -11,6 +11,8 @@ if (NOT DEBUG)
   string(APPEND FFLAGS " -O2")
   string(APPEND CFLAGS " -O2")
 endif()
+string(APPEND CXX_LIBS " -lstdc++")
+
 string(APPEND SLIBS " -L$ENV{PNETCDF_PATH}/lib -lpnetcdf")
 set(NETCDF_PATH "$ENV{NETCDF_DIR}")
 set(PNETCDF_PATH "$ENV{PNETCDF_DIR}")
