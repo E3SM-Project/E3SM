345f9d9c5bb96b8bb238e2aec4add73fe49a477d
Walter Hannah <whannah1@gmail.com>
2022-02-14 17:06:42 -0500

    add shoc_min_tke

--- components/eam/src/physics/crm/samxx/samxx_const.h
+++ components/eam/src/physics/crm/samxx/samxx_const.h
@@ -257,6 +257,8 @@ real constexpr qci0 = 1.e-4;
 real constexpr alphaelq = 1.e-3;
 real constexpr betaelq = 1.e-3;
 
+real constexpr shoc_min_tke = 0.004;
+
 real constexpr p3_mincld = 0.0001; // same as mincld in SCREAM
 real constexpr p3_qsmall = 1.e-14;
 
