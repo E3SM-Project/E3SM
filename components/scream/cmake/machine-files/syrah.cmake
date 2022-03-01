4ba2052be656e1185594c7fe495cfd8def554a6f
Luca Bertagna <lbertag@sandia.gov>
2022-03-01 10:19:39 -0700

    Revert "Add logic to specify mach-specific flags in scream"

--- components/scream/cmake/machine-files/syrah.cmake
+++ components/scream/cmake/machine-files/syrah.cmake
@@ -5,11 +5,9 @@ include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
 # Enable Sandy Bridge arch in Kokkos
 option(Kokkos_ARCH_SNB "" ON)
 
+set(CMAKE_CXX_FLAGS "-w -cxxlib=/usr/tce/packages/gcc/gcc-8.3.1/rh" CACHE STRING "" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "-L/usr/tce/packages/gcc/gcc-8.3.1/rh/lib/gcc/x86_64-redhat-linux/8/ -mkl" CACHE STRING "" FORCE)
 set(SCREAM_MPIRUN_EXE "srun" CACHE STRING "")
 set(SCREAM_MPI_NP_FLAG "-n" CACHE STRING "")
 set(SCREAM_MPI_EXTRA_ARGS "" CACHE STRING "")
 set(SCREAM_INPUT_ROOT "/usr/gdata/climdat/ccsm3data/inputdata/" CACHE STRING "")
-
-# Mach-specific flags
-set(SCREAM_CXX_FLAGS "-w -cxxlib=/usr/tce/packages/gcc/gcc-8.3.1/rh" CACHE STRING "")
-set(SCREAM_LD_FLAGS "-L/usr/tce/packages/gcc/gcc-8.3.1/rh/lib/gcc/x86_64-redhat-linux/8/ -mkl" CACHE STRING "")
