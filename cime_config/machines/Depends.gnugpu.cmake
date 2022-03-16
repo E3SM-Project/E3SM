8690d0a97e5ee2d7c66d100a798d5049eaa8fc3d
noel <ndkeen@lbl.gov>
2022-03-16 15:13:15 -0700

    revert a previous commit and remove some blank lines

--- cime_config/machines/Depends.gnugpu.cmake
+++ cime_config/machines/Depends.gnugpu.cmake
@@ -17,10 +17,8 @@ list(APPEND ALLOW_INVALID_BOZ_LIST
 )
 
 if (CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10)
-
   foreach(ITEM IN LISTS ALLOW_INVALID_BOZ_LIST)
     e3sm_add_flags("${ITEM}" "-fallow-invalid-boz") # avoids build error for integer, parameter :: gen_hash_key_offset = z'000053db'
   endforeach()
-
 endif()
 
