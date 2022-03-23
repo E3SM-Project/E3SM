ac45c4a72038b5a09c577137b16edfbb3de0c55b
James Foucar <jgfouca@sandia.gov>
2022-03-23 17:10:34 -0600

    Progress: create_raw_xml_file done

--- components/scream/scripts/yaml_to_xml.py
+++ components/scream/scripts/yaml_to_xml.py
@@ -1,4 +1,4 @@
-import os, sys, re
+import os, re
 from collections import OrderedDict
 
 from utils import ensure_yaml, expect
@@ -28,6 +28,23 @@ def ordered_load(item, Loader=yaml.SafeLoader, object_pairs_hook=OrderedDict):
         return yaml.load(open(item, "r"), OrderedLoader)
     else:
         return yaml.load(item, OrderedLoader)
+
+###############################################################################
+def print_element(name, indent, value=None, attrib=None):
+###############################################################################
+    expect("__" not in name, f"elem name '{name}' defeats space encoding system")
+    name_no_ws = name.replace(" ", "__")
+
+    print_str = f'{" "*indent}<{name_no_ws}'
+    if attrib is not None:
+        for k, v in attrib.items():
+            print_str += f' {k}="{v}"'
+
+    print_str += ">"
+    if value is not None:
+        print_str += f"{value}</{name_no_ws}>"
+
+    print(print_str)
 
 ###############################################################################
 def do_switches(k, entry, indent):
@@ -61,9 +78,9 @@ def do_switches(k, entry, indent):
         for segment in reversed(segments):
             components = [item.strip() for item in segment.split("=>", 1)]
             if len(components) == 1:
-                print(f'{" "*indent}<{k}>{components[0]}</{k}>')
+                print_element(k, indent, value=components[0])
             else:
-                print(f'{" "*indent}<{k} {value}="{components[0]}">{components[1]}</{k}>')
+                print_element(k, indent, attrib={value:components[0]}, value=components[1])
 
         m = None
 
@@ -73,20 +90,20 @@ def process_leaf(k, v, indent):
     if SWITCH_RE.match(str(v)):
         do_switches(k, v, indent)
     else:
-        print(f'{" "*indent}<{k}>{v}</{k}>')
+        print_element(k, indent, value=v)
 
 ###############################################################################
-def process_node(node, itr, indent):
+def process_node(itr, indent):
 ###############################################################################
     for k, v in itr:
 
         if isinstance(v, dict):
-            print(f'{" "*indent}<{k}>')
-            process_node(v, v.items(), indent+2)
-            print(f"{' '*indent}<\{k}>")
+            print_element(k, indent)
+            process_node(v.items(), indent+2)
+            print_element("/" + k, indent)
 
         elif isinstance(v, list):
-            print(f'{" "*indent}<{k}>{", ".join([str(item) for item in v])}</{k}>')
+            print_element(k, indent, value=", ".join([str(item) for item in v]))
         else:
             process_leaf(k, v, indent)
 
@@ -98,4 +115,4 @@ def yaml_to_xml(filepath, indent):
     # Load scream inputs from yaml
     scream_input = ordered_load(filepath)
 
-    process_node(scream_input, scream_input.items(), indent)
+    process_node(scream_input.items(), indent)
