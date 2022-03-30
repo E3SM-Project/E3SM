01d8a9f66611315e29b5ad5e6d68c4f3039c1ac5
Azamat Mametjanov <azamat@mcs.anl.gov>
2022-03-29 23:05:41 -0400

    Update PGI compiler flags

--- cime_config/machines/Depends.pgigpu.cmake
+++ cime_config/machines/Depends.pgigpu.cmake
@@ -11,10 +11,21 @@ set(O1MODELSRC
   eam/src/chemistry/modal_aero/modal_aero_convproc.F90
   eam/src/chemistry/utils/modal_aero_calcsize.F90
   eam/src/physics/cam/zm_conv.F90)
+
+set(O0MODELSRC
+  ../driver-mct/main/component_mod.F90
+)
+
 if (NOT DEBUG)
   foreach(ITEM IN LISTS O1MODELSRC)
     e3sm_remove_flags("${ITEM}" "-O2")
     e3sm_add_flags("${ITEM}" "-O1 -Mnovect")
+  endforeach()
+
+  foreach(ITEM IN LISTS O0MODELSRC)
+    e3sm_remove_flags("${ITEM}" "-O2")
+    e3sm_remove_flags("${ITEM}" "-O1")
+    e3sm_add_flags("${ITEM}" "-O0 -Mnovect")
   endforeach()
 endif()
 
