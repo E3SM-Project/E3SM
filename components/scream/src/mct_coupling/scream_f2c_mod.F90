23ff8b97cd8daf3c1fa3f3697aa9e552f2314364
Luca Bertagna <lbertag@sandia.gov>
2022-03-31 18:01:19 -0600

    SCREAM: make mct interface set up atm log file in the AD

--- components/scream/src/mct_coupling/scream_f2c_mod.F90
+++ components/scream/src/mct_coupling/scream_f2c_mod.F90
@@ -13,13 +13,13 @@ interface
   ! It does *NOT* initialize the fields and the atm processes, nor it initializes
   ! any structure related to the component coupler. Other subroutines
   ! will have to be called *after* this one, to achieve that.
-  subroutine scream_create_atm_instance (f_comm,yaml_fname) bind(c)
+  subroutine scream_create_atm_instance (f_comm,yaml_fname,atm_log_fname) bind(c)
     use iso_c_binding, only: c_int, c_char
     !
     ! Input(s)
     !
     integer (kind=c_int),   intent(in) :: f_comm
-    character(kind=c_char), target, intent(in) :: yaml_fname(*)
+    character(kind=c_char), target, intent(in) :: yaml_fname(*), atm_log_fname(*)
   end subroutine scream_create_atm_instance
 
   subroutine scream_get_cols_latlon (lat, lon) bind(c)
