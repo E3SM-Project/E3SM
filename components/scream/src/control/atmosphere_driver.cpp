7a0d2de13fc7d1b697448ba9a0ca5b89c3c1bc2d
Luca Bertagna <lbertag@sandia.gov>
2022-04-04 16:42:49 -0600

    SCREAM: make AD set logger in atm proc params

--- components/scream/src/control/atmosphere_driver.cpp
+++ components/scream/src/control/atmosphere_driver.cpp
@@ -124,7 +124,9 @@ void AtmosphereDriver::create_atm_processes()
   // Create the group of processes. This will recursively create the processes
   // tree, storing also the information regarding parallel execution (if needed).
   // See AtmosphereProcessGroup class documentation for more details.
-  m_atm_process_group = std::make_shared<AtmosphereProcessGroup>(m_atm_comm,m_atm_params.sublist("Atmosphere Processes"));
+  auto& atm_proc_params = m_atm_params.sublist("Atmosphere Processes");
+  atm_proc_params.set("Logger",m_atm_logger);
+  m_atm_process_group = std::make_shared<AtmosphereProcessGroup>(m_atm_comm,atm_proc_params);
 
   m_ad_status |= s_procs_created;
 }
