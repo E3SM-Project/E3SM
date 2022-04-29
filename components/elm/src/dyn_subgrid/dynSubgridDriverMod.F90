fe4c9de077c07209feba346a7ab492d047ede16b
Tim Shippert <tim.shippert@pnnl.gov>
2022-04-29 10:19:04 -0700

    Added the max of monthly mean calculations to the driver section, including a new monthly alarm and a new max function for avects, and adding the additional accum avg and max calls to the main processing loop.  Also, some cleanup of old debug messages.

--- components/elm/src/dyn_subgrid/dynSubgridDriverMod.F90
+++ components/elm/src/dyn_subgrid/dynSubgridDriverMod.F90
@@ -43,13 +43,6 @@ module dynSubgridDriverMod
   use ColumnDataType      , only : column_carbon_state, column_carbon_flux
   use ColumnDataType      , only : col_ns, col_ps
   use VegetationDataType  , only : vegetation_carbon_state, veg_ns, veg_ps
-  use clm_varctl          , only : iac_active
-  use shr_log_mod           , only : errMsg => shr_log_errMsg
-  use decompMod             , only : bounds_type, BOUNDS_LEVEL_PROC
-  use shr_kind_mod , only : r8 => shr_kind_r8
-  use ColumnType   , only : col_pp
-  use VegetationType    , only : veg_pp
-  use clm_varctl, only :  iulog
 
   !
   ! !PUBLIC MEMBER FUNCTIONS:
@@ -60,11 +53,6 @@ module dynSubgridDriverMod
   public :: dynSubgrid_init             ! initialize transient land cover
   public :: dynSubgrid_driver           ! top-level driver for transient land cover
   public :: dynSubgrid_wrapup_weight_changes ! reconcile various variables after subgrid weights change
-
-  ! !PRIVATE MEMBER FUNCTIONS:
-  private :: dyn_iac_init          ! init for iac instead of files
-  private :: set_iac_veg_weights   ! calc veg_pp weights with iac
-
   !
   ! !PRIVATE TYPES:
   ! saved weights from before the subgrid weight updates
@@ -75,7 +63,6 @@ module dynSubgridDriverMod
 
   ! object used to update column-level states after subgrid weight updates
   type(column_state_updater_type), target :: column_state_updater
-
   !---------------------------------------------------------------------------
 
 contains
@@ -134,11 +121,6 @@ contains
        call dyncrop_init(bounds, dyncrop_filename=get_flanduse_timeseries())
     end if
 
-    ! Initialize for iac instead of files
-    if (iac_active) then
-       call dyn_iac_init(bounds)
-    end if
-
     ! ------------------------------------------------------------------------
     ! Set initial subgrid weights for aspects that are read from file. This is relevant
     ! for cold start and use_init_interp-based initialization.
@@ -184,7 +166,7 @@ contains
     ! OUTSIDE any loops over clumps in the driver.
     !
     ! !USES:
-    use elm_varctl           , only : use_cn, create_glacier_mec_landunit, use_fates, iac_active
+    use clm_varctl           , only : use_cn, create_glacier_mec_landunit, use_fates, iac_active
     use decompMod            , only : bounds_type, get_proc_clumps, get_clump_bounds
     use decompMod            , only : BOUNDS_LEVEL_PROC
     use dynInitColumnsMod    , only : initialize_new_columns
@@ -198,6 +180,11 @@ contains
     use CarbonStateUpdate1Mod   , only : CarbonStateUpdateDynPatch
     use NitrogenStateUpdate1Mod   , only : NitrogenStateUpdateDynPatch
     use PhosphorusStateUpdate1Mod     , only : PhosphorusStateUpdateDynPatch
+
+! avd
+use clm_varctl, only :  iulog
+!use shr_sys_mod            , only : shr_sys_flush
+
     !
     ! !ARGUMENTS:
     type(bounds_type)        , intent(in)    :: bounds_proc  ! processor-level bounds
@@ -287,12 +274,7 @@ contains
     ! avd - the above namelist values are false in this case
     !       may want to ensure this with namelist checks
 
-!write(iulog,*) 'Before iac_active check=', iac_active, ' and iac2lnd_vars update' 
-
     if (iac_active) then
-
-!write(iulog,*) 'Calling iac2lnd_vars update'
-
        call iac2lnd_vars%update_iac2lnd(bounds_proc)
     end if
 
@@ -396,14 +378,6 @@ contains
 
     call compute_higher_order_weights(bounds_clump)
 
-    if (iac_active) then
-       ! make sure weights are all in order before applying the new pft wts
-       ! this is because the active elements need to be correct
-       call reweight_wrapup(bounds_clump, &
-            glc2lnd_vars%icemask_grc(bounds_clump%begg:bounds_clump%endg))
-       call set_iac_veg_weights(bounds_clump)
-    end if   
-
     ! Here: filters are re-made
     !
     ! This call requires clump-level bounds, which is why we need to ensure that the
@@ -411,164 +385,5 @@ contains
     call reweight_wrapup(bounds_clump, glc2lnd_vars%icemask_grc(bounds_clump%begg:bounds_clump%endg))
 
   end subroutine dynSubgrid_wrapup_weight_changes
-
-  !-----------------------------------------------------------------------
-  subroutine dyn_iac_init(bounds)
-    !
-    ! !DESCRIPTION:
-    ! Initialize data structures for harvest information and check pft #s
-    ! Copies initial weights into iac weight array
-    ! This should be called once, during model initialization.
-    ! !USES:
-    use dynHarvestMod   , only : harvest, do_cn_harvest
-    use clm_varpar     , only : numpft, maxpatch_pft
-    use abortutils          , only : endrun
-    !
-    ! !ARGUMENTS:
-    type(bounds_type), intent(in) :: bounds           ! proc-level bounds
-    !
-    ! !LOCAL VARIABLES:
-    integer :: ier        ! error code
-    integer :: p
-
-    character(len=*), parameter :: subname = 'dyn_iac_init'
-  !-----------------------------------------------------------------------
-
-    SHR_ASSERT_ALL(bounds%level == BOUNDS_LEVEL_PROC, subname // &
-                     ': argument must be PROC-level bounds')
-
-    allocate(harvest(bounds%begg:bounds%endg),stat=ier)
-    if (ier /= 0) then
-       call endrun(msg=' allocation error for harvest'//errMsg(__FILE__, &
-                          __LINE__))
-    end if
-
-    harvest(:) = 0._r8
-
-    if ( maxpatch_pft /= numpft+1 ) then
-       call endrun(msg=' maxpatch_pft does NOT equal numpft+1'// &
-                   ' -- this is invalid for dynamic PFT case'// &
-                   'for dynamic PFT case'// &
-                   errMsg(__FILE__, __LINE__) )
-    end if
-
-    ! set the cn harveset flag
-    do_cn_harvest = .true.
-
-    ! fill the iac weights with initial values
-    ! so that dynsubgrid_wrapup_weight_changes works in init
-    do p = bounds%begp, bounds%endp
-       veg_pp%wtgcell_iac(p) = veg_pp%wtgcell(p)
-    end do
-
-  end subroutine dyn_iac_init
-
-  !-----------------------------------------------------------------------
-  subroutine set_iac_veg_weights(bounds)
-    !
-    ! !DESCRIPTION:
-    ! Set veg_pp weights from veg_pp%wtgcell_iac
-    ! The wtcol value sums need to be normalized to 1
-    ! This is called after all other weights have been updated,
-    !    and before checks, if the iac is active
-    !
-    ! !USES:
-    !
-    ! !ARGUMENTS:
-    implicit none
-    type(bounds_type), intent(in) :: bounds  ! clump bounds
-    !
-    ! !LOCAL VARIABLES:
-    integer :: p, c, l, t, g      ! indices for pft, col, lu, topo, gcell
-    real(r8), allocatable :: sumwtcol(:), sumwtlunit(:), sumwttopounit(:), &
-                             sumwtgcell(:)
-    character(len=*), parameter :: subname = 'set_iac_veg_weights'
-    logical :: weights_equal_1
-    real(r8), parameter :: tolerance = 1.e-12_r8  ! tolerance for weight sums
-  !-----------------------------------------------------------------------
-
-    allocate(sumwtcol(bounds%begc:bounds%endc))
-    sumwtcol(bounds%begc : bounds%endc) = 0._r8
-    allocate(sumwtlunit(bounds%begl:bounds%endl))
-    sumwtlunit(bounds%begl : bounds%endl) = 0._r8
-    allocate(sumwttopounit(bounds%begt:bounds%endt))
-    sumwttopounit(bounds%begt : bounds%endt) = 0._r8
-    allocate(sumwtgcell(bounds%begg:bounds%endg))
-    sumwtgcell(bounds%begg : bounds%endg) = 0._r8
-
-    ! First calc the wtcol and sum the values
-    ! fill bare pft value if col wtgcell is zero
-    ! calculate the higher order wts
-    do p = bounds%begp, bounds%endp
-       c = veg_pp%column(p)
-       l = veg_pp%landunit(p)
-       t = veg_pp%topounit(p)
-       g = veg_pp%gridcell(p)
-       if (col_pp%wtgcell(c) .eq. 0._r8) then
-          !write(iulog,*) subname//'setting wtcol to zero' // &
-           !              'for p, c, l, g ', p,c,l,g
-          if (veg_pp%itype(p) .eq. 0) then
-             veg_pp%wtcol(p) = 1._r8
-          else
-             veg_pp%wtcol(p) = 0._r8
-          end if
-       else
-          veg_pp%wtcol(p) = veg_pp%wtgcell_iac(p) / col_pp%wtgcell(c)
-       end if
-       veg_pp%wtlunit(p)    = veg_pp%wtcol(p) * col_pp%wtlunit(c)
-       veg_pp%wttopounit(p) = veg_pp%wtcol(p) * col_pp%wttopounit(c)
-       veg_pp%wtgcell(p)    = veg_pp%wtcol(p) * col_pp%wtgcell(c)
-       ! get the sums for normalizing below
-       sumwtcol(c) = sumwtcol(c) + veg_pp%wtcol(p)
-       !sumwtlunit(l) = sumwtlunit(l) + veg_pp%wtlunit(p)
-       !sumwttopounit(t) = sumwttopounit(t) + veg_pp%wttopounit(p)
-       !sumwtgcell(g) = sumwtgcell(g) + veg_pp%wtgcell(p)
-    end do
-
-    ! Normalize the wtcol if necessary and recalc higher order weights
-    do c = bounds%begc,bounds%endc
-       weights_equal_1 = (abs(sumwtcol(c) - 1._r8) <= tolerance)
-       if (.not.weights_equal_1) then
-          !write(iulog,*) subname//'weights not equal 1, sumwtcol = ', &
-                !           sumwtcol(c), 'c= ', c
-          do p = bounds%begp, bounds%endp
-             if (veg_pp%column(p) .eq. c) then
-                if (sumwtcol(c) .eq. 0._r8) then
-                   write(iulog,*) subname//'setting wtcol to zero'// &
-                                  'for p, c, l, g ', p,c,l,g
-                   veg_pp%wtcol(p) = 0._r8
-                else
-                   veg_pp%wtcol(p) = veg_pp%wtcol(p) / sumwtcol(c)
-                end if
-                veg_pp%wtlunit(p)    = veg_pp%wtcol(p) * col_pp%wtlunit(c)
-                veg_pp%wttopounit(p) = veg_pp%wtcol(p) * col_pp%wttopounit(c)
-                veg_pp%wtgcell(p)    = veg_pp%wtcol(p) * col_pp%wtgcell(c)
-
-
-                ! get the higher order sums
-                !l = veg_pp%landunit(p)
-                !t = veg_pp%topounit(p)
-                !g = veg_pp%gridcell(p)
-                !sumwtlunit(l) = sumwtlunit(l) + veg_pp%wtlunit(p)
-                !sumwttopounit(t) = sumwttopounit(t) + veg_pp%wttopounit(p)
-                !sumwtgcell(g) = sumwtgcell(g) + veg_pp%wtgcell(p)
-
-             end if
-          end do
-       end if
-    end do
-
-    ! Normalize the wtlunit if necessary and recalc higher order weights
-
-    ! Normalize the wttopounit if necessary and recalc higher order weights
-
-    ! Normalize the wtgcell if necessary and recalc higher order weights
-
-    deallocate(sumwtcol)
-    deallocate(sumwtlunit)
-    deallocate(sumwttopounit)
-    deallocate(sumwtgcell)
-
-  endsubroutine set_iac_veg_weights
 
 end module dynSubgridDriverMod
