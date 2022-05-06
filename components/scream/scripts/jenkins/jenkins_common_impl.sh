fabcc27bde5a2a4d435bb042898d2c2d38ce148f
Luca Bertagna <lbertag@sandia.gov>
2022-05-06 16:41:41 -0600

    SCREAM: make the AT test v1 by default

--- components/scream/scripts/jenkins/jenkins_common_impl.sh
+++ components/scream/scripts/jenkins/jenkins_common_impl.sh
@@ -7,7 +7,7 @@ IFS=';' read -r -a labels <<< "$PR_LABELS";
 skip_testing=0
 test_scripts=0
 test_v0=0
-test_v1=0
+test_v1=1
 test_SA=1
 skip_mappy=0
 skip_weaver=0
@@ -20,15 +20,12 @@ if [ ${#labels[@]} -gt 0 ]; then
       skip_testing=1
     elif [ "$label" == "AT: Skip Stand-Alone Testing" ]; then
       test_SA=0
+    elif [ "$label" == "AT: Skip v1 Testing" ]; then
+      test_v1=0
     elif [ "$label" == "scripts" ]; then
       test_scripts=1
-    elif [ "$label" == "SCREAMv1" ]; then
-      test_v1=1
     elif [ "$label" == "SCREAMv0" ]; then
       test_v0=1
-    elif [ "$label" == "CIME" ]; then
-      test_v0=1
-      test_v1=1
     elif [ "$label" == "AT: Skip mappy" ]; then
       skip_mappy=1
     elif [ "$label" == "AT: Skip weaver" ]; then
