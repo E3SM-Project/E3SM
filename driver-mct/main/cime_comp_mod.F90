023fb93f392b55e103b0c9b5fb98d93ee1bd849e
Jon Wolfe <jonbob@lanl.gov>
2022-05-11 16:59:52 -0500

    Reorder logic with do_bgc_budgets as outer check

--- driver-mct/main/cime_comp_mod.F90
+++ driver-mct/main/cime_comp_mod.F90
@@ -4655,13 +4655,15 @@ contains
        endif
        if (rof_present) then
           call seq_diag_rof_mct(rof(ens1), fractions_rx(ens1), infodata)
-          if (do_bgc_budgets) then
-             call seq_diagBGC_rof_mct(rof(ens1), fractions_rx(ens1), infodata)
-          endif
        endif
        if (ice_present) then
           call seq_diag_ice_mct(ice(ens1), fractions_ix(ens1), infodata, do_x2i=.true.)
-          if (do_bgc_budgets) then
+       endif
+       if (do_bgc_budgets) then
+          if (rof_present) then
+             call seq_diagBGC_rof_mct(rof(ens1), fractions_rx(ens1), infodata)
+          endif
+          if (ice_present) then
              call seq_diagBGC_ice_mct(ice(ens1), fractions_ix(ens1), infodata, do_x2i=.true.)
           endif
        endif
@@ -4695,23 +4697,21 @@ contains
        call t_drvstartf ('CPL:BUDGET2',cplrun=lcplrun,budget=.true.,barrier=mpicom_CPLID)
        if (atm_present) then
           call seq_diag_atm_mct(atm(ens1), fractions_ax(ens1), infodata, do_a2x=.true., do_x2a=.true.)
-          if (do_bgc_budgets) then
-             call seq_diagBGC_atm_mct(atm(ens1), fractions_ax(ens1), infodata, do_a2x=.true., do_x2a=.true.)
-          endif
        endif
        if (ice_present) then
           call seq_diag_ice_mct(ice(ens1), fractions_ix(ens1), infodata, do_i2x=.true.)
-          if (do_bgc_budgets) then
+       endif
+       if (do_bgc_budgets) then
+          if (atm_present) then
+             call seq_diagBGC_atm_mct(atm(ens1), fractions_ax(ens1), infodata, do_a2x=.true., do_x2a=.true.)
+          endif
+          if (ice_present) then
              call seq_diagBGC_ice_mct(ice(ens1), fractions_ix(ens1), infodata, do_i2x=.true.)
           endif
-       endif
-       if (lnd_present) then
-          if (do_bgc_budgets) then
+          if (lnd_present) then
              call seq_diagBGC_lnd_mct(lnd(ens1), fractions_lx(ens1), infodata, do_l2x=.true., do_x2l=.true.)
           endif
-       endif
-       if (ocn_present) then
-          if (do_bgc_budgets) then
+          if (ocn_present) then
              call seq_diagBGC_ocn_mct(ocn(ens1), xao_ox(1), fractions_ox(ens1), infodata, &
                   do_o2x=.true., do_x2o=.true., do_xao=.true.)
           endif
