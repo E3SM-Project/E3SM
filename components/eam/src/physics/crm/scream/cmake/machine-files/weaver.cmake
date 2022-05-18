0c717aec37f17b66f3a8d5cf15de99b160dd41b1
Walter Hannah <whannah1@gmail.com>
2022-05-18 10:10:41 -0600

    Add copy of SCREAM data

---
+++ components/eam/src/physics/crm/scream/cmake/machine-files/weaver.cmake
@@ -0,0 +1,6 @@
+# Load all kokkos settings from Ekat's mach file
+set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
+include (${EKAT_MACH_FILES_PATH}/weaver.cmake)
+set (BLAS_LIBRARIES /ascldap/users/projects/e3sm/scream/libs/openblas/install/weaver/gcc/8.5.0/lib/libopenblas.so CACHE STRING "")
+set (LAPACK_LIBRARIES /ascldap/users/projects/e3sm/scream/libs/openblas/install/weaver/gcc/8.5.0/lib/libopenblas.so CACHE STRING "")
+set(SCREAM_INPUT_ROOT "/home/projects/e3sm/scream/data" CACHE STRING "")
