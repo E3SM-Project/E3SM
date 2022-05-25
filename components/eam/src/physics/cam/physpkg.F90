24e5eaf2b96e305469b1e2b998a28aadbf515943
Hui Wan <hui.wan@pnnl.gov>
2022-05-25 07:12:41 -0700

    EAM stealth feature: revised aerosol process coupling
    In the released v2, the tracer mixing ratio tendencies caused by
    surface fluxes (cam_in%clfx) are calculated in tphysac,
    inside the subroutine clubb_surface.
    These tendencies are applied after clubb_surface before
    the dry removal of aerosols.
    
    In this commit, an option is added to move the calculation of
    cflx-induced tendencies and the update of tracer mixing ratios
    to tphysbc right before the cloud macro-microphysics subcycles.
    
    The new option can be turned on by setting namelist variable
    cflx_cpl_opt = 2.
    
    [BFB] (unless cflx_cpl_opt = 2)

--- components/eam/src/physics/cam/physpkg.F90
+++ components/eam/src/physics/cam/physpkg.F90
@@ -1561,6 +1561,10 @@ subroutine tphysac (ztodt,   cam_in,  &
     logical :: l_gw_drag
     logical :: l_ac_energy_chk
 
+    ! Numerical schemes for process coupling
+
+    integer :: cflx_cpl_opt   ! When to apply surface tracer fluxes
+
     !
     !-----------------------------------------------------------------------
     !
@@ -1572,6 +1576,7 @@ subroutine tphysac (ztodt,   cam_in,  &
     call phys_getopts( do_clubb_sgs_out       = do_clubb_sgs, &
                        state_debug_checks_out = state_debug_checks &
                       ,deep_scheme_out        = deep_scheme        &
+                      ,cflx_cpl_opt_out       = cflx_cpl_opt       &
                       ,l_tracer_aero_out      = l_tracer_aero      &
                       ,l_vdiff_out            = l_vdiff            &
                       ,l_rayleigh_out         = l_rayleigh         &
@@ -1699,9 +1704,11 @@ end if ! l_tracer_aero
        ! Calculate surface friction velocity (ustar) and Obukhov length
        call clubb_surface ( state, cam_in, surfric, obklen)
 
-       ! Diagnose tracer mixing ratio tendencies from surface fluxes, then update the mixing ratios
-       call cflx_tend( state, cam_in, ztodt, ptend)
-       call physics_update(state, ptend, ztodt, tend)
+       if (cflx_cpl_opt==1) then
+          ! Diagnose tracer mixing ratio tendencies from surface fluxes, then update the mixing ratios
+          call cflx_tend( state, cam_in, ztodt, ptend)
+          call physics_update(state, ptend, ztodt, tend)
+       end if
 
     else
     if (l_vdiff) then
@@ -1967,6 +1974,7 @@ subroutine tphysbc (ztodt,               &
     use phys_control,    only: use_qqflx_fixer, use_mass_borrower
     use nudging,         only: Nudge_Model,Nudge_Loc_PhysOut,nudging_calc_tend
     use lnd_infodata,    only: precip_downscaling_method
+    use cflx,            only: cflx_tend
 
     implicit none
 
@@ -2120,12 +2128,18 @@ subroutine tphysbc (ztodt,               &
     logical :: l_rad
     !HuiWan (2014/15): added for a short-term time step convergence test ==
 
+    ! Numerical schemes for process coupling
+
+    integer :: cflx_cpl_opt   ! When to apply surface tracer fluxes
+
+    !--------------------------------------------------------------------
 
     call phys_getopts( microp_scheme_out      = microp_scheme, &
                        macrop_scheme_out      = macrop_scheme, &
                        use_subcol_microp_out  = use_subcol_microp, &
                        deep_scheme_out        = deep_scheme,       &
                        state_debug_checks_out = state_debug_checks &
+                      ,cflx_cpl_opt_out       = cflx_cpl_opt       &
                       ,l_bc_energy_fix_out    = l_bc_energy_fix    &
                       ,l_dry_adj_out          = l_dry_adj          &
                       ,l_tracer_aero_out      = l_tracer_aero      &
@@ -2455,6 +2469,17 @@ end if
      end if !l_st_mac
 
     elseif( microp_scheme == 'MG' ) then
+
+       !---------------------------------------------------------------------------------------
+       ! Apply surface tracer fluxes to update tracer mixing ratios before turbulent tranport
+       !---------------------------------------------------------------------------------------
+       if (cflx_cpl_opt==2) then
+          ! Diagnose tracer mixing ratio tendencies from surface fluxes, then update the mixing ratios
+          call cflx_tend( state, cam_in, ztodt, ptend)
+          call physics_update(state, ptend, ztodt, tend)
+       end if
+
+       !-------------------------------------------------------------------
        ! Start co-substepping of macrophysics and microphysics
        cld_macmic_ztodt = ztodt/cld_macmic_num_steps
 
