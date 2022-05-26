00e8228d730337154a52f73a62f0dc168e8f0207
Walter Hannah <whannah1@gmail.com>
2022-05-26 14:19:02 -0600

    update ice_fall

--- components/eam/src/physics/crm/samxx/ice_fall.cpp
+++ components/eam/src/physics/crm/samxx/ice_fall.cpp
@@ -1,5 +1,6 @@
 
 #include "ice_fall.h"
+#include "samxx_utils.h"
 
 void ice_fall() {
   YAKL_SCOPE( qcl           , :: qcl );
@@ -16,10 +17,15 @@ void ice_fall() {
   YAKL_SCOPE( ncrms         , :: ncrms );
   YAKL_SCOPE( precsfc       , :: precsfc );
   YAKL_SCOPE( precssfc      , :: precssfc );
+  YAKL_SCOPE( microphysics_scheme , :: microphysics_scheme);
 
   int1d  kmax("kmax",ncrms);
   int1d  kmin("kmin",ncrms);
   real4d fz  ("fz"  ,nz,ny,nx,ncrms);
+
+  int index_cloud_ice = -1;
+  if (is_same_str(microphysics_scheme, "sam1mom") == 0) { index_cloud_ice = 0; }
+  if(index_cloud_ice == -1) { return;}
 
   // for (int icrm=0; icrm<ncrms; icrm++) {
   parallel_for( ncrms , YAKL_LAMBDA (int icrm) {
@@ -45,8 +51,6 @@ void ice_fall() {
     qifall(k,icrm) = 0.0;
     tlatqi(k,icrm) = 0.0;
   });
-
-  if(index_cloud_ice == -1) { return;}
 
   // for (int k=0; k<nzm; k++) {
   //   for (int j=0; j<ny; j++) {
@@ -107,8 +111,6 @@ void ice_fall() {
     fz(nz-1,j,i,icrm) = 0.0;
   });
 
-  int constexpr ici = index_cloud_ice;
-
   // for (int k=0; k<nzm; k++) {
   //   for (int j=0; j<ny; j++) {
   //     for (int i=0; i<nx; i++) {
@@ -119,7 +121,7 @@ void ice_fall() {
       // The cloud ice increment is the difference of the fluxes.
       real dqi  = coef*(fz(k,j,i,icrm)-fz(k+1,j,i,icrm));
       // Add this increment to both non-precipitating and total water.
-      micro_field(ici,k,offy_s+j,offx_s+i,icrm) += dqi;
+      micro_field(index_cloud_ice,k,offy_s+j,offx_s+i,icrm) += dqi;
       // Include this effect in the total moisture budget.
       //$acc atomic update
       yakl::atomicAdd(qifall(k,icrm),dqi);
