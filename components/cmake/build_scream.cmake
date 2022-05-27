ce1cab7f5d974f83f7bf7b382ed2975f61d59ead
Xingqiu Yuan <xyuan@anl.gov>
2022-05-27 17:31:03 -0400

    keep the use_cuda as gpu run flag

--- components/cmake/build_scream.cmake
+++ components/cmake/build_scream.cmake
@@ -6,9 +6,12 @@ function(build_scream)
   if (COMP_NAMES MATCHES ".*scream.*")
 
     # Include machine file here
-    message("Found scream component")
-    include(${CMAKE_SOURCE_DIR}/scream/cmake/machine-files/${MACH}.cmake)
-
+    message("Found scream component, USE_CUDA ${USE_CUDA}")
+    if (USE_CUDA)
+      include(${CMAKE_SOURCE_DIR}/scream/cmake/machine-files/${MACH}.cmake)
+    else()
+      include(${CMAKE_SOURCE_DIR}/scream/cmake/machine-files/${MACH}-cpu.cmake)
+    endif()
     add_subdirectory("scream")
   endif()
 
