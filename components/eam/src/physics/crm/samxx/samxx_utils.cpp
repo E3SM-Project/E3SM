8b8457eff664f4c0d333b74d2c12737fa5ba4644
Walter Hannah <whannah1@gmail.com>
2022-05-31 15:37:12 -0400

    update samxx_utils

--- components/eam/src/physics/crm/samxx/samxx_utils.cpp
+++ components/eam/src/physics/crm/samxx/samxx_utils.cpp
@@ -3,14 +3,14 @@
 
 // scream session initialize/finalize
 extern "C" {
- void scream_session_init()
- {
-   scream::initialize_scream_session();
- }
+  void scream_session_init()
+  {
+    // scream::initialize_scream_session();
+  }
 
- void scream_session_finalize()
- {
-  scream::finalize_scream_session();
- }
+  void scream_session_finalize()
+  {
+    // scream::finalize_scream_session();
+  }
 }
 
--- components/eam/src/physics/crm/samxx/samxx_utils.h
+++ components/eam/src/physics/crm/samxx/samxx_utils.h
@@ -5,10 +5,10 @@
 
 #include "samxx_const.h"
 #include "vars.h"
-#include "ekat/kokkos/ekat_kokkos_utils.hpp"
-#include "ekat/ekat_pack_kokkos.hpp"
-#include "ekat/ekat_assert.hpp"
-#include "scream_session.hpp"
+// #include "ekat/kokkos/ekat_kokkos_utils.hpp"
+// #include "ekat/ekat_pack_kokkos.hpp"
+// #include "ekat/ekat_assert.hpp"
+// #include "scream_session.hpp"
 
 // for kokkos debuge only
 #if defined(DEBUG)
@@ -38,231 +38,231 @@ YAKL_INLINE int is_same_str(const char *str_a, const char *str_b)
   return match;
 }
 
-// convert YAKL multidimensional array to Kokkos view on GPU
-template <typename SizeT, typename ViewT>
-void array_to_view(const typename ViewT::value_type::scalar* const data,
-                   const SizeT& size,
-                   ViewT& view)
-{
-  using PackT = typename ViewT::value_type;
-  EKAT_ASSERT(PackT::n >= 1);
+// // convert YAKL multidimensional array to Kokkos view on GPU
+// template <typename SizeT, typename ViewT>
+// void array_to_view(const typename ViewT::value_type::scalar* const data,
+//                    const SizeT& size,
+//                    ViewT& view)
+// {
+//   using PackT = typename ViewT::value_type;
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const size_t npack = (size + PackT::n-1)/PackT::n;
+//   const size_t npack = (size + PackT::n-1)/PackT::n;
 
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {npack, PackT::n}), KOKKOS_LAMBDA(int k, int s) {
-    const size_t scalar_offset = k*PackT::n;
-    if (scalar_offset+s < size) view(k)[s] = data[scalar_offset+s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {npack, PackT::n}), KOKKOS_LAMBDA(int k, int s) {
+//     const size_t scalar_offset = k*PackT::n;
+//     if (scalar_offset+s < size) view(k)[s] = data[scalar_offset+s];
+//   });
+// }
 
-// 2D YAKL array to Kokkos view
-template <typename SizeT, typename ViewT>
-void array_to_view(const typename ViewT::value_type::scalar* const data,
-                   const SizeT& dim1_size,
-                   const SizeT& dim2_size,
-                   ViewT& view)
-{
-  using PackT = typename ViewT::value_type;
-  EKAT_ASSERT(PackT::n >= 1);
+// // 2D YAKL array to Kokkos view
+// template <typename SizeT, typename ViewT>
+// void array_to_view(const typename ViewT::value_type::scalar* const data,
+//                    const SizeT& dim1_size,
+//                    const SizeT& dim2_size,
+//                    ViewT& view)
+// {
+//   using PackT = typename ViewT::value_type;
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const int pack_size = static_cast<int>(PackT::n);
-  const int npack     = (dim2_size+pack_size-1)/pack_size;
+//   const int pack_size = static_cast<int>(PackT::n);
+//   const int npack     = (dim2_size+pack_size-1)/pack_size;
 
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {dim1_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int k, int s) {
-     const int num_scalars = k*pack_size;
-     const int scalar_offset = i*dim2_size+num_scalars;
-     if (num_scalars+s<dim2_size)  view(i,k)[s] = data[scalar_offset+s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {dim1_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int k, int s) {
+//      const int num_scalars = k*pack_size;
+//      const int scalar_offset = i*dim2_size+num_scalars;
+//      if (num_scalars+s<dim2_size)  view(i,k)[s] = data[scalar_offset+s];
+//   });
+// }
 
-// 3D YAKL to Kokkos views
-template <typename SizeT, typename ViewT>
-void array_to_view(const typename ViewT::value_type::scalar* const data,
-                   const SizeT& dim1_size,
-                   const SizeT& dim2_size,
-                   const SizeT& dim3_size,
-                   ViewT& view)
-{
-  using PackT = typename ViewT::value_type;
-  EKAT_ASSERT(PackT::n >= 1);
+// // 3D YAKL to Kokkos views
+// template <typename SizeT, typename ViewT>
+// void array_to_view(const typename ViewT::value_type::scalar* const data,
+//                    const SizeT& dim1_size,
+//                    const SizeT& dim2_size,
+//                    const SizeT& dim3_size,
+//                    ViewT& view)
+// {
+//   using PackT = typename ViewT::value_type;
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const int pack_size = static_cast<int>(PackT::n);
-  const int npack     = (dim3_size+pack_size-1)/pack_size;
+//   const int pack_size = static_cast<int>(PackT::n);
+//   const int npack     = (dim3_size+pack_size-1)/pack_size;
 
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<4>>({0, 0, 0, 0}, {dim1_size, dim2_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int s) {
-     const int num_scalars = k*pack_size;
-     const int scalar_offset = (i*dim2_size+j)*dim3_size+num_scalars;
-     if (num_scalars+s<dim3_size) view(i,j,k)[s] = data[scalar_offset+s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<4>>({0, 0, 0, 0}, {dim1_size, dim2_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int s) {
+//      const int num_scalars = k*pack_size;
+//      const int scalar_offset = (i*dim2_size+j)*dim3_size+num_scalars;
+//      if (num_scalars+s<dim3_size) view(i,j,k)[s] = data[scalar_offset+s];
+//   });
+// }
 
-// 4D YAKL to Kokkos views
-template <typename SizeT, typename ViewT>
-void array_to_view(const typename ViewT::value_type::scalar* const data,
-                   const SizeT& dim1_size,
-                   const SizeT& dim2_size,
-                   const SizeT& dim3_size,
-                   const SizeT& dim4_size,
-                   ViewT& view)
-{
-  using PackT = typename ViewT::value_type;
-  EKAT_ASSERT(PackT::n >= 1);
+// // 4D YAKL to Kokkos views
+// template <typename SizeT, typename ViewT>
+// void array_to_view(const typename ViewT::value_type::scalar* const data,
+//                    const SizeT& dim1_size,
+//                    const SizeT& dim2_size,
+//                    const SizeT& dim3_size,
+//                    const SizeT& dim4_size,
+//                    ViewT& view)
+// {
+//   using PackT = typename ViewT::value_type;
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const int pack_size = static_cast<int>(PackT::n);
-  const int npack     = (dim3_size+pack_size-1)/pack_size;
+//   const int pack_size = static_cast<int>(PackT::n);
+//   const int npack     = (dim3_size+pack_size-1)/pack_size;
 
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<5>>({0, 0, 0, 0, 0}, {dim1_size, dim2_size, dim3_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int l, int s) {
-     const int num_scalars = k*pack_size;
-     const int scalar_offset = ((i*dim2_size+j)*dim3_size+l)*dim4_size+num_scalars;
-     if (num_scalars+s<dim4_size) view(i,j,k,l)[s] = data[scalar_offset+s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<5>>({0, 0, 0, 0, 0}, {dim1_size, dim2_size, dim3_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int l, int s) {
+//      const int num_scalars = k*pack_size;
+//      const int scalar_offset = ((i*dim2_size+j)*dim3_size+l)*dim4_size+num_scalars;
+//      if (num_scalars+s<dim4_size) view(i,j,k,l)[s] = data[scalar_offset+s];
+//   });
+// }
 
-// 5D YAKL to Kokkos views
-template <typename SizeT, typename ViewT>
-void array_to_view(const typename ViewT::value_type::scalar* const data,
-                   const SizeT& dim1_size,
-                   const SizeT& dim2_size,
-                   const SizeT& dim3_size,
-                   const SizeT& dim4_size,
-                   const SizeT& dim5_size,
-                   ViewT& view)
-{
-  using PackT = typename ViewT::value_type;
-  EKAT_ASSERT(PackT::n >= 1);
+// // 5D YAKL to Kokkos views
+// template <typename SizeT, typename ViewT>
+// void array_to_view(const typename ViewT::value_type::scalar* const data,
+//                    const SizeT& dim1_size,
+//                    const SizeT& dim2_size,
+//                    const SizeT& dim3_size,
+//                    const SizeT& dim4_size,
+//                    const SizeT& dim5_size,
+//                    ViewT& view)
+// {
+//   using PackT = typename ViewT::value_type;
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const int pack_size = static_cast<int>(PackT::n);
-  const int npack     = (dim3_size+pack_size-1)/pack_size;
+//   const int pack_size = static_cast<int>(PackT::n);
+//   const int npack     = (dim3_size+pack_size-1)/pack_size;
 
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<6>>({0, 0, 0, 0, 0, 0}, {dim1_size, dim2_size, dim3_size, dim4_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int l, int m, int s) {
-     const int num_scalars = k*pack_size;
-     const int scalar_offset = (((i*dim2_size+j)*dim3_size+l)*dim4_size+m)*dim5_size+num_scalars;
-     if (num_scalars+s<dim5_size) view(i,j,k,l,m)[s] = data[scalar_offset+s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<6>>({0, 0, 0, 0, 0, 0}, {dim1_size, dim2_size, dim3_size, dim4_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int l, int m, int s) {
+//      const int num_scalars = k*pack_size;
+//      const int scalar_offset = (((i*dim2_size+j)*dim3_size+l)*dim4_size+m)*dim5_size+num_scalars;
+//      if (num_scalars+s<dim5_size) view(i,j,k,l,m)[s] = data[scalar_offset+s];
+//   });
+// }
 
-// 1D Kokkos view to YAKL array
-template <typename SizeT, typename ViewT, typename ArrayT>
-void view_to_array(const ViewT& view,
-                   const SizeT& size,
-                   ArrayT& array)
-{
-  using PackT      = typename ViewT::value_type;
-  using scalarType = typename ViewT::value_type::scalar;
-  using arrayType  = typename ArrayT::type;
+// // 1D Kokkos view to YAKL array
+// template <typename SizeT, typename ViewT, typename ArrayT>
+// void view_to_array(const ViewT& view,
+//                    const SizeT& size,
+//                    ArrayT& array)
+// {
+//   using PackT      = typename ViewT::value_type;
+//   using scalarType = typename ViewT::value_type::scalar;
+//   using arrayType  = typename ArrayT::type;
 
-  auto is_same_type = std::is_same<scalarType, arrayType>::value;
+//   auto is_same_type = std::is_same<scalarType, arrayType>::value;
 
-  EKAT_ASSERT(is_same_type);
-  EKAT_ASSERT(PackT::n >= 1);
+//   EKAT_ASSERT(is_same_type);
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const size_t npack = (size + PackT::n-1)/PackT::n;
+//   const size_t npack = (size + PackT::n-1)/PackT::n;
     
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {npack, PackT::n}), KOKKOS_LAMBDA(int k, int s) {
-      const size_t scalar_offset = k*PackT::n;
-      if (scalar_offset+s < size) array.myData[scalar_offset+s] = view(k)[s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {npack, PackT::n}), KOKKOS_LAMBDA(int k, int s) {
+//       const size_t scalar_offset = k*PackT::n;
+//       if (scalar_offset+s < size) array.myData[scalar_offset+s] = view(k)[s];
+//   });
+// }
 
-// 2D Kokkos view to YAKL array
-template <typename SizeT, typename ViewT, typename ArrayT>
-void view_to_array(const ViewT& view,
-                   const SizeT& dim1_size,
-                   const SizeT& dim2_size,
-                   ArrayT& array)
-{
-  using PackT      = typename ViewT::value_type;
-  using scalarType = typename ViewT::value_type::scalar;
-  using arrayType  = typename ArrayT::type;
+// // 2D Kokkos view to YAKL array
+// template <typename SizeT, typename ViewT, typename ArrayT>
+// void view_to_array(const ViewT& view,
+//                    const SizeT& dim1_size,
+//                    const SizeT& dim2_size,
+//                    ArrayT& array)
+// {
+//   using PackT      = typename ViewT::value_type;
+//   using scalarType = typename ViewT::value_type::scalar;
+//   using arrayType  = typename ArrayT::type;
  
-  auto is_same_type = std::is_same<scalarType, arrayType>::value;
+//   auto is_same_type = std::is_same<scalarType, arrayType>::value;
 
-  EKAT_ASSERT(is_same_type);
-  EKAT_ASSERT(PackT::n >= 1);
+//   EKAT_ASSERT(is_same_type);
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const int pack_size = static_cast<int>(PackT::n);
-  const int npack     = (dim2_size+pack_size-1)/pack_size;
+//   const int pack_size = static_cast<int>(PackT::n);
+//   const int npack     = (dim2_size+pack_size-1)/pack_size;
 
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {dim1_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int k, int s) {
-    const int num_scalars = k*pack_size;
-    const int scalar_offset = i*dim2_size + num_scalars;
-    if (num_scalars+s<dim2_size)  array.myData[scalar_offset+s] = view(i,k)[s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {dim1_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int k, int s) {
+//     const int num_scalars = k*pack_size;
+//     const int scalar_offset = i*dim2_size + num_scalars;
+//     if (num_scalars+s<dim2_size)  array.myData[scalar_offset+s] = view(i,k)[s];
+//   });
+// }
 
-// 3D Kokkos view to YAKL array
-template <typename SizeT, typename ViewT, typename ArrayT>
-void view_to_array(const ViewT& view,
-                   const SizeT& dim1_size,
-                   const SizeT& dim2_size,
-                   const SizeT& dim3_size,
-                   ArrayT& array)
-{
-  using PackT      = typename ViewT::value_type;
-  using scalarType = typename ViewT::value_type::scalar;
-  using arrayType  = typename ArrayT::type;
+// // 3D Kokkos view to YAKL array
+// template <typename SizeT, typename ViewT, typename ArrayT>
+// void view_to_array(const ViewT& view,
+//                    const SizeT& dim1_size,
+//                    const SizeT& dim2_size,
+//                    const SizeT& dim3_size,
+//                    ArrayT& array)
+// {
+//   using PackT      = typename ViewT::value_type;
+//   using scalarType = typename ViewT::value_type::scalar;
+//   using arrayType  = typename ArrayT::type;
 
-  auto is_same_type = std::is_same<scalarType, arrayType>::value;
+//   auto is_same_type = std::is_same<scalarType, arrayType>::value;
 
-  EKAT_ASSERT(is_same_type);
-  EKAT_ASSERT(PackT::n >= 1);
+//   EKAT_ASSERT(is_same_type);
+//   EKAT_ASSERT(PackT::n >= 1);
 
-  const int pack_size = static_cast<int>(PackT::n);
-  const int npack     = (dim3_size+pack_size-1)/pack_size;
+//   const int pack_size = static_cast<int>(PackT::n);
+//   const int npack     = (dim3_size+pack_size-1)/pack_size;
 
-#if defined(DEBUG)
-  kokkos_impl_cuda_set_serial_execution(true);
-#endif
-  Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<4>>({0, 0, 0, 0}, {dim1_size, dim2_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int s) {
-    const int num_scalars = k*pack_size;
-    const int scalar_offset = (i*dim2_size+j)*dim3_size+num_scalars;
-    if (num_scalars+s<dim3_size)  array.myData[scalar_offset+s] = view(i,j,k)[s];
-  });
-}
+// #if defined(DEBUG)
+//   kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//   Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<4>>({0, 0, 0, 0}, {dim1_size, dim2_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int j, int k, int s) {
+//     const int num_scalars = k*pack_size;
+//     const int scalar_offset = (i*dim2_size+j)*dim3_size+num_scalars;
+//     if (num_scalars+s<dim3_size)  array.myData[scalar_offset+s] = view(i,j,k)[s];
+//   });
+// }
 
-// validation code 
-template <typename SizeT, typename ViewT>
-void array_to_view_2d(typename ViewT::value_type::scalar* data,
-                      const SizeT& dim1_sizes,
-                      const SizeT& dim2_sizes,
-                      ViewT& view)
-{
-  using PackT = typename ViewT::value_type;
-  EKAT_ASSERT(PackT::n >= 1);
+// // validation code 
+// template <typename SizeT, typename ViewT>
+// void array_to_view_2d(typename ViewT::value_type::scalar* data,
+//                       const SizeT& dim1_sizes,
+//                       const SizeT& dim2_sizes,
+//                       ViewT& view)
+// {
+//   using PackT = typename ViewT::value_type;
+//   EKAT_ASSERT(PackT::n >= 1);
 
-    const int dim1_size = static_cast<int>(dim1_sizes);
-    const int dim2_size = static_cast<int>(dim2_sizes);
-    const int pack_size = static_cast<int>(PackT::n);
-    const int npack     = (dim2_size+pack_size-1)/pack_size;
+//     const int dim1_size = static_cast<int>(dim1_sizes);
+//     const int dim2_size = static_cast<int>(dim2_sizes);
+//     const int pack_size = static_cast<int>(PackT::n);
+//     const int npack     = (dim2_size+pack_size-1)/pack_size;
 
-#if defined(DEBUG)
-    kokkos_impl_cuda_set_serial_execution(true);
-#endif
-    Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {dim1_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int k, int s) {
-       const int num_scalars = k*pack_size;
-       const int scalar_offset = i*dim2_size + num_scalars;
-       if (num_scalars+s<dim2_size) view(i,k)[s] = data[scalar_offset+s];
-   });
-}
+// #if defined(DEBUG)
+//     kokkos_impl_cuda_set_serial_execution(true);
+// #endif
+//     Kokkos::parallel_for(Kokkos::MDRangePolicy<Kokkos::Rank<3>>({0, 0, 0}, {dim1_size, npack, pack_size}), KOKKOS_LAMBDA(int i, int k, int s) {
+//        const int num_scalars = k*pack_size;
+//        const int scalar_offset = i*dim2_size + num_scalars;
+//        if (num_scalars+s<dim2_size) view(i,k)[s] = data[scalar_offset+s];
+//    });
+// }
 
