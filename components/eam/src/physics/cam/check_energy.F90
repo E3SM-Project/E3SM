6393b998b4e58ed39a4ad54d1ef5182f3f2c2f47
Benjamin Hillman <bhillma@sandia.gov>
2022-06-08 00:27:08 -0500

    Make check_energy_mod ideal physics friendly again

--- components/eam/src/physics/cam/check_energy.F90
+++ components/eam/src/physics/cam/check_energy.F90
@@ -1144,15 +1144,15 @@ subroutine qflx_gmean(state, tend, cam_in, dtime, nstep)
     integer, intent(in) :: ncol                   
     integer :: i,k                            
 
-    if (icldliq > 1  .and.  icldice > 1 .and. irain > 1 .and. isnow > 1) then
+    !if (icldliq > 1  .and.  icldice > 1 .and. irain > 1 .and. isnow > 1) then
        do i = 1, ncol
           call energy_helper_eam_def_column(u(i,:),v(i,:),T(i,:),q(i,1:pver,1:pcnst),&
                                    ps(i),pdel(i,:),phis(i), &
                                    ke(i),se(i),wv(i),wl(i),wi(i),wr(i),ws(i),te(i),tw(i) )                             
        enddo
-    else
-       call endrun('energy_helper...column is not implemented if water forms do not exist')
-    endif
+    !else
+    !   call endrun('energy_helper...column is not implemented if water forms do not exist')
+    !endif
 
   end subroutine energy_helper_eam_def
 
@@ -1200,9 +1200,13 @@ subroutine qflx_gmean(state, tend, cam_in, dtime, nstep)
           teloc(k) = 0.5_r8*(u(k)**2 + v(k)**2)*pdel(k)/gravit &
                    + t(k)*cpair*pdel(k)/gravit &
                    + (latvap+latice)*q(k,1       )*pdel(k)/gravit
-          teloc(k) = teloc(k) &
-                   + latice*(q(k,icldliq) + q(k,irain))*pdel(k)/gravit
        end do
+       if (icldliq > 1 .and. irain > 1) then
+          do k = 1, pver
+             teloc(k) = teloc(k) &
+                      + latice*(q(k,icldliq) + q(k,irain))*pdel(k)/gravit
+          end do
+       end if
        psterm = phis*ps/gravit
     endif
 
@@ -1213,15 +1217,19 @@ subroutine qflx_gmean(state, tend, cam_in, dtime, nstep)
     end do
     se = se + phis*ps/gravit
 
-    do k = 1, pver
-       wl = wl + q(k,icldliq)*pdel(k)/gravit
-       wi = wi + q(k,icldice)*pdel(k)/gravit
-    end do
+    if (icldliq > 1 .and. icldice > 1) then
+       do k = 1, pver
+          wl = wl + q(k,icldliq)*pdel(k)/gravit
+          wi = wi + q(k,icldice)*pdel(k)/gravit
+       end do
+    end if
 
-    do k = 1, pver
-       wr = wr + q(k,irain)*pdel(k)/gravit
-       ws = ws + q(k,isnow)*pdel(k)/gravit
-    end do
+    if (irain > 1 .and. isnow > 1) then
+       do k = 1, pver
+          wr = wr + q(k,irain)*pdel(k)/gravit
+          ws = ws + q(k,isnow)*pdel(k)/gravit
+       end do
+    end if
 
     ! Compute vertical integrals of frozen static energy and total water.
     te = se + ke + (latvap+latice)*wv + latice*( wl + wr )
