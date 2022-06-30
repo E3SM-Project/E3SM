4e7374fa015e5807f814303e1a86964402850968
Joe Hollowed <hollowed@umich.edu>
2022-06-29 17:26:28 -0700

    modified HSW with extended stratopause

--- components/eam/src/physics/cam/tphysidl.F90
+++ components/eam/src/physics/cam/tphysidl.F90
@@ -55,6 +55,15 @@ subroutine tphysidl(ztodt, state, tend, ideal_phys_option)
    real(r8) clat(pcols)                        ! latitudes(radians) for columns
    real(r8) pmid(pcols,pver)                   ! mid-point pressure
    integer  i,k                                ! Longitude, level indices
+   
+   real(r8) tmp                                ! temporary
+   real(r8) :: zero(pcols)
+   
+   real(r8) cossq(pcols)                       ! coslat**2
+   real(r8) cossqsq(pcols)                     ! coslat**4
+   real(r8) sinsq(pcols)                       ! sinlat**2
+   real(r8) coslat(pcols)                      ! cosine(latitude)
+   real(r8) teq(pcols, pver)                   ! for writing out eq temp.
 
    real(r8) tmp                                ! temporary
    real(r8) kf                                 ! 1./efolding_time for wind dissipation
@@ -88,8 +97,16 @@ subroutine tphysidl(ztodt, state, tend, ideal_phys_option)
    real(r8) pi                                 ! 3.14159...
    real(r8) coslat(pcols)                      ! cosine(latitude)
    real(r8) acoslat                            ! abs(acos(coslat))
-   real(r8) constc                             ! constant
-   real(r8) lapsec                             ! lapse rate
+   real(r8) pmax                               ! pressure given by max(pmid, apole)
+   
+   ! JH
+   ! ---------- Forcing parameters for Lin/Williamson modification ----------
+   ! 
+   real(r8) onemsig                            ! 1. - sigma_reference
+   real(r8) efold_strat                        ! efolding time for T dissipation in Strat
+   real(r8) efold_meso                         ! efolding time for T dissipation in Meso
+   real(r8) efoldv                             ! efolding time for wind dissipation
+   real(r8) p_infint                           ! effective top of model
    real(r8) lapse                              ! lapse rate
    real(r8) h0                                 ! scale height (7 km)
    real(r8) sigmab                             ! threshold sigma level
@@ -218,68 +235,88 @@ subroutine tphysidl(ztodt, state, tend, ideal_phys_option)
 !
 ! Compute idealized radiative heating rates (as dry static energy)
 !
-      efoldf  =  1._r8
-      efolda  = 40._r8
-      efoldaa = 40._r8
-      efolds  =  4._r8
-      sigmab  =  0.7_r8
-      t00     = 200._r8
-!
-      onemsig = 1._r8 - sigmab
-!
-      ka  = 1._r8/(86400._r8*efolda)
-      kaa = 1._r8/(86400._r8*efoldaa)
-      ks  = 1._r8/(86400._r8*efolds)
-!
-      pi     = 4._r8*atan(1._r8)
+      pi      = 4._r8*atan(1._r8)
+      pih     = 2._r8*atan(1._r8)
       phi0   = 60._r8*pi/180._r8
       dphi0  = 15._r8*pi/180._r8
+      efoldaa = 40._r8               ! T relaxation e-folding in days
+      kaa     = 1._r8/(86400._r8*efoldaa)
       a0     = 2.65_r8/dphi0
-      aeq    = 10000._r8
-      apole  = 200._r8
-      lapsew = -3.345e-03_r8
-      constw = rair*lapsew/gravit
-      lapsec =  2.00e-03_r8
+      aeq    = 10000._r8             ! JH: 100 mb; apply vanilla HS below this pressure
+      apole  = 200._r8              ! JH: original value from Williamson+ modificaiton
+      !apole  = (ptop_ref * 0.9_r8)   ! JH: place apole slightly above model top
+      lapsew = -3.345e-03_r8         ! lapse rate at the poles
+      constw = rair*lapsew/gravit    
+      lapsec =  2.00e-03_r8          ! lapse rate in tropics
       constc = rair*lapsec/gravit
+     
+      ! JH
+      ! --- inherited from standard HS --- 
+      efoldf  =  1._r8              ! e-folding time for wind dissipation
+      efolda  = 40._r8              ! e-folding time for temp
+      efolds  =  4._r8              ! e-folding time for temp
+      sigmab  =  0.7_r8             ! threshold for surface wind damping, latitudinal temp damping
+      t00     = 200._r8             ! min temp eq
+      onemsig = 1._r8 - sigmab        
+      ka  = 1._r8/(86400._r8*efolda) 
+      ks  = 1._r8/(86400._r8*efolds)  
+      
+      
       do k=1,pver
          if (pref_mid_norm(k) > sigmab) then
-            do i=1,ncol
+            ! below HS threshold of sigma=0.7; give T damping timescale latitude dependence
+            do i=1,ncol  
+               ! ---- standard HS
                kt = ka + (ks - ka)*cossqsq(i)*(pref_mid_norm(k) - sigmab)/onemsig
+               tmp     = kt/(1._r8 + ztodt*kt)
+               trefc   = 315._r8 - 60._r8*sinsq(i)
+               trefa   = (trefc - 10._r8*cossq(i)*log((pmid(i,k)/psurf_ref)))*(pmid(i,k)/psurf_ref)**cappa
+               trefa   = max(t00,trefa)
+               
+               ! ---- HSW modifications
                acoslat = abs(acos(coslat(i)))
                p0strat = aeq - (aeq - apole)*0.5_r8*(1._r8 + tanh(a0*(acoslat - phi0)))
-               tmp     = kt/(1._r8+ ztodt*kt)
-               trefc   = 315._r8 - 60._r8*sinsq(i)
-               trefa   = (trefc - 10._r8*cossq(i)*log((pmid(i,k)/psurf_ref)))*(pmid(i,k)/psurf_ref)** &
-                                                                                     cappa
-               trefa   = max(t00,trefa)
                if (pmid(i,k) < 10000._r8) then
+                  ! above HSW p_d threshold; temp decreases with const. lapse rate
                   trefa = t00*((pmid(i,k)/10000._r8))**constc
                   tmp   = kaa/(1._r8+ ztodt*kaa)
                endif
                if (pmid(i,k) < p0strat) then
+                  ! above HSW p_i threshold; temp increases with const. lapse rate
                   trefa = trefa + t00*( ((pmid(i,k)/p0strat))**constw - 1._r8 )
                   tmp   = kaa/(1._r8+ ztodt*kaa)
                endif
                ptend%s(i,k) = (trefa - state%t(i,k))*tmp*cpair
+               teq(i, k) = trefa
             end do
          else
+            ! above HS threshold of sigma=0.7; T damping timescale is constant
             do i=1,ncol
-               acoslat = abs(acos(coslat(i)))
-               p0strat = aeq - (aeq - apole)*0.5_r8*(1._r8 + tanh(a0*(acoslat - phi0)))
+               
+               ! instread of pmid(i,k), use max(pmid(i, k), apole) to force the equillibrium
+               ! profile to a constant everywhere above apole (~the Williamson model top)
+               pmax = MAX(pmid(i, k), apole)
+
                tmp     = ka/(1._r8+ ztodt*ka)
                trefc   = 315._r8 - 60._r8*sinsq(i)
-               trefa   = (trefc - 10._r8*cossq(i)*log((pmid(i,k)/psurf_ref)))*(pmid(i,k)/psurf_ref)** &
-                                                                                     cappa
+               trefa   = (trefc - 10._r8*cossq(i)*log((pmax/psurf_ref)))*(pmax/psurf_ref)**cappa
                trefa   = max(t00,trefa)
-               if (pmid(i,k) < 10000._r8) then
-                  trefa = t00*((pmid(i,k)/10000._r8))**constc
+               
+               ! ---- HSW modifications
+               acoslat = abs(acos(coslat(i)))
+               p0strat = aeq - (aeq - apole)*0.5_r8*(1._r8 + tanh(a0*(acoslat - phi0)))
+               if (pmax < 10000._r8) then
+                  ! above HSW p_d threshold; temp decreases with const. lapse rate
+                  trefa = t00*((pmax/10000._r8))**constc
                   tmp   = kaa/(1._r8+ ztodt*kaa)
                endif
-               if (pmid(i,k) < p0strat) then
-                  trefa = trefa + t00*( ((pmid(i,k)/p0strat))**constw - 1._r8 )
+               if (pmax < p0strat) then
+                  ! above HSW p_i threshold; temp increases with const. lapse rate
+                  trefa = trefa + t00*( ((pmax/p0strat))**constw - 1._r8 )
                   tmp   = kaa/(1._r8+ ztodt*kaa)
                endif
                ptend%s(i,k) = (trefa - state%t(i,k))*tmp*cpair
+               teq(i, k) = trefa
             end do
          endif
       end do
@@ -420,8 +457,9 @@ subroutine tphysidl(ztodt, state, tend, ideal_phys_option)
       call endrun('ideal_phys_option: invalid option')
    endif
 
-   ! write heating rate to history file
-   call outfld('HS_HEAT', ptend%s(:,:), ncol, lchnk)
+   ! write heating rate, equillibrium temperature to history file
+   !call outfld('HS_HEAT', ptend%s(:,:), ncol, lchnk)
+   call outfld('HS_HEAT', teq(:,:), ncol, lchnk)
 
 
    ! update the state and total physics tendency
