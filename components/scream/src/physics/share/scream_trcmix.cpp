a965fdfb72463d4df52bb0f2fb697c726f60b481
James Foucar <jgfouca@sandia.gov>
2022-07-20 16:26:29 -0600

    Change trcmix API to take views

--- components/scream/src/physics/share/scream_trcmix.cpp
+++ components/scream/src/physics/share/scream_trcmix.cpp
@@ -1,6 +1,10 @@
 #include "scream_trcmix.hpp"
 #include "physics_constants.hpp"
 
+#include "ekat/ekat_pack_kokkos.hpp"
+
+#include <vector>
+
 using scream::Real;
 using scream::Int;
 
@@ -15,24 +19,57 @@ void trcmix_cf2(const char* name, Int ncol, Int pcols, Int pver, Real* clat, Rea
 namespace scream {
 namespace physics {
 
-void trcmix(const char* name, Int ncol, Int pcols, Int pver, Real* clat, Real* pmid, Real* q)
+#define CHECK_TRCMIX_VIEW(vtype, ranks)           \
+static_assert(Kokkos::is_view<vtype>::value, "trcmix: " #vtype " is not a KokkosView"); \
+static_assert(std::is_same<typename vtype::non_const_value_type, Real>::value, "trcmix: " #vtype " is not View of Reals"); \
+static_assert(vtype::rank == ranks, "trcmix: " #vtype " does not have expected rank: " #ranks);
+
+template <typename ClatView, typename PmidView, typename QView>
+void trcmix(
+  const char* name, Int ncol, Int pcols, Int pver,
+  const ClatView& clat, const PmidView& pmid, QView& q,
+  const Real co2vmr_rad, const Real co2vmr, const Real n2ovmr, const Real ch4vmr, const Real f11vmr, const Real f12vmr)
 {
   using C = Constants<Real>;
 
-  Real o2mmr, co2vmr_rad, co2vmr, n2ovmr, ch4vmr, f11vmr, f12vmr;
+  //
+  // View type checking
+  //
+  CHECK_TRCMIX_VIEW(ClatView, 1);
+  CHECK_TRCMIX_VIEW(PmidView, 2);
+  CHECK_TRCMIX_VIEW(QView,    2);
 
-  //d.transpose<ekat::TransposeDirection::c2f>();
+  EKAT_REQUIRE(clat.extent(0) == pcols);
+  EKAT_REQUIRE(pmid.extent(0) == pcols && pmid.extent(1) == pver);
+  EKAT_REQUIRE(q.extent(0) == pcols && q.extent(1) == pver);
+
+  // Transpose and copy to host
+  std::vector<Real> hclat(pcols), hpmid(pcols*pver), hq(pcols*pver);
+  ekat::device_to_host(
+    {hclat.data(), hpmid.data(), hq.data()},
+    {pcols, pcols, pcols},
+    {1, pver, pver},
+    {clat, pmid, q}, true);
+
   trcmix_cf2(
-    name, ncol, pcols, pver, clat, pmid, q,
+    name, ncol, pcols, pver, hclat.data(), hpmid.data(), hq.data(),
     C::MWdry,
     C::get_gas_mol_weight("co2"),
     C::get_gas_mol_weight("n2o"),
     C::get_gas_mol_weight("ch4"),
     C::get_gas_mol_weight("cfc11"),
     C::get_gas_mol_weight("cfc12"),
-    o2mmr, co2vmr_rad, co2vmr, n2ovmr, ch4vmr, f11vmr, f12vmr);
-  //d.transpose<ekat::TransposeDirection::f2c>();
+    C::o2mmr,
+    co2vmr_rad, co2vmr, n2ovmr, ch4vmr, f11vmr, f12vmr);
+
+  ekat::host_to_device(
+    {hclat.data(), hpmid.data(), hq.data()},
+    {pcols, pcols, pcols},
+    {1, pver, pver},
+    {clat, pmid, q}, true);
 }
+
+#undef CHECK_TRCMIX_VIEW
 
 } // namespace physics
 } // namespace scream
--- components/scream/src/physics/share/scream_trcmix.hpp
+++ components/scream/src/physics/share/scream_trcmix.hpp
@@ -10,7 +10,22 @@
 namespace scream {
 namespace physics {
 
-void trcmix(const char* name, Int ncol, Int pcols, Int pver, Real* clat, Real* pmid, Real* q);
+template <typename ClatView, typename PmidView, typename QView>
+void trcmix(
+  const char* name,      // constituent name
+  Int ncol,              // number of columns
+  Int pcols,             // Maximum number of columns in a chunk
+  Int pver,              // Num vertical levels
+  const ClatView& clat,  // latitude in radians for columns
+  const PmidView& pmid,  // model pressures
+  QView& q,              // constituent mass mixing ratio (output)
+  // items below likely come from namelists/yaml
+  const Real co2vmr_rad, // co2 vmr override for radiation
+  const Real co2vmr,     // co2 volume mixing ratio
+  const Real n2ovmr,     // n2o volume mixing ratio
+  const Real ch4vmr,     // ch4 volume mixing ratio
+  const Real f11vmr,     // cfc11 volume mixing ratio
+  const Real f12vmr);    // cfc12 volume mixing ratio
 
 } // namespace physics
 } // namespace scream
