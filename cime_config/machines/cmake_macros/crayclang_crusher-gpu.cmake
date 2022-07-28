c8f1398e2eda8e68ed09d47cedbdb4b1350a0c2d
Oksana Guba <oksanaguba@gmail.com>
2022-07-28 19:09:55 -0400

    add cmake macros file for crusher-gpu

---
+++ cime_config/machines/cmake_macros/crayclang_crusher-gpu.cmake
@@ -0,0 +1,21 @@
+#if (compile_threaded)
+#  string(APPEND CFLAGS " -fopenmp")
+#  string(APPEND FFLAGS " -fopenmp")
+#  string(APPEND CXXFLAGS " -fopenmp")
+#  string(APPEND LDFLAGS " -fopenmp")
+#endif()
+
+string(APPEND SLIBS " -L$ENV{PNETCDF_PATH}/lib -lpnetcdf")
+set(NETCDF_PATH "$ENV{NETCDF_DIR}")
+set(PNETCDF_PATH "$ENV{PNETCDF_DIR}")
+set(PIO_FILESYSTEM_HINTS "gpfs")
+string(APPEND CXX_LIBS " -lstdc++")
+
+SET(CMAKE_C_COMPILER "cc" CACHE STRING "")
+SET(CMAKE_Fortran_COMPILER "ftn" CACHE STRING "")
+SET(CMAKE_CXX_COMPILER "hipcc" CACHE STRING "")
+
+string(APPEND CXXFLAGS "-I${MPICH_DIR}/include -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.12/gtl/lib -lmpi_gtl_hsa")
+string(APPEND CFLAGS "-I${MPICH_DIR}/include -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.12/gtl/lib -lmpi_gtl_hsa")
+string(APPEND FFLAGS "-I${MPICH_DIR}/include -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.12/gtl/lib -lmpi_gtl_hsa")
+
