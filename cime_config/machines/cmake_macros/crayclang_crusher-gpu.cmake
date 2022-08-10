66648c6f080d41ea3267d24720e1b9320b9b9932
Oksana Guba <oksanaguba@gmail.com>
2022-08-10 18:25:29 -0400

    work aroung mct crash for docn, thanks to Xingqiu

--- cime_config/machines/cmake_macros/crayclang_crusher-gpu.cmake
+++ cime_config/machines/cmake_macros/crayclang_crusher-gpu.cmake
@@ -15,7 +15,17 @@ SET(CMAKE_C_COMPILER "cc" CACHE STRING "")
 SET(CMAKE_Fortran_COMPILER "ftn" CACHE STRING "")
 SET(CMAKE_CXX_COMPILER "hipcc" CACHE STRING "")
 
+#recommended I and L options acc. to crusher man pages
+#need to be sorted, append for fflags and cflags prob not needed
 string(APPEND CXXFLAGS " -I${MPICH_DIR}/include -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.16/gtl/lib -lmpi_gtl_hsa")
 string(APPEND CFLAGS " -I${MPICH_DIR}/include -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.16/gtl/lib -lmpi_gtl_hsa")
 string(APPEND FFLAGS " -hnoacc -I${MPICH_DIR}/include -L${MPICH_DIR}/lib -lmpi -L/opt/cray/pe/mpich/8.1.16/gtl/lib -lmpi_gtl_hsa")
+
+#this resolves a crash in mct in docn init
+if (NOT DEBUG)
+string(APPEND CFLAGS " -O2 -hnoacc -hzero -hfp0 -hipa0")
+string(APPEND FFLAGS " -O2 -hnoacc -hzero -hfp0 -hipa0")
+endif()
+
+
 
