0f6a3c0296e975df0567691e3d4bf8b756016736
Andrew M. Bradley <ambradl@sandia.gov>
2022-08-10 19:13:33 -0400

    Switch PEM test from ne30 to ne30pg2.
    This fits within the allocated nodes on ascent (based on testing on summit).
    
    I've also lengthened the test to 90 steps, which runs in ~200s. I think a longer
    PEM test is useful.

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -474,7 +474,7 @@ _TESTS = {
         "tests" : (
             #  "SMS_D_Ln2.ne30_ne30.F2000-SCREAMv1-AQP1", # Uncomment once IC file for ne30 is ready
             "ERS_Ln22.ne30_ne30.F2010-SCREAMv1",
-            "PEM_Ln9.ne30_ne30.F2010-SCREAMv1",
+            "PEM_Ln90.ne30pg2_ne30pg2.F2010-SCREAMv1",
             "ERS_Ln22.ne30pg2_ne30pg2.F2010-SCREAMv1",
             )
     },
