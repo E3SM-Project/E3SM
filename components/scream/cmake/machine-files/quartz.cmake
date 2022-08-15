a1eae4157e31d7e401f00c08419dcafc3f621143
Maya Taylor <maya_taylor@brown.edu>
2022-08-15 09:00:02 -0700

    fixed compiler check for full e3sm build with intel

--- components/scream/cmake/machine-files/quartz.cmake
+++ components/scream/cmake/machine-files/quartz.cmake
@@ -6,10 +6,11 @@ include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
 option(Kokkos_ARCH_BDW "" ON)
 
 #if COMPILER is not defined, should be running standalone with quartz-intel or quartz-gcc
-if ("$ENV{COMPILER}" STREQUAL "intel")
+if ("${COMPILER}" STREQUAL "intel")
    set(CMAKE_CXX_FLAGS "-w -cxxlib=/usr/tce/packages/gcc/gcc-8.3.1/rh" CACHE STRING "" FORCE)
    set(CMAKE_EXE_LINKER_FLAGS "-L/usr/tce/packages/gcc/gcc-8.3.1/rh/lib/gcc/x86_64-redhat-linux/8/ -mkl" CACHE STRING "" FORCE)
-elseif ("$ENV{COMPILER}" STREQUAL "gnu")
+elseif ("${COMPILER}" STREQUAL "gnu")
+   message(WARNING "You are using an unsupported e3sm compiler. For supported quartz compilers run ./${E3SM_ROOT}/cime/scripts/query_config --machines quartz")
    set(CMAKE_CXX_FLAGS "-w" CACHE STRING "" FORCE)
    set(CMAKE_EXE_LINKER_FLAGS "-L/usr/tce/packages/gcc/gcc-8.3.1/rh/lib/gcc/x86_64-redhat-linux/8/" CACHE STRING "" FORCE)
 endif()
