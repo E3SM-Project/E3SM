d04219dbfe2d24bb4a9b6fc1712b113f630980e9
Luca Bertagna <lbertag@sandia.gov>
2022-08-16 20:24:03 -0500

    Homme: ensure host cmake project is providing csm_share target

--- components/homme/cmake/HommeMacros.cmake
+++ components/homme/cmake/HommeMacros.cmake
@@ -240,6 +240,7 @@ macro(createExecLib libName execType libSrcs inclDirs macroNP
     TARGET_COMPILE_DEFINITIONS(${libName} PUBLIC HOMME_WITHOUT_PIOLIBRARY)
   ENDIF()
 
+  target_link_libraries(${execName} csm_share)
 
   IF (CXXLIB_SUPPORTED_CACHE)
     MESSAGE(STATUS "   Linking Fortran with -cxxlib")
