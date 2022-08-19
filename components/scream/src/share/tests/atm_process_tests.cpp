be5bfbbc88b6bddcae35a4ac3c249a000b4238ad
Luca Bertagna <lbertag@sandia.gov>
2022-08-19 17:50:53 -0600

    EAMxx: remove more occurrences of reference grid

--- components/scream/src/share/tests/atm_process_tests.cpp
+++ components/scream/src/share/tests/atm_process_tests.cpp
@@ -58,7 +58,6 @@ create_gm (const ekat::Comm& comm) {
   const int num_global_cols = num_local_cols*comm.size();
 
   ekat::ParameterList gm_params;
-  gm_params.set<std::string>("Reference Grid", "Point Grid");
   gm_params.set<int>("Number of Global Columns", num_global_cols);
   gm_params.set<int>("Number of Local Elements", num_local_elems);
   gm_params.set<int>("Number of Vertical Levels", nlevs);
