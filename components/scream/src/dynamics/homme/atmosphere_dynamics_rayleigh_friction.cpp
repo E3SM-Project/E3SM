4db3cfd430bf9fd923bf7155190aecd5b39b4818
Thomas Conrad Clevenger <tccleve@mappy.sandia.gov>
2022-08-24 23:48:19 -0600

    locals for class vars, paramaters format

--- components/scream/src/dynamics/homme/atmosphere_dynamics_rayleigh_friction.cpp
+++ components/scream/src/dynamics/homme/atmosphere_dynamics_rayleigh_friction.cpp
@@ -19,9 +19,13 @@ void HommeDynamics::rayleigh_friction_init()
   m_otau = decltype(m_otau)("otau", npacks);
 
   // Rayleigh friction paramaters
-  m_rayk0     = m_params.get<int>("Rayleigh Friction Vertical Level", 1);
-  m_raykrange = m_params.get<Real>("Rayleigh Friction Range", 0.0);
-  m_raytau0   = m_params.get<Real>("Rayleigh Friction Decay Time", 5.0);
+  m_rayk0     = m_params.get<int>("rayleigh_friction_vertical_level", 2);
+  m_raykrange = m_params.get<Real>("rayleigh_friction_range", 0.0);
+  m_raytau0   = m_params.get<Real>("rayleigh_friction_decay_time", 5.0);
+
+  // Input file is read in 1-based indexing, convert 
+  // to 0-based for computations
+  m_rayk0 -= 1;
 
   // Calculate decay rate profile, otau.
   // If m_raytau0 == 0 no Rayleigh friction is applied.
@@ -37,14 +41,17 @@ void HommeDynamics::rayleigh_friction_init()
     otau0 = 0;
     if (tau0 != 0) otau0 = 1.0/tau0;
 
-    auto otau = m_otau;
     const auto nlevs = m_phys_grid->get_num_vertical_levels();
     const int npacks = ekat::PackInfo<Pack::n>::num_packs(nlevs);
 
+    // locals for lambda captures to avoid issues on GPU
+    auto otau = m_otau;
+    auto rayk0 = m_rayk0;
+    
     Kokkos::parallel_for(KT::RangePolicy(0, npacks),
                          KOKKOS_LAMBDA (const int ilev) {
       const auto range_pack = ekat::range<Pack>(ilev*Pack::n);
-      const Pack x = (m_rayk0 - range_pack)/krange;
+      const Pack x = (rayk0 - range_pack)/krange;
       otau(ilev) = otau0*(1.0 + ekat::tanh(x))/2.0;
     });
   }
@@ -65,13 +72,16 @@ void HommeDynamics::rayleigh_friction_apply(const Real dt) const
   const auto horiz_winds_view = get_field_out("horiz_winds").get_view<Pack***>();
   const auto T_mid_view       = get_field_out("T_mid").get_view<Pack**>();
 
+  // locals for lambda captures to avoid issues on GPU
+  auto raytau0 = m_raytau0;
+  auto otau = m_otau;
+
   const auto policy = ESU::get_default_team_policy(ncols, npacks);
   Kokkos::parallel_for(policy, KOKKOS_LAMBDA (const KT::MemberType& team) {
     const int& icol = team.league_rank();
 
-    // If m_raytau0 == 0 no Rayleigh friction is applied.
-    if (m_raytau0 != 0) {
-      const auto otau = m_otau;
+    // If raytau0 == 0 no Rayleigh friction is applied.
+    if (raytau0 != 0) {
       auto u_wind = ekat::subview(horiz_winds_view, icol, 0);
       auto v_wind = ekat::subview(horiz_winds_view, icol, 1);
       auto T_mid  = ekat::subview(T_mid_view, icol);
