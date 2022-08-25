888bd670c4fab3962fe62133d8c1aea8ae13c2de
Thomas Conrad Clevenger <tccleve@mappy.sandia.gov>
2022-08-25 10:49:11 -0600

    return if raytau0=0

--- components/scream/src/dynamics/homme/atmosphere_dynamics_rayleigh_friction.cpp
+++ components/scream/src/dynamics/homme/atmosphere_dynamics_rayleigh_friction.cpp
@@ -14,47 +14,45 @@ void HommeDynamics::rayleigh_friction_init()
   using KT = KokkosTypes<DefaultDevice>;
   using Pack = ekat::Pack<Real,N>;
 
-  const int nlevs = m_dyn_grid->get_num_vertical_levels();
-  const auto npacks= ekat::PackInfo<Pack::n>::num_packs(nlevs);
-  m_otau = decltype(m_otau)("otau", npacks);
-
   // Rayleigh friction paramaters
   m_rayk0     = m_params.get<int>("rayleigh_friction_vertical_level", 2);
   m_raykrange = m_params.get<Real>("rayleigh_friction_range", 0.0);
   m_raytau0   = m_params.get<Real>("rayleigh_friction_decay_time", 5.0);
+
+  // If m_raytau0==0, then no Rayleigh friction is applied. Return.
+  if (m_raytau0 == 0) return;
 
   // Input file is read in 1-based indexing, convert 
   // to 0-based for computations
   m_rayk0 -= 1;
 
   // Calculate decay rate profile, otau.
-  // If m_raytau0 == 0 no Rayleigh friction is applied.
-  if (m_raytau0 != 0) {
-    Real krange; // range of rayleigh friction profile
-    Real tau0;   // approximate value of decay time at model top
-    Real otau0;  // inverse of tau0
+  const int nlevs = m_dyn_grid->get_num_vertical_levels();
+  const auto npacks= ekat::PackInfo<Pack::n>::num_packs(nlevs);
+  m_otau = decltype(m_otau)("otau", npacks);
 
-    krange = m_raykrange;
-    if (m_raykrange == 0) krange = m_rayk0/2.0;
+  // Local paramters
+  Real krange; // range of rayleigh friction profile
+  Real tau0;   // approximate value of decay time at model top
+  Real otau0;  // inverse of tau0
 
-    tau0 = 86400.0*m_raytau0; // convert to seconds
-    otau0 = 0;
-    if (tau0 != 0) otau0 = 1.0/tau0;
+  krange = m_raykrange;
+  if (m_raykrange == 0) krange = m_rayk0/2.0;
 
-    const auto nlevs = m_phys_grid->get_num_vertical_levels();
-    const int npacks = ekat::PackInfo<Pack::n>::num_packs(nlevs);
+  tau0 = 86400.0*m_raytau0; // convert to seconds
+  otau0 = 0;
+  if (tau0 != 0) otau0 = 1.0/tau0;
 
-    // locals for lambda captures to avoid issues on GPU
-    auto otau = m_otau;
-    auto rayk0 = m_rayk0;
+  // locals for lambda captures to avoid issues on GPU
+  auto otau = m_otau;
+  auto rayk0 = m_rayk0;
     
-    Kokkos::parallel_for(KT::RangePolicy(0, npacks),
-                         KOKKOS_LAMBDA (const int ilev) {
-      const auto range_pack = ekat::range<Pack>(ilev*Pack::n);
-      const Pack x = (rayk0 - range_pack)/krange;
-      otau(ilev) = otau0*(1.0 + ekat::tanh(x))/2.0;
-    });
-  }
+  Kokkos::parallel_for(KT::RangePolicy(0, npacks),
+                       KOKKOS_LAMBDA (const int ilev) {
+    const auto range_pack = ekat::range<Pack>(ilev*Pack::n);
+    const Pack x = (rayk0 - range_pack)/krange;
+    otau(ilev) = otau0*(1.0 + ekat::tanh(x))/2.0;
+  });
 }
 
 void HommeDynamics::rayleigh_friction_apply(const Real dt) const
@@ -64,6 +62,9 @@ void HommeDynamics::rayleigh_friction_apply(const Real dt) const
   using KT = KokkosTypes<DefaultDevice>;
   using Pack = ekat::Pack<Real,N>;
   using ESU = ekat::ExeSpaceUtils<KT::ExeSpace>;
+
+  // If m_raytau0==0, then no Rayleigh friction is applied. Return.
+  if (m_raytau0 == 0) return;
 
   const auto ncols = m_phys_grid->get_num_local_dofs();
   const auto nlevs = m_phys_grid->get_num_vertical_levels();
@@ -80,13 +81,10 @@ void HommeDynamics::rayleigh_friction_apply(const Real dt) const
   Kokkos::parallel_for(policy, KOKKOS_LAMBDA (const KT::MemberType& team) {
     const int& icol = team.league_rank();
 
-    // If raytau0 == 0 no Rayleigh friction is applied.
-    if (raytau0 != 0) {
-      auto u_wind = ekat::subview(horiz_winds_view, icol, 0);
-      auto v_wind = ekat::subview(horiz_winds_view, icol, 1);
-      auto T_mid  = ekat::subview(T_mid_view, icol);
-      PF::apply_rayleigh_friction(team, dt, otau, u_wind, v_wind, T_mid);
-    }
+    auto u_wind = ekat::subview(horiz_winds_view, icol, 0);
+    auto v_wind = ekat::subview(horiz_winds_view, icol, 1);
+    auto T_mid  = ekat::subview(T_mid_view, icol);
+    PF::apply_rayleigh_friction(team, dt, otau, u_wind, v_wind, T_mid);
   });
 }
 
