547bf40335540958983575a70ff7571bc44c489d
James Foucar <jgfouca@sandia.gov>
2022-09-01 12:38:37 -0600

    Fix quartz building of kokkos
    Do not use KOKKOS_OPTIONS in E3SM macros, use SCREAM macros
    to configure Kokkos with non-deprecated options.

--- components/scream/cmake/machine-files/quartz.cmake
+++ components/scream/cmake/machine-files/quartz.cmake
@@ -1,6 +1,7 @@
 # Load all kokkos settings from Ekat's mach file
 set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
 include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
+set(Kokkos_ENABLE_SERIAL TRUE CACHE BOOL "")
 
 # Enable Broadwell arch in Kokkos
 option(Kokkos_ARCH_BDW "" ON)
