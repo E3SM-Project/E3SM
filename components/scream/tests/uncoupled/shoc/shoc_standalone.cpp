85d5ddda3962e67570b30ef5aa83640749501234
Oksana Guba <oksanaguba@gmail.com>
2022-10-28 19:15:18 -0400

    addressing comments

--- components/scream/tests/uncoupled/shoc/shoc_standalone.cpp
+++ components/scream/tests/uncoupled/shoc/shoc_standalone.cpp
@@ -23,7 +23,13 @@ TEST_CASE("shoc-stand-alone", "") {
   // Load ad parameter list
   std::string fname = "input.yaml";
   ekat::ParameterList ad_params("Atmosphere Driver");
-  //REQUIRE_NOTHROW ( parse_yaml_file(fname,ad_params) );
+  
+  #ifndef KOKKOS_ENABLE_HIP
+  REQUIRE_NOTHROW ( parse_yaml_file(fname,ad_params) );
+  #else
+  parse_yaml_file(fname,ad_params);
+  #endif
+  
   parse_yaml_file(fname,ad_params) ;
 
   // Time stepping parameters
