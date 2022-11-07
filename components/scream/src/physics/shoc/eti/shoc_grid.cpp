f4af67f997636e2c643755490e116349cca0f70e
James Foucar <jgfouca@sandia.gov>
2022-11-07 14:32:02 -0700

    Cleanup shoc and p3 dirs
    Move eti files into eti dir
    Move dispatch files into disp dir (shoc only)
    Move impl files into impl dir

---
+++ components/scream/src/physics/shoc/eti/shoc_grid.cpp
@@ -0,0 +1,13 @@
+#include "../impl/shoc_grid_impl.hpp"
+
+namespace scream {
+namespace shoc {
+
+/*
+ * Explicit instantiation for using the default device.
+ */
+
+template struct Functions<Real,DefaultDevice>;
+
+} // namespace shoc
+} // namespace scream
---
+++ components/scream/src/physics/shoc/eti/shoc_pblintd_init_pot.cpp
@@ -0,0 +1,10 @@
+#include "../impl/shoc_pblintd_init_pot_impl.hpp"
+
+namespace scream {
+namespace shoc {
+
+template struct Functions<Real,DefaultDevice>;
+
+} // namespace shoc
+} // namespace scream
+
