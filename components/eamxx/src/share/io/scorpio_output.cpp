7f8218e327b74703932f16f08d977623af8ebe8a
Aaron Donahue <donahue.aaron@gmail.com>
2022-11-22 13:56:12 -0800

    Add the capability to produce regional output.
    This commit will apply the new coarsening remapper to the scorpio output interface to give the user the ability to produce remapped output on the fly.
    
    In order to trigger regional output the output control YAML file needs to have the "remap_file" parameter given. The value should be a string that points to the remap file to be used.

--- components/eamxx/src/share/io/scorpio_output.cpp
+++ components/eamxx/src/share/io/scorpio_output.cpp
@@ -1,6 +1,7 @@
 #include "share/io/scorpio_output.hpp"
 #include "share/io/scorpio_input.hpp"
 #include "share/util/scream_array_utils.hpp"
+#include "share/grid/remap/coarsening_remapper.hpp"
 
 #include "ekat/util/ekat_units.hpp"
 #include "ekat/util/ekat_string_utils.hpp"
@@ -86,9 +87,18 @@ AtmosphereOutput (const ekat::Comm& comm, const ekat::ParameterList& params,
   // Try to set the IO grid (checks will be performed)
   set_grid (io_grid);
 
-  if (io_grid->name()!=fm_grid->name()) {
+  bool remap_from_file = params.isParameter("remap_file");
+
+  if (io_grid->name()!=fm_grid->name() || remap_from_file) {
     // We build a remapper, to remap fields from the fm grid to the io grid
-    m_remapper = grids_mgr->create_remapper(fm_grid,io_grid);
+    if (remap_from_file) {
+      auto remap_file   = params.get<std::string>("remap_file");
+      m_remapper = std::make_shared<CoarseningRemapper>(io_grid,remap_file);
+      io_grid = m_remapper->get_tgt_grid();
+      set_grid(io_grid);
+    } else {
+      m_remapper = grids_mgr->create_remapper(fm_grid,io_grid);
+    }
 
     // Register all output fields in the remapper.
     m_remapper->registration_begins();
@@ -540,8 +550,14 @@ AtmosphereOutput::get_var_dof_offsets(const FieldLayout& layout)
   //       All we need to do in this routine is to compute the offset of all the entries
   //       of the MPI-local array w.r.t. the global array. So long as the offsets are in
   //       the same order as the corresponding entry in the data to be read/written, we're good.
+  // NOTE: In the case of regional output this rank may have 0 columns to write, thus, var_dof
+  //       should be empty, we check for this special case and return an empty var_dof.
   if (layout.has_tag(ShortFieldTagsNames::COL)) {
     const int num_cols = m_io_grid->get_num_local_dofs();
+    if (num_cols==0) {
+      var_dof = std::vector<scorpio::offset_t>(0);
+      return var_dof;
+    }
 
     // Note: col_size might be *larger* than the number of vertical levels, or even smaller.
     //       E.g., (ncols,2,nlevs), or (ncols,2) respectively.
@@ -570,6 +586,10 @@ AtmosphereOutput::get_var_dof_offsets(const FieldLayout& layout)
     const int num_my_elems = layout2d.dim(0);
     const int ngp = layout2d.dim(1);
     const int num_cols = num_my_elems*ngp*ngp;
+    if (num_cols==0) {
+      var_dof = std::vector<scorpio::offset_t>(0);
+      return var_dof;
+    }
 
     // Note: col_size might be *larger* than the number of vertical levels, or even smaller.
     //       E.g., (ncols,2,nlevs), or (ncols,2) respectively.
