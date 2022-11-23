9139919b3b670fbd1445cb22c5eb11d06edab75d
Ziming Ke <ke2@llnl.gov>
2022-11-22 20:51:40 -0600

    switch btw MAM5 and cmip6_volc emission; strat AOD treatment flag

--- components/eam/src/physics/cam/phys_control.F90
+++ components/eam/src/physics/cam/phys_control.F90
@@ -194,7 +194,7 @@ logical :: l_dry_adj       = .true.
 logical :: l_st_mac        = .true.
 logical :: l_st_mic        = .true.
 logical :: l_rad           = .true.
-
+logical :: modal_strat_sulfate_aod_treatment = .false.
 ! Numerical schemes for process coupling
 
 integer :: cflx_cpl_opt = 1  ! When to apply surface tracer fluxes (not including water vapor).
@@ -249,7 +249,8 @@ subroutine phys_ctl_readnl(nlfile)
       cflx_cpl_opt, &
       l_tracer_aero, l_vdiff, l_rayleigh, l_gw_drag, l_ac_energy_chk, &
       l_bc_energy_fix, l_dry_adj, l_st_mac, l_st_mic, l_rad, prc_coef1,prc_exp,prc_exp1,cld_sed,mg_prc_coeff_fix, &
-      rrtmg_temp_fix, ideal_phys_option
+      rrtmg_temp_fix, &
+      modal_strat_sulfate_aod_treatment
    !-----------------------------------------------------------------------------
 
    if (masterproc) then
@@ -382,6 +383,8 @@ subroutine phys_ctl_readnl(nlfile)
    call mpibcast(mg_prc_coeff_fix,                1 , mpilog,  0, mpicom)
    call mpibcast(rrtmg_temp_fix,                  1 , mpilog,  0, mpicom)
    call mpibcast(cld_sed,                         1 , mpir8,   0, mpicom)
+   call mpibcast(modal_strat_sulfate_aod_treatment, 1 , mpilog,  0, mpicom)
+
 #endif
 
    call cam_ctrl_set_physics_type(cam_physpkg)
@@ -596,7 +599,7 @@ subroutine phys_getopts(deep_scheme_out, shallow_scheme_out, eddy_scheme_out, &
                        ,l_tracer_aero_out, l_vdiff_out, l_rayleigh_out, l_gw_drag_out, l_ac_energy_chk_out  &
                        ,l_bc_energy_fix_out, l_dry_adj_out, l_st_mac_out, l_st_mic_out, l_rad_out  &
                        ,prc_coef1_out,prc_exp_out,prc_exp1_out, cld_sed_out,mg_prc_coeff_fix_out,rrtmg_temp_fix_out &
-                       )
+                       ,modal_strat_sulfate_aod_treatment_out)
 
 !-----------------------------------------------------------------------
 ! Purpose: Return runtime settings
@@ -702,6 +705,7 @@ subroutine phys_getopts(deep_scheme_out, shallow_scheme_out, eddy_scheme_out, &
    logical,           intent(out), optional :: l_rad_out
    logical,           intent(out), optional :: mg_prc_coeff_fix_out
    logical,           intent(out), optional :: rrtmg_temp_fix_out
+   logical,           intent(out), optional :: modal_strat_sulfate_aod_treatment_out
    integer,           intent(out), optional :: cld_macmic_num_steps_out
    real(r8),          intent(out), optional :: prc_coef1_out
    real(r8),          intent(out), optional :: prc_exp_out
@@ -811,6 +815,8 @@ subroutine phys_getopts(deep_scheme_out, shallow_scheme_out, eddy_scheme_out, &
    if ( present(cld_sed_out             ) ) cld_sed_out              = cld_sed
    if ( present(mg_prc_coeff_fix_out    ) ) mg_prc_coeff_fix_out     = mg_prc_coeff_fix
    if ( present(rrtmg_temp_fix_out      ) ) rrtmg_temp_fix_out       = rrtmg_temp_fix
+   if ( present(modal_strat_sulfate_aod_treatment_out      ) ) modal_strat_sulfate_aod_treatment_out &
+                                                               = modal_strat_sulfate_aod_treatment
 
 end subroutine phys_getopts
 
