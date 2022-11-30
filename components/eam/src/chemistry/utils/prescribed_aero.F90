f6256eaca87ca7c6a99d817504bbaca8d3abec90
Benjamin Hillman <bhillma@sandia.gov>
2022-11-30 15:42:21 -0800

    Remove hard-coded disabling of prescribed aero perturbation

--- components/eam/src/chemistry/utils/prescribed_aero.F90
+++ components/eam/src/chemistry/utils/prescribed_aero.F90
@@ -481,11 +481,8 @@ end subroutine spec_c_to_a
     ! random sampling is off (randn=0) in single column model setting. 
     if (single_column) then
       randn = 0._r8
-   endif
+    endif
 
-   ! turn off random sampling for scream simulations
-   randn = 0._r8
-    
     do i = 1, aero_cnt
        !Species with '_a' are updated using random sampling.
        !Cloud borne species (ends with _c1,_c2, etc.) have to be skipped
