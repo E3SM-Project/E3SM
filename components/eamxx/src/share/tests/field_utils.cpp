421f9fcec3a94cea5f386b7cd1c3eb1841658857
Luca Bertagna <lbertag@sandia.gov>
2022-12-02 15:22:25 -0700

    EAMxx: added utility to print field hyperslabs

--- components/eamxx/src/share/tests/field_utils.cpp
+++ components/eamxx/src/share/tests/field_utils.cpp
@@ -7,6 +7,7 @@
 #include "share/field/field.hpp"
 #include "share/field/field_manager.hpp"
 #include "share/field/field_utils.hpp"
+#include "share/util/scream_setup_random_test.hpp"
 
 #include "share/grid/point_grid.hpp"
 
@@ -83,7 +84,7 @@ TEST_CASE("utils") {
     auto dim1 = fid.get_layout().dim(1);
     auto lsize = fid.get_layout().size();
     auto gsize = lsize*comm.size();
-    auto offset = comm.rank()*lsize; 
+    auto offset = comm.rank()*lsize;
     Kokkos::parallel_for(kt::RangePolicy(0,dim0*dim1),
                          KOKKOS_LAMBDA(int idx) {
       int i = idx / dim1;
@@ -106,7 +107,7 @@ TEST_CASE("utils") {
     auto dim1 = fid.get_layout().dim(1);
     auto lsize = fid.get_layout().size();
     auto gsize = lsize*comm.size();
-    auto offset = comm.rank()*lsize; 
+    auto offset = comm.rank()*lsize;
     Kokkos::parallel_for(kt::RangePolicy(0,dim0*dim1),
                          KOKKOS_LAMBDA(int idx) {
       int i = idx / dim1;
@@ -133,7 +134,7 @@ TEST_CASE("utils") {
     auto dim1 = fid.get_layout().dim(1);
     auto lsize = fid.get_layout().size();
     auto gsize = lsize*comm.size();
-    auto offset = comm.rank()*lsize; 
+    auto offset = comm.rank()*lsize;
     Kokkos::parallel_for(kt::RangePolicy(0,dim0*dim1),
                          KOKKOS_LAMBDA(int idx) {
       int i = idx / dim1;
@@ -155,7 +156,7 @@ TEST_CASE("utils") {
     auto dim0 = fid.get_layout().dim(0);
     auto dim1 = fid.get_layout().dim(1);
     auto lsize = fid.get_layout().size();
-    auto offset = comm.rank()*lsize; 
+    auto offset = comm.rank()*lsize;
     Kokkos::parallel_for(kt::RangePolicy(0,dim0*dim1),
                          KOKKOS_LAMBDA(int idx) {
       int i = idx / dim1;
@@ -179,6 +180,112 @@ TEST_CASE("utils") {
     REQUIRE_THROWS(field_max<int>(f1));
     REQUIRE_THROWS(field_sum<wrong_real>(f1));
     REQUIRE_THROWS(frobenius_norm<wrong_real>(f1));
+  }
+}
+
+TEST_CASE ("print_field_hyperslab") {
+  using namespace scream;
+  using namespace ekat::units;
+
+  using namespace ShortFieldTagsNames;
+  using RPDF = std::uniform_real_distribution<Real>;
+  using IPDF = std::uniform_int_distribution<int>;
+
+  // Setup random number generation
+  ekat::Comm comm(MPI_COMM_WORLD);
+  auto engine = setup_random_test ();
+  RPDF pdf(0,1);
+
+  const int nel = 2;
+  const int ncmp = 2;
+  const int ngp  = 4;
+  const int nlev = 12;
+  const int iel  = IPDF(0,nel-1)(engine);
+  const int icmp = IPDF(0,ncmp-1)(engine);
+  const int igp  = IPDF(0,ngp-1)(engine);
+  const int jgp  = IPDF(0,ngp-1)(engine);
+  const int ilev = IPDF(0,nlev-1)(engine);
+
+  // WARNING: make sure this value matches the one used in print_field_hyperslab
+  constexpr int max_per_line = 5;
+
+  // Create field (if available, use packs, to ensure we don't print garbage)
+  std::vector<FieldTag> tags = {EL, CMP, GP, GP, LEV};
+  std::vector<int>      dims = {nel,ncmp,ngp,ngp,nlev};
+  int pack_size = std::min(SCREAM_PACK_SIZE,4);
+
+  FieldIdentifier fid ("f", {tags,dims}, kg, "some_grid");
+  Field f (fid);
+  f.get_header().get_alloc_properties().request_allocation(pack_size);
+  f.allocate_view();
+  randomize (f,engine,pdf);
+
+  auto v = f.get_view<const Real*****,Host>();
+
+  SECTION ("slice_0") {
+    std::vector<FieldTag> loc_tags = {EL,CMP};
+    std::vector<int>      loc_idxs = {iel,icmp};
+    std::stringstream out;
+    print_field_hyperslab(f,loc_tags,loc_idxs,out);
+
+    std::stringstream expected;
+    expected <<
+      " -----------------------------------------------------------------------\n"
+      "     f" << to_string(fid.get_layout()) << "\n"
+      " -----------------------------------------------------------------------\n";
+      for (int gp1=0; gp1<ngp; ++gp1) {
+        for (int gp2=0; gp2<ngp; ++gp2) {
+          expected <<  "\n  f(" << iel << "," << icmp << "," << gp1 << "," << gp2 << ",:)";
+          for (int lev=0; lev<nlev; ++lev) {
+            if (lev % max_per_line == 0) {
+              expected << "\n    ";
+            }
+            expected << v(iel,icmp,gp1,gp2,lev) << ", ";
+          }
+        }
+      }
+      expected << "\n -----------------------------------------------------------------------\n";
+
+    REQUIRE (out.str()==expected.str());
+  }
+  SECTION ("slice_0234") {
+    std::vector<FieldTag> loc_tags = {EL,GP,GP,LEV};
+    std::vector<int>      loc_idxs = {iel,igp,jgp,ilev};
+    std::stringstream out;
+    print_field_hyperslab(f,loc_tags,loc_idxs,out);
+
+    std::stringstream expected;
+    expected <<
+      " -----------------------------------------------------------------------\n"
+      "     f" << to_string(fid.get_layout()) << "\n"
+      " -----------------------------------------------------------------------\n";
+      expected <<  "\n  f(" << iel << ",:," << igp << "," << jgp << "," << ilev << ")";
+      for (int cmp=0; cmp<ncmp; ++cmp) {
+        if (cmp % max_per_line == 0) {
+          expected << "\n    ";
+        }
+        expected << v(iel,cmp,igp,jgp,ilev) << ", ";
+      }
+      expected << "\n -----------------------------------------------------------------------\n";
+
+    REQUIRE (out.str()==expected.str());
+  }
+  SECTION ("slice_01234") {
+    std::vector<FieldTag> loc_tags = {EL,CMP,GP,GP,LEV};
+    std::vector<int>      loc_idxs = {iel,icmp,igp,jgp,ilev};
+    std::stringstream out;
+    print_field_hyperslab(f,loc_tags,loc_idxs,out);
+
+    std::stringstream expected;
+    expected <<
+      " -----------------------------------------------------------------------\n"
+      "     f" << to_string(fid.get_layout()) << "\n"
+      " -----------------------------------------------------------------------\n";
+      expected <<  "\n  f(" << ekat::join(loc_idxs,",") << ")";
+      expected << "\n    " << v(iel,icmp,igp,jgp,ilev) << ", ";
+      expected << "\n -----------------------------------------------------------------------\n";
+
+    REQUIRE (out.str()==expected.str());
   }
 }
 
