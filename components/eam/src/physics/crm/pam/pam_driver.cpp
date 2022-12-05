3f0d4ede3d5e0a9a8503c627066d6642c8993dc9
Hannah <hannah6@ml-9963137.the-lab.llnl.gov>
2022-12-05 10:31:38 -0700

    misc PAM interface updates

--- components/eam/src/physics/crm/pam/pam_driver.cpp
+++ components/eam/src/physics/crm/pam/pam_driver.cpp
@@ -5,6 +5,8 @@
 #include "pam_interface.h"
 #include "perturb_temperature.h"
 #include "gcm_forcing.h"
+#include "pam_feedback.h"
+#include "pam_state_update.h"
 #include "sponge_layer.h"
 #include "saturation_adjustment.h"
 #include "broadcast_initial_gcm_column.h"
@@ -14,144 +16,111 @@ extern "C" void pam_driver() {
   //------------------------------------------------------------------------------------------------
   using yakl::intrinsics::abs;
   using yakl::intrinsics::maxval;
-  //------------------------------------------------------------------------------------------------
-  auto &coupler = pam_interface::get_coupler();
+
   //------------------------------------------------------------------------------------------------
   // retreive coupler options
-  auto ncrms       = coupler.get_option<int>('ncrms');
+  auto nens        = coupler.get_option<int>('ncrms');
   auto gcm_nlev    = coupler.get_option<int>('gcm_nlev');
   auto crm_nz      = coupler.get_option<int>('crm_nz');
   auto crm_nx      = coupler.get_option<int>('crm_nx');
   auto crm_ny      = coupler.get_option<int>('crm_ny');
   auto crm_dx      = coupler.get_option<double>('crm_dx');
   auto crm_dy      = coupler.get_option<double>('crm_dy');
+  // time step increments
   auto gcm_dt      = coupler.get_option<double>('gcm_dt');
   auto crm_dt      = coupler.get_option<double>('crm_dt');
-  //------------------------------------------------------------------------------------------------
-  // TODO: add check to ensure crm_dt evenly divides gcm_dt
+  // flag to determine whether to perturb CRM state
+  auto is_first_step = coupler.get_option<double>('is_first_step');
   //------------------------------------------------------------------------------------------------
   // Allocate the coupler state and retrieve host/device data managers
+  auto &coupler = pam_interface::get_coupler();
   coupler.allocate_coupler_state( crm_nz , crm_ny , crm_nx , nens );
   auto &dm_device = coupler.get_data_manager_readwrite();
   auto &dm_host   = coupler.get_data_manager_host_readwrite();
   //------------------------------------------------------------------------------------------------
-  // wrap host data in YAKL array
-  auto input_bflxls        = dm_host.get<real const,2>("input_bflxls").createDeviceCopy();
-  auto input_wndls         = dm_host.get<real const,2>("input_wndls").createDeviceCopy();
-  auto input_zmid          = dm_host.get<real const,2>("input_zmid").createDeviceCopy();
-  auto input_zint          = dm_host.get<real const,2>("input_zint").createDeviceCopy();
-  auto input_pmid          = dm_host.get<real const,2>("input_pmid").createDeviceCopy();
-  auto input_pint          = dm_host.get<real const,2>("input_pint").createDeviceCopy();
-  auto input_pdel          = dm_host.get<real const,2>("input_pdel").createDeviceCopy();
-  auto input_ul            = dm_host.get<real const,2>("input_ul").createDeviceCopy();
-  auto input_vl            = dm_host.get<real const,2>("input_vl").createDeviceCopy();
-  auto input_tl            = dm_host.get<real const,2>("input_tl").createDeviceCopy();
-  auto input_qccl          = dm_host.get<real const,2>("input_qccl").createDeviceCopy();
-  auto input_qiil          = dm_host.get<real const,2>("input_qiil").createDeviceCopy();
-  auto input_ql            = dm_host.get<real const,2>("input_ql").createDeviceCopy();
-  auto input_tau00         = dm_host.get<real const,2>("input_tau00").createDeviceCopy();
-  auto input_ul_esmt       = dm_host.get<real const,2>("input_ul_esmt").createDeviceCopy();
-  auto input_vl_esmt       = dm_host.get<real const,2>("input_vl_esmt").createDeviceCopy();
-  auto input_t_vt          = dm_host.get<real const,2>("input_t_vt").createDeviceCopy();
-  auto input_q_vt          = dm_host.get<real const,2>("input_q_vt").createDeviceCopy();
-  auto input_u_vt          = dm_host.get<real const,2>("input_u_vt").createDeviceCopy();
+  // wrap host data in YAKL arrays
+  auto gcolp = dm_host.get<real const,1>("gcolp").createDeviceCopy();
 
-  auto state_u_wind        = dm_host.get<real const,2>("state_u_wind").createDeviceCopy();
-  auto state_v_wind        = dm_host.get<real const,2>("state_v_wind").createDeviceCopy();
-  auto state_w_wind        = dm_host.get<real const,2>("state_w_wind").createDeviceCopy();
-  auto state_temperature   = dm_host.get<real const,2>("state_temperature").createDeviceCopy();
-  auto state_qt            = dm_host.get<real const,2>("state_qt").createDeviceCopy();
-  auto state_qp            = dm_host.get<real const,2>("state_qp").createDeviceCopy();
-  auto state_qn            = dm_host.get<real const,2>("state_qn").createDeviceCopy();
+  // auto input_bflxls        = dm_host.get<real const,2>("input_bflxls").createDeviceCopy();
+  // auto input_wndls         = dm_host.get<real const,2>("input_wndls").createDeviceCopy();
+  // auto input_zmid          = dm_host.get<real const,2>("input_zmid").createDeviceCopy();
+  // auto input_zint          = dm_host.get<real const,2>("input_zint").createDeviceCopy();
+  // auto input_pmid          = dm_host.get<real const,2>("input_pmid").createDeviceCopy();
+  // auto input_pint          = dm_host.get<real const,2>("input_pint").createDeviceCopy();
+  // auto input_pdel          = dm_host.get<real const,2>("input_pdel").createDeviceCopy();
+  // auto input_tau00         = dm_host.get<real const,2>("input_tau00").createDeviceCopy();
 
   //------------------------------------------------------------------------------------------------
-  // define GCM state used for forcing
-  auto gcm_rho_d = dm.get<real,2>("gcm_density_dry");
-  auto gcm_rho_v = dm.get<real,2>("gcm_water_vapor");
-  auto gcm_uvel  = dm.get<real,2>("gcm_uvel"       );
-  auto gcm_vvel  = dm.get<real,2>("gcm_vvel"       );
-  auto gcm_wvel  = dm.get<real,2>("gcm_wvel"       );
-  auto gcm_temp  = dm.get<real,2>("gcm_temp"       );
-
-  parallel_for( Bounds<2>(crm_nz,nens) , YAKL_LAMBDA (int k, int iens) {
-    gcm_rho_d(k,iens) = rho_d_col(k);
-    gcm_rho_v(k,iens) = rho_v_col(k);
-    gcm_uvel (k,iens) = uvel_col (k);
-    gcm_vvel (k,iens) = vvel_col (k);
-    gcm_wvel (k,iens) = wvel_col (k);
-    gcm_temp (k,iens) = temp_col (k);
-  });
-  //------------------------------------------------------------------------------------------------
-  // NORMALLY THIS WOULD BE DONE INSIDE THE CRM, BUT WE'RE USING CONSTANTS DEFINED BY THE CRM MICRO SCHEME
-  // Create the dycore and the microphysics
+  // Create objects for dycor, microphysics, and turbulence
   Dycore       dycore;
   Microphysics micro;
   SGS          sgs;
-
-  // Set physical constants for coupler at thread 0 using microphysics data
-  coupler.set_phys_constants( micro.R_d , micro.R_v , micro.cp_d , micro.cp_v , micro.grav , micro.p0 );
-
-  // Set the vertical grid in the coupler
-  coupler.set_grid( crm_dx , crm_dy , input_zint );
-
+  //------------------------------------------------------------------------------------------------
+  // Initialize dycor, microphysics, and turbulence
+  dycore.init( coupler );
   micro .init( coupler );
   sgs   .init( coupler );
-  dycore.init( coupler );
-
-  // Initialize the CRM internal state from the initial GCM column and random temperature perturbations
-  modules::broadcast_initial_gcm_column( coupler );
-
-  // Now that we have an initial state, define hydrostasis for each ensemble member
+  //------------------------------------------------------------------------------------------------
+  // Set physical constants for coupler at thread 0 using microphysics data
+  coupler.set_phys_constants( micro.R_d , micro.R_v , micro.cp_d , micro.cp_v , micro.grav , micro.p0 );
+  //------------------------------------------------------------------------------------------------
+  // Set the vertical grid in the coupler
+  coupler.set_grid( crm_dx , crm_dy , input_zint );
+  //------------------------------------------------------------------------------------------------
+  // Define hydrostasis (only for PAM-A / AWFL dycor)
   coupler.update_hydrostasis();
+  //------------------------------------------------------------------------------------------------
+  // update the coupler GCM state variables using the input GCM state
+  update_gcm_state( coupler )
+  //------------------------------------------------------------------------------------------------
+  // Copy the input CRM state saved by the GCM to the PAM coupler state
+  copy_input_state_to_coupler( coupler );
+  //------------------------------------------------------------------------------------------------
+  // Compute CRM forcing tendencies
+  modules::compute_gcm_forcing_tendencies( coupler, gcm_dt );
+  //------------------------------------------------------------------------------------------------
+  // Perturb the CRM only at the beginning of the run
+  if (is_first_step) { modules::perturb_temperature( coupler , gcolp ); }
+  //------------------------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------------------------
+  // Run the CRM
+  real etime_crm = 0;
+  while (etime_crm < gcm_dt) {
+    if (crm_dt == 0.) { crm_dt = dycore.compute_time_step(coupler); }
+    if (etime_crm + crm_dt > simTime_crm) { crm_dt = simTime_crm - etime_crm; }
 
-  modules::perturb_temperature( coupler , 0 );
+    coupler.run_module( "apply_gcm_forcing_tendencies" , modules::apply_gcm_forcing_tendencies );
+    coupler.run_module( "apply_rad_forcing_tendencies" , modules::apply_rad_forcing_tendencies );
+    coupler.run_module( "sponge_layer"                 , modules::sponge_layer );
+    coupler.run_module( "dycore"                       , [&] (pam::PamCoupler &coupler) {dycore.timeStep(coupler);} );
+    coupler.run_module( "sgs"                          , [&] (pam::PamCoupler &coupler) {sgs   .timeStep(coupler);} );
+    coupler.run_module( "micro"                        , [&] (pam::PamCoupler &coupler) {micro .timeStep(coupler);} );
 
-  coupler.add_pam_function( "apply_gcm_forcing_tendencies" , modules::apply_gcm_forcing_tendencies );
-  coupler.add_pam_function( "dycore" , [&] (pam::PamCoupler &coupler, real dt) { dycore.timeStep(coupler,dt); } );
-  coupler.add_pam_function( "sgs"    , [&] (pam::PamCoupler &coupler, real dt) { sgs   .timeStep(coupler,dt); } );
-  coupler.add_pam_function( "micro"  , [&] (pam::PamCoupler &coupler, real dt) { micro .timeStep(coupler,dt); } );
-  coupler.add_pam_function( "sponge_layer"                 , modules::sponge_layer                 );
-  // coupler.add_dycore_function( "saturation_adjustment" , saturation_adjustment );
-
-  yakl::timer_start("main_loop");
-  while (etime_gcm < simTime) {
-    if (etime_gcm + gcm_dt > simTime) { gcm_dt = simTime - etime_gcm; }
-
-    modules::compute_gcm_forcing_tendencies( coupler , gcm_dt );
-
-    real etime_crm = 0;
-    real simTime_crm = gcm_dt;
-    while (etime_crm < simTime_crm) {
-      if (crm_dt == 0.) { crm_dt = dycore.compute_time_step(coupler); }
-      if (etime_crm + crm_dt > simTime_crm) { crm_dt = simTime_crm - etime_crm; }
-
-      coupler.run_pam_function( "apply_gcm_forcing_tendencies" , crm_dt );
-      coupler.run_pam_function( "dycore"                       , crm_dt );
-      coupler.run_pam_function( "sponge_layer"                 , crm_dt );
-      coupler.run_pam_function( "sgs"                          , crm_dt );
-      coupler.run_pam_function( "micro"                        , crm_dt );
-
-      etime_crm += crm_dt;
-      etime_gcm += crm_dt;
-      // if (out_freq >= 0. && etime_gcm / out_freq >= num_out+1) {
-      //   output( coupler , out_prefix , etime_gcm );
-      //   real maxw = maxval(abs(dm.get_collapsed<real const>("wvel")));
-      //   if (mainproc) {
-      //     std::cout << "Etime , dtphys, maxw: " << etime_gcm << " , " 
-      //                                           << crm_dt    << " , "
-      //                                           << std::setw(10) << maxw << std::endl;
-      //   }
-      //   num_out++;
-      // }
-    }
+    etime_crm += crm_dt;
   }
+  //------------------------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------------------------
+  //------------------------------------------------------------------------------------------------
+  // Compute primary feedback tendencies for GCM
+  compute_crm_feedback_tendencies( coupler, gcm_dt )
+  
+  // Compute horizontal means of CRM state variables that are not forced
+  compute_crm_mean_state( coupler )
 
-  if (mainproc) {
-    std::cout << "Elapsed Time: " << etime_gcm << "\n";
-  }
-
+  //------------------------------------------------------------------------------------------------
+  // Output aggregated surface flux of water specices
+  // precip_liq = dm_device.get<real const,2>("p3_output_prec_liq")
+  // precip_ice = dm_device.get<real const,2>("p3_output_prec_ice")
+  // precip_tot = precip_liq + precip_ice
+  // auto precip_tot_host = dm_host.get<real,1>("output_precc");
+  // auto precip_liq_host = dm_host.get<real,1>("output_precl");
+  // auto precip_ice_host = dm_host.get<real,1>("output_precsc");
+  // precip_tot.deep_copy_to(precip_tot_host);
+  // precip_liq.deep_copy_to(precip_liq_host);
+  // precip_ice.deep_copy_to(precip_ice_host);
+  //------------------------------------------------------------------------------------------------
+  // Finalize the coupler and clean up
   dycore.finalize( coupler );
-
   pam_interface::finalize();
-
 }
---
+++ components/eam/src/physics/crm/pam/pam_feedback.h
@@ -0,0 +1,185 @@
+#pragma once
+
+#include "pam_coupler.h"
+
+// These routines are only called once at the end of the CRM call
+// to provide the tendencies and fields to couple the CRM and GCM
+
+// Terminology: 
+//     - GCM state at the beginning of the CRM call:       state_gcm
+//     - instantaneous horizontally-averaged CRM state:    state_crm
+// The GCM forces the CRM in the MMF by computing 
+//     (state_gcm - state_crm) / gcm_dt
+// The forcing is applied to the CRM state at each CRM physics time step. 
+// Similarly, the CRM provides a feedback tendency to the GCM by computing
+//     (state_crm - state_gcm) / gcm_dt
+// using the final state of the CRM at the end of the integration
+
+
+// Compute feedback tendencies for the GCM
+inline void compute_crm_feedback_tendencies( pam::PamCoupler &coupler , real gcm_dt ) {
+  using yakl::c::parallel_for;
+  using yakl::c::SimpleBounds;
+  using yakl::atomicAdd;
+  auto &dm_device = coupler.get_data_manager_readwrite();
+  auto &dm_host   = coupler.get_data_manager_host_readwrite();
+
+  int nz   = dm_device.get_dimension_size("z"   );
+  int ny   = dm_device.get_dimension_size("y"   );
+  int nx   = dm_device.get_dimension_size("x"   );
+  int nens = dm_device.get_dimension_size("nens");
+  //------------------------------------------------------------------------------------------------
+  // Get current CRM state
+  auto rho_d = dm_device.get<real,4>("density_dry");
+  auto uvel  = dm_device.get<real,4>("uvel"       );
+  auto vvel  = dm_device.get<real,4>("vvel"       );
+  auto temp  = dm_device.get<real,4>("temp"       );
+  auto rho_v = dm_device.get<real,4>("water_vapor");
+  auto rho_l = dm_device.get<real,4>("cloud_water");
+  auto rho_i = dm_device.get<real,4>("ice"        );
+  //------------------------------------------------------------------------------------------------
+  // Get input GCM state
+  auto gcm_ul   = dm_host.get<real const,2>("input_ul").createDeviceCopy();
+  auto gcm_vl   = dm_host.get<real const,2>("input_vl").createDeviceCopy();
+  auto gcm_tl   = dm_host.get<real const,2>("input_tl").createDeviceCopy();
+  auto gcm_ql   = dm_host.get<real const,2>("input_ql").createDeviceCopy();
+  auto gcm_qccl = dm_host.get<real const,2>("input_qccl").createDeviceCopy();
+  auto gcm_qiil = dm_host.get<real const,2>("input_qiil").createDeviceCopy();
+  //------------------------------------------------------------------------------------------------
+  // Create arrays to hold the current column average of the CRM internal columns
+  real2d crm_hmean_rho_d("crm_hmean_rho_d",nz,nens);
+  real2d crm_hmean_uvel ("crm_hmean_uvel" ,nz,nens);
+  real2d crm_hmean_vvel ("crm_hmean_vvel" ,nz,nens);
+  real2d crm_hmean_temp ("crm_hmean_temp" ,nz,nens);
+  real2d crm_hmean_rho_v("crm_hmean_rho_v",nz,nens);
+  real2d crm_hmean_rho_l("crm_hmean_rho_l",nz,nens);
+  real2d crm_hmean_rho_i("crm_hmean_rho_i",nz,nens);
+  //------------------------------------------------------------------------------------------------
+  // We will be essentially reducing a summation to these variables, so initialize them to zero
+  parallel_for("Initialize horzontal means", SimpleBounds<2>(nz,nens), YAKL_LAMBDA (int k, int iens) {
+    crm_hmean_rho_d(k,iens) = 0;
+    crm_hmean_uvel (k,iens) = 0;
+    crm_hmean_vvel (k,iens) = 0;
+    crm_hmean_temp (k,iens) = 0;
+    crm_hmean_rho_v(k,iens) = 0;
+    crm_hmean_rho_l(k,iens) = 0;
+    crm_hmean_rho_i(k,iens) = 0;
+  });
+  //------------------------------------------------------------------------------------------------
+  // Compute horizontal means
+  real r_nx_ny  = 1._fp / (nx*ny);  // precompute reciprocal to avoid costly divisions
+  parallel_for("Horz mean of CRM state", SimpleBounds<4>(nz,ny,nx,nens), YAKL_LAMBDA (int k, int j, int i, int iens) {
+    // yakl::atomicAdd ensures only one thread performs an update at a time to avoid data races and wrong answers
+    atomicAdd( crm_hmean_rho_d(k,iens), rho_d(k,j,i,iens) * r_nx_ny );
+    atomicAdd( crm_hmean_uvel (k,iens), uvel (k,j,i,iens) * r_nx_ny );
+    atomicAdd( crm_hmean_vvel (k,iens), vvel (k,j,i,iens) * r_nx_ny );
+    atomicAdd( crm_hmean_temp (k,iens), temp (k,j,i,iens) * r_nx_ny );
+    atomicAdd( crm_hmean_rho_v(k,iens), rho_v(k,j,i,iens) * r_nx_ny );
+    atomicAdd( crm_hmean_rho_l(k,iens), rho_l(k,j,i,iens) * r_nx_ny );
+    atomicAdd( crm_hmean_rho_i(k,iens), rho_i(k,j,i,iens) * r_nx_ny );
+  });
+  //------------------------------------------------------------------------------------------------
+  // Compute feedback tendencies
+  real2d crm_feedback_tend_uvel("",nz,nens);
+  real2d crm_feedback_tend_vvel("",nz,nens);
+  real2d crm_feedback_tend_dse ("",nz,nens);
+  real2d crm_feedback_tend_qv  ("",nz,nens);
+  real2d crm_feedback_tend_ql  ("",nz,nens);
+  real2d crm_feedback_tend_qi  ("",nz,nens);
+  real micro_cp_d = coupler.get_option("micro_cp_d");
+  real r_gcm_dt = 1._fp / gcm_dt;  // precompute reciprocal to avoid costly divisions
+  parallel_for( "Compute CRM feedback tendencies", SimpleBounds<2>(nz,nens), YAKL_LAMBDA (int k, int iens) {
+    real crm_dse = crm_hmean_temp (k,iens) * micro_cp_d;
+    real crm_qv  = crm_hmean_rho_v(k,iens) / (crm_hmean_rho_d(k,iens) + crm_hmean_rho_v(k,iens));
+    real crm_ql  = crm_hmean_rho_l(k,iens) / (crm_hmean_rho_d(k,iens) + crm_hmean_rho_l(k,iens));
+    real crm_qi  = crm_hmean_rho_i(k,iens) / (crm_hmean_rho_d(k,iens) + crm_hmean_rho_i(k,iens));
+    crm_feedback_tend_uvel(k,iens) = ( crm_hmean_uvel (k,iens) - gcm_ul  (k,iens) ) * r_gcm_dt;
+    crm_feedback_tend_vvel(k,iens) = ( crm_hmean_vvel (k,iens) - gcm_vl  (k,iens) ) * r_gcm_dt;
+    crm_feedback_tend_dse (k,iens) = ( crm_dse                 - gcm_tl  (k,iens) ) * r_gcm_dt;
+    crm_feedback_tend_qv  (k,iens) = ( crm_qv                  - gcm_ql  (k,iens) ) * r_gcm_dt;
+    crm_feedback_tend_ql  (k,iens) = ( crm_ql                  - gcm_qccl(k,iens) ) * r_gcm_dt;
+    crm_feedback_tend_qi  (k,iens) = ( crm_qi                  - gcm_qiil(k,iens) ) * r_gcm_dt;
+  });
+  //------------------------------------------------------------------------------------------------
+  // Copy the CRM feedback tendencies to host arrays
+  auto output_sltend_host  = dm_host.get<real,1>("output_sltend");
+  auto output_qltend_host  = dm_host.get<real,1>("output_qltend");
+  auto output_qcltend_host = dm_host.get<real,1>("output_qcltend");
+  auto output_qiltend_host = dm_host.get<real,1>("output_qiltend");
+  auto output_ultend_host  = dm_host.get<real,1>("output_ultend");
+  auto output_vltend_host  = dm_host.get<real,1>("output_vltend");
+  crm_feedback_tend_uvel.deep_copy_to(output_ultend_host);
+  crm_feedback_tend_vvel.deep_copy_to(output_vltend_host);
+  crm_feedback_tend_dse .deep_copy_to(output_sltend_host);
+  crm_feedback_tend_qv  .deep_copy_to(output_qltend_host);
+  crm_feedback_tend_ql  .deep_copy_to(output_qcltend_host);
+  crm_feedback_tend_qi  .deep_copy_to(output_qiltend_host);
+  //------------------------------------------------------------------------------------------------
+}
+
+
+// Compute horizontal means for feedback tendencies of variables that are not forced
+inline void compute_crm_mean_state( pam::PamCoupler &coupler ) {
+  using yakl::c::parallel_for;
+  using yakl::c::SimpleBounds;
+  using yakl::atomicAdd;
+  auto &dm_device = coupler.get_data_manager_readwrite();
+  auto &dm_host   = coupler.get_data_manager_host_readwrite();
+
+  int nz   = dm_device.get_dimension_size("z"   );
+  int ny   = dm_device.get_dimension_size("y"   );
+  int nx   = dm_device.get_dimension_size("x"   );
+  int nens = dm_device.get_dimension_size("nens");
+  //------------------------------------------------------------------------------------------------
+  // Get current CRM state
+  auto nc = dm_device.get<real,4>("cloud_water_num");
+  auto ni = dm_device.get<real,4>("ice_num");
+  auto qr = dm_device.get<real,4>("rain");
+  auto nr = dm_device.get<real,4>("rain_num");
+  auto qm = dm_device.get<real,4>("ice_rime");
+  auto bm = dm_device.get<real,4>("ice_rime_vol");
+  //------------------------------------------------------------------------------------------------
+  // Create arrays to hold the current column average of the CRM internal columns
+  real2d nc_mean("nc_mean",nz,nens);
+  real2d ni_mean("ni_mean",nz,nens);
+  real2d qr_mean("qr_mean",nz,nens);
+  real2d nr_mean("nr_mean",nz,nens);
+  real2d qm_mean("qm_mean",nz,nens);
+  real2d bm_mean("bm_mean",nz,nens);
+  //------------------------------------------------------------------------------------------------
+  // We will be essentially reducing a summation to these variables, so initialize them to zero
+  parallel_for("Initialize horzontal means", SimpleBounds<2>(nz,nens), YAKL_LAMBDA (int k, int iens) {
+    nc_mean(k,iens) = 0;
+    ni_mean(k,iens) = 0;
+    qr_mean(k,iens) = 0;
+    nr_mean(k,iens) = 0;
+    qm_mean(k,iens) = 0;
+    bm_mean(k,iens) = 0;
+  });
+  //------------------------------------------------------------------------------------------------
+  // Compute horizontal means
+  real r_nx_ny  = 1._fp / (nx*ny);  // precompute reciprocal to avoid costly divisions
+  parallel_for("Horz mean of CRM state", SimpleBounds<4>(nz,ny,nx,nens), YAKL_LAMBDA (int k, int j, int i, int iens) {
+    // yakl::atomicAdd ensures only one thread performs an update at a time to avoid data races and wrong answers
+    atomicAdd( nc_mean(k,iens), nc(k,j,i,iens) * r_nx_ny );
+    atomicAdd( ni_mean(k,iens), ni(k,j,i,iens) * r_nx_ny );
+    atomicAdd( qr_mean(k,iens), qr(k,j,i,iens) * r_nx_ny );
+    atomicAdd( nr_mean(k,iens), nr(k,j,i,iens) * r_nx_ny );
+    atomicAdd( qm_mean(k,iens), qm(k,j,i,iens) * r_nx_ny );
+    atomicAdd( bm_mean(k,iens), bm(k,j,i,iens) * r_nx_ny );
+  });
+  //------------------------------------------------------------------------------------------------
+  // Copy the CRM mean data to host arrays
+  auto output_nc_mean = dm_host.get<real,1>("output_nc_mean");
+  auto output_ni_mean = dm_host.get<real,1>("output_ni_mean");
+  auto output_qr_mean = dm_host.get<real,1>("output_qr_mean");
+  auto output_nr_mean = dm_host.get<real,1>("output_nr_mean");
+  auto output_qm_mean = dm_host.get<real,1>("output_qm_mean");
+  auto output_bm_mean = dm_host.get<real,1>("output_bm_mean");
+  nc_mean.deep_copy_to(output_nc_mean);
+  ni_mean.deep_copy_to(output_ni_mean);
+  qr_mean.deep_copy_to(output_qr_mean);
+  nr_mean.deep_copy_to(output_nr_mean);
+  qm_mean.deep_copy_to(output_qm_mean);
+  bm_mean.deep_copy_to(output_bm_mean);
+}
