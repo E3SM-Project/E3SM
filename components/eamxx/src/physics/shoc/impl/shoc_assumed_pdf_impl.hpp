0e55af7186aa477128c47f99549a164dcb317764
Peter Bogenschutz <bogenschutz1@llnlg.ov>
2022-12-09 11:05:55 -0800

    add NaN checks for Tl1_1

--- components/eamxx/src/physics/shoc/impl/shoc_assumed_pdf_impl.hpp
+++ components/eamxx/src/physics/shoc/impl/shoc_assumed_pdf_impl.hpp
@@ -102,6 +102,7 @@ void Functions<S,D>::shoc_assumed_pdf(
     const auto qwthlsec = qwthl_sec_zt(k);
     const auto wqwsec = wqw_sec_zt(k);
     const auto wthlsec = wthl_sec_zt(k);
+    const auto w2sec = w_sec(k);
 
     // Compute square roots of some variables so we don't have to compute these again
     const auto sqrtw2 = ekat::sqrt(w_sec(k));
@@ -228,6 +229,27 @@ void Functions<S,D>::shoc_assumed_pdf(
 
       const auto index_range = ekat::range<IntSmallPack>(k*Spack::n);
       const Smask active_entries = (index_range < nlev);
+
+      // Do NaN Checking here
+      const Smask is_nan_Tl1_1 = (Tl1_1 < 160.0) && active_entries;
+      //const Smask is_nan_Tl1_1 = (Tl1_1 != Tl1_1) && active_entries;
+      //     const Smask is_nan_Tl1_2 = (Tl1_2 != Tl1_2) && active_entries;
+      if (is_nan_Tl1_1.any()) {
+        printf("WARNING: NaN Detected in Tl1_1! \n");
+	for (int i=0; i<is_nan_Tl1_1.n; i++) {
+          if (is_nan_Tl1_1[i]) {
+	    //	    printf("Tl1_1 NaN Stack trace:",thl_first[i],qw_first[i],w3var[i],thlsec[i],qwsec[i],sqrtw2[i]);
+	    printf("Tl1_1 NaN Detected, thetal, qw, temp, pressure, thl_sec, qw_sec, w2sec, lev \n");
+            printf(" %4.9f, %2.9f, %4.9f, %8.9f, %4.9f, %2.9f, %4.9f, %d \n",thl_first[i], qw_first[i], Tl1_g[i], pval[i], thlsec[i], qwsec[i], w2sec[i], k);
+	    printf("Tl1_1 NaN Detected, w_first, w3, qwthlsec, wqwsec, wthlsec, a \n");
+            printf(" %2.9f, %4.9f, %2.9f, %2.9f, %2.9f, %2.9f, \n",w_first[i], w3var[i], qwthlsec[i], wqwsec[i], wthlsec[i], a[i]);
+	    printf("Tl1_1 NaN Detected, w1_1, w1_2, w2_1, w2_2, thl1_1, thl1_2, thl2_1, thl2_2 \n");
+            printf(" %2.9f, %2.9f, %2.9f, %2.9f, %4.9f, %4.9f, %4.9f, %4.9f \n",w1_1[i], w1_2[i], w2_1[i], w2_2[i], thl1_1[i], thl1_2[i], thl2_1[i], thl2_2[i]);
+	    printf("Tl1_1 NaN Detected, qw1_1, qw1_2, qw2_1, qw2_2 \n");
+            printf(" %2.9f, %2.9f, %2.9f, %2.9f \n",qw1_1[i], qw1_2[i], qw2_1[i], qw2_2[i]);
+	  }
+	}
+      }
 
       // Check to ensure Tl1_1 and Tl1_2 are not negative, temporarily set to grid mean value if so.
       const Smask is_neg_Tl1_1 = (Tl1_1 <= 0) && active_entries;
