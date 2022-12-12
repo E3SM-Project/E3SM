2a58a73e691a4fb3822f93d15666288d667c6eac
Walter Hannah <hannah6@llnl.gov>
2022-12-12 14:39:58 -0800

    add PAM params

---
+++ components/eam/src/physics/crm/pam/params.F90
@@ -0,0 +1,7 @@
+module params
+  use iso_c_binding
+  implicit none
+  integer, parameter :: crm_rknd = c_double
+  integer, parameter :: crm_iknd = c_int
+  integer, parameter :: crm_lknd = c_bool
+end module params
