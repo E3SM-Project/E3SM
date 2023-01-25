f18963a81a7567b09ede6502567266f860b7eca5
Walter Hannah <hannah6@llnl.gov>
2023-01-24 16:17:08 -0800

    update PAM feedback routines

--- components/eam/src/physics/crm/pam/pam_feedback.h
+++ components/eam/src/physics/crm/pam/pam_feedback.h
@@ -23,9 +23,9 @@ inline void pam_feedback_compute_crm_feedback_tendencies( pam::PamCoupler &coupl
   using yakl::atomicAdd;
   auto &dm_device = coupler.get_data_manager_device_readwrite();
   auto &dm_host   = coupler.get_data_manager_host_readwrite();
-  int nz       = dm_device.get_dimension_size("z"   );
-  int ny       = dm_device.get_dimension_size("y"   );
-  int nx       = dm_device.get_dimension_size("x"   );
+  int crm_nz   = dm_device.get_dimension_size("z"   );
+  int crm_ny   = dm_device.get_dimension_size("y"   );
+  int crm_nx   = dm_device.get_dimension_size("x"   );
   int nens     = dm_device.get_dimension_size("nens");
   int gcm_nlev = coupler.get_option<int>("gcm_nlev");
   //------------------------------------------------------------------------------------------------
@@ -47,36 +47,34 @@ inline void pam_feedback_compute_crm_feedback_tendencies( pam::PamCoupler &coupl
   auto gcm_qiil = dm_host.get<real const,2>("input_qiil").createDeviceCopy();
   //------------------------------------------------------------------------------------------------
   // Create arrays to hold the current column average of the CRM internal columns
-  real2d crm_hmean_rho_d("crm_hmean_rho_d",nz,nens);
-  real2d crm_hmean_uvel ("crm_hmean_uvel" ,nz,nens);
-  real2d crm_hmean_vvel ("crm_hmean_vvel" ,nz,nens);
-  real2d crm_hmean_temp ("crm_hmean_temp" ,nz,nens);
-  real2d crm_hmean_rho_v("crm_hmean_rho_v",nz,nens);
-  real2d crm_hmean_rho_l("crm_hmean_rho_l",nz,nens);
-  real2d crm_hmean_rho_i("crm_hmean_rho_i",nz,nens);
+  real2d crm_hmean_uvel ("crm_hmean_uvel" ,crm_nz,nens);
+  real2d crm_hmean_vvel ("crm_hmean_vvel" ,crm_nz,nens);
+  real2d crm_hmean_temp ("crm_hmean_temp" ,crm_nz,nens);
+  real2d crm_hmean_qv   ("crm_hmean_qv"   ,crm_nz,nens);
+  real2d crm_hmean_ql   ("crm_hmean_ql"   ,crm_nz,nens);
+  real2d crm_hmean_qi   ("crm_hmean_qi"   ,crm_nz,nens);
   //------------------------------------------------------------------------------------------------
   // We will be essentially reducing a summation to these variables, so initialize them to zero
-  parallel_for("Initialize horzontal means", SimpleBounds<2>(nz,nens), YAKL_LAMBDA (int k, int iens) {
-    crm_hmean_rho_d(k,iens) = 0;
-    crm_hmean_uvel (k,iens) = 0;
-    crm_hmean_vvel (k,iens) = 0;
-    crm_hmean_temp (k,iens) = 0;
-    crm_hmean_rho_v(k,iens) = 0;
-    crm_hmean_rho_l(k,iens) = 0;
-    crm_hmean_rho_i(k,iens) = 0;
+  parallel_for("Initialize horzontal means", SimpleBounds<2>(crm_nz,nens), YAKL_LAMBDA (int k_crm, int iens) {
+    crm_hmean_uvel (k_crm,iens) = 0;
+    crm_hmean_vvel (k_crm,iens) = 0;
+    crm_hmean_temp (k_crm,iens) = 0;
+    crm_hmean_qv   (k_crm,iens) = 0;
+    crm_hmean_ql   (k_crm,iens) = 0;
+    crm_hmean_qi   (k_crm,iens) = 0;
   });
   //------------------------------------------------------------------------------------------------
   // Compute horizontal means
-  real r_nx_ny  = 1._fp / (nx*ny);  // precompute reciprocal to avoid costly divisions
-  parallel_for("Horz mean of CRM state", SimpleBounds<4>(nz,ny,nx,nens), YAKL_LAMBDA (int k, int j, int i, int iens) {
+  real r_nx_ny  = 1._fp / (crm_nx*crm_ny);  // precompute reciprocal to avoid costly divisions
+  parallel_for("Horz mean of CRM state", SimpleBounds<4>(crm_nz,crm_ny,crm_nx,nens), YAKL_LAMBDA (int k_crm, int j, int i, int iens) {
     // yakl::atomicAdd ensures only one thread performs an update at a time to avoid data races and wrong answers
-    atomicAdd( crm_hmean_rho_d(k,iens), rho_d(k,j,i,iens) * r_nx_ny );
-    atomicAdd( crm_hmean_uvel (k,iens), uvel (k,j,i,iens) * r_nx_ny );
-    atomicAdd( crm_hmean_vvel (k,iens), vvel (k,j,i,iens) * r_nx_ny );
-    atomicAdd( crm_hmean_temp (k,iens), temp (k,j,i,iens) * r_nx_ny );
-    atomicAdd( crm_hmean_rho_v(k,iens), rho_v(k,j,i,iens) * r_nx_ny );
-    atomicAdd( crm_hmean_rho_l(k,iens), rho_l(k,j,i,iens) * r_nx_ny );
-    atomicAdd( crm_hmean_rho_i(k,iens), rho_i(k,j,i,iens) * r_nx_ny );
+    real tmp_rho = rho_d(k_crm,j,i,iens) + rho_v(k_crm,j,i,iens);
+    atomicAdd( crm_hmean_uvel(k_crm,iens), uvel (k_crm,j,i,iens)          * r_nx_ny );
+    atomicAdd( crm_hmean_vvel(k_crm,iens), vvel (k_crm,j,i,iens)          * r_nx_ny );
+    atomicAdd( crm_hmean_temp(k_crm,iens), temp (k_crm,j,i,iens)          * r_nx_ny );
+    atomicAdd( crm_hmean_qv  (k_crm,iens),(rho_v(k_crm,j,i,iens)/tmp_rho) * r_nx_ny );
+    atomicAdd( crm_hmean_ql  (k_crm,iens),(rho_l(k_crm,j,i,iens)/tmp_rho) * r_nx_ny );
+    atomicAdd( crm_hmean_qi  (k_crm,iens),(rho_i(k_crm,j,i,iens)/tmp_rho) * r_nx_ny );
   });
   //------------------------------------------------------------------------------------------------
   // Compute feedback tendencies
@@ -89,17 +87,14 @@ inline void pam_feedback_compute_crm_feedback_tendencies( pam::PamCoupler &coupl
   real cp_d = coupler.get_option<real>("cp_d");
   real r_gcm_dt = 1._fp / gcm_dt;  // precompute reciprocal to avoid costly divisions
   parallel_for( "Compute CRM feedback tendencies", SimpleBounds<2>(gcm_nlev,nens), YAKL_LAMBDA (int k_gcm, int iens) {
-    int k_crm = nz-1-k_gcm;
-    if (k_crm>=0) {
-      real crm_qv  = crm_hmean_rho_v(k_crm,iens) / (crm_hmean_rho_d(k_crm,iens) + crm_hmean_rho_v(k_crm,iens));
-      real crm_ql  = crm_hmean_rho_l(k_crm,iens) / (crm_hmean_rho_d(k_crm,iens) + crm_hmean_rho_l(k_crm,iens));
-      real crm_qi  = crm_hmean_rho_i(k_crm,iens) / (crm_hmean_rho_d(k_crm,iens) + crm_hmean_rho_i(k_crm,iens));
-      crm_feedback_tend_uvel(k_gcm,iens) = ( crm_hmean_uvel (k_crm,iens) - gcm_ul  (k_gcm,iens) )*r_gcm_dt;
-      crm_feedback_tend_vvel(k_gcm,iens) = ( crm_hmean_vvel (k_crm,iens) - gcm_vl  (k_gcm,iens) )*r_gcm_dt;
-      crm_feedback_tend_dse (k_gcm,iens) = ( crm_hmean_temp (k_crm,iens) - gcm_tl  (k_gcm,iens) )*r_gcm_dt;
-      crm_feedback_tend_qv  (k_gcm,iens) = ( crm_qv                      - gcm_ql  (k_gcm,iens) )*r_gcm_dt;
-      crm_feedback_tend_ql  (k_gcm,iens) = ( crm_ql                      - gcm_qccl(k_gcm,iens) )*r_gcm_dt;
-      crm_feedback_tend_qi  (k_gcm,iens) = ( crm_qi                      - gcm_qiil(k_gcm,iens) )*r_gcm_dt;
+    int k_crm = gcm_nlev-1-k_gcm;
+    if (k_crm<crm_nz) {
+      crm_feedback_tend_uvel(k_gcm,iens) = ( crm_hmean_uvel(k_crm,iens) - gcm_ul  (k_gcm,iens) )*r_gcm_dt;
+      crm_feedback_tend_vvel(k_gcm,iens) = ( crm_hmean_vvel(k_crm,iens) - gcm_vl  (k_gcm,iens) )*r_gcm_dt;
+      crm_feedback_tend_dse (k_gcm,iens) = ( crm_hmean_temp(k_crm,iens) - gcm_tl  (k_gcm,iens) )*r_gcm_dt * cp_d;
+      crm_feedback_tend_qv  (k_gcm,iens) = ( crm_hmean_qv  (k_crm,iens) - gcm_ql  (k_gcm,iens) )*r_gcm_dt;
+      crm_feedback_tend_ql  (k_gcm,iens) = ( crm_hmean_ql  (k_crm,iens) - gcm_qccl(k_gcm,iens) )*r_gcm_dt;
+      crm_feedback_tend_qi  (k_gcm,iens) = ( crm_hmean_qi  (k_crm,iens) - gcm_qiil(k_gcm,iens) )*r_gcm_dt;
     } else {
       crm_feedback_tend_uvel(k_gcm,iens) = 0.;
       crm_feedback_tend_vvel(k_gcm,iens) = 0.;
@@ -111,16 +106,16 @@ inline void pam_feedback_compute_crm_feedback_tendencies( pam::PamCoupler &coupl
   });
   //------------------------------------------------------------------------------------------------
   // Copy the CRM feedback tendencies to host arrays
-  auto output_sltend_host  = dm_host.get<real,2>("output_sltend");
-  auto output_qltend_host  = dm_host.get<real,2>("output_qltend");
-  auto output_qcltend_host = dm_host.get<real,2>("output_qcltend");
-  auto output_qiltend_host = dm_host.get<real,2>("output_qiltend");
   auto output_ultend_host  = dm_host.get<real,2>("output_ultend");
   auto output_vltend_host  = dm_host.get<real,2>("output_vltend");
+  auto output_sltend_host  = dm_host.get<real,2>("output_sltend");
+  auto output_qvltend_host = dm_host.get<real,2>("output_qltend");
+  auto output_qcltend_host = dm_host.get<real,2>("output_qcltend");
+  auto output_qiltend_host = dm_host.get<real,2>("output_qiltend");
   crm_feedback_tend_uvel.deep_copy_to(output_ultend_host);
   crm_feedback_tend_vvel.deep_copy_to(output_vltend_host);
   crm_feedback_tend_dse .deep_copy_to(output_sltend_host);
-  crm_feedback_tend_qv  .deep_copy_to(output_qltend_host);
+  crm_feedback_tend_qv  .deep_copy_to(output_qvltend_host);
   crm_feedback_tend_ql  .deep_copy_to(output_qcltend_host);
   crm_feedback_tend_qi  .deep_copy_to(output_qiltend_host);
   //------------------------------------------------------------------------------------------------
@@ -134,19 +129,20 @@ inline void pam_feedback_compute_crm_mean_state( pam::PamCoupler &coupler ) {
   using yakl::atomicAdd;
   auto &dm_device = coupler.get_data_manager_device_readwrite();
   auto &dm_host   = coupler.get_data_manager_host_readwrite();
-  int nz       = dm_device.get_dimension_size("z"   );
-  int ny       = dm_device.get_dimension_size("y"   );
-  int nx       = dm_device.get_dimension_size("x"   );
+  int crm_nz   = dm_device.get_dimension_size("z"   );
+  int crm_ny   = dm_device.get_dimension_size("y"   );
+  int crm_nx   = dm_device.get_dimension_size("x"   );
   int nens     = dm_device.get_dimension_size("nens");
   int gcm_nlev = coupler.get_option<int>("gcm_nlev");
   //------------------------------------------------------------------------------------------------
   // Get current CRM state
-  auto nc = dm_device.get<real,4>("cloud_water_num");
-  auto ni = dm_device.get<real,4>("ice_num");
-  auto qr = dm_device.get<real,4>("rain");
-  auto nr = dm_device.get<real,4>("rain_num");
-  auto qm = dm_device.get<real,4>("ice_rime");
-  auto bm = dm_device.get<real,4>("ice_rime_vol");
+  auto nc      = dm_device.get<real,4>("cloud_water_num");
+  auto ni      = dm_device.get<real,4>("ice_num");
+  auto qr      = dm_device.get<real,4>("rain");
+  auto nr      = dm_device.get<real,4>("rain_num");
+  auto qm      = dm_device.get<real,4>("ice_rime");
+  auto bm      = dm_device.get<real,4>("ice_rime_vol");
+  auto cldfrac = dm_device.get<real,4>("cldfrac");
   //------------------------------------------------------------------------------------------------
   // Create arrays to hold the current column average of the CRM internal columns
   real2d nc_mean("nc_mean",gcm_nlev,nens);
@@ -155,43 +151,48 @@ inline void pam_feedback_compute_crm_mean_state( pam::PamCoupler &coupler ) {
   real2d nr_mean("nr_mean",gcm_nlev,nens);
   real2d qm_mean("qm_mean",gcm_nlev,nens);
   real2d bm_mean("bm_mean",gcm_nlev,nens);
+  real2d cldfrac_mean("cldfrac_mean",gcm_nlev,nens);
   //------------------------------------------------------------------------------------------------
   // We will be essentially reducing a summation to these variables, so initialize them to zero
-  parallel_for("Initialize horzontal means", SimpleBounds<2>(gcm_nlev,nens), YAKL_LAMBDA (int k, int iens) {
-    nc_mean(k,iens) = 0;
-    ni_mean(k,iens) = 0;
-    qr_mean(k,iens) = 0;
-    nr_mean(k,iens) = 0;
-    qm_mean(k,iens) = 0;
-    bm_mean(k,iens) = 0;
+  parallel_for("Initialize horzontal means", SimpleBounds<2>(gcm_nlev,nens), YAKL_LAMBDA (int k_gcm, int iens) {
+    nc_mean       (k_gcm,iens) = 0;
+    ni_mean       (k_gcm,iens) = 0;
+    qr_mean       (k_gcm,iens) = 0;
+    nr_mean       (k_gcm,iens) = 0;
+    qm_mean       (k_gcm,iens) = 0;
+    bm_mean       (k_gcm,iens) = 0;
+    cldfrac_mean  (k_gcm,iens) = 0;
   });
   //------------------------------------------------------------------------------------------------
   // Compute horizontal means
-  real r_nx_ny  = 1._fp / (nx*ny);  // precompute reciprocal to avoid costly divisions
-  parallel_for("Horz mean of CRM state", SimpleBounds<4>(nz,ny,nx,nens), YAKL_LAMBDA (int k, int j, int i, int iens) {
-    int k_gcm = gcm_nlev-1-k;
+  real r_nx_ny  = 1._fp / (crm_nx*crm_ny);  // precompute reciprocal to avoid costly divisions
+  parallel_for("Horz mean of CRM state", SimpleBounds<4>(crm_nz,crm_ny,crm_nx,nens), YAKL_LAMBDA (int k_crm, int j, int i, int iens) {
+    int k_gcm = gcm_nlev-1-k_crm;
     // yakl::atomicAdd ensures only one thread performs an update at a time to avoid data races and wrong answers
-    atomicAdd( nc_mean(k_gcm,iens), nc(k,j,i,iens) * r_nx_ny );
-    atomicAdd( ni_mean(k_gcm,iens), ni(k,j,i,iens) * r_nx_ny );
-    atomicAdd( qr_mean(k_gcm,iens), qr(k,j,i,iens) * r_nx_ny );
-    atomicAdd( nr_mean(k_gcm,iens), nr(k,j,i,iens) * r_nx_ny );
-    atomicAdd( qm_mean(k_gcm,iens), qm(k,j,i,iens) * r_nx_ny );
-    atomicAdd( bm_mean(k_gcm,iens), bm(k,j,i,iens) * r_nx_ny );
+    atomicAdd( nc_mean        (k_gcm,iens), nc        (k_crm,j,i,iens) * r_nx_ny );
+    atomicAdd( ni_mean        (k_gcm,iens), ni        (k_crm,j,i,iens) * r_nx_ny );
+    atomicAdd( qr_mean        (k_gcm,iens), qr        (k_crm,j,i,iens) * r_nx_ny );
+    atomicAdd( nr_mean        (k_gcm,iens), nr        (k_crm,j,i,iens) * r_nx_ny );
+    atomicAdd( qm_mean        (k_gcm,iens), qm        (k_crm,j,i,iens) * r_nx_ny );
+    atomicAdd( bm_mean        (k_gcm,iens), bm        (k_crm,j,i,iens) * r_nx_ny );
+    atomicAdd( cldfrac_mean   (k_gcm,iens), cldfrac   (k_crm,j,i,iens) * r_nx_ny );
   });
   //------------------------------------------------------------------------------------------------
   // Copy the CRM mean data to host arrays
-  auto output_nc_mean = dm_host.get<real,2>("output_nc_mean");
-  auto output_ni_mean = dm_host.get<real,2>("output_ni_mean");
-  auto output_qr_mean = dm_host.get<real,2>("output_qr_mean");
-  auto output_nr_mean = dm_host.get<real,2>("output_nr_mean");
-  auto output_qm_mean = dm_host.get<real,2>("output_qm_mean");
-  auto output_bm_mean = dm_host.get<real,2>("output_bm_mean");
-  nc_mean.deep_copy_to(output_nc_mean);
-  ni_mean.deep_copy_to(output_ni_mean);
-  qr_mean.deep_copy_to(output_qr_mean);
-  nr_mean.deep_copy_to(output_nr_mean);
-  qm_mean.deep_copy_to(output_qm_mean);
-  bm_mean.deep_copy_to(output_bm_mean);
+  auto output_nc_mean   = dm_host.get<real,2>("output_nc_mean");
+  auto output_ni_mean   = dm_host.get<real,2>("output_ni_mean");
+  auto output_qr_mean   = dm_host.get<real,2>("output_qr_mean");
+  auto output_nr_mean   = dm_host.get<real,2>("output_nr_mean");
+  auto output_qm_mean   = dm_host.get<real,2>("output_qm_mean");
+  auto output_bm_mean   = dm_host.get<real,2>("output_bm_mean");
+  auto output_cld       = dm_host.get<real,2>("output_cld");
+  nc_mean       .deep_copy_to(output_nc_mean);
+  ni_mean       .deep_copy_to(output_ni_mean);
+  qr_mean       .deep_copy_to(output_qr_mean);
+  nr_mean       .deep_copy_to(output_nr_mean);
+  qm_mean       .deep_copy_to(output_qm_mean);
+  bm_mean       .deep_copy_to(output_bm_mean);
+  cldfrac_mean  .deep_copy_to(output_cld);
   //------------------------------------------------------------------------------------------------
 }
 
