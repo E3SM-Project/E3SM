218ccf228d061b28427aeebbf499ed4e148efca5
Andrew M. Bradley <ambradl@sandia.gov>
2023-02-12 17:09:17 -0500

    Frontier: tweaks; not sure why first commit didn't get SLIBS right

--- cime_config/machines/cmake_macros/crayclang-scream_frontier-scream-gpu.cmake
+++ cime_config/machines/cmake_macros/crayclang-scream_frontier-scream-gpu.cmake
@@ -11,6 +11,9 @@ set(PNETCDF_PATH "$ENV{PNETCDF_DIR}")
 set(PIO_FILESYSTEM_HINTS "gpfs")
 string(APPEND CXX_LIBS " -lstdc++")
 
+string(APPEND SLIBS " -L$ENV{ROCM_PATH}/lib -lamdhip64 $ENV{OLCF_LIBUNWIND_ROOT}/lib/libunwind.a /sw/frontier/spack-envs/base/opt/cray-sles15-zen3/clang-14.0.0-rocm5.2.0/gperftools-2.10-6g5acp4pcilrl62tddbsbxlut67pp7qn/lib/libtcmalloc.a")
+string(APPEND FFLAGS " -hipa0 -hzero -hsystem_alloc -f free -N 255 -h byteswapio")
+
 SET(CMAKE_C_COMPILER "cc" CACHE STRING "")
 SET(CMAKE_Fortran_COMPILER "ftn" CACHE STRING "")
 SET(CMAKE_CXX_COMPILER "hipcc" CACHE STRING "")
