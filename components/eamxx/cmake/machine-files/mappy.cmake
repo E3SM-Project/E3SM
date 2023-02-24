bac8fa1e842d1c9888a85c3f492b1e1b540bddf7
James Foucar <jgfouca@sandia.gov>
2023-02-24 13:38:38 -0700

    Move MPI knowledge into EKAT and refactor cmake mach files
    Change list:
    1) Change SCREAM_MPI* to EKAT_MPI* and handle them in EKAT
    2) Remove confusing CUDA/HIP prints in CMakeLists.txt
    3) Remove unused SCREAM_MPI_ERRORS_ARE_FATAL
    4) MPI global configs no longer need to be passed to CUT
    5) New common.cmake with common_setup macro that all machine files should use
    6) Redo rank/thread spreading test. We have to make mappy go back to pre-slurm.

--- components/eamxx/cmake/machine-files/mappy.cmake
+++ components/eamxx/cmake/machine-files/mappy.cmake
@@ -1,10 +1,2 @@
-# Load all kokkos settings from Ekat's mach file
-set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
-set(SCREAM_MACHINE "mappy" CACHE STRING "")
-
-include (${EKAT_MACH_FILES_PATH}/mappy.cmake)
-
-set(SCREAM_MPIRUN_EXE "srun" CACHE STRING "")
-set(SCREAM_MPI_NP_FLAG "-n" CACHE STRING "")
-set(SCREAM_MPI_EXTRA_ARGS "--cpu_bind=threads" CACHE STRING "")
-set(SCREAM_MPI_THREAD_FLAG "-c" CACHE STRING "")
+include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
+common_setup()
