42f49fdf981df33798daba75c680ca1581fe7af7
Luca Bertagna <lbertag@sandia.gov>
2023-03-06 18:17:24 -0700

    EAMxx: make shorter names for current atm procs

--- components/eamxx/src/dynamics/homme/atmosphere_dynamics.hpp
+++ components/eamxx/src/dynamics/homme/atmosphere_dynamics.hpp
@@ -31,7 +31,7 @@ public:
   AtmosphereProcessType type () const { return AtmosphereProcessType::Dynamics; }
 
   // The name of the subcomponent
-  std::string name () const { return "Dynamics"; }
+  std::string name () const { return "homme"; }
 
   // Set the grid
   void set_grids (const std::shared_ptr<const GridsManager> grids_manager);
