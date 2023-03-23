e9e079b7cedfb9cbef2660e639db93cc82c40a78
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-03-23 10:21:27 -0700

    Re-adding buffer for local variables.

--- components/eamxx/src/physics/mam/eamxx_mam_microphysics.cpp
+++ components/eamxx/src/physics/mam/eamxx_mam_microphysics.cpp
@@ -105,6 +105,64 @@ set_computed_group_impl(const FieldGroup& group) {
     "Error! MAM4 requires at least " << num_aero_tracers << " aerosol tracers.");
 }
 
+size_t MAMMicrophysics::requested_buffer_size_in_bytes() const
+{
+  // number of Reals needed by local views in interface
+  const size_t iface_request = sizeof(Real) *
+    (Buffer::num_2d_mid * ncol_ * nlev_ +
+     Buffer::num_2d_iface * ncol_ * (nlev_+1));
+
+  // FIXME: Need to figure out whether we need this stuff
+  /*
+  // Number of Reals needed by the WorkspaceManager passed to shoc_main
+  const auto policy       = ekat::ExeSpaceUtils<KT::ExeSpace>::get_default_team_policy(m_num_cols, nlev_packs);
+  const int n_wind_slots  = ekat::npack<Spack>(2)*Spack::n;
+  const int n_trac_slots  = ekat::npack<Spack>(m_num_tracers+3)*Spack::n;
+  const size_t wsm_request= WSM::get_total_bytes_needed(nlevi_packs, 13+(n_wind_slots+n_trac_slots), policy);
+  */
+
+  return iface_request;// + wsm_request;
+}
+
+void MAMMicrophysics::init_buffers(const ATMBufferManager &buffer_manager) {
+  EKAT_REQUIRE_MSG(buffer_manager.allocated_bytes() >= requested_buffer_size_in_bytes(),
+                   "Error! Insufficient buffer size.\n");
+
+  Real* mem = reinterpret_cast<Real*>(buffer_manager.get_memory());
+
+  // set view pointers for midpoint fields
+  using view_2d_t = decltype(buffer_.z_mid);
+  view_2d_t* view_2d_mid_ptrs[Buffer::num_2d_mid] = {&buffer_.z_mid, &buffer_.dz};
+  for (int i = 0; i < Buffer::num_2d_mid; ++i) {
+    *view_2d_mid_ptrs[i] = view_2d_t(mem, ncol_, nlev_);
+    mem += view_2d_mid_ptrs[i]->size();
+  }
+
+  // set view pointers for interface fields
+  view_2d_t* view_2d_iface_ptrs[Buffer::num_2d_iface] = {&buffer_.z_iface};
+  for (int i = 0; i < Buffer::num_2d_iface; ++i) {
+    *view_2d_iface_ptrs[i] = view_2d_t(mem, ncol_, nlev_+1);
+    mem += view_2d_iface_ptrs[i]->size();
+  }
+
+  // WSM data
+  buffer_.wsm_data = mem;
+
+  /* FIXME: this corresponds to the FIXME in the above function
+  // Compute workspace manager size to check used memory
+  // vs. requested memory
+  const auto policy      = ekat::ExeSpaceUtils<KT::ExeSpace>::get_default_team_policy(ncol_, nlev_);
+  const int n_wind_slots = ekat::npack<Spack>(2)*Spack::n;
+  const int n_trac_slots = ekat::npack<Spack>(m_num_tracers+3)*Spack::n;
+  const int wsm_size     = WSM::get_total_bytes_needed(nlevi_packs, 13+(n_wind_slots+n_trac_slots), policy)/sizeof(Spack);
+  mem += wsm_size;
+  */
+
+  size_t used_mem = (mem - buffer_manager.get_memory())*sizeof(Real);
+  EKAT_REQUIRE_MSG(used_mem==requested_buffer_size_in_bytes(),
+                   "Error! Used memory != requested memory for MAMMicrophysics.");
+}
+
 void MAMMicrophysics::initialize_impl(const RunType run_type) {
   const auto& T_mid = get_field_in("T_mid").get_view<Real**>();
   const auto& p_mid = get_field_in("p_mid").get_view<const Real**>();
@@ -114,9 +172,12 @@ void MAMMicrophysics::initialize_impl(const RunType run_type) {
   const auto& cldfrac = get_field_in("cldfrac_tot").get_view<Real**>(); // FIXME: tot or liq?
   const auto& tracers = get_group_out("tracers");
   const auto& tracers_info = tracers.m_info;
+  int num_tracers = tracers_info->size();
 
   // Alias local variables from temporary buffer
-  // e.g. auto z_mid       = buffer_.z_mid;
+  auto z_mid = buffer_.z_mid;
+  auto dz    = buffer_.dz;
+  auto z_iface = buffer_.z_iface;
 
   // Perform any initialization work.
   if (run_type==RunType::Initial){
@@ -129,21 +190,35 @@ void MAMMicrophysics::initialize_impl(const RunType run_type) {
     */
   }
 
-  // Find indices of qv and aerosol-related quantities
-  auto qv_index  = tracers_info->m_subview_idx.at("qv");
-  // FIXME
-
   // set atmosphere state data
   T_mid_ = T_mid;
   p_mid_ = p_mid;
   qv_ = qv;
-  height_ = height;
   pdel_ = p_del;
   cloud_f_ = cloud_f; // FIXME??
   uv_ = uv;
 
-  preprocess_.set_variables(ncol_, nlev_, T_mid, p_mid, qv, height,
-                            p_del, pblh, q_soag, q_h2so4, q_nh3, q_aitken_so4);
+  // For now, set z_surf to zero.
+  const Real z_surf = 0.0;
+
+  // Determine indices of aerosol/gas tracers for wet<->dry conversion
+  auto q_aitken_so4_index  = tracers_info->m_subview_idx.at("q_aitken_so4");
+  auto q_soag_index        = tracers_info->m_subview_idx.at("q_soag");
+  auto q_h2so4_index       = tracers_info->m_subview_idx.at("q_h2so4");
+  int num_aero_tracers = 4; // for now, just 2 gases + aitken so4 n,q
+  view_1d_int convert_wet_dry_idx_d("convert_wet_dry_idx_d", num_aero_tracers);
+  auto convert_wet_dry_idx_h = Kokkos::create_mirror_view(convert_wet_dry_idx_d);
+  for (int it=0, iq=0; it < num_tracers; ++it) {
+    if ((it == q_aitken_so4_index) || (it == q_soag_index) || (it == q_h2so4_index)) {
+      convert_wet_dry_idx_h(iq) = it;
+      ++iq;
+    }
+  }
+  Kokkos::deep_copy(convert_wet_dry_idx_d, convert_wet_dry_idx_h);
+
+  preprocess_.set_variables(ncol_, nlev_, z_surf, convert_wet_dry_idx_d, T_mid,
+                            p_mid, qv, z_mid, z_iface, dz, p_del, pblh, q_soag,
+                            q_h2so4, q_nh3, q_aitken_so4);
 
   // FIXME: here we run the aerosol microphysics parameterizations
 
--- components/eamxx/src/physics/mam/eamxx_mam_microphysics.hpp
+++ components/eamxx/src/physics/mam/eamxx_mam_microphysics.hpp
@@ -57,11 +57,9 @@ protected:
   // grid
   void set_grids(const std::shared_ptr<const GridsManager> grids_manager) override;
 
-  /*
   // management of common atm process memory
   size_t requested_buffer_size_in_bytes() const override;
   void init_buffers(const ATMBufferManager &buffer_manager) override;
-  */
 
   // process behavior
   void initialize_impl(const RunType run_type) override;
@@ -84,7 +82,17 @@ private:
 
     KOKKOS_INLINE_FUNCTION
     void operator()(const Kokkos::TeamPolicy<KT::ExeSpace>::member_type& team) const {
-      const int i = team.league_rank();
+      const int i = team.league_rank(); // column index
+
+      // Compute vertical layer heights
+      const auto dz_i      = ekat::subview(dz_,    i);
+      const auto z_iface_i = ekat::subview(z_iface_, i);
+      const auto z_mid_i   = ekat::subview(z_mid_, i);
+      PF::calculate_z_int(team, nlev_, dz_i, z_surf_, z_iface_i);
+      team.team_barrier();
+      PF::calculate_z_mid(team, nlev_, z_iface_i, z_mid_i);
+      team.team_barrier();
+
       Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nlev_), [&](const int k) {
         //--------------------------
         // Wet to dry mixing ratios
@@ -115,6 +123,9 @@ private:
     // number of horizontal columns and vertical levels
     int ncol_, nlev_;
 
+    // height of bottom of atmosphere
+    Real z_surf_;
+
     // used for converting between wet and dry mixing ratios
     view_1d_int convert_wet_dry_idx_d_;
 
@@ -123,7 +134,9 @@ private:
     view_2d_const p_mid_;   // total pressure at grid midpoints [Pa]
     view_2d       qv_;      // water vapor mass mixing ratio, not const because it
                             // must be converted from wet to dry [kg vapor/kg dry air]
-    view_2d_const height_;  // height at grid interfaces [m]
+    view_2d_const z_mid_;   // height at layer midpoints [m]
+    view_2d_const z_iface_; // height at layer interfaces [m]
+    view_2d_const dz_;      // layer thickness [m]
     view_2d_const pdel_;    // hydrostatic "pressure thickness" at grid
                             // interfaces [Pa]
     view_2d_const cloud_f_; // cloud fraction [-]
@@ -139,11 +152,14 @@ private:
     view_2d q_aitken_so4_; // SO4 mass mixing ratio in aitken mode [kg/kg dry air]
 
     // assigns local variables
-    void set_variables(const int ncol, const int nlev,
+    void set_variables(const int ncol, const int nlev, const Real z_surf,
+                       const view_1d_int& convert_wet_dry_idx_d,
                        const view_2d_const& T_mid,
                        const view_2d_const& p_mid,
                        const view_2d&       qv,
-                       const view_2d&       height,
+                       const view_2d&       z_mid,
+                       const view_2d&       z_iface,
+                       const view_2d&       dz,
                        const view_2d&       pdel,
                        const view_1d_const& pblh,
                        const view_2d&       q_soag,
@@ -151,17 +167,21 @@ private:
                        const view_2d&       q_aitken_so4) {
       ncol_ = ncol;
       nlev_ = nlev;
+      z_surf_ = z_surf;
+      convert_wet_dry_idx_d_ = convert_wet_dry_idx_d;
       T_mid_ = T_mid;
       p_mid_ = p_mid;
       qv_ = qv;
-      height_ = height;
+      z_mid_ = z_mid;
+      z_iface_ = z_iface;
+      dz_ = dz;
       pdel_ = pdel;
       pblh_ = pblh;
       q_soag_ = q_soag;
       q_h2so4_ = q_h2so4;
       q_aitken_so4_ = q_aitken_so4;
     } // set_variables
-  }; // MAM4AerosolMicrophysics::Preprocess
+  }; // MAMMicrophysics::Preprocess
 
   // Postprocessing functor
   struct Postprocess {
@@ -169,9 +189,6 @@ private:
 
     KOKKOS_INLINE_FUNCTION
     void operator()(const Kokkos::TeamPolicy<KT::ExeSpace>::member_type& team) const {
-      // Copy the updated aerosol tracers back into the tracer array.
-      // FIXME
-
       // After these updates, all tracers are converted from dry mmr to wet mmr
       const int i = team.league_rank();
       Kokkos::parallel_for(Kokkos::TeamThreadRange(team, nlev_), [&](const int k) {
@@ -193,6 +210,9 @@ private:
     int ncol_, nlev_;
     view_2d qv_;
 
+    // used for converting between wet and dry mixing ratios
+    view_1d_int convert_wet_dry_idx_d_;
+
     // local aerosol-related gases
     view_2d       q_soag_;  // secondary organic aerosol gas [kg gas/kg dry air]
     view_2d       q_h2so4_; // H2SO3 gas [kg/kg dry air]
@@ -203,18 +223,39 @@ private:
     // assigns local variables
     void set_variables(const int ncol,
                        const int nlev,
+                       const view_1d_int& convert_wet_dry_idx_d,
                        const view_2d& qv,
                        const view_2d& q_soag,
                        const view_2d& q_h2so4,
                        const view_2d& q_aitken_so4) {
       ncol_ = ncol;
       nlev_ = nlev;
+      convert_wet_dry_idx_d_ = convert_wet_dry_idx_d;
       qv_ = qv;
       q_soag_ = q_soag;
       q_h2so4_ = q_h2so4;
       q_aitken_so4_ = q_aitken_so4;
     } // set_variables
-  }; // MAM4AerosolMicrophysics::Postprocess
+  }; // MAMMicrophysics::Postprocess
+
+  // storage for local variables, initialized with ATMBufferManager
+  struct Buffer {
+    // number of fields stored at column midpoints
+    static constexpr int num_2d_mid = 2;
+
+    // number of fields stored at column interfaces
+    static constexpr int num_2d_iface = 1;
+
+    // column midpoint fields
+    uview_2d z_mid; // height at midpoints
+    uview_2d dz;    // layer thickness
+
+    // column interface fields
+    uview_2d z_iface; // height at interfaces
+
+    // storage
+    Real* wsm_data;
+  };
 
   // MAM4 aerosol particle size description
   mam4::AeroConfig aero_config_;
@@ -246,11 +287,9 @@ private:
   view_2d n_aitken_; // aitken mode number mixing ratio [1/kg dry air]
   view_2d q_aitken_so4_; // SO4 mass mixing ratio in aitken mode [kg/kg dry air]
 
-  /*
-  // WSM for internal local variables
+  // workspace manager for internal local variables
   ekat::WorkspaceManager<Real, KT::Device> workspace_mgr_;
   Buffer buffer_;
-  */
 
   // physics grid for column information
   std::shared_ptr<const AbstractGrid> grid_;
