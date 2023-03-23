2f00db81868a780e7a3e68486738a2b862cc5c09
Wuyin Lin <wlin@bnl.gov>
2023-03-23 07:47:49 -0700

    Add DP-SCREAM test to atm_developer suite

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -91,7 +91,8 @@ _TESTS = {
             "SMS_R_Ld5.ne4_ne4.FSCM-ARM97.eam-scm",
             "SMS_D_Ln5.ne4_oQU240.F2010",
             "SMS_Ln5.ne4pg2_oQU480.F2010",
-            "ERS_D.ne4_oQU240.F2010.eam-hommexx"
+            "ERS_D.ne4_oQU240.F2010.eam-hommexx",
+            "SMS_Ln9_P24x1.ne4_ne4.FDPSCREAM-ARM97",
             )
         },
 
