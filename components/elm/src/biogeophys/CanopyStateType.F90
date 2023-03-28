6fb4fc3d23883ed2cb67eea7c4a2c69db9f69e82
Gautam Bisht <gautam.bisht@pnnl.gov>
2023-03-28 12:34:20 -0700

    Fixes the initialization of a variable
    Only the c-th index of `altmax_lastyear_indx_col` is set to zero within
    the do-loop.
    
    Fixes #5526
    [BFB]

--- components/elm/src/biogeophys/CanopyStateType.F90
+++ components/elm/src/biogeophys/CanopyStateType.F90
@@ -499,12 +499,12 @@ contains
        l = col_pp%landunit(c)
 
        if (lun_pp%itype(l) == istsoil .or. lun_pp%itype(l) == istcrop) then
-          this%alt_col(c)               = 0._r8 !iniitialized to spval for all columns
-          this%altmax_col(c)            = 0._r8 !iniitialized to spval for all columns
-          this%altmax_lastyear_col(c)   = 0._r8 !iniitialized to spval for all columns
-          this%alt_indx_col(c)          = 0     !initiialized to huge  for all columns
-          this%altmax_indx_col(c)       = 0     !initiialized to huge  for all columns
-          this%altmax_lastyear_indx_col = 0     !initiialized to huge  for all columns
+          this%alt_col(c)                  = 0._r8
+          this%altmax_col(c)               = 0._r8
+          this%altmax_lastyear_col(c)      = 0._r8
+          this%alt_indx_col(c)             = 0
+          this%altmax_indx_col(c)          = 0
+          this%altmax_lastyear_indx_col(c) = 0
        end if
     end do
 
