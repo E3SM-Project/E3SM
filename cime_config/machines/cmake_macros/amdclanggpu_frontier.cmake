9e0214e2a2d90aec5f39ab3f5b1367b6ba1c82a4
Sarat Sreepathi <sarat@ornl.gov>
2023-04-03 19:55:44 -0400

    Frontier: Replace deprecated --amdgpu-target and cleanup HIP flags

--- cime_config/machines/cmake_macros/amdclanggpu_frontier.cmake
+++ cime_config/machines/cmake_macros/amdclanggpu_frontier.cmake
@@ -12,10 +12,10 @@ string(APPEND SLIBS " -L$ENV{CRAY_LIBSCI_PREFIX_DIR}/lib -lsci_amd")
 
 set(MPICXX "hipcc")
 set(SCXX "hipcc")
-string(APPEND CXXFLAGS " -I$ENV{MPICH_DIR}/include --amdgpu-target=gfx90a")
+string(APPEND CXXFLAGS " -I$ENV{MPICH_DIR}/include --offload-arch=gfx90a")
 string(APPEND SLIBS    " -L$ENV{MPICH_DIR}/lib -lmpi -L$ENV{CRAY_MPICH_ROOTDIR}/gtl/lib -lmpi_gtl_hsa")
 
 string(APPEND KOKKOS_OPTIONS " -DKokkos_ENABLE_HIP=On -DKokkos_ARCH_ZEN3=On -DKokkos_ARCH_VEGA90A=On")
 
 set(USE_HIP "TRUE")
-string(APPEND HIP_FLAGS "-munsafe-fp-atomics -D__HIP_ROCclr__ -D__HIP_ARCH_GFX90A__=1 --rocm-path=$ENV{ROCM_PATH} --offload-arch=gfx90a -x hip")
+string(APPEND HIP_FLAGS "${CXXFLAGS} -munsafe-fp-atomics -x hip")
