94d4439d65a075b16b2d21f68d0928065534b68f
Oksana Guba <oksana.guba@gmail.com>
2023-04-05 14:01:01 -0500

    finalized chrys file

--- components/eamxx/cmake/machine-files/chrysalis.cmake
+++ components/eamxx/cmake/machine-files/chrysalis.cmake
@@ -1,16 +1,11 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
 
-set (SCREAM_INPUT_ROOT "/lcrc/group/e3sm/ccsm-data/inputdata/" CACHE STRING "" FORCE)
-
 set (EKAT_MACH_FILES_PATH ${CMAKE_CURRENT_LIST_DIR}/../../../../externals/ekat/cmake/machine-files)
 include (${EKAT_MACH_FILES_PATH}/kokkos/serial.cmake)
 include (${EKAT_MACH_FILES_PATH}/kokkos/openmp.cmake)
 
 include (${EKAT_MACH_FILES_PATH}/mpi/srun.cmake)
-
-set(SCREAM_MPIRUN_EXE "srun" CACHE STRING "")
-set(SCREAM_MACHINE "chrysalis" CACHE STRING "")
 
 
 
