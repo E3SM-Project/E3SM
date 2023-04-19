293269736d67ed32637a8a482e9b0053397863ec
Elynn Wu <elynn510@gmail.com>
2023-04-19 14:02:28 -0700

    update to work with latest cime base image

--- components/eamxx/cmake/machine-files/docker.cmake
+++ components/eamxx/cmake/machine-files/docker.cmake
@@ -9,3 +9,13 @@ set(LAPACK_LIBRARIES /opt/conda/lib/liblapack.so CACHE STRING "")
 set(SCREAM_INPUT_ROOT "/storage/inputdata/" CACHE STRING "")
 set(PYBIND11_PYTHON_VERSION 3.9 CACHE STRING "")
 set(RUN_ML_CORRECTION_TEST TRUE CACHE BOOL "")
+
+if ("${PROJECT_NAME}" STREQUAL "E3SM")
+  if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+    if (CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL 10)
+      set(CMAKE_Fortran_FLAGS "-fallow-argument-mismatch"  CACHE STRING "" FORCE) # only works with gnu v10 and above
+    endif()
+  endif()
+else()
+  set(CMAKE_Fortran_FLAGS "-fallow-argument-mismatch"  CACHE STRING "" FORCE) # only works with gnu v10 and above
+endif()
