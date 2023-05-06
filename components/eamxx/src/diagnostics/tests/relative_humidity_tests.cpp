feae06db5d839dc8ba044a8efc85a839000712ed
Oksana Guba <oksana.guba@gmail.com>
2023-05-05 23:20:05 -0500

    remove line left by mistake

--- components/eamxx/src/diagnostics/tests/relative_humidity_tests.cpp
+++ components/eamxx/src/diagnostics/tests/relative_humidity_tests.cpp
@@ -154,7 +154,6 @@ void run(std::mt19937_64& engine)
     }
 
     // Run diagnostic and compare with manual calculation
-    diag->compute_diagnostic();
     Field rh_f = T_mid_f.clone();
     rh_f.deep_copy<double,Host>(0.0);
     rh_f.sync_to_dev();
