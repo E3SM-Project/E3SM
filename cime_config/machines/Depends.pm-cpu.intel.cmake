39789d3c0286f8b1ca897628c4b930f0f49179d9
noel <ndkeen@lbl.gov>
2023-05-09 08:36:56 -0700

    for pm-cpu intel build, which uses new oneapi version, add Depends rule to build one certain mpas file with ifor instead of ifx as a work-around for internal compielr error

--- cime_config/machines/Depends.pm-cpu.intel.cmake
+++ cime_config/machines/Depends.pm-cpu.intel.cmake
@@ -9,6 +9,5 @@ if (NOT DEBUG)
   endforeach()
 endif()
 
-
-
-
+# compile mpas_seaice_core_interface.f90 with ifort, not ifx
+e3sm_add_flags("${CMAKE_BINARY_DIR}/core_seaice/model_forward/mpas_seaice_core_interface.f90" "-fc=ifort")
