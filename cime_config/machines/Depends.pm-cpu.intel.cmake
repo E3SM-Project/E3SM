a8f7cc0f30c50fc2abdda9f0af15b69365730a3d
noel <ndkeen@lbl.gov>
2023-05-09 16:01:06 -0700

    Revert "Merge branch 'ndk/machinefiles/fix-for-pm-cpu-intel' into next (PR #5671)"
    This reverts commit 6cb7ed320c824ab17e44dbce7f608ae841b90ad1, reversing
    changes made to 8b5396415e5378d949cb9b67de58938c4eb1861d.

--- cime_config/machines/Depends.pm-cpu.intel.cmake
+++ cime_config/machines/Depends.pm-cpu.intel.cmake
@@ -9,5 +9,6 @@ if (NOT DEBUG)
   endforeach()
 endif()
 
-# compile mpas_seaice_core_interface.f90 with ifort, not ifx
-e3sm_add_flags("${CMAKE_BINARY_DIR}/core_seaice/model_forward/mpas_seaice_core_interface.f90" "-fc=ifort")
+
+
+
