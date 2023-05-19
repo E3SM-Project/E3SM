29567acf97628cf71db35b10ade52f924ca1766b
Chris Terai <terai1@llnl.gov>
2023-05-19 09:04:01 -0700

    Remove optimization change on pm-cpu_gnu

--- cime_config/machines/Depends.pm-cpu.gnu.cmake
+++ cime_config/machines/Depends.pm-cpu.gnu.cmake
@@ -1,13 +1,13 @@
 # For this file, see internal compiler error on pm-cpu with -O2
-set(NOOPT
-  eam/src/physics/cam/zm_conv.F90)
+#set(NOOPT
+#  eam/src/physics/cam/zm_conv.F90)
 
-if (NOT DEBUG)
-  foreach(ITEM IN LISTS NOOPT)
-    e3sm_remove_flags("${ITEM}" "-O2")
-    e3sm_remove_flags("${ITEM}" "-O")
-  endforeach()
-endif()
+#if (NOT DEBUG)
+#  foreach(ITEM IN LISTS NOOPT)
+#    e3sm_remove_flags("${ITEM}" "-O2")
+#    e3sm_remove_flags("${ITEM}" "-O")
+#  endforeach()
+#endif()
 
 
 
