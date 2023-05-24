85718b4c76c4b3d8f6b921de8f3cf97dc76a430a
Sara Calandrini <scalandrini@lanl.gov>
2023-05-24 17:21:20 -0500

    possible fix for problem with ocn_vvel_hmix_del4

--- components/mpas-ocean/src/ocean.cmake
+++ components/mpas-ocean/src/ocean.cmake
@@ -110,6 +110,7 @@ list(APPEND RAW_SOURCES
   core_ocean/shared/mpas_ocn_wetting_drying.F
   core_ocean/shared/mpas_ocn_vel_tidal_potential.F
   core_ocean/shared/mpas_ocn_vvel_hmix_del2.F
+  core_ocean/shared/mpas_ocn_vvel_hmix_del4.F
   core_ocean/shared/mpas_ocn_nonhydrostatic_pressure_solve.F
   core_ocean/shared/mpas_ocn_vvel_pgrad.F
   core_ocean/shared/mpas_ocn_vvel_coriolis.F
