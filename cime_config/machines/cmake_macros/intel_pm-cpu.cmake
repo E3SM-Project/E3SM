b87b811479aca75b55f4cf138914b60d8144d635
noel <ndkeen@lbl.gov>
2023-07-17 17:36:39 -0700

    For maint-2.0, add intel compiler to pm-cpu.
    Also update module versions to match those on master for pm-cpu/pm-gpu.

---
+++ cime_config/machines/cmake_macros/intel_pm-cpu.cmake
@@ -0,0 +1,44 @@
+string(APPEND CONFIG_ARGS " --host=cray")
+if (COMP_NAME STREQUAL gptl)
+  string(APPEND CPPDEFS " -DHAVE_NANOTIME -DBIT64 -DHAVE_SLASHPROC -DHAVE_GETTIMEOFDAY")
+endif()
+string(APPEND SLIBS " -L$ENV{CRAY_HDF5_PARALLEL_PREFIX}/lib -lhdf5_hl -lhdf5 -L$ENV{CRAY_NETCDF_HDF5PARALLEL_PREFIX}/lib -L$ENV{CRAY_PARALLEL_NETCDF_PREFIX}/lib -lpnetcdf -lnetcdf -lnetcdff")
+string(APPEND SLIBS " -qmkl")
+set(CXX_LINKER "FORTRAN")
+set(NETCDF_PATH "$ENV{CRAY_NETCDF_HDF5PARALLEL_PREFIX}")
+set(NETCDF_C_PATH "$ENV{CRAY_NETCDF_HDF5PARALLEL_PREFIX}")
+set(NETCDF_FORTRAN_PATH "$ENV{CRAY_NETCDF_HDF5PARALLEL_PREFIX}")
+set(HDF5_PATH "$ENV{CRAY_HDF5_PARALLEL_PREFIX}")
+set(PNETCDF_PATH "$ENV{CRAY_PARALLEL_NETCDF_PREFIX}")
+
+set(MPICC "cc")
+set(MPICXX "CC")
+set(MPIFC "ftn")
+set(SCC "icx")
+set(SCXX "icpx")
+set(SFC "ifx")
+
+# Bit of a hack here. For whatever reason, the intel version on pm-cpu (both intel and intel-oneapi, and both icpc/icpx)
+# does not seem to have the -fp-model=source flag (docs still show it).  And I was unable to find a reliable way of testing
+# on the compiler ID or version, so for now, simply manually adjust the CXXFLAG setting for pm-cpu/intel
+# Try to manually remove -fp-model=source (and replace with -fp-model=precise) from CXXFLAGS
+set(CXXFLAGS " ") # hardcode it here to blank, then try to do same things as in intel.cmake
+if (compile_threaded)
+  string(APPEND CXXFLAGS " -qopenmp")
+endif()
+if (DEBUG)
+  string(APPEND CXXFLAGS " -O0 -g")
+endif()
+if (NOT DEBUG)
+  string(APPEND CXXFLAGS " -O2")
+endif()
+string(APPEND CXXFLAGS " -fp-model=precise") # and manually add precise
+
+string(APPEND FFLAGS " -fp-model=consistent -fimf-use-svml")
+if (NOT DEBUG)
+   #  string(APPEND FFLAGS " -qno-opt-dynamic-align")
+   string(APPEND FFLAGS " -g -traceback")
+   string(APPEND CXXFLAGS " -g -traceback")
+endif()
+string(APPEND FFLAGS " -DHAVE_ERF_INTRINSICS")
+string(APPEND CXXFLAGS " -fp-model=consistent")
