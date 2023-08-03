0799a18d33bdecee183cf1eaa7983553c6fa0b2f
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-08-03 16:39:37 -0700

    Normalized mam4 process names.

--- components/eamxx/src/physics/register_physics.hpp
+++ components/eamxx/src/physics/register_physics.hpp
@@ -51,7 +51,7 @@ inline void register_physics () {
   proc_factory.register_product("Nudging",&create_atmosphere_process<Nudging>);
 #endif
 #ifdef EAMXX_HAS_MAM
-  proc_factory.register_product("MAM4Microphysics",&create_atmosphere_process<MAMMicrophysics>);
+  proc_factory.register_product("mam4_micro",&create_atmosphere_process<MAMMicrophysics>);
 #endif
 }
 
