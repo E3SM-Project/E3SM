69976aa72a0e7808e7afa0e8dc7956fa341ff553
Walter Hannah <whannah1@gmail.com>
2023-08-04 18:21:37 -0400

    add dummy modules to fix build issues

---
+++ components/eam/src/physics/crm/dummy_modules/scream_abortutils.F90
@@ -0,0 +1,5 @@
+module scream_abortutils
+!-------------------------------------------------------------------------------
+! Dummy module to override src/physics/cam/scream_abortutils.F90
+!-------------------------------------------------------------------------------
+end module scream_abortutils
---
+++ components/eam/src/physics/crm/dummy_modules/wv_sat_scream.F90
@@ -0,0 +1,5 @@
+module wv_sat_scream
+!-------------------------------------------------------------------------------
+! Dummy module to override src/physics/cam/wv_sat_scream.F90
+!-------------------------------------------------------------------------------
+end module wv_sat_scream
