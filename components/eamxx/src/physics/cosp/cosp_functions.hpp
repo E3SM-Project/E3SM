0fe9c4b03a67027fe5e96456b21a57c8decb0a53
Benjamin Hillman <bhillma@sandia.gov>
2023-08-08 11:54:22 -0700

    Remove unnecessary host copies

--- components/eamxx/src/physics/cosp/cosp_functions.hpp
+++ components/eamxx/src/physics/cosp/cosp_functions.hpp
@@ -4,10 +4,10 @@
 using scream::Real;
 extern "C" void cosp_c2f_init(int ncol, int nsubcol, int nlay);
 extern "C" void cosp_c2f_final();
-extern "C" void cosp_c2f_run(int ncol, int nsubcol, int nlay, int ntau, int nctp,
-    Real emsfc_lw, Real* sunlit, Real* skt,
-    Real* T_mid, Real* p_mid, Real* p_int, Real* qv,
-    Real* cldfrac, Real* reff_qc, Real* reff_qi, Real* dtau067, Real* dtau105,
+extern "C" void cosp_c2f_run(const int ncol, const int nsubcol, const int nlay, const int ntau, const int nctp,
+    const Real emsfc_lw, const Real* sunlit, const Real* skt,
+    const Real* T_mid, const Real* p_mid, const Real* p_int, const Real* qv,
+    const Real* cldfrac, const Real* reff_qc, const Real* reff_qi, const Real* dtau067, const Real* dtau105,
     Real* isccp_cldtot, Real* isccp_ctptau);
 
 namespace scream {
@@ -30,7 +30,7 @@ namespace scream {
             cosp_c2f_final();
         };
         void main(
-                Int ncol, Int nsubcol, Int nlay, Int ntau, Int nctp, Real emsfc_lw,
+                const Int ncol, const Int nsubcol, const Int nlay, const Int ntau, const Int nctp, const Real emsfc_lw,
                 view_1d<const Real>& sunlit , view_1d<const Real>& skt,
                 view_2d<const Real>& T_mid  , view_2d<const Real>& p_mid  , view_2d<const Real>& p_int,
                 view_2d<const Real>& qv     , view_2d<const Real>& cldfrac,
@@ -45,15 +45,8 @@ namespace scream {
                   reff_qc_h("reff_qc_h", ncol, nlay), reff_qi_h("reff_qi_h", ncol, nlay),
                   dtau067_h("dtau_067_h", ncol, nlay), dtau105_h("dtau105_h", ncol, nlay);
             lview_host_3d isccp_ctptau_h("isccp_ctptau_h", ncol, ntau, nctp);
-            // NOTE: these should already be host views, so we could probably
-            // skip creating the mirror views here
-            auto sunlit_h = Kokkos::create_mirror_view(sunlit);
-            auto skt_h = Kokkos::create_mirror_view(skt);
-            auto isccp_cldtot_h = create_mirror_view(isccp_cldtot);
 
             // Copy to layoutLeft host views
-            Kokkos::deep_copy(sunlit_h, sunlit);
-            Kokkos::deep_copy(skt_h, skt);
             for (int i = 0; i < ncol; i++) {
                 for (int j = 0; j < nlay; j++) {
                     T_mid_h(i,j) = T_mid(i,j);
@@ -76,13 +69,12 @@ namespace scream {
 
             // Call COSP wrapper
             cosp_c2f_run(ncol, nsubcol, nlay, ntau, nctp,
-                    emsfc_lw, sunlit_h.data(), skt_h.data(), T_mid_h.data(), p_mid_h.data(), p_int_h.data(),
+                    emsfc_lw, sunlit.data(), skt.data(), T_mid_h.data(), p_mid_h.data(), p_int_h.data(),
                     qv_h.data(),
                     cldfrac_h.data(), reff_qc_h.data(), reff_qi_h.data(), dtau067_h.data(), dtau105_h.data(),
-                    isccp_cldtot_h.data(), isccp_ctptau_h.data());
+                    isccp_cldtot.data(), isccp_ctptau_h.data());
 
             // Copy outputs back to layoutRight views
-            Kokkos::deep_copy(isccp_cldtot, isccp_cldtot_h);
             for (int i = 0; i < ncol; i++) {
                 for (int j = 0; j < ntau; j++) {
                     for (int k = 0; k < nctp; k++) {
