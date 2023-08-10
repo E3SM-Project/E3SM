eb7e1bab25dc77933ec8515da176abc9ce776257
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-08-10 11:03:28 -0700

    Moved a dellocation to fix a potential leak.

--- components/eamxx/src/share/io/scream_scorpio_interface.F90
+++ components/eamxx/src/share/io/scream_scorpio_interface.F90
@@ -890,11 +890,11 @@ contains
             if (associated(var%iodesc_list)) then
               ! Remove this variable as a customer of the associated iodesc
               var%iodesc_list%num_customers = var%iodesc_list%num_customers - 1
-              ! Dellocate select memory from this variable.  Note we can't just
-              ! deallocate the whole var structure because this would also
-              ! deallocate the iodesc_list.
-              call deallocate_hist_var_t(var)
             end if ! associated(var%iodesc_list)
+            ! Dellocate select memory from this variable.  Note we can't just
+            ! deallocate the whole var structure because this would also
+            ! deallocate the iodesc_list.
+            call deallocate_hist_var_t(var)
           end if ! associated(var)
           curr_var_list => curr_var_list%next  ! Move on to the next variable
         end do ! associated(curr_var_list)
