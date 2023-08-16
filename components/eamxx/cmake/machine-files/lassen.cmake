531b7817e77fec061bd98802ec023b3eebf299fd
James Foucar <jgfouca@sandia.gov>
2023-08-16 15:12:36 -0700

    Move netcdf to public place

--- components/eamxx/cmake/machine-files/lassen.cmake
+++ components/eamxx/cmake/machine-files/lassen.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
 
-set(NetCDF_PATH /g/g20/foucar1/packages/netcdf CACHE STRING "")
+set(NetCDF_PATH /usr/gdata/climdat/netcdf CACHE STRING "")
 set(LAPACK_LIBRARIES /usr/lib64/liblapack.so CACHE STRING "")
 set(CMAKE_CXX_FLAGS "-DTHRUST_IGNORE_CUB_VERSION_CHECK" CACHE STRING "" FORCE)
 
