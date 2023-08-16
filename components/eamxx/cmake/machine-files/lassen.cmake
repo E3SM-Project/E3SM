f0b60ee4e8bc10b012810be4e0e7810d2201c5ee
James Foucar <jgfouca@sandia.gov>
2023-08-16 14:36:15 -0700

    Get eamxx to build on lassen.
    A few changes were needed.
    1) A working netcdf toolchain. I built it and put it in a public place
    2) Fix lapack linking
    3) Fix thrust version check
    4) Use newer cuda module

--- components/eamxx/cmake/machine-files/lassen.cmake
+++ components/eamxx/cmake/machine-files/lassen.cmake
@@ -1,8 +1,8 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
 
-set(NetCDF_Fortran_PATH /usr/gdata/climdat/libs/netcdf-fortran/install/lassen/fortran CACHE STRING "")
-set(BLAS_LIBRARIES /usr/gdata/climdat/libs/blas/libblas.a CACHE STRING "")
-set(LAPACK_LIBRARIES /usr/gdata/climdat/libs/lapack/liblapack.a CACHE STRING "")
+set(NetCDF_PATH /g/g20/foucar1/packages/netcdf CACHE STRING "")
+set(LAPACK_LIBRARIES /usr/lib64/liblapack.so CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-DTHRUST_IGNORE_CUB_VERSION_CHECK" CACHE STRING "" FORCE)
 
 set(SCREAM_INPUT_ROOT "/usr/gdata/climdat/ccsm3data/inputdata/" CACHE STRING "")
