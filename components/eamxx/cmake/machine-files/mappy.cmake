d6c697f981ad6d27bd90c0a2ac68fb10ac919268
Elynn Wu <elynn510@gmail.com>
2023-08-28 09:53:39 -0700

    try pinning mappy python version

--- components/eamxx/cmake/machine-files/mappy.cmake
+++ components/eamxx/cmake/machine-files/mappy.cmake
@@ -1,2 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
+set(PYTHON_EXECUTABLE "/ascldap/users/jgfouca/packages/Python-3.8.5/bin/python3.8" CACHE STRING "" FORCE)
+set(PYTHON_LIBRARIES "/ascldap/users/jgfouca/packages/Python-3.8.5/lib/libpython3.so" CACHE STRING "" FORCE)
