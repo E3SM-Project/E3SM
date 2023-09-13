e58bc45e5e23b79df991ae4316db04f4152e85d3
Aaron Donahue <donahue.aaron@gmail.com>
2023-09-13 12:13:46 -0700

    Add a test case which uses vertically remapped data for nudging.
    This commit extends the test coverage by generating nudging source
    data from a vertical remap.  This will allow us to test cases where
    the vertical data structure is different for nudging than the actual
    simulation.  One key component of the new test is the guarantee that
    nudging will have some data that is masked, so we will test the way
    nudging handles masked data.
    
    NOTE: At present the new test fails, it already exposed a bug in the
    nudging implementation.  A subsequent commit will have the fix.

---
+++ components/eamxx/tests/coupled/physics_only/shoc_p3_nudging/create_vert_remap.cpp
@@ -0,0 +1,37 @@
+#include <catch2/catch.hpp>
+#include "share/io/scream_output_manager.hpp"
+#include "share/io/scream_scorpio_interface.hpp"
+
+namespace {
+
+using namespace scream;
+
+TEST_CASE("create_vert_remap","create_vert_remap")
+{
+  // Simple function to create a 1D remap column to test nudging w/ remapped data
+  ekat::Comm io_comm(MPI_COMM_WORLD);  // MPI communicator group used for I/O set as ekat object.
+  MPI_Fint fcomm = MPI_Comm_c2f(io_comm.mpi_comm());  // MPI communicator group used for I/O.  In our simple test we use MPI_COMM_WORLD, however a subset could be used.
+  scorpio::eam_init_pio_subsystem(fcomm);   // Gather the initial PIO subsystem data creater by component coupler
+
+  int nlevs = 5*SCREAM_PACK_SIZE;
+  std::vector<std::int64_t> dofs_levs(nlevs);
+  std::iota(dofs_levs.begin(),dofs_levs.end(),0);
+  std::vector<Real> p_tgt;
+  Real p_top=0, p_bot=102500;
+  Real dp = (p_bot - p_top) / (nlevs-1);
+  for (int ii=0; ii<nlevs; ++ii) {
+    Real p_loc = p_top + dp*ii;
+    p_tgt.push_back(p_loc);
+  }  
+
+  std::string remap_filename = "vertical_remap.nc";
+
+  scorpio::register_file(remap_filename, scorpio::FileMode::Write);
+  scorpio::register_dimension(remap_filename,"lev", "lev", nlevs, false);
+  scorpio::register_variable(remap_filename,"p_levs","p_levs","none",{"lev"},"real","real","Real-lev");
+  scorpio::set_dof(remap_filename,"p_levs",dofs_levs.size(),dofs_levs.data()); 
+  scorpio::eam_pio_enddef(remap_filename);
+  scorpio::grid_write_data_array(remap_filename,"p_levs",p_tgt.data(),nlevs);
+  scorpio::eam_pio_closefile(remap_filename);
+}
+} // end namespace
