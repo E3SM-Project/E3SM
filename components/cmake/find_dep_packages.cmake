c4ecf6377dc7d919ec74d3e14e27f2eb83b96408
James Foucar <jgfouca@sandia.gov>
2023-09-22 15:12:01 -0600

    Improve Kokkos handling

--- components/cmake/find_dep_packages.cmake
+++ components/cmake/find_dep_packages.cmake
@@ -11,37 +11,26 @@
 # This will allow users to easily specify a different location for all their cases by
 # simply setting ${Package}_ROOT in their shell.
 
+# Kokkos' find_package needs to come before other find_packages
+# that may define Kokkos targets so we can avoid duplicate target
+# errors.
 if (USE_KOKKOS)
-  # LB 01/23
-  # CMake's find_package, when used with multiple PATHS and PATH_SUFFIXES,
-  # follows the following rule when looking in paths:
-  #  1. look in all the path suffixes of the first PATHS entry, in the order provided
-  #  2. look in the first PATH provided
-  #  3. repeat 1-2 with the following entry of PATHS
-  #  4. look in cmake/system default paths (unless told not to).
-  # So the following cmd will fist look in the KOKKOS_PATH folder and subfolders,
-  # if KOKKOS_PATH is non-empty. Then will proceed to look in the lib, lib/cmake,
-  # and lib64/cmake subfolders of the INSTALL_SHAREDPATH. If all of these fail,
-  # it will look in INSTALL_SHAREDPATH.
 
-  if (KOKKOS_PATH)
-    set (PATHS ${KOKKOS_PATH} ${INSTALL_SHAREDPATH})
-  elseif(DEFINED ENV{KOKKOS_PATH})
-    set (PATHS $ENV{KOKKOS_PATH} ${INSTALL_SHAREDPATH})
-  else()
-    set (PATHS ${INSTALL_SHAREDPATH})
+  # Kokkos will be built in the sharedlibs if Kokkos_ROOT is
+  # unset.
+  if (NOT DEFINED ENV{Kokkos_ROOT})
+    set(ENV{Kokkos_ROOT} ${INSTALL_SHAREDPATH})
   endif()
-  find_package(Kokkos REQUIRED
-               PATHS ${PATHS}
-               PATH_SUFFIXES lib lib/cmake lib64/cmake
-               NO_DEFAULT_PATH)
-endif()
 
-if (USE_ALBANY)
-  find_package(Albany REQUIRED)
+  find_package(Kokkos REQUIRED)
 endif()
 
 # Albany depends on Trilinos
 if (USE_ALBANY OR USE_TRILINOS)
   find_package(Trilinos REQUIRED)
 endif()
+
+if (USE_ALBANY)
+  find_package(Albany REQUIRED)
+endif()
+
