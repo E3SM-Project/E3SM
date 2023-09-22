b29aebde6193762b346babebf92158ad14d74d9e
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2023-09-21 17:51:08 -0700

    add ghas mach, demo jobs, test matrix

--- .circleci/checkout.sh
+++ .circleci/checkout.sh
@@ -2,5 +2,4 @@
 
 # Replace all ssh URLs to submodules with HTTP URLs
 sed -i 's/git@github.com:/https:\/\/github.com\//' .gitmodules
-sed -i 's/git@github.com:/https:\/\/github.com\//' cime/.gitmodules
 git submodule update --init --recursive
