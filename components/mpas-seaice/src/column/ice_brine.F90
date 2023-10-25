3744bf9702b26ac112a7d1ef32b3143a29ff3a24
njeffery <njeffery@lanl.gov>
2023-10-25 13:36:34 -0700

    Removed Yohei's background vertical diffusion
    Uses Mat's implementation.
    Removed extra reference to ecosys_diag_POP_FLUX_IN
    Cleaned up some extra spaces.
    
    nonBFB

--- components/mpas-seaice/src/column/ice_brine.F90
+++ components/mpas-seaice/src/column/ice_brine.F90
@@ -317,7 +317,6 @@
       do k= 2, nblyr+1
          ikin(k) = k_o*iphin(k)**exp_h
          if (hbr_old .GT. puny) iDin(k) = iphin(k)*Dm/hbr_old**2
-
          if (hbr_old .GE. Ra_c) &
             iDin(k) = iDin(k) &
                     + l_sk*ikin(k)*gravit/viscos_dynamic*drho(k)/hbr_old**2
