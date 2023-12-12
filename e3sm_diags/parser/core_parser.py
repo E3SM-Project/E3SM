7e4361286b1920624a3a4daf99907b04bb92f6ab
Tom Vo <tomvothecoder@gmail.com>
2023-12-12 12:36:34 -0800

    Replace `parser.readfp()` with `parser.read_file()` for Python 3.12 support (#765)

--- e3sm_diags/parser/core_parser.py
+++ e3sm_diags/parser/core_parser.py
@@ -815,21 +815,21 @@ class CoreParser:
         parameters = []
 
         cfg_file_obj = self._create_cfg_hash_titles(cfg_file)
-        kwargs = (
-            {"strict": False} if sys.version_info[0] >= 3 else {}
-        )  # 'strict' keyword doesn't work in Python 2.
-        config = configparser.ConfigParser(  # type: ignore
-            **kwargs
-        )  # Allow for two lines to be the same.
-        config.readfp(cfg_file_obj)
 
-        for section in config.sections():
+        # Setting `strict=False` enables the parser to allow for any section
+        # or duplicates while reading from a single source. This is required
+        # because .cfg diagnostic files might contain duplicate sections with
+        # slight tweaks based on the set.
+        parser = configparser.ConfigParser(strict=False)
+        parser.read_file(cfg_file_obj)
+
+        for section in parser.sections():
             p = self._parameter_cls()
 
             # Remove all of the variables.
             p.__dict__.clear()
 
-            for k, v in config.items(section):
+            for k, v in parser.items(section):
                 v = yaml.safe_load(v)
                 setattr(p, k, v)
 
