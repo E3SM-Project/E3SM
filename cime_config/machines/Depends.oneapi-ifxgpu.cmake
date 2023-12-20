a06b48cd4c40ed59da9363ac1c8e9c79db491085
Azamat Mametjanov <azamat@mcs.anl.gov>
2023-12-20 04:30:16 +0000

    Work around ifx ICE with ifort

--- cime_config/machines/Depends.oneapi-ifxgpu.cmake
+++ cime_config/machines/Depends.oneapi-ifxgpu.cmake
@@ -9,3 +9,6 @@ list(APPEND MPAS_ADD_ACC_FLAGS
 foreach(ITEM IN LISTS MPAS_ADD_ACC_FLAGS)
   e3sm_add_flags("${ITEM}" "-fiopenmp -fopenmp-targets=spir64")
 endforeach()
+
+# compile mpas_seaice_core_interface.f90 with ifort, not ifx
+e3sm_add_flags("${CMAKE_BINARY_DIR}/core_seaice/model_forward/mpas_seaice_core_interface.f90" "-fc=ifort")
