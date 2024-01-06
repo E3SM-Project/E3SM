cfd410a9390f319ecfac4c0ef82353e48af4d102
Gautam Bisht <gautam.bisht@pnnl.gov>
2024-01-05 20:23:26 -0800

    Fixes an ELM setup error related to FAN model
    Changes the check to determine if the FAN model is active.
    
    Fixes #6141
    [BFB]

--- components/elm/bld/ELMBuildNamelist.pm
+++ components/elm/bld/ELMBuildNamelist.pm
@@ -3005,7 +3005,7 @@ sub setup_logic_fan {
   # Flags to control FAN (Flow of Agricultural Nitrogen) nitrogen deposition (manure and fertilizer)
   #
       if ( $nl_flags->{'bgc_mode'} =~/cn|bgc/ ) { 
-          if( $opts->{'fan'} ) {
+          if( $nl_flags->{'use_fan'} eq ".true." ) {
              add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'use_fan',
                          'use_cn'=>$nl_flags->{'use_cn'} );
              $nl_flags->{'use_fan'} = $nl->get_value('use_fan');
@@ -3021,7 +3021,7 @@ sub setup_logic_fan {
             fatal_error('Cannot use_fan if use_crop is false');
           }   #
 
-          if( $opts->{'fan'} ) {
+          if( $nl_flags->{'use_fan'} eq ".true." ) {
              add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, "fanmapalgo", 'phys'=>$nl_flags->{'phys'},
                       'use_cn'=>$nl_flags->{'use_cn'}, 'hgrid'=>$nl_flags->{'res'} );
              add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, "stream_year_first_fan", 'phys'=>$nl_flags->{'phys'},
