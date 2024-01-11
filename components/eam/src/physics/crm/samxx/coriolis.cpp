649eeff19ca0f42bee4517b64a27cc2f72887cab
Walter Hannah <hannah6@llnl.gov>
2024-01-11 13:02:16 -0800

    more coriolis updates

--- components/eam/src/physics/crm/samxx/coriolis.cpp
+++ components/eam/src/physics/crm/samxx/coriolis.cpp
@@ -8,6 +8,7 @@ void coriolis() {
   YAKL_SCOPE( fcorzy  , ::fcorzy);
   YAKL_SCOPE( dudt    , ::dudt);
   YAKL_SCOPE( dvdt    , ::dvdt);
+  YAKL_SCOPE( dwdt    , ::dwdt);
   YAKL_SCOPE( na      , ::na);
   YAKL_SCOPE( vg0     , ::vg0);
   YAKL_SCOPE( ug0     , ::ug0);
@@ -33,21 +34,50 @@ void coriolis() {
                       u(k,jb+offy_u,ic+offx_u,icrm));
       dvdt(na-1,k,j,i,icrm)=dvdt(na-1,k,j,i,icrm)-0.5*(fcory(j+offy_fcory,icrm)+fcory(jb+offy_fcory,icrm))*
                             (u_av-ug0(k,icrm));
+      #ifdef MMF_DO_CORIOLIS_W             
+        real u_av_on_w = 0.25*( u(k ,j+offy_u,i+offx_u,icrm) + u(k ,j+offy_u,ic+offx_u,icrm)
+                               +u(kc,j+offy_u,i+offx_u,icrm) + u(kc,j+offy_u,ic+offx_u,icrm));
+        dwdt(na-1,kc,j,i,icrm)= dwdt(na-1,kc,j,i,icrm) + fcorzy(j,icrm)*u_av_on_w;
+      #endif
     });
   } else {
     // for (int k=0; k<nzm; k++) {
     //   for (int j=0; j<ny; j++) {
     //     for (int i=0; i<nx; i++) {
     //       for (int icrm=0; icrm<ncrms; icrm++) {
+    // parallel_for( SimpleBounds<4>(nzm,ny,nx,ncrms) , YAKL_LAMBDA (int k, int j, int i, int icrm) {
+    //   int kc=k+1;
+    //   int kb=k-1;
+    //   int ib=i-1;
+    //   int ic=i+1;
+      // real w_av=0.25*(w(kc,j+offy_w,i+offx_w,icrm)+w(kc,j+offy_w,ib+offx_w,icrm)+
+      //                 w(k,j+offy_w,i+offx_w,icrm)+w(k,j+offy_w,ib+offx_w,icrm));
+      // dudt(na-1,k,j,i,icrm)=dudt(na-1,k,j,i,icrm)+fcory(j+offy_fcory,icrm)*(v(k,j+offy_v,i+offx_v,icrm)-vg0(k,icrm))-fcorzy(j,icrm)*w_av;
+      // dvdt(na-1,k,j,i,icrm)=dvdt(na-1,k,j,i,icrm)-fcory(j+offy_fcory,icrm)*(u(k,j+offy_u,i+offx_u,icrm)-ug0(k,icrm));
+    // });
+
+    // ignore V wind terms for testing non-traditional coriolis terms
     parallel_for( SimpleBounds<4>(nzm,ny,nx,ncrms) , YAKL_LAMBDA (int k, int j, int i, int icrm) {
       int kc=k+1;
+      int kb=k-1;
       int ib=i-1;
       int ic=i+1;
-      real w_av=0.25*(w(kc,j+offy_w,i+offx_w,icrm)+w(kc,j+offy_w,ib+offx_w,icrm)+
-                      w(k,j+offy_w,i+offx_w,icrm)+w(k,j+offy_w,ib+offx_w,icrm));
-      // dudt(na-1,k,j,i,icrm)=dudt(na-1,k,j,i,icrm)+fcory(j+offy_fcory,icrm)*(v(k,j+offy_v,i+offx_v,icrm)-vg0(k,icrm))-fcorzy(j,icrm)*w_av;
-      dudt(na-1,k,j,i,icrm)=dudt(na-1,k,j,i,icrm) - fcorzy(j,icrm)*w_av;
-      // dvdt(na-1,k,j,i,icrm)=dvdt(na-1,k,j,i,icrm)-fcory(j+offy_fcory,icrm)*(u(k,j+offy_u,i+offx_u,icrm)-ug0(k,icrm));
+      #ifdef MMF_DO_CORIOLIS_U
+        real w_av_on_u = 0.25*( w(kc,j+offy_w,i+offx_w,icrm) + w(kc,j+offy_w,ib+offx_w,icrm)
+                               +w(k ,j+offy_w,i+offx_w,icrm) + w(k ,j+offy_w,ib+offx_w,icrm));
+        dudt(na-1,k,j,i,icrm) = dudt(na-1,k ,j,i,icrm) - fcorzy(j,icrm)*w_av_on_u;
+      #endif
+      #ifdef MMF_DO_CORIOLIS_W
+        real u_av_on_w = 0.25*( u(k ,j+offy_u,i+offx_u,icrm) + u(k ,j+offy_u,ic+offx_u,icrm)
+                               +u(kc,j+offy_u,i+offx_u,icrm) + u(kc,j+offy_u,ic+offx_u,icrm));
+        dwdt(na-1,kc,j,i,icrm)= dwdt(na-1,kc,j,i,icrm) + fcorzy(j,icrm)*u_av_on_w;
+      #endif
+      #ifdef MMF_DO_CORIOLIS_ESMT
+      if (use_ESMT) {
+        real w_av_on_s = 0.5*( w(kc,j+offy_w,i+offx_w,icrm) + w(k,j+offy_w,i+offx_w,icrm) );
+        u_esmt(k,j+offy_s,i+offx_s,icrm) = u_esmt(k,j+offy_s,i+offx_s,icrm) + - fcorzy(j,icrm)*w_av_on_s;
+      }
+      #endif
     });
   }
 
