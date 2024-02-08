822e3db78f8de3188d04ac724feb9760b43a9040
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2024-02-07 18:43:41 -0800

    use mahf708/e3sm-imgs:0.0.3

--- .circleci/container.sh
+++ .circleci/container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 singularity pull e3sm.sif \
-    docker://ghcr.io/mahf708/e3sm-imgs@sha256:d1030a6f4e3a53f682859436a26b30a9477d69423829ae1d9c1b5ab4e255430d
+    docker://ghcr.io/mahf708/e3sm-imgs:0.0.3
 
 if [ $? -ne 0 ]; then
     exit -1
