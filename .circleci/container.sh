3c60a8809021f8ed8474b45839ce0aa24047c08c
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2024-02-07 23:00:16 -0800

    go back to sha for now

--- .circleci/container.sh
+++ .circleci/container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 singularity pull e3sm.sif \
-    docker://ghcr.io/mahf708/e3sm-imgs:0.0.4
+    docker://ghcr.io/mahf708/e3sm-imgs@sha256:bb0facbb01b6221931e8702d7ad1427bc31a4bf0551cca3ee4561706ada39519
 
 if [ $? -ne 0 ]; then
     exit -1
