ecc0b523ccd5d6ccdea50dc7574b9e5b6c4893c5
Balwinder Singh <balwindersingh@gmail.com>
2024-02-19 19:48:50 -0800

    Minimal set of additions for drydep interface

---
+++ components/eamxx/src/physics/mam/eamxx_mam_dry_deposition_process_interface.cpp
@@ -0,0 +1,93 @@
+#include "physics/mam/eamxx_mam_dry_deposition_process_interface.hpp"
+
+/*
+Future work:
+Wirte comments
+write in/outs for all variables clearly
+*/
+
+namespace scream {
+
+// =========================================================================================
+MAMDryDep::MAMDryDep(const ekat::Comm &comm, const ekat::ParameterList &params)
+    : AtmosphereProcess(comm, params) {
+  /* Anything that can be initialized without grid information can be
+   * initialized here. Like universal constants, mam wetscav options.
+   */
+}
+
+// =========================================================================================
+void MAMDryDep::set_grids(
+    const std::shared_ptr<const GridsManager> grids_manager) {
+  using namespace ekat::units;
+
+  // The units of mixing ratio Q are technically non-dimensional.
+  // Nevertheless, for output reasons, we like to see 'kg/kg'.
+  auto q_unit = kg / kg;
+  q_unit.set_string("kg/kg");
+
+  auto n_unit = 1 / kg;  // units of number mixing ratios of tracers
+  n_unit.set_string("#/kg");
+
+  grid_                = grids_manager->get_grid("Physics");
+  const auto &grid_name = grid_->name();
+
+  ncol_ = grid_->get_num_local_dofs();       // Number of columns on this rank
+  nlev_ = grid_->get_num_vertical_levels();  // Number of levels per column
+
+  // Define the different field layouts that will be used for this process
+  using namespace ShortFieldTagsNames;
+
+  // Layout for 3D (2d horiz X 1d vertical) variable defined at mid-level and
+  // interfaces
+  const FieldLayout scalar3d_layout_mid{{COL, LEV}, {ncol_, nlev_}};
+
+  // Layout for 2D (2d horiz) variable defined at mid-level and
+  // interfaces
+  const FieldLayout scalar2d_layout_mid{{COL}, {ncol_}};
+
+  // -------------------------------------------------------------------------------------------------------------------------
+  // These variables are "required" or pure inputs for the process
+  // -------------------------------------------------------------------------------------------------------------------------
+
+  
+}
+
+// =========================================================================================
+// ON HOST, returns the number of bytes of device memory needed by the above
+// Buffer type given the number of columns and vertical levels
+size_t MAMDryDep::requested_buffer_size_in_bytes() const {
+  return mam_coupling::buffer_size(ncol_, nlev_);
+}
+
+// =========================================================================================
+// ON HOST, initializeѕ the Buffer type with sufficient memory to store
+// intermediate (dry) quantities on the given number of columns with the given
+// number of vertical levels. Returns the number of bytes allocated.
+void MAMDryDep::init_buffers(const ATMBufferManager &buffer_manager) {
+  EKAT_REQUIRE_MSG(
+      buffer_manager.allocated_bytes() >= requested_buffer_size_in_bytes(),
+      "Error! Insufficient buffer size.\n");
+
+  size_t used_mem =
+      mam_coupling::init_buffer(buffer_manager, ncol_, nlev_, buffer_);
+  EKAT_REQUIRE_MSG(used_mem == requested_buffer_size_in_bytes(),
+                   "Error! Used memory != requested memory for MAMDryDep.");
+}
+
+// =========================================================================================
+void MAMDryDep::initialize_impl(const RunType run_type) {
+  // Gather runtime options
+  //(e.g.) runtime_options.lambda_low    = m_params.get<double>("lambda_low");
+
+  
+}
+
+// =========================================================================================
+void MAMDryDep::run_impl(const double dt) {
+  
+  std::cout << "End of derydep run" << std::endl;
+}
+
+// =========================================================================================
+}  // namespace scream
