21fb690acb99654f89b2f0c8ff0552a6914085e1
Aaron Donahue <donahue.aaron@gmail.com>
2024-02-27 09:44:31 -0800

    Update machine config for Livermore Computing machines
    This update reflects the new directory structure for storing input
    data to E3SM runs.  The switch from `climdat` to `e3sm` in the dir
    name.

--- components/eamxx/cmake/machine-files/lassen.cmake
+++ components/eamxx/cmake/machine-files/lassen.cmake
@@ -1,8 +1,8 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
 
-set(NetCDF_Fortran_PATH /usr/gdata/climdat/libs/netcdf-fortran/install/lassen/fortran CACHE STRING "")
-set(BLAS_LIBRARIES /usr/gdata/climdat/libs/blas/libblas.a CACHE STRING "")
-set(LAPACK_LIBRARIES /usr/gdata/climdat/libs/lapack/liblapack.a CACHE STRING "")
+set(NetCDF_Fortran_PATH /usr/gdata/e3sm/libs/netcdf-fortran/install/lassen/fortran CACHE STRING "")
+set(BLAS_LIBRARIES /usr/gdata/e3sm/libs/blas/libblas.a CACHE STRING "")
+set(LAPACK_LIBRARIES /usr/gdata/e3sm/libs/lapack/liblapack.a CACHE STRING "")
 
-set(SCREAM_INPUT_ROOT "/usr/gdata/climdat/ccsm3data/inputdata/" CACHE STRING "")
+set(SCREAM_INPUT_ROOT "/usr/gdata/e3sm/ccsm3data/inputdata/" CACHE STRING "")
--- components/eamxx/cmake/machine-files/syrah.cmake
+++ components/eamxx/cmake/machine-files/syrah.cmake
@@ -10,4 +10,4 @@ option(Kokkos_ARCH_SNB "" ON)
 set(CMAKE_CXX_FLAGS "-w -cxxlib=/usr/tce/packages/gcc/gcc-8.3.1/rh" CACHE STRING "" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS "-L/usr/tce/packages/gcc/gcc-8.3.1/rh/lib/gcc/x86_64-redhat-linux/8/ -mkl" CACHE STRING "" FORCE)
 
-set(SCREAM_INPUT_ROOT "/usr/gdata/climdat/ccsm3data/inputdata/" CACHE STRING "")
+set(SCREAM_INPUT_ROOT "/usr/gdata/e3sm/ccsm3data/inputdata/" CACHE STRING "")
