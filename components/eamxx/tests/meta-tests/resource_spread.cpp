5de66a6310d2678dc5c6af9a92750fa2a766b17f
Luca Bertagna <lbertag@sandia.gov>
2024-03-12 18:55:20 -0600

    EAMxx: do not build Ekat if SCREAM_FAKE_ONLY is defined in the env
    Makes scripts-tests a bit faster

--- components/eamxx/tests/meta-tests/resource_spread.cpp
+++ components/eamxx/tests/meta-tests/resource_spread.cpp
@@ -1,19 +1,11 @@
-#include "ekat/kokkos/ekat_kokkos_types.hpp"
-
-#include <catch2/catch.hpp>
-
-#include <iostream>
 #include <chrono>
 #include <thread>
 
-namespace scream {
-
-TEST_CASE("rank_and_thread_spread", "[fake_infra_test]")
+int main(int, char**)
 {
   // Nothing needs to be done here except sleeping to give a chance
   // for tests to run concurrently.
   const auto seconds_to_sleep = 5;
   std::this_thread::sleep_for(std::chrono::seconds(seconds_to_sleep));
+  return 0;
 }
-
-} // empty namespace
