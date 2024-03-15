63c0447af113408ee19af2cac9e4b1beca0616c4
Luca Bertagna <lbertag@sandia.gov>
2024-03-14 22:15:41 -0600

    EAMxx: handle OpenMP and MPI manually for resource_spread target
    We build these tests without ekat (or kokkos even), for speed.
    So we must take care of linking to OpenMP/MPI ourselves.

--- components/eamxx/tests/meta-tests/resource_spread.cpp
+++ components/eamxx/tests/meta-tests/resource_spread.cpp
@@ -1,11 +1,17 @@
 #include <chrono>
 #include <thread>
 
-int main(int, char**)
+#include <mpi.h>
+
+int main(int argc, char** argv)
 {
+  MPI_Init(&argc,&argv);
+
   // Nothing needs to be done here except sleeping to give a chance
   // for tests to run concurrently.
   const auto seconds_to_sleep = 5;
   std::this_thread::sleep_for(std::chrono::seconds(seconds_to_sleep));
+
+  MPI_Finalize();
   return 0;
 }
