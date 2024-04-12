63df74c0bb17552d825f55cb3b04edc5f5b83014
Walter Hannah <whannah1@gmail.com>
2024-04-12 12:30:50 -0400

    implement PAM driver subcycling

--- components/eam/src/physics/crm/pam/pam_driver.cpp
+++ components/eam/src/physics/crm/pam/pam_driver.cpp
@@ -1,5 +1,4 @@
 #include "pam_coupler.h"
-// #include "params.h"
 #include "Dycore.h"
 #include "Microphysics.h"
 #include "SGS.h"
@@ -14,26 +13,90 @@
 #include "pam_output.h"
 #include "pam_accelerate.h"
 #include "pam_variance_transport.h"
+#include "pam_hyperdiffusion.h"
 #include "sponge_layer.h"
 #include "surface_friction.h"
 #include "scream_cxx_interface_finalize.h"
-
-#include "pam_hyperdiffusion.h"
 
 // Needed for p3_init
 #include "p3_functions.hpp"
 #include "p3_f90.hpp"
 
 #include "pam_debug.h"
-bool constexpr enable_check_state = true;
+bool constexpr enable_check_state = false;
 
-extern "C" void pam_driver() {
+
+inline int pam_driver_set_subcycle_timestep( pam::PamCoupler &coupler, real crm_dt_fixed ) {
+  // calculate the CFL condition and adjust the PAM time loop subcylcing
   //------------------------------------------------------------------------------------------------
-  using yakl::intrinsics::abs;
-  using yakl::intrinsics::maxval;
-  using yakl::atomicAdd;
   using yakl::c::parallel_for;
   using yakl::c::SimpleBounds;
+  using yakl::atomicMax;
+  //------------------------------------------------------------------------------------------------
+  auto nens          = coupler.get_option<int>("ncrms");
+  auto gcm_nlev      = coupler.get_option<int>("gcm_nlev");
+  auto crm_nz        = coupler.get_option<int>("crm_nz");
+  auto crm_nx        = coupler.get_option<int>("crm_nx");
+  auto crm_ny        = coupler.get_option<int>("crm_ny");
+  auto crm_dx        = coupler.get_option<real>("crm_dx");
+  auto crm_dy        = coupler.get_option<real>("crm_dy");
+  auto &dm_device = coupler.get_data_manager_device_readonly();
+  auto &dm_host   = coupler.get_data_manager_host_readonly();
+  auto uvel       = dm_device.get<real const,4>("uvel");
+  auto wvel       = dm_device.get<real const,4>("wvel");
+  auto input_zint = dm_host.get<real const,2>("input_zint").createDeviceCopy();
+  //------------------------------------------------------------------------------------------------
+  yakl::ParallelMax<real,yakl::memDevice> pmax( crm_nz*nens );
+  real cfl = 0;
+  int num_subcycle = 1;
+  int constexpr max_num_subcycle = 10;
+  real2d wvel_max("wvel_max",crm_nz,nens);
+  real2d uvel_max("uvel_max",crm_nz,nens);
+  real2d cfl_max("cfl_max",  crm_nz,nens);
+  //------------------------------------------------------------------------------------------------
+  // initialize max U and W arrays
+  parallel_for( SimpleBounds<2>(crm_nz,nens) , YAKL_LAMBDA (int k, int n) {
+    wvel_max(k,n) = 0.0;
+    uvel_max(k,n) = 0.0;
+  });
+  // calculate max U and W
+  parallel_for( SimpleBounds<4>(crm_nz,crm_ny,crm_nx,nens) , YAKL_LAMBDA (int k, int j, int i, int n) {
+    yakl::atomicMax(uvel_max(k,n), sqrt(uvel(k,j,i,n)*uvel(k,j,i,n)) );
+    yakl::atomicMax(wvel_max(k,n), fabs(wvel(k,j,i,n)) );
+  });
+  // find max CFL between horizontal and vertical CFL values
+  parallel_for( SimpleBounds<2>(crm_nz,nens) , YAKL_LAMBDA (int k, int n) {
+    int k_gcm = gcm_nlev-1-k;
+    real crm_dz = input_zint(k_gcm,n) - input_zint(k_gcm+1,n);
+    real cfl_u = uvel_max(k,n)*crm_dt_fixed/crm_dx;
+    real cfl_w = wvel_max(k,n)*crm_dt_fixed/crm_dz;
+    cfl_max(k,n) = max(cfl_u,cfl_w);
+  });
+  // calculate final CFL across ensemble
+  real cfl_loc = pmax(cfl_max.data());
+  cfl = max(cfl,cfl_loc);
+  // update number of subcycles and time step
+  num_subcycle = max(num_subcycle,max(1,static_cast<int>(ceil(cfl/0.7))));
+  real crm_dt_subcycle = crm_dt_fixed / num_subcycle;
+  coupler.set_option<real>("crm_dt",crm_dt_subcycle);
+  // check for excessive subcylcing - don't exit, just print
+  if(num_subcycle > max_num_subcycle) {
+    real umax = pmax(uvel_max.data());
+    real wmax = pmax(wvel_max.data());
+    printf("PAM_DRIVER - WARNING: excessive subcycling!"+\
+          " - num_subcycle: %3.3d  dt: %8.4f  cfl: %8.4f  umax: %8.2f  wmax: %8.2f \n",
+          num_subcycle,crm_dt_subcycle,cfl,umax,wmax);
+    // exit(-1);
+  }
+
+  return num_subcycle;
+  //------------------------------------------------------------------------------------------------
+}
+
+
+extern "C" void pam_driver() {
+  // This is the primary method for running the PAM CRM in E3SM-MMF.
+  //------------------------------------------------------------------------------------------------
   auto &coupler = pam_interface::get_coupler();
   //------------------------------------------------------------------------------------------------
   // retreive coupler options
@@ -42,8 +105,10 @@ extern "C" void pam_driver() {
   auto crm_nz        = coupler.get_option<int>("crm_nz");
   auto crm_nx        = coupler.get_option<int>("crm_nx");
   auto crm_ny        = coupler.get_option<int>("crm_ny");
+  // auto crm_dx        = coupler.get_option<real>("crm_dx");
+  // auto crm_dy        = coupler.get_option<real>("crm_dy");
   auto gcm_dt        = coupler.get_option<real>("gcm_dt");
-  auto crm_dt        = coupler.get_option<real>("crm_dt");
+  auto crm_dt_fixed  = coupler.get_option<real>("crm_dt");
   auto is_first_step = coupler.get_option<bool>("is_first_step");
   auto is_restart    = coupler.get_option<bool>("is_restart");
   bool use_crm_accel = coupler.get_option<bool>("use_crm_accel");
@@ -162,7 +227,7 @@ extern "C" void pam_driver() {
   //------------------------------------------------------------------------------------------------
 
   // set number of CRM steps
-  int nstop = int(gcm_dt/crm_dt);
+  int nstop = int(gcm_dt/crm_dt_fixed);
 
   // for mean-state acceleration adjust nstop and diagnose horizontal means
   if (use_crm_accel) { 
@@ -171,51 +236,58 @@ extern "C" void pam_driver() {
   };
 
   // Run the CRM
-  real etime_crm = 0;
   int nstep = 0;
-  // while (etime_crm < gcm_dt) {
   while (nstep < nstop) {
-    if (crm_dt == 0.) { crm_dt = dycore.compute_time_step(coupler); }
-    if (etime_crm + crm_dt > gcm_dt) { crm_dt = gcm_dt - etime_crm; }
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    auto num_subcycle = pam_driver_set_subcycle_timestep(coupler,crm_dt_fixed);
+    #if defined(MMF_PAM_DYCOR_SPAM)
+      dycore.update_dt(coupler);
+    #endif
 
     if (enable_check_state) { pam_debug_check_state(coupler, 1, nstep); }
 
-    // Apply forcing tendencies
-    if (use_MMF_VT) { pam_variance_transport_apply_forcing(coupler); }
-    if (enable_check_state) { pam_debug_check_state(coupler, 2, nstep); }
-    coupler.run_module( "apply_gcm_forcing_tendencies" , modules::apply_gcm_forcing_tendencies );
-    if (enable_check_state) { pam_debug_check_state(coupler, 3, nstep); }
-    coupler.run_module( "radiation"                    , [&] (pam::PamCoupler &coupler) {rad   .timeStep(coupler);} );
-    if (enable_check_state) { pam_debug_check_state(coupler, 4, nstep); }
+    // loop for adaptive subcyling based on CFL
+    for(int icycle=1; icycle<=num_subcycle; icycle++) {
 
-    // Dynamics
-    if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
-    if (do_density_save_recall)    { pam_state_save_dry_density(coupler); }
-    coupler.run_module( "dycore", [&] (pam::PamCoupler &coupler) {dycore.timeStep(coupler);} );
-    if (do_density_save_recall)    { pam_state_recall_dry_density(coupler); }
-    if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"dycor"); }
-    if (enable_check_state)        { pam_debug_check_state(coupler, 5, nstep); }
+      // Apply forcing tendencies
+      if (use_MMF_VT) { pam_variance_transport_apply_forcing(coupler); }
+      if (enable_check_state) { pam_debug_check_state(coupler, 2, nstep); }
+      coupler.run_module( "apply_gcm_forcing_tendencies" , modules::apply_gcm_forcing_tendencies );
+      if (enable_check_state) { pam_debug_check_state(coupler, 3, nstep); }
+      coupler.run_module( "radiation"                    , [&] (pam::PamCoupler &coupler) {rad   .timeStep(coupler);} );
+      if (enable_check_state) { pam_debug_check_state(coupler, 4, nstep); }
 
-    // Sponge layer damping
-    if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
-    coupler.run_module( "sponge_layer", modules::sponge_layer );
-    if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"sponge"); }
+      // Dynamics
+      if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
+      if (do_density_save_recall)    { pam_state_save_dry_density(coupler); }
+      coupler.run_module( "dycore", [&] (pam::PamCoupler &coupler) {dycore.timeStep(coupler);} );
+      if (do_density_save_recall)    { pam_state_recall_dry_density(coupler); }
+      if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"dycor"); }
+      if (enable_check_state)        { pam_debug_check_state(coupler, 5, nstep); }
 
-    // Apply hyperdiffusion to account for lack of horizontal mixing in SHOC
-    pam_hyperdiffusion(coupler);
+      // Sponge layer damping
+      if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
+      coupler.run_module( "sponge_layer", modules::sponge_layer );
+      if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"sponge"); }
 
-    // Turbulence - SHOC
-    coupler.run_module( "compute_surface_friction", modules::compute_surface_friction );
-    if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
-    coupler.run_module( "sgs", [&] (pam::PamCoupler &coupler) {sgs   .timeStep(coupler);} );
-    if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"sgs"); }
-    if (enable_check_state)        { pam_debug_check_state(coupler, 6, nstep); }
+      // Apply hyperdiffusion to account for lack of horizontal mixing in SHOC
+      pam_hyperdiffusion(coupler);
 
-    // Microphysics - P3
-    if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
-    coupler.run_module( "micro", [&] (pam::PamCoupler &coupler) {micro .timeStep(coupler);} );
-    if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"micro"); }
-    if (enable_check_state)        { pam_debug_check_state(coupler, 7, nstep); }
+      // Turbulence - SHOC
+      coupler.run_module( "compute_surface_friction", modules::compute_surface_friction );
+      if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
+      coupler.run_module( "sgs", [&] (pam::PamCoupler &coupler) {sgs   .timeStep(coupler);} );
+      if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"sgs"); }
+      if (enable_check_state)        { pam_debug_check_state(coupler, 6, nstep); }
+
+      // Microphysics - P3
+      if (enable_physics_tend_stats) { pam_statistics_save_state(coupler); }
+      coupler.run_module( "micro", [&] (pam::PamCoupler &coupler) {micro .timeStep(coupler);} );
+      if (enable_physics_tend_stats) { pam_statistics_aggregate_tendency(coupler,"micro"); }
+      if (enable_check_state)        { pam_debug_check_state(coupler, 7, nstep); }
+
+    } // num_subcycle
 
     // CRM mean state acceleration
     if (use_crm_accel && !coupler.get_option<bool>("crm_acceleration_ceaseflag")) {
@@ -227,7 +299,6 @@ extern "C" void pam_driver() {
     pam_radiation_timestep_aggregation(coupler);
     pam_statistics_timestep_aggregation(coupler);
 
-    etime_crm += crm_dt;
     nstep += 1;
   }
   //------------------------------------------------------------------------------------------------
