E3SM-Project/scream

EAMxx: allow to set names for each dimension inside a field layout
If not provided, we default to what e2str(FieldTag) returns

components/eamxx/src/share/field/field_tag.hpp
@@ -122,6 +122,15 @@ inline std::string e2str (const FieldTag ft) {
   return name;
 }
 
+inline std::vector<std::string> evec2str (const std::vector<FieldTag>& tags) {
+  std::vector<std::string> names;
+  names.reserve(tags.size());
+  for (auto t : tags) {
+    names.push_back(e2str(t));
+  }
+  return names;
+}
+
 // Allow to stream FieldTag values as strings.
 inline std::ostream& operator<< (std::ostream& out, const FieldTag t) {
   out << e2str(t);

E3SM-Project/E3SM

components/eamxx/src/share/field/field_tag.hpp
@@ -151,6 +151,15 @@ inline std::string e2str (const FieldTag ft) {
   return name;
 }
 
+inline std::vector<std::string> tags2str (const std::vector<FieldTag>& tags) {
+  std::vector<std::string> names;
+  names.reserve(tags.size());
+  for (auto t : tags) {
+    names.push_back(e2str(t));
+  }
+  return names;
+}
+
 // Allow to stream FieldTag values as strings.
 inline std::ostream& operator<< (std::ostream& out, const FieldTag t) {
   out << e2str(t);