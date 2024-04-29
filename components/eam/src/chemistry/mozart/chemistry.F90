432757a4ad3632925f2d34b6eafc4a38c7e183cd
Hsiang-He Lee <ac.lee1061@chrlogin2.lcrc.anl.gov>
2024-04-29 13:40:15 -0500

    skip gas_ac calculation when nstep=0

--- components/eam/src/chemistry/mozart/chemistry.F90
+++ components/eam/src/chemistry/mozart/chemistry.F90
@@ -1574,38 +1574,43 @@ end function chem_is_active
            end if
            ! HHLEE 20210923
            if (history_gaschmbudget_2D_levels .or. history_chemdyg_summary) then
-             ftem_layers = 0.0_r8
-             gas_ac_layers = 0.0_r8
+             if( nstep == 0 ) then
+                ftem_layers = 0.0_r8
+                gas_ac_layers = 0.0_r8
+             else
+                ftem_layers = 0.0_r8
+                gas_ac_layers = 0.0_r8
 
-             gas_ac_idx = pbuf_get_index(gas_ac_name(n))
-             call pbuf_get_field(pbuf, gas_ac_idx, gas_ac )
+                gas_ac_idx = pbuf_get_index(gas_ac_name(n))
+                call pbuf_get_field(pbuf, gas_ac_idx, gas_ac )
 
-             if (gaschmbudget_2D_L1_e .lt. gaschmbudget_2D_L1_s .or. gaschmbudget_2D_L2_e .lt. gaschmbudget_2D_L2_s .or. &
-                 gaschmbudget_2D_L3_e .lt. gaschmbudget_2D_L3_s .or. gaschmbudget_2D_L4_e .lt. gaschmbudget_2D_L4_s ) then
-                   call endrun('chem_readnl: ERROR 2D chem diags layers, layer ending index is less than starting index')
-             end if
+                if (gaschmbudget_2D_L1_e .lt. gaschmbudget_2D_L1_s .or. gaschmbudget_2D_L2_e .lt. gaschmbudget_2D_L2_s .or. &
+                        gaschmbudget_2D_L3_e .lt. gaschmbudget_2D_L3_s .or. gaschmbudget_2D_L4_e .lt. gaschmbudget_2D_L4_s ) then
+                        call endrun('chem_readnl: ERROR 2D chem diags layers, layer ending index is less than starting index')
+                end if
 
-             do k= gaschmbudget_2D_L1_s, gaschmbudget_2D_L1_e ! 0-90 hPa
-               ftem_layers(:ncol,1) = ftem_layers(:ncol,1) + ftem(:ncol,k)
-               gas_ac_layers(:ncol,1) = gas_ac_layers(:ncol,1) + gas_ac(:ncol,k)
-             end do
-             do k= gaschmbudget_2D_L2_s, gaschmbudget_2D_L2_e ! 90-300 hPa
-               ftem_layers(:ncol,2) = ftem_layers(:ncol,2) + ftem(:ncol,k)
-               gas_ac_layers(:ncol,2) = gas_ac_layers(:ncol,2) + gas_ac(:ncol,k)
-             end do
-             do k= gaschmbudget_2D_L3_s, gaschmbudget_2D_L3_e ! 300-850 hPa
-               ftem_layers(:ncol,3) = ftem_layers(:ncol,3) + ftem(:ncol,k)
-               gas_ac_layers(:ncol,3) = gas_ac_layers(:ncol,3) + gas_ac(:ncol,k)
-             end do
-             do k= gaschmbudget_2D_L4_s, gaschmbudget_2D_L4_e ! 850 hPa - surface
-               ftem_layers(:ncol,4) = ftem_layers(:ncol,4) + ftem(:ncol,k)
-               gas_ac_layers(:ncol,4) = gas_ac_layers(:ncol,4) + gas_ac(:ncol,k)
-             end do
-             if (history_gaschmbudget_2D_levels ) then
-             call outfld(trim(solsym(n))//'_2DMSB_L1', ftem_layers(:ncol,1), pcols, lchnk)
-             call outfld(trim(solsym(n))//'_2DMSB_L2', ftem_layers(:ncol,2), pcols, lchnk)
-             call outfld(trim(solsym(n))//'_2DMSB_L3', ftem_layers(:ncol,3), pcols, lchnk)
-             call outfld(trim(solsym(n))//'_2DMSB_L4', ftem_layers(:ncol,4), pcols, lchnk)
+                do k= gaschmbudget_2D_L1_s, gaschmbudget_2D_L1_e ! 0-90 hPa
+                   ftem_layers(:ncol,1) = ftem_layers(:ncol,1) + ftem(:ncol,k)
+                   gas_ac_layers(:ncol,1) = gas_ac_layers(:ncol,1) + gas_ac(:ncol,k)
+                end do
+                do k= gaschmbudget_2D_L2_s, gaschmbudget_2D_L2_e ! 90-300 hPa
+                   ftem_layers(:ncol,2) = ftem_layers(:ncol,2) + ftem(:ncol,k)
+                   gas_ac_layers(:ncol,2) = gas_ac_layers(:ncol,2) + gas_ac(:ncol,k)
+                end do
+                do k= gaschmbudget_2D_L3_s, gaschmbudget_2D_L3_e ! 300-850 hPa
+                   ftem_layers(:ncol,3) = ftem_layers(:ncol,3) + ftem(:ncol,k)
+                   gas_ac_layers(:ncol,3) = gas_ac_layers(:ncol,3) + gas_ac(:ncol,k)
+                end do
+                do k= gaschmbudget_2D_L4_s, gaschmbudget_2D_L4_e ! 850 hPa - surface
+                   ftem_layers(:ncol,4) = ftem_layers(:ncol,4) + ftem(:ncol,k)
+                   gas_ac_layers(:ncol,4) = gas_ac_layers(:ncol,4) + gas_ac(:ncol,k)
+                end do
+                if (history_gaschmbudget_2D_levels ) then
+                call outfld(trim(solsym(n))//'_2DMSB_L1', ftem_layers(:ncol,1), pcols, lchnk)
+                call outfld(trim(solsym(n))//'_2DMSB_L2', ftem_layers(:ncol,2), pcols, lchnk)
+                call outfld(trim(solsym(n))//'_2DMSB_L3', ftem_layers(:ncol,3), pcols, lchnk)
+                call outfld(trim(solsym(n))//'_2DMSB_L4', ftem_layers(:ncol,4), pcols, lchnk)
+                endif
              endif
 
              if( nstep == 0 ) then
@@ -1627,14 +1632,19 @@ end function chem_is_active
              ! for the tropospheric budget diagnostics
              if (trim(solsym(n))=='O3' .or. trim(solsym(n))=='O3LNZ' .or. &
                  trim(solsym(n))=='N2OLNZ' .or. trim(solsym(n))=='CH4LNZ') then
-                ftem_layers = 0.0_r8
-                gas_ac_layers = 0.0_r8
-                do k=1,pver
-                   ftem_layers(:ncol,1) = ftem_layers(:ncol,1) + ftem(:ncol,k) * tropFlagInt(:ncol,k)
-                   gas_ac_layers(:ncol,1) = gas_ac_layers(:ncol,1) + gas_ac(:ncol,k) * tropFlagInt(:ncol,k)
-                end do
-                if (history_gaschmbudget_2D_levels ) then
-                call outfld(trim(solsym(n))//'_2DMSB_trop', ftem_layers(:ncol,1), pcols, lchnk )
+                if( nstep == 0 ) then
+                    ftem_layers = 0.0_r8
+                    gas_ac_layers = 0.0_r8
+                else
+                    ftem_layers = 0.0_r8
+                    gas_ac_layers = 0.0_r8
+                    do k=1,pver
+                       ftem_layers(:ncol,1) = ftem_layers(:ncol,1) + ftem(:ncol,k) * tropFlagInt(:ncol,k)
+                       gas_ac_layers(:ncol,1) = gas_ac_layers(:ncol,1) + gas_ac(:ncol,k) * tropFlagInt(:ncol,k)
+                    end do
+                    if (history_gaschmbudget_2D_levels ) then
+                    call outfld(trim(solsym(n))//'_2DMSB_trop', ftem_layers(:ncol,1), pcols, lchnk )
+                    endif
                 endif
 
                 if( nstep == 0 ) then
