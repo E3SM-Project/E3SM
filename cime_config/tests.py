d2d725191a6dbd8612531f5828993435347078fe
donghuix <donghui.xu@pnnl.gov>
2024-05-02 14:44:47 -0700

    change the grid to r05_r05 for lnd_rof_two_way test

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -91,7 +91,7 @@ _TESTS = {
             "SMS_Ly2_P1x1.1x1_smallvilleIA.IELMCNCROP.elm-fan",
             "SMS.r05_r05.IELM.elm-topounit",
             "ERS.ELM_USRDAT.I1850ELM.elm-usrdat",
-            "ERS.f09_f09.IELM.elm-lnd_rof_2way",
+            "ERS.r05_r05.IELM.elm-lnd_rof_2way",
             "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
             "ERS.ELM_USRDAT.IELM.elm-surface_water_dynamics"
             )
