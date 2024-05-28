268539e47435a71318cd2f1c0400e78e8d2a8243
Oksana Guba <oksana.guba@gmail.com>
2024-05-28 15:15:37 -0600

    cmake files

---
+++ components/homme/cmake/machineFiles/hops.cmake
@@ -0,0 +1,68 @@
+## config that worked for this file:
+#
+#module purge
+#
+#module load cmake/3.25.2 aue/gcc/10.3.0 aue/hdf5/1.14.2-gcc-10.3.0-openmpi-4.1.6 aue/netcdf-c/4.9.2-gcc-10.3.0-openmpi-4.1.6
+#module load aue/binutils/2.41 aue/cuda/11.8.0-gcc-10.3.0 aue/parallel-netcdf/1.12.3-gcc-10.3.0-openmpi-4.1.6 aue/openmpi/4.1.6-gcc-10.3.0
+
+#EXECUTE_PROCESS(COMMAND nf-config --prefix
+#  RESULT_VARIABLE NFCONFIG_RESULT
+#  OUTPUT_VARIABLE NFCONFIG_OUTPUT
+#  ERROR_VARIABLE  NFCONFIG_ERROR
+#  OUTPUT_STRIP_TRAILING_WHITESPACE
+#)
+#SET (NetCDF_Fortran_PATH "${NFCONFIG_OUTPUT}" CACHE STRING "")
+#SET (NetCDF_C_PATH "${NCCONFIG_OUTPUT}" CACHE STRING "")
+
+#SET (HOMMEXX_BFB_TESTING TRUE CACHE BOOL "")
+
+# TPL settings
+#set(HDF5_DIR $ENV{HDF5_ROOT} CACHE FILEPATH "")
+#set(ZLIB_DIR $ENV{ZLIB_ROOT} CACHE FILEPATH "")
+#set(CURL_ROOT $ENV{CURL_ROOT} CACHE FILEPATH "")
+#set(CURL_LIBRARY -L$ENV{CURL_ROOT}/lib -lcurl CACHE LIST "")
+
+# Flag tweaks
+set(CMAKE_C_FLAGS "-w" CACHE STRING "")
+set(ADD_CXX_FLAGS "-Xcudafe --diag_suppress=esa_on_defaulted_function_ignored -Wno-unknown-pragmas -I/projects/aue/hpc/builds/x86_64/rhel8/ompiflux/openmpi-4.1.6-gcc-10.3.0/install/linux-rhel8-x86_64/gcc-10.3.0/openmpi-4.1.6-67j5mxl/include/" CACHE STRING "")
+set(ADD_Fortran_FLAGS "-I/projects/aue/hpc/builds/x86_64/rhel8/ompiflux/openmpi-4.1.6-gcc-10.3.0/install/linux-rhel8-x86_64/gcc-10.3.0/openmpi-4.1.6-67j5mxl/include/" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS "-ldl" CACHE STRING "")
+set(OPT_FLAGS "-O3" CACHE STRING "")
+#set(DEBUG_FLAGS "-ffp-contract=off -g"CACHE STRING "")
+
+# Homme settings
+set(BUILD_HOMME_WITHOUT_PIOLIBRARY TRUE CACHE BOOL "")
+
+set(HOMMEXX_VECTOR_SIZE 1 CACHE STRING "")
+set(HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
+set(USE_QUEUING FALSE CACHE BOOL "")
+set(USE_TRILINOS OFF CACHE BOOL "")
+#set(WITH_PNETCDF FALSE CACHE FILEPATH "")
+set(USE_NUM_PROCS 4 CACHE STRING "Num mpiprocs to use")
+set(HAVE_EXTRAE TRUE CACHE BOOL "")
+set(ENABLE_COLUMN_OPENMP OFF CACHE BOOL "")
+set(ENABLE_HORIZ_OPENMP OFF CACHE BOOL "")
+set(HOMMEXX_EXEC_SPACE "CUDA" CACHE STRING "")
+set(HOMMEXX_CUDA_MAX_WARP_PER_TEAM 8 CACHE STRING "")
+
+SET(BUILD_HOMME_PREQX_KOKKOS FALSE CACHE BOOL "")
+SET(BUILD_HOMME_THETA_KOKKOS TRUE CACHE BOOL "")
+#sET(HOMME_ENABLE_COMPOSE FALSE CACHE BOOL "")
+
+#set(HOMMEXX_BFB_TESTING TRUE CACHE STRING ON)
+
+# Kokkos settings
+set(ENABLE_OPENMP FALSE CACHE BOOL "")
+set(Kokkos_ENABLE_DEBUG FALSE CACHE BOOL "")
+set(Kokkos_ENABLE_AGGRESSIVE_VECTORIZATION FALSE CACHE BOOL "")
+set(Kokkos_ENABLE_CUDA TRUE CACHE BOOL "")
+set(Kokkos_ENABLE_CUDA_LAMBDA TRUE CACHE BOOL "")
+set(Kokkos_ARCH_HOPPER90 TRUE CACHE BOOL "")
+set(Kokkos_ENABLE_DEPRECATED_CODE FALSE CACHE BOOL "")
+set(Kokkos_ENABLE_EXPLICIT_INSTANTIATION FALSE CACHE BOOL "")
+
+# Compilers
+set(CMAKE_C_COMPILER mpicc CACHE STRING "")
+set(CMAKE_Fortran_COMPILER mpif90 CACHE STRING "")
+######## CHANGE THAT
+set(CMAKE_CXX_COMPILER "/home/onguba/acme-b2/externals/ekat/extern/kokkos/bin/nvcc_wrapper" CACHE STRING "")
