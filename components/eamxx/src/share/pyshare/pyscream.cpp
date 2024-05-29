57919ad53fccc1a0ff3f7740ff60d68eb8a4c3f3
Luca Bertagna <lbertag@sandia.gov>
2024-05-28 23:57:08 -0600

    EAMxx: add units to pyscream

--- components/eamxx/src/share/pyshare/pyscream.cpp
+++ components/eamxx/src/share/pyshare/pyscream.cpp
@@ -1,11 +1,13 @@
 #include "scream_session.hpp"
 #include "pyfield.hpp"
 #include "pygrid.hpp"
+#include "pyunits.hpp"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
 
+namespace py = pybind11;
 namespace scream {
 
 void initialize () {
@@ -15,8 +17,6 @@ void finalize () {
   finalize_scream_session();
 }
 
-namespace py = pybind11;
-
 PYBIND11_MODULE (pyscream,m) {
 
   m.doc() = "Python interfaces to certain EAMxx infrastructure code";
@@ -24,6 +24,27 @@ PYBIND11_MODULE (pyscream,m) {
   // Scream Session
   m.def("init",&initialize);
   m.def("finalize",&finalize);
+
+  // Units
+  py::module units = m.def_submodule("units");
+  py::class_<PyUnits>(units,"Units")
+    .def(py::init<>())
+    .def(py::init<const ekat::units::Units&>())
+    .def("str",&PyUnits::str)
+    .def("__mul__",[](const PyUnits& x, const PyUnits& y) { return PyUnits(x.units*y.units); })
+    .def("__truediv__",[](const PyUnits& x, const PyUnits& y) { return PyUnits(x.units/y.units); });
+
+  units.def("pow",[](const PyUnits& x, const int n) { return PyUnits(pow(x.units,ekat::RationalConstant(n))); });
+  units.def("root",[](const PyUnits& x, const int n) { return PyUnits(pow(x.units,ekat::RationalConstant(1,n))); });
+  units.attr("one") = &short_units::one;
+  units.attr("m") = &short_units::m;
+  units.attr("s") = &short_units::s;
+  units.attr("kg") = &short_units::kg;
+  units.attr("K") = &short_units::K;
+  units.attr("N") = &short_units::N;
+  units.attr("J") = &short_units::J;
+  units.attr("W") = &short_units::W;
+  units.attr("Pa") = &short_units::Pa;
 
   // Field class
   py::class_<PyField>(m,"Field")
