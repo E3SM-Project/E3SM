ae886f7ccfb0ade274ca52f771f656b80206136b
Luca Bertagna <lbertag@sandia.gov>
2024-05-30 22:10:46 -0600

    EAMxx: fix PyP3 and add unit test to compare against AD driven test

--- components/eamxx/src/physics/p3/python/pyp3.cpp
+++ components/eamxx/src/physics/p3/python/pyp3.cpp
@@ -2,6 +2,7 @@
 #include "pygrid.hpp"
 #include "pyfield.hpp"
 #include "pyatmproc.hpp"
+#include "pyparamlist.hpp"
 
 #include <pybind11/pybind11.h>
 
@@ -10,14 +11,24 @@ namespace scream {
 namespace py = pybind11;
 
 struct PyP3 : public PyAtmProc {
+  PyP3 (const PyGrid& pygrid, const PyParamList& params)
+   : PyAtmProc(pygrid)
+  {
+    init (params.pl);
+  }
   PyP3 (const PyGrid& pygrid)
    : PyAtmProc(pygrid)
   {
-    ekat::Comm comm(MPI_COMM_WORLD);
+    // P3 does not have a default (in the C++ code) for this parameter,
+    // so an entry MUST be set
     ekat::ParameterList pl;
-    pl.set("max_total_ni",10.0);
+    pl.set("max_total_ni",740.0e3);
+    init(pl);
+  }
 
-    ap = create_atmosphere_process<P3Microphysics>(comm,pl);
+  void init(const ekat::ParameterList& params) {
+    auto comm = pygrid.grid->get_comm();
+    ap = create_atmosphere_process<P3Microphysics>(comm,params);
 
     // Create a grids manager on the fly
     auto gm = std::make_shared<SingleGridGM>(pygrid.grid);
@@ -32,7 +43,8 @@ PYBIND11_MODULE (pyp3,m) {
   m.doc() = "Python interfaces to P3Microphysics";
 
   py::class_<PyP3,PyAtmProc>(m,"P3")
-    .def(py::init<const PyGrid&>());
+    .def(py::init<const PyGrid&>())
+    .def(py::init<const PyGrid&,const PyParamList&>());
 
 }
 
