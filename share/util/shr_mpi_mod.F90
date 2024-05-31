965e25d9379e0fbb93e188abeb94bcf1663912f7
Skyler Ruiter <saruite@sandia.gov>
2024-05-31 08:13:09 -0600

    gets weaver cache file working with kokkos and parallel io and cleans up some unneeded args

--- share/util/shr_mpi_mod.F90
+++ share/util/shr_mpi_mod.F90
@@ -92,7 +92,8 @@ Module shr_mpi_mod
        shr_mpi_maxr1
   end interface shr_mpi_max
 
-#include <mpif.h>         ! mpi library include file
+! mpi library include file
+#include <mpif.h>
 
   !===============================================================================
 CONTAINS
