bb08b46f3df624efaf74ff9870e673387c2e72bd
mahf708 <naser.mahfouz@pnnl.gov>
2024-06-01 00:34:32 -0500

    add conda recipe that builds correctly

--- components/eamxx/src/python/setup.py
+++ components/eamxx/src/python/setup.py
@@ -7,7 +7,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name='pyeamxx',
-    version='0.0.1.dev4',
+    version='0.0.1.dev5',
     author='E3SM SCREAM',
     description='EAMxx wrapper',
     packages=['', 'pyeamxx'],
@@ -17,13 +17,3 @@ setup(
     distclass=BinaryDistribution,
     zip_safe=False
 )
-
-# TODOs:
-# 1. We need to figure out how to handle the complex env needed by scream
-# 2. We need to think forward about packaging this neatly
-
-# for both issues, I think the most straightforward solution for now is the following:
-# We simply build manually-ish for support machines and just upload the wheels
-# to pypi. That way, we can ensure the build works on the machine,
-# we can set its environment as we wish, etc..
-# The downside is that it is manual...
--- components/eamxx/src/python/tools/build.sh
+++ components/eamxx/src/python/tools/build.sh
@@ -1,25 +1,30 @@
 #!/bin/env bash
 
+cd components/eamxx/src/python
+
 cmake \
-    -S ../../../ \
-    -B ../build_src \
+    -S ../../ \
+    -B build_src \
     -DCMAKE_BUILD_TYPE='Release' \
     -DEAMXX_ENABLE_PYBIND='ON' \
-    -DNetCDF_Fortran_PATH=$CONDA_PREFIX \
-    -DNetCDF_C_PATH=$CONDA_PREFIX \
-    -DCMAKE_CXX_FLAGS='-fvisibility-inlines-hidden -fmessage-length=0' \
+    -DNETCDF_FORTRAN_PATH=$PREFIX \
+    -DNETCDF_C_PATH=$PREFIX \
+    -DCMAKE_CXX_FLAGS='-fvisibility-inlines-hidden -fmessage-length=0 -Wno-use-after-free -Wno-unused-variable -Wno-maybe-uninitialized' \
     -DCMAKE_C_FLAGS='' \
-    -DCMAKE_Fortran_FLAGS='' \
+    -DCMAKE_Fortran_FLAGS='-Wno-maybe-uninitialized -Wno-unused-dummy-argument' \
     -DCMAKE_CXX_COMPILER=mpic++ \
     -DCMAKE_C_COMPILER=mpicc \
     -DCMAKE_Fortran_COMPILER=mpif90 \
     -DBUILD_SHARED_LIBS=ON \
-    -DSCREAM_INPUT_ROOT="../build_src" \
+    -DSCREAM_INPUT_ROOT="build_src" \
     -DSCREAM_ENABLE_BASELINE_TESTS=OFF \
-    -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DPYTHON_EXECUTABLE=$PYTHON
 
-cmake --build ../build_src/src/python -j${CPU_COUNT:-128}
+cmake --build build_src/src/python -j${CPU_COUNT:-128}
 
-find ../build_src -type f -name "*.so" | xargs cp -t ../libpyeamxx
+find build_src -type f -name "*.so" | xargs cp -t libpyeamxx/
 
-python -m pip install -vvv ../
+$PYTHON -m build -w -n -x
+
+pip install dist/*.whl -vv
