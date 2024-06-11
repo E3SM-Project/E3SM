8cf5d386989ebfcba4f124f6fd28e04b26bd911e
Luca Bertagna <lbertag@sandia.gov>
2024-06-11 13:20:34 -0600

    EAMxx: add support for passing MPI comm from py to C in pyeamxx

--- components/eamxx/src/python/pyeamxx.cpp
+++ components/eamxx/src/python/pyeamxx.cpp
@@ -1,18 +1,31 @@
-#include "scream_session.hpp"
+#include <share/scream_session.hpp>
 #include "pyfield.hpp"
 #include "pygrid.hpp"
 #include "pyatmproc.hpp"
 #include "pyparamlist.hpp"
+#include "pyutils.hpp"
+
+#include <ekat/mpi/ekat_comm.hpp>
 
 #include <pybind11/pybind11.h>
 
+#include <mpi.h>
+
 namespace py = pybind11;
+
 namespace scream {
 
-void initialize () {
-  ekat::Comm comm(MPI_COMM_WORLD);
+void initialize (MPI_Comm mpi_comm) {
+  ekat::Comm comm(mpi_comm);
   initialize_scream_session(comm.am_i_root());
   scorpio::init_subsystem(comm);
+}
+
+void initialize () {
+  initialize(MPI_COMM_WORLD);
+}
+void initialize (pybind11::object py_comm) {
+  initialize(get_c_comm(py_comm));
 }
 void finalize () {
   scorpio::finalize_subsystem();
@@ -24,7 +37,8 @@ PYBIND11_MODULE (pyeamxx,m) {
   m.doc() = "Python interfaces to certain EAMxx infrastructure code";
 
   // Scream Session
-  m.def("init",&initialize);
+  m.def("init",py::overload_cast<>(&initialize));
+  m.def("init",py::overload_cast<pybind11::object>(&initialize));
   m.def("finalize",&finalize);
 
   // Call all other headers' registration routines
--- components/eamxx/src/python/pygrid.hpp
+++ components/eamxx/src/python/pygrid.hpp
@@ -4,8 +4,11 @@
 #include "share/grid/grids_manager.hpp"
 #include "share/grid/point_grid.hpp"
 #include "pyfield.hpp"
+#include "pyutils.hpp"
 
 #include <pybind11/pybind11.h>
+
+#include <mpi.h>
 
 namespace scream {
 
@@ -38,15 +41,24 @@ struct PyGrid {
   PyGrid () = default;
 
   PyGrid(const std::string& name, int ncols, int nlevs)
+    : PyGrid (name,ncols,nlevs,MPI_COMM_WORLD)
+  {}
+
+  PyGrid(const std::string& name, int ncols, int nlevs, pybind11::object py_comm)
+    : PyGrid (name,ncols,nlevs,get_c_comm(py_comm))
+  {}
+
+  PyGrid(const std::string& name, int ncols, int nlevs, MPI_Comm mpi_comm)
   {
-    ekat::Comm comm(MPI_COMM_WORLD);
+    ekat::Comm comm(mpi_comm);
     grid = create_point_grid(name,ncols,nlevs,comm);
   }
 };
 
 inline void pybind_pygrid (pybind11::module& m) {
   pybind11::class_<PyGrid>(m,"Grid")
-    .def(pybind11::init<const std::string&,int,int>());
+    .def(pybind11::init<const std::string&,int,int>())
+    .def(pybind11::init<const std::string&,int,int,pybind11::object>());
 }
 
 } // namespace scream
