6de6c33682b86c3d4caae1a2ac6a18879e4b53fc
Balwinder Singh <balwindersingh@gmail.com>
2024-07-05 18:02:08 -0700

    Adds the new script for computing number of tracers

---
+++ components/eamxx/cime_config/testdefs/testmods_dirs/scream/mam4xx/update_eamxx_num_tracers.sh
@@ -0,0 +1,22 @@
+#------------------------------------------------------
+# MAM4xx adds additionaltracers to the simulation
+# Increase number of tracers for MAM4xx simulations
+#------------------------------------------------------
+
+# Additional MAM4xx tracers (MAM4xx adds 31 tracers)
+ADDITIONAL_MAM4xx_TRACERS=31
+
+# Original CMAKE options in env_build.xml
+orig_cmake_opt=`./xmlquery --value SCREAM_CMAKE_OPTIONS`
+
+# Extract the number of tracers
+orig_tracer_num=$(echo $orig_cmake_opt | grep -oP 'SCREAM_NUM_TRACERS \K[0-9]+')
+
+# Update number of tracers
+new_tracer_num=$((orig_tracer_num + ADDITIONAL_MAM4xx_TRACERS))
+
+# Form the new CMake options string by replacing the original number with the new number
+new_cmake_opt=$(echo $orig_cmake_opt | sed "s/SCREAM_NUM_TRACERS $orig_tracer_num/SCREAM_NUM_TRACERS $new_tracer_num/")
+
+# Update cmake options string
+./xmlchange  SCREAM_CMAKE_OPTIONS="$new_cmake_opt"
