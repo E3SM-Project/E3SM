f5c86c19e50e944ea5b944754b936fd006b09d20
Skyler Ruiter <saruite@sandia.gov>
2024-07-10 11:54:52 -0600

    small change to ghost

--- components/homme/cmake/machineFiles/ghost.cmake
+++ components/homme/cmake/machineFiles/ghost.cmake
@@ -5,9 +5,11 @@ SET (CMAKE_C_COMPILER mpicc CACHE FILEPATH "")
 SET (CMAKE_CXX_COMPILER mpicc CACHE FILEPATH "")
 
 # Flag Settings
-set(CMAKE_EXE_LINKER_FLAGS "-ldl" CACHE STRING "")
-set(OPT_FLAGS "-O3" CACHE STRING "")
-set(ADD_Fortran_FLAGS " -O3" CACHE STRING "")
+SET (CMAKE_C_FLAGS "-w -O3 -DNDEBUG" CACHE STRING "") # disable warnings
+SET (ADD_CXX_FLAGS "-Xcudafe --diag_suppress=esa_on_defaulted_function_ignored -Wno-unknown-pragmas -I/projects/ppc64le-pwr9-rhel8/tpls/openmpi/4.1.4/gcc/11.3.0/base/vu2aei6/include -O3 -DNDEBUG" CACHE STRING "")
+SET (ADD_Fortran_FLAGS " -I/projects/ppc64le-pwr9-rhel8/tpls/openmpi/4.1.4/gcc/11.3.0/base/vu2aei6/include -O3 -DNDEBUG" CACHE STRING "")
+SET (CMAKE_EXE_LINKER_FLAGS "-ldl -lopenblas" CACHE STRING "")
+
 
 SET (WITH_PNETCDF TRUE CACHE FILEPATH "")
 SET (PnetCDF_PATH /projects/ccsm/src/parallel-netcdf-1.5.0-src/ CACHE FILEPATH "")
@@ -26,7 +28,8 @@ SET (ZLIB_DIR $ENV{SEMS_ZLIB_ROOT} CACHE FILEPATH "")
 #SET (CPRNC_DIR /projects/ccsm/cprnc/build.toss3 CACHE FILEPATH "")
 
 # Turn IO on/off
-# SET (BUILD_HOMME_WITHOUT_PIOLIBRARY TRUE CACHE BOOL "")
+SET (BUILD_HOMME_WITHOUT_PIOLIBRARY TRUE CACHE BOOL "")
+SET (HOMMEXX_BFB_TESTING FALSE CACHE BOOL "")
 
 SET (USE_QUEUING FALSE CACHE BOOL "")
 SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
