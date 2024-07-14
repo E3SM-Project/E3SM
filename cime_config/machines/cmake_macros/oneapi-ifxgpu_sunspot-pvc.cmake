8b9716523ecec4ba919160feaf775d32a23134e9
Oksana Guba <oksana.guba@gmail.com>
2024-07-14 21:50:59 +0000

    add mpi options

--- cime_config/machines/cmake_macros/oneapi-ifxgpu_sunspot-pvc.cmake
+++ cime_config/machines/cmake_macros/oneapi-ifxgpu_sunspot-pvc.cmake
@@ -14,7 +14,7 @@ endif()
 string(APPEND KOKKOS_OPTIONS " -DCMAKE_CXX_STANDARD=17 -DKokkos_ENABLE_SERIAL=On -DKokkos_ARCH_INTEL_PVC=On -DKokkos_ENABLE_SYCL=On -DKokkos_ENABLE_EXPLICIT_INSTANTIATION=Off -DCMAKE_CXX_FLAGS='-fsycl-device-code-split=per_kernel'")
 string(APPEND SYCL_FLAGS " -\-intel -fsycl -fsycl-targets=spir64_gen -mlong-double-64 -Xsycl-target-backend \"-device 12.60.7\"")
 
-set(SCREAM_MPI_ON_DEVICE OFF CACHE STRING "")
+set(SCREAM_MPI_ON_DEVICE ON CACHE STRING "")
 
 
 
