839f50eb37c67701510e43277cc2f7a48debe0c6
Hyun Kang <kangh@ornl.gov>
2024-08-14 10:56:17 -0400

    Add comments and LaTex Eq. in the RK2 code

--- components/omega/src/timeStepping/RungeKutta2Stepper.cpp
+++ components/omega/src/timeStepping/RungeKutta2Stepper.cpp
@@ -15,12 +15,20 @@ void RungeKutta2Stepper::doStep(OceanState *State, Real Time,
    const int CurLevel  = 0;
    const int NextLevel = 1;
 
+
+   // $k_1=\Delta f(\phi_n, t_n)$
    Tend->computeAllTendencies(State, AuxState, CurLevel, Time);
+  
+   // $\phi_{n+{1\over2}}=\phi_n+{k_1 \over2}$
    updateStateByTend(State, NextLevel, State, CurLevel,  0.5*TimeStep);
 
+   // $k_2=\Delta f(\phi_n+{1\over2}, t_n+{\Delta t\over2})$
    Tend->computeAllTendencies(State, AuxState, NextLevel, Time + 0.5*TimeStep);
+
+   // $\phi_{n+1}=\phi_n+k_2$
    updateStateByTend(State, NextLevel, State, CurLevel, TimeStep);
 
+   // Update time levels (New -> Old) of prognostic variables with halo exchanges
    State->updateTimeLevels();
 }
 
