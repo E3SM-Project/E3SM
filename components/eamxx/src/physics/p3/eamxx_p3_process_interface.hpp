E3SM-Project/scream

view alloc fixes for P3 small kernels

components/eamxx/src/physics/p3/eamxx_p3_process_interface.hpp
@@ -158,7 +158,7 @@ public:
     // Assigning local variables
     void set_variables(const int ncol, const int npack,
            const view_2d_const& pmid_, const view_2d_const& pmid_dry_,
-           const view_2d_const& pseudo_density_, 
+           const view_2d_const& pseudo_density_,
            const view_2d_const& pseudo_density_dry_, const view_2d& T_atm_,
            const view_2d_const& cld_frac_t_, const view_2d& qv_, const view_2d& qc_,
            const view_2d& nc_, const view_2d& qr_, const view_2d& nr_, const view_2d& qi_,
@@ -361,7 +361,11 @@ public:
     // 1d view scalar, size (ncol)
     static constexpr int num_1d_scalar = 2; //no 2d vars now, but keeping 1d struct for future expansion
     // 2d view packed, size (ncol, nlev_packs)
+#ifdef SCREAM_SMALL_KERNELS
+    static constexpr int num_2d_vector = 64;
+#else
     static constexpr int num_2d_vector = 8;
+#endif
     static constexpr int num_2dp1_vector = 2;
 
     uview_1d precip_liq_surf_flux;
@@ -376,6 +380,21 @@ public:
     uview_2d precip_liq_flux; //nlev+1
     uview_2d precip_ice_flux; //nlev+1
     uview_2d unused;
+
+#ifdef SCREAM_SMALL_KERNELS
+    uview_2d
+      mu_r, T_atm, lamr, logn0r, nu, cdist, cdist1, cdistr,
+      inv_cld_frac_i, inv_cld_frac_l, inv_cld_frac_r,
+      qc_incld, qr_incld, qi_incld, qm_incld,
+      nc_incld, nr_incld, ni_incld, bm_incld,
+      inv_dz, inv_rho, ze_ice, ze_rain, prec, rho, rhofacr,
+      rhofaci, acn, qv_sat_l, qv_sat_i, sup, qv_supersat_i,
+      tmparr2, exner, diag_equiv_reflectivity, diag_vm_qi,
+      diag_diam_qi, pratot, prctot, qtend_ignore, ntend_ignore,
+      mu_c, lamc, qr_evap_tend, v_qc, v_nc, flux_qx, flux_nx,
+      v_qit, v_nit, flux_nit, flux_bir, flux_qir, flux_qit,
+      v_qr, v_nr;
+#endif
 
     suview_2d col_location;
 
@@ -410,6 +429,9 @@ protected:
   P3F::P3DiagnosticOutputs diag_outputs;
   P3F::P3HistoryOnly       history_only;
   P3F::P3LookupTables      lookup_tables;
+#ifdef SCREAM_SMALL_KERNELS
+  P3F::P3Temporaries       temporaries;
+#endif
   P3F::P3Infrastructure    infrastructure;
   P3F::P3Runtime           runtime_options;
   p3_preamble              p3_preproc;

components/eamxx/src/physics/p3/eamxx_p3_run.cpp
@@ -29,7 +29,11 @@ void P3Microphysics::run_impl (const double dt)
   get_field_out("micro_vap_ice_exchange").deep_copy(0.0);
 
   P3F::p3_main(runtime_options, prog_state, diag_inputs, diag_outputs, infrastructure,
-               history_only, lookup_tables, workspace_mgr, m_num_cols, m_num_levs, m_p3constants);
+               history_only, lookup_tables,
+#ifdef SCREAM_SMALL_KERNELS
+               temporaries,
+#endif
+               workspace_mgr, m_num_cols, m_num_levs, m_p3constants);
 
   // Conduct the post-processing of the p3_main output.
   Kokkos::parallel_for(

components/eamxx/src/physics/p3/p3_functions.hpp
@@ -246,6 +246,45 @@ struct Functions
     view_dnu_table dnu_table_vals;
   };
 
+#ifdef SCREAM_SMALL_KERNELS
+  struct P3Temporaries {
+    P3Temporaries() = default;
+    // shape parameter of rain
+    view_2d<Spack> mu_r;
+    // temperature at the beginning of the microphysics step [K]
+    view_2d<Spack> T_atm;
+    // 2D size distribution and fallspeed parameters
+    view_2d<Spack> lamr, logn0r, nu;
+    view_2d<Spack> cdist, cdist1, cdistr;
+    // Variables needed for in-cloud calculations
+    // Inverse cloud fractions (1/cld)
+    view_2d<Spack> inv_cld_frac_i, inv_cld_frac_l, inv_cld_frac_r;
+    // In cloud mass-mixing ratios
+    view_2d<Spack> qc_incld, qr_incld, qi_incld, qm_incld;
+    // In cloud number concentrations
+    view_2d<Spack> nc_incld, nr_incld, ni_incld, bm_incld;
+    // Other
+    view_2d<Spack> inv_dz, inv_rho, ze_ice, ze_rain;
+    view_2d<Spack> prec, rho, rhofacr, rhofaci;
+    view_2d<Spack> acn, qv_sat_l, qv_sat_i, sup;
+    view_2d<Spack> qv_supersat_i, tmparr2, exner;
+    view_2d<Spack> diag_equiv_reflectivity, diag_vm_qi, diag_diam_qi;
+    view_2d<Spack> pratot, prctot;
+    // p3_tend_out, may not need these
+    view_2d<Spack> qtend_ignore, ntend_ignore;
+    // Variables still used in F90 but removed from C++ interface
+    view_2d<Spack> mu_c, lamc;
+    view_2d<Spack> qr_evap_tend;
+    // cloud sedimentation
+    view_2d<Spack> v_qc, v_nc, flux_qx, flux_nx;
+    // ice sedimentation
+    view_2d<Spack> v_qit, v_nit, flux_nit, flux_bir;
+    view_2d<Spack> flux_qir, flux_qit;
+    // rain sedimentation
+    view_2d<Spack> v_qr, v_nr;
+  };
+#endif
+
   // -- Table3 --
 
   struct Table3 {
@@ -1301,6 +1340,9 @@ struct Functions
     const P3Infrastructure& infrastructure,
     const P3HistoryOnly& history_only,
     const P3LookupTables& lookup_tables,
+#ifdef SCREAM_SMALL_KERNELS
+    const P3Temporaries& temporaries,
+#endif
     const WorkspaceManager& workspace_mgr,
     Int nj, // number of columns
     Int nk, // number of vertical cells per column
@@ -1328,6 +1370,7 @@ struct Functions
     const P3Infrastructure& infrastructure,
     const P3HistoryOnly& history_only,
     const P3LookupTables& lookup_tables,
+    const P3Temporaries& temporaries,
     const WorkspaceManager& workspace_mgr,
     Int nj, // number of columns
     Int nk, // number of vertical cells per column

components/eamxx/src/physics/p3/p3_functions_f90.cpp
@@ -2054,6 +2054,36 @@ Int p3_main_f(
   P3F::P3HistoryOnly history_only{liq_ice_exchange_d, vap_liq_exchange_d,
                                   vap_ice_exchange_d};
 
+  const Int nk_pack = ekat::npack<Spack>(nk);
+#ifdef SCREAM_SMALL_KERNELS
+  view_2d
+    mu_r("mu_r", nj, nk_pack), T_atm("T_atm", nj, nk_pack), lamr("lamr", nj, nk_pack), logn0r("logn0r", nj, nk_pack), nu("nu", nj, nk_pack),
+    cdist("cdist", nj, nk_pack), cdist1("cdist1", nj, nk_pack), cdistr("cdistr", nj, nk_pack), inv_cld_frac_i("inv_cld_frac_i", nj, nk_pack),
+    inv_cld_frac_l("inv_cld_frac_l", nj, nk_pack), inv_cld_frac_r("inv_cld_frac_r", nj, nk_pack), qc_incld("qc_incld", nj, nk_pack),
+    qr_incld("qr_incld", nj, nk_pack), qi_incld("qi_incld", nj, nk_pack), qm_incld("qm_incld", nj, nk_pack), nc_incld("nc_incld", nj, nk_pack),
+    nr_incld("nr_incld", nj, nk_pack), ni_incld("ni_incld", nj, nk_pack), bm_incld("bm_incld", nj, nk_pack), inv_dz("inv_dz", nj, nk_pack),
+    inv_rho("inv_rho", nj, nk_pack), ze_ice("ze_ice", nj, nk_pack), ze_rain("ze_rain", nj, nk_pack), prec("prec", nj, nk_pack),
+    rho("rho", nj, nk_pack), rhofacr("rhofacr", nj, nk_pack), rhofaci("rhofaci", nj, nk_pack),  acn("acn", nj, nk_pack), qv_sat_l("qv_sat", nj, nk_pack),
+    qv_sat_i("qv_sat_i", nj, nk_pack), sup("sup", nj, nk_pack), qv_supersat_i("qv_supersat", nj, nk_pack), tmparr2("tmparr2", nj, nk_pack),
+    exner("exner", nj, nk_pack), diag_equiv_reflectivity("diag_equiv_ref", nj, nk_pack), diag_vm_qi("diag_vm_qi", nj, nk_pack),
+    diag_diam_qi("diag_diam_qi", nj, nk_pack), pratot("pratot", nj, nk_pack), prctot("prctot", nj, nk_pack), qtend_ignore("qtend_ignore", nj, nk_pack),
+    ntend_ignore("ntend_ignore", nj, nk_pack), mu_c("mu_c", nj, nk_pack), lamc("lamc", nj, nk_pack), qr_evap_tend("qr_evap_tend", nj, nk_pack),
+    v_qc("v_qc", nj, nk_pack), v_nc("v_nc", nj, nk_pack), flux_qx("flux_qx", nj, nk_pack), flux_nx("flux_nx", nj, nk_pack), v_qit("v_qit", nj, nk_pack),
+    v_nit("v_nit", nj, nk_pack), flux_nit("flux_nit", nj, nk_pack), flux_bir("flux_bir", nj, nk_pack), flux_qir("flux_qir", nj, nk_pack),
+    flux_qit("flux_qit", nj, nk_pack), v_qr("v_qr", nj, nk_pack), v_nr("v_nr", nj, nk_pack);
+
+  P3F::P3Temporaries temporaries{
+    mu_r, T_atm, lamr, logn0r, nu, cdist, cdist1, cdistr, inv_cld_frac_i,
+    inv_cld_frac_l, inv_cld_frac_r, qc_incld, qr_incld, qi_incld, qm_incld,
+    nc_incld, nr_incld, ni_incld, bm_incld, inv_dz, inv_rho, ze_ice, ze_rain,
+    prec, rho, rhofacr, rhofaci, acn, qv_sat_l, qv_sat_i, sup, qv_supersat_i,
+    tmparr2, exner, diag_equiv_reflectivity, diag_vm_qi, diag_diam_qi,
+    pratot, prctot, qtend_ignore, ntend_ignore, mu_c, lamc, qr_evap_tend,
+    v_qc, v_nc, flux_qx, flux_nx, v_qit, v_nit, flux_nit, flux_bir, flux_qir,
+    flux_qit, v_qr, v_nr
+  };
+#endif
+
   // load tables
   view_1d_table mu_r_table_vals;
   view_2d_table vn_table_vals, vm_table_vals, revap_table_vals;
@@ -2068,12 +2098,15 @@ Int p3_main_f(
   P3F::P3Runtime runtime_options{740.0e3};
 
   // Create local workspace
-  const Int nk_pack = ekat::npack<Spack>(nk);
   const auto policy = ekat::ExeSpaceUtils<KT::ExeSpace>::get_default_team_policy(nj, nk_pack);
   ekat::WorkspaceManager<Spack, KT::Device> workspace_mgr(nk_pack, 52, policy);
 
   auto elapsed_microsec = P3F::p3_main(runtime_options, prog_state, diag_inputs, diag_outputs, infrastructure,
-                                       history_only, lookup_tables, workspace_mgr, nj, nk, physics::P3_Constants<Real>());
+                                       history_only, lookup_tables,
+#ifdef SCREAM_SMALL_KERNELS
+                                       temporaries,
+#endif
+                                       workspace_mgr, nj, nk, physics::P3_Constants<Real>());
 
   Kokkos::parallel_for(nj, KOKKOS_LAMBDA(const Int& i) {
     precip_liq_surf_temp_d(0, i / Spack::n)[i % Spack::n] = precip_liq_surf_d(i);

E3SM-Project/E3SM

components/eamxx/src/physics/p3/eamxx_p3_process_interface.hpp
@@ -158,7 +158,7 @@ public:
     // Assigning local variables
     void set_variables(const int ncol, const int npack,
            const view_2d_const& pmid_, const view_2d_const& pmid_dry_,
-           const view_2d_const& pseudo_density_, 
+           const view_2d_const& pseudo_density_,
            const view_2d_const& pseudo_density_dry_, const view_2d& T_atm_,
            const view_2d_const& cld_frac_t_, const view_2d& qv_, const view_2d& qc_,
            const view_2d& nc_, const view_2d& qr_, const view_2d& nr_, const view_2d& qi_,
@@ -361,7 +361,11 @@ public:
     // 1d view scalar, size (ncol)
     static constexpr int num_1d_scalar = 2; //no 2d vars now, but keeping 1d struct for future expansion
     // 2d view packed, size (ncol, nlev_packs)
+#ifdef SCREAM_P3_SMALL_KERNELS
+    static constexpr int num_2d_vector = 64;
+#else
     static constexpr int num_2d_vector = 8;
+#endif
     static constexpr int num_2dp1_vector = 2;
 
     uview_1d precip_liq_surf_flux;
@@ -376,6 +380,21 @@ public:
     uview_2d precip_liq_flux; //nlev+1
     uview_2d precip_ice_flux; //nlev+1
     uview_2d unused;
+
+#ifdef SCREAM_P3_SMALL_KERNELS
+    uview_2d
+      mu_r, T_atm, lamr, logn0r, nu, cdist, cdist1, cdistr,
+      inv_cld_frac_i, inv_cld_frac_l, inv_cld_frac_r,
+      qc_incld, qr_incld, qi_incld, qm_incld,
+      nc_incld, nr_incld, ni_incld, bm_incld,
+      inv_dz, inv_rho, ze_ice, ze_rain, prec, rho, rhofacr,
+      rhofaci, acn, qv_sat_l, qv_sat_i, sup, qv_supersat_i,
+      tmparr2, exner, diag_equiv_reflectivity, diag_vm_qi,
+      diag_diam_qi, pratot, prctot, qtend_ignore, ntend_ignore,
+      mu_c, lamc, qr_evap_tend, v_qc, v_nc, flux_qx, flux_nx,
+      v_qit, v_nit, flux_nit, flux_bir, flux_qir, flux_qit,
+      v_qr, v_nr;
+#endif
 
     suview_2d col_location;
 
@@ -410,6 +429,9 @@ protected:
   P3F::P3DiagnosticOutputs diag_outputs;
   P3F::P3HistoryOnly       history_only;
   P3F::P3LookupTables      lookup_tables;
+#ifdef SCREAM_P3_SMALL_KERNELS
+  P3F::P3Temporaries       temporaries;
+#endif
   P3F::P3Infrastructure    infrastructure;
   P3F::P3Runtime           runtime_options;
   p3_preamble              p3_preproc;

components/eamxx/src/physics/p3/eamxx_p3_run.cpp
@@ -29,7 +29,11 @@ void P3Microphysics::run_impl (const double dt)
   get_field_out("micro_vap_ice_exchange").deep_copy(0.0);
 
   P3F::p3_main(runtime_options, prog_state, diag_inputs, diag_outputs, infrastructure,
-               history_only, lookup_tables, workspace_mgr, m_num_cols, m_num_levs, m_p3constants);
+               history_only, lookup_tables,
+#ifdef SCREAM_P3_SMALL_KERNELS
+               temporaries,
+#endif
+               workspace_mgr, m_num_cols, m_num_levs, m_p3constants);
 
   // Conduct the post-processing of the p3_main output.
   Kokkos::parallel_for(

components/eamxx/src/physics/p3/p3_functions.hpp
@@ -246,6 +246,45 @@ struct Functions
     view_dnu_table dnu_table_vals;
   };
 
+#ifdef SCREAM_P3_SMALL_KERNELS
+  struct P3Temporaries {
+    P3Temporaries() = default;
+    // shape parameter of rain
+    view_2d<Spack> mu_r;
+    // temperature at the beginning of the microphysics step [K]
+    view_2d<Spack> T_atm;
+    // 2D size distribution and fallspeed parameters
+    view_2d<Spack> lamr, logn0r, nu;
+    view_2d<Spack> cdist, cdist1, cdistr;
+    // Variables needed for in-cloud calculations
+    // Inverse cloud fractions (1/cld)
+    view_2d<Spack> inv_cld_frac_i, inv_cld_frac_l, inv_cld_frac_r;
+    // In cloud mass-mixing ratios
+    view_2d<Spack> qc_incld, qr_incld, qi_incld, qm_incld;
+    // In cloud number concentrations
+    view_2d<Spack> nc_incld, nr_incld, ni_incld, bm_incld;
+    // Other
+    view_2d<Spack> inv_dz, inv_rho, ze_ice, ze_rain;
+    view_2d<Spack> prec, rho, rhofacr, rhofaci;
+    view_2d<Spack> acn, qv_sat_l, qv_sat_i, sup;
+    view_2d<Spack> qv_supersat_i, tmparr2, exner;
+    view_2d<Spack> diag_equiv_reflectivity, diag_vm_qi, diag_diam_qi;
+    view_2d<Spack> pratot, prctot;
+    // p3_tend_out, may not need these
+    view_2d<Spack> qtend_ignore, ntend_ignore;
+    // Variables still used in F90 but removed from C++ interface
+    view_2d<Spack> mu_c, lamc;
+    view_2d<Spack> qr_evap_tend;
+    // cloud sedimentation
+    view_2d<Spack> v_qc, v_nc, flux_qx, flux_nx;
+    // ice sedimentation
+    view_2d<Spack> v_qit, v_nit, flux_nit, flux_bir;
+    view_2d<Spack> flux_qir, flux_qit;
+    // rain sedimentation
+    view_2d<Spack> v_qr, v_nr;
+  };
+#endif
+
   // -- Table3 --
 
   struct Table3 {
@@ -1301,6 +1340,9 @@ struct Functions
     const P3Infrastructure& infrastructure,
     const P3HistoryOnly& history_only,
     const P3LookupTables& lookup_tables,
+#ifdef SCREAM_P3_SMALL_KERNELS
+    const P3Temporaries& temporaries,
+#endif
     const WorkspaceManager& workspace_mgr,
     Int nj, // number of columns
     Int nk, // number of vertical cells per column
@@ -1328,6 +1370,7 @@ struct Functions
     const P3Infrastructure& infrastructure,
     const P3HistoryOnly& history_only,
     const P3LookupTables& lookup_tables,
+    const P3Temporaries& temporaries,
     const WorkspaceManager& workspace_mgr,
     Int nj, // number of columns
     Int nk, // number of vertical cells per column

components/eamxx/src/physics/p3/p3_functions_f90.cpp
@@ -2054,6 +2054,36 @@ Int p3_main_f(
   P3F::P3HistoryOnly history_only{liq_ice_exchange_d, vap_liq_exchange_d,
                                   vap_ice_exchange_d};
 
+  const Int nk_pack = ekat::npack<Spack>(nk);
+#ifdef SCREAM_P3_SMALL_KERNELS
+  view_2d
+    mu_r("mu_r", nj, nk_pack), T_atm("T_atm", nj, nk_pack), lamr("lamr", nj, nk_pack), logn0r("logn0r", nj, nk_pack), nu("nu", nj, nk_pack),
+    cdist("cdist", nj, nk_pack), cdist1("cdist1", nj, nk_pack), cdistr("cdistr", nj, nk_pack), inv_cld_frac_i("inv_cld_frac_i", nj, nk_pack),
+    inv_cld_frac_l("inv_cld_frac_l", nj, nk_pack), inv_cld_frac_r("inv_cld_frac_r", nj, nk_pack), qc_incld("qc_incld", nj, nk_pack),
+    qr_incld("qr_incld", nj, nk_pack), qi_incld("qi_incld", nj, nk_pack), qm_incld("qm_incld", nj, nk_pack), nc_incld("nc_incld", nj, nk_pack),
+    nr_incld("nr_incld", nj, nk_pack), ni_incld("ni_incld", nj, nk_pack), bm_incld("bm_incld", nj, nk_pack), inv_dz("inv_dz", nj, nk_pack),
+    inv_rho("inv_rho", nj, nk_pack), ze_ice("ze_ice", nj, nk_pack), ze_rain("ze_rain", nj, nk_pack), prec("prec", nj, nk_pack),
+    rho("rho", nj, nk_pack), rhofacr("rhofacr", nj, nk_pack), rhofaci("rhofaci", nj, nk_pack),  acn("acn", nj, nk_pack), qv_sat_l("qv_sat", nj, nk_pack),
+    qv_sat_i("qv_sat_i", nj, nk_pack), sup("sup", nj, nk_pack), qv_supersat_i("qv_supersat", nj, nk_pack), tmparr2("tmparr2", nj, nk_pack),
+    exner("exner", nj, nk_pack), diag_equiv_reflectivity("diag_equiv_ref", nj, nk_pack), diag_vm_qi("diag_vm_qi", nj, nk_pack),
+    diag_diam_qi("diag_diam_qi", nj, nk_pack), pratot("pratot", nj, nk_pack), prctot("prctot", nj, nk_pack), qtend_ignore("qtend_ignore", nj, nk_pack),
+    ntend_ignore("ntend_ignore", nj, nk_pack), mu_c("mu_c", nj, nk_pack), lamc("lamc", nj, nk_pack), qr_evap_tend("qr_evap_tend", nj, nk_pack),
+    v_qc("v_qc", nj, nk_pack), v_nc("v_nc", nj, nk_pack), flux_qx("flux_qx", nj, nk_pack), flux_nx("flux_nx", nj, nk_pack), v_qit("v_qit", nj, nk_pack),
+    v_nit("v_nit", nj, nk_pack), flux_nit("flux_nit", nj, nk_pack), flux_bir("flux_bir", nj, nk_pack), flux_qir("flux_qir", nj, nk_pack),
+    flux_qit("flux_qit", nj, nk_pack), v_qr("v_qr", nj, nk_pack), v_nr("v_nr", nj, nk_pack);
+
+  P3F::P3Temporaries temporaries{
+    mu_r, T_atm, lamr, logn0r, nu, cdist, cdist1, cdistr, inv_cld_frac_i,
+    inv_cld_frac_l, inv_cld_frac_r, qc_incld, qr_incld, qi_incld, qm_incld,
+    nc_incld, nr_incld, ni_incld, bm_incld, inv_dz, inv_rho, ze_ice, ze_rain,
+    prec, rho, rhofacr, rhofaci, acn, qv_sat_l, qv_sat_i, sup, qv_supersat_i,
+    tmparr2, exner, diag_equiv_reflectivity, diag_vm_qi, diag_diam_qi,
+    pratot, prctot, qtend_ignore, ntend_ignore, mu_c, lamc, qr_evap_tend,
+    v_qc, v_nc, flux_qx, flux_nx, v_qit, v_nit, flux_nit, flux_bir, flux_qir,
+    flux_qit, v_qr, v_nr
+  };
+#endif
+
   // load tables
   view_1d_table mu_r_table_vals;
   view_2d_table vn_table_vals, vm_table_vals, revap_table_vals;
@@ -2068,12 +2098,15 @@ Int p3_main_f(
   P3F::P3Runtime runtime_options{740.0e3};
 
   // Create local workspace
-  const Int nk_pack = ekat::npack<Spack>(nk);
   const auto policy = ekat::ExeSpaceUtils<KT::ExeSpace>::get_default_team_policy(nj, nk_pack);
   ekat::WorkspaceManager<Spack, KT::Device> workspace_mgr(nk_pack, 52, policy);
 
   auto elapsed_microsec = P3F::p3_main(runtime_options, prog_state, diag_inputs, diag_outputs, infrastructure,
-                                       history_only, lookup_tables, workspace_mgr, nj, nk, physics::P3_Constants<Real>());
+                                       history_only, lookup_tables,
+#ifdef SCREAM_P3_SMALL_KERNELS
+                                       temporaries,
+#endif
+                                       workspace_mgr, nj, nk, physics::P3_Constants<Real>());
 
   Kokkos::parallel_for(nj, KOKKOS_LAMBDA(const Int& i) {
     precip_liq_surf_temp_d(0, i / Spack::n)[i % Spack::n] = precip_liq_surf_d(i);