132b3a506b8a90274bcb9cc5f7c67cf2eafdfb20
Oksana Guba <oksana.guba@gmail.com>
2024-09-05 16:30:50 -0600

    HOMME: minor GPTL timing related mods
    * Disable timing in prim_main for the first two teps
    * Add some timers in EulerStepFunctor

--- components/homme/src/prim_main.F90
+++ components/homme/src/prim_main.F90
@@ -20,7 +20,7 @@ program prim_main
   use element_mod,      only: element_t
   use common_io_mod,    only: output_dir, infilenames
   use common_movie_mod, only: nextoutputstep
-  use perf_mod,         only: t_initf, t_prf, t_finalizef, t_startf, t_stopf ! _EXTERNAL
+  use perf_mod,         only: t_initf, t_prf, t_finalizef, t_startf, t_stopf, t_disablef, t_enablef ! _EXTERNAL
   use restart_io_mod ,  only: restartheader_t, writerestart
   use hybrid_mod,       only: hybrid_create
 #if (defined MODEL_THETA_L && defined ARKODE)
@@ -240,6 +240,11 @@ program prim_main
      
      nstep = nextoutputstep(tl)
      do while(tl%nstep<nstep)
+
+     if(tl%nstep < 2) then
+        call t_disablef()
+     endif
+     if(tl%nstep >= 2) call t_enablef()
         call t_startf('prim_run')
         call prim_run_subcycle(elem, hybrid,nets,nete, tstep, .false., tl, hvcoord,1)
         call t_stopf('prim_run')
