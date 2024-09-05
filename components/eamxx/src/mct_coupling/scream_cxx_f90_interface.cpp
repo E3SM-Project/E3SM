70222af46f2d41d247ce0cda4c327c439b7e6e04
Luca Bertagna <lbertag@sandia.gov>
2024-09-05 17:30:34 -0600

    EAMxx: in the fpe_guard_wrapper for CIME runs, capture all exceptions

--- components/eamxx/src/mct_coupling/scream_cxx_f90_interface.cpp
+++ components/eamxx/src/mct_coupling/scream_cxx_f90_interface.cpp
@@ -66,6 +66,14 @@ void fpe_guard_wrapper (const Lambda& f) {
     c.clean_up();
 
     MPI_Abort (raw_comm,1);
+  } catch (...) {
+
+    // Get raw comm before cleaning up singleton
+    auto& c = ScreamContext::singleton();
+    auto raw_comm = c.get<ekat::Comm>().mpi_comm();
+    c.clean_up();
+
+    MPI_Abort (raw_comm,1);
   }
 
   // Restore the FPE flag as it was when control was handed to us.
