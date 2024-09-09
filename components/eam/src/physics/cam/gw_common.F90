78ed454e390e248cde74bb0b31b0ac5fd0147d05
xie7 <xie7@llnl.gov>
2024-09-08 20:47:35 -0700

    use this.

--- components/eam/src/physics/cam/gw_common.F90
+++ components/eam/src/physics/cam/gw_common.F90
@@ -1325,7 +1325,7 @@ real(r8):: ncd  !!tunable parameter for fbd
  !===================================
 
 ncleff    = 3._r8
-ncd       = 3._r8!1._r8!3._r8
+ncd       = 3._r8!1._r8!1._r8!3._r8
 !
 !---- constants                                                         
 !                                                                       
@@ -1948,7 +1948,6 @@ enddo
           if (.not.icrilv(i) .and. taup(i,k) .gt. 0.0_r8 ) then
             temv = 1.0_r8 / velco(i,k)
             tem1 = coefm(i)/(dxy(i)/ncleff)*(ro(i,kp1)+ro(i,k))*brvf(i)*velco(i,k)*0.5_r8
-            !tem1 = coefm(i)/(sqrt(dxy(i)**2._r8 + dxyp(i)**2._r8)/ncleff)*(ro(i,kp1)+ro(i,k))*brvf(i)*velco(i,k)*0.5_r8
             hd   = sqrt(taup(i,k) / tem1)
             fro  = brvf(i) * hd * temv
 
