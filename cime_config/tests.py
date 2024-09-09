9059c5a821b68d20c1678246aa85fd5b882aa11d
Rich Fiorella <rfiorella@lanl.gov>
2024-09-09 13:19:16 -0600

    add polygon test to tests.py

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -100,6 +100,7 @@ _TESTS = {
             "ERS.r05_r05.IELM.elm-lnd_rof_2way",
             "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
             "ERS.ELM_USRDAT.IELM.elm-surface_water_dynamics"
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-polygonal_tundra"
             )
         },
 
