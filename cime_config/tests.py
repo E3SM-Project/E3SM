28a25ddffecc08781756c201fa56d765be5c8f53
Matthew Hoffman <mhoffman@lanl.gov>
2024-09-11 17:10:14 -0500

    Create testmod and test for TF coupling feature

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -305,6 +305,7 @@ _TESTS = {
             "ERS_Ld5.T62_oQU120.CMPASO-NYF",
             "ERS.f09_g16_g.MALISIA",
             "ERS_Ld5.TL319_oQU240wLI_ais8to30.MPAS_LISIO_JRA1p5.mpaso-ocn_glcshelf",
+            "ERS_Ld5.TL319_IcoswISC30E3r5_gis20.MPAS_LISIO_JRA1p5.mpaso-ocn_glc_tf_coupling",
             "SMS_P12x2.ne4pg2_oQU480.WCYCL1850NS.allactive-mach_mods",
             "ERS_Ln9.ne4pg2_ne4pg2.F2010-MMF1.eam-mmf_crmout",
             )
