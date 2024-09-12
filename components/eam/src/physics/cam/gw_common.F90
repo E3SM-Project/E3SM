999c0d410aba8c1169cda8e912b72a7812ca2113
xie7 <xie7@llnl.gov>
2024-09-12 11:15:24 -0700

    3GWD-3FBD-sGWD-TOFD
    *********1*********2*********3*********4*********5*********6*********7**
    Longer commit message body describing the commit.
    Can contain lists as follows:
    	* Item 1
    	* Item 2
    	* Item 3
    
    A good commit message should be written like an email, a subject
    followed by a blank line, followed by a more descriptive body.
    
    Can also contain a tag at the bottom describing what type of commit this is.
    [BFB] - Bit-For-Bit
    [FCC] - Flag Climate Changing
    [Non-BFB] - Non Bit-For-Bit
    [CC] - Climate Changing
    [NML] - Namelist Changing
    
    See confluence for a more detailed description about these tags.

--- components/eam/src/physics/cam/gw_common.F90
+++ components/eam/src/physics/cam/gw_common.F90
@@ -1322,6 +1322,9 @@ real(r8):: ncd
  logical  :: iint
  real(r8) :: zl_hint(its:ite)
  !===================================
+
+ ncleff   = 3._r8
+ ncd=3._r8
 !
 !---- constants                                                         
 !                                                                       
@@ -1702,7 +1705,6 @@ IF ( ((gsd_gwd_ls .EQ. 1).or.(gsd_gwd_bl .EQ. 1)).and.   &
        cleff    = sqrt(dxy(i)**2._r8 + dxyp(i)**2._r8)
 !==============Jinbo Xie=============================================
        !cleff    = 3._r8 * max(dxmeter(i),cleff)!turned dxmeter to array
-        ncleff   = 1._r8
         cleff    = (3._r8/ncleff) * max(dxmax_ls,cleff)
         !cleff    = 2._r8 * max(dxmax_ls,cleff)
 !==============Jinbo Xie=============================================
@@ -2050,7 +2052,6 @@ zblk_col(i)=zblk
 !ref Lott and Miller (1997) original scheme
           cd = max(2.0_r8-1.0_r8/od(i),0.0_r8)
 
-ncd=1._r8
 cd=ncd*cd
 
           taufb(i,kts) = 0.5_r8 * roll(i) * coefm(i) / max(dxmax_ls,dxy(i))**2 * cd * dxyp(i)   &
