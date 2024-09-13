5835a87f138e6ba015bdc8dcdf4f4f7b00d284bb
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2024-09-13 14:49:38 -0600

    Fix for tendency terms no longer disabled by default

--- components/omega/test/timeStepping/TimeStepperTest.cpp
+++ components/omega/test/timeStepping/TimeStepperTest.cpp
@@ -195,7 +195,6 @@ int initTimeStepperTest(const std::string &mesh) {
    Config Options;
 
    // Creating non-default tendencies with custom velocity tendencies
-   // All other tendencies are disabled by default
    auto *TestTendencies = Tendencies::create(
        "TestTendencies", DefMesh, NVertLevels, &Options,
        Tendencies::CustomTendencyType{}, DecayVelocityTendency{});
@@ -203,6 +202,14 @@ int initTimeStepperTest(const std::string &mesh) {
       Err++;
       LOG_ERROR("TimeStepperTest: error creating test tendencies");
    }
+
+   // Disable all other tendencies
+   TestTendencies->ThicknessFluxDiv.Enabled   = false;
+   TestTendencies->PotientialVortHAdv.Enabled = false;
+   TestTendencies->KEGrad.Enabled             = false;
+   TestTendencies->SSHGrad.Enabled            = false;
+   TestTendencies->VelocityDiffusion.Enabled  = false;
+   TestTendencies->VelocityHyperDiff.Enabled  = false;
 
    return Err;
 }
