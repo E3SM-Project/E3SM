3422a999eda3bc16f1ac68e75ece95bb7f99e502
Luca Bertagna <lbertag@sandia.gov>
2024-10-24 14:28:44 -0600

    EAMxx: enable branch run type
    It's just like a restart run, but we may have a different case name

--- components/eamxx/src/control/atmosphere_driver.cpp
+++ components/eamxx/src/control/atmosphere_driver.cpp
@@ -153,17 +153,31 @@ init_scorpio(const int atm_id)
 }
 
 void AtmosphereDriver::
-init_time_stamps (const util::TimeStamp& run_t0, const util::TimeStamp& case_t0)
+init_time_stamps (const util::TimeStamp& run_t0, const util::TimeStamp& case_t0, int run_type)
 {
   m_atm_logger->info("  [EAMxx] Run  start time stamp: " + run_t0.to_string());
   m_atm_logger->info("  [EAMxx] Case start time stamp: " + case_t0.to_string());
 
+  EKAT_REQUIRE_MSG (case_t0<=run_t0,
+      "Error! Case t0 time stamp must precede the run t0 time stamp.\n"
+      "  - case t0: " + case_t0.to_string() + "\n"
+      "  - run  t0: " + run_t0.to_string() + "\n");
+
   // Initialize time stamps
   m_run_t0 = m_current_ts = run_t0;
   m_case_t0 = case_t0;
+
+  switch (run_type) {
+    case 0:
+      m_run_type = RunType::Initial; break;
+    case 1:
+      m_run_type = RunType::Restart; break;
+    case -1:
+      m_run_type = case_t0==run_t0 ? RunType::Initial : RunType::Restart; break;
+    default:
+      EKAT_ERROR_MSG ("Unsupported/unrecognized run_type: " + std::to_string(run_type) + "\n");
+  }
 }
-
-
 
 void AtmosphereDriver::
 setup_iop ()
@@ -243,7 +257,7 @@ void AtmosphereDriver::create_grids()
   // To avoid having to pass the same data twice in the input file,
   // we have the AD add the IC file name to the GM params
   const auto& ic_pl = m_atm_params.sublist("initial_conditions");
-  if (m_case_t0<m_run_t0) {
+  if (m_run_type==RunType::Restart) {
     // Restarted run -> read geo data from restart file
     const auto& casename = ic_pl.get<std::string>("restart_casename");
     auto filename = find_filename_in_rpointer (casename,true,m_atm_comm,m_run_t0);
@@ -789,16 +803,19 @@ void AtmosphereDriver::initialize_output_managers () {
 
 void AtmosphereDriver::
 set_provenance_data (std::string caseid,
+                     std::string rest_caseid,
                      std::string hostname,
                      std::string username)
 {
 #ifdef SCREAM_CIME_BUILD
   // Check the inputs are valid
   EKAT_REQUIRE_MSG (caseid!="", "Error! Invalid case id: " + caseid + "\n");
+  EKAT_REQUIRE_MSG (m_run_type==RunType::Initial or rest_caseid!="",
+      "Error! Invalid restart case id: " + rest_caseid + "\n");
   EKAT_REQUIRE_MSG (hostname!="", "Error! Invalid hostname: " + hostname + "\n");
   EKAT_REQUIRE_MSG (username!="", "Error! Invalid username: " + username + "\n");
 #else
-  caseid = "EAMxx standalone";
+  caseid = rest_caseid = "EAMxx standalone";
   char* user = new char[32];
   char* host = new char[256];
   int err;
@@ -819,6 +836,7 @@ set_provenance_data (std::string caseid,
 #endif
   auto& provenance = m_atm_params.sublist("provenance");
   provenance.set("caseid",caseid);
+  provenance.set("rest_caseid",rest_caseid);
   provenance.set("hostname",hostname);
   provenance.set("username",username);
   provenance.set("version",std::string(EAMXX_GIT_VERSION));
@@ -859,7 +877,7 @@ initialize_fields ()
   }
 
   // Initialize fields
-  if (m_case_t0<m_run_t0) {
+  if (m_run_type==RunType::Restart) {
     restart_model ();
   } else {
     set_initial_conditions ();
@@ -1555,15 +1573,13 @@ void AtmosphereDriver::initialize_atm_procs ()
   m_memory_buffer->allocate();
   m_atm_process_group->init_buffers(*m_memory_buffer);
 
-  const bool restarted_run = m_case_t0 < m_run_t0;
-
   // Setup SurfaceCoupling import and export (if they exist)
   if (m_surface_coupling_import_data_manager || m_surface_coupling_export_data_manager) {
     setup_surface_coupling_processes();
   }
 
   // Initialize the processes
-  m_atm_process_group->initialize(m_current_ts, restarted_run ? RunType::Restarted : RunType::Initial);
+  m_atm_process_group->initialize(m_current_ts, m_run_type);
 
   // Create and add energy and mass conservation check to appropriate atm procs
   setup_column_conservation_checks();
