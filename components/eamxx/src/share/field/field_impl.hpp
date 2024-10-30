1fb9d2bead643531b7c861ac1ca1241b2dfc13ae
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2024-10-30 10:40:26 -0400

    EAMxx: fix field deep copy on rank-0 fields

--- components/eamxx/src/share/field/field_impl.hpp
+++ components/eamxx/src/share/field/field_impl.hpp
@@ -543,7 +543,7 @@ void Field::deep_copy_impl (const ST value) const {
     case 0:
       {
         auto v = get_view<ST,HD>();
-        v() = value;
+        Kokkos::deep_copy(v,value);
       }
       break;
     case 1:
