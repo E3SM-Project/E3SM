f9dc13ff1957ba625e54854fee0cb3315331164b
Oksana Guba <oksana.guba@gmail.com>
2024-11-23 21:18:47 -0600

    make preqx compile with new baro test

--- components/homme/src/preqx/derivative_mod.F90
+++ components/homme/src/preqx/derivative_mod.F90
@@ -8,6 +8,7 @@ module derivative_mod
        gradient_sphere_wk_testcov, gradient_sphere_wk_testcontra, ugradv_sphere, vorticity_sphere, vorticity_sphere_diag,    &
        curl_sphere,curl_sphere_wk_testcov, vlaplace_sphere_wk, element_boundary_integral, &
        limiter_optim_iter_full, limiter_clip_and_sum, laplace_sphere_wk, divergence_sphere_wk, gradient_sphere,              &
-       divergence_sphere, laplace_z
+       divergence_sphere, laplace_z, &
+       get_deriv
   implicit none
 end module derivative_mod
