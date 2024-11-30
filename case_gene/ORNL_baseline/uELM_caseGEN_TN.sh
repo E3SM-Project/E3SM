0f82f2f04aa03a0efc92c34755ccde3ee658cb0c
Dali Wang <wangd@ornl.gov>
2024-11-30 10:36:39 -0500

    TES case on cades-baseline

---
+++ case_gene/ORNL_baseline/uELM_caseGEN_TN.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -e
+
+# Create a test case uELM_AKSP_I1850uELMCNPRDCTCBC
+
+#E3SM_DIN="/gpfs/wolf2/cades/cli185/proj-shared/pt-e3sm-inputdata"
+E3SM_DIN="//gpfs/wolf2/cades/cli185/world-shared/e3sm"
+DATA_ROOT="/gpfs/wolf2/cades/cli185/proj-shared/wangd/kiloCraft/TES_cases_data/Daymet_ERA5_TESSFA2/"
+E3SM_SRCROOT=$(git rev-parse --show-toplevel)
+echo "E3SM_SRCROOT: $E3SM_SRCROOT"
+echo "E3SM_DIN: $E3SM_DIN"
+
+EXPID="TN"
+CASEDIR="$E3SM_SRCROOT/e3sm_cases/uELM_${EXPID}_I1850uELMCNPRDCTCBC"
+CASE_DATA="${DATA_ROOT}/${EXPID}"
+DOMAIN_FILE="${EXPID}_domain.lnd.TES_SE.4km.1d.c241127.nc"
+SURFDATA_FILE="${EXPID}_surfdata.TES_SE.4km.1d.NLCD.c241128.nc"
+
+\rm -rf "${CASEDIR}"
+
+#${E3SM_SRCROOT}/cime/scripts/create_newcase --case "${CASEDIR}" --mach summitPlus --compiler pgi --mpilib spectrum-mpi --compset I1850uELMCNPRDCTCBC --res ELM_USRDAT --pecount "${PECOUNT}" --handle-preexisting-dirs r --srcroot "${E3SM_SRCROOT}"
+
+${E3SM_SRCROOT}/cime/scripts/create_newcase --case "${CASEDIR}" --mach cades-baseline --compiler gnu --mpilib openmpi --compset I1850uELMCNPRDCTCBC --res ELM_USRDAT  --handle-preexisting-dirs r --srcroot "${E3SM_SRCROOT}"
+
+cd "${CASEDIR}"
+
+./xmlchange PIO_TYPENAME="pnetcdf"
+
+./xmlchange PIO_NETCDF_FORMAT="64bit_data"
+
+./xmlchange DIN_LOC_ROOT="${E3SM_DIN}"
+
+./xmlchange DIN_LOC_ROOT_CLMFORC="${CASE_DATA}"
+
+./xmlchange ELM_FORCE_COLDSTART=on
+
+./xmlchange DATM_MODE="uELMTES"
+
+./xmlchange DATM_CLMNCEP_YR_START="2014"
+
+./xmlchange DATM_CLMNCEP_YR_END="2014"
+
+./xmlchange ATM_NCPL=24
+
+./xmlchange STOP_N=30
+
+./xmlchange STOP_OPTION=ndays
+
+./xmlchange NTASKS_LND=128
+
+./xmlchange MAX_MPITASKS_PER_NODE=128
+
+./xmlchange ATM_DOMAIN_PATH="${CASE_DATA}/domain_surfdata/"
+
+./xmlchange ATM_DOMAIN_FILE="${DOMAIN_FILE}"
+
+./xmlchange LND_DOMAIN_PATH="${CASE_DATA}/domain_surfdata/"
+
+./xmlchange LND_DOMAIN_FILE="${DOMAIN_FILE}"
+
+./xmlchange JOB_WALLCLOCK_TIME="1:00:00"
+
+./xmlchange USER_REQUESTED_WALLTIME="1:00:00"
+
+echo "fsurdat = '${CASE_DATA}/domain_surfdata/${SURFDATA_FILE}'
+      hist_nhtfrq=0
+      hist_mfilt=1
+     " >> user_nl_elm
+
+./case.setup --reset
+
+./case.setup
+
+./case.build --clean-all
+
+./case.build
+
+./case.submit
+
