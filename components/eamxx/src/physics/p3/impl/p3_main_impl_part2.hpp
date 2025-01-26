8829fba6020a15076b6d24d852b7be77f554264b
mahf708 <naser.mahfouz@pnnl.gov>
2025-01-26 07:34:28 -0800

    only assign when we have to

--- components/eamxx/src/physics/p3/impl/p3_main_impl_part2.hpp
+++ components/eamxx/src/physics/p3/impl/p3_main_impl_part2.hpp
@@ -122,6 +122,7 @@ void Functions<S,D>
   const bool do_ice_production   = runtime_options.do_ice_production;
   const bool use_hetfrz_classnuc = runtime_options.use_hetfrz_classnuc;
   const bool use_separate_ice_liq_frac = runtime_options.use_separate_ice_liq_frac;
+  const bool p3_extra_diags = runtime_options.p3_extra_diags;
 
   team.team_barrier();
   hydrometeorsPresent = false;
@@ -514,19 +515,21 @@ void Functions<S,D>
     vap_liq_exchange(k).set(not_skip_all, -qr2qv_evap_tend);
     liq_ice_exchange(k).set(not_skip_all, qc2qi_hetero_freeze_tend + qr2qi_immers_freeze_tend - qi2qr_melt_tend + qc2qi_berg_tend + qc2qi_collect_tend + qr2qi_collect_tend);
 
-   // set tendencies
-    P3_qr2qv_evap(k).set(not_skip_all, qr2qv_evap_tend);
-    P3_qi2qv_sublim(k).set(not_skip_all, qi2qv_sublim_tend);
-    P3_qc2qr_accret(k).set(not_skip_all, qc2qr_accret_tend);
-    P3_qc2qr_autoconv(k).set(not_skip_all, qc2qr_autoconv_tend);
-    P3_qv2qi_vapdep(k).set(not_skip_all, qv2qi_vapdep_tend);
-    P3_qc2qi_berg(k).set(not_skip_all, qc2qi_berg_tend);
-    P3_qc2qr_ice_shed(k).set(not_skip_all, qc2qr_ice_shed_tend);
-    P3_qc2qi_collect(k).set(not_skip_all, qc2qi_collect_tend);
-    P3_qr2qi_collect(k).set(not_skip_all, qr2qi_collect_tend);
-    P3_qc2qi_hetero_freeze(k).set(not_skip_all, qc2qi_hetero_freeze_tend);
-    P3_qr2qi_immers_freeze(k).set(not_skip_all, qr2qi_immers_freeze_tend);
-    P3_qi2qr_melt(k).set(not_skip_all, qi2qr_melt_tend);
+    // set tendencies if p3_extra_diags is true
+    if(p3_extra_diags) {
+      P3_qr2qv_evap(k).set(not_skip_all, qr2qv_evap_tend);
+      P3_qi2qv_sublim(k).set(not_skip_all, qi2qv_sublim_tend);
+      P3_qc2qr_accret(k).set(not_skip_all, qc2qr_accret_tend);
+      P3_qc2qr_autoconv(k).set(not_skip_all, qc2qr_autoconv_tend);
+      P3_qv2qi_vapdep(k).set(not_skip_all, qv2qi_vapdep_tend);
+      P3_qc2qi_berg(k).set(not_skip_all, qc2qi_berg_tend);
+      P3_qc2qr_ice_shed(k).set(not_skip_all, qc2qr_ice_shed_tend);
+      P3_qc2qi_collect(k).set(not_skip_all, qc2qi_collect_tend);
+      P3_qr2qi_collect(k).set(not_skip_all, qr2qi_collect_tend);
+      P3_qc2qi_hetero_freeze(k).set(not_skip_all, qc2qi_hetero_freeze_tend);
+      P3_qr2qi_immers_freeze(k).set(not_skip_all, qr2qi_immers_freeze_tend);
+      P3_qi2qr_melt(k).set(not_skip_all, qi2qr_melt_tend);
+    }
 
     // clipping for small hydrometeor values
     const auto qc_small    = qc(k) < qsmall    && not_skip_all;
