02c71fd39340249d58702c3741eed43ac3e4a54a
Peter Andrew Bogenschutz <bogensch@dane3.llnl.gov>
2025-01-28 13:59:13 -0800

    add stability correction term to dz based length scale computation for 1p5 TKE closure

--- components/eamxx/src/physics/shoc/impl/shoc_compute_shoc_mix_shoc_length_impl.hpp
+++ components/eamxx/src/physics/shoc/impl/shoc_compute_shoc_mix_shoc_length_impl.hpp
@@ -34,12 +34,12 @@ void Functions<S,D>
     const Spack brunt2 = ekat::max(0, brunt(k));
 
     if (tke_1p5_closure){
-       shoc_mix(k) = dz_zt(k); // may need to add possible stability correction
-//       const auto stable_mask = brunt > 0;
-//       if (stable_mask.any()){
-//         shoc_mix(k) = ekat::min(dz_zt(k),ekat::max(0.1*dz_zt(k),ekat::sqrt(0.76
-//	                        *tk(k)/0.1/ekat::sqrt(brunt+1.e-10))));
-//       }
+       shoc_mix(k) = dz_zt(k);
+       const auto stable_mask = brunt(k) > 0;
+       if (stable_mask.any()){
+         shoc_mix(k) = ekat::min(dz_zt(k),ekat::max(0.1*dz_zt(k),ekat::sqrt(0.76
+	                        *tk(k)/0.1/ekat::sqrt(brunt(k)+1.e-10))));
+       }
     }else{
        shoc_mix(k) = ekat::min(maxlen,
                             sp(2.8284)*(ekat::sqrt(1/((1/(tscale*tkes*vk*zt_grid(k)))
