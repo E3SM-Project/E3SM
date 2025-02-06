30c94105c85a7fc1daf5cec8a59e01fd29ebebfc
Youngsung Kim <youngsung.kim.act2@gmail.com>
2025-02-06 10:56:49 -0500

    Unify the Frontier machine file and rename compiler configurations
    * Merge the Frontier machine and compiler configurations of E3SM and Scream.
    * Rename compiler names using the syntax: "[Fortran wrapper]<Fortran CPU compiler>[-[C++ wrapper]C++ DEVICE compiler]".

--- cime_config/machines/cmake_macros/craycray.cmake
+++ cime_config/machines/cmake_macros/craycray.cmake
@@ -9,8 +9,8 @@ string(APPEND CMAKE_Fortran_FLAGS_DEBUG   " -O0 -g")
 string(APPEND CMAKE_CXX_FLAGS_DEBUG " -O0 -g")
 string(APPEND CPPDEFS_DEBUG  " -DYAKL_DEBUG")
 string(APPEND CPPDEFS " -DFORTRANUNDERSCORE -DNO_R16 -DCPRCRAY")
-# -em (default) generates MODULENAME.mod files
-string(APPEND CMAKE_Fortran_FLAGS " -f free -em")
+# -em -ef generates modulename.mod files (lowercase), which we must have
+string(APPEND CMAKE_Fortran_FLAGS " -f free -em -ef")
 if (NOT compile_threaded)
 	# -M1077 flag used to suppress message about OpenMP directives
 	# that are ignored for non-threaded builds. (-h omp inactive)
@@ -18,17 +18,11 @@ if (NOT compile_threaded)
   string(APPEND CMAKE_Fortran_FLAGS " -M1077")
 endif()
 set(HAS_F2008_CONTIGUOUS "TRUE")
-string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,--allow-multiple-definition")
-
+string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,--allow-multiple-definition -ldl")
+set(E3SM_LINK_WITH_FORTRAN "TRUE")
 set(MPICC "cc")
 set(MPICXX "CC")
 set(MPIFC "ftn")
 set(SCC "cc")
 set(SCXX "CC")
 set(SFC "ftn")
-
-string(APPEND CPPDEFS " -DLINUX")
-if (COMP_NAME STREQUAL gptl)
-    string(APPEND CPPDEFS " -DHAVE_NANOTIME -DBIT64 -DHAVE_SLASHPROC -DHAVE_COMM_F2C -DHAVE_TIMES -DHAVE_GETTIMEOFDAY")
-endif()
-
