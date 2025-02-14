f9145f9489efd70270cecfbdf03bf8a0e4e618f1
Luca Bertagna <lbertag@sandia.gov>
2025-02-14 14:47:33 -0700

    EAMxx: swap template args order in Field::update
    Users should prob not run update kernels on Host anyways,
    so it is more likely they will need to change the update method

--- components/eamxx/src/share/field/eti/field_eti_update_double_host.cpp
+++ components/eamxx/src/share/field/eti/field_eti_update_double_host.cpp
@@ -2,8 +2,8 @@
 
 namespace scream {
 
-template void Field::update<Host, CombineMode::Update,  double>(const Field&, const double, const double);
-template void Field::update<Host, CombineMode::Multiply,double>(const Field&, const double, const double);
-template void Field::update<Host, CombineMode::Divide,  double>(const Field&, const double, const double);
+template void Field::update<CombineMode::Update,  Host, double>(const Field&, const double, const double);
+template void Field::update<CombineMode::Multiply,Host, double>(const Field&, const double, const double);
+template void Field::update<CombineMode::Divide,  Host, double>(const Field&, const double, const double);
 
 } // namespace scream
--- components/eamxx/src/share/field/eti/field_eti_update_float_host.cpp
+++ components/eamxx/src/share/field/eti/field_eti_update_float_host.cpp
@@ -2,8 +2,8 @@
 
 namespace scream {
 
-template void Field::update<Host, CombineMode::Update,  float>(const Field&, const float, const float);
-template void Field::update<Host, CombineMode::Multiply,float>(const Field&, const float, const float);
-template void Field::update<Host, CombineMode::Divide,  float>(const Field&, const float, const float);
+template void Field::update<CombineMode::Update,  Host, float>(const Field&, const float, const float);
+template void Field::update<CombineMode::Multiply,Host, float>(const Field&, const float, const float);
+template void Field::update<CombineMode::Divide,  Host, float>(const Field&, const float, const float);
 
 } // namespace scream
