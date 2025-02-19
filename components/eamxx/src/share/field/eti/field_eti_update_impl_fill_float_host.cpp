98a36d9bade23ba5d690d6b7fc136c63b1b26a85
Luca Bertagna <lbertag@sandia.gov>
2025-02-19 15:28:05 -0700

    EAMxx: allow to call Field::update with a rhs with different data type
    The RHS data type must be convertible to the LHS data type
    without any narrowing (like for alpha/beta coefficients)

--- components/eamxx/src/share/field/eti/field_eti_update_impl_fill_float_host.cpp
+++ components/eamxx/src/share/field/eti/field_eti_update_impl_fill_float_host.cpp
@@ -2,8 +2,12 @@
 
 namespace scream {
 
-template void Field::update_impl<CombineMode::Update,  Host, true, float>(const Field&, const float, const float);
-template void Field::update_impl<CombineMode::Multiply,Host, true, float>(const Field&, const float, const float);
-template void Field::update_impl<CombineMode::Divide,  Host, true, float>(const Field&, const float, const float);
+template void Field::update_impl<CombineMode::Update,  Host, true, float, float>(const Field&, const float, const float);
+template void Field::update_impl<CombineMode::Multiply,Host, true, float, float>(const Field&, const float, const float);
+template void Field::update_impl<CombineMode::Divide,  Host, true, float, float>(const Field&, const float, const float);
+
+template void Field::update_impl<CombineMode::Update,  Host, true, float, int>(const Field&, const float, const float);
+template void Field::update_impl<CombineMode::Multiply,Host, true, float, int>(const Field&, const float, const float);
+template void Field::update_impl<CombineMode::Divide,  Host, true, float, int>(const Field&, const float, const float);
 
 } // namespace scream
