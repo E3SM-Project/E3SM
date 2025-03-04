c179105f023f66ea6d3854a96f3099256f258882
noel <ndkeen@lbl.gov>
2025-03-03 22:39:46 -0800

    for pm-cpu DEBUG builds, don't use invalid FPE exceptions on coupler sources

--- cime_config/machines/cmake_macros/intel_pm-cpu.cmake
+++ cime_config/machines/cmake_macros/intel_pm-cpu.cmake
@@ -25,7 +25,21 @@ string(APPEND CMAKE_CXX_FLAGS " -fp-model=precise") # and manually add precise
 #message(STATUS "ndk CXXFLAGS=${CXXFLAGS}")
 
 string(APPEND CMAKE_Fortran_FLAGS " -fp-model=consistent -fimf-use-svml")
-   #  string(APPEND FFLAGS " -qno-opt-dynamic-align")
- string(APPEND CMAKE_Fortran_FLAGS_RELEASE " -g -traceback")
+#  string(APPEND FFLAGS " -qno-opt-dynamic-align")
+string(APPEND CMAKE_Fortran_FLAGS_RELEASE " -g -traceback")
 string(APPEND CMAKE_Fortran_FLAGS " -DHAVE_ERF_INTRINSICS")
 string(APPEND CMAKE_CXX_FLAGS " -fp-model=consistent")
+
+
+# For MCT coupler sources only, try resetting CMAKE_Fortran_FLAGS_DEBUG to avoid setting FPE invalid exceptions
+# https://github.com/E3SM-Project/E3SM/issues/7049
+if (COMP_NAME STREQUAL cpl)
+  set(CMAKE_Fortran_FLAGS_DEBUG " ") # set to blank and rebuild
+  if (compile_threaded)
+    string(APPEND CMAKE_Fortran_FLAGS_DEBUG   " -qopenmp")
+  endif()
+  #original string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -O0 -g -check uninit -check bounds -check pointers -fpe0 -check noarg_temp_created -init=snan,arrays")
+  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -O0 -g -check uninit -check bounds -check pointers -check noarg_temp_created -init=nosnan,arrays")
+  string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -convert big_endian -assume byterecl -ftz -traceback -assume realloc_lhs -fp-model source")
+endif()
+
