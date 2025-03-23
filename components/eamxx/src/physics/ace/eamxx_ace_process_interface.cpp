8ab21e52601d32ef145ca335272b9b3df05c6abb
mahf708 <naser.mahfouz@pnnl.gov>
2025-03-23 14:15:05 -0700

    need to sync memory...

--- components/eamxx/src/physics/ace/eamxx_ace_process_interface.cpp
+++ components/eamxx/src/physics/ace/eamxx_ace_process_interface.cpp
@@ -21,16 +21,25 @@ void ACE::set_grids(const std::shared_ptr<const GridsManager> grids_manager) {
   constexpr auto K      = ekat::units::K;
   constexpr auto nondim = ekat::units::Units::nondimensional();
 
-  const auto horiz_wind_layout =
-      m_grid->get_2d_vector_layout(2);
-  const auto layout_3d = m_grid->get_3d_scalar_layout(true);
-  const auto layout_2d = m_grid->get_2d_scalar_layout();
+  const auto horiz_wind_layout = m_grid->get_2d_vector_layout(2);
+  const auto layout_3d         = m_grid->get_3d_scalar_layout(true);
+  const auto layout_2d         = m_grid->get_2d_scalar_layout();
 
   add_field<Updated>("T_mid", layout_3d, K, m_grid->name());
   add_field<Updated>("horiz_winds", horiz_wind_layout, m / s, m_grid->name());
   add_field<Updated>("ocnfrac", layout_2d, nondim, m_grid->name());
-  add_field<Updated>("landfrac", layout_2d, nondim, m_grid->name());
+  add_field<Required>("landfrac", layout_2d, nondim, m_grid->name());
   add_field<Updated>("icefrac", layout_2d, nondim, m_grid->name());
+
+  // declare a layouts of 7x180x360
+  using namespace ShortFieldTagsNames;
+  FieldLayout scalar3d_39x180x360_mid{{CMP, CMP, CMP, CMP}, {1, 39, 180, 360}};
+  FieldLayout scalar3d_44x180x360_mid{{CMP, CMP, CMP, CMP}, {1, 44, 180, 360}};
+
+  add_field<Required>("InField", scalar3d_39x180x360_mid, nondim,
+                      m_grid->name());
+  add_field<Computed>("OutField", scalar3d_44x180x360_mid, nondim,
+                      m_grid->name());
 }
 
 void ACE::initialize_impl(const RunType /* run_type */) {
@@ -61,22 +70,59 @@ void ACE::initialize_impl(const RunType /* run_type */) {
 }
 
 void ACE::run_impl(const double dt) {
+  // Get a view from an input field
+  auto in_field_view = get_field_in("InField").get_view<const Real ****>();
+  // Get a view from an output field
+  auto out_field = get_field_out("OutField");
+  out_field.deep_copy(0.0);
+  auto out_field_view = out_field.get_view<Real ****>();
+
   auto device = (*m_module.parameters().begin()).device();
-  // TODO: Replace with actual input data
-  // For now, we are using random data for testing purposes
-  // The input tensor shape is (batch_size, num_channels, height, width)
-  // Where batch_size is 1, num_channels is 39, height is 180, and width is 360
-  // See comment on bottom for mechanics
-  torch::Tensor input = torch::rand({1, 39, 180, 360});
-  input               = input.to(device);
-  std::vector<torch::jit::IValue> inputs{input};
-  std::cout << "Input tensor shape: " << input.sizes() << std::endl;
+
+  // Create Torch tensors from Kokkos views
+  torch::Tensor in_tensor = torch::from_blob((void *)in_field_view.data(),
+                                             {1, 39, 180, 360}, torch::kFloat32)
+                                .to(device);
+  torch::Tensor out_tensor =
+      torch::from_blob((void *)out_field_view.data(), {1, 44, 180, 360},
+                       torch::kFloat32)
+          .to(device);
+
+  std::cout << "in_tensor shape: " << in_tensor.sizes() << std::endl;
+  std::cout << "in_tensor device: " << in_tensor.device() << std::endl;
+  std::cout << "in_tensor dtype: " << in_tensor.dtype() << std::endl;
+
+  // Perform inference
+  std::vector<torch::jit::IValue> inputs{in_tensor};
+  std::cout << "Input tensor shape: " << in_tensor.sizes() << std::endl;
   std::cout << "Performing inference for " << m_forward_steps << " steps..."
             << std::endl;
-  torch::Tensor output = m_module.forward(inputs).toTensor();
-  std::cout << "Inference output shape: " << output.sizes() << std::endl;
-  std::cout << "Inference completed. Results stored in: " << m_checkpoint_path
-            << std::endl;
+  // torch::Tensor temp_out;
+  try {
+    torch::Tensor temp_out = m_module.forward(inputs).toTensor();
+    std::memcpy(out_tensor.data_ptr<float>(), temp_out.data_ptr<float>(),
+                temp_out.numel() * sizeof(float));
+  } catch(const c10::Error &e) {
+    std::cerr << "Error during inference: " << e.what() << std::endl;
+    return;
+  }
+
+  // verify that the values in OutField are the same as in temp_out?
+  out_field.sync_to_host();
+  auto out_field_view_2 =
+      get_field_out("OutField").get_view<const Real ****, Host>();
+  auto out_tensor_cpu = out_tensor.to(torch::kCPU);
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 3; j++) {
+      for(int k = 0; k < 3; k++) {
+        std::cout << "OutField[0][" << i << "][" << j << "][" << k
+                  << "] = " << out_field_view_2(0, i, j, k) << std::endl;
+        std::cout << "out_tensor[0][" << i << "][" << j << "][" << k
+                  << "] = " << out_tensor[0][i][j][k].item<float>()
+                  << std::endl;
+      }
+    }
+  }
 }
 
 void ACE::finalize_impl() {}
