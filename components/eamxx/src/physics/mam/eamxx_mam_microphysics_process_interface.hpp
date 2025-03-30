cf2051e6924c3dce6373b153cd7f5612a7e29b56
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2025-03-30 14:30:40 -0700

    EAMxx: Using buffer_ to set temporal views in microphysics.

--- components/eamxx/src/physics/mam/eamxx_mam_microphysics_process_interface.hpp
+++ components/eamxx/src/physics/mam/eamxx_mam_microphysics_process_interface.hpp
@@ -158,6 +158,10 @@ class MAMMicrophysics final : public MAMGenericInterface {
   view_2d dflx_;
   view_2d dvel_;
   int num_2d_scratch_= 9;
+  int get_len_temporal_views();
+  void init_temporal_views();
+  int len_temporal_views_{0};
+
 };  // MAMMicrophysics
 
 }  // namespace scream
