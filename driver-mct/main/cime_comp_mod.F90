9e760499f26bf6e7455ec9c7b2cb37249157d3ad
Stephen Price <sprice@lanl.gov>
2025-04-08 09:04:36 -0500

    Numerous minor modifications in response to pull request review

--- driver-mct/main/cime_comp_mod.F90
+++ driver-mct/main/cime_comp_mod.F90
@@ -4790,7 +4790,7 @@ contains
           call seq_diag_ice_mct(ice(ens1), fractions_ix(ens1), infodata, do_x2i=.true.)
        endif
        if (glc_present) then
-               call seq_diag_glc_mct(glc(ens1), fractions_gx(ens1), infodata, do_x2g=.true., do_g2x=.true.)
+          call seq_diag_glc_mct(glc(ens1), fractions_gx(ens1), infodata, do_x2g=.true., do_g2x=.true.)
        endif
        if (do_bgc_budgets) then
           if (rof_present) then
