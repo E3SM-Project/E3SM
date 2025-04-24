2f07eb246373bb666b787dde29e6a8b534ee7d2b
dqwu <wuda@mcs.anl.gov>
2025-04-24 12:15:08 -0500

    Fix E3SM link errors for NCAR PIO

--- components/cmake/modules/FindPIO.cmake
+++ components/cmake/modules/FindPIO.cmake
@@ -18,9 +18,6 @@ endif()
 if (PIO_VERSION STREQUAL 2)
   # This is a pio2 library
   set(PIOLIBS "${PIO_LIBDIR}/libpiof.a;${PIO_LIBDIR}/libpioc.a")
-  if (DEFINED ENV{ADIOS2_ROOT})
-    list(APPEND PIOLIBS "${PIO_LIBDIR}/libadios2pio-nm-lib.a")
-  endif()
 else()
   # This is a pio1 library
   set(PIOLIBS "${PIO_LIBDIR}/libpio.a")
@@ -30,25 +27,6 @@ endif()
 list(APPEND PIOLIBS "${INSTALL_SHAREDPATH}/lib/libgptl.a")
 
 find_package(NETCDF REQUIRED)
-# Check if scorpio has hdf5 enabled
-if (DEFINED ENV{HDF5_ROOT})
-  if (DEFINED ENV{HDF5_USE_STATIC_LIBRARIES})
-    set(HDF5_USE_STATIC_LIBRARIES On)
-  endif()
-  find_package(HDF5 REQUIRED)
-endif()
-
-# Not all machines/PIO installations use ADIOS but, for now,
-# we can assume that an MPI case with ADIOS2_ROOT set is probably
-# using adios.
-if (NOT MPILIB STREQUAL "mpi-serial" AND DEFINED ENV{ADIOS2_ROOT})
-  if(DEFINED ENV{BLOSC2_ROOT})
-    set(ENV{Blosc2_DIR} "$ENV{BLOSC2_ROOT}")
-  endif()
-  find_package(MPI REQUIRED COMPONENTS C)
-  find_package(ADIOS2 REQUIRED COMPONENTS C)
-  list(APPEND PIOLIBS adios2::adios2)
-endif()
 
 list(APPEND PIOLIBS netcdf)
 
@@ -57,10 +35,6 @@ if (MPILIB STREQUAL "mpi-serial")
 else()
   find_package(MPI REQUIRED COMPONENTS C Fortran)
   list(APPEND PIOLIBS MPI::MPI_C MPI::MPI_Fortran)
-endif()
-
-if (DEFINED ENV{HDF5_ROOT})
-  list(APPEND PIOLIBS hdf5)
 endif()
 
 # Create the interface library, and set target properties
