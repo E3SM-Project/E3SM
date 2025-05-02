329abed8bd3e7cc76243aca2b2702f155d6d4e3e
Taufiq Hassan <taufiq.hassan06@gmail.com>
2025-05-02 11:32:21 -0700

    Update components/eamxx/src/physics/mam/eamxx_mam_microphysics_process_interface.cpp
    Co-authored-by: Naser Mahfouz <naser.mahfouz@pnnl.gov>

--- components/eamxx/src/physics/mam/eamxx_mam_microphysics_process_interface.cpp
+++ components/eamxx/src/physics/mam/eamxx_mam_microphysics_process_interface.cpp
@@ -947,9 +947,6 @@ void MAMMicrophysics::run_impl(const double dt) {
   // TODO: getting rid of hard-coded indices
   const int extfrc_pcnst_index[extcnt] = {3, 6, 14, 27, 28, 13, 18, 30, 5};
 
-  // Heights at vertical interfaces [m]
-  const auto z_int = dry_atm_.z_iface;
-
   // Transpose extfrc_ from internal layout [ncol][nlev][extcnt]
   // to output layout [ncol][extcnt][nlev]
   // This aligns with expected field storage in the EAMxx infrastructure.
