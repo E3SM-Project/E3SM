4662bdd3076d4e118024c133c23597b28ecffdcc
Luca Bertagna <lbertag@sandia.gov>
2025-05-15 09:47:41 -0600

    EAMxx: assert macro related changes

--- components/eamxx/src/share/util/eamxx_combine_ops.hpp
+++ components/eamxx/src/share/util/eamxx_combine_ops.hpp
@@ -6,6 +6,7 @@
 #include <ekat_scalar_traits.hpp>
 #include <ekat_math_utils.hpp>
 #include <ekat_pack_where.hpp>
+#include <ekat_kernel_assert.hpp>
 
 // For KOKKOS_INLINE_FUNCTION
 #include <Kokkos_Core.hpp>
