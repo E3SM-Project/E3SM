69e3c00793e1ce4a6cb78121cb139711139b18a6
jayeshkrishna <jayesh@mcs.anl.gov>
2025-05-21 21:51:00 +0000

    Disable VNI for DAOS
    For the DAOS mpiexec disable VNI. Without this fix multi node
    runs will fail with DAOS

--- cime_config/machines/mpiexec_daos.sh
+++ cime_config/machines/mpiexec_daos.sh
@@ -4,4 +4,6 @@
 #printenv
 echo "============== MPIEXEC for AURORA ==================="
 echo "Launching job after pre-loading the DAOS Interception library..."
-mpiexec -genv LD_PRELOAD=/usr/lib64/libpil4dfs.so $@
+#mpiexec $@
+mpiexec -genv LD_PRELOAD=/usr/lib64/libpil4dfs.so --no-vni $@
+#mpiexec -genv LD_PRELOAD=/soft/perftools/darshan/darshan-3.4.7/lib/libdarshan.so:/opt/aurora/24.347.0/spack/unified/0.9.2/install/linux-sles15-x86_64/oneapi-2025.0.5/hdf5-1.14.5-zrlo32i/lib/libhdf5.so:/opt/aurora/24.347.0/spack/unified/0.9.2/install/linux-sles15-x86_64/oneapi-2025.0.5/parallel-netcdf-1.12.3-cszcp66/lib/libpnetcdf.so:/usr/lib64/libpil4dfs.so --no-vni $@
