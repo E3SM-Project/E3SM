f9c9cc616d8c0989c854332726d0aa23e007f9f6
Jon Wolfe <jonbob@lanl.gov>
2025-06-19 15:13:40 -0700

    Update to use PIO routines and reorder into modules

--- components/rdycore/src/main/rdycoreMod.F90
+++ components/rdycore/src/main/rdycoreMod.F90
@@ -15,12 +15,16 @@ module rdycoreMod
 
   PetscInt              , public          :: num_cells_owned           ! number of cells that locally owned
   PetscInt              , public          :: num_cells_global          ! total number of cells in the mesh
-  PetscInt              , pointer         :: natural_id_cells_owned(:) ! natural IDs of cells that are locally owned
+  PetscInt              , public, pointer :: natural_id_cells_owned(:) ! natural IDs of cells that are locally owned
 
   integer               , public, pointer :: rdycore_pocn(:)           ! PE rank for each grid cell
 
   PetscReal             , pointer         :: total_runoff_data(:)      ! the water source to RDycore's SWE
 
   integer               , public          :: iulog = 6
+  character(len=16)     , public          :: inst_name
+  character(len=16)     , public          :: inst_suffix         ! char string associated with instance (ie. "_0001" or "")
+  integer               , public          :: inst_index          ! number of current instance (ie. 1)
+
 
 end module rdycoreMod
