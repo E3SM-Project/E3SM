f3379c9e48577815e8e4737354a03a99ed211b9e
Walter Hannah <hannah6@llnl.gov>
2025-07-14 09:39:10 -0500

    misc ZM bridge updates

--- components/eamxx/src/physics/zm/eamxx_zm_process_interface.hpp
+++ components/eamxx/src/physics/zm/eamxx_zm_process_interface.hpp
@@ -2,16 +2,16 @@
 #define EAMXX_ZM_PROCESS_INTERFACE_HPP
 
 #include "share/atm_process/atmosphere_process.hpp"
-#include "physics/zm/zm_functions.hpp"
-
-#include <ekat_parameter_list.hpp>
+#include "ekat/ekat_parameter_list.hpp"
+#include "share/util/eamxx_common_physics_functions.hpp"
+#include "zm_functions.hpp"
 
 namespace scream
 {
 
 // Zhang-McFarlane Deep Convection scheme
 
-class ZMDeepConvection : public AtmosphereProcess
+class zm_deep_convection : public AtmosphereProcess
 {
   using KT  = ekat::KokkosTypes<DefaultDevice>;
   using ZMF = zm::Functions<Real, DefaultDevice>;
@@ -31,7 +31,7 @@ class ZMDeepConvection : public AtmosphereProcess
   public:
 
     // Constructors
-    ZMDeepConvection(const ekat::Comm& comm, const ekat::ParameterList& params);
+    zm_deep_convection(const ekat::Comm& comm, const ekat::ParameterList& params);
 
     // The type of subcomponent
     AtmosphereProcessType type() const override { return AtmosphereProcessType::Physics; }
@@ -49,10 +49,17 @@ class ZMDeepConvection : public AtmosphereProcess
 
     // define ZM process variables
     std::shared_ptr<const AbstractGrid> m_grid;
+    int m_pcols;
     int m_ncols;
     int m_nlevs;
+
+    // Structures for arguments to ZM
+    ZMF::zm_runtime_opt zm_opts;
+    ZMF::zm_input_state zm_input;
+    ZMF::zm_output_tend zm_tend;
+    ZMF::zm_output_diag zm_diag;
     
-}; // class ZMDeepConvection
+}; // class zm_deep_convection
 
 } // namespace scream
 
--- components/eamxx/src/physics/zm/fortran_bridge/zm_eamxx_bridge.hpp
+++ components/eamxx/src/physics/zm/fortran_bridge/zm_eamxx_bridge.hpp
@@ -4,13 +4,18 @@
 #include <utility>
 #include <memory>   // for shared_ptr
 
+#include "zm_functions.hpp"
+
 // Bridge functions to call fortran version of ZM functions from C++
 
 namespace scream {
 namespace zm {
 
+using ZMF = zm::Functions<Real, DefaultDevice>;
+
 // Glue functions to call fortran from from C++ with the Data struct
-void zm_eamxx_bridge_init();
+void zm_eamxx_bridge_init( Int pcols, Int pver );
+void zm_eamxx_bridge_run( ZMF::zm_input_state& zm_input );
 
 extern "C" { // _f function decls
 }
