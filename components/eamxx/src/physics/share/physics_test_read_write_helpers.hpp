6d8a47ecb93e0c42aaf5a6cb160079fefb4e75c4
Luca Bertagna <lbertag@sandia.gov>
2025-07-16 10:54:48 -0600

    EAMxx: add possibility to use ASCII format for physics baselines files

--- components/eamxx/src/physics/share/physics_test_read_write_helpers.hpp
+++ components/eamxx/src/physics/share/physics_test_read_write_helpers.hpp
@@ -5,13 +5,22 @@
 #include <vector>
 #include <type_traits>
 
+#ifdef EAMXX_ASCII_BASELINES
+#include <iomanip>
+#include <limits>
+#endif
+
 namespace scream {
 namespace impl {
 
 template <typename T>
 void read_scalars(std::ifstream& ifile, T& data)
 {
+#ifdef EAMXX_ASCII_BASELINES
+  ifile >> data;
+#else
   ifile.read(reinterpret_cast<char*>(&data),sizeof(data));
+#endif
 }
 
 template <typename T>
@@ -47,7 +56,15 @@ void read_scalars(std::ifstream& ifile, T& data, S&... tail)
 template <typename T>
 void write_scalars(std::ofstream& ofile, const T& data)
 {
+#ifdef EAMXX_ASCII_BASELINES
+  if constexpr (std::is_floating_point_v<T>) {
+    ofile << std::fixed << std::setprecision(std::numeric_limits<T>::digits10+1) << data << " ";
+  } else {
+    ofile << data << " ";
+  }
+#else
   ofile.write(reinterpret_cast<const char*>(&data),sizeof(data));
+#endif
 }
 
 template <typename T>
