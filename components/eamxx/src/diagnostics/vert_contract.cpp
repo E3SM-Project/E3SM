b5af134c65499126df83334cc1034be756cbff5b
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2025-07-15 22:54:36 -0400

    EAMxx: handle 1d wts in vert_contract

--- components/eamxx/src/diagnostics/vert_contract.cpp
+++ components/eamxx/src/diagnostics/vert_contract.cpp
@@ -33,7 +33,10 @@ void VertContractDiag::set_grids(
       m_weighting_method == "dp" || m_weighting_method == "dz" || m_weighting_method == "none",
       "Error! VertContractDiag only supports 'dp' or 'dz' or 'none' as weighting_method.\n"
       " - weighting_method: " + m_weighting_method + "\n");
-  m_diag_name = fn + "_vert_" + m_contract_method + "_" + m_weighting_method + "_weighted";
+  
+  m_diag_name = fn + "_vert_" + m_contract_method;
+  // append weighting_method to name if needed
+  m_diag_name = (m_weighting_method == "none") ? m_diag_name : m_diag_name + "_" + m_weighting_method + "weighted";
 
   auto scalar3d = g->get_3d_scalar_layout(true);
   if (m_weighting_method == "dp") {
@@ -47,9 +50,6 @@ void VertContractDiag::set_grids(
     add_field<Required>("p_mid", scalar3d, Pa, gn);
     add_field<Required>("T_mid", scalar3d, K, gn);
 
-  } else {
-    // Nothing needed, but let's override m_diag_name (trim weighting info)
-    m_diag_name = fn + "_vert_" + m_contract_method;
   }
 }
 
@@ -61,8 +61,12 @@ void VertContractDiag::initialize_impl(const RunType /*run_type*/) {
   const auto &fid    = f.get_header().get_identifier();
   const auto &layout = fid.get_layout();
 
-  EKAT_REQUIRE_MSG(layout.rank() >= 1 && layout.rank() <= 3,
+  // if dp or dz, gotta give us ncol,nlev as inputs (so rank>=2)
+  // else, can give us just nlev
+  auto min_rank = (m_weighting_method == "none") ? 1 : 2;
+  EKAT_REQUIRE_MSG(layout.rank() >= min_rank && layout.rank() <= 3,
                    "Error! Field rank not supported by VertContractDiag.\n"
+                   "For dp- and dz-weighted contractions, need at rank-2 fields.\n"
                    " - field name: " + fid.name() + "\n"
                    " - field layout: " + layout.to_string() + "\n");
   EKAT_REQUIRE_MSG(layout.tags().back() == LEV,
@@ -82,8 +86,8 @@ void VertContractDiag::initialize_impl(const RunType /*run_type*/) {
     // m_weighting = get_field_in("dz").clone("vert_contract_wts");
     m_weighting = get_field_in("pseudo_density").clone("vert_contract_wts");
   } else {
-    // no weighting needed, so we set it to 1 with layout of (col, lev)
-    FieldLayout layout_wts = {{COL, LEV}, {layout.dim(COL), layout.dim(LEV)}};
+    // no weighting needed, so we set it to 1 with layout of (lev)
+    FieldLayout layout_wts = {{LEV}, {layout.dim(LEV)}};
     FieldIdentifier f_id("vert_contract_wts", layout_wts, ekat::units::Units::nondimensional(), fid.get_grid_name());
     m_weighting = Field(f_id);
     m_weighting.allocate_view();
@@ -127,23 +131,45 @@ void VertContractDiag::scale_wts(Field &wts, const Field &wts_sum) {
   using KT = KokkosTypes<DefaultDevice>;
   using RP = typename KT::RangePolicy;
 
-  auto wts_l = wts.get_header().get_identifier().get_layout();
-  const int ncols = wts_l.dim(0);
-  const int nlevs = wts_l.dim(1);
+  auto wts_l          = wts.get_header().get_identifier().get_layout();
+  const auto wts_rank = wts_l.rank();
 
-  const auto wts_v = wts.get_view<Real**>();
-  const auto wts_sum_v = wts_sum.get_view<const Real*>();
+  if (wts_rank == 1) {
+    // no ncols, just nlevs
+    const int nlevs      = wts_l.dim(0);
+    const auto wts_v     = wts.get_view<Real *>();
+    const auto wts_sum_v = wts_sum.get_view<const Real>();
 
-  Kokkos::parallel_for("VertContractDiag::scale_wts" + m_diag_name, RP(0, nlevs*ncols),
-                       KOKKOS_LAMBDA(const int& idx) {
-    const int icol = idx / nlevs;
-    const int ilev = idx % nlevs;
-    if (wts_sum_v(icol) != 0) {
-      wts_v(icol, ilev) /= wts_sum_v(icol);
-    } else {
-      wts_v(icol, ilev) = 0; // Handle division by zero by setting to 0
-    }
-  });
+    Kokkos::parallel_for(
+        "VertContractDiag::scale_wts" + m_diag_name, RP(0, nlevs), KOKKOS_LAMBDA(const int &ilev) {
+          if (wts_sum_v() != 0) {
+            wts_v(ilev) /= wts_sum_v();
+          } else {
+            wts_v(ilev) = 0; // Handle division by zero by setting to 0
+          }
+        });
+
+  } else if (wts_rank == 2) {
+    // we have both ncols and nlevs
+    const int ncols      = wts_l.dim(0);
+    const int nlevs      = wts_l.dim(1);
+    const auto wts_v     = wts.get_view<Real **>();
+    const auto wts_sum_v = wts_sum.get_view<const Real *>();
+    Kokkos::parallel_for(
+        "VertContractDiag::scale_wts" + m_diag_name, RP(0, nlevs * ncols),
+        KOKKOS_LAMBDA(const int &idx) {
+          const int icol = idx / nlevs;
+          const int ilev = idx % nlevs;
+          if (wts_sum_v(icol) != 0) {
+            wts_v(icol, ilev) /= wts_sum_v(icol);
+          } else {
+            wts_v(icol, ilev) = 0; // Handle division by zero by setting to 0
+          }
+        });
+  } else {
+    // you shouldn't have arrived here, error out
+    EKAT_ERROR_MSG("Error!! VertContractDiag::scale_wts, unexpected field layout.");
+  }
 }
 
 void VertContractDiag::compute_diagnostic_impl() {
@@ -158,29 +184,29 @@ void VertContractDiag::compute_diagnostic_impl() {
     // TODO: for some reason the dz field keeps getting set to 0
     // TODO: as a workaround, just calculate dz here (sigh...)
     // m_weighting.update(get_field_in("dz"), 1.0, 0.0);
-    using KT  = KokkosTypes<DefaultDevice>;
-    using MT  = typename KT::MemberType;
-    using ESU = ekat::ExeSpaceUtils<typename KT::ExeSpace>;
-    using PF = scream::PhysicsFunctions<DefaultDevice>;
-    const int ncols = m_weighting.get_header().get_identifier().get_layout().dim(0);
-    const int nlevs = m_weighting.get_header().get_identifier().get_layout().dim(1);
+    using KT          = KokkosTypes<DefaultDevice>;
+    using MT          = typename KT::MemberType;
+    using ESU         = ekat::ExeSpaceUtils<typename KT::ExeSpace>;
+    using PF          = scream::PhysicsFunctions<DefaultDevice>;
+    const int ncols   = m_weighting.get_header().get_identifier().get_layout().dim(0);
+    const int nlevs   = m_weighting.get_header().get_identifier().get_layout().dim(1);
     const auto policy = ESU::get_default_team_policy(ncols, nlevs);
 
-    auto dz_v = m_weighting.get_view<Real**>();
-    auto dp_v = get_field_in("pseudo_density").get_view<const Real**>();
-    auto pm_v = get_field_in("p_mid").get_view<const Real**>();
-    auto tm_v = get_field_in("T_mid").get_view<const Real**>();
-    auto qv_v = get_field_in("qv").get_view<const Real**>();
+    auto dz_v = m_weighting.get_view<Real **>();
+    auto dp_v = get_field_in("pseudo_density").get_view<const Real **>();
+    auto pm_v = get_field_in("p_mid").get_view<const Real **>();
+    auto tm_v = get_field_in("T_mid").get_view<const Real **>();
+    auto qv_v = get_field_in("qv").get_view<const Real **>();
     Kokkos::parallel_for(
-      "Compute dz for " + m_diagnostic_output.name(), policy, KOKKOS_LAMBDA(const MT &team) {
-        const int icol = team.league_rank();
-        auto dz_icol = ekat::subview(dz_v, icol);
-        auto dp_icol = ekat::subview(dp_v, icol);
-        auto pm_icol = ekat::subview(pm_v, icol);
-        auto tm_icol = ekat::subview(tm_v, icol);
-        auto qv_icol = ekat::subview(qv_v, icol);
-        PF::calculate_dz(team, dp_icol, pm_icol, tm_icol, qv_icol, dz_icol);
-      });
+        "Compute dz for " + m_diagnostic_output.name(), policy, KOKKOS_LAMBDA(const MT &team) {
+          const int icol = team.league_rank();
+          auto dz_icol   = ekat::subview(dz_v, icol);
+          auto dp_icol   = ekat::subview(dp_v, icol);
+          auto pm_icol   = ekat::subview(pm_v, icol);
+          auto tm_icol   = ekat::subview(tm_v, icol);
+          auto qv_icol   = ekat::subview(qv_v, icol);
+          PF::calculate_dz(team, dp_icol, pm_icol, tm_icol, qv_icol, dz_icol);
+        });
   }
 
   // if dp|dz_weighted and avg, we need to scale the weighting by its 1/sum
@@ -193,4 +219,4 @@ void VertContractDiag::compute_diagnostic_impl() {
   vert_contraction<Real>(d, f, m_weighting);
 }
 
-}  // namespace scream
+} // namespace scream
