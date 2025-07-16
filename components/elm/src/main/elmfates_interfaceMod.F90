aa96dcdfdb178e7f7f65cb2c4ca3a04ed49dfcd8
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-07-16 15:59:22 -0700

    add use of transferbcin from fatesinterfacemod

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -3981,6 +3981,7 @@ end subroutine wrap_update_hifrq_hist
    ! ---------------------------------------------------------------------------------
 
    ! !USES:
+   use FatesInterfaceMod, only : TransferBCIn
    !
    ! !ARGUMENTS:
    class(hlm_fates_interface_type), intent(inout) :: this
