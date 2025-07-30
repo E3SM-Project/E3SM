342d2f0847dd1b02fb8ff78cc16c2f92891a5897
James Foucar <jgfouca@sandia.gov>
2025-07-30 17:25:04 -0600

    Encapsulate common good init data

--- components/eamxx/src/physics/gw/tests/gw_gw_cm_src_tests.cpp
+++ components/eamxx/src/physics/gw/tests/gw_gw_cm_src_tests.cpp
@@ -20,17 +20,7 @@ struct UnitWrap::UnitTest<D>::TestGwCmSrc : public UnitWrap::UnitTest<D>::Base {
     auto engine = Base::get_engine();
 
     // Set up init data
-    GwInit init_data[] = {
-          // pver, pgwv,   dc, orog_only, molec_diff, tau_0_ubc, nbot_molec, ktop, kbotbg, fcrit2, kwv
-      GwInit(  72,   20, 0.75,     false,      false,     false,         16,   60,     16,    .67, 6.28e-5),
-      GwInit(  72,   20, 0.75,     true ,      false,     true ,         16,   60,     16,    .67, 6.28e-5),
-      GwInit(  72,   20, 0.75,     false,      true ,     true ,         16,   60,     16,    .67, 6.28e-5),
-      GwInit(  72,   20, 0.75,     true ,      true ,     false,         16,   60,     16,    .67, 6.28e-5),
-    };
-
-    for (auto& d : init_data) {
-      d.randomize(engine);
-    }
+    auto init_data = get_common_init_data(engine);
 
     // Set up init data
     GwFrontInitData front_init_data[] = {
