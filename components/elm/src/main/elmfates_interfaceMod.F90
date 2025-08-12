22bbbf83489991d5e2029d03d2e5d693ea3af42a
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-08-12 12:03:24 -0700

    fix num landunits definiton

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -902,6 +902,7 @@ contains
       integer                                        :: nmaxcol
       integer                                        :: ndecomp
       integer                                        :: numg
+      integer                                        :: num_landunits_veg
 
       real(r8), allocatable :: landuse_pft_map(:,:,:)
       real(r8), allocatable :: landuse_bareground(:)
