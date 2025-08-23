2d49c79bc3c9d2697f0a152af306cf91cba2904d
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-08-23 14:32:49 -0700

    add the decomposition level to the values to be passed to FATES

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -3487,7 +3487,7 @@ end subroutine wrap_update_hifrq_hist
 
    ! !LOCAL:
    integer :: ivar                               ! array index
-   integer, parameter :: num_bc_in = 2           ! number of HLM variables
+   integer, parameter :: num_bc_in  = 3          ! number of HLM variables
    integer, parameter :: num_bc_out = 3          ! number of HLM variables
    
    ! Allocate the arrays
@@ -3498,6 +3498,11 @@ end subroutine wrap_update_hifrq_hist
    ivar = 0
 
    ! ! HLM -> FATES (bc_in)
+   ! Scalar data
+   ivar = ivar + 1
+   this%bc_in(ivar)%api_str = 'nlevdecomp'
+   this%bc_in(ivar)%hlm_var => nlevdecomp
+
    ! 2D arrays
    ivar = ivar + 1
    this%bc_in(ivar)%api_str = 'decomp_frac_moisture'
