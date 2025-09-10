b9bc3a7c1f06a98b2dc49a47a6bc6e906382356c
njeffery <njeffery@lanl.gov>
2025-09-10 11:07:52 -0700

    Adds sinking advection tendency
    Currently acts on bgc tracer (DOC)
    Used for testing sediment transport capabilities in mpas

--- components/mpas-ocean/src/ocean.cmake
+++ components/mpas-ocean/src/ocean.cmake
@@ -101,6 +101,7 @@ list(APPEND RAW_SOURCES
   core_ocean/shared/mpas_ocn_constants.F
   core_ocean/shared/mpas_ocn_config.F
   core_ocean/shared/mpas_ocn_forcing.F
+  core_ocean/shared/mpas_ocn_sediment_settling.F
   core_ocean/shared/mpas_ocn_surface_bulk_forcing.F
   core_ocean/shared/mpas_ocn_surface_land_ice_fluxes.F
   core_ocean/shared/mpas_ocn_effective_density_in_land_ice.F
