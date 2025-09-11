64b674abdcf2723170573ded112788ef51e4334d
Ryan Knox <rgknox@gmail.com>
2025-09-11 10:19:47 -0700

    removed unused call to build gpp/ar bc_out

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -1967,10 +1967,6 @@ contains
                        this%fates(nc)%bc_in(s), &
                        this%fates(nc)%bc_out(s))
 
-                  ! Update bc_out variables from the restart vectors that need to be accumulated
-                  ! from the cohort data structure restart variables
-                  call this%fates(nc)%RestartUpdateBCOut(s)
-
                end do
 
                if(use_fates_sp)then
