28103d7d92c753731e35d0f58868b5636a76365f
Luca Bertagna <lbertag@sandia.gov>
2025-09-15 14:41:10 -0600

    EAMxx: split grid tests into multiple unit tests

--- components/eamxx/src/share/grid/tests/grid_tests.cpp
+++ components/eamxx/src/share/grid/tests/grid_tests.cpp
@@ -1,115 +1,12 @@
 #include <catch2/catch.hpp>
 
-#include "share/manager/mesh_free_grids_manager.hpp"
 #include "share/grid/point_grid.hpp"
-#include "share/grid/se_grid.hpp"
-#include "share/grid/grid_utils.hpp"
 #include "share/core/eamxx_setup_random_test.hpp"
 #include "share/core/eamxx_types.hpp"
 
-#include <ekat_pack.hpp>
-
 #include <algorithm>
 
-namespace {
-
-using namespace scream;
-using namespace scream::ShortFieldTagsNames;
-
-TEST_CASE("point_grid", "") {
-
-  ekat::Comm comm(MPI_COMM_WORLD);
-
-  const int num_procs = comm.size();
-  const int num_local_cols = 128;
-  const int num_global_cols = num_local_cols*num_procs;
-  const int num_levels = 72;
-
-  auto grid = create_point_grid("my_grid", num_global_cols, num_levels, comm);
-  REQUIRE(grid->type() == GridType::Point);
-  REQUIRE(grid->name() == "my_grid");
-  REQUIRE(grid->get_num_vertical_levels() == num_levels);
-  REQUIRE(grid->get_num_local_dofs()  == num_local_cols);
-  REQUIRE(grid->get_num_global_dofs() == num_global_cols);
-  REQUIRE(grid->is_unique());
-
-  // Point grids should have (global) gids spanning the interval [min_gid, min_gid+num_global_dofs)
-  const auto max_gid = grid->get_global_max_dof_gid();
-  const auto min_gid = grid->get_global_min_dof_gid();
-  REQUIRE( (max_gid-min_gid+1)==grid->get_num_global_dofs() );
-
-  auto lid_to_idx = grid->get_lid_to_idx_map().get_view<int**,Host>();
-  for (int i = 0; i < grid->get_num_local_dofs(); ++i) {
-    REQUIRE(lid_to_idx.extent_int(1) == 1);
-    REQUIRE(i == lid_to_idx(i, 0));
-  }
-
-  auto layout = grid->get_2d_scalar_layout();
-  REQUIRE(layout.tags().size() == 1);
-  REQUIRE(layout.tag(0) == COL);
-
-  auto shallow_copy = grid->clone("shallow",true);
-  auto deep_copy    = grid->clone("deep",false);
-
-  using gid_type = AbstractGrid::gid_type;
-
-  auto grid_gids = grid->get_dofs_gids().get_view<const gid_type*,Host>();
-  auto scopy_gids = shallow_copy->get_dofs_gids().get_view<const gid_type*,Host>();
-  auto dcopy_gids = deep_copy->get_dofs_gids().get_view<const gid_type*,Host>();
-  REQUIRE (scopy_gids.data()==grid_gids.data());
-  REQUIRE (dcopy_gids.data()!=grid_gids.data());
-  for (int i=0; i<grid->get_num_local_dofs(); ++i) {
-    REQUIRE (dcopy_gids[i]==grid_gids[i]);
-  }
-
-  shallow_copy->reset_num_vertical_lev(4);
-  REQUIRE (shallow_copy->get_num_vertical_levels()==4);
-}
-
-TEST_CASE("se_grid", "") {
-  ekat::Comm comm(MPI_COMM_WORLD);
-
-  const int num_local_elems = 10;
-  const int num_gp = 4;
-  const int num_levels = 72;
-
-  auto gm = create_mesh_free_grids_manager(comm,num_local_elems,num_gp,num_levels,0);
-  gm->build_grids();
-
-  // SE grid
-  auto se_grid = gm->get_grid("SE Grid");
-
-  REQUIRE(se_grid->type() == GridType::SE);
-  REQUIRE(se_grid->name() == "SE Grid");
-  REQUIRE(se_grid->get_num_vertical_levels() == num_levels);
-  REQUIRE(se_grid->get_num_local_dofs() == num_local_elems*num_gp*num_gp);
-
-  auto layout = se_grid->get_2d_scalar_layout();
-  REQUIRE(layout.tags().size() == 3);
-  REQUIRE(layout.tag(0) == EL);
-  REQUIRE(layout.tag(1) == GP);
-  REQUIRE(layout.tag(2) == GP);
-
-  REQUIRE (se_grid->is_unique());
-
-  const auto max_gid = se_grid->get_global_max_dof_gid();
-  const auto min_gid = se_grid->get_global_min_dof_gid();
-  REQUIRE( (max_gid-min_gid+1)==se_grid->get_num_global_dofs() );
-
-  auto shallow_copy = se_grid->clone("shallow",true);
-  auto deep_copy    = se_grid->clone("deep",false);
-
-  using gid_type = AbstractGrid::gid_type;
-
-  auto grid_gids = se_grid->get_dofs_gids().get_view<const gid_type*,Host>();
-  auto scopy_gids = shallow_copy->get_dofs_gids().get_view<const gid_type*,Host>();
-  auto dcopy_gids = deep_copy->get_dofs_gids().get_view<const gid_type*,Host>();
-  REQUIRE (scopy_gids.data()==grid_gids.data());
-  REQUIRE (dcopy_gids.data()!=grid_gids.data());
-  for (int i=0; i<se_grid->get_num_local_dofs(); ++i) {
-    REQUIRE (dcopy_gids[i]==grid_gids[i]);
-  }
-}
+namespace scream {
 
 TEST_CASE ("get_owners") {
   ekat::Comm comm(MPI_COMM_WORLD);
@@ -218,4 +115,4 @@ TEST_CASE ("get_remote_pids_and_lids") {
   }
 }
 
-} // anonymous namespace
+} // namespace scream
