79b34a9796d9c5978911c09c19bfa68e713529f1
Jason Boutte <boutte3@llnl.gov>
2025-09-23 15:05:11 -0700

    Updates dane configuration

---
+++ cime_config/machines/cmake_macros/oneapi-ifx_dane.cmake
@@ -0,0 +1,9 @@
+list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/hdf5-1.14.5-oq4xvuvma5emdmqlrhhugf5f2fbl2miq/lib" )
+list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/netcdf-c-4.9.2-3a4qa33n36d4ryrne2des3653ka4rsfa/lib" )
+list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/netcdf-fortran-4.6.1-7mybbfylyzezbv2sc2rjhnkewhnyx3ge/lib" )
+list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/parallel-netcdf-1.14.0-d6xfqf4wccp4eiohquw3idjppqff7mtj/lib" )
+
+# get_cmake_property(_variableNames VARIABLES)
+# foreach (_variableName ${_variableNames})
+#     message("${_variableName}=${${_variableName}}")
+# endforeach()
