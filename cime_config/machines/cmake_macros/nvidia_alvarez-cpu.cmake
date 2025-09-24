fc7a2b58fb598f5e449e61d187c9972e5fac17b1
noel <ndkeen@lbl.gov>
2025-09-24 15:14:06 -0700

    add PR number in a comment

--- cime_config/machines/cmake_macros/nvidia_alvarez-cpu.cmake
+++ cime_config/machines/cmake_macros/nvidia_alvarez-cpu.cmake
@@ -8,13 +8,13 @@ string(APPEND CMAKE_Fortran_FLAGS_RELEASE " -g")
 
 set(HOMME_QUAD_PREC FALSE CACHE BOOL "") # nvidia does not seem to support QUAD
 
-# Appears no longer needed after PR
+# Appears no longer needed after PR7730
 # currently, there is known issue with nvidia compiler installation (not seeing all relevant include files)
 # and this is temporary work-around github.com/E3SM-Project/E3SM/issues/7003
 #string(APPEND CMAKE_CXX_FLAGS_RELEASE " --gcc-toolchain=/usr/bin/gcc")
 #string(APPEND CMAKE_CXX_FLAGS_DEBUG " --gcc-toolchain=/usr/bin/gcc")
 
-# Also appears no longer needed after PR
+# Also appears no longer needed after PR7730
 #if (compile_threaded)
 #  string(APPEND KOKKOS_OPTIONS " -DKokkos_ENABLE_OPENMP=Off") # work-around for nvidia as kokkos is not passing "-mp" for threaded build
 #endif()
