55d2f5f4b93dc4ad793c43218c1d59d3c0cc58fd
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2025-10-02 14:08:41 -0400

    EAMxx: allow diags in sci notation

--- components/eamxx/src/share/io/eamxx_io_utils.cpp
+++ components/eamxx/src/share/io/eamxx_io_utils.cpp
@@ -146,7 +146,7 @@ create_diagnostic (const std::string& diag_field_name,
   std::regex horiz_avg (generic_field + "_horiz_avg$");
   std::regex vert_contract (generic_field + "_vert_(avg|sum)(_((dp|dz)_weighted))?$");
   std::regex zonal_avg (R"()" + generic_field + R"(_zonal_avg_(\d+)_bins$)");
-  std::regex conditional_sampling (R"()" + generic_field + R"(_where_)" + generic_field + R"(_(gt|ge|eq|ne|le|lt)_([+-]?\d+(?:\.\d+)?)$)");
+  std::regex conditional_sampling (R"()" + generic_field + R"(_where_)" + generic_field + R"(_(gt|ge|eq|ne|le|lt)_([+-]?\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)$)");
   std::regex binary_ops (generic_field + "_" "(plus|minus|times|over)" + "_" + generic_field + "$");
   std::regex histogram (R"()" + generic_field + R"(_histogram_(\d+(\.\d+)?(_\d+(\.\d+)?)+)$)");
   std::regex vert_derivative (generic_field + "_(p|z)vert_derivative$");
