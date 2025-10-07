a1dbc45f59436017dfe67a968ec4c10eb77ef927
Jason Boutte <boutte3@llnl.gov>
2025-10-07 15:47:38 -0700

    Adds missing modules and removes hard coded paths

--- cime_config/machines/cmake_macros/oneapi-ifx_dane.cmake
+++ cime_config/machines/cmake_macros/oneapi-ifx_dane.cmake
@@ -1,7 +1,7 @@
-list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/hdf5-1.14.5-oq4xvuvma5emdmqlrhhugf5f2fbl2miq/lib" )
-list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/netcdf-c-4.9.2-3a4qa33n36d4ryrne2des3653ka4rsfa/lib" )
-list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/netcdf-fortran-4.6.1-7mybbfylyzezbv2sc2rjhnkewhnyx3ge/lib" )
-list(APPEND CMAKE_BUILD_RPATH "/usr/workspace/e3sm/spack/dane/intel/libs/linux-sapphirerapids/parallel-netcdf-1.14.0-d6xfqf4wccp4eiohquw3idjppqff7mtj/lib" )
+list(APPEND CMAKE_BUILD_RPATH "$ENV{HDF5_ROOT}/lib" )
+list(APPEND CMAKE_BUILD_RPATH "$ENV{NETCDF_C_PATH}/lib" )
+list(APPEND CMAKE_BUILD_RPATH "$ENV{NETCDF_FORTRAN_PATH}/lib" )
+list(APPEND CMAKE_BUILD_RPATH "$ENV{PNETCDF_PATH}/lib" )
 
 # get_cmake_property(_variableNames VARIABLES)
 # foreach (_variableName ${_variableNames})
