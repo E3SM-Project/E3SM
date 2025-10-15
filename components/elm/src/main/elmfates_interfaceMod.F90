30b0212c3bd6dcc32278abe8c98c5ddd74519cd1
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-10-15 15:55:13 -0700

    update fluxintolitterpools call

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -2061,8 +2061,7 @@ contains
                   ! output boundary condition structures. Note: this is called
                   ! internally in fates dynamics as well.
 
-                  call FluxIntoLitterPools(this%fates(nc)%sites(s), &
-                       this%fates(nc)%bc_in(s), this%fates(nc)%bc_out(s))
+                  call FluxIntoLitterPools(this%fates(nc)%sites(s))
                        
                end do
 
@@ -2320,9 +2319,8 @@ contains
               ! output boundary condition structures. Note: this is called
               ! internally in fates dynamics as well.
 
-              call FluxIntoLitterPools(this%fates(nc)%sites(s), &
-                   this%fates(nc)%bc_in(s), &
-                   this%fates(nc)%bc_out(s))
+              call FluxIntoLitterPools(this%fates(nc)%sites(s))
+
            end do
 
            ! ------------------------------------------------------------------------
