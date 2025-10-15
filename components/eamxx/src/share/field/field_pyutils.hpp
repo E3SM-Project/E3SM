9db8cdfc4de2073171d73772d01f37e20c13f105
Luca Bertagna <lbertag@sandia.gov>
2025-10-15 11:21:44 -0600

    BKP: this commit will be amended/erased/rewritten

--- components/eamxx/src/share/field/field_pyutils.hpp
+++ components/eamxx/src/share/field/field_pyutils.hpp
@@ -9,11 +9,15 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 
+#ifdef EAMXX_HAS_TORCH
+#include <torch/torch.h>
+#endif
+
 namespace scream
 {
 
-template<typename T,HostOrDevice HD>
-void get_ptr_and_strides (std::vector<ssize_t>& strides, const void*& p, const Field& f)
+template<typename Strides, typename T,HostOrDevice HD>
+void get_ptr_and_strides (Strides& strides, const void*& p, const Field& f)
 {
   const auto& fl  = f.get_header().get_identifier().get_layout();
   strides.resize(fl.rank());
@@ -97,6 +101,73 @@ pybind11::array create_py_field (const Field& f)
 
   return py::array(dt, shape, strides, data, py::none());
 }
+
+#ifdef EAMXX_HAS_PYTORCH
+
+torch::ScalarType get_torch_dt(const DataType dt)
+{
+  switch (dt) {
+    case DataType::IntType:
+      return torch::kInt32;
+    case DataType::FloatType:
+      return torch::kFloat32;
+    case DataType::DoubleType:
+      return torch::kFloat64;
+    default:
+      EKAT_ERROR_MSG ("Unrecognized/unsupported data type.\n");
+  }
+}
+
+template<HostOrDevice HD>
+torch::Tensor create_torch_tensor (const Field& f)
+{
+  auto t_dev = torch::kCPU;
+  int dev_idx = -1;
+  if (HD==Device and ekat::OnGpu<DefaultDevice::execution_space>::value) {
+#ifdef KOKKOS_ENABLE_CUDA
+    t_dev = torch::kCUDA;
+#elif defined(KOKKOS_ENABLE_HIP)
+    t_dev = torch::kHIP;
+#else
+#error "Unrecognized/unsupported device for torch interoperability"
+#endif
+    dev_idx = DefaultDevice::execution_space().impl_idx();
+  }
+  torch::ScalarType dt;
+
+  const auto& fh  = f.get_header();
+  const auto& fid = fh.get_identifier();
+  for (auto d : fid.get_layout().dims()) {
+    shape.push_back(d);
+  }
+
+  // TODO: must check if strides are bytes or number of entries
+  std::vector<int> strides
+  switch (fid.data_type()) {
+    case DataType::IntType:
+      dt = torch::kInt32;
+      get_ptr_and_strides<int,HD>(strides,data,f);
+      break;
+    case DataType::FloatType:
+      dt = torch::kFloat32;
+      get_ptr_and_strides<float,HD>(strides,data,f);
+      break;
+    case DataType::DoubleType:
+      dt = torch::kFloat64;
+      get_ptr_and_strides<double,HD>(strides,data,f);
+      break;
+    default:
+      EKAT_ERROR_MSG ("Unrecognized/unsupported data type.\n");
+  }
+
+  auto options = torch::TensorOptions()
+    .dtype(get_torch_dt(f.data_type()))
+    .layout(torch::kStrided)
+    .device(t_dev, dev_ids);
+
+  auto tensor = torch::from_blob(data,sizes,strides,options);
+}
+#endif
 
 } // namespace scream
 
