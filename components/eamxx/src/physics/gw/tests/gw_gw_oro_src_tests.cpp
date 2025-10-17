8ce6705ef940d613b632e07555340eb766698711
James Foucar <jgfouca@sandia.gov>
2025-10-17 13:45:42 -0600

    Fixes works on CPU

--- components/eamxx/src/physics/gw/tests/gw_gw_oro_src_tests.cpp
+++ components/eamxx/src/physics/gw/tests/gw_gw_oro_src_tests.cpp
@@ -36,7 +36,7 @@ struct UnitWrap::UnitTest<D>::TestGwOroSrc : public UnitWrap::UnitTest<D>::Base 
     // Generate random input data
     // Alternatively, you can use the baseline_data construtors/initializer lists to hardcode data
     for (auto& d : baseline_data) {
-      d.randomize(engine);
+      d.randomize(engine, { {d.sgh, {2, 7}} });
     }
 
     // Create copies of data for use by test. Needs to happen before read calls so that
@@ -57,41 +57,52 @@ struct UnitWrap::UnitTest<D>::TestGwOroSrc : public UnitWrap::UnitTest<D>::Base 
 
     // Get data from test
     for (auto& d : test_data) {
-      gw_oro_src(d);
+      if (this->m_baseline_action == GENERATE) {
+        gw_oro_src_f(d);
+      }
+      else {
+        gw_oro_src(d);
+      }
     }
+
+    // We need a tolerance since the order of operations is different from f90.
+    // This tol can be removed once we are no longer using
+    // fortran to generate baselines.
+    const auto margin = std::numeric_limits<Real>::epsilon() *
+      (ekat::is_single_precision<Real>::value ? 1000 : 1);
+
 
     // Verify BFB results, all data should be in C layout
     if (SCREAM_BFB_TESTING && this->m_baseline_action == COMPARE) {
       for (Int i = 0; i < num_runs; ++i) {
         GwOroSrcData& d_baseline = baseline_data[i];
         GwOroSrcData& d_test = test_data[i];
+        REQUIRE(d_baseline.total(d_baseline.tau) == d_test.total(d_test.tau));
         for (Int k = 0; k < d_baseline.total(d_baseline.tau); ++k) {
-          REQUIRE(d_baseline.total(d_baseline.tau) == d_test.total(d_test.tau));
-          REQUIRE(d_baseline.tau[k] == d_test.tau[k]);
+          REQUIRE(d_baseline.tau[k] == Approx(d_test.tau[k]).margin(margin));
         }
+        REQUIRE(d_baseline.total(d_baseline.ubm) == d_test.total(d_test.ubm));
         for (Int k = 0; k < d_baseline.total(d_baseline.ubm); ++k) {
-          REQUIRE(d_baseline.total(d_baseline.ubm) == d_test.total(d_test.ubm));
-          REQUIRE(d_baseline.ubm[k] == d_test.ubm[k]);
+          REQUIRE(d_baseline.ubm[k] == Approx(d_test.ubm[k]).margin(margin));
         }
+        REQUIRE(d_baseline.total(d_baseline.ubi) == d_test.total(d_test.ubi));
         for (Int k = 0; k < d_baseline.total(d_baseline.ubi); ++k) {
-          REQUIRE(d_baseline.total(d_baseline.ubi) == d_test.total(d_test.ubi));
-          REQUIRE(d_baseline.ubi[k] == d_test.ubi[k]);
+          REQUIRE(d_baseline.ubi[k] == Approx(d_test.ubi[k]).margin(margin));
         }
+        REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.xv));
+        REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.yv));
+        REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.src_level));
+        REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.tend_level));
         for (Int k = 0; k < d_baseline.total(d_baseline.xv); ++k) {
-          REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.xv));
-          REQUIRE(d_baseline.xv[k] == d_test.xv[k]);
-          REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.yv));
-          REQUIRE(d_baseline.yv[k] == d_test.yv[k]);
-          REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.src_level));
+          REQUIRE(d_baseline.xv[k] == Approx(d_test.xv[k]).margin(margin));
+          REQUIRE(d_baseline.yv[k] == Approx(d_test.yv[k]).margin(margin));
           REQUIRE(d_baseline.src_level[k] == d_test.src_level[k]);
-          REQUIRE(d_baseline.total(d_baseline.xv) == d_test.total(d_test.tend_level));
           REQUIRE(d_baseline.tend_level[k] == d_test.tend_level[k]);
         }
+        REQUIRE(d_baseline.total(d_baseline.c) == d_test.total(d_test.c));
         for (Int k = 0; k < d_baseline.total(d_baseline.c); ++k) {
-          REQUIRE(d_baseline.total(d_baseline.c) == d_test.total(d_test.c));
           REQUIRE(d_baseline.c[k] == d_test.c[k]);
         }
-
       }
     }
     else if (this->m_baseline_action == GENERATE) {
