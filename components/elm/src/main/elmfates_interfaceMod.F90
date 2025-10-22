e7573badf4737c48fc288ac4a5a906f76b905edf
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-10-21 23:08:24 -0700

    add interface variable initialization update during cold start

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -2106,6 +2106,9 @@ contains
            ! Register interface variables
            call this%RegisterInterfaceVariablesColdStart(nc, canopystate_inst)
 
+           ! Update the interface variables
+           call this%fates(nc)%UpdateInterfaceVariables(initialize=.true.)
+
            call get_clump_bounds(nc, bounds_clump)
 
            do s = 1,this%fates(nc)%nsites
