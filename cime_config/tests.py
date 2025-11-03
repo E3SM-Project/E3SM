dcf1e893ead5964e159d9b31b198db898c2ae1bd
Fengming Yuan <fmyuan@gmail.com>
2025-11-03 12:04:33 -0500

    Allow a test of Arctic PFT case, one of 6 ecosystems from B. Sulman et al. (2021), to be included in ELM land developer test suites.

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -55,6 +55,7 @@ _TESTS = {
             "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_codetest_I1850CNPRDCTCBC",
             "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_codetest_I1850CNPRDCTCBC",
             "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_onsetgdd_I1850CNPRDCTCBC",
+            "ERS_Ly5.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_maxpft12_I1850CNPRDCTCBC",
             "ERS.1x1_icycape.I1850GSWCNPRDCTCBC.elm-polygonal_tundra",
             "SMS_Ly1.ELM_USRDAT.I1850CNPRDCTCBC.elm-kilocraft",
             )
