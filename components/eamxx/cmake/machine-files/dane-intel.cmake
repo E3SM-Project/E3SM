a97469f81bb327177c81d42fbc16499d4973c161
Aaron Donahue <donahue.aaron@gmail.com>
2025-11-12 11:10:13 -0800

    Livermore machines config, move ML environment
    This commit moves the ML environment that is loaded on livermore
    computing resources to the new "e3sm" user workspace.  The old
    "climdat" workspace is deprecated.

--- components/eamxx/cmake/machine-files/dane-intel.cmake
+++ components/eamxx/cmake/machine-files/dane-intel.cmake
@@ -3,4 +3,4 @@ set(CMAKE_EXE_LINKER_FLAGS "-L/usr/tce/packages/mkl/mkl-2022.1.0/lib/intel64/ -q
 set(PYTHON_LIBRARIES "/usr/lib64/libpython3.9.so.1.0" CACHE STRING "" FORCE)
 option (SCREAM_ENABLE_ML_CORRECTION "Whether to enable ML correction parametrization" ON)
 set(HDF5_DISABLE_VERSION_CHECK 1 CACHE STRING "" FORCE)
-execute_process(COMMAND source /usr/WS1/climdat/python_venv/3.9.2/screamML/bin/activate)
+execute_process(COMMAND source /usr/WS1/e3sm/python_venv/3.9.2/screamML/bin/activate)
