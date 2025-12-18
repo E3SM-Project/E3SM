5b591726803dbbe2a2888a12cd3c6c9b475e613a
Jason Boutte <boutte3@llnl.gov>
2025-12-18 10:21:37 -0800

    Update LC dane machine
    Update LC dane machine

---
+++ cime_config/machines/Depends.dane.oneapi-ifx.cmake
@@ -0,0 +1,6 @@
+# compile mpas_seaice_core_interface.f90 with ifort, not ifx
+if (NOT MPILIB STREQUAL "openmpi")
+	e3sm_add_flags("${CMAKE_BINARY_DIR}/core_seaice/model_forward/mpas_seaice_core_interface.f90" "-fc=ifort")
+	e3sm_add_flags("${CMAKE_BINARY_DIR}/core_landice/mode_forward/mpas_li_core_interface.f90" "-fc=ifort")
+endif()
+
