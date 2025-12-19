62bf7b0f9cd860b3903d8fdb96496e8bdc6022cb
noel <ndkeen@lbl.gov>
2025-12-19 11:14:17 -0800

    correct a formatting issue in a comment

--- cime_config/machines/scripts/pm-gpu_set_affinity_npergpu.sh
+++ cime_config/machines/scripts/pm-gpu_set_affinity_npergpu.sh
@@ -4,6 +4,6 @@ tasks_per_node=$1
 tasks_per_gpu=$(( ${tasks_per_node} / 4 ))
 gpu=$(( (${SLURM_LOCALID} / ${tasks_per_gpu}) % 4 ))
 export CUDA_VISIBLE_DEVICES=$gpu
-#echo “RANK= ${SLURM_PROCID} LOCAL_RANK= ${SLURM_LOCALID} gpu= ${gpu}”
+#printf '?RANK= %s LOCAL_RANK= %s gpu= %s?\n' ${SLURM_PROCID} ${SLURM_LOCALID} ${gpu}
 shift
 "$@"
