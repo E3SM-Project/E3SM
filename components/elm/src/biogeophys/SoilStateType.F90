077be52a51308f2d5358e145e3658b89866553c9
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-01-05 10:51:17 -0800

    Updates multiple conditionals for initialization
    The conitionals are updated to check the value for the column,
    instead of the value for landunit.

--- components/elm/src/biogeophys/SoilStateType.F90
+++ components/elm/src/biogeophys/SoilStateType.F90
@@ -722,7 +722,7 @@ contains
                    this%cellorg_col(c,lev)  = om_frac*organic_max
                 end if
 
-             else if (lun_pp%itype(l) /= istdlak) then  ! soil columns of both urban and non-urban types
+             else if (.not. col_pp%is_lake(c)) then  ! soil columns of both urban and non-urban types
 
                 if (lun_pp%urbpoi(l)) then
                    om_frac = 0._r8 ! No organic matter for urban
