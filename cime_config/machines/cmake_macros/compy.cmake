618cc054a8b313275f3ef2cd3e511ee190256f63
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-01-06 12:38:04 -0800

    Adds Python-related include dir and lib

--- cime_config/machines/cmake_macros/compy.cmake
+++ cime_config/machines/cmake_macros/compy.cmake
@@ -1,1 +1,3 @@
 string(APPEND KOKKOS_OPTIONS " -DKokkos_ENABLE_OPENMP=On -DKokkos_ENABLE_AGGRESSIVE_VECTORIZATION=On -DKokkos_ENABLE_EXPLICIT_INSTANTIATION=Off -DKokkos_ENABLE_DEPRECATED_CODE_4=Off")
+string(APPEND CMAKE_C_FLAGS " -I/share/apps/python/anaconda3-2020.02/include/python3.7m")
+string(APPEND SLIBS " /share/apps/python/anaconda3-2020.02/lib/libpython3.7m.a -lutil -lpthread -ldl -lm")
