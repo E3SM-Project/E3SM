a00f702c888e57c5bf535ee3b3ef6008752e1326
Gregory Lemieux <glemieux@users.noreply.github.com>
2026-01-07 15:03:52 -0800

    add timestep conversion

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -1494,6 +1494,38 @@ contains
       
       call this%fates(nc)%UpdateLitterFluxes(dtime)
 
+      ! Scale the decomposition pools post accumulation at the column level
+      do c = bounds_clump%begc,bounds_clump%endc
+
+         if (col_pp%is_fates(c)) then
+        col_cf%decomp_cpools_sourcesink(c,1:nlevdecomp,i_met_lit) =  &
+             col_cf%decomp_cpools_sourcesink(c,1:nlevdecomp,i_met_lit) * dtime
+        col_cf%decomp_cpools_sourcesink(c,1:nlevdecomp,i_cel_lit) =  &
+             col_cf%decomp_cpools_sourcesink(c,1:nlevdecomp,i_cel_lit) * dtime
+        col_cf%decomp_cpools_sourcesink(c,1:nlevdecomp,i_lig_lit) =  &
+             col_cf%decomp_cpools_sourcesink(c,1:nlevdecomp,i_lig_lit) * dtime
+      
+         select case(fates_parteh_mode)
+            case (prt_cnp_flex_allom_hyp )
+
+               col_pf%decomp_ppools_sourcesink(c,1:nlevdecomp,i_met_lit) =  &
+                    col_pf%decomp_ppools_sourcesink(c,1:nlevdecomp,i_met_lit) * dtime
+               col_pf%decomp_ppools_sourcesink(c,1:nlevdecomp,i_cel_lit) =  &
+                    col_pf%decomp_ppools_sourcesink(c,1:nlevdecomp,i_cel_lit) * dtime
+               col_pf%decomp_ppools_sourcesink(c,1:nlevdecomp,i_lig_lit) =  &
+                    col_pf%decomp_ppools_sourcesink(c,1:nlevdecomp,i_lig_lit) * dtime
+
+               col_nf%decomp_npools_sourcesink(c,1:nlevdecomp,i_met_lit) =  &
+                    col_nf%decomp_npools_sourcesink(c,1:nlevdecomp,i_met_lit) * dtime
+               col_nf%decomp_npools_sourcesink(c,1:nlevdecomp,i_cel_lit) =  &
+                    col_nf%decomp_npools_sourcesink(c,1:nlevdecomp,i_cel_lit) * dtime
+               col_nf%decomp_npools_sourcesink(c,1:nlevdecomp,i_lig_lit) =  &
+                    col_nf%decomp_npools_sourcesink(c,1:nlevdecomp,i_lig_lit) * dtime
+
+         end select
+
+         end if
+      end do
    end subroutine UpdateLitterFluxes
 
    !--------------------------------------------------------------------------------------
