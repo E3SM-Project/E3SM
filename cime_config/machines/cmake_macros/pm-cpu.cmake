4c9f9332cd973b3d5595cbb9151d5593a6eea583
Gautam Bisht <gautam.bisht@pnnl.gov>
2026-01-12 09:56:02 -0800

    Updates settings for perlmutter-cpu

--- cime_config/machines/cmake_macros/pm-cpu.cmake
+++ cime_config/machines/cmake_macros/pm-cpu.cmake
@@ -1,1 +1,4 @@
 set(KOKKOS_USE_EKAT_MACH_FILE "TRUE")
+string(APPEND CMAKE_C_FLAGS " -I/global/common/software/nersc/pe/conda-envs/24.1.0/python-3.11/nersc-python/include/python3.11")
+string(APPEND SLIBS " -L/global/common/software/nersc/pe/conda-envs/24.1.0/python-3.11/nersc-python/lib -lpython3.11")
+set(Python_EXECUTABLE "/global/common/software/nersc/pe/conda-envs/24.1.0/python-3.11/nersc-python/bin/python")
