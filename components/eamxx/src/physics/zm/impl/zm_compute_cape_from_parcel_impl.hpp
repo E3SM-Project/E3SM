fccf0ee9ca2e6a9814f8e21eb44abcead8561a67
James Foucar <jgfouca@sandia.gov>
2026-02-19 13:44:29 -0700

    AI attempt 1

--- components/eamxx/src/physics/zm/impl/zm_compute_cape_from_parcel_impl.hpp
+++ components/eamxx/src/physics/zm/impl/zm_compute_cape_from_parcel_impl.hpp
@@ -16,6 +16,7 @@ KOKKOS_FUNCTION
 void Functions<S,D>::compute_cape_from_parcel(
   // Inputs
   const MemberType& team,
+  const Workspace& workspace,
   const Int& pver, // number of mid-point vertical levels
   const Int& pverp, // number of interface vertical levels
   const Int& num_cin, // num of negative buoyancy regions that are allowed before the conv. top and CAPE calc are completed
@@ -28,6 +29,8 @@ void Functions<S,D>::compute_cape_from_parcel(
   const Int& msemax_klev, // index of max MSE at parcel launch level
   const Real& lcl_pmid, // lifting condensation level (LCL) pressure
   const Int& lcl_klev, // lifting condensation level (LCL) index
+  const Real& tiedke_add, // tiedke adjustment parameter
+  const Real& lcl_pressure_threshold, // LCL pressure threshold
   // Inputs/Outputs
   const uview_1d<Real>& parcel_qsat, // parcel saturation mixing ratio
   const uview_1d<Real>& parcel_temp, // parcel temperature
@@ -35,6 +38,82 @@ void Functions<S,D>::compute_cape_from_parcel(
   Int& eql_klev, // index of equilibrium level (i.e. cloud top)
   Real& cape) // convective available potential energy
 {
+  // Allocate temporary arrays
+  uview_1d<Real> buoyancy1d, cape_tmp1d, eql_klev_tmp1d;
+  workspace.template take_many_contiguous_unsafe<3>(
+    {"buoyancy", "cape_tmp", "eql_klev_tmp"},
+    {&buoyancy1d, &cape_tmp1d, &eql_klev_tmp1d});
+
+  // Create 2D views from 1D workspace arrays
+  uview_1d<Real> buoyancy(buoyancy1d.data(), pver);
+  uview_1d<Real> cape_tmp(cape_tmp1d.data(), num_cin);
+  uview_1d<Int> eql_klev_tmp(reinterpret_cast<Int*>(eql_klev_tmp1d.data()), num_cin);
+
+  Int neg_buoyancy_cnt = 0;
+
+  // Initialize variables
+  eql_klev = pver - 1;
+  cape = 0.0;
+
+  Kokkos::parallel_for(Kokkos::TeamThreadRange(team, num_cin), [&] (const Int& n) {
+    eql_klev_tmp(n) = pver - 1;
+    cape_tmp(n) = 0.0;
+  });
+  team.team_barrier();
+
+  Kokkos::parallel_for(Kokkos::TeamThreadRange(team, pver), [&] (const Int& k) {
+    buoyancy(k) = 0.0;
+  });
+  team.team_barrier();
+
+  // Calculate buoyancy
+  Kokkos::parallel_for(Kokkos::TeamThreadRange(team, num_msg + 1, pver), [&] (const Int& k) {
+    // Define buoyancy from launch level to equilibrium level
+    if (k <= msemax_klev && lcl_pmid >= lcl_pressure_threshold) {
+      buoyancy(k) = parcel_vtemp(k) - tv(k) + tiedke_add;
+    } else {
+      parcel_qsat(k) = sp_humidity(k);
+      parcel_temp(k) = temperature(k);
+      parcel_vtemp(k) = tv(k);
+    }
+  });
+  team.team_barrier();
+
+  // Find convective equilibrium level accounting for negative buoyancy levels
+  for (Int k = num_msg + 2; k < pver; ++k) {
+    if (k < lcl_klev && lcl_pmid >= lcl_pressure_threshold) {
+      if (buoyancy(k + 1) > 0.0 && buoyancy(k) <= 0.0) {
+        neg_buoyancy_cnt = ekat::impl::min(num_cin, neg_buoyancy_cnt + 1);
+        eql_klev_tmp(neg_buoyancy_cnt - 1) = k;
+      }
+    }
+  }
+
+  // Integrate buoyancy to obtain possible CAPE values
+  for (Int n = 0; n < num_cin; ++n) {
+    Kokkos::parallel_reduce(Kokkos::TeamThreadRange(team, num_msg + 1, pver),
+      [&] (const Int& k, Real& cape_n) {
+        if (lcl_pmid >= lcl_pressure_threshold &&
+            k <= msemax_klev && k > eql_klev_tmp(n)) {
+          cape_n += ZMC::rdair * buoyancy(k) * std::log(pint(k + 1) / pint(k));
+        }
+      }, cape_tmp(n));
+  }
+  team.team_barrier();
+
+  // Find maximum cape from all possible tentative CAPE values
+  for (Int n = 0; n < num_cin; ++n) {
+    if (cape_tmp(n) > cape) {
+      cape = cape_tmp(n);
+      eql_klev = eql_klev_tmp(n);
+    }
+  }
+
+  // Apply limiter to ensure CAPE is positive
+  cape = ekat::impl::max(cape, 0.0);
+
+  workspace.template release_many_contiguous<3>(
+    {&buoyancy1d, &cape_tmp1d, &eql_klev_tmp1d});
 }
 
 } // namespace zm
