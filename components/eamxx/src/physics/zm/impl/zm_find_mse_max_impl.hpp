2f2fee92dcdcff8869d97b15f0ef8906dfb2efbb
James Foucar <jgfouca@sandia.gov>
2026-02-20 14:35:20 -0700

    AI attempt at find_mse_max

--- components/eamxx/src/physics/zm/impl/zm_find_mse_max_impl.hpp
+++ components/eamxx/src/physics/zm/impl/zm_find_mse_max_impl.hpp
@@ -16,6 +16,8 @@ KOKKOS_FUNCTION
 void Functions<S,D>::find_mse_max(
   // Inputs
   const MemberType& team,
+  const Workspace& workspace,
+  const ZmRuntimeOpt& runtime_opt,
   const Int& pver, // number of mid-point vertical levels
   const Int& num_msg, // number of missing moisture levels at the top of model
   const Int& msemax_top_k, // upper limit index of max MSE search
@@ -27,6 +29,45 @@ void Functions<S,D>::find_mse_max(
   Int& msemax_klev, // index of max MSE at parcel launch level
   Real& mse_max_val) // value of max MSE at parcel launch level)
 {
+  //----------------------------------------------------------------------------
+  // Purpose: find level of max moist static energy for parcel initialization
+  //----------------------------------------------------------------------------
+  // Local variables
+  constexpr Real pergro_rhd_threshold = -1.e-4; // MSE difference threshold for perturbation growth test
+
+  //----------------------------------------------------------------------------
+  // initialize values
+  mse_max_val = 0.0;
+  msemax_klev = 0;
+  const Int bot_layer = pver - 1 - runtime_opt.mx_bot_lyr_adj; // set lower limit to search for launch level with max MSE
+
+  //----------------------------------------------------------------------------
+  // Use parallel_reduce to find max moist static energy
+  Kokkos::parallel_reduce(Kokkos::TeamThreadRange(team, num_msg + 1, bot_layer + 1),
+    [&] (const Int& k, Real& max_mse, Int& max_klev) {
+      // calculate moist static energy
+      const Real mse_env = PC::Cpair.value * temperature(k) +
+                           PC::gravit.value * zmid(k) +
+                           PC::LatVap.value * sp_humidity(k);
+
+      if (pergro_active) {
+        // Reset max moist static energy level when relative difference exceeds threshold
+        const Real pergro_rhd = (mse_env - max_mse) / (mse_env + max_mse);
+        if (k >= msemax_top_k && pergro_rhd > pergro_rhd_threshold) {
+          max_mse = mse_env;
+          max_klev = k;
+        }
+      } else {
+        // find level and value of max moist static energy
+        if (k >= msemax_top_k && mse_env > max_mse) {
+          max_mse = mse_env;
+          max_klev = k;
+        }
+      }
+    },
+    Kokkos::Max<Real>(mse_max_val), Kokkos::Max<Int>(msemax_klev)
+  );
+  team.team_barrier();
 }
 
 } // namespace zm
