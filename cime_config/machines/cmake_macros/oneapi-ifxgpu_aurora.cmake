afa1791ba892f9073e6e190fa00a080e688ff079
Azamat Mametjanov <azamat@mcs.anl.gov>
2026-02-24 09:01:49 +0000

    Add mcmodel=large for no memory restriction on code or data in exe
    Also,
    - decrease exe-size with -fno-system-debug --offload-compress
    - enable huge device code with -flink-huge-device-code
    - make kokkos gen PICode
    - clean-up -device 12.60.7 -> -device pvc
    - rm deprecated -lsycl

--- cime_config/machines/cmake_macros/oneapi-ifxgpu_aurora.cmake
+++ cime_config/machines/cmake_macros/oneapi-ifxgpu_aurora.cmake
@@ -4,9 +4,9 @@ if (compile_threaded)
   string(APPEND CMAKE_EXE_LINKER_FLAGS " -fiopenmp -fopenmp-targets=spir64")
 endif()
 
-string(APPEND KOKKOS_OPTIONS " -DCMAKE_CXX_STANDARD=17 -DKokkos_ENABLE_SERIAL=On -DKokkos_ARCH_INTEL_PVC=On -DKokkos_ENABLE_SYCL=On -DKokkos_ENABLE_EXPLICIT_INSTANTIATION=Off")
+string(APPEND KOKKOS_OPTIONS " -DCMAKE_CXX_STANDARD=17 -DKokkos_ENABLE_SERIAL=On -DKokkos_ARCH_INTEL_PVC=On -DKokkos_ENABLE_SYCL=On -DKokkos_ENABLE_EXPLICIT_INSTANTIATION=Off -DCMAKE_POSITION_INDEPENDENT_CODE=ON")
 string(APPEND SYCL_FLAGS " -\-intel -fsycl -fsycl-targets=spir64_gen -mlong-double-64 ")
-string(APPEND OMEGA_SYCL_EXE_LINKER_FLAGS " -Xsycl-target-backend \"-device 12.60.7\" ")
+string(APPEND OMEGA_SYCL_EXE_LINKER_FLAGS " -Xsycl-target-backend \"-device pvc\" ")
 
 # Let's start with the best case: using device buffers in MPI calls by default.
 # This is paired with MPIR_CVAR_ENABLE_GPU=1 in config_machines.xml. If this
