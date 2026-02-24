96bcbb44892ce5a6e7e99da38bf051ae30468d09
Azamat Mametjanov <azamat@mcs.anl.gov>
2026-02-23 21:24:38 -0600

    Update Bebop machine config
    Update modules, batching, cmake, PEs.

--- cime_config/machines/cmake_macros/gnu_bebop.cmake
+++ cime_config/machines/cmake_macros/gnu_bebop.cmake
@@ -1,3 +1,8 @@
 if (COMP_NAME STREQUAL gptl)
   string(APPEND CPPDEFS " -DHAVE_SLASHPROC")
 endif()
+string(APPEND CMAKE_C_FLAGS_RELEASE " -O2")
+string(APPEND CMAKE_Fortran_FLAGS_RELEASE " -O2")
+set(PIO_FILESYSTEM_HINTS "gpfs")
+string(APPEND SLIBS " /lcrc/group/e3sm/soft/bebop/netlib-lapack/3.12.0/gcc-13.2.0/liblapack.a /lcrc/group/e3sm/soft/bebop/netlib-lapack/3.12.0/gcc-13.2.0/libblas.a")
+
