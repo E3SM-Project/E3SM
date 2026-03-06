b6214c1507b70f2f7644b1c6b0cc68aa4866f6ee
noel <ndkeen@lbl.gov>
2026-03-06 11:57:19 -0800

    Add a flag for one source file and only for intel/debug/pm-cpu

--- cime_config/machines/Depends.muller-cpu.intel.cmake
+++ cime_config/machines/Depends.muller-cpu.intel.cmake
@@ -1,13 +1,11 @@
-# For this file, we see internal compiler error with ifx (via intel-oneapi module) on pm-cpu with -O2
-# Commenting for now as we are using intel module which is not seeing build issue
-#set(NOOPT
-#  eam/src/physics/cam/debug_info.F90)
+# https://github.com/E3SM-Project/E3SM/issues/8036
+set(INITZERO eam/src/physics/cosp2/external/src/simulator/MISR_simulator/MISR_simulator.F90)
 
-#if (NOT DEBUG)
-#  foreach(ITEM IN LISTS NOOPT)
-#    e3sm_add_flags("${ITEM}" "-O0")
-#  endforeach()
-#endif()
+if (DEBUG)
+  foreach(ITEM IN LISTS INITZERO)
+    e3sm_add_flags("${ITEM}" "-init=zero")
+  endforeach()
+endif()
 
 
 
