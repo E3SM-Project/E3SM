1466c387a1e8eeaa481f63ad2f4ee53f970bd969
James Foucar <jgfouca@sandia.gov>
2026-03-09 15:23:52 -0600

    Fixes and remove state_v stuff that was never used

--- components/eamxx/src/physics/zm/impl/zm_zm_conv_mcsp_calculate_shear_impl.hpp
+++ components/eamxx/src/physics/zm/impl/zm_zm_conv_mcsp_calculate_shear_impl.hpp
@@ -29,9 +29,6 @@ void Functions<S,D>::zm_conv_mcsp_calculate_shear(
 
   // Local variables
   Real storm_u = 0.0;         // u wind at storm reference level set by MCSP_storm_speed_pref
-  Real storm_v = 0.0;         // v wind at storm reference level set by MCSP_storm_speed_pref
-  Real storm_u_shear = 0.0;   // u shear at storm reference level set by MCSP_storm_speed_pref
-  Real storm_v_shear = 0.0;   // v shear at storm reference level set by MCSP_storm_speed_pref
 
   //----------------------------------------------------------------------------
   // Interpolate wind to pressure level specified by MCSP_storm_speed_pref
@@ -40,7 +37,7 @@ void Functions<S,D>::zm_conv_mcsp_calculate_shear(
   Int k_below = pver - 1; // default to lowest level
   Kokkos::parallel_reduce(Kokkos::TeamVectorRange(team, pver - 1),
     [&] (const Int& k, Int& min_k) {
-      if (state_pmid(k) >= ZMC::MCSP_storm_speed_pref && state_pmid(k + 1) < ZMC::MCSP_storm_speed_pref) {
+      if (state_pmid(k) < ZMC::MCSP_storm_speed_pref && state_pmid(k+1) >= ZMC::MCSP_storm_speed_pref) {
         if (k < min_k) {
           min_k = k;
         }
@@ -51,29 +48,25 @@ void Functions<S,D>::zm_conv_mcsp_calculate_shear(
 
   // Linear interpolation
   Kokkos::single(Kokkos::PerTeam(team), [&] () {
-    if (k_below < pver - 1) {
-      const Real p_above = state_pmid(k_below);
-      const Real p_below = state_pmid(k_below + 1);
-      const Real weight = (ZMC::MCSP_storm_speed_pref - p_below) / (p_above - p_below);
-
-      storm_u = state_u(k_below + 1) + weight * (state_u(k_below) - state_u(k_below + 1));
-      storm_v = state_v(k_below + 1) + weight * (state_v(k_below) - state_v(k_below + 1));
-    } else {
-      // Target pressure is below all model levels or exact match at lowest level
+    if (state_pmid(0) >= ZMC::MCSP_storm_speed_pref) {
+      storm_u = state_u(0);
+    }
+    else if (state_pmid(pver - 1) < ZMC::MCSP_storm_speed_pref) {
       storm_u = state_u(pver - 1);
-      storm_v = state_v(pver - 1);
+    }
+    else {
+      const Real dpu = ZMC::MCSP_storm_speed_pref - state_pmid(k_below);
+      const Real dpl = state_pmid(k_below+1) - ZMC::MCSP_storm_speed_pref;
+      storm_u = (state_u(k_below)*dpl + state_u(k_below+1)*dpu) / (dpl + dpu);
     }
 
     //----------------------------------------------------------------------------
     // calculate low-level shear
     if (state_pmid(pver - 1) > ZMC::MCSP_storm_speed_pref) {
-      storm_u_shear = storm_u - state_u(pver - 1);
-      storm_v_shear = storm_v - state_v(pver - 1);
+      mcsp_shear = storm_u - state_u(pver - 1);
     } else {
-      storm_u_shear = -999.0;
-      storm_v_shear = -999.0;
+      mcsp_shear = -999.0;
     }
-    mcsp_shear = storm_u_shear;
   });
 }
 
