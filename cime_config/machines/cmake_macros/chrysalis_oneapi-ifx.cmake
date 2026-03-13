41da8705d942c8fa9f39a030fa8d8b2d611abd01
Azamat Mametjanov <azamat@mcs.anl.gov>
2026-03-13 15:21:41 -0500

    Fix ICE with oneapi-ifx v2025.2.0 on Chrysalis

--- cime_config/machines/cmake_macros/chrysalis_oneapi-ifx.cmake
+++ cime_config/machines/cmake_macros/chrysalis_oneapi-ifx.cmake
@@ -1,3 +1,6 @@
 
 string(APPEND CMAKE_EXE_LINKER_FLAGS " -L/gpfs/fs1/soft/chrysalis/spack-latest/opt/spack/linux-rhel8-x86_64/gcc-8.5.0/gcc-11.3.0-jkpmtgq/lib64 -lstdc++")
 
+# -mllvm -disable-hir-temp-cleanup for oneapi v2025.2.0 https://github.com/argonne-lcf/AuroraBugTracking/issues/64
+string(APPEND CMAKE_Fortran_FLAGS " -mllvm -disable-hir-temp-cleanup")
+
