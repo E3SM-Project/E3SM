ed77c81f8cb259af7a030cc3bf259ad512e92649
noel <ndkeen@lbl.gov>
2026-03-25 19:47:30 -0700

    get HOMMEBFB test passing on pm-cpu with gnu by adjusting flags add a manual approach to detecting compiler fix minor bug in prim remove old perlmutter cmake files use soft-links for alvarez/muller

--- components/homme/cmake/machineFiles/pm-cpu.cmake
+++ components/homme/cmake/machineFiles/pm-cpu.cmake
@@ -2,13 +2,47 @@
 #
 # This machine file works with either Intel or gnu
 # (selected by which modules are loaded)
-# 
+#
 #
 # Perlmutter generic MPI enabled compiler wrappers:
 SET (CMAKE_Fortran_COMPILER ftn   CACHE FILEPATH "")
 SET (CMAKE_C_COMPILER       cc    CACHE FILEPATH "")
 SET (CMAKE_CXX_COMPILER     CC    CACHE FILEPATH "")
 
+# For some reason, cmake does not yet know CMAKE_Fortran_COMPILER_ID here, so use a manual approach until can
+# fix more elegantly
+#project(TestProject LANGUAGES Fortran)
+#enable_language(Fortran)
+#MESSAGE(STATUS "Fortran Compiler: ${CMAKE_Fortran_COMPILER}")
+#MESSAGE(STATUS "Fortran Compiler ID: ${CMAKE_Fortran_COMPILER_ID}")
+
+# Manually probe the compiler before project()
+execute_process(
+    COMMAND ftn --version
+    OUTPUT_VARIABLE FTN_VERSION_RAW
+    ERROR_VARIABLE  FTN_VERSION_RAW
+    RESULT_VARIABLE FTN_PROBE_RESULT
+)
+
+# Extract the first line for parsing
+string(REGEX MATCH "^[^\n]*" FTN_VERSION_SUMMARY "${FTN_VERSION_RAW}")
+
+# 1. Detection Logic for the "Big Three" on NERSC
+if(FTN_VERSION_SUMMARY MATCHES "ifx|ifort|Intel")
+    set(MY_COMPILER_TYPE "Intel" CACHE STRING "Manual Compiler Identification")
+    message(STATUS "Detected Intel Compiler (oneAPI/Classic)")
+
+elseif(FTN_VERSION_SUMMARY MATCHES "gfortran|GNU")
+    set(MY_COMPILER_TYPE "GNU" CACHE STRING "Manual Compiler Identification")
+    message(STATUS "Detected GNU Compiler (gfortran)")
+
+elseif(FTN_VERSION_SUMMARY MATCHES "nvfortran|NVIDIA|pgf90|PGI")
+    set(MY_COMPILER_TYPE "NVHPC" CACHE STRING "Manual Compiler Identification")
+    message(STATUS "Detected NVIDIA/NVHPC Compiler")
+else()
+    set(MY_COMPILER_TYPE "Unknown" CACHE STRING "Manual Compiler Identification")
+    message(WARNING "Could not identify compiler from: ${FTN_VERSION_SUMMARY}")
+endif()
 
 # Set kokkos arch, to get correct avx flags
 SET (Kokkos_ARCH_ZEN3 ON CACHE BOOL "")
@@ -35,15 +69,21 @@ SET (USE_QUEUING FALSE CACHE BOOL "")
 # for standalone HOMME builds:
 SET(CPRNC_DIR /global/cfs/cdirs/e3sm/tools/cprnc CACHE FILEPATH "")
 
-SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
-IF(DEFINED ENV{MKLROOT})
-  SET (HOMME_USE_MKL "TRUE" CACHE FILEPATH "")
-  # turn on additional intel compiler flags
-  SET (ADD_Fortran_FLAGS "-traceback" CACHE STRING "")
-  SET (ADD_C_FLAGS       "-traceback" CACHE STRING "")
-  SET (ADD_CXX_FLAGS     "-traceback" CACHE STRING "")
-ENDIF()
+SET (HOMMEXX_BFB_TESTING      FALSE   CACHE BOOL "")
+SET (BUILD_HOMME_PREQX_KOKKOS FALSE   CACHE BOOL "")
+SET (BUILD_HOMME_THETA_KOKKOS FALSE   CACHE BOOL "")
+SET (HOMME_TESTING_PROFILE    "short" CACHE STRING "")
 
+SET (HOMME_FIND_BLASLAPACK TRUE CACHE BOOL "")
+
+IF (MY_COMPILER_TYPE MATCHES "Intel")
+    # Works for both 'Intel' (ifort) and 'IntelLLVM' (ifx)
+    SET (ADD_Fortran_FLAGS "-traceback" CACHE STRING "")
+
+    IF(DEFINED ENV{MKLROOT})
+      SET (HOMME_USE_MKL TRUE CACHE BOOL "")
+    ENDIF()
+ENDIF()
 
 SET(USE_MPIEXEC "srun" CACHE STRING "")
 SET(USE_MPI_OPTIONS "-K --cpu_bind=cores" CACHE STRING "")
