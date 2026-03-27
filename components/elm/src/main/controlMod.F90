607b8855052b44d9cfe9e181a947e9421303ae33
Peter Schwartz <schwartzpd@ornl.gov>
2026-03-27 12:22:31 -0400

    ensure onset_gdd_extension is available to controlmod

--- components/elm/src/main/controlMod.F90
+++ components/elm/src/main/controlMod.F90
@@ -55,6 +55,7 @@ module controlMod
   use elm_varctl              , only: const_climate_hist
   use elm_varctl              , only: use_top_solar_rad
   use elm_varctl              , only: snow_shape, snicar_atm_type, use_dust_snow_internal_mixing
+  use elm_varctl              , only: onset_gdd_extension
   use EcosystemBalanceCheckMod, only: bgc_balance_check_tolerance => balance_check_tolerance
 
   !
