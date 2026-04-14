1c3d6fda4e2fdf51876f09f91101d6b8be53f464
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2026-04-14 14:10:09 -0400

    remove mentions of ghci-oci machine

--- components/eamxx/scripts/machines_specs.py
+++ components/eamxx/scripts/machines_specs.py
@@ -294,15 +294,6 @@ class GHCISNLCuda(Machine):
         cls.num_run_res = int(run_cmd_no_fail("nvidia-smi --query-gpu=name --format=csv,noheader | wc -l"))
 
 ###############################################################################
-class GHCIOCI(Machine):
-###############################################################################
-    concrete = True
-    @classmethod
-    def setup(cls):
-        super().setup_base(name="ghci-oci")
-        cls.env_setup = [f"eval $({CIMEROOT}/CIME/Tools/get_case_env -c SMS.ne4pg2_ne4pg2.F2010-SCREAMv1.ghci-oci_gnu)"]
-
-###############################################################################
 class Lassen(Machine):
 ###############################################################################
     concrete = True
