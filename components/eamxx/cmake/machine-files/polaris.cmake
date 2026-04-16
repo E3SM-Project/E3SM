19e5c60116739dad4e754550a09ee5190a9a8ccb
Oscar H. Diaz-Ibarra <odiazib@sandia.gov>
2026-04-16 14:58:29 -0700

    save for polaris

--- components/eamxx/cmake/machine-files/polaris.cmake
+++ components/eamxx/cmake/machine-files/polaris.cmake
@@ -14,3 +14,20 @@ include (${EKAT_MACH_FILES_PATH}/mpi/other.cmake)
 set(EKAT_MPIRUN_EXE "mpiexec" CACHE STRING "" FORCE)
 set(EKAT_MPI_NP_FLAG "-np" CACHE STRING "" FORCE)
 set(EKAT_MPI_EXTRA_ARGS "--label --cpu-bind core -d 8" CACHE STRING "")
+
+# Compiler wrappers (Cray PE)
+set(SCC "cc" CACHE STRING "" FORCE)
+set(SCXX "CC" CACHE STRING "" FORCE)
+set(SFC "ftn" CACHE STRING "" FORCE)
+
+# Common flags (from gnugpu.cmake)
+set(CMAKE_C_FLAGS " -mcmodel=medium" CACHE STRING "" FORCE)
+set(CMAKE_Fortran_FLAGS " -mcmodel=medium -fconvert=big-endian -ffree-line-length-none -ffixed-line-length-none -fallow-argument-mismatch" CACHE STRING "" FORCE)
+set(CMAKE_Fortran_FLAGS_RELEASE "-O2" CACHE STRING "" FORCE)
+set(CMAKE_Fortran_FLAGS_DEBUG   "-O0 -g" CACHE STRING "" FORCE)
+
+if (USE_CUDA)
+  # From polaris_gnugpu.cmake
+  set(CMAKE_CUDA_FLAGS "-ccbin CC -O2 -arch sm_80 --use_fast_math" CACHE STRING "" FORCE)
+  set(CMAKE_EXE_LINKER_FLAGS " -L$ENV{CUDA_HOME}/lib64/ -lstdc++" CACHE STRING "" FORCE)
+endif()
