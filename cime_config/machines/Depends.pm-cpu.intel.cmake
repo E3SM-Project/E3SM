920e5b7112955cfcfed1f08af1d31ac8268ad04a
noel <ndkeen@lbl.gov>
2026-04-21 15:53:31 -0700

    lower compiler opt on quickbeam_optics.F90 for intel and pm-cpu to avoid MMF cosp issue

--- cime_config/machines/Depends.pm-cpu.intel.cmake
+++ cime_config/machines/Depends.pm-cpu.intel.cmake
@@ -1,4 +1,4 @@
-# https://github.com/E3SM-Project/E3SM/issues/8036
+# https://github.com/E3SM-Project/E3SM/issues/8140
 if (DEBUG)
   if (CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM" AND
       CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL "2025.0")
@@ -6,6 +6,11 @@ if (DEBUG)
   endif()
 endif()
 
-
-
-
+# https://github.com/E3SM-Project/E3SM/issues/8036
+if (NOT DEBUG)
+  foreach(ITEM IN LISTS REDUCEDOPT)
+    if (CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM" AND
+      CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL "2025.0")
+    e3sm_add_flags("eam/src/physics/cosp2/optics/quickbeam_optics.F90" "-O1")
+  endif()
+endif()
