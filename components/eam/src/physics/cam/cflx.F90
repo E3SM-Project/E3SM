acfeaa4744db2d7baf2243a5534a9c05bdf40bc7
Sha Feng <sfeng@pnnl.gov>
2026-05-19 17:02:48 -0700

    eam: fix CO2 duplication with cflx_cpl_opt==2 (GH #8201)
    With cflx_cpl_opt==2, cflx_tend() was called in tphysbc (cam_run1),
    which is invoked multiple times during initialization, causing CO2
    surface fluxes (CO2_OCN, CO2_FFF, CO2_LND, CO2) to be applied
    redundantly and adding duplicate mass to the atmosphere.
    
    Fix (following @huiwanpnnl): split cflx_tend into two paths:
    - tphysbc (cam_run1): apply aerosol fluxes only (skip_co2=.true.)
    - tphysac (cam_run2): apply CO2 fluxes only (co2_only=.true.)
    
    This preserves the aerosol numerical coupling improvement of
    cflx_cpl_opt==2 while moving CO2 tracers back to cam_run2 to
    avoid the redundant flux application during the init sequence.
    
    Changes:
    - cflx.F90: add optional skip_co2 and co2_only arguments to
      cflx_tend(); build CO2 mask from c_i array to identify CO2
      tracers (CO2_OCN, CO2_FFF, CO2_LND, CO2)
    - physpkg.F90: call cflx_tend with skip_co2=.true. in tphysbc;
      add elseif branch in tphysac to call cflx_tend with co2_only=.true.
      when cflx_cpl_opt==2; make get_carbon_sfc_fluxes unconditional
      in tphysac

--- components/eam/src/physics/cam/cflx.F90
+++ components/eam/src/physics/cam/cflx.F90
@@ -12,7 +12,7 @@ module cflx
 
 contains
 
-    subroutine cflx_tend (state, cam_in, ztodt, ptend)
+    subroutine cflx_tend (state, cam_in, ztodt, ptend, skip_co2, co2_only)
 
     use physics_types,          only: physics_state, physics_ptend, &
                                       physics_ptend_init, &
@@ -20,7 +20,7 @@ contains
     use physconst,              only: gravit
     use ppgrid,                 only: pver, pcols
     use constituents,           only: pcnst, cnst_get_ind, cnst_type
-    use co2_cycle,              only: co2_cycle_set_cnst_type
+    use co2_cycle,              only: co2_cycle_set_cnst_type, co2_transport, c_i
     use camsrfexch,             only: cam_in_t
 
     implicit none
@@ -30,6 +30,8 @@ contains
     type(physics_state), intent(inout)  :: state                ! Physics state variables
     type(cam_in_t),      intent(in)     :: cam_in               ! contains surface fluxes of constituents
     real(r8),            intent(in)     :: ztodt                ! 2 delta-t        [ s ]
+    logical,             intent(in), optional :: skip_co2        ! if .true., skip CO2 tracers (apply all others)
+    logical,             intent(in), optional :: co2_only        ! if .true., apply CO2 tracers only
 
     ! Output Auguments
 
@@ -42,14 +44,30 @@ contains
 
     real(r8) :: tmp1(pcols)
     real(r8) :: rztodt                                          ! 1./ztodt
-    integer  :: m
+    integer  :: m, k
 
     logical  :: lq(pcnst)
+    logical  :: l_skip_co2, l_co2_only
+    logical  :: co2_mask(pcnst)                                 ! .true. for CO2 tracer indices
 
     character(len=3), dimension(pcnst) :: cnst_type_loc         ! local override option for constituents cnst_type
 
 
     ncol = state%ncol
+
+    ! Process optional arguments
+    l_skip_co2 = .false.
+    if (present(skip_co2)) l_skip_co2 = skip_co2
+    l_co2_only = .false.
+    if (present(co2_only)) l_co2_only = co2_only
+
+    ! Build a mask identifying CO2 tracer indices
+    co2_mask(:) = .false.
+    if (co2_transport()) then
+       do k = 1, size(c_i)
+          if (c_i(k) >= 1 .and. c_i(k) <= pcnst) co2_mask(c_i(k)) = .true.
+       end do
+    end if
 
     !-------------------------------------------------------
     ! Assume 'wet' mixing ratios in surface diffusion code.
@@ -60,9 +78,17 @@ contains
     call set_dry_to_wet(state, cnst_type_loc)
 
     !-------------------------------------------------------
-    ! Initialize ptend
+    ! Initialize ptend with appropriate tracer mask
+    ! skip_co2=.true.: apply all tracers except CO2 (used by tphysbc when cflx_cpl_opt==2)
+    ! co2_only=.true.: apply CO2 tracers only (used by tphysac when cflx_cpl_opt==2)
+    ! default (both .false.): apply all tracers
 
-    lq(:) = .TRUE.
+    if (l_co2_only) then
+       lq(:) = co2_mask(:)
+    else
+       lq(:) = .TRUE.
+       if (l_skip_co2) lq(:) = lq(:) .and. (.not. co2_mask(:))
+    end if
     call physics_ptend_init(ptend, state%psetcols, 'clubb_srf', lq=lq)
 
     !-------------------------------------------------------
@@ -73,6 +99,7 @@ contains
     tmp1(:ncol)            = ztodt * gravit * state%rpdel(:ncol,pver)
 
     do m = 2, pcnst
+      if (.not. lq(m)) cycle
       ptend%q(:ncol,pver,m) = ptend%q(:ncol,pver,m) + tmp1(:ncol) * cam_in%cflx(:ncol,m)
     enddo
 
