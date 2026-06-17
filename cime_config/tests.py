16b4324d9fe3ffc5033f5a6ce5174f933c0027b2
James Foucar <jgfouca@sandia.gov>
2026-06-17 14:00:15 -0600

    Turn off batched build for mappy now that i've tested it. update tests.py docs

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -4,7 +4,7 @@
 # suite_name : {
 #     "inherit" : (suite1, suite2, ...), # Optional. Suites to inherit tests from. Default is None. Tuple, list, or str.
 #     "time"    : "HH:MM:SS",            # Optional. Recommended upper-limit on test time.
-#     "share"   : True|False,            # Optional. If True, all tests in this suite share a build. Default is False.
+#     "share"   : True|False|"testname", # Optional. If True, all tests share a build (first in suite order leads). If a test name string, that test's build is shared. Default is False.
 #     "tests"   : (test1, test2, ...)    # Optional. The list of tests for this suite. See above for format. Tuple, list, or str.
 # }
 
