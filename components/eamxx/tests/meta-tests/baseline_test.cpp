61939a554982dc9db9c0ebb2c61afad86b9fcee3
James Foucar <jgfouca@sandia.gov>
2026-06-29 13:04:19 -0600

    Make a baseline test

---
+++ components/eamxx/tests/meta-tests/baseline_test.cpp
@@ -0,0 +1,126 @@
+#include "share/util/eamxx_utils.hpp"
+#include "share/core/eamxx_session.hpp"
+#include "share/core/eamxx_types.hpp"
+
+#include <string>
+#include <fstream>
+
+namespace scream {
+namespace physics {
+namespace unit_test {
+
+struct FakeBaselineTest
+{
+  FakeBaselineTest () {}
+
+  Int generate_baseline (const std::string& filename) {
+    std::ofstream ofile(filename,std::ios::binary);
+    EKAT_REQUIRE_MSG( ofile.good(), "generate_baseline can't write " + filename + "\n");
+
+
+    Real result = 42;
+    impl::write_scalars(ofile, result);
+
+    return 0;
+  }
+
+  Int run_and_cmp (const std::string& filename, const Scalar& tol, bool no_baseline) {
+    std::ifstream ifile;
+    if (!no_baseline) {
+      ifile.open(filename,std::ios::binary);
+      EKAT_REQUIRE_MSG( ifile.good(), "run_and_cmp can't read " + filename + "\n");
+    }
+
+    Real result = 42;
+
+    if (!no_baseline) {
+      Real ref;
+      impl::read_scalars(ifile, ref);
+      if (ref != result) {
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+};
+
+}
+}
+}
+
+namespace {
+
+void expect_another_arg (int i, int argc) {
+  EKAT_REQUIRE_MSG(i != argc-1, "Expected another cmd-line arg.");
+}
+
+} // empty namespace
+
+int main (int argc, char** argv) {
+  using UnitTest = scream::physics::unit_test::UnitWrap::UnitTest<scream::DefaultDevice>;
+  using FakeBaselineTest = UnitTest::FakeBaselineTest;
+
+  int nerr = 0;
+
+  if (argc == 1) {
+    std::cout <<
+      argv[0] << " [options]\n"
+      "Options:\n"
+      "  -g                  Generate baseline file. Default False.\n"
+      "  -c                  Compare baseline file. Default False.\n"
+      "  -n                  Run without baseline actions. Default True.\n"
+      "  -b <baseline_path>  Path to directory containing baselines.\n"
+      "  -t <tol>            Tolerance for relative error. Default machine eps (*10000 for Release).\n";
+    return 1;
+  }
+
+  // Set up options with defaults
+  bool generate = false, no_baseline = true;
+  scream::Real tol = UnitTest::C::macheps
+#ifdef NDEBUG
+      * 10000
+#endif
+    ;
+  std::string baseline_fn;
+
+  // Parse options
+  for (int i = 1; i < argc-1; ++i) {
+    if (ekat::argv_matches(argv[i], "-g", "--generate")) {generate = true; no_baseline = false;}
+    if (ekat::argv_matches(argv[i], "-c", "--compare"))  {no_baseline = false;}
+    if (ekat::argv_matches(argv[i], "-t", "--tol")) {
+      expect_another_arg(i, argc);
+      ++i;
+      tol = std::atof(argv[i]);
+    }
+    if (ekat::argv_matches(argv[i], "-b", "--baseline-file")) {
+      expect_another_arg(i, argc);
+      ++i;
+      baseline_fn = argv[i];
+    }
+  }
+
+  // Compute full baseline file name with precision.
+  baseline_fn += "/baseline_test.baseline" + std::to_string(sizeof(scream::Real));
+
+  std::vector<char*> args;
+  for (int i=0; i<argc; ++i) {
+    args.push_back(argv[i]);
+  }
+
+  scream::initialize_eamxx_session(args.size(), args.data()); {
+    FakeBaselineTest bln;
+    if (generate) {
+      std::cout << "Generating to " << baseline_fn << "\n";
+      nerr += bln.generate_baseline(baseline_fn);
+    } else if (no_baseline) {
+      printf("Running with no baseline actions\n");
+      nerr += bln.run_and_cmp(baseline_fn, tol, no_baseline);
+    } else {
+      printf("Comparing with %s at tol %1.1e\n", baseline_fn.c_str(), tol);
+      nerr += bln.run_and_cmp(baseline_fn, tol, no_baseline);
+    }
+  } scream::finalize_eamxx_session();
+
+  return nerr != 0 ? 1 : 0;
+}
