0d7f763a400cf42d491f50760919eeb90c8cdf6c
Conrad Clevenger <tccleve@sandia.gov>
2026-07-02 13:03:57 -0400

    Add sanitizer workaround for debug builds

--- cime_config/machines/cmake_macros/chrysalis_intel.cmake
+++ cime_config/machines/cmake_macros/chrysalis_intel.cmake
@@ -1,11 +1,16 @@
 string(APPEND CMAKE_EXE_LINKER_FLAGS " -L/gpfs/fs1/soft/chrysalis/spack-latest/opt/spack/linux-rhel8-x86_64/gcc-8.5.0/gcc-11.3.0-jkpmtgq/lib64 -lstdc++")
 
-# Workaround for oneapi ifx v2025.2.0 (and earlier than 2025.3.0):
-# use -mllvm -disable-hir-temp-cleanup to avoid ICE
-# See: https://github.com/argonne-lcf/AuroraBugTracking/issues/64
-if (CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM"
-    AND CMAKE_Fortran_COMPILER_VERSION VERSION_LESS "2025.3.0")
-  string(APPEND CMAKE_Fortran_FLAGS " -mllvm -disable-hir-temp-cleanup")
+if (CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM")
+  if (CMAKE_Fortran_COMPILER_VERSION VERSION_LESS "2025.3.0")
+    # Workaround for oneapi ifx v2025.2.0 (and earlier than 2025.3.0):
+    # use -mllvm -disable-hir-temp-cleanup to avoid ICE
+    # See: https://github.com/argonne-lcf/AuroraBugTracking/issues/64
+    string(APPEND CMAKE_Fortran_FLAGS " -mllvm -disable-hir-temp-cleanup")
+  endif()
+  if (CMAKE_Fortran_COMPILER_VERSION VERSION_GREATER_EQUAL "2025.0")
+      # Sanitization workaround for Intel 2025.0 and later
+      string(APPEND CMAKE_Fortran_FLAGS_DEBUG " -check nouninit")
+  endif()
 endif()
 
 # Same hack as pm-cpu_intel. For whatever reason, the intel version on chrysalis
