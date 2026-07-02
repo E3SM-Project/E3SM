c7699c55ddfdbf2647717bbbd513652721745953
Walter Hannah <whannah1@gmail.com>
2026-07-02 14:28:31 -0600

    Add options for minimum and maximum land fraction

--- tools/generate_domain_files/generate_domain_files_E3SM.py
+++ tools/generate_domain_files/generate_domain_files_E3SM.py
@@ -94,10 +94,12 @@ parser.add_option('--date-stamp',
                   help='Creation date stamp for domain files')
 parser.add_option('--fminval',
                   dest='fminval',
+                  type='float',
                   default=1e-8,
                   help='Minimum allowable land fraction (reset to 0 below fminval)')
 parser.add_option('--fmaxval',
                   dest='fmaxval',
+                  type='float',
                   default=1,
                   help='Maximum allowable land fraction (reset to 1 above fmaxval)')
 parser.add_option('--set-omask',
