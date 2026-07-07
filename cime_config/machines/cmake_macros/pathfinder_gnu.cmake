1d804c2bfd3f5024601ac96d23cb611a8ce49378
Fengming Yuan <fmyuan@gmail.com>
2026-07-07 10:49:39 -0400

    In case libmoab.so may not be found, explicitly add its path.
    (cherry picked from commit d7a05b73687e07a363b6c8f02f09204b39c852a7)

--- cime_config/machines/cmake_macros/pathfinder_gnu.cmake
+++ cime_config/machines/cmake_macros/pathfinder_gnu.cmake
@@ -1,3 +1,10 @@
 set(SUPPORTS_CXX "TRUE")
 #set(PIO_FILESYSTEM_HINTS "nfs") # need a check
 string(APPEND CMAKE_Fortran_FLAGS " -fallow-argument-mismatch")
+#sometimes cannot find libMoab.so on pathfinder
+set(MOAB_ROOT /projects/hpcl-cli185/proj-shared/ccsi-apps/moab)
+if (COMP_INTERFACE STREQUAL "moab")
+  if (COMP_NAME STREQUAL "cpl")
+    string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,-rpath,${MOAB_ROOT}/lib -L${MOAB_ROOT}/lib")
+  endif()
+endif()
