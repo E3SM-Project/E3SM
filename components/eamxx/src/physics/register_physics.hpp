5a4ef23235c30279eeae8ab023410a996a122deb
Rich Fiorella <rfiorella@lanl.gov>
2026-07-08 15:43:30 -0600

    Restructure water tracers/isotopes into separate libraries
    Move water tracer and isotope processes from physics/water_tracers/
    to physics/aux_tracers/{water_tracers,water_isotopes}/ with separate
    libraries per process. Water isotopes depend on and extend water tracers.
    
    - Add aux_tracers/CMakeLists.txt with EAMXX_ENABLE_WATER_TRACERS and
      EAMXX_ENABLE_WATER_ISOTOPES options (both default OFF)
    - Enforce dependency: isotopes require tracers
    - Create separate water_tracers and water_isotopes libraries
    - Update register_physics.hpp include paths
    - Use full include path in water_isotopes header for robustness

--- components/eamxx/src/physics/register_physics.hpp
+++ components/eamxx/src/physics/register_physics.hpp
@@ -51,10 +51,10 @@
 #include "physics/cld_fraction/cld_frac_net/eamxx_cld_frac_net_process_interface.hpp"
 #endif
 #ifdef EAMXX_HAS_WATER_TRACERS
-#include "physics/water_tracers/eamxx_water_tracers_process_interface.hpp"
+#include "physics/aux_tracers/water_tracers/eamxx_water_tracers_process_interface.hpp"
 #endif
 #ifdef EAMXX_HAS_WATER_ISOTOPES
-#include "physics/water_tracers/eamxx_water_isotopes_process_interface.hpp"
+#include "physics/aux_tracers/water_isotopes/eamxx_water_isotopes_process_interface.hpp"
 #endif
 
 namespace scream {
