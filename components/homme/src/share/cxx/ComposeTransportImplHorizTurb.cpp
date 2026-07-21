7bfe30b5b95ba80e5c42927baa5a823b9ffdcda2
Peter Bogenschutz <bogenschutz1@login04.frontier.olcf.ornl.gov>
2026-07-20 20:36:26 -0400

    do not operate horizontal diffusion for tracers in the sponge layer

--- components/homme/src/share/cxx/ComposeTransportImplHorizTurb.cpp
+++ components/homme/src/share/cxx/ComposeTransportImplHorizTurb.cpp
@@ -43,6 +43,15 @@ Real get_local_laplace_metric_ct (const Real a, const Real b, const Real c, cons
   return lambda_vis * (scale_factor_inv * norm_dinv) * (scale_factor_inv * norm_dinv);
 }
 
+KOKKOS_INLINE_FUNCTION
+bool is_tom_sponge_level_ct (const Real eta_top, const Real eta_mid)
+{
+  const Real ptop_over_press = eta_top / eta_mid;
+  const Real ptop_over_press_sq = ptop_over_press * ptop_over_press;
+  const Real nu_scale_top = 16.0 * ptop_over_press_sq / (ptop_over_press_sq + 1.0);
+  return nu_scale_top >= 0.15;
+}
+
 } // namespace
 
 void ComposeTransportImpl::advance_horizontal_turbulent_diffusion_scalar (const Real dt_q) {
@@ -57,6 +66,20 @@ void ComposeTransportImpl::advance_horizontal_turbulent_diffusion_scalar (const 
   const Real lambda_vis = get_lambda_vis_ct();
   const auto tu_ne_hv_q = m_tu_ne_hv_q;
   const auto sphere_ops = m_sphere_ops;
+  const bool tom_sponge_active = Context::singleton().get<SimulationParams>().nu_top > 0;
+  int num_tom_sponge_levels = 0;
+  if (tom_sponge_active) {
+    const auto etam_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), m_hvcoord.etam);
+    const auto etai_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), m_hvcoord.etai);
+    const Real eta_top = etai_h(0) == 0.0 ? etam_h(0)[0] : etai_h(0);
+    for (int phys_lev = 0; phys_lev < NUM_PHYSICAL_LEV; ++phys_lev) {
+      const int lev = phys_lev / VECTOR_SIZE;
+      const int s = phys_lev % VECTOR_SIZE;
+      if (is_tom_sponge_level_ct(eta_top, etam_h(lev)[s])) {
+        num_tom_sponge_levels = phys_lev + 1;
+      }
+    }
+  }
 
   if (print_tracer_sgs_diffusivity_clipping && lambda_vis > 0) {
     const auto kh_h = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), Kh);
@@ -80,6 +103,7 @@ void ComposeTransportImpl::advance_horizontal_turbulent_diffusion_scalar (const 
             for (int s = 0; s < VECTOR_SIZE; ++s) {
               const int phys_lev = lev * VECTOR_SIZE + s;
               if (phys_lev >= NUM_PHYSICAL_LEV) continue;
+              if (phys_lev < num_tom_sponge_levels) continue;
               if (kh[s] > max_diffusivity) {
                 printf("Warning: rank %d clipped tracer SGS Kh at ie=%d igp=%d jgp=%d lev=%d from %.16e to %.16e.\n",
                        rank, ie, i, j, phys_lev, kh[s], max_diffusivity);
@@ -132,14 +156,23 @@ void ComposeTransportImpl::advance_horizontal_turbulent_diffusion_scalar (const 
             const Real max_diffusivity = 2.0 * tracer_sgs_cfl_target / (dt * laplace_metric);
             for (int s = 0; s < VECTOR_SIZE; ++s) {
               const int phys_lev = lev * VECTOR_SIZE + s;
-              if (phys_lev < NUM_PHYSICAL_LEV && kh_eff[s] > max_diffusivity) {
+              if (phys_lev >= num_tom_sponge_levels &&
+                  phys_lev < NUM_PHYSICAL_LEV && kh_eff[s] > max_diffusivity) {
                 kh_eff[s] = max_diffusivity;
               }
             }
           }
         }
-        Q(ie,q,i,j,lev) = (Q(ie,q,i,j,lev) * spheremp(ie,i,j)
-                           - dt * kh_eff * Qtens(ie,q,i,j,lev));
+        auto q_new = Q(ie,q,i,j,lev) * spheremp(ie,i,j);
+        for (int s = 0; s < VECTOR_SIZE; ++s) {
+          const int phys_lev = lev * VECTOR_SIZE + s;
+          if (phys_lev < NUM_PHYSICAL_LEV &&
+              phys_lev >= num_tom_sponge_levels) {
+            q_new[s] = (Q(ie,q,i,j,lev)[s] * spheremp(ie,i,j)
+                        - dt * kh_eff[s] * Qtens(ie,q,i,j,lev)[s]);
+          }
+        }
+        Q(ie,q,i,j,lev) = q_new;
       };
       launch_ie_q_ij_nlev<num_lev_pack>(hv_q, f);
     }
