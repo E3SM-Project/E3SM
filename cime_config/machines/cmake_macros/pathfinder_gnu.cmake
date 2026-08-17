07688137f0ab41a7e2393da3da2569e6b569cc34
Fengming Yuan <fmyuan@gmail.com>
2026-08-17 14:18:33 -0400

    On ORNL ITSD cluster Pathfinder, when in Debug mode, mpi_init(ierr) throw a seg. fault. Removal of ' -ffpe-trap=invalid' will fix it.

--- cime_config/machines/cmake_macros/pathfinder_gnu.cmake
+++ cime_config/machines/cmake_macros/pathfinder_gnu.cmake
@@ -1,5 +1,7 @@
 set(SUPPORTS_CXX "TRUE")
 #set(PIO_FILESYSTEM_HINTS "nfs") # need a check
+#the following will override 'CMAKE_Fortran_FLAGS_DEBUG' from gnu.cmake, in which '-ffpt-trap=invalid' has seg. fault of 'mpi_init(ierr)' on pathfinder. A possible reason may be 'ierr' is 64-bit but should be 32-bit.
+set(CMAKE_Fortran_FLAGS_DEBUG " -g -Wall -fbacktrace -fcheck=bounds,pointer -ffpe-trap=zero,overflow")
 string(APPEND CMAKE_Fortran_FLAGS " -fallow-argument-mismatch")
 #sometimes cannot find libMoab.so on pathfinder
 set(MOAB_ROOT /projects/hpcl-cli185/proj-shared/ccsi-apps/moab)
