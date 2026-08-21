b4cfefea6e7d8b0c56f4cad0e74fa123e5081cc6
Luca Bertagna <lbertag@sandia.gov>
2026-08-21 17:21:32 -0600

    Machines: disable fortran linking on aurora/sunspot intelgpu machines

--- cime_config/machines/cmake_macros/aurora_intelgpu.cmake
+++ cime_config/machines/cmake_macros/aurora_intelgpu.cmake
@@ -14,3 +14,6 @@ string(APPEND OMEGA_SYCL_EXE_LINKER_FLAGS " -Xsycl-target-backend \"-device pvc\
 set(SCREAM_MPI_ON_DEVICE ON CACHE STRING "")
 
 set(USE_SYCL "TRUE")
+
+# Override the value TRUE set by intelgpu.cmake (via intel.cmake)
+set(E3SM_LINK_WITH_FORTRAN "FALSE")
