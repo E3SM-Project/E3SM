c3e8e246676e0a41f6df0d2dd15b6e4db5faf953
Peter Bogenschutz <bogenschutz1@login09.frontier.olcf.ornl.gov>
2026-09-12 18:04:00 -0400

    Fix MOAB coupling for multi-column doubly periodic EAMxx point clouds

--- driver-moab/main/prep_aoflux_mod.F90
+++ driver-moab/main/prep_aoflux_mod.F90
@@ -9,7 +9,7 @@ module prep_aoflux_mod
   use seq_comm_mct,     only: CPLID, logunit
   use seq_comm_mct,     only : mbofxid ! iMOAB id for mpas ocean migrated mesh to coupler pes, just for xao flux calculations
   use seq_comm_mct,     only : mbaxid ! iMOAB app id for atm on cpl pes
-  use seq_comm_mct,     only: atm_pg_active  ! whether the atm uses FV mesh or not ; made true if fv_nphys > 0
+  use seq_comm_mct,     only: atm_pg_active, mb_scm_atm
   use seq_comm_mct,     only: seq_comm_getData=>seq_comm_setptrs
   use seq_comm_mct, only : num_moab_exports
   use seq_comm_mct, only : mb_dead_comps
@@ -204,7 +204,7 @@ contains
        ! find out the number of local elements in moab mesh
        ierr  = iMOAB_GetMeshInfo ( mbaxid, nvert, nvise, nbl, nsurf, nvisBC ); ! could be different of lsize_o
       ! local size of vertices is different from lsize_o
-       if(atm_pg_active .or. mb_dead_comps) then
+       if ((atm_pg_active .and. .not. mb_scm_atm) .or. mb_dead_comps) then
           arrSize = nvise(1) * size_list ! there are size_list tags that need to be zeroed out
           ent_type = 1 ! cell type now, not a point cloud anymore
        else
