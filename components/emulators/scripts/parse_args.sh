8b1f0042130db151bca17c0057df6fda904a90ef
mahf708 <naser.mahfouz@pnnl.gov>
2026-09-18 07:31:56 -0700

    Let the emulators test script take CMake options

--- components/emulators/scripts/parse_args.sh
+++ components/emulators/scripts/parse_args.sh
@@ -4,13 +4,14 @@
 #
 # Usage: source this file, then call parse_test_args "$@"
 # After calling, the following variables will be set:
-#   CLEAN_ONLY, BUILD_ONLY, VERBOSE
+#   CLEAN_ONLY, BUILD_ONLY, VERBOSE, CMAKE_ARGS
 #
 
 # Default values
 CLEAN_ONLY=false
 BUILD_ONLY=false
 VERBOSE=false
+CMAKE_ARGS=()
 
 parse_test_args() {
     while [[ $# -gt 0 ]]; do
@@ -27,6 +28,10 @@ parse_test_args() {
                 VERBOSE=true
                 shift
                 ;;
+            -D*)
+                CMAKE_ARGS+=("$1")
+                shift
+                ;;
             --help|-h)
                 echo "Usage: $0 [OPTIONS]"
                 echo ""
@@ -34,6 +39,8 @@ parse_test_args() {
                 echo "  --clean-only  Remove build directory and exit"
                 echo "  --build-only  Build without running tests"
                 echo "  --verbose     Show verbose test output"
+                echo "  -D<var>=<val> Pass an option to CMake, e.g."
+                echo "                -DEMULATOR_ENABLE_LIBTORCH=ON"
                 echo "  --help        Show this help message"
                 exit 0
                 ;;
